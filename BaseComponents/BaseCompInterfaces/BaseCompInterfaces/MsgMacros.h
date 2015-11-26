b905255a52c72ea61021a8c0c5304a5970fca208
jhrdinka <julia.hrdinka@cern.ch>
2015-11-26 13:18:07 +0100

    changed repository structure of BaseComponents

---
+++ BaseComponents/BaseCompInterfaces/BaseCompInterfaces/MsgMacros.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////
+// AlgToolBase.h, ATS project
+///////////////////////////////////////////////////////////////////
+
+#ifndef BASECOMPSINTERFACES_MSGMACROS_H
+#define BASECOMPSINTERFACES_MSGMACROS_H
+
+// Athena version
+#include "AthenaBaseComps/AthMsgStreamMacros.h"
+
+#define TRK_MSG_VERBOSE(x)  ATH_MSG_VERBOSE(x)
+#define TRK_MSG_DEBUG(x)    ATH_MSG_DEBUG(x) 
+#define TRK_MSG_INFO(x)     ATH_MSG_INFO(x)   
+#define TRK_MSG_WARNING(x)  ATH_MSG_WARNING(x)
+#define TRK_MSG_ERROR(x)    ATH_MSG_ERROR(x)  
+#define TRK_MSG_FATAL(x)    ATH_MSG_FATAL(x)  
+#define TRK_MSG_ALWAYS(x)   ATH_MSG_ALWAYS(x) 
+
+#endif
