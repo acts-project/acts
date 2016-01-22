6ead4e2c829fc1989d57a611e106b8d98064aa53
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2016-01-22 14:34:16 +0100

    working version of TrkExEnigne as Service

---
+++ Control/AtsBaseComponents/AtsBaseComponents/BaseMacros.h
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////
+// BaseMacros.h, ATS project
+///////////////////////////////////////////////////////////////////
+
+#ifndef ATS_BASECOMPSINTERFACES_MSGMACROS_H
+#define ATS_BASECOMPSINTERFACES_MSGMACROS_H 1
+
+// these macros simplify/harmonize the retrieval ot TOOLS/SERVICES
+#define RETRIEVE_FATAL(handle)                                                                          \
+        if (handle.empty()){                                                                            \
+            MSG_FATAL("<retrieve> Empty handle for crucial component provided. Arborting.");            \
+            return StatusCode::FAILURE;                                                                 \
+        } else if (handle.retrieve().isFailure()){                                                  \
+            MSG_FATAL("<retrieve> Failed to retrieve InterfaceHandle '" << handle << "'. Arborting.");   \
+            return StatusCode::FAILURE;                                                                 \
+        } else                                                                                          \
+            MSG_DEBUG("<retrieve> Successfully retrieved InterfaceHandle '" << handle <<"'.");                     
+
+// these macros simplify/harmonize the retrieval ot TOOLS/SERVICES
+#define RETRIEVE_NONEMPTY_FATAL(handle)                                                                            \
+        if (!handle.empty() && handle.retrieve().isFailure()){                                                 \
+            MSG_FATAL("<retrieve> Failed to retrieve non-empty InterfaceHandle '" << handle << "'. Arborting.");   \
+            return StatusCode::FAILURE;                                                                            \
+        } else if (!handle.empty())                                                                                \
+            MSG_DEBUG("<retrieve> Successfully retrieved InterfaceHandle '" << handle <<";.");                     \
+        else                                                                                                       \
+            MSG_DEBUG("<retrieve> Empty InterfaceHandle, no retrieval attempted.");                                
+
+#endif
