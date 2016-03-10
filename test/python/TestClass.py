4036a8e24b2e1d5fbf94a69a41d8b346a377cf2c
jhrdinka <julia.hrdinka@cern.ch>
2016-03-10 09:08:45 +0100

    added example for testing python modul retrieval

---
+++ test/python/TestClass.py
@@ -0,0 +1,5 @@
+class TestClass(object):
+   def __init__(self, name, output):
+	self._name = name
+	self._output = output
+	print "Initialising TestClass:", name, output
---
+++ testPyExample.py
@@ -0,0 +1,5 @@
+from Gaudi.Configuration import *
+
+from test.TestClass import TestClass
+
+test = TestClass("test", 0)
