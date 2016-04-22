05516975488b858aae7d05daa9d464a1d9cc367a
cgumpert <christian.gumpert@cern.ch>
2016-04-22 12:36:03 +0200

    remove unnecessary definition of stream operator

--- Core/include/ACTS/EventData/TrackParameters.hpp
+++ Core/include/ACTS/EventData/TrackParameters.hpp
@@ -21,8 +21,4 @@ namespace Acts
     typedef SingleBoundTrackParameters<ChargedPolicy> BoundParameters;
 } // end of namespace Acts
 
-
-/**Overload of << operator for both, MsgStream and std::ostream for debug output*/
-std::ostream& operator << ( std::ostream& sl, const Acts::TrackParameters& pars);
-
 #endif // ACTS_TRACKPARAMETERS_H
