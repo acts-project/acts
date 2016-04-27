2667858ff7d0f5bbe98fe929476658d917ece5c6
Christian Gumpert <christian.gumpert@cern.ch>
2016-04-27 16:20:07 +0200

    update CMake configuration
    - make ConfigPackageLocation visible to sub-directories
    - add_definitions does not properly propagate as target dependency
    --> using target_* to pick up those definition in dependent (down-
        stream) projects
    - add fix for generated cmake targets file which has problems with escaping quotes
    - explicitly specify target properties for proper dependency propagation

---
+++ cmake/fix-cmake-target-file.py
@@ -0,0 +1,22 @@
+import re
+import shutil
+
+def main(file_name,pattern):
+    f = open(file_name,'r')
+    fix_file_name = file_name + ".fix"
+    new = open(fix_file_name,"w")
+    lines = f.readlines()
+    for l in lines:
+        if re.search(pattern,l):
+            r = re.match('([^"]*")(.*)("[^"]*)',l)
+            inner = r.group(2)
+            inner = inner.replace('"','\\"')
+            l = r.group(1) + inner + r.group(3)
+        new.write(l)
+    new.close()
+    f.close()
+    shutil.move(fix_file_name,file_name)
+            
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1],"INTERFACE_COMPILE_DEFINITIONS")
