59f1cd419322676a7591d6c9b65f5d43b17005eb
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2016-04-28 11:34:51 +0200

    Add MSG_ERROR

--- Core/include/ACTS/Utilities/detail/DefaultMsgMacros.h
+++ Core/include/ACTS/Utilities/detail/DefaultMsgMacros.h
@@ -8,8 +8,9 @@ namespace Acts
 {
 #define MSG_VERBOSE(x) std::cout << "VERBOSE " << x << std::endl;
 #define MSG_DEBUG(x)   std::cout << "DEBUG   " << x << std::endl;
-#define MSG_WARNING(x) std::cout << "WARNING " << x << std::endl;
 #define MSG_INFO(x)    std::cout << "INFO    " << x << std::endl;
+#define MSG_ERROR(x)   std::cout << "ERROR   " << x << std::endl;
+#define MSG_WARNING(x) std::cout << "WARNING " << x << std::endl;
 #define MSG_FATAL(x)   std::cout << "FATAL   " << x << std::endl;
 }  // end of namespace Acts
 
