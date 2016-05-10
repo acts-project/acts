2467e9f819d69202c702f21e44deb75fefb82108
Christian Gumpert <christian.gumpert@cern.ch>
2016-05-10 13:05:36 +0200

    add header file with version number

---
+++ ACTSVersion.h
@@ -0,0 +1,16 @@
+#ifndef ACTSVersion_h
+#define ACTSVersion_h
+
+//
+//  Caution: this is the only ACTS header that is guaranteed
+//  to change with every ACTS release. Including this header
+//  will cause a recompile every time a new ACTS version is
+//  used.
+//
+//  ACTS_VERSION % 100 is the patch level
+//  ACTS_VERSION / 100 % 100 is the minor version
+//  ACTS_VERSION / 10000 is the major version
+
+#define ACTS_VERSION @ACTS_VERSION_NUMBER@
+
+#endif
