24a79b80ce0264ad495251c474c703182ef2d102
jhrdinka <julia.hrdinka@cern.ch>
2016-08-18 14:31:49 +0200

    correct bin positions

--- Plugins/MaterialPlugins/src/LayerMaterialRecord.cpp
+++ Plugins/MaterialPlugins/src/LayerMaterialRecord.cpp
@@ -13,20 +13,12 @@
 #include "ACTS/Plugins/MaterialPlugins/LayerMaterialRecord.hpp"
 
 Acts::LayerMaterialRecord::LayerMaterialRecord()
-  : m_binUtility(nullptr)
-  , m_materialMatrix()
-  , m_entries(0)
-  , m_sumThickness(0.)
-  , m_sumRho(0.)
+  : m_binUtility(nullptr), m_materialMatrix()
 {
 }
 
 Acts::LayerMaterialRecord::LayerMaterialRecord(const BinUtility* binutility)
-  : m_binUtility(binutility)
-  , m_materialMatrix()
-  , m_entries(0)
-  , m_sumThickness(0.)
-  , m_sumRho(0.)
+  : m_binUtility(binutility), m_materialMatrix()
 {
   // reserve
   m_materialMatrix.reserve(m_binUtility->max(1) + 1);
@@ -37,7 +29,7 @@ Acts::LayerMaterialRecord::LayerMaterialRecord(const BinUtility* binutility)
     matVec.reserve(m_binUtility->max(0) + 1);
     for (unsigned int ibin = 0; ibin < (unsigned int)m_binUtility->max(0) + 1;
          ++ibin)
-      matVec.push_back(new MaterialProperties());
+      matVec.push_back(new MaterialProperties(0., 0., 0., 0., 0., 0.));
     m_materialMatrix.push_back(matVec);
   }
 }
@@ -46,9 +38,6 @@ Acts::LayerMaterialRecord::LayerMaterialRecord(
     const LayerMaterialRecord& lmrecord)
   : m_binUtility(lmrecord.m_binUtility)
   , m_materialMatrix(lmrecord.m_materialMatrix)
-  , m_entries(lmrecord.m_entries)
-  , m_sumThickness(lmrecord.m_sumThickness)
-  , m_sumRho(lmrecord.m_sumRho)
 {
 }
 
@@ -64,9 +53,6 @@ Acts::LayerMaterialRecord::operator=(const LayerMaterialRecord& lmrecord)
   if (this != &lmrecord) {
     m_binUtility     = lmrecord.m_binUtility;
     m_materialMatrix = lmrecord.m_materialMatrix;
-    m_entries        = lmrecord.m_entries;
-    m_sumThickness   = lmrecord.m_sumThickness;
-    m_sumRho         = lmrecord.m_sumRho;
   }
   return (*this);
 }
@@ -76,13 +62,12 @@ Acts::LayerMaterialRecord::addLayerMaterialProperties(
     const Acts::Vector3D&           pos,
     const Acts::MaterialProperties* newMaterial)
 {
-  ++m_entries;
   // get the bins corresponding to the position
   size_t bin1 = m_binUtility->bin(pos, 0);
   size_t bin2 = m_binUtility->bin(pos, 1);
-  // get the material which might be there already and add new material and
+  // get the material which might be there already, add new material and
   // weigh it
-  const Acts::MaterialProperties* material = m_materialMatrix.at(bin1).at(bin2);
+  const Acts::MaterialProperties* material = m_materialMatrix.at(bin2).at(bin1);
   float                           newThickness = newMaterial->thickness();
   float                           newRho       = newMaterial->averageRho();
   float                           thickness    = 0.;
@@ -91,6 +76,7 @@ Acts::LayerMaterialRecord::addLayerMaterialProperties(
   float                           l0           = 0.;
   float                           A            = 0.;
   float                           Z            = 0.;
+  // access the new material properties
   if (material) {
     thickness += material->thickness();
     rho += material->averageRho();
@@ -99,48 +85,49 @@ Acts::LayerMaterialRecord::addLayerMaterialProperties(
     A += material->averageA();
     Z += material->averageZ();
   }
+  // add the new material properties and weigh them
   thickness += newThickness;
-  m_sumThickness += newThickness;
   rho += newRho * newThickness;
-  m_sumRho += newRho;
   x0 += newMaterial->x0() * newThickness;
   l0 += newMaterial->l0() * newThickness;
   A += newMaterial->averageA() * newRho;
   Z += newMaterial->averageZ() * newRho;
-  // set the new current material
+  // set the new current material (not averaged yet)
   const Acts::Material updatedMaterial(x0, l0, A, Z, rho);
-  m_materialMatrix.at(bin1).at(bin2)->setMaterial(updatedMaterial, thickness);
+  m_materialMatrix.at(bin2).at(bin1)->setMaterial(updatedMaterial, thickness);
 }
 
 void
 Acts::LayerMaterialRecord::averageMaterial()
 {
-  if (m_entries > 0) {
-    // first finalize the material by averaging over it
-    size_t bins1 = m_binUtility->bins(0);
-    size_t bins2 = m_binUtility->bins(1);
+  // access the bins
+  size_t bins1 = m_binUtility->bins(0);
+  size_t bins2 = m_binUtility->bins(1);
+  // loop through the material properties matrix and average
+  for (size_t bin1 = 0; bin1 < bins1; bin1++) {
+    for (size_t bin2 = 0; bin2 < bins2; bin2++) {
+      const Acts::MaterialProperties* material
+          = m_materialMatrix.at(bin2).at(bin1);
 
-    for (size_t bin1 = 0; bin1 < bins1; bin1++) {
-      for (size_t bin2 = 0; bin2 < bins2; bin2++) {
-        const Acts::MaterialProperties* material
-            = m_materialMatrix.at(bin1).at(bin2);
-        float thickness = material->thickness() / m_entries;
-        float rho       = material->averageRho() / m_sumThickness;
-        float x0        = material->x0() / m_sumThickness;
-        float l0        = material->l0() / m_sumThickness;
-        float A         = material->averageA() / m_sumRho;
-        float Z         = material->averageZ() / m_sumRho;
-        // set the new current material
-        const Acts::Material updatedMaterial(x0, l0, A, Z, rho);
-        m_materialMatrix.at(bin1).at(bin2)->setMaterial(updatedMaterial,
-                                                        thickness);
-      }  // b2
-    }    // b1
-  }
-  // reset counter and sums
-  m_entries      = 0;
-  m_sumThickness = 0.;
-  m_sumRho       = 0.;
+      float thickness = material->thickness();
+      float rho       = material->averageRho();
+      float x0        = material->x0();
+      float l0        = material->l0();
+      float A         = material->averageA();
+      float Z         = material->averageZ();
+      // divide
+      if (x0 != 0.) x0 /= thickness;
+      if (l0 != 0.) l0 /= thickness;
+      if (A != 0.) A /= rho;
+      if (Z != 0.) Z /= rho;
+      if (rho != 0.) rho /= thickness;
+      if (thickness != 0.) thickness /= material->entries();
+      // set the new current material
+      const Acts::Material updatedMaterial(x0, l0, A, Z, rho);
+      m_materialMatrix.at(bin2).at(bin1)->setMaterial(updatedMaterial,
+                                                      thickness);
+    }  // b2
+  }    // b1
 }
 
 std::shared_ptr<const Acts::BinnedSurfaceMaterial>
