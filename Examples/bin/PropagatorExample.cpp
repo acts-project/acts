1454d9e88ada65c9bd725458b621166f30b81872
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-02 23:16:46 +0200

    use example to generate hits for simple detector layout

--- Examples/bin/PropagatorExample.cpp
+++ Examples/bin/PropagatorExample.cpp
@@ -1,4 +1,5 @@
 #include "ACTS/Extrapolation/Propagator.hpp"
+#include <random>
 #include "ACTS/EventData/TrackParameters.hpp"
 #include "ACTS/Extrapolation/GSLStepper.hpp"
 #include "ACTS/Extrapolation/ObserverList.hpp"
@@ -11,6 +12,10 @@ using namespace Acts;
 int
 main()
 {
+  // random numbers for smearing measurements
+  std::default_random_engine             e;
+  std::uniform_real_distribution<double> uniform(-1, 1);
+
   typedef ConstantFieldSvc      BField_t;
   typedef GSLStepper<BField_t>  Stepper_t;
   typedef Propagator<Stepper_t> Propagator_t;
@@ -21,19 +26,42 @@ main()
   Stepper_t    gsl_stepper(std::move(bField));
   Propagator_t propagator(std::move(gsl_stepper));
 
-  Vector3D              pos(0, 0, 0);
-  Vector3D              mom(1 * units::_GeV, 0, 0);
-  CurvilinearParameters pars(nullptr, pos, mom, +1);
-
   typedef Propagator_t::observer_list_t<CurvilinearParameters,
-                                        DebugObserver,
-                                        PathLengthObserver>
-       ObsList_t;
-  auto r = propagator.propagate(pars, ObsList_t());
+                                        PathLengthObserver,
+                                        HitSimulator>
+      ObsList_t;
 
-  std::cout << "total path length = "
-            << r.get<PathLengthObserver::result_type>().pathLength / units::_m
+  ObsList_t     ol;
+  HitSimulator& hit_sim = ol.get<HitSimulator>();
+  typedef std::tuple<float, float, float> tuple_t;
+  hit_sim.barrel
+      = {tuple_t(5 * units::_cm, -50 * units::_cm, 50 * units::_cm),
+         tuple_t(15 * units::_cm, -55 * units::_cm, 55 * units::_cm),
+         tuple_t(25 * units::_cm, -60 * units::_cm, 60 * units::_cm),
+         tuple_t(50 * units::_cm, -75 * units::_cm, 75 * units::_cm),
+         tuple_t(60 * units::_cm, -80 * units::_cm, 80 * units::_cm)};
+
+  hit_sim.endcaps
+      = {tuple_t(70 * units::_cm, 5 * units::_cm, 45 * units::_cm),
+         tuple_t(85 * units::_cm, 15 * units::_cm, 60 * units::_cm),
+         tuple_t(-70 * units::_cm, 5 * units::_cm, 45 * units::_cm),
+         tuple_t(-85 * units::_cm, 15 * units::_cm, 60 * units::_cm)};
+
+  HitSimulator::result_type hits;
+  double                    totalPathLength = 0;
+  for (unsigned int i = 0; i < 1000; ++i) {
+    Vector3D pos(0, 0, 0);
+    Vector3D mom(uniform(e), uniform(e), uniform(e));
+    mom *= 1 * units::_GeV;
+    CurvilinearParameters pars(nullptr, pos, mom, +1);
+    auto                  r = propagator.propagate(pars, ol);
+    totalPathLength += r.get<PathLengthObserver::result_type>().pathLength;
+    hits.add(r.get<HitSimulator::result_type>());
+  }
+  std::cout << "total path length = " << totalPathLength / units::_m
             << std::endl;
+  std::cout << "simulated hits:" << std::endl;
+  hits.print();
 
   return 0;
 }
