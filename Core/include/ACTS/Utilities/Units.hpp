bf0f83fed272c715aed14bb222d7a2b7d44443f8
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-02 12:22:19 +0200

    implement unit conversions and respective tests

--- Core/include/ACTS/Utilities/Units.hpp
+++ Core/include/ACTS/Utilities/Units.hpp
@@ -42,9 +42,9 @@ namespace units {
 #ifdef DOXYGEN
   const double _m = unspecified;
 #else
-  const double _m   = 1000;
+  const double _m   = 1e3;
 #endif  // DOXYGEN
-  const double _km = 1000 * _m;
+  const double _km = 1e3 * _m;
   const double _cm = 1e-2 * _m;
   const double _mm = 1e-3 * _m;
   const double _um = 1e-6 * _m;
@@ -58,7 +58,7 @@ namespace units {
 #ifdef DOXYGEN
   const double _kg = unspecified;
 #else
-  const double _kg  = 1000;
+  const double _kg  = 1e3;
 #endif  // DOXYGEN
   const double _g  = 1e-3 * _kg;
   const double _mg = 1e-6 * _kg;
@@ -82,16 +82,12 @@ namespace units {
 #ifdef DOXYGEN
   const double _MeV = unspecified;
 #else
-  const double _MeV = 1;
+  const double _MeV = 1e-3;
 #endif  // DOXYGEN
   const double _GeV = 1e3 * _MeV;
   const double _TeV = 1e6 * _MeV;
   const double _keV = 1e-3 * _MeV;
   const double _eV  = 1e-6 * _MeV;
-  /// @brief Joule expressed in eV
-  /// @attention This constant can only be used to convert eV-based energy
-  ///            quantities, but not with mechanical units (e.g. kg * m^2 / s^2)
-  const double _J = 1. / 1.60217733e-19 * _eV;
 /// @}
 
 /// @name charge units
@@ -107,17 +103,111 @@ namespace units {
 
   /// @name derived units
   /// @{
+  const double _N = _kg * _m / (_s * _s);
+  const double _J = _N * _m;
   const double _T = _kg / (_C * _s);
-  const double _N = _kg * _m * _m / (_s * _s);
   /// @}
 
-  /// @name conversion and other constants
+  /// @name fundamental physical constants in SI units
   /// @{
-  const double _c            = 2.99792458e8 * _m / _s;
-  const double _hbar         = 1.05457266e-34 * _J * _s;
-  const double _fm_times_GeV = _c * _hbar / (_fm * _GeV);
-  const double _kg_per_MeV   = 1. / (_c * _c) * (_kg / _J) / (_kg / _MeV);
+  const double _c         = 2.99792458e8 * _m / _s;
+  const double _hbar      = 1.05457266e-34 * _J * _s;
+  const double _el_charge = _e / _C;
   /// @}
+
+  /// @cond
+  /// @brief internally used conversion constants
+  namespace {
+    // 1 GeV = 1e9 * e * 1 V = 1.60217733e-10 As * 1 V = 1.60217733e-10 J
+    // ==> 1 J = 1 / 1.60217733e-10 GeV
+    const double _GeV_per_J = _GeV / (_el_charge * 1e9 * _J);
+    // hbar * c = 3.161529298809983e-26 J * m
+    // ==> hbar * c * _GeV_per_J = 1.973270523563071e-16 GeV * m
+    const double _mm_times_GeV = _c * _hbar * _GeV_per_J;
+  }
+  /// @endcond
+
+  /// @brief physical quantities
+  ///
+  /// These constants can be used to select the correct conversion function
+  /// from SI units to natural units and vice versa.
+  enum Quantity { MOMENTUM, ENERGY, LENGTH, MASS };
+
+  template <Quantity>
+  double
+  SI2Nat(const double);
+
+  template <Quantity>
+  double
+  Nat2SI(const double);
+
+  template <>
+  double
+  SI2Nat<ENERGY>(const double E)
+  {
+    static const double conversion = _GeV_per_J;
+    return E * conversion;
+  }
+
+  template <>
+  double
+  Nat2SI<ENERGY>(const double E)
+  {
+    static const double conversion = 1. / _GeV_per_J;
+    return E * conversion;
+  }
+
+  template <>
+  double
+  SI2Nat<LENGTH>(const double l)
+  {
+    static const double conversion = 1. / _mm_times_GeV;
+    return l * conversion;
+  }
+
+  template <>
+  double
+  Nat2SI<LENGTH>(const double l)
+  {
+    static const double conversion = _mm_times_GeV;
+    return l * conversion;
+  }
+
+  template <>
+  double
+  SI2Nat<MOMENTUM>(const double p)
+  {
+    // p(NU) = p(SI) * c
+    static const double conversion = _c * _GeV_per_J;
+    return p * conversion;
+  }
+
+  template <>
+  double
+  Nat2SI<MOMENTUM>(const double p)
+  {
+    // p(SI) = p(NU)/c
+    static const double conversion = 1. / (_c * _GeV_per_J);
+    return p * conversion;
+  }
+
+  template <>
+  double
+  SI2Nat<MASS>(const double m)
+  {
+    // p(NU) = p(SI) * c
+    static const double conversion = _c * _c * _GeV_per_J;
+    return m * conversion;
+  }
+
+  template <>
+  double
+  Nat2SI<MASS>(const double m)
+  {
+    // p(SI) = p(NU)/c
+    static const double conversion = 1. / (_c * _c * _GeV_per_J);
+    return m * conversion;
+  }
 }  // namespace units
 
 }  // namespace Acts
--- Tests/Utilities/unit_conversion.cpp
+++ Tests/Utilities/unit_conversion.cpp
@@ -46,7 +46,35 @@ BOOST_AUTO_TEST_CASE(energy_conversions)
   BOOST_TEST(_MeV == 1e-6 * _TeV);
   BOOST_TEST(_MeV == 1e3 * _keV);
   BOOST_TEST(_MeV == 1e6 * _eV);
-  BOOST_TEST(_MeV == 1.60217733e-13 * _J);
 }
+
+// clang-format off
+BOOST_AUTO_TEST_CASE(si_nat_conversions, *utf::tolerance(1e-8))
+{
+  BOOST_TEST(SI2Nat<ENERGY>(1 * _J) == 1. / 1.60217733e-19 * _eV);
+  BOOST_TEST(SI2Nat<ENERGY>(1 * _g * _m * _m / (_s * _s)) == 1. / 1.60217733e-16 * _eV);
+  BOOST_TEST(SI2Nat<ENERGY>(1 * _J) == 1. / 1.60217733e-10 * _GeV);
+  BOOST_TEST(Nat2SI<ENERGY>(100 * _keV) == 1.60217733e-14 * _J);
+  BOOST_TEST(Nat2SI<ENERGY>(SI2Nat<ENERGY>(100 * _J)) == 100 * _J);
+  BOOST_TEST(SI2Nat<ENERGY>(Nat2SI<ENERGY>(57.3 * _MeV)) == 57300 * _keV);
+
+  BOOST_TEST(SI2Nat<LENGTH>(1 * _m) == 5.0677289e15 / _GeV);
+  BOOST_TEST(Nat2SI<LENGTH>(1. / (197.3270523 * _MeV)) == 1 * _fm);
+  BOOST_TEST(Nat2SI<LENGTH>(SI2Nat<LENGTH>(10 * _m)) == 1e-2 * _km);
+  BOOST_TEST(SI2Nat<LENGTH>(Nat2SI<LENGTH>(1. / _keV)) == 1 / (1e-3 * _MeV));
+
+  BOOST_TEST(SI2Nat<MOMENTUM>(2 * _g * _m / _s) == 3.742313068429198e15 * _GeV);
+  BOOST_TEST(Nat2SI<MOMENTUM>(13 * _TeV) == 6.947574808569e-15 * _kg * _m / _s);
+  BOOST_TEST(Nat2SI<MOMENTUM>(SI2Nat<MOMENTUM>(3.6 * _mg * _km / _h)) == 1e-6 * _kg * _m / _s);
+  BOOST_TEST(SI2Nat<MOMENTUM>(Nat2SI<MOMENTUM>(6.5 * _GeV)) == 6.5e6 * _keV);
+
+  BOOST_TEST(SI2Nat<MASS>(1.67262263806e-27 * _kg) == 938.2720813 * _MeV);
+  BOOST_TEST(Nat2SI<MASS>(125.09 * _GeV) == 2.229932766466e-19 * _mg);
+  BOOST_TEST(Nat2SI<MASS>(SI2Nat<MASS>(3 * _mg)) == 3e-3 * _g);
+  BOOST_TEST(SI2Nat<MASS>(Nat2SI<MASS>(1.77 * _GeV)) == 1770 * _MeV);
+
+  BOOST_TEST(SI2Nat<ENERGY>(_hbar * _c) == 0.197327052356 * _fm * _GeV);
+}
+// clang-format on
 
 BOOST_AUTO_TEST_SUITE_END();
