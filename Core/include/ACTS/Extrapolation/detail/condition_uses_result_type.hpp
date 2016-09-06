019f62f8c7e693b0768976902df887d74b69aefc
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-06 15:40:58 +0200

    implementation details belong in anonymous namespace

--- Core/include/ACTS/Extrapolation/detail/condition_uses_result_type.hpp
+++ Core/include/ACTS/Extrapolation/detail/condition_uses_result_type.hpp
@@ -1,22 +1,25 @@
 #ifndef ACTS_CONDITION_USES_RESULT_TYPE_HPP
 #define ACTS_CONDITION_USES_RESULT_TYPE_HPP 1
 
+#include <type_traits>
 #include "ACTS/Utilities/detail/MPL/type_collector.hpp"
 
 namespace Acts {
 
 namespace detail {
 
-  template <typename T, bool has_observer = true>
-  struct condition_uses_result_type_impl
-  {
-    static constexpr bool value = has_result_type_v<observer_type_t<T>>;
-  };
+  namespace {
+    template <typename T, bool has_observer = true>
+    struct condition_uses_result_type_impl
+    {
+      static constexpr bool value = has_result_type_v<observer_type_t<T>>;
+    };
 
-  template <typename T>
-  struct condition_uses_result_type_impl<T, false> : std::false_type
-  {
-  };
+    template <typename T>
+    struct condition_uses_result_type_impl<T, false> : std::false_type
+    {
+    };
+  }  // end of anonymous namespace
 
   template <typename T>
   struct condition_uses_result_type
