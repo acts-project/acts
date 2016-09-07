ff1f8b523b5592f92b2c8a168fecac774a489bfc
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-07 16:39:37 +0200

    for simplicity asume that extensions are default/copy/move constructible
    - default and copy construction is required for creation of result
    object in Propagator::propagate() call and its return value
    - Move construction can be useful for observer and abort lists
    
    Better catch them through a static assertion than relying on the cryptic
    error messages from gcc.

--- Core/include/ACTS/Extrapolation/detail/Extendable.hpp
+++ Core/include/ACTS/Extrapolation/detail/Extendable.hpp
@@ -2,6 +2,8 @@
 #define ACTS_EXTENDABLE_HPP 1
 
 #include <tuple>
+#include <type_traits>
+#include "ACTS/Utilities/detail/MPL/all_of.hpp"
 
 namespace Acts {
 
@@ -10,6 +12,15 @@ namespace detail {
   template <typename... Extensions>
   struct Extendable
   {
+    // clang-format off
+    static_assert(detail::all_of_v<std::is_default_constructible<Extensions>::value...>,
+                  "all extensions must be default constructible");
+    static_assert(detail::all_of_v<std::is_copy_constructible<Extensions>::value...>,
+                  "all extensions must be copy constructible");
+    static_assert(detail::all_of_v<std::is_move_constructible<Extensions>::value...>,
+                  "all extensions must be move constructible");
+    // clang-format on
+
     template <typename R>
     const R&
     get() const
