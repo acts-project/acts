295ee80180ca08fa0341b668982a243093a27811
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-07 11:09:58 +0200

    executable to profile gsl stepper

---
+++ Tests/profiling/gsl_stepper.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "ACTS/EventData/TrackParameters.hpp"
+#include "ACTS/Extrapolation/AbortConditions.hpp"
+#include "ACTS/Extrapolation/AbortList.hpp"
+#include "ACTS/Extrapolation/GSLStepper.hpp"
+#include "ACTS/Extrapolation/ObserverList.hpp"
+#include "ACTS/Extrapolation/Observers.hpp"
+#include "ACTS/Extrapolation/Propagator.hpp"
+#include "ACTS/MagneticField/ConstantFieldSvc.hpp"
+#include "ACTS/Utilities/Units.hpp"
+
+using namespace Acts;
+
+int
+main()
+{
+  typedef ConstantFieldSvc         BField_type;
+  typedef GSLStepper<BField_type>  Stepper_type;
+  typedef Propagator<Stepper_type> Propagator_type;
+
+  BField_type::Config c;
+  c.field = {0, 0, 2 * units::_T};
+  BField_type     bField(std::move(c));
+  Stepper_type    gsl_stepper(std::move(bField));
+  Propagator_type propagator(std::move(gsl_stepper));
+
+  typedef ObserverList<PathLengthObserver> ObsList_type;
+  typedef AbortList<MaxPathLength>         AbortConditions_type;
+
+  AbortConditions_type al;
+  al.get<MaxPathLength>().maxPathLength = 35 * units::_cm;
+
+  ObsList_type ol;
+
+  Vector3D              pos(0, 0, 0);
+  Vector3D              mom(1 * units::_GeV, 0, 0);
+  CurvilinearParameters pars(nullptr, pos, mom, +1);
+  double                totalPathLength = 0;
+  for (unsigned int i = 0; i < 100000; ++i) {
+    auto r = propagator.propagate(pars, ol, al);
+    totalPathLength += r.get<PathLengthObserver::result_type>().pathLength;
+  }
+
+  std::cout << totalPathLength << std::endl;
+  return 0;
+}
