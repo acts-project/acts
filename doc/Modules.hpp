d5f7939d2f5066515f872ea1ee2673d1d7a77729
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-17 21:11:37 +0200

    add page describing the different ACTS modules

---
+++ doc/Modules.hpp
@@ -0,0 +1,59 @@
+#ifndef ACTS_MODULES_HPP
+#define ACTS_MODULES_HPP 1
+
+/// @defgroup Core Core classes
+/// @brief ACTS core classes
+
+/// @defgroup Detector Tracking geometry
+/// @ingroup Core
+/// @brief Description of the tracking geometry
+
+/// @defgroup EventData Event data model
+/// @ingroup Core
+/// @brief Event data model
+
+/// @defgroup Extrapolation Track extrapolation
+/// @ingroup Core
+/// @brief Algorithms for extrapolation of track parameters
+
+/// @defgroup Fitter Track fitters
+/// @ingroup Core
+/// @brief Algorithms for track fitting
+
+/// @defgroup Layers Layers
+/// @ingroup Core
+/// @brief Description of detector layers
+
+/// @defgroup MagneticField Magnetic field
+/// @ingroup Core
+/// @brief Description of magnetic field properties
+
+/// @defgroup Material Material
+/// @ingroup Core
+/// @brief Description of material properties
+
+/// @defgroup Surfaces Geometric surfaces
+/// @ingroup Core
+/// @brief Description of geometric surfaces
+
+/// @defgroup Tools Tools
+/// @ingroup Core
+/// @brief Geometry building tools
+
+/// @defgroup Utilities Helper classes
+/// @ingroup Core
+/// @brief Helper utilities
+
+/// @defgroup Volumes Volumes
+/// @ingroup Core
+/// @brief Description of geometric volumes
+
+/// @defgroup Examples Examples
+/// @brief ACTS Examples
+
+/// @defgroup Plugins Plugins
+/// @brief ACTS extensions
+
+/// @defgroup Contributing Contribution guide
+
+#endif  // ACTS_MODULES_HPP
