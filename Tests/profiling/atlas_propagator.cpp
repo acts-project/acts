83bb7a83fcd23799f18081c68b3d375954b2a50d
Christian Gumpert <christian.gumpert@cern.ch>
2016-09-22 10:37:34 +0200

    make profilers better configurable
    Add support for command line options to executables meant for profiling.

--- Tests/profiling/atlas_propagator.cpp
+++ Tests/profiling/atlas_propagator.cpp
@@ -1,5 +1,5 @@
+#include <boost/program_options.hpp>
 #include <iostream>
-#include "../Extrapolation/atlas_propagator_fixture.hpp"
 #include "ACTS/EventData/TrackParameters.hpp"
 #include "ACTS/Extrapolation/RungeKuttaEngine.hpp"
 #include "ACTS/MagneticField/ConstantFieldSvc.hpp"
@@ -7,28 +7,75 @@
 #include "ACTS/Utilities/Logger.hpp"
 #include "ACTS/Utilities/Units.hpp"
 
+namespace po = boost::program_options;
 using namespace Acts;
 
 int
-main()
+main(int argc, char* argv[])
 {
-  Test::atlas_propagator_fixture f_propagator;
+  int    toys    = 1;
+  double pT      = 1;
+  double Bz      = 1;
+  double maxPath = 1;
+
+  try {
+    po::options_description desc("Allowed options");
+    // clang-format off
+  desc.add_options()
+      ("help", "produce help message")
+      ("toys",po::value<int>(&toys)->default_value(10000),"number of tracks to propagate")
+      ("pT",po::value<double>(&pT)->default_value(1 * units::_GeV),"transverse momentum in GeV")
+      ("B",po::value<double>(&Bz)->default_value(2),"z-component of B-field in T")
+      ("path",po::value<double>(&maxPath)->default_value(5 * units::_m),"maximum path length in m");
+    // clang-format on
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+  } catch (std::exception& e) {
+    std::cerr << "error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  // print information about profiling setup
+  std::cout << "propagating " << toys
+            << " tracks with pT = " << pT / units::_GeV << "GeV in a " << Bz
+            << "T B-field" << std::endl;
+
+  typedef ConstantFieldSvc BField_type;
+  BField_type::Config      b_config;
+  b_config.field = {0, 0, Bz / 1000};
+  std::unique_ptr<BField_type> magnetic_field
+      = std::make_unique<BField_type>(b_config);
+
+  RungeKuttaEngine::Config c;
+  c.fieldService  = std::move(magnetic_field);
+  c.maxPathLength = maxPath;
+
+  RungeKuttaEngine propagator(c);
+
+  CylinderSurface surface(nullptr, 100 * units::_m, 30 * units::_m);
 
   Vector3D                           pos(0, 0, 0);
-  Vector3D                           mom(1000, 0, 0);
+  Vector3D                           mom(pT * 1000, 0, 0);
   CurvilinearParameters              pars(nullptr, pos, mom, +1);
   ExtrapolationCell<TrackParameters> exCell(pars);
   exCell.addConfigurationMode(ExtrapolationMode::StopWithPathLimit);
 
   double totalPathLength = 0;
-  for (unsigned int i = 0; i < 10000; ++i) {
+  for (unsigned int i = 0; i < toys; ++i) {
     exCell.pathLength         = 0;
     exCell.leadParameters     = &pars;
     exCell.lastLeadParameters = 0;
-    f_propagator.propagator->propagate(exCell, *f_propagator.surface);
+    propagator.propagate(exCell, surface);
     totalPathLength += exCell.pathLength;
   }
 
-  std::cout << totalPathLength << std::endl;
+  std::cout << "average path length = " << totalPathLength / toys / units::_mm
+            << "mm" << std::endl;
   return 0;
 }
--- Tests/profiling/gsl_stepper.cpp
+++ Tests/profiling/gsl_stepper.cpp
@@ -1,3 +1,4 @@
+#include <boost/program_options.hpp>
 #include <iostream>
 #include "ACTS/EventData/TrackParameters.hpp"
 #include "ACTS/Extrapolation/AbortConditions.hpp"
@@ -9,17 +10,51 @@
 #include "ACTS/MagneticField/ConstantFieldSvc.hpp"
 #include "ACTS/Utilities/Units.hpp"
 
+namespace po = boost::program_options;
 using namespace Acts;
 
 int
-main()
+main(int argc, char* argv[])
 {
+  int    toys    = 1;
+  double pT      = 1;
+  double Bz      = 1;
+  double maxPath = 1;
+
+  try {
+    po::options_description desc("Allowed options");
+    // clang-format off
+  desc.add_options()
+      ("help", "produce help message")
+      ("toys",po::value<int>(&toys)->default_value(10000),"number of tracks to propagate")
+      ("pT",po::value<double>(&pT)->default_value(1 * units::_GeV),"transverse momentum in GeV")
+      ("B",po::value<double>(&Bz)->default_value(2 * units::_T),"z-component of B-field in T")
+      ("path",po::value<double>(&maxPath)->default_value(5 * units::_m),"maximum path length in m");
+    // clang-format on
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+  } catch (std::exception& e) {
+    std::cerr << "error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  // print information about profiling setup
+  std::cout << "propagating " << toys
+            << " tracks with pT = " << pT / units::_GeV << "GeV in a "
+            << Bz / units::_T << "T B-field" << std::endl;
+
   typedef ConstantFieldSvc         BField_type;
   typedef GSLStepper<BField_type>  Stepper_type;
   typedef Propagator<Stepper_type> Propagator_type;
 
   BField_type::Config c;
-  c.field = {0, 0, 2 * units::_T};
+  c.field = {0, 0, Bz};
   BField_type     bField(std::move(c));
   Stepper_type    gsl_stepper(std::move(bField));
   Propagator_type propagator(std::move(gsl_stepper));
@@ -29,17 +64,18 @@ main()
 
   Propagator_type::Options<ObsList_type, AbortConditions_type> options;
   AbortConditions_type& al              = options.stop_conditions;
-  al.get<MaxPathLength>().maxPathLength = 5 * units::_m;
+  al.get<MaxPathLength>().maxPathLength = maxPath;
 
   Vector3D              pos(0, 0, 0);
-  Vector3D              mom(1 * units::_GeV, 0, 0);
+  Vector3D              mom(pT, 0, 0);
   CurvilinearParameters pars(nullptr, pos, mom, +1);
   double                totalPathLength = 0;
-  for (unsigned int i = 0; i < 10000; ++i) {
+  for (unsigned int i = 0; i < toys; ++i) {
     auto r = propagator.propagate(pars, options);
     totalPathLength += r.get<PathLengthObserver::result_type>().pathLength;
   }
 
-  std::cout << totalPathLength / units::_cm << std::endl;
+  std::cout << "average path length = " << totalPathLength / toys / units::_mm
+            << "mm" << std::endl;
   return 0;
 }
