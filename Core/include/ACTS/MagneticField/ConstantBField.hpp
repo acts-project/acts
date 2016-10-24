fbd6186d5e2580e5fd589024da776927aeb8e517
Christian Gumpert <christian.gumpert@cern.ch>
2016-10-24 10:28:44 +0200

    simplify ConstantBField class
    - remove left-over conventions from ATLAS
    - simplify initialisation
    - update documentation
    - add unit tests

---
+++ Core/include/ACTS/MagneticField/ConstantBField.hpp
@@ -0,0 +1,118 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef ACTS_MAGNETICFIELDINTERFACES_CONSTANTFIELDSVC_H
+#define ACTS_MAGNETICFIELDINTERFACES_CONSTANTFIELDSVC_H 1
+
+#include "ACTS/Utilities/Definitions.hpp"
+
+namespace Acts {
+
+/// @brief returns a given constant field value at every point
+///
+/// This class implements a simple constant magnetic field. The
+/// magnetic field value has to be set at creation time, but can
+/// be updated later on.
+class ConstantBField final
+{
+public:
+  /// @brief construct constant magnetic field from field vector
+  ///
+  /// @param [in] B magnetic field vector in global coordinate system
+  explicit ConstantBField(Vector3D B) : m_BField(std::move(B)) {}
+
+  /// @brief construct constant magnetic field from components
+  ///
+  /// @param [in] Bx magnetic field component in global x-direction
+  /// @param [in] By magnetic field component in global y-direction
+  /// @param [in] Bz magnetic field component in global z-direction
+  ConstantBField(double Bx, double By, double Bz) : m_BField(Bx, By, Bz) {}
+
+  /// @brief retrieve magnetic field value
+  ///
+  /// @param [in]  xyz   global position
+  /// @param [out] bxyz  magnetic field vector
+  ///
+  /// @note The position @p xyz is ignored and only kept as argument to provide
+  ///       a consistent interface with other magnetic field services.
+  void
+  getField(const double* xyz, double* bxyz) const
+  {
+    bxyz[0] = m_BField[0];
+    bxyz[1] = m_BField[1];
+    bxyz[2] = m_BField[2];
+  }
+
+  /// @brief retrieve magnetic field value
+  ///
+  /// @param [in]  xyz   global position
+  /// @param [out] bxyz  magnetic field vector
+  /// @param [out] deriv gradient of magnetic field vector as (3x3) matrix
+  ///
+  /// @note The position @p xyz is ignored and only kept as argument to provide
+  ///       a consistent interface with other magnetic field services. For the
+  ///       same reason the parameter @p deriv is accepted even though the
+  ///       gradient is always zero for a constant field.
+  void
+  getField(const double* xyz, double* bxyz, double* deriv) const
+  {
+    bxyz[0] = m_BField[0];
+    bxyz[1] = m_BField[1];
+    bxyz[2] = m_BField[2];
+
+    deriv[0] = 0;
+    deriv[1] = 0;
+    deriv[2] = 0;
+    deriv[3] = 0;
+    deriv[4] = 0;
+    deriv[5] = 0;
+    deriv[6] = 0;
+    deriv[7] = 0;
+    deriv[8] = 0;
+  }
+
+  /// @brief retrieve magnetic field value
+  ///
+  /// @param [in]  pos  global position
+  /// @return magnetic field vector
+  ///
+  /// @note The position @p pos is ignored and only kept as argument to provide
+  ///       a consistent interface with other magnetic field services.
+  Vector3D
+  getField(const Vector3D& pos) const
+  {
+    return m_BField;
+  }
+
+  /// @brief update magnetic field vector from components
+  ///
+  /// @param [in] Bx magnetic field component in global x-direction
+  /// @param [in] By magnetic field component in global y-direction
+  /// @param [in] Bz magnetic field component in global z-direction
+  void
+  setField(double Bx, double By, double Bz)
+  {
+    m_BField << Bx, By, Bz;
+  }
+
+  /// @brief update magnetic field vector
+  ///
+  /// @param [in] B magnetic field vector in global coordinate system
+  void
+  setField(const Vector3D& B)
+  {
+    m_BField = B;
+  }
+
+private:
+  /// magnetic field vector
+  Vector3D m_BField;
+};
+}  // end of namespace Acts
+
+#endif  //> ! ACTS_MAGNETICFIELDINTERFACES_CONSTANTFIELDSVC_H
---
+++ Tests/MagneticField/ConstantBField_tests.cpp
@@ -0,0 +1,192 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/// @file ConstantBField_tests.cpp
+
+#define BOOST_TEST_MODULE Constant magnetic field tests
+#include <boost/test/included/unit_test.hpp>
+
+#include <boost/test/data/test_case.hpp>
+#include "ACTS/MagneticField/ConstantBField.hpp"
+#include "ACTS/Utilities/Definitions.hpp"
+#include "ACTS/Utilities/Units.hpp"
+
+namespace bdata = boost::unit_test::data;
+namespace tt    = boost::test_tools;
+
+namespace Acts {
+
+namespace Test {
+
+  /// @brief unit test for construction of constant magnetic field
+  ///
+  /// Tests the correct behavior and consistency of
+  /// -# ConstantBField::ConstantBField(double Bx,double By,double Bz)
+  /// -# ConstantBField::ConstantBField(Vector3D B)
+  /// -# ConstantBField::getField(const double[3] xyz, double[3] B) const
+  /// -# ConstantBField::getField(const Vector3D& pos) const
+  BOOST_DATA_TEST_CASE(ConstantBField_components,
+                       bdata::random(-2. * units::_T, 2. * units::_T)
+                           ^ bdata::random(-1. * units::_T, 4. * units::_T)
+                           ^ bdata::random(0. * units::_T, 10. * units::_T)
+                           ^ bdata::random(-10. * units::_m, 10. * units::_m)
+                           ^ bdata::random(-10. * units::_m, 10. * units::_m)
+                           ^ bdata::random(-10. * units::_m, 10. * units::_m)
+                           ^ bdata::xrange(10),
+                       x,
+                       y,
+                       z,
+                       bx,
+                       by,
+                       bz,
+                       index)
+  {
+    BOOST_TEST_CONTEXT("Eigen interface")
+    {
+      const Vector3D       Btrue(bx, by, bz);
+      const Vector3D       pos(x, y, z);
+      const ConstantBField BField(Btrue);
+
+      BOOST_TEST(Btrue == BField.getField(pos));
+      BOOST_TEST(Btrue == BField.getField(Vector3D(0, 0, 0)));
+      BOOST_TEST(Btrue == BField.getField(-2 * pos));
+    }
+
+    BOOST_TEST_CONTEXT("C-array interface")
+    {
+      const double         Btrue[3] = {bx, by, bz};
+      const double         pos[3]   = {x, y, z};
+      const ConstantBField BField(bx, by, bz);
+      double               Btest[3] = {0, 0, 0};
+
+      BField.getField(pos, Btest);
+      BOOST_TEST(Btrue[0] == Btest[0]);
+      BOOST_TEST(Btrue[1] == Btest[1]);
+      BOOST_TEST(Btrue[2] == Btest[2]);
+
+      BField.getField(0, Btest);
+      BOOST_TEST(Btrue[0] == Btest[0]);
+      BOOST_TEST(Btrue[1] == Btest[1]);
+      BOOST_TEST(Btrue[2] == Btest[2]);
+    }
+
+    BOOST_TEST_CONTEXT("Eigen initialised - C-array retrieved")
+    {
+      const Vector3D       Btrue(bx, by, bz);
+      const ConstantBField BField(Btrue);
+      const double         pos[3]   = {x, y, z};
+      double               Btest[3] = {0, 0, 0};
+
+      BField.getField(pos, Btest);
+      BOOST_TEST(Btrue(0) == Btest[0]);
+      BOOST_TEST(Btrue(1) == Btest[1]);
+      BOOST_TEST(Btrue(2) == Btest[2]);
+
+      BField.getField(0, Btest);
+      BOOST_TEST(Btrue(0) == Btest[0]);
+      BOOST_TEST(Btrue(1) == Btest[1]);
+      BOOST_TEST(Btrue(2) == Btest[2]);
+    }
+
+    BOOST_TEST_CONTEXT("C-array initialised - Eigen retrieved")
+    {
+      const ConstantBField BField(bx, by, bz);
+      const Vector3D       Btrue(bx, by, bz);
+      const Vector3D       pos(x, y, z);
+
+      BOOST_TEST(Btrue == BField.getField(pos));
+      BOOST_TEST(Btrue == BField.getField(Vector3D(0, 0, 0)));
+      BOOST_TEST(Btrue == BField.getField(-2 * pos));
+    }
+  }
+
+  /// @brief unit test for update of constant magnetic field
+  ///
+  /// Tests the correct behavior and consistency of
+  /// -# ConstantBField::setField(double Bx, double By, double Bz)
+  /// -# ConstantBField::setField(const Vector3D& B)
+  /// -# ConstantBField::getField(const double[3] xyz, double[3] B) const
+  /// -# ConstantBField::getField(const Vector3D& pos) const
+  BOOST_DATA_TEST_CASE(ConstantBField_update,
+                       bdata::random(-2. * units::_T, 2. * units::_T)
+                           ^ bdata::random(-1. * units::_T, 4. * units::_T)
+                           ^ bdata::random(0. * units::_T, 10. * units::_T)
+                           ^ bdata::random(-10. * units::_m, 10. * units::_m)
+                           ^ bdata::random(-10. * units::_m, 10. * units::_m)
+                           ^ bdata::random(-10. * units::_m, 10. * units::_m)
+                           ^ bdata::xrange(10),
+                       x,
+                       y,
+                       z,
+                       bx,
+                       by,
+                       bz,
+                       index)
+  {
+    ConstantBField BField(0, 0, 0);
+
+    BOOST_TEST_CONTEXT("Eigen interface")
+    {
+      const Vector3D Btrue(bx, by, bz);
+      const Vector3D pos(x, y, z);
+      BField.setField(Btrue);
+
+      BOOST_TEST(Btrue == BField.getField(pos));
+      BOOST_TEST(Btrue == BField.getField(Vector3D(0, 0, 0)));
+      BOOST_TEST(Btrue == BField.getField(-2 * pos));
+    }
+
+    BOOST_TEST_CONTEXT("C-array interface")
+    {
+      const double pos[3]   = {x, y, z};
+      double       Btest[3] = {0, 0, 0};
+      BField.setField(bx, by, bz);
+
+      BField.getField(pos, Btest);
+      BOOST_TEST(bx == Btest[0]);
+      BOOST_TEST(by == Btest[1]);
+      BOOST_TEST(bz == Btest[2]);
+
+      BField.getField(0, Btest);
+      BOOST_TEST(bx == Btest[0]);
+      BOOST_TEST(by == Btest[1]);
+      BOOST_TEST(bz == Btest[2]);
+    }
+
+    BOOST_TEST_CONTEXT("Eigen initialised - C-array retrieved")
+    {
+      const Vector3D Btrue(bx, by, bz);
+      const double   pos[3]   = {x, y, z};
+      double         Btest[3] = {0, 0, 0};
+      BField.setField(Btrue);
+
+      BField.getField(pos, Btest);
+      BOOST_TEST(Btrue(0) == Btest[0]);
+      BOOST_TEST(Btrue(1) == Btest[1]);
+      BOOST_TEST(Btrue(2) == Btest[2]);
+
+      BField.getField(0, Btest);
+      BOOST_TEST(Btrue(0) == Btest[0]);
+      BOOST_TEST(Btrue(1) == Btest[1]);
+      BOOST_TEST(Btrue(2) == Btest[2]);
+    }
+
+    BOOST_TEST_CONTEXT("C-array initialised - Eigen retrieved")
+    {
+      const Vector3D Btrue(bx, by, bz);
+      const Vector3D pos(x, y, z);
+      BField.setField(bx, by, bz);
+
+      BOOST_TEST(Btrue == BField.getField(pos));
+      BOOST_TEST(Btrue == BField.getField(Vector3D(0, 0, 0)));
+      BOOST_TEST(Btrue == BField.getField(-2 * pos));
+    }
+  }
+}  // end of namespace Test
+
+}  // end of namespace Acts
