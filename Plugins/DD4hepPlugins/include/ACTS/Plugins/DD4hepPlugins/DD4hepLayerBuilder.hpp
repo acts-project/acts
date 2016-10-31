ef583adb3b5957947c2dbd9361428b1f0a73f38b
jhrdinka <julia.hrdinka@cern.ch>
2016-10-31 16:30:57 +0100

    introduced new DD4hepLayerBuilder

---
+++ Plugins/DD4hepPlugins/include/ACTS/Plugins/DD4hepPlugins/DD4hepLayerBuilder.hpp
@@ -0,0 +1,159 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef ACTS_DD4HEPPLUGIN_DD4HEPLAYERBUILDER_H
+#define ACTS_DD4HEPPLUGIN_DD4HEPLAYERBUILDER_H 1
+
+#include "ACTS/Tools/ILayerBuilder.hpp"
+#include "ACTS/Utilities/BinUtility.hpp"
+#include "ACTS/Utilities/Definitions.hpp"
+#include "ACTS/Utilities/Logger.hpp"
+
+class TGeoMatrix;
+
+namespace DD4hep {
+namespace Geometry {
+  class DetElement;
+}
+}
+
+namespace Acts {
+
+class ILayerCreator;
+class Surface;
+
+/// @brief build layers of one cylinder-endcap setup from DD4hep input
+///
+/// This class is an implementation of the Acts::ILayerBuilder,
+/// creating the central (layers of barrel), the negative and positive layers
+/// (layers of endcaps) of one hierarchy (e.g. PixelDetector, StripDetector,...)
+/// with input from DD4hep.
+
+class DD4hepLayerBuilder : public ILayerBuilder
+{
+public:
+  /// @struct Config
+  /// nested configuration struct for steering of the layer builder
+  struct Config
+  {
+    /// string based identification
+    std::string configurationName = "undefined";
+    /// layer creator which is internally used to build layers
+    std::shared_ptr<ILayerCreator> layerCreator;
+    /// the binning type of the contained surfaces in phi
+    /// (equidistant/arbitrary)
+    BinningType bTypePhi;
+    /// the binning type of the contained surfaces in r
+    /// (equidistant/arbitrary)
+    BinningType bTypeR;
+    /// the binning type of the contained surfaces in z
+    /// (equidistant/arbitrary)
+    BinningType bTypeZ;
+    /// the DD4hep::DetElements of the layers of the negative volume (negative
+    /// endcap)
+    /// @note if the current volume has no endcaps or no layers this parameter
+    /// will not be set
+    std::vector<DD4hep::Geometry::DetElement> negativeLayers;
+    /// the DD4hep::DetElements of the layers of the central volume (barrel)
+    /// @note if the current volume has no layers this parameter will not be set
+    std::vector<DD4hep::Geometry::DetElement> centralLayers;
+    /// the DD4hep::DetElements of the layers of the positive volume (positive
+    /// endcap)
+    /// @note if the current volume has no endcaps or no layers this parameter
+    /// will not be set
+    std::vector<DD4hep::Geometry::DetElement> positiveLayers;
+  };
+
+  /// Constructor
+  /// @param config is the configuration struct
+  /// @param logger is the logging instance
+  DD4hepLayerBuilder(const Acts::DD4hepLayerBuilder::Config& config,
+                     std::unique_ptr<Logger>                 logger);
+  /// Destructor
+  ~DD4hepLayerBuilder();
+
+  /// LayerBuilder interface method
+  /// @return  the layers at negative side
+  virtual const LayerVector
+  negativeLayers() const final;
+
+  /// LayerBuilder interface method
+  /// @return the layers at the central sector
+  virtual const LayerVector
+  centralLayers() const final;
+
+  /// LayerBuilder interface method
+  /// @return  the layers at positive side
+  virtual const LayerVector
+  positiveLayers() const final;
+
+  /// Name identification
+  /// @return the string based identification of this configuration
+  virtual const std::string&
+  identification() const final;
+
+  /// set the configuration object
+  /// @param cfg is the configuration struct
+  void
+  setConfiguration(const Config& cfg);
+
+  /// get the configuration object
+  Config
+  getConfiguration() const;
+
+  /// set logging instance
+  void
+  setLogger(std::unique_ptr<Logger> logger);
+
+private:
+  /// configruation object
+  Config m_cfg;
+
+  /// logging instance
+  std::unique_ptr<Logger> m_logger;
+
+  /// Private access to the logger
+  const Logger&
+  logger() const
+  {
+    return *m_logger;
+  }
+
+  /// Private helper function collecting all sensitive detector elements of a
+  /// layer
+  /// @param detElement the DD4hep::DetElement of the layer
+  /// @param surfaces the vector of surfaces which should be filled with the
+  /// sensitive detector elements
+  /// @param axes the orientation of the modules to the ACTS frame
+  void
+  collectSensitive(const DD4hep::Geometry::DetElement& detElement,
+                   std::vector<const Acts::Surface*>&  surfaces,
+                   const std::string&                  axes = "XYZ") const;
+
+  // Private helper function to convert the TGeo transformation matrix into a
+  // ACTS transformation matrix
+  /// @param tGeoTrans TGeo transformation matrix which should be converted
+  std::shared_ptr<Acts::Transform3D>
+  convertTransform(const TGeoMatrix* tGeoTrans) const;
+};
+
+inline const std::string&
+DD4hepLayerBuilder::identification() const
+{
+  return m_cfg.configurationName;
+}
+
+inline DD4hepLayerBuilder::Config
+DD4hepLayerBuilder::getConfiguration() const
+{
+  return m_cfg;
+}
+
+}  // end of namespace
+
+#endif  // ACTS_DD4HEPPLUGIN_DD4HEPLAYERBUILDER_H
---
+++ Plugins/DD4hepPlugins/src/DD4hepLayerBuilder.cpp
@@ -0,0 +1,232 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ACTS/Plugins/DD4hepPlugins/DD4hepLayerBuilder.hpp"
+#include "ACTS/Plugins/DD4hepPlugins/DD4hepDetElement.hpp"
+#include "ACTS/Plugins/DD4hepPlugins/IActsExtension.hpp"
+#include "ACTS/Surfaces/Surface.hpp"
+#include "ACTS/Tools/ILayerCreator.hpp"
+#include "ACTS/Utilities/Units.hpp"
+#include "DD4hep/Detector.h"
+#include "TGeoManager.h"
+#include "TGeoMatrix.h"
+
+Acts::DD4hepLayerBuilder::DD4hepLayerBuilder(
+    const Acts::DD4hepLayerBuilder::Config& config,
+    std::unique_ptr<Logger>                 logger)
+  : m_cfg(), m_logger(std::move(logger))
+{
+  setConfiguration(config);
+}
+
+Acts::DD4hepLayerBuilder::~DD4hepLayerBuilder()
+{
+}
+
+void
+Acts::DD4hepLayerBuilder::setConfiguration(
+    const Acts::DD4hepLayerBuilder::Config& config)
+{
+  m_cfg = config;
+}
+
+const Acts::LayerVector
+Acts::DD4hepLayerBuilder::negativeLayers() const
+{
+  LayerVector layers;
+  if (m_cfg.negativeLayers.empty()) {
+    ACTS_VERBOSE("[L] No layers handed over for negative volume.");
+  } else {
+    ACTS_VERBOSE("[L] Received layers for negative volume -> creating "
+                 "disc layers");
+    // go through layers
+    for (auto& detElement : m_cfg.negativeLayers) {
+      // prepare the layer surfaces
+      std::vector<const Surface*> layerSurfaces;
+      // access the extension of the layer
+      // at this stage all layer detElements have extension (checked in
+      // ConvertDD4hepDetector)
+      Acts::IActsExtension* detExtension
+          = detElement.extension<Acts::IActsExtension>();
+      // access the axis orienation of the modules
+      std::string axes = detExtension->axes();
+      // collect the sensitive detector elements possibly contained by the layer
+      collectSensitive(detElement, layerSurfaces, axes);
+      // access the global transformation matrix of the layer
+      auto transform = convertTransform(&(detElement.worldTransformation()));
+      // get the shape of the layer
+      TGeoShape* geoShape
+          = detElement.placement().ptr()->GetVolume()->GetShape();
+      TGeoConeSeg* tube = dynamic_cast<TGeoConeSeg*>(geoShape);
+      if (!tube)
+        ACTS_ERROR("[L] Disc layer has wrong shape - needs to be TGeoConeSeg!");
+      // extract the boundaries
+      double rMin = tube->GetRmin1() * units::_cm;
+      double rMax = tube->GetRmax1() * units::_cm;
+      double zMin
+          = (transform->translation()
+             - transform->rotation().col(2) * tube->GetDz() * units::_cm)
+                .z();
+      double zMax
+          = (transform->translation()
+             + transform->rotation().col(2) * tube->GetDz() * units::_cm)
+                .z();
+      if (zMin > zMax) std::swap(zMin, zMax);
+      layers.push_back(m_cfg.layerCreator->discLayer(layerSurfaces,
+                                                     zMin,
+                                                     zMax,
+                                                     rMin,
+                                                     rMax,
+                                                     m_cfg.bTypeR,
+                                                     m_cfg.bTypePhi,
+                                                     transform));
+    }
+  }
+  return layers;
+}
+
+const Acts::LayerVector
+Acts::DD4hepLayerBuilder::centralLayers() const
+{
+  LayerVector layers;
+  if (m_cfg.centralLayers.empty()) {
+    ACTS_VERBOSE("[L] No layers handed over for central volume!");
+  } else {
+    ACTS_VERBOSE("[L] Received layers for central volume -> creating "
+                 "cylindrical layers");
+    // go through layers
+    for (auto& detElement : m_cfg.centralLayers) {
+      // prepare the layer surfaces
+      std::vector<const Surface*> layerSurfaces;
+      // access the extension of the layer
+      // at this stage all layer detElements have extension (checked in
+      // ConvertDD4hepDetector)
+      Acts::IActsExtension* detExtension
+          = detElement.extension<Acts::IActsExtension>();
+      // access the axis orienation of the modules
+      std::string axes = detExtension->axes();
+      // collect the sensitive detector elements possibly contained by the layer
+      collectSensitive(detElement, layerSurfaces, axes);
+      // access the global transformation matrix of the layer
+      auto transform = convertTransform(&(detElement.worldTransformation()));
+      // get the shape of the layer
+      TGeoShape* geoShape
+          = detElement.placement().ptr()->GetVolume()->GetShape();
+      TGeoConeSeg* tube = dynamic_cast<TGeoConeSeg*>(geoShape);
+      if (!tube)
+        ACTS_ERROR(
+            "[L] Cylinder layer has wrong shape - needs to be TGeoConeSeg!");
+      // extract the boundaries
+      double rMin  = tube->GetRmin1() * units::_cm;
+      double rMax  = tube->GetRmax1() * units::_cm;
+      double halfZ = tube->GetDz() * units::_cm;
+      layers.push_back(m_cfg.layerCreator->cylinderLayer(layerSurfaces,
+                                                         rMin,
+                                                         rMax,
+                                                         halfZ,
+                                                         m_cfg.bTypePhi,
+                                                         m_cfg.bTypeZ,
+                                                         transform));
+    }
+  }
+  return layers;
+}
+
+const Acts::LayerVector
+Acts::DD4hepLayerBuilder::positiveLayers() const
+{
+  LayerVector layers;
+  if (m_cfg.positiveLayers.empty()) {
+    ACTS_VERBOSE("[L] No layers handed over for negative volume!");
+  } else {
+    ACTS_VERBOSE("[L] Received layers for negative volume -> creating "
+                 "disc layers");
+    // go through layers
+    for (auto& detElement : m_cfg.positiveLayers) {
+      // prepare the layer surfaces
+      std::vector<const Surface*> layerSurfaces;
+      // access the extension of the layer
+      // at this stage all layer detElements have extension (checked in
+      // ConvertDD4hepDetector)
+      Acts::IActsExtension* detExtension
+          = detElement.extension<Acts::IActsExtension>();
+      // access the axis orienation of the modules
+      std::string axes = detExtension->axes();
+      // collect the sensitive detector elements possibly contained by the layer
+      collectSensitive(detElement, layerSurfaces, axes);
+      // access the global transformation matrix of the layer
+      auto transform = convertTransform(&(detElement.worldTransformation()));
+      // get the shape of the layer
+      TGeoShape* geoShape
+          = detElement.placement().ptr()->GetVolume()->GetShape();
+      TGeoConeSeg* tube = dynamic_cast<TGeoConeSeg*>(geoShape);
+      if (!tube)
+        ACTS_ERROR("[L] Disc layer has wrong shape - needs to be TGeoConeSeg!");
+      // extract the boundaries
+      double rMin = tube->GetRmin1() * units::_cm;
+      double rMax = tube->GetRmax1() * units::_cm;
+      double zMin
+          = (transform->translation()
+             - transform->rotation().col(2) * tube->GetDz() * units::_cm)
+                .z();
+      double zMax
+          = (transform->translation()
+             + transform->rotation().col(2) * tube->GetDz() * units::_cm)
+                .z();
+      if (zMin > zMax) std::swap(zMin, zMax);
+      layers.push_back(m_cfg.layerCreator->discLayer(layerSurfaces,
+                                                     zMin,
+                                                     zMax,
+                                                     rMin,
+                                                     rMax,
+                                                     m_cfg.bTypeR,
+                                                     m_cfg.bTypePhi,
+                                                     transform));
+    }
+  }
+  return layers;
+}
+
+void
+Acts::DD4hepLayerBuilder::collectSensitive(
+    const DD4hep::Geometry::DetElement& detElement,
+    std::vector<const Acts::Surface*>&  surfaces,
+    const std::string&                  axes) const
+{
+  const DD4hep::Geometry::DetElement::Children& children
+      = detElement.children();
+  if (!children.empty()) {
+    for (auto& child : children) {
+      DD4hep::Geometry::DetElement childDetElement = child.second;
+      if (childDetElement.volume().isSensitive()) {
+        // create the corresponding detector element
+        Acts::DD4hepDetElement* dd4hepDetElement
+            = new Acts::DD4hepDetElement(childDetElement, axes, units::_cm);
+        // add surface to surface vector
+        surfaces.push_back(&(dd4hepDetElement->surface()));
+      }
+      collectSensitive(childDetElement, surfaces);
+    }
+  }
+}
+
+std::shared_ptr<Acts::Transform3D>
+Acts::DD4hepLayerBuilder::convertTransform(const TGeoMatrix* tGeoTrans) const
+{
+  // get the placement and orientation in respect to its mother
+  const Double_t* rotation    = tGeoTrans->GetRotationMatrix();
+  const Double_t* translation = tGeoTrans->GetTranslation();
+  auto            transform   = std::make_shared<Acts::Transform3D>(
+      Acts::Vector3D(rotation[0], rotation[3], rotation[6]),
+      Acts::Vector3D(rotation[1], rotation[4], rotation[7]),
+      Acts::Vector3D(rotation[2], rotation[5], rotation[8]),
+      Acts::Vector3D(translation[0] * units::_cm,
+                     translation[1] * units::_cm,
+                     translation[2] * units::_cm));
+  return (transform);
+}
