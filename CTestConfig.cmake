66d296b8a8a0b1c768902cef5d06c4e380897f39
Christian Gumpert <christian.gumpert@cern.ch>
2016-11-09 15:28:30 +0100

    use consistent style

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,16 +10,19 @@ set(CTEST_PROJECT_NAME "ACTS")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 # CDash configuration
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=ACTS")
-set(CTEST_DROP_SITE_CDASH TRUE)
+set (CTEST_DROP_METHOD "http")
+set (CTEST_DROP_SITE "my.cdash.org")
+set (CTEST_DROP_LOCATION "/submit.php?project=ACTS")
+set (CTEST_DROP_SITE_CDASH TRUE)
 
 # sub projects
-set(CTEST_PROJECT_SUBPROJECTS ACore Examples DD4hepPlugin MaterialPlugin TGeoPlugin)
+set (CTEST_PROJECT_SUBPROJECTS ACore Examples DD4hepPlugin MaterialPlugin TGeoPlugin)
+
+# do not count unit tests for test coverage
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE "Tests")
 
 # site name
-execute_process(COMMAND hostname OUTPUT_VARIABLE HOSTNAME OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND whoami OUTPUT_VARIABLE USER OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND printf %s@%s ${USER} ${HOSTNAME} OUTPUT_VARIABLE CTEST_SITE)
+execute_process (COMMAND hostname OUTPUT_VARIABLE HOSTNAME OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process (COMMAND whoami OUTPUT_VARIABLE USER OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process (COMMAND printf %s@%s ${USER} ${HOSTNAME} OUTPUT_VARIABLE CTEST_SITE)
 
