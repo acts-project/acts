9f6de880487f19539d7b8fed6dfe6bf61c7bf14d
Moritz Kiehn <msmk@cern.ch>
2017-03-10 08:17:46 +0100

    Surfaces/TriangleBounds: use updated api and BoundaryCheck

--- Core/include/ACTS/Surfaces/TriangleBounds.hpp
+++ Core/include/ACTS/Surfaces/TriangleBounds.hpp
@@ -1,6 +1,6 @@
 // This file is part of the ACTS project.
 //
-// Copyright (C) 2016 ACTS project team
+// Copyright (C) 2016-2017 ACTS project team
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -14,6 +14,7 @@
 #define ACTS_SURFACESTRIANGLEBOUNDS_H
 
 #include <utility>
+
 #include "ACTS/Surfaces/PlanarBounds.hpp"
 #include "ACTS/Surfaces/RectangleBounds.hpp"
 #include "ACTS/Utilities/Definitions.hpp"
@@ -41,7 +42,6 @@ public:
     bv_length = 6
   };
 
-  /// Default Constructor - deleted
   TriangleBounds() = delete;
 
   /// Constructor with coordinates of vertices
@@ -49,69 +49,30 @@ public:
   /// @param vertices is the vector of vertices
   TriangleBounds(const std::vector<Vector2D>& vertices);
 
-  /// Copy constructor
-  ///
-  /// @param tribo are the source bounds for assignment
-  TriangleBounds(const TriangleBounds& tribo)
-    : PlanarBounds(tribo), m_boundingBox(0., 0.)
-  {
-  }
-
-  /// Destructor
   virtual ~TriangleBounds();
 
-  /// Assignment Operator
-  ///
-  /// @param tribo are the source bounds for assignment
-  TriangleBounds&
-  operator=(const TriangleBounds& tribo);
-
-  /// Virtual constructor
   virtual TriangleBounds*
   clone() const final override;
 
-  /// Return the type of the bounds for persistency
   virtual BoundsType
-  type() const final
-  {
-    return SurfaceBounds::Triangle;
-  }
+  type() const final override;
+
+  virtual std::vector<TDD_real_t>
+  valueStore() const final override;
 
   /// This method checks if the provided local coordinates are inside the
   /// surface bounds
   ///
   /// @param lpos local position in 2D local carthesian frame
   /// @param bcheck is the boundary check directive
-  ///
   /// @return boolean indicator for the success of this operation
   virtual bool
   inside(const Vector2D&      lpos,
          const BoundaryCheck& bcheck) const final override;
 
-  /// This method checks if the provided local coordinate 1 is inside the
-  /// surface bounds
-  ///
-  /// @param lpos local position in 2D local carthesian frame
-  /// @param tol0 is the absolute tolerance on the local first coordinate
-  ///
-  /// @return boolean indicator for the success of this operation
-  virtual bool
-  insideLoc0(const Vector2D& lpos, double tol0 = 0.) const final override;
-
-  /// This method checks if the provided local coordinate 2 is inside the
-  /// surface bounds
-  ///
-  /// @param lpos local position in 2D local carthesian frame
-  /// @param tol1 is the absolute tolerance on the local first coordinate
-  ///
-  /// @return boolean indicator for the success of this operation
-  virtual bool
-  insideLoc1(const Vector2D& lpos, double tol1 = 0.) const final override;
-
   /// Minimal distance to boundary ( > 0 if outside and <=0 if inside)
   ///
   /// @param lpos is the local position to check for the distance
-  ///
   /// @return is a signed distance parameter
   virtual double
   distanceToBoundary(const Vector2D& lpos) const final override;
@@ -131,49 +92,9 @@ public:
   dump(std::ostream& sl) const final override;
 
 private:
+  Vector2D        m_vertices[3];
   RectangleBounds m_boundingBox;  ///< internal bounding box cache
 };
-
-inline TriangleBounds*
-TriangleBounds::clone() const
-{
-  return new TriangleBounds(*this);
-}
-
-inline bool
-TriangleBounds::inside(const Vector2D& lpos, const BoundaryCheck& bcheck) const
-{
-  return bcheck.isInsidePolygon(lpos, vertices());
-}
-
-inline bool
-TriangleBounds::insideLoc0(const Vector2D& lpos, double tol0) const
-{
-  return m_boundingBox.insideLoc0(lpos, tol0);
-}
-
-inline bool
-TriangleBounds::insideLoc1(const Vector2D& lpos, double tol1) const
-{
-  return m_boundingBox.insideLoc1(lpos, tol1);
-}
-
-inline const std::vector<Vector2D>
-TriangleBounds::vertices() const
-{
-  return {{m_valueStore[TriangleBounds::bv_x1],
-           m_valueStore[TriangleBounds::bv_y1]},
-          {m_valueStore[TriangleBounds::bv_x2],
-           m_valueStore[TriangleBounds::bv_y2]},
-          {m_valueStore[TriangleBounds::bv_x3],
-           m_valueStore[TriangleBounds::bv_y3]}};
-}
-
-inline const RectangleBounds&
-TriangleBounds::boundingBox() const
-{
-  return m_boundingBox;
-}
 
 }  // end of namespace
 
