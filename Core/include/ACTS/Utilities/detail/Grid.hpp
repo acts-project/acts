0f608802add74a04460c14b4f5df89c2b1c30cf4
Christian Gumpert <christian.gumpert@cern.ch>
2017-04-05 15:27:53 +0200

    add some documentation

--- Core/include/ACTS/Utilities/detail/Grid.hpp
+++ Core/include/ACTS/Utilities/detail/Grid.hpp
@@ -22,12 +22,23 @@ namespace Test {
 
 namespace detail {
 
+  /// @brief class for describing a regular multi-dimensional grid
+  ///
+  /// @tparam T    type of values stored inside the bins of the grid
+  /// @tparam Axes parameter pack of axis types defining the grid
+  ///
+  /// Class describing a multi-dimensional, regular grid which can store objects
+  /// in its multi-dimensional bins. Bins are hyper-cubes and can be accessed
+  /// either by global bin index, local bin indices or position.
+  ///
+  /// @note @c T must be default-constructible.
   template <typename T, class... Axes>
   class Grid
   {
     /// unit test helper class
     friend struct Test::GridTester<T, Axes...>;
 
+    /// number of dimensions of the grid
     static constexpr size_t dimension = sizeof...(Axes);
 
   public:
@@ -40,6 +51,13 @@ namespace detail {
       m_values.resize(size());
     }
 
+    /// @brief access value stored in bin for a given point
+    ///
+    /// @tparam Point any type with point semantics supporting component access
+    ///               through @c operator[]
+    /// @param [in] point point used to look up the corresponding bin in the
+    ///                   grid
+    /// @return reference to value stored in bin containing the given point
     template <class Point>
     reference
     at(const Point& point)
@@ -47,6 +65,14 @@ namespace detail {
       return m_values.at(getGlobalBinIndex(point));
     }
 
+    /// @brief access value stored in bin for a given point
+    ///
+    /// @tparam Point any type with point semantics supporting component access
+    ///               through @c operator[]
+    /// @param [in] point point used to look up the corresponding bin in the
+    ///                   grid
+    /// @return const-reference to value stored in bin containing the given
+    ///         point
     template <class Point>
     const_reference
     at(const Point& point) const
@@ -54,6 +80,11 @@ namespace detail {
       return m_values.at(getGlobalBinIndex(point));
     }
 
+    /// @brief total number of bins
+    ///
+    /// @return total number of bins in the grid
+    ///
+    /// @note This number contains under-/overflow bins along one or more axes.
     size_t
     size() const
     {
@@ -61,6 +92,15 @@ namespace detail {
     }
 
   private:
+    /// @brief determine global bin index in grid defined by a set of axes
+    ///
+    /// @tparam Point any type with point semantics supporting component access
+    ///               through @c operator[]
+    ///
+    /// @param  [in] point point to look up in the grid
+    /// @return global index for bin containing the given point
+    ///
+    /// @note This could be a under-/overflow bin along one or more axes.
     template <class Point>
     size_t
     getGlobalBinIndex(const Point& point) const
@@ -68,14 +108,23 @@ namespace detail {
       return global_bin_helper::getGlobalBin(point, m_axes);
     }
 
+    /// @brief determine local bin index for each axis from global bin index
+    ///
+    /// @param  [in] bin global bin index
+    /// @return array with local bin indices along each axis (in same order as
+    ///         given @c axes object)
+    ///
+    /// @note Local bin indices could be a under-/overflow bin along this axis.
     std::array<size_t, dimension>
     getLocalBinIndices(size_t bin) const
     {
       return global_bin_helper::getLocalBinIndices(bin, m_axes);
     }
 
+    /// set of axis defining the multi-dimensional grid
     std::tuple<Axes...> m_axes;
-    std::vector<T>      m_values;
+    /// linear value store for each bin
+    std::vector<T> m_values;
   };
 }  // namespace detail
 
