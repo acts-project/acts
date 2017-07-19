b5e65ac988019c79372d2020628bc204ebf5f946
Shaun Roe <shaun.roe@cern.ch>
2017-07-19 16:10:02 +0200

    Add DiscLayerTests; reformat all code

--- Tests/Layers/DiscLayerTests.cpp
+++ Tests/Layers/DiscLayerTests.cpp
@@ -18,8 +18,8 @@
 // leave blank line
 
 //#include <limits>
-#include "ACTS/Layers/PlaneLayer.hpp"
-#include "ACTS/Surfaces/RectangleBounds.hpp"
+#include "ACTS/Layers/DiscLayer.hpp"
+#include "ACTS/Surfaces/RadialBounds.hpp"
 //#include "ACTS/Utilities/Definitions.hpp"
 #include "ACTS/EventData/SingleTrackParameters.hpp"
 #include "ACTS/Layers/GenericApproachDescriptor.hpp"
@@ -36,18 +36,18 @@ namespace Test {
   namespace Layers {
     BOOST_AUTO_TEST_SUITE(Layers);
 
-    /// Unit test for creating compliant/non-compliant PlaneLayer object
-    BOOST_AUTO_TEST_CASE(PlaneLayerConstruction)
+    /// Unit test for creating compliant/non-compliant DiscLayer object
+    BOOST_AUTO_TEST_CASE(DiscLayerConstruction)
     {
       // default constructor, copy and assignment are all deleted
       // minimally need a Transform3D and a PlanarBounds object (e.g.
-      // RectangleBounds) to construct
+      // RadialBounds) to construct
       Translation3D translation{0., 1., 2.};
       auto pTransform = std::make_shared<const Transform3D>(translation);
-      const double halfX(10.), halfY(5.);  // 20 x 10 rectangle
-      auto pRectangle  = std::make_shared<const RectangleBounds>(halfX, halfY);
-      auto pPlaneLayer = PlaneLayer::create(pTransform, pRectangle);
-      BOOST_TEST(pPlaneLayer->layerType() == LayerType::active);
+      const double minRad(10.), maxRad(5.);  // 20 x 10 disc
+      auto         pDisc = std::make_shared<const RadialBounds>(minRad, maxRad);
+      auto         pDiscLayer = DiscLayer::create(pTransform, pDisc);
+      BOOST_TEST(pDiscLayer->layerType() == LayerType::passive);
       // next level: need an array of Surfaces;
       const std::vector<const Surface*> aSurfaces{new SurfaceStub(),
                                                   new SurfaceStub()};
@@ -55,35 +55,34 @@ namespace Test {
       SurfaceArrayCreator sac;
       size_t              binsX(2), binsY(4);
       auto                pSurfaceArray
-          = sac.surfaceArrayOnPlane(aSurfaces, halfX, halfY, binsX, binsY);
-      auto pPlaneLayerFromSurfaces = PlaneLayer::create(
-          pTransform, pRectangle, std::move(pSurfaceArray));
-      BOOST_TEST(pPlaneLayerFromSurfaces->layerType() == LayerType::active);
+          = sac.surfaceArrayOnPlane(aSurfaces, minRad, maxRad, binsX, binsY);
+      auto pDiscLayerFromSurfaces
+          = DiscLayer::create(pTransform, pDisc, std::move(pSurfaceArray));
+      BOOST_TEST(pDiscLayerFromSurfaces->layerType() == LayerType::passive);
       // construct with thickness:
-      auto pPlaneLayerWithThickness = PlaneLayer::create(
-          pTransform, pRectangle, std::move(pSurfaceArray), thickness);
-      BOOST_TEST(pPlaneLayerWithThickness->thickness() == thickness);
+      auto pDiscLayerWithThickness = DiscLayer::create(
+          pTransform, pDisc, std::move(pSurfaceArray), thickness);
+      BOOST_TEST(pDiscLayerWithThickness->thickness() == thickness);
       // with an approach descriptor...
       std::unique_ptr<ApproachDescriptor> ad(
           new GenericApproachDescriptor<Surface>(aSurfaces));
       auto adPtr = ad.get();
-      auto pPlaneLayerWithApproachDescriptor
-          = PlaneLayer::create(pTransform,
-                               pRectangle,
-                               std::move(pSurfaceArray),
-                               thickness,
-                               std::move(ad));
-      BOOST_TEST(pPlaneLayerWithApproachDescriptor->approachDescriptor()
+      auto pDiscLayerWithApproachDescriptor
+          = DiscLayer::create(pTransform,
+                              pDisc,
+                              std::move(pSurfaceArray),
+                              thickness,
+                              std::move(ad));
+      BOOST_TEST(pDiscLayerWithApproachDescriptor->approachDescriptor()
                  == adPtr);
       // with the layerType specified...
-      auto pPlaneLayerWithLayerType
-          = PlaneLayer::create(pTransform,
-                               pRectangle,
-                               std::move(pSurfaceArray),
-                               thickness,
-                               std::move(ad),
-                               LayerType::passive);
-      BOOST_TEST(pPlaneLayerWithLayerType->layerType() == LayerType::passive);
+      auto pDiscLayerWithLayerType = DiscLayer::create(pTransform,
+                                                       pDisc,
+                                                       std::move(pSurfaceArray),
+                                                       thickness,
+                                                       std::move(ad),
+                                                       LayerType::passive);
+      BOOST_TEST(pDiscLayerWithLayerType->layerType() == LayerType::passive);
     }
 
     /// Unit test for testing Layer properties
@@ -91,12 +90,12 @@ namespace Test {
     {
       Translation3D translation{0., 1., 2.};
       auto pTransform = std::make_shared<const Transform3D>(translation);
-      const double halfX(10.), halfY(5.);  // 20 x 10 rectangle
-      auto pRectangle  = std::make_shared<const RectangleBounds>(halfX, halfY);
-      auto pPlaneLayer = PlaneLayer::create(pTransform, pRectangle);
-      // auto planeSurface = pPlaneLayer->surfaceRepresentation();
-      BOOST_TEST(pPlaneLayer->surfaceRepresentation().name()
-                 == std::string("Acts::PlaneSurface"));
+      const double minRad(10.), maxRad(5.);  // 20 x 10 disc
+      auto         pDisc = std::make_shared<const RadialBounds>(minRad, maxRad);
+      auto         pDiscLayer = DiscLayer::create(pTransform, pDisc);
+      // auto planeSurface = pDiscLayer->surfaceRepresentation();
+      BOOST_TEST(pDiscLayer->surfaceRepresentation().name()
+                 == std::string("Acts::DiscSurface"));
     }
 
     BOOST_AUTO_TEST_SUITE_END();
--- Tests/Layers/LayerStub.hpp
+++ Tests/Layers/LayerStub.hpp
@@ -8,48 +8,51 @@
 #ifndef ACTS_TEST_LAYERSTUB
 #define ACTS_TEST_LAYERSTUB 1
 
-#include "ACTS/Layers/Layer.hpp"
 #include "../Surfaces/SurfaceStub.hpp"
+#include "ACTS/Layers/Layer.hpp"
 
 namespace Acts {
 /// Layer derived class stub
 class LayerStub : virtual public SurfaceStub, public Layer
 {
 public:
-  ///constructor (deleted in Surface baseclass)
+  /// constructor (deleted in Surface baseclass)
   LayerStub() = delete;
-  ///copy constructor (deleted in Surface baseclass)
+  /// copy constructor (deleted in Surface baseclass)
   LayerStub(const LayerStub& otherLayer) = delete;
-  ///constructor with pointer to SurfaceArray (protected in Layer baseclass)
-  LayerStub(std::unique_ptr<SurfaceArray> surfaceArray,
-            double thickness=0,
-            std::unique_ptr<ApproachDescriptor> ad=nullptr,
-            LayerType ltype=passive)
-    : SurfaceStub(), Layer(std::move(surfaceArray), thickness, std::move(ad), ltype)
+  /// constructor with pointer to SurfaceArray (protected in Layer baseclass)
+  LayerStub(std::unique_ptr<SurfaceArray>       surfaceArray,
+            double                              thickness = 0,
+            std::unique_ptr<ApproachDescriptor> ad        = nullptr,
+            LayerType                           ltype     = passive)
+    : SurfaceStub()
+    , Layer(std::move(surfaceArray), thickness, std::move(ad), ltype)
   {
   }
 
   /// Destructor
   virtual ~LayerStub() {}
 
-  ///Assignment is deleted in the Layer baseclass
-  LayerStub & 	operator= (const LayerStub &lay) = delete;
-  
-  ///surfaceRepresentation is pure virtual in baseclass
-  const Surface & 
-  surfaceRepresentation() const{
-    return (*this);
-  }
-  
-  Surface &
-  surfaceRepresentation(){
-    return (*this);
-  }
-  
-  ///Other methods have implementation in baseclass
-  ///templated 'onLayer()' from baseclass ?
-  
+  /// Assignment is deleted in the Layer baseclass
+  LayerStub&
+  operator=(const LayerStub& lay)
+      = delete;
 
-  };
+  /// surfaceRepresentation is pure virtual in baseclass
+  const Surface&
+  surfaceRepresentation() const
+  {
+    return (*this);
+  }
+
+  Surface&
+  surfaceRepresentation()
+  {
+    return (*this);
+  }
+
+  /// Other methods have implementation in baseclass
+  /// templated 'onLayer()' from baseclass ?
+};
 }
 #endif
