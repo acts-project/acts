30c7adb235c5a064c05bb106e843be4f09ddb855
jhrdinka <julia.hrdinka@cern.ch>
2017-07-31 16:28:53 +0200

    remove class concept and many implementation of MaterialInteractions - implement global function

---
+++ Core/include/ACTS/Utilities/MaterialInteraction.hpp
@@ -0,0 +1,91 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+///////////////////////////////////////////////////////////////////
+// MaterialInteraction.hpp, ACTS project
+///////////////////////////////////////////////////////////////////
+
+#ifndef ACTS_EXTRAPOLATIONUTILS_MATERIALINTERACTION_H
+#define ACTS_EXTRAPOLATIONUTILS_MATERIALINTERACTION_H 1
+
+#include "ACTS/EventData/ParticleDefinitions.hpp"
+#include "ACTS/Material/Material.hpp"
+#include "ACTS/Utilities/Definitions.hpp"
+
+/// Collection of parametrizations used for
+/// energy loss and scattering
+
+namespace Acts {
+
+/// @enum Acts::InteractionType
+/// Depending on this type, the energy loss will be calculated differently
+enum InteractionType {
+  /// Reconstruction - the mean value of the energy loss will be calcuated
+  reco = 0,
+  /// Simulation - the most probable value of the energy loss will be calculated
+  sim = 1,
+};
+
+/// The total energy loss consists of the energy loss due to ionization and the
+/// energy loss due to radiation
+
+/// The ionization energy loss along a given path length
+///
+///  Calculate the mean ionization that is pathlength dependent
+///
+/// The energy loss is calculated using the following paper
+/// http://http://pdg.lbl.gov/2014/reviews/rpp2014-rev-passage-particles-matter.pdf
+/// Formula 32.5 is used to calculate the mean energy loss [reco mode]
+/// Formula 32.11 is used to calculate the mean energy loss [sim mode]
+/// Sigma is calculated using the Landau width (FHWM) times the conversion
+/// factor 1. / (2. * &radic(2. * log2))
+///
+/// @param[in] interactionType The type of interaction can either be reco
+/// (Reconstruction) - mean energy loss is needed or sim (Simulation) - most
+/// probable energy loss needed
+/// @param[out] sigma Returns the sigma of the distribution (since it does not
+/// exist for the landau distribution the 'Landau Width' is returned)
+/// @param[in] p The value of the momentum
+/// @param[in] mat The material
+/// @param[in] particle The particle type
+/// @param[in] particleMasses The masses of the different particles
+/// @param[in] path The path length (optional)
+/// @return The energy loss due to ionization along a given path length
+double
+ionizationEnergyLoss(InteractionType       interactionType,
+                     double&               sigma,
+                     double                p,
+                     const Material&       mat,
+                     ParticleType          particle,
+                     const ParticleMasses& particleMasses,
+                     double                path = 1.);
+
+/// @todo To be validated
+/// Radiation energy loss along a given path length
+///
+/// @param[in] p The value of particle momentum
+/// @param[in] mat The material
+/// @param[out] sigma The addition to the energy noise
+/// @param[in] particleMasses The masses of the different particles
+/// @return dEdx from radiation
+double
+radiationEnergyLoss(double                p,
+                    const Material&       mat,
+                    ParticleType          particle,
+                    double&               sigma,
+                    const ParticleMasses& particleMasses);
+
+/// multiple scattering as function of dInX0
+///
+///
+double
+sigmaMS(double dInX0, double p, double beta);
+
+}  // end of namespace
+
+#endif
---
+++ Core/src/Utilities/MaterialInteraction.cpp
@@ -0,0 +1,146 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 ACTS project team
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+///////////////////////////////////////////////////////////////////
+// MaterialInteraction.cpp, ACTS project
+///////////////////////////////////////////////////////////////////
+
+#include "ACTS/Utilities/MaterialInteraction.hpp"
+
+double
+Acts::ionizationEnergyLoss(InteractionType       interactionType,
+                           double&               sigma,
+                           double                p,
+                           const Material&       mat,
+                           ParticleType          particle,
+                           const ParticleMasses& particleMasses,
+                           double                path)
+{
+  // the return value
+  double dE = 0.;
+  // kinetic variables
+  // and the electron mass in MeV
+
+  double m     = particleMasses.mass[particle];
+  double E     = sqrt(p * p + m * m);
+  double me    = particleMasses.mass[electron];
+  double beta  = p / E;
+  double gamma = E / m;
+
+  // Ionization - Bethe-Bloch
+  // See ATL-SOFT-PUB-2008-003 equation (4)
+  // 16 eV * Z**0.9 - bring to MeV
+  double I = 16 * Acts::units::_eV * std::pow(mat.Z(), 0.9);
+
+  // See (1) table 32.1
+  // K/A*Z = 0.5 * 30.7075MeV/(g/mm2) * Z/A * rho[g/mm3]  / scale to mm by this
+  double kaz = 0.5 * 30.7075
+      * (Acts::units::_MeV * Acts::units::_mm * Acts::units::_mm)
+      * mat.zOverAtimesRho();
+  double eta2 = beta * gamma;
+  eta2 *= eta2;
+  // density effect, only valid for high energies (gamma > 10 -> p > 1GeV for
+  // muons)
+  double delta = 0.;
+  if (gamma > 10.) {
+    // See (1) table 32.1
+    double eplasma
+        = 28.816 * Acts::units::_eV * sqrt(1000. * mat.zOverAtimesRho());
+    // See (1) formula 32.6
+    delta = 2. * log(eplasma / I) + log(eta2) - 1.;
+  }
+
+  //@todo implement energy loss for electrons
+
+  // divide by beta^2 for non-electrons
+  kaz /= beta * beta;
+  double kazL = kaz * path;
+
+  if (interactionType == Acts::InteractionType::reco) {
+    // Calculate the mean value for reconstruction
+    // See ATL-SOFT-PUB-2008-003 equation (2)
+    double tMax = 2. * eta2 * me / (1. + 2. * gamma * mfrac + mfrac * mfrac);
+    // See ATL-SOFT-PUB-2008-003 equation (1)
+    // or
+    // http://pdg.lbl.gov/2014/reviews/rpp2014-rev-passage-particles-matter.pdf
+    // PDG formula 32.5
+    dE = -kaz * 0.5
+        * (log(2. * me * eta2 * tMax / (I * I)) - (beta * beta) - delta * 0.5);
+    dE *= path;
+  } else {
+    // Calculate the most probably value for simulation
+    //
+    // the landau sigmaL is path length dependent
+    //    PDG formula 32.11 for MOP value from
+    //    http://pdg.lbl.gov/2014/reviews/rpp2014-rev-passage-particles-matter.pdf
+    //
+    dE = kazL * (log(2. * m * eta2 / I) + log(kazL / I) + 0.2 - (beta * beta)
+                 - delta);
+    // The landau width (FWHM) is 4.*kazL
+    // The first factor is the conversion factor from FWHM to sigma
+    sigma = 1. / (2. * sqrt(2. * log2)) * 4. * kazL;
+  }
+
+  return dE;
+}
+
+double
+Acts::radiationEnergyLoss(double                p,
+                          const Material&       mat,
+                          ParticleType          particle,
+                          double&               sigma,
+                          const ParticleMasses& particleMasses)
+{
+  sigma = 0.;
+  if (!(mat)) return 0.;
+
+  // preparation of kinetic constants
+  double m     = particleMasses.mass[particle];
+  double me    = particleMasses.mass[Acts::electron];
+  double mfrac = me / m;
+  double E     = sqrt(p * p + m * m);
+
+  // Bremsstrahlung - Bethe-Heitler
+  // See also ATL-SOFT-PUB-2008-003 equation (6)
+  double Radiation = -E * mfrac * mfrac;
+  // sigma is rms of steep exponential part of radiation
+  sigma = -Radiation;
+
+  // Add e+e- pair production and photonuclear effect for muons at energies
+  // above 8 GeV
+  //    Radiation gives mean Eloss including the long tail from 'catastrophic'
+  //    Eloss
+  //    sigma the rms of steep exponential
+  /// @todo Units?
+  // See also ATL-SOFT-PUB-2008-003 equation (7)(8)
+  if ((particle == Acts::muon) && (E > 8000.)) {
+    if (E < 1.e6) {
+      Radiation += 0.5345 - 6.803e-5 * E - 2.278e-11 * E * E
+          + 9.899e-18 * E * E * E;                            // E below 1 TeV
+      sigma += (0.1828 - 3.966e-3 * sqrt(E) + 2.151e-5 * E);  // idem
+    } else {
+      Radiation += 2.986 - 9.253e-5 * E;           // E above 1 TeV
+      sigma += 17.73 + 2.409e-5 * (E - 1000000.);  // idem
+    }
+  }
+
+  sigma = sigma / mat.X0();
+
+  return Radiation / mat.X0();
+}
+
+double
+Acts::sigmaMS(double dInX0, double p, double beta)
+{
+  if (dInX0 == 0. || p == 0. || beta == 0.) return 0.;
+
+  // Highland formula - projected sigma_s
+  double sig_ms = 13.6 * sqrt(dInX0) / (beta * p)
+      * (1. + 0.038 * log(dInX0 / (beta * beta)));
+  return sig_ms;
+}
