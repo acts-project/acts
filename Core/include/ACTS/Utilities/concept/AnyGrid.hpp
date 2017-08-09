2772ac94fa0c3c75a429c6a929d629ea2f7c9da6
jhrdinka <julia.hrdinka@cern.ch>
2017-08-09 10:09:27 +0200

    Introduce getMin, getMax in n-dimensional grid concept

--- Core/include/ACTS/Utilities/concept/AnyGrid.hpp
+++ Core/include/ACTS/Utilities/concept/AnyGrid.hpp
@@ -29,6 +29,8 @@ BOOST_TYPE_ERASURE_MEMBER((Acts)(concept)(any_grid_detail)(has_interpolate), int
 BOOST_TYPE_ERASURE_MEMBER((Acts)(concept)(any_grid_detail)(has_isInside), isInside, 1);
 BOOST_TYPE_ERASURE_MEMBER((Acts)(concept)(any_grid_detail)(has_neighborHoodIndices), neighborHoodIndices, 2);
 BOOST_TYPE_ERASURE_MEMBER((Acts)(concept)(any_grid_detail)(has_size), size, 0);
+BOOST_TYPE_ERASURE_MEMBER((Acts)(concept)(any_grid_detail)(has_getMin), getMin, 0);
+BOOST_TYPE_ERASURE_MEMBER((Acts)(concept)(any_grid_detail)(has_getMax), getMax, 0);
 // clang-format on
 
 namespace Acts {
@@ -68,7 +70,9 @@ namespace concept {
                       has_getLocalBinIndices<std::array<size_t, DIM>(size_t), const bte::_self>,
                       has_getLowerLeftBinEdge<std::array<double, DIM>(const std::array<size_t, DIM>&), const bte::_self>,
                       has_getUpperRightBinEdge<std::array<double, DIM>(const std::array<size_t, DIM>&), const bte::_self>,
-                      has_neighborHoodIndices<std::set<size_t>(const std::array<size_t, DIM>&, size_t),const bte::_self>
+                      has_neighborHoodIndices<std::set<size_t>(const std::array<size_t, DIM>&, size_t),const bte::_self>,
+					  has_getMin<std::array<double, DIM>(),const bte::_self>,
+					  has_getMax<std::array<double, DIM>(),const bte::_self>
                       >;
     // clang-format off
   }  // namespace any_grid_detail
