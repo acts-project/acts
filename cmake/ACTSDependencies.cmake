4a9f0d6203e233ff339c96237f4b7b6f2a362ddf
Moritz Kiehn <msmk@cern.ch>
2018-01-09 18:16:59 +0100

    cmake: define package versions directly

--- cmake/ACTSDependencies.cmake
+++ cmake/ACTSDependencies.cmake
@@ -4,10 +4,3 @@ if (${ACTS_FIND_QUIET})
 else ()
   set (ACTS_FIND_QUIET_TEXT "")
 endif()
-
-# specify version numbers for dependencies of ACTS
-set (ACTS_BOOST_VERSION "1.62")
-set (ACTS_EIGEN_VERSION "3.2.9")
-set (ACTS_DOXYGEN_VERSION "1.8.11")
-set (ACTS_ROOT_VERSION "6.08.00")
-set (ACTS_DD4HEP_VERSION "1.02")
