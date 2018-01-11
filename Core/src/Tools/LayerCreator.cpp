36f641b8ac80f717ffae14abee7d18b2210bbf28
jhrdinka <julia.hrdinka@cern.ch>
2018-01-11 10:03:48 +0100

    Add default thickness for layers which do not contain any surfaces

--- Core/src/Tools/LayerCreator.cpp
+++ Core/src/Tools/LayerCreator.cpp
@@ -59,13 +59,16 @@ Acts::LayerCreator::cylinderLayer(const std::vector<const Surface*>& surfaces,
       = 0.5 * (protoLayer.minR - protoLayer.envR.first + protoLayer.maxR
                + protoLayer.envR.second);
   double binPosZ = 0.5 * (protoLayer.minZ + protoLayer.maxZ);
-
+  // calculate thickness only if surfaces are present
+  double layerThickness = (protoLayer.maxR - protoLayer.minR)
+      + protoLayer.envR.first + protoLayer.envR.second;
+  // reset layer thickness in case no surfaces ar handed over
+   if (!surfaces.size()) layerThickness = m_cfg.defaultThickness;
   double envZShift = 0.5 * (-protoLayer.envZ.first + protoLayer.envZ.second);
   double layerZ    = binPosZ + envZShift;
-
   double layerHalfZ
       = std::abs(protoLayer.maxZ + protoLayer.envZ.second - layerZ);
-  double layerThickness = (protoLayer.maxR - protoLayer.minR)
+  layerThickness = (protoLayer.maxR - protoLayer.minR)
       + protoLayer.envR.first + protoLayer.envR.second;
 
   ACTS_VERBOSE("Creating a cylindrical Layer:");
@@ -103,8 +106,6 @@ Acts::LayerCreator::cylinderLayer(const std::vector<const Surface*>& surfaces,
                                         << " x "
                                         << binsZ
                                         << ")");
-
-  // create the surface array
   std::unique_ptr<SurfaceArray> sArray;
   if (surfaces.size() > 0) {
     sArray = m_cfg.surfaceArrayCreator->surfaceArrayOnCylinder(
