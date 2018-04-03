55323008e07b0969da6eb3ce550cac1da0893ca2
Robert Langenberg <rlangenb@cern.ch>
2018-04-03 17:47:00 +0200

    current clang-format configuration disallows more readable formatting

--- Tests/LegacySeeding/ATLSeedingIntegrationTest.cpp
+++ Tests/LegacySeeding/ATLSeedingIntegrationTest.cpp
@@ -54,29 +54,23 @@ BOOST_AUTO_TEST_CASE(number_of_seeds_correct_)
   std::vector<SpacePoint*> spVec;
   std::vector<int>         layerVec{1, 2, 2, 3, 4, 11, 13, 14};
   std::vector<float>       xVec{-33.3403,
-                                -48.2369,
-                                -49.4129,
-                                -88.8567,
-                                -122.5566,
-                                -283.169,
-                                -412.277,
-                                -462.5564};
-  std::vector<float>       yVec{2.7288,
-                                4.5193,
-                                4.6755,
-                                11.1935,
-                                18.7696,
-                                83.1666,
-                                179.1006,
-                                232.9765};
-  std::vector<float>       zVec{-74.5553,
-                                -91.9763,
-                                -93.3541,
-                                -139.779,
-                                -179.889,
-                                -381.403,
-                                -568.641,
-                                -654.2494};
+                          -48.2369,
+                          -49.4129,
+                          -88.8567,
+                          -122.5566,
+                          -283.169,
+                          -412.277,
+                          -462.5564};
+  std::vector<float> yVec{
+      2.7288, 4.5193, 4.6755, 11.1935, 18.7696, 83.1666, 179.1006, 232.9765};
+  std::vector<float> zVec{-74.5553,
+                          -91.9763,
+                          -93.3541,
+                          -139.779,
+                          -179.889,
+                          -381.403,
+                          -568.641,
+                          -654.2494};
   for (unsigned int i = 0; i < layerVec.size(); i++) {
     SpacePoint* sp = new SpacePoint();
     sp->surface    = layerVec.at(i);
@@ -96,3 +90,4 @@ BOOST_AUTO_TEST_CASE(number_of_seeds_correct_)
     delete sp;
   }
 }
+
