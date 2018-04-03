f24d19fa27a1ab4903a938025d67029b6d5c4da6
Robert Langenberg <rlangenb@cern.ch>
2018-04-03 16:52:02 +0200

    moved SpacePoint data inside test cpp

--- Tests/LegacySeeding/ATLSeedingIntegrationTest.cpp
+++ Tests/LegacySeeding/ATLSeedingIntegrationTest.cpp
@@ -23,41 +23,6 @@ struct SpacePoint{
 
 };
 
-std::vector<SpacePoint*> readFile(std::string filename){
-
-  std::string line;
-  int   layer;//, ns, hitid; are never used
-  std::vector<SpacePoint*> readSP;
-
-  std::ifstream spFile(filename);
-  if (spFile.is_open()){
-    while(!spFile.eof())
-    {
-      std::getline(spFile, line);
-      std::stringstream ss(line);
-      std::string linetype;
-      ss >> linetype;
-      float x, y, z, r;
-      if (linetype == "lxyz"){
-        ss >> layer >> x >> y >> z;
-        r = std::sqrt(x*x+y*y);
-        SpacePoint* sp = new SpacePoint();
-        sp->x=x;
-        sp->y=y;
-        sp->z=z;
-//        sp->x=r; //straight line for debug purposes
-        sp->r=r;
-        sp->surface = layer;
-        if(r < 200){
-          sp->setClusterList(1,0);
-        }
-        readSP.push_back(sp);
-      }
-    }
-  }
-  return readSP;
-}
-
 int runSeeding(std::vector<SpacePoint*> spVec ){
 
   Acts::ATL_Seedmaker<SpacePoint> seedMaker;
@@ -69,14 +34,31 @@ int runSeeding(std::vector<SpacePoint*> spVec ){
     numSeeds++;
     seed = seedMaker.next();
   }
-  for(auto sp : spVec){
-    delete sp;
-  }
   return numSeeds;
 }
 
 BOOST_AUTO_TEST_CASE(number_of_seeds_correct_){
-  std::vector<SpacePoint*> sp = readFile("/afs/cern.ch/user/r/rlangenb/workspace/acts2/acts/Tests/LegacySeeding/singleTrack.txt");
+  std::vector<SpacePoint*> spVec;
+  std::vector<int> layerVec{1,2,2,3,4,11,13,14};
+  std::vector<float> xVec{-33.3403 ,-48.2369 ,-49.4129 ,-88.8567 ,-122.5566, -283.169, -412.277, -462.5564};
+  std::vector<float> yVec{2.7288 ,4.5193 ,4.6755 ,11.1935,18.7696,83.1666,179.1006,232.9765};
+  std::vector<float> zVec{-74.5553 , -91.9763, -93.3541, -139.779, -179.889, -381.403, -568.641, -654.2494};
+  for (unsigned int i = 0; i< layerVec.size(); i++){
+    SpacePoint * sp = new SpacePoint();
+    sp->surface = layerVec.at(i);
+    sp->x = xVec.at(i);
+    sp->y = yVec.at(i);
+    sp->z = zVec.at(i);
+    sp->r = std::sqrt(sp->x*sp->x+sp->y*sp->y);
+    if(sp->r < 200.){
+      sp->setClusterList(1,0);
+    }
+    spVec.push_back(sp);
+  }
+
   // compare with result obtained from original ATLAS code
-  BOOST_CHECK(runSeeding(sp)==8);
+  BOOST_CHECK(runSeeding(spVec)==8);
+  for(auto sp : spVec){
+    delete sp;
+  }
 }
