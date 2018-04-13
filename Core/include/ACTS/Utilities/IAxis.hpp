9ef2b07e2debf9d217f002ac5570b1b0bab3c2d8
Paul Gessinger <hello@paulgessinger.com>
2018-04-13 09:14:58 +0200

    replace pragma once with normal include guard

--- Core/include/ACTS/Utilities/IAxis.hpp
+++ Core/include/ACTS/Utilities/IAxis.hpp
@@ -6,7 +6,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#pragma once
+#ifndef ACTS_UTILITIES_IAXIS_H
+#define ACTS_UTILITIES_IAXIS_H
 
 #include <vector>
 
@@ -64,3 +65,5 @@ public:
   getNBins() const = 0;
 };
 }
+
+#endif
