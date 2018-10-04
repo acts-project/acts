4430fcafe87418444644b57e4b5bb28f25569484
Fabian Klimpel <fklimpel@asdf123.cern.ch>
2018-10-04 13:41:31 +0200

    Sorting of boundary surfaces templated for TrackingVolume

--- Core/include/Acts/Extrapolator/Navigator.hpp
+++ Core/include/Acts/Extrapolator/Navigator.hpp
@@ -18,6 +18,7 @@
 #include "Acts/Layers/Layer.hpp"
 #include "Acts/Propagator/detail/ConstrainedStep.hpp"
 #include "Acts/Surfaces/Surface.hpp"
+#include "Acts/Utilities/Sorter.hpp"
 #include "Acts/Volumes/BoundarySurfaceT.hpp"
 
 namespace Acts {
