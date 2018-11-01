2326c4507a7724d1f280836ace1d4e4911bd4014
FabianKlimpel <fabian.klimpel@tum.de>
2018-11-01 10:26:49 +0100

    Surface production implemented

--- Tests/Core/Tools/BoxGeometryBuilderTests.cpp
+++ Tests/Core/Tools/BoxGeometryBuilderTests.cpp
@@ -19,13 +19,66 @@
 namespace Acts {
 namespace Test {
 
+  ///
+  /// @brief Stub implementation of the detector element
+  ///
+  class DetElem : public DetectorElementBase
+  {
+  public:
+    /// @brief Constructor
+    ///
+    /// @param [in] trafo Transformation of the detector element
+    /// @param [in] rBounds Rectangle boundaries of the plane surface
+    /// @param [in] thickness Thickness of the detector element
+    DetElem(std::shared_ptr<const Transform3D>     trafo,
+            std::shared_ptr<const RectangleBounds> rBounds,
+            double                                 thickness)
+      : DetectorElementBase()
+      , m_trafo(trafo)
+      , m_surface(new PlaneSurface(rBounds, *this))
+      , m_thickness(thickness)
+    {
+    }
+
+    /// @brief Getter of the transformation
+    virtual const Transform3D&
+    transform() const
+    {
+      return *m_trafo;
+    }
+
+    /// @brief Getter of the surface
+    virtual const Surface&
+    surface() const
+    {
+      return *m_surface;
+    }
+
+    /// @brief Getter of the thickness
+    virtual double
+    thickness() const
+    {
+      return m_thickness;
+    }
+
+    // Pointer to the transformation
+    std::shared_ptr<const Transform3D> m_trafo;
+    // Surface related to the detector element
+    Surface const* m_surface;
+    // Thickness of the detector element
+    double m_thickness;
+  };
+  
   BOOST_AUTO_TEST_CASE(BoxGeometryBuilderTest)
   {
+	  // Construct builder
 	BoxGeometryBuilder bgb;
 	
+	// Create configurations for surfaces
 	std::vector<BoxGeometryBuilder::SurfaceConfig> surcfg;
 	for(unsigned int i = 1; i < 5; i++)
 	{
+		// Position of the surfaces
 		BoxGeometryBuilder::SurfaceConfig sc;
 		sc.position = {i * units::_m, 0., 0.};
 
@@ -33,7 +86,7 @@ namespace Test {
 				std::shared_ptr<const SurfaceMaterial> surMat;
 				double thickness;
 				
-		// Construct the rotation
+		// Rotation of the surfaces
 		double           rotationAngle = M_PI * 0.5;
 		Vector3D         xPos(cos(rotationAngle), 0., sin(rotationAngle));
 		Vector3D         yPos(0., 1., 0.);
@@ -50,11 +103,20 @@ namespace Test {
 		MaterialProperties matProp(352.8, 407., 9.012, 4., 1.848e-3, 0.5 * units::_mm);
 		sc.surMat = std::shared_ptr<const SurfaceMaterial>(new HomogeneousSurfaceMaterial(matProp));
 		
+		// Thickness of the detector element
 		sc.thickness = 1. * units::_um;
+		
 		surcfg.push_back(sc);
 	}
-   
+	
    BOOST_TEST(surcfg.size() == 4);
+   
+   bgb.buildSensitiveSurfaces<DetElem>(surcfg);
+   for(const auto& sc : surcfg)
+	BOOST_TEST(sc.surface != nullptr);
+   bgb.buildPassiveSurfaces(surcfg);
+   for(const auto& sc : surcfg)
+	BOOST_TEST(sc.surface != nullptr);
   }
 }  // namespace Test
 }  // namespace Acts
