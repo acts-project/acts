7042190fa21ef6233779f8c42795dc7fbd7b8e3e
Paul Gessinger <paul.gessinger@cern.ch>
2018-11-02 18:45:32 +0100

    rewrite coverage deploy script

--- CI/publish_coverage.py
+++ CI/publish_coverage.py
@@ -1,59 +1,87 @@
-#!/usr/bin/env python
-from __future__ import print_function
+#!/usr/bin/env python3
 import os
 import sys
 import argparse
 import subprocess as sp
 import requests
-from urllib import quote_plus
-from urlparse import urljoin
 import tempfile
+from urllib.parse import urljoin
 
-base_url = "https://gitlab.cern.ch/api/v4/"
-ssh_config_file = os.path.join(os.path.dirname(__file__), "ssh_config")
+from fs.sshfs import SSHFS
+from fs.osfs import OSFS
+import fs.copy
+import gitlab
+from datetime import datetime
+from dateutil.parser import parse
 
-def env_or_val(key, default):
-    if key in os.environ:
-        return os.environ[key]
-    else:
-        return default
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--deploy-user", default="atsjenkins")
+    p.add_argument("--deploy-pwd", default=os.getenv("ATSJENKINS_PASSWORD"))
+    p.add_argument("--coverage-source", required=True)
+    p.add_argument("--commit-hash", default=os.getenv("CI_COMMIT_SHA"))
+    p.add_argument("--coverage-commit-limit", default=int(os.getenv("COVERAGE_COMMIT_LIMIT", 10)), type=int)
+    p.add_argument("--coverage-root", default=os.getenv("COVERAGE_WEBSITE_ROOT", "/eos/user/a/atsjenkins/www/ACTS/coverage"))
+    p.add_argument("--website-public-url", default=os.getenv("COVERAGE_WEBSITE_URL", "https://acts.web.cern.ch/ACTS/coverage/"))
 
-def get_commit(project, commit):
-    slug = quote_plus(project, safe="")
-    url = base_url+"projects/"+slug+"/repository/commits/{}".format(commit)
-    # print(url)
-    res = requests.get(url, params={})
-    data = res.json()
-    return data
+    args = p.parse_args()
 
-def ssh_cmd(cmd):
-    return "ssh atsjenkins@lxplus.cern.ch -F {} \"{}\"".format(ssh_config_file, cmd)
+    try:
+        www_fs = SSHFS(host="lxplus.cern.ch",
+                       user=args.deploy_user,
+                       passwd=args.deploy_pwd).opendir(args.coverage_root)
+        # www_fs = OSFS("www")
+        listdir = www_fs.listdir(".")
+    except:
+        print("Unable to establish SSH connection to lxplus")
+        print("This might indicate a problem with the credentials")
+        print("or a temporary connection / configuration problem")
+        sys.exit(1)
+
+    gl = gitlab.Gitlab("https://gitlab.cern.ch/")
+    project = gl.projects.get(3031) # acts/acts-core
+
+    commit_slug = args.commit_hash[:7]
+    coverage_dest = os.path.join(args.coverage_root, commit_slug)
+    print("Going to deploy coverage for", commit_slug, "to", coverage_dest)
+    print("Will be publicly available under", urljoin(args.website_public_url, commit_slug))
+
+    src_fs = OSFS(args.coverage_source)
+
+    fs.copy.copy_dir(src_fs, ".", www_fs, commit_slug)
+
+    # cleanup
+    # get all deployed commits
+    deployed_commits = set(filter(www_fs.isdir, www_fs.listdir(".")))
+    deployed_time = [parse(project.commits.get(c).committed_date) for c in deployed_commits]
+
+    deployed_commits_with_time = list(reversed(sorted(zip(deployed_commits, deployed_time), key=lambda i: i[1])))
+
+    # take the n newest commits
+    commits_to_keep = set(h for h,_ in deployed_commits_with_time[:args.coverage_commit_limit])
+
+    print("Currently deployed commits:")
+    for idx, (h, t) in enumerate(deployed_commits_with_time):
+        if idx < args.coverage_commit_limit:
+            print(" o", h, "-", t)
+        else:
+            print(" x", h, "-", t)
 
 
-p = argparse.ArgumentParser()
-p.add_argument("--commit-hash", default=os.environ["CI_COMMIT_SHA"])
-p.add_argument("--coverage-commit-limit", default=int(env_or_val("COVERAGE_COMMIT_LIMIT", 10)), type=int)
-p.add_argument("--website-root", default=env_or_val("COVERAGE_WEBSITE_ROOT", "/eos/user/a/atsjenkins/www/ACTS"))
-p.add_argument("--website-public-url", default=env_or_val("COVERAGE_WEBSITE_URL", "https://acts.web.cern.ch/ACTS/coverage/"))
+    print("Keeping commits:", ", ".join(commits_to_keep))
 
-args = p.parse_args()
+    commits_to_delete = deployed_commits - commits_to_keep
 
+    if len(commits_to_delete) > 0:
+        print("Removing:", ", ".join(commits_to_delete))
 
-COMMIT_HASH = args.commit_hash
-coverage_commit_limit = args.coverage_commit_limit
-WEBSITE_ROOT = args.website_root
-commit_slug = COMMIT_HASH[:7]
-coverage_base = os.path.join(WEBSITE_ROOT, "coverage")
-coverage_dest = os.path.join(coverage_base, commit_slug)
-coverage_src = os.path.join(os.getcwd(), "build/coverage/")
-base_public_url = args.website_public_url
+        for commit in commits_to_delete:
+            www_fs.removetree(commit)
 
-
-def install_index(commit):
-    latest_coverage_url = urljoin(base_public_url, commit[:7])
-    print("Installing index redirect to", latest_coverage_url)
-    with tempfile.NamedTemporaryFile(mode="w+") as f:
-        content = """
+    # install / update indexfile
+    latest_commit = deployed_commits_with_time[0][0]
+    latest_coverage_url = urljoin(args.website_public_url, latest_commit)
+    index_content = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -63,70 +91,11 @@ def install_index(commit):
 Redirecting to <a href"{0}">{0}</a>
 </body>
 </html>
-        """
-        f.write(content.format(latest_coverage_url))
-        f.flush()
-        index_dest = os.path.join(coverage_base, "index.html")
-        scp_cmd = "scp -F {} {} atsjenkins@lxplus.cern.ch:{}".format(ssh_config_file, f.name, index_dest)
-        print(scp_cmd)
-        print(sp.check_output(scp_cmd, shell=True))
+    """.format(latest_coverage_url)
 
+    with www_fs.open("index.html", "w") as f:
+        print("Writing index file redirecting to", latest_coverage_url)
+        f.write(index_content)
 
-
-print("Going to deploy coverage for", COMMIT_HASH, "to", coverage_dest)
-print("Will be publicly available under", urljoin(base_public_url, commit_slug))
-
-if not os.path.exists(coverage_src):
-    print("Coverage path does not exist")
-    sys.exit(1)
-
-
-mkdir_cmd = "ssh atsjenkins@lxplus.cern.ch -F {} \"mkdir -p {}\"".format(ssh_config_file, coverage_dest)
-print(mkdir_cmd)
-print(sp.check_output(mkdir_cmd, shell=True))
-
-copy_cmd = "rsync -e \"ssh -F {}\" -ruv {} atsjenkins@lxplus.cern.ch:{}".format(ssh_config_file, coverage_src, coverage_dest)
-print(copy_cmd)
-print(sp.check_output(copy_cmd, shell=True))
-
-
-
-# figure out what's deployed right now
-ls_cmd = ssh_cmd("ls {}".format(coverage_base))
-print(ls_cmd)
-output = sp.check_output(ls_cmd, shell=True).decode("utf-8")
-commits = output.strip().split("\n")
-commits = [c for c in commits if c != "index.html"]
-
-print("Currently have", len(commits), "deployed, commits to keep is", coverage_commit_limit)
-
-commit_info = map(lambda c: (c, get_commit("acts/acts-core", c)), commits)
-    
-commits_to_delete = [h for h,c in commit_info if "committed_date" not in c]
-commit_info = [c for h,c in commit_info if "committed_date" in c]
-commit_sorted = list(reversed(sorted(commit_info, key=lambda c: c["committed_date"])))
-
-print()
-
-for c in commit_sorted:
-    print(c["id"], c["committed_date"])
-
-commits_to_delete += map(lambda c: c["id"], commit_sorted[coverage_commit_limit:])
-
-install_index(commit_sorted[0]["id"])
-
-if len(commits_to_delete) > 0:
-    print("performing cleanup")
-
-    # for c in commit_info:
-        # print(c)
-    for c in commits_to_delete:
-        commit_dir = c[:7]
-        print("Deleting", commit_dir)
-        assert commit_dir != ""
-        rm_cmd = ssh_cmd("rm -rf {}".format(os.path.join(coverage_base, commit_dir)))
-        print(rm_cmd)
-        # print(sp.check_outputr(rm_cmd, shell=True))
-else:
-    print("no cleanup required")
-
+if "__main__" == __name__:
+    main()
