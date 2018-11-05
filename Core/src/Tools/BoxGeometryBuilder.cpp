af6ec1e9779b0219bcf088b1fd4b9f47c93ad23e
FabianKlimpel <fabian.klimpel@tum.de>
2018-11-05 09:19:05 +0100

    Changed the config structure; added templated sensitive/passive selection

--- Core/src/Tools/BoxGeometryBuilder.cpp
+++ Core/src/Tools/BoxGeometryBuilder.cpp
@@ -12,40 +12,4 @@
 
 #include "Acts/Tools/BoxGeometryBuilder.hpp"
 
-void
-Acts::BoxGeometryBuilder::buildPassiveSurfaces(std::vector<Config>& config) const
-{
-	PlaneSurface* surface;
-	
-	for(Config& cfg : config)
-	{
-		// Build transformation
-	  Transform3D trafo(Transform3D::Identity() * cfg.surfaceCfg.rotation);
-	  trafo.translation() = cfg.surfaceCfg.position;
 
-		// Create and store surface
-	  surface = new PlaneSurface(std::make_shared<const Transform3D>(trafo), cfg.surfaceCfg.rBounds);
-	  surface->setAssociatedMaterial(cfg.surfaceCfg.surMat);
-	  cfg.surface = surface;
-	}
-}
-			
-void
-Acts::BoxGeometryBuilder::buildLayers(std::vector<Config>& config) const
-{
-	// TODO: thickness of layer
-	for (Config& cfg : config) {
-		// Build transformation centered at the surface position
-	  Transform3D trafo(Transform3D::Identity() * cfg.surfaceCfg.rotation);
-	  trafo.translation() = cfg.surfaceCfg.position;
-	  
-	  // Get (1!) surface, build layer and store it
-	  std::unique_ptr<SurfaceArray> surArray(new SurfaceArray(cfg.surface));
-
-	  cfg.layer = PlaneLayer::create(std::make_shared<const Transform3D>(trafo),
-									 cfg.surfaceCfg.rBounds,
-									 std::move(surArray),
-									 1. * units::_mm);
-	  cfg.surface->associateLayer(*cfg.layer);
-	}
-}
