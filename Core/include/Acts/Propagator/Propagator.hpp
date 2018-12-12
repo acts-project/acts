2d3b8c7ddf0e19b09d5cbdd73e0483ef7aa9c227
FabianKlimpel <fabian.klimpel@tum.de>
2018-12-12 12:47:39 +0100

    Format2

--- Core/include/Acts/Propagator/Propagator.hpp
+++ Core/include/Acts/Propagator/Propagator.hpp
@@ -13,7 +13,6 @@
 #include <memory>
 #include <type_traits>
 #include "Acts/EventData/TrackParameters.hpp"
-#include "Acts/EventData/TrackParameters.hpp"
 #include "Acts/Propagator/AbortList.hpp"
 #include "Acts/Propagator/ActionList.hpp"
 #include "Acts/Propagator/detail/LoopProtection.hpp"
