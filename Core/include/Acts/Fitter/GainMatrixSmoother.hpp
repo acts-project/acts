163a11e7d763aa82502bd99e4b87d776bc004bc0
Paul Gessinger <paul.gessinger@cern.ch>
2019-01-08 19:52:45 +0100

    Reshuffle impl of GainMatrixSmoother a bit
    (same functionality right now)

--- Core/include/Acts/Fitter/GainMatrixSmoother.hpp
+++ Core/include/Acts/Fitter/GainMatrixSmoother.hpp
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include <boost/range/adaptors.hpp>
 #include <boost/variant.hpp>
 #include <memory>
 #include "Acts/EventData/TrackParameters.hpp"
@@ -24,6 +25,7 @@ class GainMatrixSmoother
 {
 
   using jacobian_t = typename parameters_t::CovMatrix_t;
+
 public:
   /// @todo write documentation
 
@@ -31,57 +33,68 @@ public:
   boost::optional<parameters_t>
   operator()(track_states_t& filteredStates) const
   {
-    // The reverse iteration
-    auto rit = filteredStates.rbegin();
+    using namespace boost::adaptors;
 
-    using ParVector_t = typename parameters_t::ParVector_t;
-    using CovMatrix_t = typename parameters_t::CovMatrix_t;
+    using track_state_t = typename track_states_t::value_type;
+    using ParVector_t   = typename parameters_t::ParVector_t;
+    using CovMatrix_t   = typename parameters_t::CovMatrix_t;
+    using gain_matrix_t = CovMatrix_t;
+
     // smoothed parameter vector and covariance matrix
     ParVector_t smoothedPars;
     CovMatrix_t smoothedCov;
 
     // For the last state: smoothed is filtered - also: switch to next
-    auto& lState    = rit->parameter;
-    lState.smoothed = lState.filtered.get();
+    auto& prev_ts              = filteredStates.back();
+    prev_ts.parameter.smoothed = *prev_ts.parameter.filtered;
 
     // Smoothing gain matrix
-    using GMatrix = ActsMatrixD<Acts::NGlobalPars, Acts::NGlobalPars>;
-    GMatrix G;
+    gain_matrix_t G;
 
     // Loop and smooth the remaining states
-    for (++rit; rit != filteredStates.rend(); ++rit) {
+    for (track_state_t& ts :
+         filteredStates | sliced(0, filteredStates.size()) | reversed) {
 
       // The current state
-      auto& cState = rit->parameter;
+      assert(ts.parameter.filtered);
+      assert(ts.parameter.predicted);
+      assert(ts.parameter.jacobian);
+      assert(ts.parameter.predicted->covariance() != nullptr);
+      assert(ts.parameter.filtered->covariance() != nullptr);
 
-      /// Gain smoothing matrix
-      G = (*cState.filtered.get().covariance())
-          * cState.jacobian.get().transpose()
-          * (*lState.predicted.get().covariance()).inverse();
+      assert(prev_ts.parameter.smoothed);
+      assert(prev_ts.parameter.predicted);
+
+      // clang-format off
+
+      // Gain smoothing matrix
+      G = (*ts.parameter.filtered->covariance())
+          * ts.parameter.jacobian->transpose()
+          * (*prev_ts.parameter.predicted->covariance()).inverse();
       // Calculate the smoothed parameters
-      smoothedPars = cState.filtered.get().parameters()
-          + G * (lState.smoothed.get().parameters()
-                 - lState.predicted.get().parameters());
+      smoothedPars = ts.parameter.filtered->parameters()
+                     + G * (prev_ts.parameter.smoothed->parameters()
+                            - prev_ts.parameter.predicted->parameters());
 
       // And the smoothed covariance
-      smoothedCov = (*cState.filtered.get().covariance())
-          - G * (*(lState.predicted.get().covariance())
-                 - (*lState.smoothed.get().covariance()))
-              * G.transpose();
+      smoothedCov = (*ts.parameter.filtered->covariance())
+                    - G * (*(prev_ts.parameter.predicted->covariance())
+                           - (*prev_ts.parameter.smoothed->covariance()))
+                           * G.transpose();
+
+      // clang-format on
 
       // Create smoothed track parameters
-      parameters_t smoothed(
-          std::make_unique<CovMatrix_t>(std::move(smoothedCov)),
-          smoothedPars,
-          cState.filtered.get().referenceSurface().getSharedPtr());
+      ts.parameter.smoothed
+          = parameters_t(std::make_unique<CovMatrix_t>(std::move(smoothedCov)),
+                         smoothedPars,
+                         ts.referenceSurface().getSharedPtr());
 
-      cState.smoothed = std::move(smoothed);
-
-      // Point last state to current state
-      lState = cState;
+      // Point prev state to current state
+      prev_ts = ts;
     }
     // The result is the pointer to the last smoothed state - for the cache
-    return lState.smoothed;
+    return prev_ts.parameter.smoothed;
   }
 };
 }
