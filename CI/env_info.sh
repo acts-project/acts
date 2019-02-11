9712327970a5e4cb2db9adcbb1cdab57e84458a2
Stewart Martin-Haugh <smh@cern.ch>
2019-02-11 15:44:45 +0100

    Update to GCC 8

--- CI/env_info.sh
+++ CI/env_info.sh
@@ -3,7 +3,7 @@ if [ "$(cat /etc/redhat-release | grep '^Scientific Linux .*6.*')" ]; then
   export ACTS_COMPILER=gcc62-opt
 elif [ "$(cat /etc/centos-release | grep 'CentOS Linux release 7')" ]; then
   export ACTS_OS=centos7
-  export ACTS_COMPILER=gcc7-opt
+  export ACTS_COMPILER=gcc8-opt
 else
   echo "Unknown OS" 1>&2
   exit 1
