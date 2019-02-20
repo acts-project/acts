940aebf8de120bee895e2f637851231857287f7b
FabianKlimpel <fabian.klimpel@tum.de>
2019-02-20 11:11:44 +0100

    Merged both classes and removed unnecessary functions

--- Plugins/MaterialMapping/include/Acts/Plugins/MaterialMapping/AccumulatedVolumeMaterial.hpp
+++ Plugins/MaterialMapping/include/Acts/Plugins/MaterialMapping/AccumulatedVolumeMaterial.hpp
@@ -12,43 +12,35 @@
 
 #pragma once
 
-#include "Acts/Material/MaterialProperties.hpp"
-#include "Acts/Plugins/MaterialMapping/AccumulatedMaterialProperties.hpp"
-#include "Acts/Utilities/Definitions.hpp"
+#include "Acts/Material/Material.hpp"
 
 namespace Acts {
-
-// TODO: probably combine both classes into a single one
-struct AccumulatedMaterial
+/// @class AccumulatedVolumeMaterial
+///
+/// This class is used by the VolumeMaterialMapper in order to
+/// accumulate/collect material information during the mapping process. The object represents the collection of a single grid point.
+///
+/// It calculates the average of the material classification values when called, and returns a material with the averaged properties.
+class AccumulatedVolumeMaterial
 {
-   void
-  accumulate(const Material& mat)
-  {
-	  m_eventX0 += mat.X0();
-	  m_eventL0 += mat.L0();
-	  m_eventA += mat.A();
-	  m_eventZ += mat.Z();
-	  m_eventRho += mat.rho();
-	  m_totalEntries++;
-	}
+public:
+  /// @brief Default constructor
+  AccumulatedVolumeMaterial() = default;
 
-  std::pair<Material, unsigned int>
-  totalAverage()
-  {
-	    if (m_totalEntires > 0) {
-		double eventScalor = 1. / (double) m_totalEntries;
-		m_totalX0 *= eventScalor;
-		m_totalL0 *= eventScalor;
-		m_totalA *= eventScalor;
-		m_totalZ *= eventScalor;
-		m_totalRho *= eventScalor;
-		// Create the material
-		Material mat(m_totalX0, m_totalL0, m_totalA, m_totalZ, m_totalRho);
-		return std::pair<Material, unsigned int>(std::move(mat), m_totalEntries);
-	}
-	else{
-	return std::pair<Material, unsigned int>(Material(), m_totalEntries);}
-  }
+  /// @brief Default destructor
+  ~AccumulatedVolumeMaterial() = default;
+
+  /// @brief This function collects the classification values of a material in the container
+  ///
+  /// @param [in] mat Material that will be collected
+  void
+  accumulate(const Material& mat);
+
+  /// @brief Total average of each material classification value stored in the object independently
+  ///
+  /// @return Material consisting of the averaged values
+  Material
+  totalAverage();
 
 private:
   float m_eventX0{0.};  //!< event: accumulate the contribution to X0
@@ -59,140 +51,4 @@ private:
 
   unsigned int m_totalEntries{0};  //!< the number of events
 };
-
-/// @class AccumulatedSurfaceMaterial
-///
-/// This class is used by the SurfaceMaterialMapper in order to
-/// accumulate/collect material information during the mapping process.
-///
-/// It performs event- and run-average when called, and returns
-/// a new SurfaceMaterial object as a unique_ptr after finalisation
-class AccumulatedVolumeMaterial
-{
-public:
-  using AccumulatedVector = std::vector<AccumulatedMaterialProperties>;
-  using AccumulatedMatrix = std::vector<AccumulatedVector>;
-
-  /// Default Constructor - for homogeneous material
-  ///
-  /// @param splitFactor is the pre/post splitting directive
-  AccumulatedSurfaceMaterial(double splitFactor = 0.);
-
-  /// Explicit constructor with only full MaterialProperties,
-  /// for one-dimensional binning.
-  ///
-  /// The split factors:
-  ///    - 1. : oppositePre
-  ///    - 0. : alongPre
-  ///  ===> 1 Dimensional array
-  ///
-  /// @param binUtility defines the binning structure on the surface
-  /// @param splitFactor is the pre/post splitting directive
-  AccumulatedSurfaceMaterial(const BinUtility& binUtility,
-                             double            splitFactor = 0.);
-
-  /// Copy Constructor
-  ///
-  /// @param asma is the source object to be copied
-  AccumulatedSurfaceMaterial(const AccumulatedSurfaceMaterial& asma) = default;
-
-  /// Copy Move Constructor
-  ///
-  /// @param asma is the source object to be copied
-  AccumulatedSurfaceMaterial(AccumulatedSurfaceMaterial&& asma) = default;
-
-  /// Assignment Move operator
-  ///
-  /// @param asma is the source object to be copied
-  AccumulatedSurfaceMaterial&
-  operator=(AccumulatedSurfaceMaterial&& asma)
-      = default;
-
-  /// Assignment operator
-  ///
-  /// @param asma is the source object to be copied
-  AccumulatedSurfaceMaterial&
-  operator=(const AccumulatedSurfaceMaterial& asma)
-      = default;
-
-  /// Destructor
-  ~AccumulatedSurfaceMaterial() = default;
-
-  /// Return the BinUtility
-  const BinUtility&
-  binUtility() const;
-
-  /// Assign a material properites object
-  ///
-  /// @param lp local position for the bin assignment
-  /// @param mp material properties to be assigned
-  void
-  accumulate(const Vector2D&           lp,
-             const MaterialProperties& mp,
-             double                    pathCorrection = 1.);
-
-  /// Assign a material properites object
-  ///
-  /// @param gp local position for the bin assignment
-  /// @param mp material properties to be assigned
-  void
-  accumulate(const Vector3D&           gp,
-             const MaterialProperties& mp,
-             double                    pathCorrection = 1.);
-
-  /// Assign a material properites object
-  ///
-  /// @param gp local position for the bin assignment
-  /// @param mps Vector of recorded material properties to be assigned
-  /// @param pathCorrection The geometric path correction due to incident
-  void
-  accumulate(const Vector3D& gp,
-             const std::vector<std::pair<MaterialProperties, Vector3D>>& mps,
-             double pathCorrection = 1.);
-
-  /// Average the information accumulated during one event
-  /// using the event weights
-  void
-  eventAverage();
-
-  /// Total average creates SurfaceMaterial
-  std::unique_ptr<const SurfaceMaterial>
-  totalAverage();
-
-  /// Access to the accumulated material
-  const AccumulatedMatrix&
-  accumulatedMaterial() const;
-
-  /// Access to the split factor
-  double
-  splitFactor() const;
-
-private:
-  /// The helper for the bin finding
-  BinUtility m_binUtility{};
-
-  /// the split factor
-  double m_splitFactor{0.};
-
-  /// The stored accumulated material matrix
-  AccumulatedMatrix m_accumulatedMaterial;
-};
-
-inline const BinUtility&
-AccumulatedSurfaceMaterial::binUtility() const
-{
-  return (m_binUtility);
-}
-
-inline const AccumulatedSurfaceMaterial::AccumulatedMatrix&
-AccumulatedSurfaceMaterial::accumulatedMaterial() const
-{
-  return (m_accumulatedMaterial);
-}
-
-inline double
-AccumulatedSurfaceMaterial::splitFactor() const
-{
-  return m_splitFactor;
-}
 }
--- Plugins/MaterialMapping/src/AccumulatedVolumeMaterial.cpp
+++ Plugins/MaterialMapping/src/AccumulatedVolumeMaterial.cpp
@@ -1,120 +1,43 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2018 Acts project team
+// Copyright (C) 2019 Acts project team
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 ///////////////////////////////////////////////////////////////////
-// AccumulatedSurfaceMaterial.cpp, Acts project
+// AccumulatedVolumeMaterial.cpp, Acts project
 ///////////////////////////////////////////////////////////////////
 
-#include "Acts/Plugins/MaterialMapping/AccumulatedSurfaceMaterial.hpp"
-#include "Acts/Material/BinnedSurfaceMaterial.hpp"
-#include "Acts/Material/HomogeneousSurfaceMaterial.hpp"
+#include "Acts/Plugins/MaterialMapping/AccumulatedVolumeMaterial.hpp"
 
-// Default Constructor - for homogeneous material
-Acts::AccumulatedSurfaceMaterial::AccumulatedSurfaceMaterial(double splitFactor)
-  : m_splitFactor(splitFactor)
-{
-  AccumulatedVector accMat = {{AccumulatedMaterialProperties()}};
-  m_accumulatedMaterial    = {{accMat}};
-}
-
-// Binned Material constructor with split factor
-Acts::AccumulatedSurfaceMaterial::AccumulatedSurfaceMaterial(
-    const BinUtility& binUtility,
-    double            splitFactor)
-  : m_binUtility(binUtility), m_splitFactor(splitFactor)
-{
-  size_t            bins0 = m_binUtility.bins(0);
-  size_t            bins1 = m_binUtility.bins(1);
-  AccumulatedVector accVec(bins0, AccumulatedMaterialProperties());
-  m_accumulatedMaterial = AccumulatedMatrix(bins1, accVec);
-}
-
-// Assign a material properites object
 void
-Acts::AccumulatedSurfaceMaterial::accumulate(const Vector2D&           lp,
-                                             const MaterialProperties& mp,
-                                             double pathCorrection)
+Acts::AccumulatedVolumeMaterial::accumulate(
+    const Material& mat)
 {
-  if (m_binUtility.dimensions() == 0) {
-    m_accumulatedMaterial[0][0].accumulate(mp, pathCorrection);
-  } else {
-    size_t bin0 = m_binUtility.bin(lp, 0);
-    size_t bin1 = m_binUtility.bin(lp, 1);
-    m_accumulatedMaterial[bin1][bin0].accumulate(mp, pathCorrection);
-  }
+	  m_eventX0 += mat.X0();
+	  m_eventL0 += mat.L0();
+	  m_eventA += mat.A();
+	  m_eventZ += mat.Z();
+	  m_eventRho += mat.rho();
+	  m_totalEntries++;
 }
 
-// Assign a material properites object
-void
-Acts::AccumulatedSurfaceMaterial::accumulate(const Vector3D&           gp,
-                                             const MaterialProperties& mp,
-                                             double pathCorrection)
+Acts::Material
+Acts::AccumulatedVolumeMaterial::eventAverage()
 {
-  if (m_binUtility.dimensions() == 0) {
-    m_accumulatedMaterial[0][0].accumulate(mp, pathCorrection);
-  } else {
-    std::array<size_t, 3> bTriple = m_binUtility.binTriple(gp);
-    m_accumulatedMaterial[bTriple[1]][bTriple[0]].accumulate(mp,
-                                                             pathCorrection);
-  }
-}
-
-// Assign a vector of material properites object
-void
-Acts::AccumulatedSurfaceMaterial::accumulate(
-    const Vector3D& gp,
-    const std::vector<std::pair<MaterialProperties, Vector3D>>& mps,
-    double pathCorrection)
-{
-  if (m_binUtility.dimensions() == 0) {
-    for (auto mp : mps) {
-      m_accumulatedMaterial[0][0].accumulate(mp.first, pathCorrection);
-    }
-  } else {
-    std::array<size_t, 3> bTriple = m_binUtility.binTriple(gp);
-    for (auto mp : mps) {
-      m_accumulatedMaterial[bTriple[1]][bTriple[0]].accumulate(mp.first,
-                                                               pathCorrection);
-    }
-  }
-}
-
-// Average the information accumulated during one event
-void
-Acts::AccumulatedSurfaceMaterial::eventAverage()
-{
-  for (auto& matVec : m_accumulatedMaterial) {
-    for (auto& mat : matVec) {
-      mat.eventAverage();
-    }
-  }
-}
-
-/// Total average creates SurfaceMaterial
-std::unique_ptr<const Acts::SurfaceMaterial>
-Acts::AccumulatedSurfaceMaterial::totalAverage()
-{
-  if (m_binUtility.bins() == 1) {
-    // Return HomogeneousSurfaceMaterial
-    return std::make_unique<HomogeneousSurfaceMaterial>(
-        m_accumulatedMaterial[0][0].totalAverage().first, m_splitFactor);
-  }
-  // Create the properties matrix
-  MaterialPropertiesMatrix mpMatrix(
-      m_binUtility.bins(1),
-      MaterialPropertiesVector(m_binUtility.bins(0), MaterialProperties()));
-  // Loop over and fill
-  for (size_t ib1 = 0; ib1 < m_binUtility.bins(1); ++ib1) {
-    for (size_t ib0 = 0; ib0 < m_binUtility.bins(0); ++ib0) {
-      mpMatrix[ib1][ib0] = m_accumulatedMaterial[ib1][ib0].totalAverage().first;
-    }
-  }
-  // Now return the BinnedSurfaceMaterial
-  return std::make_unique<const BinnedSurfaceMaterial>(
-      m_binUtility, std::move(mpMatrix), m_splitFactor);
+	if (m_totalEntires > 0) {
+		double eventScalor = 1. / (double) m_totalEntries;
+		m_totalX0 *= eventScalor;
+		m_totalL0 *= eventScalor;
+		m_totalA *= eventScalor;
+		m_totalZ *= eventScalor;
+		m_totalRho *= eventScalor;
+		// Create the material
+		return Material(m_totalX0, m_totalL0, m_totalA, m_totalZ, m_totalRho);
+	}
+	else{
+		// Create vacuum
+	return Material();}
 }
