f11af9fca0b79053bbff8543cb4e9af71345062c
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2019-05-02 17:06:38 +0200

    fix approach/sensitive material assignment

--- Core/src/Layers/Layer.cpp
+++ Core/src/Layers/Layer.cpp
@@ -69,7 +69,6 @@ Acts::Layer::closeGeometry(const IMaterialDecorator* materialDecorator,
   if (surfaceRepresentation().surfaceMaterial() != nullptr) {
     m_ssRepresentingSurface = 2;
   }
-
   // loop over the approach surfaces
   if (m_approachDescriptor) {
     // indicates the existance of approach surfaces
@@ -82,7 +81,7 @@ Acts::Layer::closeGeometry(const IMaterialDecorator* materialDecorator,
       auto mutableASurface = const_cast<Surface*>(aSurface);
       mutableASurface->assignGeoID(asurfaceID);
       if (materialDecorator != nullptr) {
-        materialDecorator->decorate(*rSurface);
+        materialDecorator->decorate(*mutableASurface);
       }
       // if any of the approach surfaces has material
       if (aSurface->surfaceMaterial() != nullptr) {
@@ -102,7 +101,7 @@ Acts::Layer::closeGeometry(const IMaterialDecorator* materialDecorator,
       auto mutableSSurface = const_cast<Surface*>(sSurface);
       mutableSSurface->assignGeoID(ssurfaceID);
       if (materialDecorator != nullptr) {
-        materialDecorator->decorate(*rSurface);
+        materialDecorator->decorate(*mutableSSurface);
       }
       // if any of the sensitive surfaces has material
       if (sSurface->surfaceMaterial() != nullptr) {
