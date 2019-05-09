1e9d64efe5e27b48e5158a7c9d6b8a7ab28e229e
Fabian Klimpel <fklimpel@cern.ch>
2019-05-09 13:08:30 +0200

    P4

--- Tests/Integration/PropagationTestHelper.hpp
+++ Tests/Integration/PropagationTestHelper.hpp
@@ -378,7 +378,7 @@ void covariance_curvilinear(const Propagator_type& propagator, double pT,
   options.maxStepSize = plimit;
   options.pathLimit = plimit;
   options.debug = debug;
-  options.tolerance = 2e-7;
+  options.tolerance = 1e-9;
   options.propagateTime = true;
 
   // define start parameters
