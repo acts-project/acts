544cc48a1ede89d4c03b820b0e5af94ad66eb7d1
Moritz Kiehn <msmk@cern.ch>
2019-05-24 16:40:01 +0200

    Tests: update units in event data tests

--- Tests/Core/EventData/CurvilinearParametersTests.cpp
+++ Tests/Core/EventData/CurvilinearParametersTests.cpp
@@ -28,20 +28,20 @@ namespace Test {
 /// @brief Unit test for Curvilinear parameters
 ///
 BOOST_AUTO_TEST_CASE(curvilinear_initialization) {
+  using namespace Acts::UnitLiterals;
+
   // Create a test context
   GeometryContext tgContext = GeometryContext();
 
   // some position and momentum
-  Vector3D pos(1.34 * units::_mm, 2.34 * units::_mm, 3.45 * units::_mm);
-  Vector3D mom(1000. * units::_GeV, 1000. * units::_GeV, -0.100 * units::_GeV);
+  Vector3D pos(1.34_mm, 2.34_mm, 3.45_mm);
+  Vector3D mom(1000_GeV, 1000_GeV, -0.100_GeV);
   Vector3D dir(mom.normalized());
   Vector3D z_axis_global(0., 0., 1.);
   /// create curvilinear parameters without covariance +1/-1 charge
-  CurvilinearParameters curvilinear_pos(nullptr, pos, mom, 1. * units::_e,
-                                        1. * units::_s);
-  CurvilinearParameters curvilinear_neg(nullptr, pos, mom, -1. * units::_e,
-                                        2.5 * units::_s);
-  NeutralCurvilinearParameters curvilinear_neut(nullptr, pos, mom, 33.33);
+  CurvilinearParameters curvilinear_pos(nullptr, pos, mom, 1_e, 1_s);
+  CurvilinearParameters curvilinear_neg(nullptr, pos, mom, -1_e, 2.5_s);
+  NeutralCurvilinearParameters curvilinear_neut(nullptr, pos, mom, 33.33_s);
 
   /// check local coordinates
   const auto fphi = phi(mom);
@@ -49,12 +49,12 @@ BOOST_AUTO_TEST_CASE(curvilinear_initialization) {
   const double oOp = 1. / mom.norm();
 
   // check parameters
-  consistencyCheck(curvilinear_pos, pos, mom, +1. * units::_e, 1. * units::_s,
-                   {{0., 0., fphi, ftheta, oOp, 1. * units::_s}});
-  consistencyCheck(curvilinear_neg, pos, mom, -1. * units::_e, 2.5 * units::_s,
-                   {{0., 0., fphi, ftheta, -oOp, 2.5 * units::_s}});
-  consistencyCheck(curvilinear_neut, pos, mom, 0., 33.33 * units::_s,
-                   {{0., 0., fphi, ftheta, oOp, 33.33 * units::_s}});
+  consistencyCheck(curvilinear_pos, pos, mom, +1_e, 1_s,
+                   {0., 0., fphi, ftheta, oOp, 1_s});
+  consistencyCheck(curvilinear_neg, pos, mom, -1_e, 2.5_s,
+                   {0., 0., fphi, ftheta, -oOp, 2.5_s});
+  consistencyCheck(curvilinear_neut, pos, mom, 0., 33.33_s,
+                   {0., 0., fphi, ftheta, oOp, 33.33_s});
 
   // check that the created surface is at the position
   CHECK_CLOSE_REL(curvilinear_pos.referenceSurface().center(tgContext), pos,
@@ -98,8 +98,8 @@ BOOST_AUTO_TEST_CASE(curvilinear_initialization) {
   BOOST_CHECK_EQUAL(curvilinear_neut_copy, curvilinear_neut);
 
   /// modification test with set methods
-  double ux = 0.1 * units::_mm;
-  double uy = 0.5 * units::_mm;
+  double ux = 0.1_mm;
+  double uy = 0.5_mm;
   curvilinear_pos_copy.set<eLOC_0>(tgContext, ux);
   curvilinear_pos_copy.set<eLOC_1>(tgContext, uy);
   // the local parameter should still be (0,0) for Curvilinear
@@ -117,8 +117,8 @@ BOOST_AUTO_TEST_CASE(curvilinear_initialization) {
 
   double uphi = 1.2;
   double utheta = 0.2;
-  double uqop = 0.025 / units::_GeV;
-  double ut = 1337. * units::_s;
+  double uqop = 0.025_e / 1_GeV;
+  double ut = 1337_s;
 
   curvilinear_pos_copy.set<ePHI>(tgContext, uphi);
   curvilinear_pos_copy.set<eTHETA>(tgContext, utheta);
@@ -128,7 +128,7 @@ BOOST_AUTO_TEST_CASE(curvilinear_initialization) {
   Vector3D umomentum =
       40. *
       Vector3D(cos(uphi) * sin(utheta), sin(uphi) * sin(utheta), cos(utheta)) *
-      units::_GeV;
+      1_GeV;
   CHECK_CLOSE_REL(umomentum, curvilinear_pos_copy.momentum(), 1e-6);
   // the updated momentum should be the col(2) of the transform
   CHECK_CLOSE_REL(umomentum.normalized(),
