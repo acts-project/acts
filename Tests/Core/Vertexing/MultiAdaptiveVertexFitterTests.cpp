a203cae462bb78bcb2d4b69a8688654cf4818cd1
Bastian Schlag <bastian.schlag@cern.ch>
2019-06-05 16:20:34 +0200

    updated MultiAdaptiveVertexFitterTests

--- Tests/Core/Vertexing/MultiAdaptiveVertexFitterTests.cpp
+++ Tests/Core/Vertexing/MultiAdaptiveVertexFitterTests.cpp
@@ -27,13 +27,45 @@
 namespace Acts {
 namespace Test {
 
+using Covariance = BoundSymMatrix;
+
 // Create a test context
 GeometryContext tgContext = GeometryContext();
 MagneticFieldContext mfContext = MagneticFieldContext();
 
+// Vertex x/y position distribution
+std::uniform_real_distribution<> vXYDist(-0.1 * units::_mm, 0.1 * units::_mm);
+// Vertex z position distribution
+std::uniform_real_distribution<> vZDist(-20 * units::_mm, 20 * units::_mm);
+// Track d0 distribution
+std::uniform_real_distribution<> d0Dist(-0.01 * units::_mm, 0.01 * units::_mm);
+// Track z0 distribution
+std::uniform_real_distribution<> z0Dist(-0.2 * units::_mm, 0.2 * units::_mm);
+// Track pT distribution
+std::uniform_real_distribution<> pTDist(0.4 * units::_GeV, 10. * units::_GeV);
+// Track phi distribution
+std::uniform_real_distribution<> phiDist(-M_PI, M_PI);
+// Track theta distribution
+std::uniform_real_distribution<> thetaDist(1.0, M_PI - 1.0);
+// Track charge helper distribution
+std::uniform_real_distribution<> qDist(-1, 1);
+// Track IP resolution distribution
+std::uniform_real_distribution<> resIPDist(0., 100. * units::_um);
+// Track angular distribution
+std::uniform_real_distribution<> resAngDist(0., 0.1);
+// Track q/p resolution distribution
+std::uniform_real_distribution<> resQoPDist(-0.1, 0.1);
+// Number of tracks distritbution
+std::uniform_int_distribution<> nTracksDist(3, 10);
+
 /// @brief Unit test for MultiAdaptiveVertexFitter
 ///
 BOOST_AUTO_TEST_CASE(multi_adaptive_vertex_fitter_test) {
+
+	// Set up RNG
+	int mySeed = 31415;
+  std::mt19937 gen(mySeed);
+
   // Set up constant B-Field
   ConstantBField bField(Vector3D(0., 0., 1.) * units::_T);
 
@@ -50,19 +82,92 @@ BOOST_AUTO_TEST_CASE(multi_adaptive_vertex_fitter_test) {
       config(bField, propagator);
 
   MultiAdaptiveVertexFitter<ConstantBField, BoundParameters,
-                            Propagator<EigenStepper<ConstantBField>>>::State
-      state;
-
-  MultiAdaptiveVertexFitter<ConstantBField, BoundParameters,
                             Propagator<EigenStepper<ConstantBField>>>
       fitter(config);
 
-  Vertex<BoundParameters> vtx;
+  MultiAdaptiveVertexFitter<ConstantBField, BoundParameters,
+                            Propagator<EigenStepper<ConstantBField>>>::State
+      state;
+
+  // Create position of vertex and perigee surface
+    double x = vXYDist(gen);
+    double y = vXYDist(gen);
+    double z = vZDist(gen);
+
+    Vector3D vertexPosition(x, y, z);
+    std::shared_ptr<PerigeeSurface> perigeeSurface =
+        Surface::makeShared<PerigeeSurface>(Vector3D(0., 0., 0.));
+
+    // Calculate d0 and z0 corresponding to vertex position
+    double d0V = sqrt(x * x + y * y);
+    double z0V = z;
+
+    // Start constructing nTracks tracks in the following
+    std::vector<TrackAtVertex<BoundParameters>> tracks;
+
+    unsigned int nTracks = 3;
+    // Construct random track emerging from vicinity of vertex position
+    // Vector to store track objects used for vertex fit
+    for (unsigned int iTrack = 0; iTrack < nTracks; iTrack++) {
+      // Construct positive or negative charge randomly
+      double q = qDist(gen) < 0 ? -1. : 1.;
+
+      // Construct random track parameters
+      TrackParametersBase::ParVector_t paramVec;
+      paramVec << d0V + d0Dist(gen), z0V + z0Dist(gen), phiDist(gen),
+          thetaDist(gen), q / pTDist(gen), 0.;
+
+      // Fill vector of track objects with simple covariance matrix
+      std::unique_ptr<Covariance> covMat = std::make_unique<Covariance>();
+      // Resolutions
+      double resD0 = resIPDist(gen);
+      double resZ0 = resIPDist(gen);
+      double resPh = resAngDist(gen);
+      double resTh = resAngDist(gen);
+      double resQp = resQoPDist(gen);
+
+      (*covMat) << resD0 * resD0, 0., 0., 0., 0., 0., 0., resZ0 * resZ0, 0., 0.,
+          0., 0., 0., 0., resPh * resPh, 0., 0., 0., 0., 0., 0., resTh * resTh,
+          0., 0., 0., 0., 0., 0., resQp * resQp, 0., 0., 0., 0., 0., 0., 1.;
+
+    auto trk = BoundParameters(tgContext, std::move(covMat), paramVec,
+                                       perigeeSurface);
+
+      tracks.push_back(TrackAtVertex(1., trk, trk));
+    }
+
+  Vertex<BoundParameters> vtx(vertexPosition);
+  vtx.setTracksAtVertex(tracks);
+
+  state.vertexCollection.push_back(vtx);
 
   auto res1 = fitter.addVertexToFit(state, vtx, fitterOptions);
 
-  auto res2 = fitter.fit(state, fitterOptions);
+  BOOST_CHECK(res1.ok());
+
+  //auto res2 = fitter.fit(state, fitterOptions);
+
+
+//BOOST_CHECK(res2.ok());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 }  // namespace Test
 }  // namespace Acts
+
+
