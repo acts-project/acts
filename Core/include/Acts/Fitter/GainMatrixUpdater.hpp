c79a468970ad2567c16de14338ea50a6a739e154
Paul Gessinger <paul.gessinger@cern.ch>
2019-07-08 10:58:37 +0200

    Change how chi2 scalar is extracted

--- Core/include/Acts/Fitter/GainMatrixUpdater.hpp
+++ Core/include/Acts/Fitter/GainMatrixUpdater.hpp
@@ -118,7 +118,7 @@ class GainMatrixUpdater {
               (residual.transpose() *
                ((cov_t::Identity() - H * K) * calibrated_covariance).inverse() *
                residual)
-                  .eval()(0, 0);
+                  .value();
         });
 
     return Result<void>::success();
