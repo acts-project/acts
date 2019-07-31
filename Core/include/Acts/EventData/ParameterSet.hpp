405d4ace4cf00b2b6ae7e9e87443349a319f1991
Paul Gessinger <paul.gessinger@cern.ch>
2019-07-31 13:18:41 +0200

    Rename m_pCovariance to m_optCovariance

--- Core/include/Acts/EventData/ParameterSet.hpp
+++ Core/include/Acts/EventData/ParameterSet.hpp
@@ -124,7 +124,7 @@ class ParameterSet {
                Tail... values)
       : m_vValues(NPars) {
     if (cov) {
-      m_pCovariance = std::move(*cov);
+      m_optCovariance = std::move(*cov);
     }
     detail::initialize_parset<ParID_t, params...>::init(*this, head, values...);
   }
@@ -144,7 +144,7 @@ class ParameterSet {
   ParameterSet(std::optional<CovMatrix_t> cov, const ParVector_t& values)
       : m_vValues(NPars) {
     if (cov) {
-      m_pCovariance = std::move(*cov);
+      m_optCovariance = std::move(*cov);
     }
     detail::initialize_parset<ParID_t, params...>::init(*this, values);
   }
@@ -156,7 +156,7 @@ class ParameterSet {
    * object
    */
   ParameterSet(const ParSet_t& copy)
-      : m_vValues(copy.m_vValues), m_pCovariance(copy.m_pCovariance) {}
+      : m_vValues(copy.m_vValues), m_optCovariance(copy.m_optCovariance) {}
 
   /**
    * @brief move constructor
@@ -165,8 +165,8 @@ class ParameterSet {
    * object
    */
   ParameterSet(ParSet_t&& copy) : m_vValues(std::move(copy.m_vValues)) {
-    if (copy.m_pCovariance) {
-      m_pCovariance = std::move(*copy.m_pCovariance);
+    if (copy.m_optCovariance) {
+      m_optCovariance = std::move(*copy.m_optCovariance);
     }
   }
 
@@ -182,7 +182,7 @@ class ParameterSet {
    */
   ParSet_t& operator=(const ParSet_t& rhs) {
     m_vValues = rhs.m_vValues;
-    m_pCovariance = rhs.m_pCovariance;
+    m_optCovariance = rhs.m_optCovariance;
     return *this;
   }
 
@@ -193,7 +193,7 @@ class ParameterSet {
    */
   ParSet_t& operator=(ParSet_t&& rhs) {
     m_vValues = std::move(rhs.m_vValues);
-    m_pCovariance = std::move(rhs.m_pCovariance);
+    m_optCovariance = std::move(rhs.m_optCovariance);
     return *this;
   }
 
@@ -203,7 +203,7 @@ class ParameterSet {
   friend void swap(ParSet_t& first, ParSet_t& second) noexcept {
     using std::swap;
     swap(first.m_vValues, second.m_vValues);
-    swap(first.m_pCovariance, second.m_pCovariance);
+    swap(first.m_optCovariance, second.m_optCovariance);
   }
 
   /**
@@ -311,7 +311,7 @@ class ParameterSet {
    * @return raw pointer to covariance matrix (can be a nullptr)
    */
   const std::optional<CovMatrix_t> getCovariance() const {
-    return m_pCovariance;
+    return m_optCovariance;
   }
 
   /**
@@ -328,9 +328,9 @@ class ParameterSet {
    */
   template <ParID_t parameter>
   ParValue_t getUncertainty() const {
-    if (m_pCovariance) {
+    if (m_optCovariance) {
       size_t index = getIndex<parameter>();
-      return sqrt((*m_pCovariance)(index, index));
+      return sqrt((*m_optCovariance)(index, index));
     } else {
       return -1;
     }
@@ -343,7 +343,7 @@ class ParameterSet {
    *
    * @param cov unique pointer to new covariance matrix (nullptr is accepted)
    */
-  void setCovariance(const CovMatrix_t& cov) { m_pCovariance = cov; }
+  void setCovariance(const CovMatrix_t& cov) { m_optCovariance = cov; }
 
   /**
    * @brief equality operator
@@ -363,13 +363,13 @@ class ParameterSet {
       return false;
     }
     // both have covariance matrices set
-    if ((m_pCovariance && rhs.m_pCovariance) &&
-        (*m_pCovariance != *rhs.m_pCovariance)) {
+    if ((m_optCovariance && rhs.m_optCovariance) &&
+        (*m_optCovariance != *rhs.m_optCovariance)) {
       return false;
     }
     // only one has a covariance matrix set
-    if ((m_pCovariance && !rhs.m_pCovariance) ||
-        (!m_pCovariance && rhs.m_pCovariance)) {
+    if ((m_optCovariance && !rhs.m_optCovariance) ||
+        (!m_optCovariance && rhs.m_optCovariance)) {
       return false;
     }
 
@@ -535,7 +535,7 @@ class ParameterSet {
  private:
   ParVector_t m_vValues{ParVector_t::Zero()};  ///< column vector containing
                                                ///< values of local parameters
-  std::optional<CovMatrix_t> m_pCovariance{
+  std::optional<CovMatrix_t> m_optCovariance{
       std::nullopt};  ///< an optional covariance matrix
 
   static const Projection_t sProjector;  ///< matrix to project full parameter
