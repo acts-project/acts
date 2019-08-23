a373153e978ce1b01823b30f16bb4c462b2b29d9
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-08-23 10:38:53 +0200

    Please clang-format

--- Tests/Core/Geometry/PlaneLayerTests.cpp
+++ Tests/Core/Geometry/PlaneLayerTests.cpp
@@ -64,12 +64,8 @@ BOOST_AUTO_TEST_CASE(PlaneLayerConstruction) {
   const double thickness(1.0);
   SurfaceArrayCreator sac;
   size_t binsX(2), binsY(4);
-  auto pSurfaceArray =
-      sac.surfaceArrayOnPlane(tgContext,
-                              aSurfaces,
-                              binsX,
-                              binsY,
-                              BinningValue::binZ);
+  auto pSurfaceArray = sac.surfaceArrayOnPlane(tgContext, aSurfaces, binsX,
+                                               binsY, BinningValue::binZ);
   auto pPlaneLayerFromSurfaces =
       PlaneLayer::create(pTransform, pRectangle, std::move(pSurfaceArray));
   BOOST_CHECK_EQUAL(pPlaneLayerFromSurfaces->layerType(), LayerType::active);
