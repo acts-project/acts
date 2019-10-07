04856b42fa8d7b91c49d670354886b2eb3a1103c
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2019-10-07 16:39:24 +0200

    changing xml schema for proto material

--- Plugins/DD4hep/include/Acts/Plugins/DD4hep/ConvertDD4hepMaterial.hpp
+++ Plugins/DD4hep/include/Acts/Plugins/DD4hep/ConvertDD4hepMaterial.hpp
@@ -80,30 +80,8 @@ std::shared_ptr<Acts::ProtoSurfaceMaterial> createProtoMaterial(
 /// @param x_material the material tag to be inspected
 /// @param actsExtension the extension that is augmented
 /// @param baseTag the xml tag to be checked
-/// @param materialOptions the placement options (inner / representing / outer)
-/// @param binOptions the material binning options
-void xmlToProtoMaterial(const xml_comp_t& x_material,
-                        ActsExtension& actsExtension,
-                        const std::string& baseTag,
-                        const std::vector<std::string>& materialOptions,
-                        const std::pair<std::string, std::string>& binOptions);
-
-/// Helper method that decorates an ActsExtension with proto material
-/// description,
-/// - it assigns bins for inner / representing / outer
-///
-/// @param x_layer the cylinder layer
-/// @param actsExtension the extension that is augmented
-void xmlToCylinderProtoMaterial(const xml_comp_t& x_layer,
-                                Acts::ActsExtension& actsExtension);
-
-/// Helper method that decorates an ActsExtension with proto material
-/// description,
-/// - it assigns bins for inner / representing / outer
-///
-/// @param x_layer the disc layer
-/// @param actsExtension the extension that is augmented
-void xmlToDiscProtoMaterial(const xml_comp_t& x_layer,
-                            Acts::ActsExtension& actsExtension);
+void xmlToProtoSurfaceMaterial(const xml_comp_t& x_material,
+                               ActsExtension& actsExtension,
+                               const std::string& baseTag);
 
 }  // namespace Acts
