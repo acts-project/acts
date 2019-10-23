e4a42ccdd2a5335c780d28e96d229433775c51b2
Moritz Kiehn <msmk@cern.ch>
2019-10-23 10:06:17 +0200

    GeometryID: move geo_id_value into the Acts namespace

--- Core/include/Acts/Geometry/GeometryID.hpp
+++ Core/include/Acts/Geometry/GeometryID.hpp
@@ -1,23 +1,21 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2016-2018 CERN for the benefit of the Acts project
+// Copyright (C) 2016-2019 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-///////////////////////////////////////////////////////////////////
-// GeometryID.h, Acts project
-///////////////////////////////////////////////////////////////////
-
 #pragma once
 
+#include <cstdint>
 #include <iostream>
+
 #include "Acts/Utilities/Helpers.hpp"
 
-using geo_id_value = uint64_t;
-
 namespace Acts {
+
+using geo_id_value = uint64_t;
 
 /// @class GeometryID
 ///
