e5af04f86d9f999df6326bfc8c9bae8e874269de
Moritz Kiehn <msmk@cern.ch>
2019-10-30 15:40:43 +0100

    Material: refactor ionisation loss formulae

--- Core/include/Acts/Material/Interactions.hpp
+++ Core/include/Acts/Material/Interactions.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2018 CERN for the benefit of the Acts project
+// Copyright (C) 2018-2019 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,190 +8,62 @@
 
 #pragma once
 
+#include <utility>
+
 #include "Acts/Material/Material.hpp"
 #include "Acts/Material/detail/Constants.hpp"
 #include "Acts/Utilities/Definitions.hpp"
 #include "Acts/Utilities/Units.hpp"
 
 namespace Acts {
+
+/// Compute the mean energy loss due to ionisation.
+///
+/// @param material  Properties of the traversed material
+/// @param thickness Thickness of the traversed material
+/// @param m         Particle mass
+/// @param qOverP    Particle charge divided by absolute momentum
+/// @param q         Particle charge
+/// @return Energy loss distribution mean and width
+///
+/// This computes the expected mean energy loss -dE(x) through a material of
+/// the given properties and thickness x, i.e. it computes
+///
+///     -dE(x) = -dE/dx * x
+///
+std::pair<float, float> computeIonisationLossMean(const Material& material,
+                                                  float thickness, float m,
+                                                  float qOverP,
+                                                  float q = UnitConstants::e);
+
+/// Derivative of the mean ionisation energy loss with respect to q/p.
+///
+/// @see computeIonisationLossMean for parameters description
+float deriveIonisationLossMeanQOverP(const Material& material, float thickness,
+                                     float m, float qOverP,
+                                     float q = UnitConstants::e);
+
+/// Compute the most propable energy loss due to ionisation.
+///
+/// @see computeEnergyLossIonisationMean for parameters description
+/// @return Energy loss distribution most probable value and width
+///
+/// This computes the most probable energy loss dE through a material of the
+/// given properties and thickness as described by the mode of the
+/// Landau-Vavilov-Bichsel distribution.
+std::pair<float, float> computeIonisationLossMode(const Material& material,
+                                                  float thickness, float m,
+                                                  float qOverP,
+                                                  float q = UnitConstants::e);
+
+/// Derivative of the most probable ionisation energy loss with respect to q/p.
+///
+/// @see computeIonisationLossMode for parameters description
+float deriveIonisationLossModeQOverP(const Material& material, float thickness,
+                                     float m, float qOverP,
+                                     float q = UnitConstants::e);
+
 namespace detail {
-
-/// The ionization energy loss along a given path length.
-///
-/// The mean energy loss should be used for reconstruction,
-/// the most probable energy loss is more suited for fast simulation
-///
-/// The energy loss is calculated using the following paper
-/// http://pdg.lbl.gov/2018/reviews/rpp2018-rev-passage-particles-matter.pdf
-///
-/// Formula 33.5 is used to calculate the mean energy loss [reco mode]
-/// Formula 33.11 is used to calculate the most probable energy loss [sim
-/// mode]
-/// Sigma is calculated using the Landau width (FHWM) times the conversion
-/// factor 1. / (2. * &radic(2. * log2))
-///
-struct IonisationLoss {
-  /// @brief call operator for the Ionisation class
-  ///
-  /// @tparam material_t Type of the material class
-  /// @param [in] m      Mass of the particle
-  /// @param [in] lbeta  Beta factor of the particle
-  /// @param [in] lgamma Gamma factor of the particle
-  /// @param [in] mat    The traversed material
-  /// @param [in] path   The path length (optional)
-  /// @param [in] mean   Toggle between mean (true) and mode (false)
-  /// @return A std::pair. The first entry is the mean energy loss due to
-  /// ionization along a given path length or its mode. The second entry is
-  /// the sigma of the distribution.
-  template <typename material_t>
-  std::pair<double, double> dEds(double m, double lbeta, double lgamma,
-                                 const material_t& mat, double path = 1.,
-                                 bool mean = true) const {
-    // the return value
-    double dE = 0.;
-
-    // Ionization - Bethe-Bloch
-    // See ATL-SOFT-PUB-2008-003 equation (4)
-    // 16 eV * Z**0.9
-    double I = constants::eionisation * std::pow(mat.Z(), 0.9);
-
-    // See (1) table 33.1
-    // K/A*Z = 0.5 * 30.7075MeV/(g/mm2) * Z/A * rho[g/mm3]
-    double kaz = 0.5 * constants::ka_BetheBloch * mat.zOverAtimesRho();
-    double eta2 = lbeta * lgamma;
-    eta2 *= eta2;
-    // density effect, only valid for high energies
-    // (lgamma > 10 -> p > 1 GeV for muons)
-    double delta = 0.;
-    if (lbeta * lgamma > 10.) {
-      // See (1) table 33.1
-      double eplasma = constants::eplasma * sqrt(1000. * mat.zOverAtimesRho());
-      // See (1) formula 33.6
-      delta = 2. * std::log(eplasma / I) + std::log(eta2) - 1.;
-    }
-
-    // divide by lbeta^2 for non-electrons
-    kaz /= lbeta * lbeta;
-    double kazL = kaz * path;
-
-    // The landau width (FWHM) is 4.*kazL
-    // The factor is the conversion factor from FWHM to sigma for
-    // gaussian curve: 1. / (2. * sqrt(2. * log(2.))).
-    double sigma = kazL * constants::landau2gauss;
-    if (mean) {
-      // Calculate the fraction to the electron mass
-      double mfrac = constants::me / m;
-      // Calculate the mean value for reconstruction
-      // See ATL-SOFT-PUB-2008-003 equation (2)
-      double tMax = 2. * eta2 * constants::me /
-                    (1. + 2. * lgamma * mfrac + mfrac * mfrac);
-      // See ATL-SOFT-PUB-2008-003 equation (1)
-      // or:
-      // http://pdg.lbl.gov/2018/reviews/rpp2018-rev-passage-particles-matter.pdf
-      // PDG formula 33.5
-      dE = -kazL * 2.0 *
-           (0.5 * std::log(2. * constants::me * eta2 * tMax / (I * I)) -
-            (lbeta * lbeta) - delta * 0.5);
-    } else {
-      // Calculate the most probably value for simulation
-      //
-      // the landau sigmaL is path length dependent
-      //    PDG formula 33.11 for MOP value from
-      //    http://pdg.lbl.gov/2018/reviews/rpp2018-rev-passage-particles-matter.pdf
-      //
-      dE = kazL * (std::log(2. * m * eta2 / I) + std::log(kazL / I) + 0.2 -
-                   (lbeta * lbeta) - delta);
-    }
-    // return the energy loss and stragling
-    return std::make_pair(dE, sigma);
-  }
-
-  /// @brief Evaluation of the energy loss dEds by ionisation derived by q/p
-  /// (=d(dE/ds)/d(q/p)). The underlying equations for dE/ds are given by
-  /// Formula 33.5 (mean energy loss) and Formula 33.11 (most probable energy
-  /// loss).
-  ///
-  /// @tparam material_t   Type of the material
-  /// @param [in] energy   Energy of the particle
-  /// @param [in] qop      Charge over momentum of the particle
-  /// @param [in] mass     Mass of the particle
-  /// @param [in] material Material that is penetrated
-  /// @param [in] mean     Toggle between mean (true) and mode (false)
-  /// @return The evaluated derivative
-  template <typename material_t>
-  double dqop(const double energy, const double qOverP, const double mass,
-              const material_t& material, const bool mean = true) const {
-    // Fast exit if material is invalid
-    if (material.Z() == 0 || material.zOverAtimesRho() == 0) {
-      return 0.;
-    }
-
-    // Constants for readability
-    const double m2 = mass * mass;
-    const double I = constants::eionisation * std::pow(material.Z(), 0.9);
-    const double gamma = energy / mass;
-    const double beta = std::abs(1 / (energy * qOverP));
-    const double beta2 = beta * beta;
-    const double kaz =
-        0.5 * constants::ka_BetheBloch * material.zOverAtimesRho();
-    // Storage of the result
-    double betheBlochDerivative = 0.;
-    if (mean) {
-      // Constants related to the mean evaluation
-      const double me = constants::me;
-      const double me2 = me * me;
-      const double qop3 = qOverP * qOverP * qOverP;
-      const double qop4 = qop3 * qOverP;
-      const double m4 = m2 * m2;
-      const double I2 = I * I;
-
-      // Parts of the derivative
-      const double lnCore = 4. * me2 / (m4 * I2 * qop4) /
-                            (1. + 2. * gamma * me / mass + me2 / m2);
-      const double lnCoreDerivative =
-          -4. * me2 / (m4 * I2) *
-          std::pow(qop4 + 2. * gamma * qop4 * me / mass + qop4 * me2 / m2,
-                   -2.) *
-          (4. * qop3 + 8. * me * qop3 * gamma / mass -
-           2. * me * qOverP / (m2 * mass * gamma) + 4. * qop3 * me2 / m2);
-
-      // Combine parts
-      const double lnDerivative = 2. * qOverP * m2 * std::log(lnCore) +
-                                  lnCoreDerivative / (lnCore * beta2);
-      betheBlochDerivative = -kaz * lnDerivative;
-    } else {
-      // Parts of the mode derivative
-      const double lnCore = std::log(2. * mass * beta2 * gamma * gamma / I);
-      const double lnCoreXi = std::log(kaz / (beta2 * I));
-      const double lnCoreDerivative = -2. * mass * beta * gamma;
-      const double lnCoreXiDerivative = 2. * beta2 * m2;
-
-      // Result evaluation (without the density effect)
-      betheBlochDerivative =
-          kaz * (2. * m2 * qOverP * (lnCore + lnCoreXi + 0.2) +
-                 (lnCoreDerivative + lnCoreXiDerivative) / beta2);
-    }
-    // Density effect, only valid for high energies (gamma > 10 -> p > 1GeV
-    // for muons). The derivative includes the kinematic parts of the
-    // pre-factor
-    if (gamma > 10.) {
-      const double delta =
-          2. * std::log(constants::eplasma *
-                        std::sqrt(1000. * material.zOverAtimesRho()) / I) +
-          2. * std::log(beta * gamma) - 1.;
-      if (mean) {
-        const double deltaDerivative =
-            -2. / (qOverP * beta2) + 2. * delta * qOverP * m2;
-        betheBlochDerivative += kaz * deltaDerivative;
-      } else {
-        const double deltaDerivative =
-            -kaz * 2. / (qOverP * beta2) + kaz * 2. * m2 * qOverP * delta;
-        betheBlochDerivative -= deltaDerivative;
-      }
-    }
-    return betheBlochDerivative;
-  }
-};
 
 /// @brief Multiple scattering as function of dInX0
 ///
---
+++ Core/src/Material/Interactions.cpp
@@ -0,0 +1,266 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "Acts/Material/Interactions.hpp"
+
+#include <cassert>
+#include <cmath>
+
+using namespace Acts::UnitLiterals;
+
+namespace {
+
+// values from RPP2018 table 33.1
+// electron mass
+static constexpr float Me = 0.5109989461_MeV;
+// Bethe formular prefactor
+static constexpr float K = 0.307075_MeV * 1_cm * 1_cm;
+// Energy scale for plasma energy.
+static constexpr float PlasmaEnergyScale = 28.816_eV;
+
+// defined in ATL-SOFT-PUB-2008-003
+static constexpr float MeanExitationPotentialScale = 16_eV;
+// 4 * scaling factor for Landau FWHM to equivalent Gaussian sigma
+// values is 2 / sqrt(2 * log(2))
+static constexpr float Landau2Gauss = 2.577567883f;
+
+/// Additional derived relativistic quantities.
+struct RelativisticQuantities {
+  float q2OverBeta2 = 0.0f;
+  float beta2 = 0.0f;
+  float betaGamma = 0.0f;
+  float gamma = 0.0f;
+
+  RelativisticQuantities(float m, float qOverP, float q) {
+    // beta²/q² = (p/E)²/q² = p²/(q²m² + q²p²) = 1/(q² + (m²(q/p)²)
+    // q²/beta² = q² + m²(q/p)²
+    q2OverBeta2 = q * q + (m * qOverP) * (m * qOverP);
+    // 1/p = q/(qp) = (q/p)/q
+    const auto mOverP = m * std::abs(qOverP / q);
+    const auto pOverM = 1.0f / mOverP;
+    // beta² = p²/E² = p²/(m² + p²) = 1/(1 + (m/p)²)
+    beta2 = 1.0f / (1.0f + mOverP * mOverP);
+    // beta*gamma = (p/sqrt(m² + p²))*(sqrt(m² + p²)/m) = p/m
+    betaGamma = pOverM;
+    // gamma = sqrt(m² + p²)/m = sqrt(1 + (p/m)²)
+    gamma = std::sqrt(1.0f + pOverM * pOverM);
+  }
+};
+
+/// Compute q/p derivative of beta².
+inline float deriveBeta2(float qOverP, const RelativisticQuantities& rq) {
+  return -2 / (qOverP * rq.gamma * rq.gamma);
+}
+
+/// Compute the 2 * m * (beta * gamma)² mass term.
+inline float computeMassTerm(float m, const RelativisticQuantities& rq) {
+  return 2 * m * rq.betaGamma * rq.betaGamma;
+}
+/// Compute mass term logarithmic derivative w/ respect to q/p.
+inline float logDeriveMassTerm(float qOverP) {
+  // only need to compute d((beta*gamma)²)/(beta*gamma)²; rest cancels.
+  return -2 / qOverP;
+}
+
+/// Compute the maximum energy transfer in a single collision.
+///
+/// Uses RPP2018 eq. 33.4.
+inline float computeWMax(float m, const RelativisticQuantities& rq) {
+  const auto mfrac = Me / m;
+  const auto nominator = 2 * Me * rq.betaGamma * rq.betaGamma;
+  const auto denonimator = 1.0f + 2 * rq.gamma * mfrac + mfrac * mfrac;
+  return nominator / denonimator;
+}
+/// Compute WMax logarithmic derivative w/ respect to q/p.
+inline float logDeriveWMax(float m, float qOverP,
+                           const RelativisticQuantities& rq) {
+  // this is (q/p) * (beta/q).
+  // both quantities have the same sign and the product must always be
+  // positive. we can thus reuse the known (unsigned) quantity (q/beta)².
+  const auto a = std::abs(qOverP / std::sqrt(rq.q2OverBeta2));
+  // (m² + me²) / me = me (1 + (m/me)²)
+  const auto b = Me * (1.0f + (m / Me) * (m / Me));
+  return -2 * (a * b - 2 + rq.beta2) / (qOverP * (a * b + 2));
+}
+
+/// Compute the mean excitation potential.
+///
+/// See ATL-SOFT-PUB-2008-003 equation (4)
+inline float computeMeanExcitationPotential(float nuclearCharge) {
+  return MeanExitationPotentialScale * std::pow(nuclearCharge, 0.9f);
+}
+
+/// Compute epsilon energy pre-factor for RPP2018 eq. 33.11.
+///
+/// Defined as
+///
+///     (K/2) * (Z/A)*rho * x * (q²/beta²)
+///
+/// where (Z/A)*rho it the electron density in the material and x is the
+/// traversed length (thickness) of the material.
+inline float computeEpsilon(float electronDensity, float thickness,
+                            const RelativisticQuantities& rq) {
+  return 0.5f * K * electronDensity * thickness * rq.q2OverBeta2;
+}
+/// Compute epsilon logarithmic derivative w/ respect to q/p.
+inline float logDeriveEpsilon(float qOverP, const RelativisticQuantities& rq) {
+  // only need to compute d(q²/beta²)/(q²/beta²); everything else cancels.
+  return 2 / (qOverP * rq.gamma * rq.gamma);
+}
+
+/// Compute the density correction factor delta/2.
+///
+/// Uses RPP2018 eq. 33.6 which is only valid for high energies.
+///
+/// @todo Should we use RPP2018 eq. 33.7 instead w/ tabulated constants?
+inline float computeDeltaHalf(float meanExitationPotential,
+                              float electronDensity,
+                              const RelativisticQuantities& rq) {
+  // only relevant for very high ernergies; use arbitrary cutoff
+  if (rq.betaGamma < 10.0f) {
+    return 0.0f;
+  }
+  // pre-factor according to RPP2019 table 33.1
+  const auto plasmaEnergy = PlasmaEnergyScale * std::sqrt(electronDensity);
+  return std::log(rq.betaGamma) +
+         std::log(plasmaEnergy / meanExitationPotential) - 0.5f;
+}
+/// Compute derivative w/ respect to q/p for the density correction.
+inline float deriveDeltaHalf(float qOverP, const RelativisticQuantities& rq) {
+  // original equation is of the form
+  //     log(beta*gamma) + log(eplasma/I) - 1/2
+  // which the resulting derivative as
+  //     d(beta*gamma)/(beta*gamma)
+  return (rq.betaGamma < 10.0f) ? 0.0f : (-1.0f / qOverP);
+}
+
+}  // namespace
+
+#define ASSERT_IONISATION_INPUTS(thickness, m, qOverP, q) \
+  assert(0 < thickness and "Thickness must be positive"); \
+  assert(0 < m and "Mass must be positive");              \
+  assert(0 < (qOverP * q) and "Inconsistent q/p and q signs");
+
+std::pair<float, float> Acts::computeIonisationLossMean(
+    const Material& material, float thickness, float m, float qOverP, float q) {
+  ASSERT_IONISATION_INPUTS(thickness, m, qOverP, q)
+
+  // return early in case of vacuum
+  if (not material) {
+    return {0.0f, 0.0f};
+  }
+
+  const auto I = computeMeanExcitationPotential(material.Z());
+  const auto rq = RelativisticQuantities(m, qOverP, q);
+  const auto eps = computeEpsilon(material.zOverAtimesRho(), thickness, rq);
+  const auto dhalf = computeDeltaHalf(I, material.zOverAtimesRho(), rq);
+  const auto u = computeMassTerm(Me, rq);
+  const auto wmax = computeWMax(m, rq);
+  // uses RPP2018 eq. 33.5 scaled from mass stopping power to linear stopping
+  // power and multiplied with the material thickness to get a total energy loss
+  // instead of an energy loss per length.
+  // the required modification only change the prefactor which becomes
+  // identical to the prefactor epsilon for the most probable value.
+  const auto running =
+      0.5f * std::log(u / I) + 0.5f * std::log(wmax / I) - rq.beta2 - dhalf;
+  return {eps * running, eps * Landau2Gauss};
+}
+
+float Acts::deriveIonisationLossMeanQOverP(const Material& material,
+                                           float thickness, float m,
+                                           float qOverP, float q) {
+  ASSERT_IONISATION_INPUTS(thickness, m, qOverP, q)
+
+  // return early in case of vacuum
+  if (not material) {
+    return 0.0f;
+  }
+
+  const auto I = computeMeanExcitationPotential(material.Z());
+  const auto rq = RelativisticQuantities(m, qOverP, q);
+  const auto eps = computeEpsilon(material.zOverAtimesRho(), thickness, rq);
+  const auto dhalf = computeDeltaHalf(I, material.zOverAtimesRho(), rq);
+  const auto u = computeMassTerm(Me, rq);
+  const auto wmax = computeWMax(m, rq);
+  // original equation is of the form
+  //
+  //     eps * (log(u/I)/2 + log(wmax/I)/2 - beta² - delta/2)
+  //     = eps * (log(u)/2 + log(wmax/I)/2 - log(I) - beta² - delta/2)
+  //
+  // with the resulting derivative as
+  //
+  //     d(eps) * (log(u/I)/2 + log(wmax/I)/2 - beta² - delta/2)
+  //     + eps * (d(u)/(2*u) + d(wmax)/(2*wmax) - d(beta²) - d(delta/2))
+  //
+  // where we can use d(eps) = eps * (d(eps)/eps) for further simplification.
+  const auto logDerEps = logDeriveEpsilon(qOverP, rq);
+  const auto derDHalf = deriveDeltaHalf(qOverP, rq);
+  const auto logDerU = logDeriveMassTerm(qOverP);
+  const auto logDerWmax = logDeriveWMax(m, qOverP, rq);
+  const auto derBeta2 = deriveBeta2(qOverP, rq);
+  const auto rel = logDerEps * (0.5f * std::log(u / I) +
+                                0.5f * std::log(wmax / I) - rq.beta2 - dhalf) +
+                   0.5f * logDerU + 0.5f * logDerWmax - derBeta2 - derDHalf;
+  return eps * rel;
+}
+
+std::pair<float, float> Acts::computeIonisationLossMode(
+    const Material& material, float thickness, float m, float qOverP, float q) {
+  ASSERT_IONISATION_INPUTS(thickness, m, qOverP, q)
+
+  // return early in case of vacuum
+  if (not material) {
+    return {0.0f, 0.0f};
+  }
+
+  const auto I = computeMeanExcitationPotential(material.Z());
+  const auto rq = RelativisticQuantities(m, qOverP, q);
+  const auto eps = computeEpsilon(material.zOverAtimesRho(), thickness, rq);
+  const auto dhalf = computeDeltaHalf(I, material.zOverAtimesRho(), rq);
+  const auto t = computeMassTerm(m, rq);
+  // uses RPP2018 eq. 33.11
+  const auto running =
+      std::log(t / I) + std::log(eps / I) + 0.2f - rq.beta2 - 2 * dhalf;
+  return {eps * running, eps * Landau2Gauss};
+}
+
+float Acts::deriveIonisationLossModeQOverP(const Material& material,
+                                           float thickness, float m,
+                                           float qOverP, float q) {
+  ASSERT_IONISATION_INPUTS(thickness, m, qOverP, q)
+
+  // return early in case of vacuum
+  if (not material) {
+    return 0.0f;
+  }
+
+  const auto I = computeMeanExcitationPotential(material.Z());
+  const auto rq = RelativisticQuantities(m, qOverP, q);
+  const auto eps = computeEpsilon(material.zOverAtimesRho(), thickness, rq);
+  const auto dhalf = computeDeltaHalf(I, material.zOverAtimesRho(), rq);
+  const auto t = computeMassTerm(m, rq);
+  // original equation is of the form
+  //
+  //     eps * (log(t/I) - log(eps/I) - 0.2 - beta² - delta)
+  //     = eps * (log(t) - log(eps) - 2*log(I) - 0.2 - beta² - delta)
+  //
+  // with the resulting derivative as
+  //
+  //     d(eps) * (log(t/I) - log(eps/I) - 0.2 - beta² - delta)
+  //     + eps * (d(t)/t + d(eps)/eps - d(beta²) - 2*d(delta/2))
+  //
+  // where we can use d(eps) = eps * (d(eps)/eps) for further simplification.
+  const auto logDerEps = logDeriveEpsilon(qOverP, rq);
+  const auto derDHalf = deriveDeltaHalf(qOverP, rq);
+  const auto logDerT = logDeriveMassTerm(qOverP);
+  const auto derBeta2 = deriveBeta2(qOverP, rq);
+  const auto rel = logDerEps * (std::log(t / I) + std::log(eps / I) - 0.2f -
+                                rq.beta2 - 2 * dhalf) +
+                   logDerT + logDerEps - derBeta2 - 2 * derDHalf;
+  return eps * rel;
+}
