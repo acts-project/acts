24f0ca4073bf181918e8761a6219e6bdf4b391ee
Paul Gessinger <paul.gessinger@cern.ch>
2019-10-31 10:08:39 +0100

    add another nan check

--- Core/include/Acts/Fitter/GainMatrixUpdater.hpp
+++ Core/include/Acts/Fitter/GainMatrixUpdater.hpp
@@ -12,6 +12,7 @@
 #include <variant>
 #include "Acts/EventData/Measurement.hpp"
 #include "Acts/EventData/TrackParameters.hpp"
+#include "Acts/Fitter/KalmanFitterError.hpp"
 #include "Acts/Fitter/detail/VoidKalmanComponents.hpp"
 #include "Acts/Utilities/Definitions.hpp"
 #include "Acts/Utilities/Logger.hpp"
@@ -89,8 +90,8 @@ class GainMatrixUpdater {
 
     // we need to remove type-erasure on the measurement type
     // to access its methods
-    std::visit(
-        [&](const auto& calibrated) {
+    return std::visit(
+        [&](const auto& calibrated) -> Result<void> {
           // type of measurement
           using meas_t = typename std::remove_const<
               typename std::remove_reference<decltype(calibrated)>::type>::type;
@@ -122,6 +123,10 @@ class GainMatrixUpdater {
                                 .inverse();
 
           ACTS_VERBOSE("Gain Matrix K:\n" << K);
+
+          if (K.hasNaN()) {
+            return KalmanFitterError::UpdateFailed;
+          }
 
           // filtered new parameters after update
           filtered_parameters =
@@ -158,11 +163,11 @@ class GainMatrixUpdater {
           ACTS_VERBOSE("Chi2: " << trackState.parameter.chi2);
 
           trackState.parameter.filtered = std::move(filtered);
+
+          // if we get here: always succeed, no outlier logic yet
+          return Result<void>::success();
         },
         *trackState.measurement.calibrated);
-
-    // always succeed, no outlier logic yet
-    return Result<void>::success();
   }
 
   /// Pointer to a logger that is owned by the parent, KalmanFilter
