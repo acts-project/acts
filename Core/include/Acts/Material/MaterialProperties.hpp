c9eb0929b89c9ea19e27bd65265886551858e321
Moritz Kiehn <msmk@cern.ch>
2019-10-31 17:18:34 +0100

    Material: simplify MaterialProperties

--- Core/include/Acts/Material/MaterialProperties.hpp
+++ Core/include/Acts/Material/MaterialProperties.hpp
@@ -12,95 +12,54 @@
 
 #pragma once
 
-#include <iostream>
+#include <iosfwd>
+#include <vector>
+
 #include "Acts/Material/Material.hpp"
 
 namespace Acts {
 
-/// @class MaterialProperties
+/// Material description for an object with defined thickness.
 ///
-/// Material with information associated to a thickness of material
-/// This class is targeted for surface based material description.
-/// A volume based material description is to be described by the
-/// Material class.
+/// This is intended to describe concrete surface materials.
 ///
-/// the units are :
-///  - thickness [mm] (only used for layer description)
-///  - X0  [mm]
-///  - L0  [mm]
-///  - A   [g/mole]
-///  - Z
-///  - rho [g/mm3]
+/// @see Material for a description of the available parameters.
 class MaterialProperties {
  public:
-  /// Default Constructor
+  /// Construct vacuum without thickness.
   MaterialProperties() = default;
-
-  /// Constructor - for empty material (vacuum step)
-  /// @param thickness is the thickness of the material
+  /// Construct vacuum with thickness.
   MaterialProperties(float thickness);
-
-  /// Constructor - for averaged material
+  /// Construct from material parameters.
   ///
-  /// @param Xo is the radiation length in mm
-  /// @param Lo is the nuclear interaction length in mm
-  /// @param averageA is the average atomic weight
-  /// @param averageZ is the average atomic number
-  /// @param averageRho is the average density in g/mm3
+  /// @param X0        is the radiation length parameter
+  /// @param L0        is the nuclear interaction length
+  /// @param A         is the relative atomic mass
+  /// @param Z         is the atomic number
+  /// @param rho       is the density
   /// @param thickness is the thickness of the material
-  MaterialProperties(float Xo, float Lo, float averageA, float averageZ,
-                     float averageRho, float thickness);
-
-  /// Constructor - for full Material class
+  MaterialProperties(float X0, float L0, float A, float Z, float rho,
+                     float thickness);
+  /// Construct from material description.
   ///
-  /// @param material is the material
+  /// @param material  is the material description
   /// @param thickness is the thickness of the material
   MaterialProperties(const Material& material, float thickness);
-
-  /// Constructor - for different layers of Material
+  /// Construct by averaging over multiple layers.
   ///
-  /// @param matLayers The vector of pairs of material and thickness
-  /// @param unitThickness Boolean to set compound is set to unit thickness
-  MaterialProperties(const std::vector<MaterialProperties>& matLayers,
-                     bool unitThickness = true);
+  /// @param layer     Input layers to average over.
+  /// @param normalize Wether to normalize the result to unit thickness.
+  MaterialProperties(const std::vector<MaterialProperties>& layers,
+                     bool normalize = true);
+  ~MaterialProperties() = default;
 
-  /// Copy Constructor
-  ///
-  /// @param mprop is the source material properties to be copied
-  MaterialProperties(const MaterialProperties& mprop) = default;
-
-  /// Copy Move Constructor
-  ///
-  /// @param mprop is the source material properties to be copied
   MaterialProperties(MaterialProperties&& mprop) = default;
+  MaterialProperties(const MaterialProperties&) = default;
+  MaterialProperties& operator=(MaterialProperties&&) = default;
+  MaterialProperties& operator=(const MaterialProperties&) = default;
 
-  /// Destructor
-  virtual ~MaterialProperties() = default;
-
-  /// Assignment Operator
-  ///
-  /// @param mprop is the source material properties object
-  MaterialProperties& operator=(const MaterialProperties& mprop) = default;
-
-  /// Assignment Move Operator
-  ///
-  /// @param mprop is the source material properties object
-  MaterialProperties& operator=(MaterialProperties&& mprop) = default;
-
-  /// Scale operator - scales the material thickness
-  ///
-  /// @param scale is the material scaling parameter
+  /// Scale the material thickness.
   MaterialProperties& operator*=(float scale);
-
-  /// Comparison operator
-  ///
-  /// @param mprop is the source material properties object
-  bool operator==(const MaterialProperties& mprop) const;
-
-  /// Comparison operator
-  ///
-  /// @param mprop is the source material properties object
-  bool operator!=(const MaterialProperties& mprop) const;
 
   /// Scale to unit thickness
   ///
@@ -116,101 +75,38 @@ class MaterialProperties {
   /// - tInX0, tInL0, A, Z
   void scaleToUnitThickness();
 
-  /// Boolean operator
-  /// false indicates it's vacuum
-  operator bool() const { return bool(m_material); }
+  /// Check if the material is valid, i.e. it is not vacuum.
+  constexpr operator bool() const { return m_material; }
 
-  /// Return the stored Material
-  const Material& material() const;
+  /// Access the average material parameters.
+  constexpr const Material& material() const { return m_material; }
+  /// Return the thickness.
+  constexpr float thickness() const { return m_thickness; }
+  /// Return the radiation length fraction.
+  constexpr float thicknessInX0() const { return m_dInX0; }
+  /// Return the nuclear interaction length fraction.
+  constexpr float thicknessInL0() const { return m_dInL0; }
 
-  /// Return the thickness in mm
-  float thickness() const;
+ private:
+  Material m_material;
+  float m_thickness = 0.0f;
+  float m_dInX0 = 0.0f;
+  float m_dInL0 = 0.0f;
 
-  /// Return the radiationlength fraction
-  float thicknessInX0() const;
-
-  /// Return the nuclear interaction length fraction
-  float thicknessInL0() const;
-
-  /// Returns the average X0 of the material
-  float averageX0() const;
-
-  /// Return the average L0 of the material
-  float averageL0() const;
-
-  /// Returns the average Z of the material
-  float averageZ() const;
-
-  /// Return the average A of the material [gram/mole]
-  float averageA() const;
-
-  /// Return the average density of the material
-  /// - in [g/mm^3]
-  float averageRho() const;
-
-  /// Return the @f$ Z/A * rho @f$
-  float zOverAtimesRho() const;
-
- protected:
-  Material m_material{};  //!< the material description
-  float m_thickness{0.};  //!< the thickness of material
-  float m_dInX0{0.};      //!< thickness in units of radiation length
-  float m_dInL0{0.};      //!< thickness in units of nucl. interaction length
+  friend constexpr bool operator==(const MaterialProperties& lhs,
+                                   const MaterialProperties& rhs) {
+    // d/X0 and l/X0 are fully defined by material and thickness
+    return (lhs.m_material == rhs.m_material) and
+           (lhs.m_thickness == rhs.m_thickness);
+  }
+  friend constexpr bool operator!=(const MaterialProperties& lhs,
+                                   const MaterialProperties& rhs) {
+    return !(lhs == rhs);
+  }
 };
 
-inline const Material& MaterialProperties::material() const {
-  return m_material;
-}
-
-inline float MaterialProperties::thicknessInX0() const {
-  return m_dInX0;
-}
-
-inline float MaterialProperties::thicknessInL0() const {
-  return m_dInL0;
-}
-
-inline float MaterialProperties::thickness() const {
-  return m_thickness;
-}
-
-inline float MaterialProperties::zOverAtimesRho() const {
-  return m_material.zOverAtimesRho();
-}
-
-inline float MaterialProperties::averageX0() const {
-  return m_material.X0();
-}
-
-inline float MaterialProperties::averageL0() const {
-  return m_material.L0();
-}
-
-inline float MaterialProperties::averageA() const {
-  return m_material.A();
-}
-
-inline float MaterialProperties::averageZ() const {
-  return m_material.Z();
-}
-
-inline float MaterialProperties::averageRho() const {
-  return m_material.rho();
-}
-
-inline bool MaterialProperties::operator==(
-    const MaterialProperties& mprop) const {
-  return (m_material == mprop.m_material && m_dInX0 == mprop.m_dInX0 &&
-          m_dInL0 == mprop.m_dInL0);
-}
-
-inline bool MaterialProperties::operator!=(
-    const MaterialProperties& mprop) const {
-  return (!operator==(mprop));
-}
-
-// Overload of << operator for std::ostream for debug output
-std::ostream& operator<<(std::ostream& sl, const MaterialProperties& mprop);
+std::ostream& operator<<(std::ostream& os,
+                         const MaterialProperties& materialProperties);
 
 // Useful typedefs
 using MaterialPropertiesVector = std::vector<MaterialProperties>;
--- Core/src/Material/MaterialProperties.cpp
+++ Core/src/Material/MaterialProperties.cpp
@@ -11,7 +11,9 @@
 ///////////////////////////////////////////////////////////////////
 
 #include "Acts/Material/MaterialProperties.hpp"
+
 #include <climits>
+#include <ostream>
 
 Acts::MaterialProperties::MaterialProperties(float thickness)
     : m_thickness(thickness) {}
@@ -34,8 +36,8 @@ Acts::MaterialProperties::MaterialProperties(const Material& material,
                                                      : 0.) {}
 
 Acts::MaterialProperties::MaterialProperties(
-    const std::vector<MaterialProperties>& matLayers, bool unitThickness)
-    : m_material(), m_thickness(0.), m_dInX0(0.), m_dInL0(0.) {
+    const std::vector<MaterialProperties>& matLayers, bool normalize)
+    : MaterialProperties() {
   double rho = 0.;
   double A = 0.;
   double Z = 0.;
@@ -47,13 +49,13 @@ Acts::MaterialProperties::MaterialProperties(
     m_dInX0 += mat.thicknessInX0();
     m_dInL0 += mat.thicknessInL0();
     double t = mat.thickness();
-    double r = mat.averageRho();
+    double r = mat.material().rho();
     m_thickness += t;
     // density scales with thickness
     rho += r * t;
     // A/Z scale with thickness * density
-    A += mat.averageA() * r * t;
-    Z += mat.averageZ() * r * t;
+    A += mat.material().A() * r * t;
+    Z += mat.material().Z() * r * t;
   }
   // Now create the average
   X0 = m_thickness / m_dInX0;
@@ -63,7 +65,7 @@ Acts::MaterialProperties::MaterialProperties(
   rho /= m_thickness;
   // set the material
   m_material = Material(X0, L0, A, Z, rho);
-  if (unitThickness) {
+  if (normalize) {
     scaleToUnitThickness();
   }
 }
@@ -88,33 +90,9 @@ void Acts::MaterialProperties::scaleToUnitThickness() {
   m_thickness = 1.;
 }
 
-std::ostream& Acts::operator<<(std::ostream& sl,
-                               const MaterialProperties& mprop) {
-  if (mprop) {
-    sl << "Acts::MaterialProperties: " << std::endl;
-    sl << "   - thickness/X0                          = "
-       << mprop.thicknessInX0() << std::endl;
-    sl << "   - thickness                       [mm]  = " << mprop.thickness()
-       << std::endl;
-    sl << "   - radiation length X0             [mm]  = " << mprop.averageX0()
-       << std::endl;
-    sl << "   - nuclear interaction length L0   [mm]  = " << mprop.averageL0()
-       << std::endl;
-    sl << "   - average material Z/A*rho [gram/mm^3]  = "
-       << mprop.zOverAtimesRho() << '\n';
-  } else {
-    sl << "Vaccum" << std::endl;
-  }
-
-  /*  interface not finalized
-  if (mprop.material().composition){
-      sl << "   - material composition from " <<
-  mprop.material().composition->size() << " elements " << std::endl;
-      sl << "       listing them (prob. ordereded ) : " << std::endl;
-      for ( auto& eIter : (*mprop.material().composition) )
-          sl << "         -> Z : " << eIter.element() << "( fraction : "  <<
-  eIter.fraction() << " )" << std::endl;
-  }
-  */
-  return sl;
+std::ostream& Acts::operator<<(std::ostream& os,
+                               const MaterialProperties& materialProperties) {
+  os << materialProperties.material()
+     << "|t=" << materialProperties.thickness();
+  return os;
 }
