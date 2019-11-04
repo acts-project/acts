8222ba42d944a5c15081b54bf944466a7814d333
Moritz Kiehn <msmk@cern.ch>
2019-11-04 14:50:24 +0100

    Material: streamline MaterialComposition

--- Core/include/Acts/Material/MaterialComposition.hpp
+++ Core/include/Acts/Material/MaterialComposition.hpp
@@ -19,16 +19,16 @@ namespace Acts {
 /// This can be used to define materials that are compounds of multiple elements
 /// with varying fractions. The element is identified by its atomic number
 /// stored as a single byte (allows up to 256 elements; more than we need).
-/// Its fraction is also stored as a a single byte with values between 0 and
+/// Its fraction is also stored as a single byte with values between 0 and
 /// 255. This gives an accuracy of 1/256 ~ 0.5 %.
 ///
 /// The element fraction allows you to store element composition in merged
-/// material in a merged materials with a large number of bins. Depending on the
+/// materials with a large number of bins. Depending on the
 /// detector and the description granularity this can be a lot of information
 /// and thus requires the reduced memory footprint. This is really only needed
 /// for nuclear interaction in the fast simulation where the reduced fractional
-/// accuracy is not a problem. It should be much better than the parametrization
-/// uncertainty for hadronic interactions.
+/// accuracy is not a problem. The fractional accuracy should be much better
+/// than the parametrization uncertainty for hadronic interactions.
 class ElementFraction {
  public:
   /// Construct from atomic number and relative fraction.
@@ -82,69 +82,56 @@ class ElementFraction {
   friend constexpr bool operator<(ElementFraction lhs, ElementFraction rhs) {
     return lhs.m_fraction < rhs.m_fraction;
   }
+  friend class MaterialComposition;
 };
 
-/// @class MaterialComposition
+/// Material composed from multiple elements with varying factions.
 ///
-/// This helper struct allows to create a material composition
-/// as terms of element fraction objects
+/// @see ElementFraction for details.
 class MaterialComposition {
  public:
+  /// Construct an empty composition corresponding to vacuum.
   MaterialComposition() = default;
-
-  /// Destructor
-  ~MaterialComposition() = default;
-
-  /// Constructor from vector of pairs
+  /// Constructor from element fractions.
   ///
-  /// It does perform a rescaling sucht that the fractions add up
-  /// to one within tolerance
-  ///
-  /// @param efracs are the element fractions
-  MaterialComposition(const std::vector<ElementFraction>& efracs) {
-    m_elements = efracs;
+  /// Rescales the fractions so they all add up to unity within the accuracy.
+  MaterialComposition(std::vector<ElementFraction> elements)
+      : m_elements(std::move(elements)) {
     std::sort(m_elements.begin(), m_elements.end());
+    // compute the total weight first
+    unsigned total = 0u;
+    for (auto element : m_elements) {
+      total += element.m_fraction;
+    }
+    // compute scale factor into the [0, 256) range
+    float scale = float(UINT8_MAX) / float(total);
+    for (auto& element : m_elements) {
+      element.m_fraction *= scale;
+    }
   }
 
-  /// Copy Constructor
-  ///
-  /// @param mc is the element fraction vector
-  MaterialComposition(const MaterialComposition& mc) = default;
+  MaterialComposition(MaterialComposition&&) = default;
+  MaterialComposition(const MaterialComposition&) = default;
+  ~MaterialComposition() = default;
+  MaterialComposition& operator=(MaterialComposition&&) = default;
+  MaterialComposition& operator=(const MaterialComposition&) = default;
 
-  /// Copy Move constructor
-  ///
-  /// @param mc is the element fraction vector
-  MaterialComposition(MaterialComposition&& mc) = default;
+  // Support range-based iteration over contained elements.
+  auto begin() const { return m_elements.begin(); }
+  auto end() const { return m_elements.end(); }
 
-  /// Assignment operator
-  ///
-  /// @param mc is the source object
-  MaterialComposition& operator=(const MaterialComposition& mc) = default;
-
-  /// Assignment move operator
-  ///
-  /// @param mc is the source object
-  MaterialComposition& operator=(MaterialComposition&& mc) = default;
-
-  /// Access to the elements themselves
-  const std::vector<ElementFraction>& elements() const { return m_elements; }
-
-  /// Boolean operator to indicate if this is empty
-  operator bool() const { return !empty(); }
-
-  /// How many elements you have
+  /// Check if the composed material is valid, i.e. it is not vacuum.
+  operator bool() const { return !m_elements.empty(); }
+  /// Return the number of elements.
   size_t size() const { return m_elements.size(); }
 
-  /// Check if empty
-  bool empty() const { return m_elements.empty(); }
-
-  /// Euality operator
-  bool operator==(const MaterialComposition& mc) const {
-    return (mc.m_elements == m_elements);
-  }
-
  private:
-  std::vector<ElementFraction> m_elements = {};
+  std::vector<ElementFraction> m_elements;
+
+  friend inline bool operator==(const MaterialComposition& lhs,
+                                const MaterialComposition& rhs) {
+    return (lhs.m_elements == rhs.m_elements);
+  }
 };
 
 }  // namespace Acts
