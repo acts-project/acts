6b5d90c1b44fffef89b66f5c2b452d148461cb21
Moritz Kiehn <msmk@cern.ch>
2019-11-07 11:49:44 +0100

    Utilities: add enum for commonly use pdg ids

---
+++ Core/include/Acts/Utilities/PdgParticle.hpp
@@ -0,0 +1,32 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+namespace Acts {
+
+/// Symbolic values for commonly used PDG particle numbers.
+enum PdgParticle : int {
+  eElectron = 11,
+  eAntiElectron = -eElectron,
+  ePositron = -eElectron,
+  eMuon = 13,
+  eAntiMuon = -eMuon,
+  eTau = 15,
+  eAntiTau = -eTau,
+  eGamma = 22,
+  ePionZero = 111,
+  ePionPlus = 211,
+  ePionMinus = -ePionPlus,
+  eNeutron = 2112,
+  eAntiNeutron = -eNeutron,
+  eProton = 2212,
+  eAntiProton = -eProton,
+};
+
+}  // namespace Acts
