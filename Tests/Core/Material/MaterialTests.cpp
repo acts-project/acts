4b64b0a468e28579334075a2f9d4be98782992a5
Moritz Kiehn <msmk@cern.ch>
2019-11-19 16:29:04 +0100

    Tests/Materials: use predefined materials

--- Tests/Core/Material/MaterialTests.cpp
+++ Tests/Core/Material/MaterialTests.cpp
@@ -16,50 +16,43 @@
 
 #include "Acts/Material/Material.hpp"
 #include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
+#include "Acts/Tests/CommonHelpers/PredefinedMaterials.hpp"
 #include "Acts/Utilities/Units.hpp"
 
 using namespace Acts::UnitLiterals;
 
 static constexpr auto eps = 2 * std::numeric_limits<float>::epsilon();
+// manually calculated derived values for silicon
+static constexpr float SiNe = 1.160954941 / 1_cm3;
+static constexpr float SiI = 172.042290036_eV;
 
 BOOST_AUTO_TEST_SUITE(material)
 
 BOOST_AUTO_TEST_CASE(construct_vacuum) {
-  // default constructor build invalid material a.k.a. vacuum
+  // default constructor builds invalid material a.k.a. vacuum
   Acts::Material vacuum;
   BOOST_TEST(!vacuum);
 }
 
 BOOST_AUTO_TEST_CASE(construct_something) {
-  // anything with non-zero A is valid material
+  // anything with non-zero Ar is a valid material
   Acts::Material notVacuum(1, 2, 3, 4, 5);
   BOOST_TEST(!!notVacuum);
 }
 
-// example values for silicon
-static constexpr float SiX0 = 9.370_cm;
-static constexpr float SiL0 = 46.52_cm;
-static constexpr float SiAr = 28.0855;
-static constexpr float SiZ = 14.0;
-static constexpr float SiRho = 2.329_g / 1_cm3;
-// manually calculated
-static constexpr float SiNe = 1.160954941 / 1_cm3;
-static constexpr float SiI = 172.042290036_eV;
-
 BOOST_AUTO_TEST_CASE(units) {
-  Acts::Material silicon(SiX0, SiL0, SiAr, SiZ, SiRho);
+  Acts::Material silicon = Acts::Test::makeSilicon();
 
-  // check values directly
-  CHECK_CLOSE_REL(silicon.X0(), SiX0, eps);
-  CHECK_CLOSE_REL(silicon.L0(), SiL0, eps);
-  CHECK_CLOSE_REL(silicon.Ar(), SiAr, eps);
-  CHECK_CLOSE_REL(silicon.Z(), SiZ, eps);
-  CHECK_CLOSE_REL(silicon.rho(), SiRho, eps);
   // check values w/ different units if possible
   CHECK_CLOSE_REL(silicon.X0(), 93.70_mm, eps);
+  CHECK_CLOSE_REL(silicon.X0(), 9.370_cm, eps);
   CHECK_CLOSE_REL(silicon.X0(), 0.09370_m, eps);
   CHECK_CLOSE_REL(silicon.L0(), 465.2_mm, eps);
+  CHECK_CLOSE_REL(silicon.L0(), 46.52_cm, eps);
   CHECK_CLOSE_REL(silicon.L0(), 0.4652_m, eps);
+  CHECK_CLOSE_REL(silicon.Ar(), 28.0855, eps);
+  CHECK_CLOSE_REL(silicon.Z(), 14.0, eps);
+  CHECK_CLOSE_REL(silicon.rho(), 2.329_g / 1_cm3, eps);
   CHECK_CLOSE_REL(silicon.rho(), 0.002329_kg / 1_cm3, eps);
   CHECK_CLOSE_REL(silicon.rho(), 0.002329_g / 1_mm3, eps);
   // check derived values
@@ -70,18 +63,18 @@ BOOST_AUTO_TEST_CASE(units) {
 // encode values as classification vector
 static Acts::ActsVectorF<5> makeSiClassificationNumbers() {
   Acts::ActsVectorF<5> values;
-  values[Acts::Material::eX0] = SiX0;
-  values[Acts::Material::eL0] = SiL0;
-  values[Acts::Material::eAr] = SiAr;
-  values[Acts::Material::eZ] = SiZ;
-  values[Acts::Material::eRho] = SiRho;
+  values[Acts::Material::eX0] = Acts::Test::makeSilicon().X0();
+  values[Acts::Material::eL0] = Acts::Test::makeSilicon().L0();
+  values[Acts::Material::eAr] = Acts::Test::makeSilicon().Ar();
+  values[Acts::Material::eZ] = Acts::Test::makeSilicon().Z();
+  values[Acts::Material::eRho] = Acts::Test::makeSilicon().rho();
   return values;
 }
 
 BOOST_AUTO_TEST_CASE(classification_numbers) {
   const auto numbers = makeSiClassificationNumbers();
   Acts::Material fromNumbers(numbers);
-  Acts::Material manual(SiX0, SiL0, SiAr, SiZ, SiRho);
+  Acts::Material manual = Acts::Test::makeSilicon();
 
   BOOST_TEST(fromNumbers == manual);
   CHECK_CLOSE_REL(fromNumbers.classificationNumbers(), numbers, eps);
