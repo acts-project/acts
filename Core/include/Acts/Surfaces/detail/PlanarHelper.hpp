924f79385fa30bbcfe513d21c1360d7a17dfd80a
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2019-11-26 15:24:16 +0100

    fix clang-format

--- Core/include/Acts/Surfaces/detail/PlanarHelper.hpp
+++ Core/include/Acts/Surfaces/detail/PlanarHelper.hpp
@@ -1,4 +1,4 @@
-Core/include/Acts/Surfaces/detail/PlanarHelper.hpp// This file is part of the Acts project.
+// This file is part of the Acts project.
 //
 // Copyright (C) 2019 CERN for the benefit of the Acts project
 //
@@ -15,38 +15,38 @@ Core/include/Acts/Surfaces/detail/PlanarHelper.hpp// This file is part of the Ac
 #include "Acts/Utilities/Definitions.hpp"
 #include "Acts/Utilities/Intersection.hpp"
 
-namespace Acts {
+    namespace Acts {
 
-/// @brief Helpers for planar surfaces that share the same maths
-namespace PlanarHelper {
+  /// @brief Helpers for planar surfaces that share the same maths
+  namespace PlanarHelper {
 
-/// Intersection with a planar surface
-///
-/// @param transform The 3D affine transform that places the surface
-/// @param position The starting position for the intersection
-/// @param direction The starting direction for the intersection
-static Intersection intersectionEstimate(const Transform3D& transform,
-                                         const Vector3D& position,
-                                         const Vector3D& direction) {
-  // Get the matrix from the transform (faster access)
-  const auto& tMatrix = transform.matrix();
-  const Vector3D pnormal = tMatrix.block<3, 1>(0, 2).transpose();
-  const Vector3D pcenter = tMatrix.block<3, 1>(0, 3).transpose();
-  // It is solvable, so go on
-  double denom = direction.dot(pnormal);
-  if (denom != 0.0) {
-    // Translate that into a path
-    double path = (pnormal.dot((pcenter - position))) / (denom);
-    // Is valid hence either on surface or reachable
-    Intersection::Status status =
-        (path * path < s_onSurfaceTolerance * s_onSurfaceTolerance)
-            ? Intersection::Status::onSurface
-            : Intersection::Status::reachable;
-    // Return the intersection
-    return Intersection(position + path * direction, path, status);
+  /// Intersection with a planar surface
+  ///
+  /// @param transform The 3D affine transform that places the surface
+  /// @param position The starting position for the intersection
+  /// @param direction The starting direction for the intersection
+  static Intersection intersectionEstimate(const Transform3D& transform,
+                                           const Vector3D& position,
+                                           const Vector3D& direction) {
+    // Get the matrix from the transform (faster access)
+    const auto& tMatrix = transform.matrix();
+    const Vector3D pnormal = tMatrix.block<3, 1>(0, 2).transpose();
+    const Vector3D pcenter = tMatrix.block<3, 1>(0, 3).transpose();
+    // It is solvable, so go on
+    double denom = direction.dot(pnormal);
+    if (denom != 0.0) {
+      // Translate that into a path
+      double path = (pnormal.dot((pcenter - position))) / (denom);
+      // Is valid hence either on surface or reachable
+      Intersection::Status status =
+          (path * path < s_onSurfaceTolerance * s_onSurfaceTolerance)
+              ? Intersection::Status::onSurface
+              : Intersection::Status::reachable;
+      // Return the intersection
+      return Intersection(position + path * direction, path, status);
+    }
+    return Intersection();
   }
-  return Intersection();
-}
 
-}  // namespace PlanarHelper
+  }  // namespace PlanarHelper
 }  // namespace Acts
