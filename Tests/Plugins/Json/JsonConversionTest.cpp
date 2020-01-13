6f59a52b5104d039ebe3b072e28ba69c8b4333a2
Paul Gessinger <hello@paulgessinger.com>
2020-01-13 17:51:36 +0100

    Fix test and also actually build it

--- Tests/Plugins/Json/JsonConversionTest.cpp
+++ Tests/Plugins/Json/JsonConversionTest.cpp
@@ -7,7 +7,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // clang-format off
-#define BOOST_TEST_MODULE CartesianSegmentation Tests
+#define BOOST_TEST_MODULE JsonConversionTest Tests
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
