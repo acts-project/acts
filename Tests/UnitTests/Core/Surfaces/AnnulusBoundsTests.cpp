267d00263f5c92813be9d786d95282e31b1f9a3e
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-02-04 10:03:45 +0100

    remove #define BOOST_TEST_BLABLA

--- Tests/UnitTests/Core/Surfaces/AnnulusBoundsTests.cpp
+++ Tests/UnitTests/Core/Surfaces/AnnulusBoundsTests.cpp
@@ -7,8 +7,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // clang-format off
-#define BOOST_TEST_MODULE Annulus Bounds Tests
-#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/output_test_stream.hpp>
