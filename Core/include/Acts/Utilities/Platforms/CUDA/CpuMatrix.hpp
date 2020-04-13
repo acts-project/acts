e4f6d1fe5f000d0107df68fe69d8cd662b1da9b0
Bomki <byeo@kaist.ac.kr>
2020-04-13 21:30:25 +0900

    include license

--- Core/include/Acts/Utilities/Platforms/CUDA/CpuMatrix.hpp
+++ Core/include/Acts/Utilities/Platforms/CUDA/CpuMatrix.hpp
@@ -1,5 +1,12 @@
-#ifndef CPUMATRIX
-#define CPUMATRIX
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
 
 #include "Acts/Utilities/Platforms/CUDA/CudaMatrix.cu"
 
@@ -104,5 +111,3 @@ private:
 };
   
 }
-
-#endif
