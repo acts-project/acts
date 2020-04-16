23c6f98ae80683f475d9339c8db6890e47553d4b
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-04-16 15:14:59 +0200

    added pragmas

--- Tests/UnitTests/Core/Visualization/SurfaceVisualizationBase.hpp
+++ Tests/UnitTests/Core/Visualization/SurfaceVisualizationBase.hpp
@@ -6,6 +6,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#pragma once
+
 #include "Acts/Visualization/GeometryVisualization.hpp"
 #include "Acts/Visualization/IVisualization.hpp"
 
