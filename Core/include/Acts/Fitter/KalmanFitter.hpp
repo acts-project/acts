sPHENIX-Collaboration/acts-core

Removed flag from calls

Core/include/Acts/Fitter/KalmanFitter.hpp
@@ -294,7 +294,7 @@ class KalmanFitter {
         ACTS_VERBOSE("Completing");
         // Transport & bind the parameter to the final surface
         auto fittedState =
-            stepper.boundState(state.stepping, *targetSurface, true);
+            stepper.boundState(state.stepping, *targetSurface);
         // Assign the fitted parameters
         result.fittedParameters = std::get<BoundParameters>(fittedState);
         // Break the navigation for stopping the Propagation
@@ -338,7 +338,7 @@ class KalmanFitter {
 
         // Transport & bind the state to the current surface
         auto [boundParams, jacobian, pathLength] =
-            stepper.boundState(state.stepping, *surface, true);
+            stepper.boundState(state.stepping, *surface);
 
         // add a full TrackState entry multi trajectory
         // (this allocates storage for all components, we will set them later)
@@ -426,7 +426,7 @@ class KalmanFitter {
 
           // Transport & bind the state to the current surface
           auto [boundParams, jacobian, pathLength] =
-              stepper.boundState(state.stepping, *surface, true);
+              stepper.boundState(state.stepping, *surface);
 
           // Fill the track state
           trackStateProxy.predicted() = boundParams.parameters();
@@ -438,7 +438,7 @@ class KalmanFitter {
 
           // Transport & get curvilinear state instead of bound state
           auto [curvilinearParams, jacobian, pathLength] =
-              stepper.curvilinearState(state.stepping, true);
+              stepper.curvilinearState(state.stepping);
 
           // Fill the track state
           trackStateProxy.predicted() = curvilinearParams.parameters();

acts-project/acts

Core/include/Acts/Fitter/KalmanFitter.hpp
@@ -355,7 +355,7 @@ class KalmanFitter {
         ACTS_VERBOSE("Completing");
         // Transport & bind the parameter to the final surface
         auto fittedState =
-            stepper.boundState(state.stepping, *targetSurface, true);
+            stepper.boundState(state.stepping, *targetSurface);
         // Assign the fitted parameters
         result.fittedParameters = std::get<BoundParameters>(fittedState);
         // Break the navigation for stopping the Propagation
@@ -478,7 +478,7 @@ class KalmanFitter {
 
         // Transport & bind the state to the current surface
         auto [boundParams, jacobian, pathLength] =
-            stepper.boundState(state.stepping, *surface, true);
+            stepper.boundState(state.stepping, *surface);
 
         // add a full TrackState entry multi trajectory
         // (this allocates storage for all components, we will set them later)
@@ -715,13 +715,13 @@ class KalmanFitter {
           ACTS_VERBOSE("Detected hole on " << surface->geoID()
                                            << " in backward filtering");
           if (state.stepping.covTransport) {
-            stepper.covarianceTransport(state.stepping, *surface, true);
+            stepper.covarianceTransport(state.stepping, *surface);
           }
         } else {
           ACTS_VERBOSE("Detected in-sensitive surface "
                        << surface->geoID() << " in backward filtering");
           if (state.stepping.covTransport) {
-            stepper.covarianceTransport(state.stepping, true);
+            stepper.covarianceTransport(state.stepping);
           }
         }
 