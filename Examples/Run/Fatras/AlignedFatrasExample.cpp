b066c9e862d67d7f94dea769c79dec733eb25c08
Hadrien G <knights_of_ni@gmx.com>
2020-04-28 09:48:32 +0200

    Don't pass a nullptr to fatrasMain in the aligned Fatras example

--- Examples/Run/Fatras/AlignedFatrasExample.cpp
+++ Examples/Run/Fatras/AlignedFatrasExample.cpp
@@ -10,5 +10,5 @@
 #include "FatrasMain.hpp"
 
 int main(int argc, char* argv[]) {
-  return FW::fatrasMain(argc, argv, std::shared_ptr<AlignedDetector>());
+  return FW::fatrasMain(argc, argv, std::make_shared<AlignedDetector>());
 }
