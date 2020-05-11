887022fc5b56e66681d3d257b6cec4eb15c11258
Moritz Kiehn <msmk@cern.ch>
2020-05-11 21:09:22 +0200

    Refactor Geant4 modules (#187)
    * Examples: consolidate Geant4/DD4hep code
    
    * Geant4: simplify DD4hep detector construction
    
    * Geant4: add explicit Gdml detector construction
    
    * Geant4: simplify geantino recording
    
    * Geant4DD4hep: split DD4hep tools into separate module
    
    * Geant4: remove obsolete DD4hepG4 service
    
    * Geant4: remove prefix from user actions
    
    * Geant4: make classes final
    
    * Examples: split geantino recording into separate directory
    
    * Geant4: follow naming guidelines for static variables
    
    * Geant4: consistent naming/ following naming guidelines
    
    * Geant4: protect run manager with lock
    
    * Geant4: remove commented code from Geantino
    
    * appease the ci gods

---
+++ Examples/Algorithms/Geant4/src/GdmlDetectorConstruction.cpp
@@ -0,0 +1,23 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2017-2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/Geant4/GdmlDetectorConstruction.hpp"
+
+#include <G4GDMLParser.hh>
+
+using namespace ActsExamples;
+
+GdmlDetectorConstruction::GdmlDetectorConstruction(std::string path)
+    : G4VUserDetectorConstruction(), m_path(std::move(path)) {}
+
+G4VPhysicalVolume* GdmlDetectorConstruction::Construct() {
+  G4GDMLParser parser;
+  // TODO how to handle errors
+  parser.Read(m_path);
+  return parser.GetWorldVolume();
+}
