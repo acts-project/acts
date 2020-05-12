df28cca12ecd2cf1d14fba28e0d58d1eae7613aa
Moritz Kiehn <msmk@cern.ch>
2020-05-12 15:07:20 +0200

    Generators: generate flat particles as event

--- Examples/Algorithms/Generators/ActsExamples/Generators/EventGenerator.hpp
+++ Examples/Algorithms/Generators/ActsExamples/Generators/EventGenerator.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2019 CERN for the benefit of the Acts project
+// Copyright (C) 2019-2020 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -14,7 +14,7 @@
 
 #include "Acts/Utilities/Definitions.hpp"
 #include "Acts/Utilities/Logger.hpp"
-#include "ActsExamples/EventData/SimVertex.hpp"
+#include "ActsExamples/EventData/SimParticle.hpp"
 #include "ActsExamples/Framework/IReader.hpp"
 #include "ActsExamples/Framework/RandomNumbers.hpp"
 
@@ -25,10 +25,10 @@
 
 namespace ActsExamples {
 
-/// Event generator based on separate process and vertex generators.
+/// Event generator based on separate particles and vertex generators.
 ///
 /// This must be a reader and not just an algorithm since it might read in
-/// pre-computed generator samples, e.g. via HEPMC, and therefore has an
+/// pre-computed generator samples, e.g. via HepMC, and therefore has an
 /// internal state that will be modified.
 class EventGenerator final : public ActsExamples::IReader {
  public:
@@ -46,32 +46,30 @@ class EventGenerator final : public ActsExamples::IReader {
   /// set/overwritten by the event generator.
   using MultiplicityGenerator = std::function<size_t(RandomEngine&)>;
   using VertexGenerator = std::function<Acts::Vector4D(RandomEngine&)>;
-  using ProcessGenerator = std::function<std::vector<SimVertex>(RandomEngine&)>;
+  using ParticlesGenerator = std::function<SimParticleContainer(RandomEngine&)>;
   struct Generator {
     MultiplicityGenerator multiplicity = nullptr;
     VertexGenerator vertex = nullptr;
-    ProcessGenerator process = nullptr;
+    ParticlesGenerator particles = nullptr;
   };
 
   struct Config {
-    /// Name of the output collection
-    std::string output;
-    /// List of generators that should be used to generate the event
+    /// Name of the output particles collection.
+    std::string outputParticles;
+    /// List of generators that should be used to generate the event.
     std::vector<Generator> generators;
-    /// Shuffle generated vertices to mix generator output
-    bool shuffle = false;
-    /// The random number service
+    /// The random number service.
     std::shared_ptr<const RandomNumbers> randomNumbers;
   };
 
   EventGenerator(const Config& cfg, Acts::Logging::Level lvl);
 
   /// Name of the reader.
-  std::string name() const override final;
+  std::string name() const final;
   /// Available events range. Always return [0,SIZE_MAX) since we generate them.
-  std::pair<size_t, size_t> availableEvents() const final override;
+  std::pair<size_t, size_t> availableEvents() const final;
   /// Generate an event.
-  ProcessCode read(const AlgorithmContext& context) override final;
+  ProcessCode read(const AlgorithmContext& context) final;
 
  private:
   const Acts::Logger& logger() const { return *m_logger; }
