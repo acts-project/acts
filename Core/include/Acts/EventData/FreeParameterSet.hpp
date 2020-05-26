372b78f4aea62c998d4ded7d2a6e3ad8b4fc4ea5
Fabian Klimpel <fklimpel@cern.ch>
2020-05-26 18:14:58 +0200

    Testing format

--- Core/include/Acts/EventData/FreeParameterSet.hpp
+++ Core/include/Acts/EventData/FreeParameterSet.hpp
@@ -35,49 +35,40 @@ namespace Acts {
 /// @endcond
 
 //~ /**
- //~ * @class ParameterSet
- //~ *
- //~ * @brief Description of a set of (local) parameters
- //~ *
- //~ * @pre
- //~ * The template parameter @c ParameterPolicy must fulfill the following
- //~ * requirements:
- //~ *  -# It must contain a <tt>typedef #ParID_t</tt> specifying an integral
- //~ * type used to identify different
- //~ *     parameters. This could for example be an @c enum, @c short, or
- //~ * <tt>unsigned int</tt>.
- //~ *     This @c typedef must be convertible to an <tt>unsigned int</tt>
- //~ *  -# It must contain a <tt>typedef #ParValue_t</tt> specifying the type of
- //~ * the parameter values. This could for
- //~ *     instance be @c double, or @c float.
- //~ *  -# It must contain a definition of an integral constant named @c N which is
- //~ * assignable to an <tt>unsigned
- //~ *     int</tt> and which is equal to the total number of parameters in the
- //~ * system.
- //~ * @pre
- //~ *
- //~ * The template parameter pack @c params must be given in a strictly ascending
- //~ * order. The parameter pack must
- //~ * be non-empty and it cannot contain more elements than
- //~ * <tt>Acts::eBoundParametersSize</tt>.
- //~ *
- //~ * @test The behavior of this class is tested in the following unit tests:
- //~ *       - \link Acts::Test::BOOST_AUTO_TEST_CASE(parset_consistency_tests)
- //~ * general consistency\endlink
- //~ *       - \link Acts::Test::BOOST_AUTO_TEST_CASE(parset_copy_assignment_tests)
- //~ * copy/assignment/swap\endlink
- //~ *       - \link Acts::Test::BOOST_AUTO_TEST_CASE(parset_comparison_tests)
- //~ * comparison operators\endlink
- //~ *       - \link Acts::Test::BOOST_AUTO_TEST_CASE(parset_projection_tests)
- //~ * projection matrices\endlink
- //~ *       - \link Acts::Test::BOOST_AUTO_TEST_CASE(parset_residual_tests)
- //~ * residual calculation\endlink
- //~ *
- //~ * @tparam ParameterPolicy  struct or class containing the parameter definitions
- //~ * (see above)
- //~ * @tparam params           parameter pack containing the (local) parameters
- //~ * stored in this class
- //~ */
+//~ * @class ParameterSet
+//~ *
+//~ * @brief Description of a set of (local) parameters
+//~ *
+//~ * @pre
+//~ * The template parameter @c ParameterPolicy must fulfill the following
+//~ * requirements:
+//~ *  -# It must contain a <tt>typedef #ParID_t</tt> specifying an integral
+//~ * type used to identify different
+//~ *     parameters. This could for example be an @c enum, @c short, or
+//~ * <tt>unsigned int</tt>.
+//~ *     This @c typedef must be convertible to an <tt>unsigned int</tt>
+//~ *  -# It must contain a <tt>typedef #ParValue_t</tt> specifying the type of
+//~ * the parameter values. This could for
+//~ *     instance be @c double, or @c float.
+//~ *  -# It must contain a definition of an integral constant named @c N which
+//is ~ * assignable to an <tt>unsigned ~ *     int</tt> and which is equal to
+//the total number of parameters in the ~ * system. ~ * @pre ~ * ~ * The
+//template parameter pack @c params must be given in a strictly ascending ~ *
+//order. The parameter pack must ~ * be non-empty and it cannot contain more
+//elements than ~ * <tt>Acts::eBoundParametersSize</tt>. ~ * ~ * @test The
+//behavior of this class is tested in the following unit tests: ~ *       -
+//\link Acts::Test::BOOST_AUTO_TEST_CASE(parset_consistency_tests) ~ * general
+//consistency\endlink ~ *       - \link
+//Acts::Test::BOOST_AUTO_TEST_CASE(parset_copy_assignment_tests) ~ *
+//copy/assignment/swap\endlink ~ *       - \link
+//Acts::Test::BOOST_AUTO_TEST_CASE(parset_comparison_tests) ~ * comparison
+//operators\endlink ~ *       - \link
+//Acts::Test::BOOST_AUTO_TEST_CASE(parset_projection_tests) ~ * projection
+//matrices\endlink ~ *       - \link
+//Acts::Test::BOOST_AUTO_TEST_CASE(parset_residual_tests) ~ * residual
+//calculation\endlink ~ * ~ * @tparam ParameterPolicy  struct or class
+//containing the parameter definitions ~ * (see above) ~ * @tparam params
+//parameter pack containing the (local) parameters ~ * stored in this class ~ */
 template <FreeParametersIndices... params>
 class FreeParameterSet {
  private:
@@ -87,8 +78,9 @@ class FreeParameterSet {
       sizeof...(params);  ///< number of parameters stored in this class
 
   // static assert to check that the template parameters are consistent
-  static_assert(detail::are_sorted<true, true, FreeParametersIndices, params...>::value,
-                "parameter identifiers are not sorted");
+  static_assert(
+      detail::are_sorted<true, true, FreeParametersIndices, params...>::value,
+      "parameter identifiers are not sorted");
   static_assert(
       detail::are_within<unsigned int, 0, eBoundParametersSize,
                          static_cast<unsigned int>(params)...>::value,
@@ -120,14 +112,16 @@ class FreeParameterSet {
    * @param values values for the remaining stored parameters
    */
   template <typename... Tail>
-  FreeParameterSet(std::optional<CovMatrix_t> cov,
-               std::enable_if_t<sizeof...(Tail) + 1 == NPars, ParValue_t> head,
-               Tail... values)
+  FreeParameterSet(
+      std::optional<CovMatrix_t> cov,
+      std::enable_if_t<sizeof...(Tail) + 1 == NPars, ParValue_t> head,
+      Tail... values)
       : m_vValues(NPars) {
     if (cov) {
       m_optCovariance = std::move(*cov);
     }
-    detail::initialize_parset<FreeParametersIndices, params...>::init(*this, head, values...);
+    detail::initialize_parset<FreeParametersIndices, params...>::init(
+        *this, head, values...);
   }
 
   /**
@@ -147,7 +141,8 @@ class FreeParameterSet {
     if (cov) {
       m_optCovariance = std::move(*cov);
     }
-    detail::initialize_parset<FreeParametersIndices, params...>::init(*this, values);
+    detail::initialize_parset<FreeParametersIndices, params...>::init(*this,
+                                                                      values);
   }
 
   /**
@@ -179,7 +174,8 @@ class FreeParameterSet {
   /**
    * @brief assignment operator
    *
-   * @param rhs object whose content is assigned to this @c FreeParameterSet object
+   * @param rhs object whose content is assigned to this @c FreeParameterSet
+   * object
    */
   ParSet_t& operator=(const ParSet_t& rhs) {
     m_vValues = rhs.m_vValues;
@@ -190,7 +186,8 @@ class FreeParameterSet {
   /**
    * @brief move assignment operator
    *
-   * @param rhs object whose content is moved into this @c FreeParameterSet object
+   * @param rhs object whose content is moved into this @c FreeParameterSet
+   * object
    */
   ParSet_t& operator=(ParSet_t&& rhs) {
     m_vValues = std::move(rhs.m_vValues);
@@ -218,23 +215,24 @@ class FreeParameterSet {
    */
   template <FreeParametersIndices parameter>
   static constexpr size_t getIndex() {
-    return detail::get_position<FreeParametersIndices, parameter, params...>::value;
+    return detail::get_position<FreeParametersIndices, parameter,
+                                params...>::value;
   }
 
   //~ /**
-   //~ * @brief return parameter identifier for given index
-   //~ *
-   //~ * @tparam index position of parameter identifier in @c params
-   //~ * @remark @c index must be a positive number smaller than the size of the
-   //~ *         parameter pack @c params. Otherwise a compile-time error is
-   //~ *         generated.
-   //~ *
-   //~ * @return parameter identifier at position @c index in variadic template
-   //~ *         parameter set @c params
-   //~ */
+  //~ * @brief return parameter identifier for given index
+  //~ *
+  //~ * @tparam index position of parameter identifier in @c params
+  //~ * @remark @c index must be a positive number smaller than the size of the
+  //~ *         parameter pack @c params. Otherwise a compile-time error is
+  //~ *         generated.
+  //~ *
+  //~ * @return parameter identifier at position @c index in variadic template
+  //~ *         parameter set @c params
+  //~ */
   //~ template <size_t index>
   //~ static constexpr FreeParametersIndices getParID() {
-    //~ return detail::at_index<FreeParametersIndices, index, params...>::value;
+  //~ return detail::at_index<FreeParametersIndices, index, params...>::value;
   //~ }
 
   /**
@@ -275,16 +273,14 @@ class FreeParameterSet {
   }
 
   //~ /**
-   //~ * @brief sets values of stored parameters
-   //~ *
-   //~ * The values of the given vector are interpreted as parameter values in the
-   //~ * order
-   //~ * of the class template `params...`.
-   //~ *
-   //~ * @param values vector of length #NPars
-   //~ */
+  //~ * @brief sets values of stored parameters
+  //~ *
+  //~ * The values of the given vector are interpreted as parameter values in
+  //the ~ * order ~ * of the class template `params...`. ~ * ~ * @param values
+  //vector of length #NPars ~ */
   //~ void setParameters(const ParVector_t& values) {
-    //~ detail::initialize_parset<FreeParametersIndices, params...>::init(*this, values);
+  //~ detail::initialize_parset<FreeParametersIndices, params...>::init(*this,
+  //values);
   //~ }
 
   /**
@@ -299,7 +295,8 @@ class FreeParameterSet {
    */
   template <FreeParametersIndices parameter>
   bool contains() const {
-    return detail::is_contained<FreeParametersIndices, parameter, params...>::value;
+    return detail::is_contained<FreeParametersIndices, parameter,
+                                params...>::value;
   }
 
   /**
@@ -383,113 +380,82 @@ class FreeParameterSet {
   bool operator!=(const ParSet_t& rhs) const { return !(*this == rhs); }
 
   //~ /**
-   //~ * @brief project vector of full parameter set onto parameter sub-space
-   //~ *
-   //~ * Let \f$ \left(p_1 \dots p_N \right)\f$ be the full set of parameters out of
-   //~ * which the \f$m\f$
-   //~ * parameters \f$ \left( p_{i_1} \dots p_{i_m} \right), i_1 < i_2 < \dots <
-   //~ * i_m, m \le N, i_j \le N\f$
-   //~ * are stored in this FreeParameterSet object. Let \f$ \left(v^0_1 \dots v^0_N
-   //~ * \right)\f$ be the parameter
-   //~ * values given in the full FreeParameterSet, then this methods applies the
-   //~ * following mapping:
-   //~ * \f[
-   //~ * \mathbb{R}^{N \times 1} \mapsto \mathbb{R}^{m \times 1} : \left(
-   //~ * \begin{array}{c} v_1^0 \\ \vdots \\ v_N^0 \end{array} \right) \mapsto
-   //~ * \left( \begin{array}{c} v_{i_1}^0 \\ \vdots \\ v_{i_m}^0 \end{array}
-   //~ * \right)
-   //~ * \f]
-   //~ *
-   //~ * @param fullParSet FreeParameterSet object containing values for all defined
-   //~ * parameters
-   //~ *
-   //~ * @return vector containing only the parameter values from the full parameter
-   //~ * vector
-   //~ *         which are also defined for this FreeParameterSet object
-   //~ */
+  //~ * @brief project vector of full parameter set onto parameter sub-space
+  //~ *
+  //~ * Let \f$ \left(p_1 \dots p_N \right)\f$ be the full set of parameters out
+  //of ~ * which the \f$m\f$ ~ * parameters \f$ \left( p_{i_1} \dots p_{i_m}
+  //\right), i_1 < i_2 < \dots < ~ * i_m, m \le N, i_j \le N\f$ ~ * are stored
+  //in this FreeParameterSet object. Let \f$ \left(v^0_1 \dots v^0_N ~ *
+  //\right)\f$ be the parameter ~ * values given in the full FreeParameterSet,
+  //then this methods applies the ~ * following mapping: ~ * \f[ ~ *
+  //\mathbb{R}^{N \times 1} \mapsto \mathbb{R}^{m \times 1} : \left( ~ *
+  //\begin{array}{c} v_1^0 \\ \vdots \\ v_N^0 \end{array} \right) \mapsto ~ *
+  //\left( \begin{array}{c} v_{i_1}^0 \\ \vdots \\ v_{i_m}^0 \end{array} ~ *
+  //\right) ~ * \f] ~ * ~ * @param fullParSet FreeParameterSet object containing
+  //values for all defined ~ * parameters ~ * ~ * @return vector containing only
+  //the parameter values from the full parameter ~ * vector ~ *         which
+  //are also defined for this FreeParameterSet object ~ */
   //~ ParVector_t project(const FullFreeParameterSet& fullParSet) const {
-    //~ return projector() * fullParSet.getParameters();
+  //~ return projector() * fullParSet.getParameters();
   //~ }
 
   //~ /**
-   //~ * @brief calculate residual difference to full parameter vector
-   //~ *
-   //~ * Calculate the residual differences of the stored parameter values with
-   //~ * respect to the corresponding
-   //~ * parameter values in the full parameter vector. Hereby, the residual vector
-   //~ * is defined as
-   //~ *
-   //~ * \f[
-   //~ * \vec{r} = \left( \begin{array}{c} r_{i_1} \\ \vdots \\ r_{i_m} \end{array}
-   //~ * \right)
-   //~ *  = \left( \begin{array}{c} v_{i_1} \\ \vdots \\ v_{i_m} \end{array} \right)
-   //~ * -  \mathrm{Proj} \left( \begin{array}{c} v^0_{1} \\ \vdots \\ v^0_{N}
-   //~ * \end{array} \right)
-   //~ *  = \vec{v} - \mathrm{Proj} \left( \vec{v}^0 \right)
-   //~ * \f]
-   //~ *
-   //~ * where \f$\mathrm{Proj}\f$ is the projection matrix, \f$\vec{v}\f$ is the
-   //~ * vector of parameter values of
-   //~ * this FreeParameterSet object and \f$\vec{v}^0\f$ is the full parameter value
-   //~ * vector.
-   //~ *
-   //~ * @note Constraint and cyclic parameter value ranges are taken into account
-   //~ * when calculating
-   //~ *       the residual values.
-   //~ *
-   //~ * @param fullParSet FreeParameterSet object containing the full set of parameters
-   //~ *
-   //~ * @return vector containing the residual parameter values of this
-   //~ * FreeParameterSet object
-   //~ *         with respect to the given full parameter vector
-   //~ *
-   //~ * @sa FreeParameterSet::projector
-   //~ */
+  //~ * @brief calculate residual difference to full parameter vector
+  //~ *
+  //~ * Calculate the residual differences of the stored parameter values with
+  //~ * respect to the corresponding
+  //~ * parameter values in the full parameter vector. Hereby, the residual
+  //vector ~ * is defined as ~ * ~ * \f[ ~ * \vec{r} = \left( \begin{array}{c}
+  //r_{i_1} \\ \vdots \\ r_{i_m} \end{array} ~ * \right) ~ *  = \left(
+  //\begin{array}{c} v_{i_1} \\ \vdots \\ v_{i_m} \end{array} \right) ~ * -
+  //\mathrm{Proj} \left( \begin{array}{c} v^0_{1} \\ \vdots \\ v^0_{N} ~ *
+  //\end{array} \right) ~ *  = \vec{v} - \mathrm{Proj} \left( \vec{v}^0 \right)
+  //~ * \f]
+  //~ *
+  //~ * where \f$\mathrm{Proj}\f$ is the projection matrix, \f$\vec{v}\f$ is the
+  //~ * vector of parameter values of
+  //~ * this FreeParameterSet object and \f$\vec{v}^0\f$ is the full parameter
+  //value ~ * vector. ~ * ~ * @note Constraint and cyclic parameter value ranges
+  //are taken into account ~ * when calculating ~ *       the residual values.
+  //~ *
+  //~ * @param fullParSet FreeParameterSet object containing the full set of
+  //parameters ~ * ~ * @return vector containing the residual parameter values
+  //of this ~ * FreeParameterSet object ~ *         with respect to the given
+  //full parameter vector ~ * ~ * @sa FreeParameterSet::projector ~ */
   //~ /// @cond
   //~ template <
-      //~ typename T = ParSet_t,
-      //~ std::enable_if_t<not std::is_same<T, FullFreeParameterSet>::value, int> = 0>
+  //~ typename T = ParSet_t,
+  //~ std::enable_if_t<not std::is_same<T, FullFreeParameterSet>::value, int> =
+  //0>
   //~ /// @endcond
   //~ ParVector_t residual(const FullFreeParameterSet& fullParSet) const {
-    //~ return detail::residual_calculator<params...>::result(
-        //~ m_vValues, projector() * fullParSet.getParameters());
+  //~ return detail::residual_calculator<params...>::result(
+  //~ m_vValues, projector() * fullParSet.getParameters());
   //~ }
 
   //~ /**
-   //~ * @brief calculate residual difference to other parameter vector
-   //~ *
-   //~ * Calculate the residual differences of the stored parameter values with
-   //~ * respect to the values of
-   //~ * another FreeParameterSet object containing the same set of parameters. Hereby,
-   //~ * the residual vector is
-   //~ * defined as
-   //~ *
-   //~ * \f[
-   //~ * \vec{r} = \left( \begin{array}{c} r_{i_1} \\ \vdots \\ r_{i_m} \end{array}
-   //~ * \right)
-   //~ *  = \left( \begin{array}{c} v_{i_1} \\ \vdots \\ v_{i_m} \end{array} \right)
-   //~ * -  \left( \begin{array}{c} v^0_{1} \\ \vdots \\ v^0_{N} \end{array} \right)
-   //~ *  = \vec{v} - \left( \vec{v}^0 \right)
-   //~ * \f]
-   //~ *
-   //~ * where \f$\vec{v}\f$ is the vector of parameter values of this FreeParameterSet
-   //~ * object and \f$\vec{v}^0\f$
-   //~ * is the parameter value vector of the other FreeParameterSet object.
-   //~ *
-   //~ * @note Constraint and cyclic parameter value ranges are taken into account
-   //~ * when calculating
-   //~ *       the residual values.
-   //~ *
-   //~ * @param otherParSet FreeParameterSet object with identical set of contained
-   //~ * parameters
-   //~ *
-   //~ * @return vector containing the residual parameter values of this
-   //~ * FreeParameterSet object
-   //~ *         with respect to the given other parameter set
-   //~ */
+  //~ * @brief calculate residual difference to other parameter vector
+  //~ *
+  //~ * Calculate the residual differences of the stored parameter values with
+  //~ * respect to the values of
+  //~ * another FreeParameterSet object containing the same set of parameters.
+  //Hereby, ~ * the residual vector is ~ * defined as ~ * ~ * \f[ ~ * \vec{r} =
+  //\left( \begin{array}{c} r_{i_1} \\ \vdots \\ r_{i_m} \end{array} ~ * \right)
+  //~ *  = \left( \begin{array}{c} v_{i_1} \\ \vdots \\ v_{i_m} \end{array}
+  //\right) ~ * -  \left( \begin{array}{c} v^0_{1} \\ \vdots \\ v^0_{N}
+  //\end{array} \right) ~ *  = \vec{v} - \left( \vec{v}^0 \right) ~ * \f] ~ * ~
+  //* where \f$\vec{v}\f$ is the vector of parameter values of this
+  //FreeParameterSet ~ * object and \f$\vec{v}^0\f$ ~ * is the parameter value
+  //vector of the other FreeParameterSet object. ~ * ~ * @note Constraint and
+  //cyclic parameter value ranges are taken into account ~ * when calculating ~
+  //*       the residual values. ~ * ~ * @param otherParSet FreeParameterSet
+  //object with identical set of contained ~ * parameters ~ * ~ * @return vector
+  //containing the residual parameter values of this ~ * FreeParameterSet object
+  //~ *         with respect to the given other parameter set
+  //~ */
   //~ ParVector_t residual(const ParSet_t& otherParSet) const {
-    //~ return detail::residual_calculator<params...>::result(
-        //~ m_vValues, otherParSet.m_vValues);
+  //~ return detail::residual_calculator<params...>::result(
+  //~ m_vValues, otherParSet.m_vValues);
   //~ }
 
   /**
@@ -514,19 +480,18 @@ class FreeParameterSet {
   static constexpr unsigned int size() { return NPars; }
 
   //~ /**
-   //~ * @brief correct given parameter values
-   //~ *
-   //~ * Check that the given values are within in a valid range for the
-   //~ * corresponding parameter. If not, an
-   //~ * in-place correction is applied. The values are interpreted as parameter
-   //~ * values in the same order as
-   //~ * specified in the class template @c params.
-   //~ *
-   //~ * @param values vector with parameter values to be checked and corrected if
-   //~ * necessary
-   //~ */
+  //~ * @brief correct given parameter values
+  //~ *
+  //~ * Check that the given values are within in a valid range for the
+  //~ * corresponding parameter. If not, an
+  //~ * in-place correction is applied. The values are interpreted as parameter
+  //~ * values in the same order as
+  //~ * specified in the class template @c params.
+  //~ *
+  //~ * @param values vector with parameter values to be checked and corrected
+  //if ~ * necessary ~ */
   //~ static void correctValues(ParVector_t& values) {
-    //~ detail::value_corrector<params...>::result(values);
+  //~ detail::value_corrector<params...>::result(values);
   //~ }
 
  private:
