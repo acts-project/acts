20d17376cdc3659021948f5950cc56836d82c176
Moritz Kiehn <msmk@cern.ch>
2020-06-11 10:12:24 +0200

    Tests: add data path utility

---
+++ Tests/CommonHelpers/Acts/Tests/CommonHelpers/DataDirectory.hpp
@@ -0,0 +1,23 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace Acts {
+namespace Test {
+
+/// Get the full path to a file in the test data directory.
+///
+/// @param relativePath file path relative to the data directory
+std::string getDataPath(std::string_view relativePath);
+
+}  // namespace Test
+}  // namespace Acts
