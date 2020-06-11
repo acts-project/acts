ef38feeb56d64dae461e51bdbec10a8073d1c394
Moritz Kiehn <msmk@cern.ch>
2020-06-11 10:17:00 +0200

    Tests: add data directory access example

---
+++ Tests/IntegrationTests/PrintDataDirectory.cpp
@@ -0,0 +1,24 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// This is an example on how to access data from the test data directory.
+// It is not really an integration test, but since it does not use the
+// the unit test framework, placing it into the integration tests directory
+// is the least akward place.
+
+#include <cstddef>
+#include <iostream>
+
+#include "Acts/Tests/CommonHelpers/DataDirectory.hpp"
+
+int main(void) {
+  std::cout << Acts::Test::getDataPath("") << std::endl;
+  std::cout << Acts::Test::getDataPath("missing-dir/does_not_exists.txt")
+            << std::endl;
+  return EXIT_SUCCESS;
+}
