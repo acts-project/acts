8a15866808ba5a666b175fcb87d03aa4f831a5b4
Fabian Klimpel <fabian.klimpel@tum.de>
2020-07-10 09:32:43 +0200

    Stepper update methods without BoundParameters (#297)
    Currently the (C)KF needs to build multiple `BoundParameters` objects in order to update the parameters and the covariance matrix in the stepper state. This PR allows implements an explicit update method for this case. The old method becomes therewith deprecated. The corresponding methods in the `MultiTrajectory` become functions that deliver the parameters in global representation in order to be passed to the stepper state. Alongside a reinitialisation of the Jacobian in the `AtlasStepper` is removed in order to synchronise the behaviour of the `update()` method with the other steppers.
    
    Co-authored-by: Moritz Kiehn <msmk@cern.ch>

--- Core/include/Acts/Propagator/AtlasStepper.hpp
+++ Core/include/Acts/Propagator/AtlasStepper.hpp
@@ -463,29 +463,21 @@ class AtlasStepper {
   ///
   /// @param [in,out] state The stepper state for
   /// @param [in] pars The new track parameters at start
-  void update(State& state, const BoundParameters& pars) const {
+  void update(State& state, const FreeVector& parameters,
+              const Covariance& covariance) const {
     // state is ready - noting to do
     if (state.state_ready) {
       return;
     }
 
-    const Vector3D pos = pars.position();
-    const auto Vp = pars.parameters();
-
-    double Sf, Cf, Ce, Se;
-    Sf = sin(Vp(2));
-    Cf = cos(Vp(2));
-    Se = sin(Vp(3));
-    Ce = cos(Vp(3));
-
-    state.pVector[0] = pos(0);
-    state.pVector[1] = pos(1);
-    state.pVector[2] = pos(2);
-    state.pVector[3] = pars.time();
-    state.pVector[4] = Cf * Se;
-    state.pVector[5] = Sf * Se;
-    state.pVector[6] = Ce;
-    state.pVector[7] = Vp[4];
+    state.pVector[0] = parameters[eFreePos0];
+    state.pVector[1] = parameters[eFreePos1];
+    state.pVector[2] = parameters[eFreePos2];
+    state.pVector[3] = parameters[eFreeTime];
+    state.pVector[4] = parameters[eFreeDir0];
+    state.pVector[5] = parameters[eFreeDir1];
+    state.pVector[6] = parameters[eFreeDir2];
+    state.pVector[7] = parameters[eFreeQOverP];
 
     // @todo: remove magic numbers - is that the charge ?
     if (std::abs(state.pVector[7]) < .000000000000001) {
@@ -494,138 +486,11 @@ class AtlasStepper {
     }
 
     // prepare the jacobian if we have a covariance
-    if (pars.covariance()) {
-      // copy the covariance matrix
-      state.covariance =
-          new ActsSymMatrixD<eBoundParametersSize>(*pars.covariance());
-      state.covTransport = true;
-      state.useJacobian = true;
-      const auto transform = pars.referenceFrame(state.geoContext);
+    // copy the covariance matrix
+    state.covariance = new ActsSymMatrixD<eBoundParametersSize>(covariance);
+    state.covTransport = true;
+    state.useJacobian = true;
 
-      state.pVector[8] = transform(0, eLOC_0);
-      state.pVector[16] = transform(0, eLOC_1);
-      state.pVector[24] = 0.;
-      state.pVector[32] = 0.;
-      state.pVector[40] = 0.;
-      state.pVector[48] = 0.;  // dX /
-
-      state.pVector[9] = transform(1, eLOC_0);
-      state.pVector[17] = transform(1, eLOC_1);
-      state.pVector[25] = 0.;
-      state.pVector[33] = 0.;
-      state.pVector[41] = 0.;
-      state.pVector[49] = 0.;  // dY /
-
-      state.pVector[10] = transform(2, eLOC_0);
-      state.pVector[18] = transform(2, eLOC_1);
-      state.pVector[26] = 0.;
-      state.pVector[34] = 0.;
-      state.pVector[42] = 0.;
-      state.pVector[50] = 0.;  // dZ /
-
-      state.pVector[11] = 0.;
-      state.pVector[19] = 0.;
-      state.pVector[27] = 0.;
-      state.pVector[35] = 0.;
-      state.pVector[43] = 0.;
-      state.pVector[51] = 1.;  // dT/
-
-      state.pVector[12] = 0.;
-      state.pVector[20] = 0.;
-      state.pVector[28] = -Sf * Se;  // - sin(phi) * cos(theta)
-      state.pVector[36] = Cf * Ce;   // cos(phi) * cos(theta)
-      state.pVector[44] = 0.;
-      state.pVector[52] = 0.;  // dAx/
-
-      state.pVector[13] = 0.;
-      state.pVector[21] = 0.;
-      state.pVector[29] = Cf * Se;  // cos(phi) * sin(theta)
-      state.pVector[37] = Sf * Ce;  // sin(phi) * cos(theta)
-      state.pVector[45] = 0.;
-      state.pVector[53] = 0.;  // dAy/
-
-      state.pVector[14] = 0.;
-      state.pVector[22] = 0.;
-      state.pVector[30] = 0.;
-      state.pVector[38] = -Se;  // - sin(theta)
-      state.pVector[46] = 0.;
-      state.pVector[54] = 0.;  // dAz/
-
-      state.pVector[15] = 0.;
-      state.pVector[23] = 0.;
-      state.pVector[31] = 0.;
-      state.pVector[39] = 0.;
-      state.pVector[47] = 1.;
-      state.pVector[55] = 0.;  // dCM/
-
-      state.pVector[56] = 0.;
-      state.pVector[57] = 0.;
-      state.pVector[58] = 0.;
-      state.pVector[59] = 0.;
-
-      // special treatment for surface types
-      const auto& surface = pars.referenceSurface();
-      // the disc needs polar coordinate adaptations
-      if (surface.type() == Surface::Disc) {
-        double lCf = cos(Vp[1]);
-        double lSf = sin(Vp[1]);
-        double Ax[3] = {transform(0, 0), transform(1, 0), transform(2, 0)};
-        double Ay[3] = {transform(0, 1), transform(1, 1), transform(2, 1)};
-        double d0 = lCf * Ax[0] + lSf * Ay[0];
-        double d1 = lCf * Ax[1] + lSf * Ay[1];
-        double d2 = lCf * Ax[2] + lSf * Ay[2];
-        state.pVector[8] = d0;
-        state.pVector[9] = d1;
-        state.pVector[10] = d2;
-        state.pVector[16] = Vp[0] * (lCf * Ay[0] - lSf * Ax[0]);
-        state.pVector[17] = Vp[0] * (lCf * Ay[1] - lSf * Ax[1]);
-        state.pVector[18] = Vp[0] * (lCf * Ay[2] - lSf * Ax[2]);
-      }
-      // the line needs components that relate direction change
-      // with global frame change
-      if (surface.type() == Surface::Perigee ||
-          surface.type() == Surface::Straw) {
-        // sticking to the nomenclature of the original RkPropagator
-        // - axis pointing along the drift/transverse direction
-        double B[3] = {transform(0, 0), transform(1, 0), transform(2, 0)};
-        // - axis along the straw
-        double A[3] = {transform(0, 1), transform(1, 1), transform(2, 1)};
-        // - normal vector of the reference frame
-        double C[3] = {transform(0, 2), transform(1, 2), transform(2, 2)};
-
-        // projection of direction onto normal vector of reference frame
-        double PC = state.pVector[4] * C[0] + state.pVector[5] * C[1] +
-                    state.pVector[6] * C[2];
-        double Bn = 1. / PC;
-
-        double Bx2 = -A[2] * state.pVector[29];
-        double Bx3 = A[1] * state.pVector[38] - A[2] * state.pVector[37];
-
-        double By2 = A[2] * state.pVector[28];
-        double By3 = A[2] * state.pVector[36] - A[0] * state.pVector[38];
-
-        double Bz2 = A[0] * state.pVector[29] - A[1] * state.pVector[28];
-        double Bz3 = A[0] * state.pVector[37] - A[1] * state.pVector[36];
-
-        double B2 = B[0] * Bx2 + B[1] * By2 + B[2] * Bz2;
-        double B3 = B[0] * Bx3 + B[1] * By3 + B[2] * Bz3;
-
-        Bx2 = (Bx2 - B[0] * B2) * Bn;
-        Bx3 = (Bx3 - B[0] * B3) * Bn;
-        By2 = (By2 - B[1] * B2) * Bn;
-        By3 = (By3 - B[1] * B3) * Bn;
-        Bz2 = (Bz2 - B[2] * B2) * Bn;
-        Bz3 = (Bz3 - B[2] * B3) * Bn;
-
-        //  /dPhi      |     /dThe       |
-        state.pVector[24] = Bx2 * Vp[0];
-        state.pVector[32] = Bx3 * Vp[0];  // dX/
-        state.pVector[25] = By2 * Vp[0];
-        state.pVector[33] = By3 * Vp[0];  // dY/
-        state.pVector[26] = Bz2 * Vp[0];
-        state.pVector[34] = Bz3 * Vp[0];  // dZ/
-      }
-    }
     // now declare the state as ready
     state.state_ready = true;
   }
