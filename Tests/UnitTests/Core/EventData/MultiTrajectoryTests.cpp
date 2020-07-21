d3abc1b15c91eedf4fa2a2ebe214baf6bd3b2d1a
FabianKlimpel <fabian.klimpel@tum.de>
2020-07-21 17:00:47 +0200

    Format fixed

--- Tests/UnitTests/Core/EventData/MultiTrajectoryTests.cpp
+++ Tests/UnitTests/Core/EventData/MultiTrajectoryTests.cpp
@@ -548,7 +548,9 @@ BOOST_AUTO_TEST_CASE(trackstate_reassignment) {
 
   ActsMatrixD<maxmeasdim, eFreeParametersSize> projFull;
   projFull.setZero();
-  projFull.template topLeftCorner<decltype(m2)::size(), eBoundParametersSize>() = m2.projector();
+  projFull
+      .template topLeftCorner<decltype(m2)::size(), eBoundParametersSize>() =
+      m2.projector();
   BOOST_CHECK_EQUAL(ts.projector(), projFull);
 }
 
