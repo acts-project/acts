fb2f4b24fe9843dea7168c4e414c8af3b13c3b0e
Fabian Klimpel <fabian.klimpel@tum.de>
2020-07-27 13:52:07 +0200

    RootTrajectoryWriter cleanup (#325)
    * Renamed indices
    
    * Removed BoundParameters construction
    
    * Removed last traces of BoundParameters
    
    * Allow residual calculation from vector
    
    * Rebase
    
    * Fixed vector dimension
    
    * Format fixed

--- Core/include/Acts/EventData/Measurement.hpp
+++ Core/include/Acts/EventData/Measurement.hpp
@@ -267,6 +267,28 @@ class Measurement {
     return m_oParameters.residual(trackPars.getParameterSet());
   }
 
+  /// @brief calculate residual with respect to given track parameters
+  ///
+  /// @note It is checked that the residual for non-local parameters are in
+  /// valid
+  /// range (e.g.
+  ///       residuals in \f$\phi\f$ are corrected).
+  ///
+  /// @todo Implement validity check for residuals of local parameters.
+  ///
+  /// @param boundParameters reference bound parameters
+  /// @note The parameter ranges and the reference object of @p boundParameters
+  /// are not tested
+  ///
+  /// @return vector with the residual parameter values (in valid range)
+  ///
+  /// @sa ParameterSet::residual
+  ParameterVector residual(
+      const ActsVectorD<detail::ParametersSize<parameter_indices_t>::size>&
+          boundParameters) const {
+    return m_oParameters.residual(boundParameters);
+  }
+
   /// @brief equality operator
   ///
   /// @return @c true if parameter sets and associated surfaces/volumes compare
