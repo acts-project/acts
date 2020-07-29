b955302195a72124fe1710f51b66e3eb1fb2075c
Paul Gessinger <hello@paulgessinger.com>
2020-07-29 10:52:22 +0200

    format

--- Core/include/Acts/Propagator/Navigator.hpp
+++ Core/include/Acts/Propagator/Navigator.hpp
@@ -435,7 +435,8 @@ class Navigator {
     // - geometry and volume tree search for the lowest volume
     if (state.navigation.startSurface &&
         state.navigation.startSurface->associatedLayer()) {
-      ACTS_VERBOSE("Fast start initialization through association from Surface.");
+      ACTS_VERBOSE(
+          "Fast start initialization through association from Surface.");
       // assign the current layer and volume by association
       state.navigation.startLayer =
           state.navigation.startSurface->associatedLayer();
@@ -445,7 +446,8 @@ class Navigator {
       state.navigation.currentVolume = state.navigation.startVolume;
     } else {
       if (state.navigation.startVolume) {
-        ACTS_VERBOSE("Fast start initialization through association from Volume.");
+        ACTS_VERBOSE(
+            "Fast start initialization through association from Volume.");
         state.navigation.startLayer =
             state.navigation.startVolume->associatedLayer(
                 state.geoContext, stepper.position(state.stepping));
@@ -469,7 +471,6 @@ class Navigator {
         state.navigation.currentVolume = state.navigation.startVolume;
         if (state.navigation.startVolume) {
           ACTS_VERBOSE("Start volume resolved.");
-
         }
       }
     }
