21c72ceebd84726ed13a1f7e24bf411e61ff5356
Paul Gessinger <hello@paulgessinger.com>
2020-08-06 11:22:52 +0200

    format fix

--- Plugins/Cuda/include/Acts/Plugins/Cuda/Seeding/Seedfinder.hpp
+++ Plugins/Cuda/include/Acts/Plugins/Cuda/Seeding/Seedfinder.hpp
@@ -13,8 +13,8 @@
 #include "Acts/Seeding/InternalSeed.hpp"
 #include "Acts/Seeding/InternalSpacePoint.hpp"
 #include "Acts/Seeding/SeedFilter.hpp"
-#include "Acts/Seeding/SeedfinderConfig.hpp"
 #include "Acts/Seeding/Seedfinder.hpp"
+#include "Acts/Seeding/SeedfinderConfig.hpp"
 
 #include <array>
 #include <list>
