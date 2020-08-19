e3f9d4f83fb6d6cb4368687519d6136b265a4217
Moritz Kiehn <msmk@cern.ch>
2020-08-19 14:58:13 +0200

    TransformFreeToBound: add additional input variants

--- Core/src/EventData/TransformationFreeToBound.cpp
+++ Core/src/EventData/TransformationFreeToBound.cpp
@@ -13,22 +13,49 @@
 #include "Acts/Utilities/Logger.hpp"
 
 Acts::BoundVector Acts::detail::transformFreeToBoundParameters(
-    const Acts::Vector3D& position, double time,
-    const Acts::Vector3D& direction, double qOverP,
-    const Acts::Surface& surface, const Acts::GeometryContext& geoCtx) {
-  // construct the bound vector
+    const FreeVector& freeParams, const Surface& surface,
+    const GeometryContext& geoCtx) {
+  // initialize the bound vector
   BoundVector bp = BoundVector::Zero();
-  // this assumes the position is already on the surface
-  auto lpResult = surface.globalToLocal(geoCtx, position, direction);
-  if (not lpResult.ok()) {
+  // convert global to local position on the surface
+  auto position = freeParams.segment<3>(eFreePos0);
+  auto direction = freeParams.segment<3>(eFreeDir0);
+  auto result = surface.globalToLocal(geoCtx, position, direction);
+  if (result.ok()) {
+    auto localPosition = result.value();
+    bp[eBoundLoc0] = localPosition[ePos0];
+    bp[eBoundLoc1] = localPosition[ePos1];
+  } else {
     ACTS_LOCAL_LOGGER(
         Acts::getDefaultLogger("ParameterTransformation", Logging::INFO));
     ACTS_FATAL(
         "Inconsistency in global to local transformation from free to bound.")
   }
-  auto loc = lpResult.value();
-  bp[eBoundLoc0] = loc[ePos0];
-  bp[eBoundLoc1] = loc[ePos1];
+  bp[eBoundTime] = freeParams[eFreeTime];
+  bp[eBoundPhi] = VectorHelpers::phi(direction);
+  bp[eBoundTheta] = VectorHelpers::theta(direction);
+  bp[eBoundQOverP] = freeParams[eFreeQOverP];
+  return bp;
+}
+
+Acts::BoundVector Acts::detail::transformFreeToBoundParameters(
+    const Acts::Vector3D& position, FreeScalar time,
+    const Acts::Vector3D& direction, FreeScalar qOverP,
+    const Acts::Surface& surface, const Acts::GeometryContext& geoCtx) {
+  // initialize the bound vector
+  BoundVector bp = BoundVector::Zero();
+  // convert global to local position on the surface
+  auto result = surface.globalToLocal(geoCtx, position, direction);
+  if (result.ok()) {
+    auto localPosition = result.value();
+    bp[eBoundLoc0] = localPosition[ePos0];
+    bp[eBoundLoc1] = localPosition[ePos1];
+  } else {
+    ACTS_LOCAL_LOGGER(
+        Acts::getDefaultLogger("ParameterTransformation", Logging::INFO));
+    ACTS_FATAL(
+        "Inconsistency in global to local transformation from free to bound.")
+  }
   bp[eBoundTime] = time;
   bp[eBoundPhi] = VectorHelpers::phi(direction);
   bp[eBoundTheta] = VectorHelpers::theta(direction);
@@ -37,7 +64,18 @@ Acts::BoundVector Acts::detail::transformFreeToBoundParameters(
 }
 
 Acts::BoundVector Acts::detail::transformFreeToCurvilinearParameters(
-    double time, const Acts::Vector3D& direction, double qOverP) {
+    FreeScalar time, FreeScalar phi, FreeScalar theta, FreeScalar qOverP) {
+  BoundVector bp = BoundVector::Zero();
+  // local coordinates are zero by construction
+  bp[eBoundTime] = time;
+  bp[eBoundPhi] = phi;
+  bp[eBoundTheta] = theta;
+  bp[eBoundQOverP] = qOverP;
+  return bp;
+}
+
+Acts::BoundVector Acts::detail::transformFreeToCurvilinearParameters(
+    FreeScalar time, const Vector3D& direction, FreeScalar qOverP) {
   BoundVector bp = BoundVector::Zero();
   // local coordinates are zero by construction
   bp[eBoundTime] = time;
--- Tests/UnitTests/Core/EventData/TransformFreeToBoundTests.cpp
+++ Tests/UnitTests/Core/EventData/TransformFreeToBoundTests.cpp
@@ -8,13 +8,15 @@
 
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/unit_test.hpp>
-#include <cmath>
-#include <limits>
 
 #include "Acts/EventData/detail/TransformationFreeToBound.hpp"
 #include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
 #include "Acts/Utilities/UnitVectors.hpp"
 #include "Acts/Utilities/Units.hpp"
+
+#include <cmath>
+#include <limits>
+
 #include "TrackParametersTestData.hpp"
 
 using namespace Acts;
@@ -39,17 +41,43 @@ BOOST_DATA_TEST_CASE(
   // transform reference position
   Vector3D pos = surface->localToGlobal(geoCtx, loc, dir);
 
-  // convert to free parameters
-  BoundVector bv = detail::transformFreeToBoundParameters(
-      pos, time, dir, qOverP, *surface, geoCtx);
+  // convert free parameters to bound parameters
+  {
+    BOOST_TEST_INFO("Transform free parameters vector onto surface "
+                    << surface->name());
 
-  BOOST_TEST_INFO("Using surface " << surface->name());
-  CHECK_CLOSE_OR_SMALL(bv[eBoundLoc0], l0, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundLoc1], l1, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundTime], time, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundPhi], phi, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundTheta], theta, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundQOverP], qOverP, eps, eps);
+    FreeVector fv = FreeVector::Zero();
+    fv[eFreePos0] = pos[ePos0];
+    fv[eFreePos1] = pos[ePos1];
+    fv[eFreePos2] = pos[ePos2];
+    fv[eFreeTime] = time;
+    fv[eFreeDir0] = dir[eMom0];
+    fv[eFreeDir1] = dir[eMom1];
+    fv[eFreeDir2] = dir[eMom2];
+    fv[eFreeQOverP] = qOverP;
+    BoundVector bv =
+        detail::transformFreeToBoundParameters(fv, *surface, geoCtx);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundLoc0], l0, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundLoc1], l1, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTime], time, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundPhi], phi, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTheta], theta, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundQOverP], qOverP, eps, eps);
+  }
+  // convert separate components to bound parameters
+  {
+    BOOST_TEST_INFO("Transform free parameters components onto surface "
+                    << surface->name());
+
+    BoundVector bv = detail::transformFreeToBoundParameters(
+        pos, time, dir, qOverP, *surface, geoCtx);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundLoc0], l0, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundLoc1], l1, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTime], time, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundPhi], phi, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTheta], theta, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundQOverP], qOverP, eps, eps);
+  }
 }
 
 BOOST_DATA_TEST_CASE(GlobalToCurvilinearParameters, ts* phis* thetas* qOverPs,
@@ -60,16 +88,28 @@ BOOST_DATA_TEST_CASE(GlobalToCurvilinearParameters, ts* phis* thetas* qOverPs,
   GeometryContext geoCtx;
   Vector3D dir = makeDirectionUnitFromPhiTheta(phi, theta);
 
-  // convert to free parameters
-  BoundVector bv =
-      detail::transformFreeToCurvilinearParameters(time, dir, qOverP);
-
-  CHECK_SMALL(bv[eBoundLoc0], eps);
-  CHECK_SMALL(bv[eBoundLoc1], eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundTime], time, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundPhi], phi, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundTheta], theta, eps, eps);
-  CHECK_CLOSE_OR_SMALL(bv[eBoundQOverP], qOverP, eps, eps);
+  // convert w/ direction
+  {
+    BoundVector bv =
+        detail::transformFreeToCurvilinearParameters(time, dir, qOverP);
+    CHECK_SMALL(bv[eBoundLoc0], eps);
+    CHECK_SMALL(bv[eBoundLoc1], eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTime], time, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundPhi], phi, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTheta], theta, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundQOverP], qOverP, eps, eps);
+  }
+  // convert w/ angles
+  {
+    BoundVector bv =
+        detail::transformFreeToCurvilinearParameters(time, phi, theta, qOverP);
+    CHECK_SMALL(bv[eBoundLoc0], eps);
+    CHECK_SMALL(bv[eBoundLoc1], eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTime], time, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundPhi], phi, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundTheta], theta, eps, eps);
+    CHECK_CLOSE_OR_SMALL(bv[eBoundQOverP], qOverP, eps, eps);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
