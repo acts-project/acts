6014a4ac41334c6a0277e75d2912cf7e79a8b71f
Paul Gessinger <hello@paulgessinger.com>
2020-08-24 09:06:32 +0200

    Apply format to the cuda file

--- Plugins/Cuda/include/Acts/Plugins/Cuda/Utilities/CudaUtils.cu
+++ Plugins/Cuda/include/Acts/Plugins/Cuda/Utilities/CudaUtils.cu
@@ -8,16 +8,18 @@
 
 #pragma once
 
+#include <iostream>
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <iostream>
 
-#define ACTS_CUDA_ERROR_CHECK(ans) { cudaAssert((ans), __FILE__, __LINE__); }
-inline void cudaAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-  if (code != cudaSuccess)
-    {
-      fprintf(stderr,"CUDAassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-    }
+#define ACTS_CUDA_ERROR_CHECK(ans) \
+  { cudaAssert((ans), __FILE__, __LINE__); }
+inline void cudaAssert(cudaError_t code, const char *file, int line,
+                       bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "CUDAassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
+    if (abort)
+      exit(code);
+  }
 }
