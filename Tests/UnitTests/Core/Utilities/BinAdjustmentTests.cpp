249c40daeaa4c3475229f4c39142c3931c646191
Corentin Allaire <corentin.allaire@cern.ch>
2020-08-25 14:23:26 +0200

    added test for BinAdjustment

---
+++ Tests/UnitTests/Core/Utilities/BinAdjustmentTests.cpp
@@ -0,0 +1,54 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2017-2018 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Surfaces/CylinderBounds.hpp"
+#include "Acts/Surfaces/RadialBounds.hpp"
+
+#include "Acts/Utilities/BinAdjustment.hpp"
+#include "Acts/Utilities/BinUtility.hpp"
+#include "Acts/Utilities/Definitions.hpp"
+
+#include <cmath>
+
+namespace Acts {
+namespace Test {
+
+// Test Radial
+BOOST_AUTO_TEST_CASE(BinAdjustment_Radial) {
+  RadialBounds bound(50, 75, M_PI, 0);
+  BinUtility bu;
+  bu += BinUtility(1, 0, 1, Acts::open, Acts::binR);
+  bu += BinUtility(1, 0, 1, Acts::closed, Acts::binPhi);
+
+  adjustBinUtility(bu, bound, Transform3D::Identity());
+
+  BOOST_CHECK_EQUAL(bu.binningData()[0].min, 50);
+  BOOST_CHECK_EQUAL(bu.binningData()[0].max, 75);
+  BOOST_CHECK_EQUAL(bu.binningData()[1].min, -M_PI);
+  BOOST_CHECK_EQUAL(bu.binningData()[1].max, M_PI);
+}
+
+// Test Cylinder
+BOOST_AUTO_TEST_CASE(BinAdjustment_Cylinder) {
+  CylinderBounds bound(25, 50, M_PI / 4, 0);
+  BinUtility bu;
+  bu += BinUtility(1, 0, 1, Acts::open, Acts::binPhi);
+  bu += BinUtility(1, 0, 1, Acts::open, Acts::binZ);
+
+  adjustBinUtility(bu, bound, Transform3D::Identity());
+
+  BOOST_CHECK_EQUAL(bu.binningData()[0].min, -M_PI / 4);
+  BOOST_CHECK_EQUAL(bu.binningData()[0].max, M_PI) / 4;
+  BOOST_CHECK_EQUAL(bu.binningData()[1].min, -50);
+  BOOST_CHECK_EQUAL(bu.binningData()[1].max, 50);
+}
+
+}  // namespace Test
+}  // namespace Acts
