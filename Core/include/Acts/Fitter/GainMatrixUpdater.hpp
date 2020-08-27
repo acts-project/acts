acts-project/acts

commit 4e53ea123a9712c88817d261692c54c1230301dc

Check the outlier before Kalman filtering (#359)
* Check the outlier before Kalman update

* Fix the KF unit test regarding outlier finder

* Remove KF redundancy

Core/include/Acts/Fitter/GainMatrixUpdater.hpp
@@ -121,8 +121,7 @@ class GainMatrixUpdater {
           ACTS_VERBOSE("Filtered covariance:\n" << filtered_covariance);
 
           // calculate filtered residual
-          par_t residual(trackState.calibratedSize());
-          residual = (calibrated - H * filtered);
+          const par_t residual = (calibrated - H * filtered);
           ACTS_VERBOSE("Residual: " << residual.transpose());
 
           trackState.chi2() =

Core/include/Acts/Fitter/KalmanFitter.hpp
@@ -499,40 +499,41 @@ class KalmanFitter {
         typeFlags.set(TrackStateFlag::MaterialFlag);
         typeFlags.set(TrackStateFlag::ParameterFlag);
 
-        // If the update is successful, set covariance and
-        auto updateRes = m_updater(state.geoContext, trackStateProxy, forward);
-        if (!updateRes.ok()) {
-          ACTS_ERROR("Update step failed: " << updateRes.error());
-          return updateRes.error();
-        } else {
-          if (not m_outlierFinder(trackStateProxy)) {
-            // Set the measurement type flag
-            typeFlags.set(TrackStateFlag::MeasurementFlag);
-            // Update the stepping state with filtered parameters
-            ACTS_VERBOSE(
-                "Filtering step successful, updated parameters are : \n"
-                << trackStateProxy.filtered().transpose());
-            // update stepping state using filtered parameters after kalman
-            // update We need to (re-)construct a BoundParameters instance
-            // here, which is a bit awkward.
-            stepper.update(state.stepping,
-                           MultiTrajectoryHelpers::freeFiltered(
-                               state.options.geoContext, trackStateProxy),
-                           trackStateProxy.filteredCovariance());
-            // We count the state with measurement
-            ++result.measurementStates;
-          } else {
-            ACTS_VERBOSE(
-                "Filtering step successful. But measurement is deterimined "
-                "to "
-                "be an outlier. Stepping state is not updated.")
-            // Set the outlier type flag
-            typeFlags.set(TrackStateFlag::OutlierFlag);
+        // Check if the state is an outlier.
+        // If not, run Kalman update, tag it as a
+        // measurement and update the stepping state. Otherwise, just tag it as
+        // an outlier
+        if (not m_outlierFinder(trackStateProxy)) {
+          // Run Kalman update
+          auto updateRes =
+              m_updater(state.geoContext, trackStateProxy, forward);
+          if (!updateRes.ok()) {
+            ACTS_ERROR("Update step failed: " << updateRes.error());
+            return updateRes.error();
           }
-
-          // Update state and stepper with post material effects
-          materialInteractor(surface, state, stepper, postUpdate);
+          // Set the measurement type flag
+          typeFlags.set(TrackStateFlag::MeasurementFlag);
+          // Update the stepping state with filtered parameters
+          ACTS_VERBOSE("Filtering step successful, updated parameters are : \n"
+                       << trackStateProxy.filtered().transpose());
+          // update stepping state using filtered parameters after kalman
+          stepper.update(state.stepping,
+                         MultiTrajectoryHelpers::freeFiltered(
+                             state.options.geoContext, trackStateProxy),
+                         trackStateProxy.filteredCovariance());
+          // We count the state with measurement
+          ++result.measurementStates;
+        } else {
+          ACTS_VERBOSE(
+              "Filtering step successful. But measurement is deterimined "
+              "to "
+              "be an outlier. Stepping state is not updated.")
+          // Set the outlier type flag
+          typeFlags.set(TrackStateFlag::OutlierFlag);
         }
+
+        // Update state and stepper with post material effects
+        materialInteractor(surface, state, stepper, postUpdate);
         // We count the processed state
         ++result.processedStates;
       } else if (surface->surfaceMaterial() != nullptr) {

Tests/UnitTests/Core/Fitter/KalmanFitterTests.cpp
@@ -230,19 +230,54 @@ struct MinimalOutlierFinder {
   ///
   /// @tparam track_state_t Type of the track state
   ///
-  /// @param trackState The trackState to investigate
+  /// @param state The track state to investigate
   ///
   /// @return Whether it's outlier or not
   template <typename track_state_t>
-  bool operator()(const track_state_t& trackState) const {
-    double chi2 = trackState.chi2();
+  bool operator()(const track_state_t& state) const {
+    // Can't determine if it's an outlier if no calibrated measurement or no
+    // predicted parameters
+    if (not state.hasCalibrated() or not state.hasPredicted()) {
+      return false;
+    }
+
+    // The predicted parameters coefficients
+    const auto& predicted = state.predicted();
+    // The predicted parameters covariance
+    const auto& predicted_covariance = state.predictedCovariance();
+
+    // Calculate the chi2 using predicted parameters and calibrated measurement
+    double chi2 = std::numeric_limits<double>::max();
+    visit_measurement(
+        state.calibrated(), state.calibratedCovariance(),
+        state.calibratedSize(),
+        [&](const auto calibrated, const auto calibrated_covariance) {
+          constexpr size_t measdim = decltype(calibrated)::RowsAtCompileTime;
+          using par_t = ActsVectorD<measdim>;
+
+          // Take the projector (measurement mapping function)
+          const ActsMatrixD<measdim, eBoundParametersSize> H =
+              state.projector()
+                  .template topLeftCorner<measdim, eBoundParametersSize>();
+
+          // Calculate the residual
+          const par_t residual = calibrated - H * predicted;
+
+          // Calculate the chi2
+          chi2 = (residual.transpose() *
+                  ((calibrated_covariance +
+                    H * predicted_covariance * H.transpose()))
+                      .inverse() *
+                  residual)
+                     .eval()(0, 0);
+        });
+
+    // In case the chi2 is too small
     if (std::abs(chi2) < chi2Tolerance) {
       return false;
     }
-    // The measurement dimension
-    size_t ndf = trackState.calibratedSize();
     // The chisq distribution
-    boost::math::chi_squared chiDist(ndf);
+    boost::math::chi_squared chiDist(state.calibratedSize());
     // The p-Value
     double pValue = 1 - boost::math::cdf(chiDist, chi2);
     // If pValue is NOT significant enough => outlier

commit 3317ec609f01c3639b68da9c96e2840edb2df045

Resolve build regression from #359
It seems that Eigen matrix constructors from expression templates have more build-time overhead than the assignment operator equivalent. When that extra overhead is sent through the `visitMeasurement` code bloat multiplier (see #350 for context), that ultimately translates into a +0.7GB build RSS hit in `EventDataView3D` tests. Let's revert that part of #359 for now.

Core/include/Acts/Fitter/GainMatrixUpdater.hpp
@@ -113,7 +113,14 @@ class GainMatrixUpdater {
           ACTS_VERBOSE("Filtered covariance:\n" << filtered_covariance);
 
           // calculate filtered residual
-          const par_t residual = (calibrated - H * filtered);
+          //
+          // FIXME: Without separate residual construction and assignment, we
+          //        currently take a +0.7GB build memory consumption hit in the
+          //        EventDataView unit tests. Revisit this once Measurement
+          //        overhead problems (Acts issue #350) are sorted out.
+          //
+          par_t residual;
+          residual = calibrated - H * filtered;
           ACTS_VERBOSE("Residual: " << residual.transpose());
 
           trackState.chi2() =