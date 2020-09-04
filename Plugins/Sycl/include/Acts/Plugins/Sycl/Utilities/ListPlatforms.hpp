1f64ccb249090d4e57259c8dd9c47adcd939c116
czangela <33939088+czangela@users.noreply.github.com>
2020-09-04 16:45:43 +0200

    feat: Implementation of seed finding in SYCL (#416)
    Adds a SYCL plugin and an implementation of the seed finding algorithm using SYCL.

---
+++ Plugins/Sycl/include/Acts/Plugins/Sycl/Utilities/ListPlatforms.hpp
@@ -0,0 +1,15 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+namespace Acts::Sycl {
+/// @brief This function allows us to list availabe SYCL platforms and devices.
+void listPlatforms();
+
+}  // namespace Acts::Sycl
