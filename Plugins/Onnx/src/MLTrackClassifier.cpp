73a84a9d317dd0a83dd27e9accf11d5e9b987e0a
Irina Ene <irina.ene@berkeley.edu>
2020-09-09 23:36:26 -0700

    add source file for MLTrackClassifier

---
+++ Plugins/Onnx/src/MLTrackClassifier.cpp
@@ -0,0 +1,116 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <cassert>
+#include <stdexcept>
+
+#include "Acts/Plugins/Onnx/MLTrackClassifier.hpp"
+
+// parametrized constructor
+Acts::MLTrackClassifier::MLTrackClassifier(Ort::Env& env,
+                                           const char* modelPath) {
+  // set the ONNX runtime session options
+  Ort::SessionOptions sessionOptions;
+  // set graph optimization level
+  sessionOptions.SetGraphOptimizationLevel(
+      GraphOptimizationLevel::ORT_ENABLE_BASIC);
+  // create the Ort session
+  m_session = std::make_unique<Ort::Session>(env, modelPath, sessionOptions);
+
+  // default allocator
+  Ort::AllocatorWithDefaultOptions allocator;
+
+  // get the names of the input nodes of the model
+  size_t numInputNodes = m_session->GetInputCount();
+  m_inputNodeNames.resize(numInputNodes);
+
+  // iterate over all input nodes and get the name
+  for (size_t i = 0; i < numInputNodes; i++) {
+    m_inputNodeNames[i] = m_session->GetInputName(i, allocator);
+
+    // get the dimensions of the input nodes
+    // here we assume that all input nodes have the dimensions
+    Ort::TypeInfo inputTypeInfo = m_session->GetInputTypeInfo(i);
+    auto tensorInfo = inputTypeInfo.GetTensorTypeAndShapeInfo();
+    m_inputNodeDims = tensorInfo.GetShape();
+    // fix for symbolic dim = -1 from python
+    for (size_t j = 0; j < m_inputNodeDims.size(); j++) {
+      if (m_inputNodeDims[j] < 0) {
+        m_inputNodeDims[j] = 1;
+      }
+    }
+  }
+
+  // get the names of the output nodes
+  size_t numOutputNodes = m_session->GetOutputCount();
+  m_outputNodeNames.resize(numOutputNodes);
+
+  // iterate over all output nodes and get the name
+  for (size_t i = 0; i < numOutputNodes; i++) {
+    m_outputNodeNames[i] = m_session->GetOutputName(i, allocator);
+  }
+}
+
+// prediction function
+Acts::MLTrackClassifier::TrackLabels Acts::MLTrackClassifier::predictTrackLabel(
+    std::vector<float>& inputFeatures, const double& decisionThreshProb) const {
+  // check that the decision threshold is a probability
+  if ((decisionThreshProb < 0.) || (decisionThreshProb > 1.)) {
+    throw std::invalid_argument(
+        "predictTrackLabel: Decision threshold "
+        "probability is not in [0, 1].");
+  }
+
+  // run the model over the input
+  float* outputTensor = runONNXInference(inputFeatures);
+  // this is binary classification, so only need first value
+  float outputProbability = outputTensor[0];
+
+  // the output layer computes how confident the network is that the track is a
+  // duplicate, so need to convert that to a label
+  if (outputProbability > decisionThreshProb) {
+    return TrackLabels::duplicate;
+  }
+  return TrackLabels::good;
+}
+
+// inference function using ONNX runtime
+// the function assumes that the model has 1 input node and 1 output node
+float* Acts::MLTrackClassifier::runONNXInference(
+    std::vector<float>& inputTensorValues) const {
+  // create input tensor object from data values
+  // note: this assumes the model has only 1 input node
+  Ort::MemoryInfo memoryInfo =
+      Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+  Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
+      memoryInfo, inputTensorValues.data(), inputTensorValues.size(),
+      m_inputNodeDims.data(), m_inputNodeDims.size());
+  // double-check that inputTensor is a Tensor
+  if (!inputTensor.IsTensor()) {
+    throw std::runtime_error(
+        "runONNXInference: conversion of input to Tensor failed. ");
+  }
+
+  // score model on input tensors, get back output tensors
+  std::vector<Ort::Value> outputTensors =
+      m_session->Run(Ort::RunOptions{nullptr}, m_inputNodeNames.data(),
+                     &inputTensor, m_inputNodeNames.size(),
+                     m_outputNodeNames.data(), m_outputNodeNames.size());
+  // double-check that outputTensors contains Tensors and that the count matches
+  // that of output nodes
+  if (!outputTensors[0].IsTensor() ||
+      (outputTensors.size() != m_outputNodeNames.size())) {
+    throw std::runtime_error(
+        "runONNXInference: calculation of output failed. ");
+  }
+
+  // get pointer to output tensor float values
+  // note: this assumes the model has only 1 output node
+  float* outputTensor = outputTensors.front().GetTensorMutableData<float>();
+  return outputTensor;
+}
