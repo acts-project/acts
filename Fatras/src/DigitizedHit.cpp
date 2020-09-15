5c7bdbab2b192765a33448b3d4a016d4446d7953
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-09-15 15:33:21 +0200

    licence

--- Fatras/src/DigitizedHit.cpp
+++ Fatras/src/DigitizedHit.cpp
@@ -3,13 +3,15 @@
 // Copyright (C) 2020 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "ActsFatras/EventData/DigitizedHit.hpp"
 
 ActsFatras::DigitizedHit::DigitizedHit(
     std::vector<Hit>&& hits, const Acts::Surface& surface,
     std::unique_ptr<const DigitizedHit::IContent> content)
+
     : m_simulatedHits(std::move(hits)),
       m_surface(surface.getSharedPtr()),
       m_content(std::move(content)) {}
