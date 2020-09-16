4ad6db88a709bc6186dfe3db09dbd81882c2e74f
Moritz Kiehn <msmk@cern.ch>
2020-09-16 13:24:11 +0200

    docs: describe post-v1.0 API versioning (#464)
    Add documentation that outlines the public API and the versioning scheme (SemVer). Adds Github templates for issues and pull request with checklists.
    
    Closes #392. Closes #402.

---
+++ .github/issue_template.md
@@ -0,0 +1,26 @@
+PLEASE FOLLOW THE CHECKLIST BELOW WHEN CREATING A NEW ISSUE. THE CHECKLIST IS
+FOR YOUR INFORMATION AND MUST BE REMOVED BEFORE SUBMITTING THE ISSUE.
+
+## Checklist
+
+- [ ] Is the issue marked with exactly one of the following labels
+
+    - Bug
+    - Feature
+    - Improvement
+    - infrastructure
+
+- [ ] If the issue reports a bug:
+
+    - [ ] Is it also marked with one of the `Impact - {Minor,Major,Critical}` labels?
+    - [ ] Does the description contain all necessary information to reproduce
+        the error?
+
+- [ ] Is the issue assigned to a milestone? This should be `next` unless you
+    target a specific release.
+
+- [ ] If the issue is a question or the start of a discussion, please add the
+    `Needs discussion` label.
+
+- [ ] If the issue tracks a long development or multiple underlying issues
+    please add the `Long Term` label.
--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -5,12 +5,40 @@
 #   Sphinx_EXECUTABLE - Sphinx executable if if was found
 #
 
+set(_hints)
+
+# use python installation to find potential install locations for sphinx
+find_package(Python3 QUIET)
+if(Python3_EXECUTABLE)
+  execute_process(
+    COMMAND ${Python3_EXECUTABLE} -m site --user-base
+    OUTPUT_VARIABLE Python3_USER_BASE)
+  execute_process(
+    COMMAND ${Python3_EXECUTABLE} -c "import sys; print(sys.exec_prefix)"
+    OUTPUT_VARIABLE Python3_EXEC_PREFIX)
+  # strip newlines
+  string(REPLACE "\n" "" Python3_USER_BASE ${Python3_USER_BASE})
+  string(REPLACE "\n" "" Python3_EXEC_PREFIX ${Python3_EXEC_PREFIX})
+  # add derived binary dirs as hints
+  list(APPEND _hints "${Python3_USER_BASE}/bin" "${Python3_EXEC_PREFIX}/bin")
+endif()
+
+# find sphinx executable
+if(_hints)
+  message(STATUS "Using additional Sphinx search hints:")
+  foreach(_hint IN LISTS _hints)
+    message(STATUS " ${_hint}")
+  endforeach()
+endif()
 find_program(
   Sphinx_EXECUTABLE
   NAMES sphinx-build sphinx-build-3
+  HINTS ${_hints}
   DOC "Sphinx documentation generator")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   Sphinx
   REQUIRED_VARS Sphinx_EXECUTABLE)
+
+unset(_hints)
