5aae5cb42f3e2f98d9fe34fb256332ac1bc64200
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-09-16 15:16:44 +0200

    first commit

---
+++ Examples/Algorithms/Digitization/include/ActsExamples/Digitization/DigitizationOptions.hpp
@@ -0,0 +1,29 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "ActsExamples/Digitization/PlanarSteppingAlgorithm.hpp"
+#include "ActsExamples/Utilities/OptionsFwd.hpp"
+
+namespace ActsExamples {
+namespace Options {
+
+/// Add Digitization options.
+///
+/// @param desc The options description to add options to
+void addDigitizationOptions(Description& desc);
+
+/// Read Digitization Config from the options.
+///
+/// @param variables The variables to read from
+/// DigitizationAlgorithm::Config readDigitizationConfig(
+///    const Variables& variables);
+
+}  // namespace Options
+}  // namespace ActsExamples
---
+++ Examples/Algorithms/Digitization/src/DigitizationOptions.cpp
@@ -0,0 +1,50 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/Digitization/DigitizationOptions.hpp"
+#include "ActsExamples/Utilities/Options.hpp"
+
+#include <string>
+
+void ActsExamples::Options::addDigitizationOptions(
+    ActsExamples::Options::Description& desc) {
+  using boost::program_options::bool_switch;
+  using boost::program_options::value;
+  using namespace std;
+
+  auto opt = desc.add_options();
+  opt("digi-input-hits", value<string>()->default_value(""),
+      "Name of the input hit collection.");
+  opt("digi-output-measurements", value<string>()->default_value(""),
+      "Name of the output measurement collection.");
+  opt("digi-config-file", value<string>()->default_value(""),
+      "Configuration (.json) file for digitization description, overwrites "
+      "options input on command line.");
+  opt("digi-smear-volume-name",
+      value<read_strings>()->multitoken()->default_value({}),
+      "Input: sensitive volume names.");
+  opt("digi-smear-dimensions",
+      value<read_series>()->multitoken()->default_value({}),
+      "Input: smear parameters for this volume.");
+  opt("digi-smear-binvalues",
+      value<read_series>()->multitoken()->default_value({}),
+      "Input: smear binning values for this volume.");
+  opt("digi-smear-sigmas", value<read_range>()->multitoken()->default_value({}),
+      "Input: smear sigma values for this volume.");
+}
+
+/**
+ActsExamples::DigitizationAlgorithm::Config
+ActsExamples::Options::readDigitizationConfig(
+    const boost::program_options::variables_map& variables) {
+  using namespace Acts::UnitLiterals;
+  DigitizationAlgorithm::Config digiCfg;
+
+  return digiCfg;
+}
+*/
