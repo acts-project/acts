4b603714d58485df07cc58377bba7b89eda13a3b
Tomohiro Yamazaki <tomohiro.yamazaki@cern.ch>
2020-09-22 00:09:42 -0700

    Add seeding example base

---
+++ Examples/Run/Seeding/SeedingExample.cpp
@@ -0,0 +1,22 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/GenericDetector/GenericDetector.hpp"
+#include "ActsExamples/Seeding/SeedingExampleBase.hpp"
+
+/// @brief main executable
+///
+/// @param argc The argument count
+/// @param argv The argument list
+int main(int argc, char* argv[]) {
+  // --------------------------------------------------------------------------------
+  GenericDetector detector;
+
+  // now process it
+  return seedingExample(argc, argv, detector);
+}
