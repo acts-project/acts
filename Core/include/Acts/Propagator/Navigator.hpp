4e9e4bc58a51c3338d2634a518242ba0357d2bb6
Corentin Allaire <corentin.allaire@cern.ch>
2020-09-25 16:47:22 +0200

    fix

--- Core/include/Acts/Propagator/Navigator.hpp
+++ Core/include/Acts/Propagator/Navigator.hpp
@@ -705,8 +705,8 @@ class Navigator {
           // did we find any surfaces?
           auto protoNavSurfacesIter = protoNavSurfaces.begin();
           bool sameSurface = false;
-          double initialPathLength = state.navigation.navSurfaces.begin();
-          state.navigation.navSurfaceIter->intersection.pathLength;
+          double initialPathLength =
+              protoNavSurfacesIter->intersection.pathLength;
           while (protoNavSurfacesIter != protoNavSurfaces.end() &&
                  fabs(protoNavSurfacesIter->intersection.pathLength -
                       initialPathLength) < s_epsilon) {
