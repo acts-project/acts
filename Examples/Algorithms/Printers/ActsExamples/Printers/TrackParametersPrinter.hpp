80c0661aea614e2df8df9ec944a0383afa9382f5
Moritz Kiehn <msmk@cern.ch>
2020-09-25 17:56:08 +0200

    Examples/Printers: rework algorithms

---
+++ Examples/Algorithms/Printers/ActsExamples/Printers/TrackParametersPrinter.hpp
@@ -0,0 +1,33 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "ActsExamples/Framework/BareAlgorithm.hpp"
+
+#include <string>
+
+namespace ActsExamples {
+
+/// Print track parameters.
+class TrackParametersPrinter : public BareAlgorithm {
+ public:
+  struct Config {
+    /// Input tracks parameters collection.
+    std::string inputTrackParameters;
+  };
+
+  TrackParametersPrinter(const Config& cfg, Acts::Logging::Level level);
+
+  ProcessCode execute(const AlgorithmContext& ctx) const;
+
+ private:
+  Config m_cfg;
+};
+
+}  // namespace ActsExamples
