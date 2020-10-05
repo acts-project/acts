3fff55bf8ff8190c7ef2312b1d7658517e6a0937
Moritz Kiehn <msmk@cern.ch>
2020-10-05 15:28:54 +0200

    refactor(examples): extend examples printers (#499)
    Extend the printers algorithms to be able to print particles, hits, and track parameters. The reading example for CSV files is replaced by two utilties, `ActsExampleShowParticles` and `ActsExampleShowFatrasGeneric`. They either print just particles or the full simulated Fatras output using the updated printer algorithms. Contains the following related changes:
    
    - Require file stems to be explicitly set in the Csv particle reader and writer. This avoids cases, where multiple particle files exist for each event and there is no well-defined default one.
    - Use a more compact `Particle` output.
    - Print the Fatras process type by name instead of just a number.
    - Remove the (recently introduced) brackets from the `GeometryIdentifier` and `MultiIndex` output. They were introduced to better separate the id from other information in a combined output. After using it for while, I realize that it is just distracting and possible separation is better done at a different level.
    
      Example: the particle output prints an identifier encoded by an `MultiIndex` as `id=(x|y|z)|pdg=...`. Without the brackets this would end up as `id=x|y|z|pdg=...`. The bracketing is now handled by the `Particle` output, not by the `MultiIndex`.

---
+++ Examples/Algorithms/Printers/ActsExamples/Printers/ParticlesPrinter.cpp
@@ -0,0 +1,48 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ParticlesPrinter.hpp"
+
+#include "Acts/Utilities/Helpers.hpp"
+#include "Acts/Utilities/Logger.hpp"
+#include "Acts/Utilities/Units.hpp"
+#include "ActsExamples/EventData/SimParticle.hpp"
+#include "ActsExamples/Framework/WhiteBoard.hpp"
+#include "ActsFatras/Utilities/ParticleData.hpp"
+
+#include <stdexcept>
+
+ActsExamples::ParticlesPrinter::ParticlesPrinter(const Config& cfg,
+                                                 Acts::Logging::Level lvl)
+    : BareAlgorithm("ParticlesPrinter", lvl), m_cfg(cfg) {
+  if (m_cfg.inputParticles.empty()) {
+    throw std::invalid_argument("Input particles collection is not configured");
+  }
+}
+
+ActsExamples::ProcessCode ActsExamples::ParticlesPrinter::execute(
+    const ActsExamples::AlgorithmContext& ctx) const {
+  using namespace Acts::UnitLiterals;
+
+  const auto& particles =
+      ctx.eventStore.get<SimParticleContainer>(m_cfg.inputParticles);
+
+  ACTS_INFO("event " << ctx.eventNumber << " collection '"
+                     << m_cfg.inputParticles << "' contains "
+                     << particles.size() << " particles");
+  for (const auto& particle : particles) {
+    ACTS_INFO("  particle " << particle);
+    ACTS_INFO("    process_type: " << particle.process())
+    ACTS_INFO("    position:     " << particle.position().transpose() / 1_mm
+                                   << " mm");
+    ACTS_INFO("    direction:    " << particle.unitDirection().transpose());
+    ACTS_INFO("    time:         " << particle.time() / 1_ns << " ns");
+    ACTS_INFO("    |p|:          " << particle.absMomentum() / 1_GeV << " GeV");
+  }
+  return ProcessCode::SUCCESS;
+}
