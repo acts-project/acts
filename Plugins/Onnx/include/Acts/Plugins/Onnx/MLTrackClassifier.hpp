aa04a07613523f1de399509373844e6097ab55bb
Irina Ene <irina.ene@berkeley.edu>
2020-10-09 18:26:29 -0700

    split up onnx plugin into base class for general model loading and inference and specialized class for prediction of track labels

--- Plugins/Onnx/include/Acts/Plugins/Onnx/MLTrackClassifier.hpp
+++ Plugins/Onnx/include/Acts/Plugins/Onnx/MLTrackClassifier.hpp
@@ -9,27 +9,19 @@
 #pragma once
 
 #include <vector>
-#include <core/session/onnxruntime_cxx_api.h>
+
+#include "Acts/Plugins/Onnx/OnnxRuntimeBase.hpp"
 
 namespace Acts {
 
-// Classify tracks as good/duplicate/fake using using a deep neural network.
-class MLTrackClassifier {
+// Specialized class that labels tracks as good/duplicate/fake using a
+// deep neural network.
+class MLTrackClassifier : public OnnxRuntimeBase {
+  using OnnxRuntimeBase::OnnxRuntimeBase;
+
  public:
   /// @brief The labels for track quality
   enum TrackLabels { good, duplicate, fake };
-
-  /// @brief Default constructor
-  MLTrackClassifier() = default;
-
-  /// @brief Parametrized constructor
-  ///
-  /// @param env the ONNX runtime environment
-  /// @param modelPath the path to the ML model in *.onnx format
-  MLTrackClassifier(Ort::Env& env, const char* modelPath);
-
-  /// @brief Default destructor
-  ~MLTrackClassifier() = default;
 
   /// @brief Predict the track label
   ///
@@ -41,20 +33,6 @@ class MLTrackClassifier {
   /// @return The predicted track label of the trajectory
   TrackLabels predictTrackLabel(std::vector<float>& inputFeatures,
                                 const double& decisionThreshProb) const;
-
-  /// @brief Run the ONNX inference function
-  ///
-  /// @param inputTensorValues The input feature values used for prediction
-  ///
-  /// @return Pointer to output values
-  float* runONNXInference(std::vector<float>& inputTensorValues) const;
-
- private:
-  /// ONNX runtime session / model properties
-  std::unique_ptr<Ort::Session> m_session;
-  std::vector<const char*> m_inputNodeNames;
-  std::vector<int64_t> m_inputNodeDims;
-  std::vector<const char*> m_outputNodeNames;
 };
 
 }  // namespace Acts
--- Plugins/Onnx/src/MLTrackClassifier.cpp
+++ Plugins/Onnx/src/MLTrackClassifier.cpp
@@ -11,51 +11,6 @@
 
 #include "Acts/Plugins/Onnx/MLTrackClassifier.hpp"
 
-// parametrized constructor
-Acts::MLTrackClassifier::MLTrackClassifier(Ort::Env& env,
-                                           const char* modelPath) {
-  // set the ONNX runtime session options
-  Ort::SessionOptions sessionOptions;
-  // set graph optimization level
-  sessionOptions.SetGraphOptimizationLevel(
-      GraphOptimizationLevel::ORT_ENABLE_BASIC);
-  // create the Ort session
-  m_session = std::make_unique<Ort::Session>(env, modelPath, sessionOptions);
-
-  // default allocator
-  Ort::AllocatorWithDefaultOptions allocator;
-
-  // get the names of the input nodes of the model
-  size_t numInputNodes = m_session->GetInputCount();
-  m_inputNodeNames.resize(numInputNodes);
-
-  // iterate over all input nodes and get the name
-  for (size_t i = 0; i < numInputNodes; i++) {
-    m_inputNodeNames[i] = m_session->GetInputName(i, allocator);
-
-    // get the dimensions of the input nodes
-    // here we assume that all input nodes have the dimensions
-    Ort::TypeInfo inputTypeInfo = m_session->GetInputTypeInfo(i);
-    auto tensorInfo = inputTypeInfo.GetTensorTypeAndShapeInfo();
-    m_inputNodeDims = tensorInfo.GetShape();
-    // fix for symbolic dim = -1 from python
-    for (size_t j = 0; j < m_inputNodeDims.size(); j++) {
-      if (m_inputNodeDims[j] < 0) {
-        m_inputNodeDims[j] = 1;
-      }
-    }
-  }
-
-  // get the names of the output nodes
-  size_t numOutputNodes = m_session->GetOutputCount();
-  m_outputNodeNames.resize(numOutputNodes);
-
-  // iterate over all output nodes and get the name
-  for (size_t i = 0; i < numOutputNodes; i++) {
-    m_outputNodeNames[i] = m_session->GetOutputName(i, allocator);
-  }
-}
-
 // prediction function
 Acts::MLTrackClassifier::TrackLabels Acts::MLTrackClassifier::predictTrackLabel(
     std::vector<float>& inputFeatures, const double& decisionThreshProb) const {
@@ -77,40 +32,4 @@ Acts::MLTrackClassifier::TrackLabels Acts::MLTrackClassifier::predictTrackLabel(
     return TrackLabels::duplicate;
   }
   return TrackLabels::good;
-}
-
-// inference function using ONNX runtime
-// the function assumes that the model has 1 input node and 1 output node
-float* Acts::MLTrackClassifier::runONNXInference(
-    std::vector<float>& inputTensorValues) const {
-  // create input tensor object from data values
-  // note: this assumes the model has only 1 input node
-  Ort::MemoryInfo memoryInfo =
-      Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
-  Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
-      memoryInfo, inputTensorValues.data(), inputTensorValues.size(),
-      m_inputNodeDims.data(), m_inputNodeDims.size());
-  // double-check that inputTensor is a Tensor
-  if (!inputTensor.IsTensor()) {
-    throw std::runtime_error(
-        "runONNXInference: conversion of input to Tensor failed. ");
-  }
-
-  // score model on input tensors, get back output tensors
-  std::vector<Ort::Value> outputTensors =
-      m_session->Run(Ort::RunOptions{nullptr}, m_inputNodeNames.data(),
-                     &inputTensor, m_inputNodeNames.size(),
-                     m_outputNodeNames.data(), m_outputNodeNames.size());
-  // double-check that outputTensors contains Tensors and that the count matches
-  // that of output nodes
-  if (!outputTensors[0].IsTensor() ||
-      (outputTensors.size() != m_outputNodeNames.size())) {
-    throw std::runtime_error(
-        "runONNXInference: calculation of output failed. ");
-  }
-
-  // get pointer to output tensor float values
-  // note: this assumes the model has only 1 output node
-  float* outputTensor = outputTensors.front().GetTensorMutableData<float>();
-  return outputTensor;
 }
