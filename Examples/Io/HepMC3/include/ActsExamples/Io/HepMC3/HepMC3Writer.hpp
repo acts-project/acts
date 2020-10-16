e683f5a20a63228b3a65312ddfdc34ae3cd5b341
Fabian Klimpel <fabian.klimpel@tum.de>
2020-10-16 16:07:08 +0200

    refactor(examples): HepMCWriter and Reader as algorithm (#525)
    This PR modifies the `HepMCWriter` and `HepMCReader` such that they can be used as algorithms in the `Sequencer`.

--- Examples/Io/HepMC3/include/ActsExamples/Io/HepMC3/HepMC3Writer.hpp
+++ Examples/Io/HepMC3/include/ActsExamples/Io/HepMC3/HepMC3Writer.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2018 CERN for the benefit of the Acts project
+// Copyright (C) 2018-2020 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,26 +8,44 @@
 
 #pragma once
 
-#include <HepMC3/GenEvent.h>
+#include "ActsExamples/Framework/WriterT.hpp"
+#include "ActsExamples/Utilities/OptionsFwd.hpp"
+#include <string>
+#include <HepMC/GenEvent.h>
 #include <HepMC3/WriterAscii.h>
 
 namespace ActsExamples {
 
 /// HepMC3 event writer.
-struct HepMC3WriterAscii {
+class HepMC3AsciiWriter final : public WriterT<std::vector<HepMC3::GenEvent>> {
  public:
-  /// @brief Writes an event to file
-  /// @param writer writer of run files
-  /// @param event storage of the event
-  /// @return boolean indicator if the writing was successful
-  /// @note HepMC3 does not state a success or failure. The returned argument is
-  /// always true.
-  bool writeEvent(HepMC3::WriterAscii& writer,
-                  std::shared_ptr<HepMC3::GenEvent> event);
+  struct Config {
+    // The output directory
+    std::string outputDir;
+    // The stem of output file names
+    std::string outputStem;
+    // The input collection
+    std::string inputEvents;
+  };
 
-  /// @brief Reports the status of the writer
-  /// @param writer writer of run files
-  /// @return boolean status indicator
-  bool status(HepMC3::WriterAscii& writer);
+  /// Construct the writer.
+  ///
+  /// @param [in] cfg Config of the writer
+  /// @param [in] lvl The level of the logger
+  HepMC3AsciiWriter(const Config&& cfg, Acts::Logging::Level lvl);
+
+  /// Writing events to file.
+  ///
+  /// @param [in] ctx The context of this algorithm
+  /// @param [in] events The recorded HepMC3 events
+  ///
+  /// @return Code describing whether the writing was successful
+  ProcessCode writeT(
+      const ActsExamples::AlgorithmContext& ctx,
+      const std::vector<HepMC3::GenEvent>& events) final override;
+
+ private:
+  /// The configuration of this writer
+  Config m_cfg;
 };
 }  // namespace ActsExamples
