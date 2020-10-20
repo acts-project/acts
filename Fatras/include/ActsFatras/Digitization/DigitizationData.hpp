9a2aa79b087b7d7de4f54c4b87342d16073a3308
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-10-20 11:54:52 +0200

    introduce Channelizer & directory strcture in Fatras/src

---
+++ Fatras/include/ActsFatras/Digitization/DigitizationData.hpp
@@ -0,0 +1,107 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <Acts/EventData/ParameterSet.hpp>
+#include <Acts/Geometry/GeometryContext.hpp>
+#include <Acts/Utilities/BinUtility.hpp>
+#include <Acts/Utilities/ParameterDefinitions.hpp>
+
+#include <functional>
+#include <unordered_set>
+
+namespace Acts {
+class Surface;
+}  // namespace Acts
+
+namespace ActsFatras {
+
+class Hit;
+
+/// Digitization input to be used by the digitizers to harmonize
+/// the interface
+///
+struct DigitizationInput {
+  std::reference_wrapper<const Hit> hit;
+  std::reference_wrapper<const Acts::GeometryContext> geoContext;
+  const Acts::Surface* surface = nullptr;
+  Acts::BinUtility segmentation;
+
+  /// Only valid constructor, wraps the @param hit_,
+  /// the  and optionally the @param surface_
+  DigitizationInput(
+      std::reference_wrapper<const Hit> hit_,
+      std::reference_wrapper<const Acts::GeometryContext> geoContext_,
+      const Acts::Surface* surface_ = nullptr,
+      Acts::BinUtility segmentation_ = Acts::BinUtility())
+      : hit(hit_),
+        geoContext(geoContext_),
+        surface(surface_),
+        segmentation(segmentation_) {}
+
+  DigitizationInput() = delete;
+};
+
+/// A single cell definition: index, cell central value
+using Cell = std::pair<unsigned int, double>;
+
+/// A channel definition: Cell idenficiation, readout word, links
+///
+/// @tparam signal_t Type of the signal, requires += operator
+/// @tparam kParameters ... The parameters pack
+template <typename signal_t, Acts::BoundIndices... kParameters>
+struct Channel {
+  /// The cell identification in sizeof..(kParameters) dimensions
+  std::array<Cell, sizeof...(kParameters)> cellId;
+  /// The signal value, as complex as possible,
+  /// but need += operator and double() cast for the weight
+  signal_t value = 0.;
+  /// The potential (truth) linke
+  std::unordered_set<unsigned int> links = {};
+
+  /// Channel constructor
+  ///
+  /// @param cellId_ The Cell idenficiation and position
+  /// @param value_ The Cell value
+  /// @param links_ The (optional) links to e.g. truth indices
+  Channel(std::array<Cell, sizeof...(kParameters)> cellId_, signal_t value_,
+          std::unordered_set<unsigned int> links_ = {})
+      : cellId(cellId_), value(value_), links(links_) {}
+
+  Channel() = delete;
+};
+
+/// A Cluster definition.
+///
+/// @tparam signal_t Type of the signal carried, see above
+/// @tparam kParameters Parameters pack for the cluster
+///
+template <typename signal_t, Acts::BoundIndices... kParameters>
+struct Cluster {
+  /// The parameters
+  Acts::ParameterSet<Acts::BoundIndices, kParameters...> parameterSet;
+  /// The resulting cluster size
+  std::array<unsigned int, sizeof...(kParameters)> clusterSize;
+  /// The contained Channels
+  std::vector<Channel<signal_t, kParameters...> > channels;
+
+  /// Cluster constructor
+  ///
+  /// @param pSet The parameter set repesenting cluster position and error
+  /// @param cSize The cluster size definition
+  /// @param cChannels The channel
+  Cluster(Acts::ParameterSet<Acts::BoundIndices, kParameters...> pSet,
+          std::array<unsigned int, sizeof...(kParameters)> cSize,
+          std::vector<Channel<signal_t, kParameters...> > cChannels)
+      : parameterSet(pSet), clusterSize(cSize), channels(cChannels) {}
+
+  Cluster() = delete;
+};
+
+}  // namespace ActsFatras
---
+++ Tests/UnitTests/Fatras/Digitization/ChannelizerTests.cpp
@@ -0,0 +1,107 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "ActsFatras/Digitization/Channelizer.hpp"
+#include <Acts/Geometry/GeometryContext.hpp>
+#include <Acts/Surfaces/DiscSurface.hpp>
+#include <Acts/Surfaces/PlaneSurface.hpp>
+#include <Acts/Surfaces/RadialBounds.hpp>
+#include <Acts/Surfaces/RectangleBounds.hpp>
+#include <Acts/Tests/CommonHelpers/FloatComparisons.hpp>
+#include <Acts/Utilities/BinUtility.hpp>
+#include <Acts/Utilities/Definitions.hpp>
+#include <Acts/Utilities/Helpers.hpp>
+
+namespace ActsFatras {
+
+BOOST_AUTO_TEST_SUITE(Digitization)
+
+BOOST_AUTO_TEST_CASE(ChannelizerCartesian) {
+  Acts::GeometryContext geoCtx;
+
+  auto rectangleBounds = std::make_shared<Acts::RectangleBounds>(1., 1.);
+  auto planeSurface = Acts::Surface::makeShared<Acts::PlaneSurface>(
+      Acts::Transform3D::Identity(), rectangleBounds);
+
+  // The segementation
+  Acts::BinUtility pixelated(20, -1., 1., Acts::open, Acts::binX);
+  pixelated += Acts::BinUtility(20, -1., 1., Acts::open, Acts::binY);
+
+  Channelizer cl;
+
+  // Test: Normal hit into the surface
+  Acts::Vector2D nPosition(0.37, 0.76);
+  auto nSegments =
+      cl.segments(geoCtx, *planeSurface, pixelated, nPosition, nPosition);
+  BOOST_CHECK(nSegments.size() == 1);
+  BOOST_CHECK(nSegments[0].bin[0] == 13);
+  BOOST_CHECK(nSegments[0].bin[1] == 17);
+
+  // Test: Inclined hit into the surface - negative x direction
+  Acts::Vector2D ixPositionS(0.37, 0.76);
+  Acts::Vector2D ixPositionE(0.02, 0.73);
+  auto ixSegments =
+      cl.segments(geoCtx, *planeSurface, pixelated, ixPositionS, ixPositionE);
+  BOOST_CHECK(ixSegments.size() == 4);
+
+  // Test: Inclined hit into the surface - positive y direction
+  Acts::Vector2D iyPositionS(0.37, 0.76);
+  Acts::Vector2D iyPositionE(0.39, 0.91);
+  auto iySegments =
+      cl.segments(geoCtx, *planeSurface, pixelated, iyPositionS, iyPositionE);
+  BOOST_CHECK(iySegments.size() == 3);
+
+  // Test: Inclined hit into the surface - x/y direction
+  Acts::Vector2D ixyPositionS(-0.27, 0.76);
+  Acts::Vector2D ixyPositionE(-0.02, -0.73);
+  auto ixySegments =
+      cl.segments(geoCtx, *planeSurface, pixelated, ixyPositionS, ixyPositionE);
+  BOOST_CHECK(ixySegments.size() == 18);
+}
+
+BOOST_AUTO_TEST_CASE(ChannelizerPolarRadial) {
+  Acts::GeometryContext geoCtx;
+
+  auto radialBounds =
+      std::make_shared<const Acts::RadialBounds>(5., 10., 0.25, 0.);
+  auto radialDisc = Acts::Surface::makeShared<Acts::DiscSurface>(
+      Acts::Transform3D::Identity(), radialBounds);
+
+  // The segementation
+  Acts::BinUtility strips(2, 5., 10., Acts::open, Acts::binR);
+  strips += Acts::BinUtility(250, -0.25, 0.25, Acts::open, Acts::binPhi);
+
+  Channelizer cl;
+
+  // Test: Normal hit into the surface
+  Acts::Vector2D nPosition(6.76, 0.5);
+  auto nSegments =
+      cl.segments(geoCtx, *radialDisc, strips, nPosition, nPosition);
+  BOOST_CHECK(nSegments.size() == 1);
+  BOOST_CHECK(nSegments[0].bin[0] == 0);
+  BOOST_CHECK(nSegments[0].bin[1] == 161);
+
+  // Test: now opver more phi strips
+  Acts::Vector2D sPositionS(6.76, 0.5);
+  Acts::Vector2D sPositionE(7.03, -0.3);
+  auto sSegment =
+      cl.segments(geoCtx, *radialDisc, strips, sPositionS, sPositionE);
+  BOOST_CHECK(sSegment.size() == 59);
+
+  // Test: jump over R boundary, but stay in phi bin
+  sPositionS = Acts::Vector2D(6.76, 0.);
+  sPositionE = Acts::Vector2D(7.83, 0.);
+  sSegment = cl.segments(geoCtx, *radialDisc, strips, sPositionS, sPositionE);
+  BOOST_CHECK(sSegment.size() == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace ActsFatras
