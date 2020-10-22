da8c43d34337e3fd57e413413f9ceb9c5a8e83d0
FabianKlimpel <fabian.klimpel@tum.de>
2020-10-22 10:28:57 +0200

    Code for testing removed

--- Examples/Run/Geant4/HepMC/EventRecordingExample.cpp
+++ Examples/Run/Geant4/HepMC/EventRecordingExample.cpp
@@ -66,9 +66,6 @@ int main(int argc, char* argv[]) {
   erConfig.detectorConstruction = std::move(g4detector);
   erConfig.seed1 = vm["g4-rnd-seed1"].as<unsigned int>();
   erConfig.seed2 = vm["g4-rnd-seed2"].as<unsigned int>();
-  erConfig.processFilter = {"Transportation"};
-  erConfig.eventSelectionProcess = {"Inelastic"};
-  erConfig.eventRejectionProcess = {"Decay"};
 
   // Create the writer
   auto hepMC3WriterConfig = ActsExamples::Options::readHepMC3WriterOptions(vm);
