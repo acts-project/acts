1ee55427a01ca24ae268e1ea44fd9b9949a5b8ed
xai <xiaocong.ai@cern.ch>
2020-10-26 23:35:17 +0100

    Move the truth fitting and CKF reco to shared path

---
+++ Examples/Run/Reconstruction/Common/RecCKFTracks.cpp
@@ -0,0 +1,196 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2019-2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/Detector/IBaseDetector.hpp"
+#include "ActsExamples/Digitization/HitSmearing.hpp"
+#include "ActsExamples/Framework/Sequencer.hpp"
+#include "ActsExamples/Framework/WhiteBoard.hpp"
+#include "ActsExamples/Geometry/CommonGeometry.hpp"
+#include "ActsExamples/Io/Csv/CsvOptionsReader.hpp"
+#include "ActsExamples/Io/Csv/CsvParticleReader.hpp"
+#include "ActsExamples/Io/Csv/CsvSimHitReader.hpp"
+#include "ActsExamples/Io/Performance/CKFPerformanceWriter.hpp"
+#include "ActsExamples/Io/Root/RootTrajectoryParametersWriter.hpp"
+#include "ActsExamples/Io/Root/RootTrajectoryStatesWriter.hpp"
+#include "ActsExamples/Options/CommonOptions.hpp"
+#include "ActsExamples/Plugins/BField/BFieldOptions.hpp"
+#include "ActsExamples/TrackFinding/TrackFindingAlgorithm.hpp"
+#include "ActsExamples/TrackFinding/TrackFindingOptions.hpp"
+#include "ActsExamples/TruthTracking/ParticleSmearing.hpp"
+#include "ActsExamples/TruthTracking/TruthSeedSelector.hpp"
+#include "ActsExamples/Utilities/Options.hpp"
+#include "ActsExamples/Utilities/Paths.hpp"
+#include <Acts/Utilities/Units.hpp>
+
+#include <memory>
+
+using namespace Acts::UnitLiterals;
+using namespace ActsExamples;
+
+int recCKFTracks(int argc, char* argv[],
+                 std::shared_ptr<ActsExamples::IBaseDetector> detector) {
+  // setup and parse options
+  auto desc = ActsExamples::Options::makeDefaultOptions();
+  Options::addSequencerOptions(desc);
+  Options::addRandomNumbersOptions(desc);
+  Options::addGeometryOptions(desc);
+  Options::addMaterialOptions(desc);
+  Options::addInputOptions(desc);
+  Options::addOutputOptions(desc);
+  detector->addOptions(desc);
+  Options::addBFieldOptions(desc);
+  Options::addTrackFindingOptions(desc);
+
+  auto vm = Options::parse(desc, argc, argv);
+  if (vm.empty()) {
+    return EXIT_FAILURE;
+  }
+
+  Sequencer sequencer(Options::readSequencerConfig(vm));
+
+  // Read some standard options
+  auto logLevel = Options::readLogLevel(vm);
+  auto inputDir = vm["input-dir"].as<std::string>();
+  auto outputDir = ensureWritableDirectory(vm["output-dir"].as<std::string>());
+  auto rnd = std::make_shared<ActsExamples::RandomNumbers>(
+      Options::readRandomNumbersConfig(vm));
+
+  // Setup detector geometry
+  auto geometry = Geometry::build(vm, *detector);
+  auto trackingGeometry = geometry.first;
+  // Add context decorators
+  for (auto cdr : geometry.second) {
+    sequencer.addContextDecorator(cdr);
+  }
+  // Setup the magnetic field
+  auto magneticField = Options::readBField(vm);
+
+  // Read particles (initial states) and clusters from CSV files
+  auto particleReader = Options::readCsvParticleReaderConfig(vm);
+  particleReader.inputStem = "particles_initial";
+  particleReader.outputParticles = "particles_initial";
+  sequencer.addReader(
+      std::make_shared<CsvParticleReader>(particleReader, logLevel));
+  // Read truth hits from CSV files
+  auto simHitReaderCfg = Options::readCsvSimHitReaderConfig(vm);
+  simHitReaderCfg.inputStem = "simhits";
+  simHitReaderCfg.outputSimHits = "simhits";
+  sequencer.addReader(
+      std::make_shared<CsvSimHitReader>(simHitReaderCfg, logLevel));
+
+  // Create smeared measurements
+  HitSmearing::Config hitSmearingCfg;
+  hitSmearingCfg.inputSimHits = simHitReaderCfg.outputSimHits;
+  hitSmearingCfg.outputMeasurements = "measurements";
+  hitSmearingCfg.outputSourceLinks = "sourcelinks";
+  hitSmearingCfg.outputMeasurementParticlesMap = "measurement_particles_map";
+  hitSmearingCfg.outputMeasurementSimHitsMap = "measurement_simhits_map";
+  hitSmearingCfg.sigmaLoc0 = 25_um;
+  hitSmearingCfg.sigmaLoc1 = 100_um;
+  hitSmearingCfg.randomNumbers = rnd;
+  hitSmearingCfg.trackingGeometry = trackingGeometry;
+  sequencer.addAlgorithm(
+      std::make_shared<HitSmearing>(hitSmearingCfg, logLevel));
+
+  // Pre-select particles
+  // The pre-selection will select truth particles satisfying provided criteria
+  // from all particles read in by particle reader for further processing. It
+  // has no impact on the truth hits read-in by the cluster reader.
+  // @TODO: add options for truth particle selection criteria
+  TruthSeedSelector::Config particleSelectorCfg;
+  particleSelectorCfg.inputParticles = particleReader.outputParticles;
+  particleSelectorCfg.inputMeasurementParticlesMap =
+      hitSmearingCfg.outputMeasurementParticlesMap;
+  particleSelectorCfg.outputParticles = "particles_selected";
+  particleSelectorCfg.ptMin = 1_GeV;
+  particleSelectorCfg.nHitsMin = 9;
+  sequencer.addAlgorithm(
+      std::make_shared<TruthSeedSelector>(particleSelectorCfg, logLevel));
+
+  const auto& inputParticles = particleSelectorCfg.outputParticles;
+  // Create smeared particles states
+  ParticleSmearing::Config particleSmearingCfg;
+  particleSmearingCfg.inputParticles = inputParticles;
+  particleSmearingCfg.outputTrackParameters = "smearedparameters";
+  particleSmearingCfg.randomNumbers = rnd;
+  // Gaussian sigmas to smear particle parameters
+  particleSmearingCfg.sigmaD0 = 20_um;
+  particleSmearingCfg.sigmaD0PtA = 30_um;
+  particleSmearingCfg.sigmaD0PtB = 0.3 / 1_GeV;
+  particleSmearingCfg.sigmaZ0 = 20_um;
+  particleSmearingCfg.sigmaZ0PtA = 30_um;
+  particleSmearingCfg.sigmaZ0PtB = 0.3 / 1_GeV;
+  particleSmearingCfg.sigmaPhi = 1_degree;
+  particleSmearingCfg.sigmaTheta = 1_degree;
+  particleSmearingCfg.sigmaPRel = 0.01;
+  particleSmearingCfg.sigmaT0 = 1_ns;
+  sequencer.addAlgorithm(
+      std::make_shared<ParticleSmearing>(particleSmearingCfg, logLevel));
+
+  // Setup the track finding algorithm with CKF
+  // It takes all the source links created from truth hit smearing, seeds from
+  // truth particle smearing and source link selection config
+  auto trackFindingCfg = Options::readTrackFindingConfig(vm);
+  trackFindingCfg.inputMeasurements = hitSmearingCfg.outputMeasurements;
+  trackFindingCfg.inputSourceLinks = hitSmearingCfg.outputSourceLinks;
+  trackFindingCfg.inputInitialTrackParameters =
+      particleSmearingCfg.outputTrackParameters;
+  trackFindingCfg.outputTrajectories = "trajectories";
+  trackFindingCfg.findTracks = TrackFindingAlgorithm::makeTrackFinderFunction(
+      trackingGeometry, magneticField);
+  sequencer.addAlgorithm(
+      std::make_shared<TrackFindingAlgorithm>(trackFindingCfg, logLevel));
+
+  // write track states from CKF
+  RootTrajectoryStatesWriter::Config trackStatesWriter;
+  trackStatesWriter.inputTrajectories = trackFindingCfg.outputTrajectories;
+  // @note The full particles collection is used here to avoid lots of warnings
+  // since the unselected CKF track might have a majority particle not in the
+  // filtered particle collection. Thsi could be avoided when a seperate track
+  // selection algorithm is used.
+  trackStatesWriter.inputParticles = particleReader.outputParticles;
+  trackStatesWriter.inputSimHits = simHitReaderCfg.outputSimHits;
+  trackStatesWriter.inputMeasurements = hitSmearingCfg.outputMeasurements;
+  trackStatesWriter.inputMeasurementParticlesMap =
+      hitSmearingCfg.outputMeasurementParticlesMap;
+  trackStatesWriter.inputMeasurementSimHitsMap =
+      hitSmearingCfg.outputMeasurementSimHitsMap;
+  trackStatesWriter.outputDir = outputDir;
+  trackStatesWriter.outputFilename = "trackstates_ckf.root";
+  trackStatesWriter.outputTreename = "trackstates_ckf";
+  sequencer.addWriter(std::make_shared<RootTrajectoryStatesWriter>(
+      trackStatesWriter, logLevel));
+
+  // write track parameters from CKF
+  RootTrajectoryParametersWriter::Config trackParamsWriter;
+  trackParamsWriter.inputTrajectories = trackFindingCfg.outputTrajectories;
+  // @note The full particles collection is used here to avoid lots of warnings
+  // since the unselected CKF track might have a majority particle not in the
+  // filtered particle collection. Thsi could be avoided when a seperate track
+  // selection algorithm is used.
+  trackParamsWriter.inputParticles = particleReader.outputParticles;
+  trackParamsWriter.inputMeasurementParticlesMap =
+      hitSmearingCfg.outputMeasurementParticlesMap;
+  trackParamsWriter.outputDir = outputDir;
+  trackParamsWriter.outputFilename = "trackparams_ckf.root";
+  trackParamsWriter.outputTreename = "trackparams_ckf";
+  sequencer.addWriter(std::make_shared<RootTrajectoryParametersWriter>(
+      trackParamsWriter, logLevel));
+
+  // Write CKF performance data
+  CKFPerformanceWriter::Config perfWriterCfg;
+  perfWriterCfg.inputTrajectories = trackFindingCfg.outputTrajectories;
+  perfWriterCfg.inputParticles = inputParticles;
+  perfWriterCfg.inputMeasurementParticlesMap =
+      hitSmearingCfg.outputMeasurementParticlesMap;
+  perfWriterCfg.outputDir = outputDir;
+  sequencer.addWriter(
+      std::make_shared<CKFPerformanceWriter>(perfWriterCfg, logLevel));
+
+  return sequencer.run();
+}
