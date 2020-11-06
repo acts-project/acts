fff5ba2b7d10f2f3c1b24115140080593e5743a3
Fabian Klimpel <fabian.klimpel@tum.de>
2020-11-06 14:23:02 +0100

    refactor: HepMC event record reduction and selection (#541)
    The Geant4 event records can become very large. In order to reduce the memory consumption a filter is added that allows the combination of multiple steps with certain processes (e.g. Transportation) into a single vertex. Furthermore this PR adds a list to select or reject events with certain processes.

--- Examples/Algorithms/Geant4HepMC/src/EventAction.hpp
+++ Examples/Algorithms/Geant4HepMC/src/EventAction.hpp
@@ -28,7 +28,7 @@ class EventAction final : public G4UserEventAction {
   static EventAction* instance();
 
   /// Construct the action and ensure singleton usage.
-  EventAction();
+  EventAction(std::vector<std::string> processFilter);
   ~EventAction() final override;
 
   /// Interface method for begin of the event
@@ -51,5 +51,7 @@ class EventAction final : public G4UserEventAction {
   static EventAction* s_instance;
   /// The current HepMC3 event
   HepMC3::GenEvent m_event;
+  /// List of processes that can be combined to a single vertex
+  std::vector<std::string> m_processFilter;
 };
 }  // namespace ActsExamples
