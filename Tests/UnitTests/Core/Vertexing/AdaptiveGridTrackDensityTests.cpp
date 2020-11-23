2b423f2891c91c63bc779863cfecbc1d79c5a45d
Bastian Schlag <bastian.schlag@cern.ch>
2020-11-23 14:40:19 +0100

    add unit test for higest track density sum interval

--- Tests/UnitTests/Core/Vertexing/AdaptiveGridTrackDensityTests.cpp
+++ Tests/UnitTests/Core/Vertexing/AdaptiveGridTrackDensityTests.cpp
@@ -156,7 +156,71 @@ BOOST_AUTO_TEST_CASE(adaptive_gaussian_grid_density_max_z_and_width_test) {
   BOOST_CHECK(resWidth1.ok());
   BOOST_CHECK_EQUAL((*resWidth1).first, z0Trk2);
   BOOST_CHECK((*resWidth1).second > 0);
-  
+}
+
+BOOST_AUTO_TEST_CASE(adaptive_gaussian_grid_density_highest_density_sum_test) {
+
+  const int trkGridSize = 15;
+
+  double binSize = 0.1;  // mm
+
+  // Set up grid density with zMinMax
+  AdaptiveGridTrackDensity<trkGridSize>::Config cfg(binSize);
+  cfg.useHighestSumZPosition = true;
+
+  AdaptiveGridTrackDensity<trkGridSize> grid(cfg);
+
+  // Create some test tracks
+  Covariance covMat;
+  covMat << 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,
+      0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1;
+
+  float z0Trk1 = 0.25;
+  float z0Trk2 = -10.95;
+  BoundVector paramVec1;
+  paramVec1 << 0.01, z0Trk1, 0, 0, 0, 0;
+  BoundVector paramVec2;
+  paramVec2 << 0.009, z0Trk2, 0, 0, 0, 0;
+
+  // Create perigee surface
+  std::shared_ptr<PerigeeSurface> perigeeSurface =
+      Surface::makeShared<PerigeeSurface>(Vector3D(0., 0., 0.));
+
+  BoundTrackParameters params1(perigeeSurface, paramVec1, covMat);
+  BoundTrackParameters params2(perigeeSurface, paramVec2, covMat);
+
+  // Start with empty grids
+  std::vector<float> mainGridDensity;
+  std::vector<int> mainGridZValues;
+
+  // Fill grid with track densities
+  grid.addTrack(params1, mainGridDensity, mainGridZValues);
+
+  auto res1 = grid.getMaxZPosition(mainGridDensity, mainGridZValues);
+  BOOST_CHECK(res1.ok());
+  // Maximum should be at z0Trk1 position
+  BOOST_CHECK_EQUAL(*res1, z0Trk1);
+
+  // Add second track
+  grid.addTrack(params2, mainGridDensity, mainGridZValues);
+  auto res2 = grid.getMaxZPosition(mainGridDensity, mainGridZValues);
+  BOOST_CHECK(res2.ok());
+  // Trk 2 is closer to z-axis and should yield higher density values
+  // New maximum is therefore at z0Trk2
+  BOOST_CHECK_EQUAL(*res2, z0Trk2);
+
+  // Add small density values around the maximum of track 1
+  const float densityToAdd = 5e-4;
+  mainGridDensity[21] += densityToAdd;
+  mainGridDensity[23] += densityToAdd;
+
+  auto res3 = grid.getMaxZPosition(mainGridDensity, mainGridZValues);
+  BOOST_CHECK(res3.ok());
+  // Trk 2 still has the highest peak density value, however, the small
+  // added densities for track 1 around its maximum should now lead to
+  // a prediction at z0Trk1 again
+  BOOST_CHECK_EQUAL(*res3, z0Trk1);
+
 }
 
 }  // namespace Test
