ee1ab3889ec855a10adaaeb12541c395c1f659cd
Moritz Kiehn <msmk@cern.ch>
2020-11-26 17:47:21 +0100

    Tests/Units: add epsilon definitions and rename

--- Tests/UnitTests/Core/Utilities/UnitConversionTests.cpp
+++ Tests/UnitTests/Core/Utilities/UnitConversionTests.cpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2017-2018 CERN for the benefit of the Acts project
+// Copyright (C) 2017-2020 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,97 +11,102 @@
 #include "Acts/Definitions/Units.hpp"
 #include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
 
+#include <limits>
+
 using namespace Acts::UnitLiterals;
 
-BOOST_AUTO_TEST_SUITE(unit_conversions)
+static constexpr auto eps = std::numeric_limits<double>::epsilon();
 
-BOOST_AUTO_TEST_CASE(length_conversions) {
-  CHECK_CLOSE_REL(1_m, 1e-3_km, 1e-15);
-  CHECK_CLOSE_REL(1_m, 1e2_cm, 1e-15);
-  CHECK_CLOSE_REL(1_m, 1e3_mm, 1e-15);
-  CHECK_CLOSE_REL(1_m, 1e6_um, 1e-15);
-  CHECK_CLOSE_REL(1_m, 1e9_nm, 1e-15);
-  CHECK_CLOSE_REL(1_m, 1e12_pm, 1e-15);
-  CHECK_CLOSE_REL(1_m, 1e15_fm, 1e-15);
+BOOST_AUTO_TEST_SUITE(UtilitiesUnits)
+
+BOOST_AUTO_TEST_CASE(Length) {
+  CHECK_CLOSE_REL(1_m, 1e-3_km, eps);
+  CHECK_CLOSE_REL(1_m, 1e2_cm, eps);
+  CHECK_CLOSE_REL(1_m, 1e3_mm, eps);
+  CHECK_CLOSE_REL(1_m, 1e6_um, eps);
+  CHECK_CLOSE_REL(1_m, 1e9_nm, eps);
+  CHECK_CLOSE_REL(1_m, 1e12_pm, eps);
+  CHECK_CLOSE_REL(1_m, 1e15_fm, eps);
 }
 
-BOOST_AUTO_TEST_CASE(area_conversions) {
-  CHECK_CLOSE_REL(1_mm * 1_mm, 1_mm2, 1e-15);
-  CHECK_CLOSE_REL(1_mm * 1_mm, 0.01_cm2, 1e-15);
-  CHECK_CLOSE_REL(1_mm * 1_mm, 0.000001_m2, 1e-15);
-  CHECK_CLOSE_REL(1_cm * 1_cm, 100_mm2, 1e-15);
-  CHECK_CLOSE_REL(1_cm * 1_cm, 1_cm2, 1e-15);
-  CHECK_CLOSE_REL(1_cm * 1_cm, 0.0001_m2, 1e-15);
-  CHECK_CLOSE_REL(1_m * 1_m, 1000000_mm2, 1e-15);
-  CHECK_CLOSE_REL(1_m * 1_m, 10000_cm2, 1e-15);
-  CHECK_CLOSE_REL(1_m * 1_m, 1_m2, 1e-15);
+BOOST_AUTO_TEST_CASE(Area) {
+  CHECK_CLOSE_REL(1_mm * 1_mm, 1_mm2, eps);
+  CHECK_CLOSE_REL(1_mm * 1_mm, 0.01_cm2, eps);
+  CHECK_CLOSE_REL(1_mm * 1_mm, 0.000001_m2, eps);
+  CHECK_CLOSE_REL(1_cm * 1_cm, 100_mm2, eps);
+  CHECK_CLOSE_REL(1_cm * 1_cm, 1_cm2, eps);
+  CHECK_CLOSE_REL(1_cm * 1_cm, 0.0001_m2, eps);
+  CHECK_CLOSE_REL(1_m * 1_m, 1000000_mm2, eps);
+  CHECK_CLOSE_REL(1_m * 1_m, 10000_cm2, eps);
+  CHECK_CLOSE_REL(1_m * 1_m, 1_m2, eps);
 }
 
-BOOST_AUTO_TEST_CASE(volume_conversions) {
-  CHECK_CLOSE_REL(1_mm * 1_mm * 1_mm, 1_mm3, 1e-15);
-  CHECK_CLOSE_REL(1_mm2 * 1_mm, 1_mm3, 1e-15);
-  CHECK_CLOSE_REL(1_cm * 1_cm * 1_cm, 1_cm3, 1e-15);
-  CHECK_CLOSE_REL(1_cm2 * 1_cm, 1_cm3, 1e-15);
-  CHECK_CLOSE_REL(1_m * 1_m * 1_m, 1_m3, 1e-15);
-  CHECK_CLOSE_REL(1_m2 * 1_m, 1_m3, 1e-15);
+BOOST_AUTO_TEST_CASE(Volume) {
+  CHECK_CLOSE_REL(1_mm * 1_mm * 1_mm, 1_mm3, eps);
+  CHECK_CLOSE_REL(1_mm2 * 1_mm, 1_mm3, eps);
+  CHECK_CLOSE_REL(1_cm * 1_cm * 1_cm, 1_cm3, eps);
+  CHECK_CLOSE_REL(1_cm2 * 1_cm, 1_cm3, eps);
+  CHECK_CLOSE_REL(1_m * 1_m * 1_m, 1_m3, eps);
+  CHECK_CLOSE_REL(1_m2 * 1_m, 1_m3, eps);
 }
 
-BOOST_AUTO_TEST_CASE(time_conversions) {
-  CHECK_CLOSE_REL(1_h, 60_min, 1e-15);
-  CHECK_CLOSE_REL(1_h, 3600_s, 1e-15);
-  CHECK_CLOSE_REL(1_min, 60_s, 1e-15);
-  CHECK_CLOSE_REL(1_s, 1e3_ms, 1e-15);
-  CHECK_CLOSE_REL(1_s, 1e6_us, 1e-15);
-  CHECK_CLOSE_REL(1_s, 1e9_ns, 1e-15);
-  CHECK_CLOSE_REL(1_s, 1e12_ps, 1e-15);
-  CHECK_CLOSE_REL(1_s, 1e15_fs, 1e-15);
+BOOST_AUTO_TEST_CASE(Time) {
+  CHECK_CLOSE_REL(1_h, 60_min, eps);
+  CHECK_CLOSE_REL(1_h, 3600_s, eps);
+  CHECK_CLOSE_REL(1_min, 60_s, eps);
+  CHECK_CLOSE_REL(1_s, 1e3_ms, eps);
+  CHECK_CLOSE_REL(1_s, 1e6_us, eps);
+  CHECK_CLOSE_REL(1_s, 1e9_ns, eps);
+  CHECK_CLOSE_REL(1_s, 1e12_ps, eps);
+  CHECK_CLOSE_REL(1_s, 1e15_fs, eps);
 }
 
-BOOST_AUTO_TEST_CASE(angle_conversions) {
-  CHECK_CLOSE_REL(45_degree, M_PI / 4 * 1_rad, 1e-15);
-  CHECK_CLOSE_REL(90_degree, M_PI / 2 * 1_rad, 1e-15);
-  CHECK_CLOSE_REL(180_degree, M_PI * 1_rad, 1e-15);
-  CHECK_CLOSE_REL(360_degree, 2 * M_PI * 1_rad, 1e-15);
-  CHECK_CLOSE_REL(1_mm / 1_m, 1_mrad, 1e-15);
-  CHECK_CLOSE_REL(1_um / 1_mm, 1_mrad, 1e-15);
+BOOST_AUTO_TEST_CASE(Angle) {
+  CHECK_CLOSE_REL(45_degree, M_PI / 4 * 1_rad, eps);
+  CHECK_CLOSE_REL(90_degree, M_PI / 2 * 1_rad, eps);
+  CHECK_CLOSE_REL(180_degree, M_PI * 1_rad, eps);
+  CHECK_CLOSE_REL(360_degree, 2 * M_PI * 1_rad, eps);
+  CHECK_CLOSE_REL(1_mm / 1_m, 1_mrad, eps);
+  CHECK_CLOSE_REL(1_um / 1_mm, 1_mrad, eps);
 }
 
-BOOST_AUTO_TEST_CASE(energy_conversions) {
-  CHECK_CLOSE_REL(1_MeV, 1e6_eV, 1e-15);
-  CHECK_CLOSE_REL(1_MeV, 1e3_keV, 1e-15);
-  CHECK_CLOSE_REL(1_MeV, 1e-3_GeV, 1e-15);
-  CHECK_CLOSE_REL(1_MeV, 1e-6_TeV, 1e-15);
+BOOST_AUTO_TEST_CASE(Energy) {
+  CHECK_CLOSE_REL(1_MeV, 1e6_eV, eps);
+  CHECK_CLOSE_REL(1_MeV, 1e3_keV, eps);
+  CHECK_CLOSE_REL(1_MeV, 1e-3_GeV, eps);
+  CHECK_CLOSE_REL(1_MeV, 1e-6_TeV, eps);
 }
 
-BOOST_AUTO_TEST_CASE(mass_conversions) {
-  CHECK_CLOSE_REL(1_kg, 1000_g, 1e-15);
-  CHECK_CLOSE_REL(0.001_kg, 1_g, 1e-15);
-  CHECK_CLOSE_REL(1_u, 931.49410242_MeV, 1e-15);
+BOOST_AUTO_TEST_CASE(Mass) {
+  // always assume c == 1
+  CHECK_CLOSE_REL(1_kg, 1000_g, eps);
+  CHECK_CLOSE_REL(0.001_kg, 1_g, eps);
+  CHECK_CLOSE_REL(1_u, 931.49410242_MeV, eps);
   CHECK_CLOSE_REL(1_u, 1.66053906660e-24_g, 1e-7);
 }
 
-BOOST_AUTO_TEST_CASE(mass_energy_conversions) {
-  // always assumes c == 1
-  CHECK_CLOSE_REL(1.782662e-36_kg, 1_eV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-33_kg, 1_keV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-30_kg, 1_MeV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-27_kg, 1_GeV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-33_g, 1_eV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-30_g, 1_keV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-27_g, 1_MeV, 1e-15);
-  CHECK_CLOSE_REL(1.782662e-24_g, 1_GeV, 1e-15);
+BOOST_AUTO_TEST_CASE(MassEnergy) {
+  // always assume c == 1
+  CHECK_CLOSE_REL(1.782662e-36_kg, 1_eV, eps);
+  CHECK_CLOSE_REL(1.782662e-33_kg, 1_keV, eps);
+  CHECK_CLOSE_REL(1.782662e-30_kg, 1_MeV, eps);
+  CHECK_CLOSE_REL(1.782662e-27_kg, 1_GeV, eps);
+  CHECK_CLOSE_REL(1.782662e-33_g, 1_eV, eps);
+  CHECK_CLOSE_REL(1.782662e-30_g, 1_keV, eps);
+  CHECK_CLOSE_REL(1.782662e-27_g, 1_MeV, eps);
+  CHECK_CLOSE_REL(1.782662e-24_g, 1_GeV, eps);
 }
 
-BOOST_AUTO_TEST_CASE(charge_conversions) {
-  CHECK_CLOSE_REL(1_C, 1.602176634e19_e, 1e-15);
+BOOST_AUTO_TEST_CASE(Charge) {
+  CHECK_CLOSE_REL(1_C, 1.602176634e19_e, eps);
 }
 
-BOOST_AUTO_TEST_CASE(magnetic_field_conversions) {
-  CHECK_CLOSE_REL(10_kGauss, 1_T, 1e-15);
-  CHECK_CLOSE_REL(1_kGauss, 1000_Gauss, 1e-15);
+BOOST_AUTO_TEST_CASE(MagneticField) {
+  CHECK_CLOSE_REL(10_kGauss, 1_T, eps);
+  CHECK_CLOSE_REL(1_kGauss, 1000_Gauss, eps);
 }
 
-BOOST_AUTO_TEST_CASE(momentum_to_radius_conversion) {
+BOOST_AUTO_TEST_CASE(MomentumRadius) {
   // note: no conversion factors necessary
   CHECK_CLOSE_REL(1_GeV / (1_e * 1_T), 3.3336_m, 1e-3);
   CHECK_CLOSE_REL(1_GeV / (1_e * 2_T), 166.8_cm, 1e-3);
