bb8d2de22a40d8a5cb5e50fba2511626670dab42
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2020-12-14 15:28:43 +0100

    address PR comments, update to new EDM

--- Fatras/include/ActsFatras/Digitization/ChannelMerger.hpp
+++ Fatras/include/ActsFatras/Digitization/ChannelMerger.hpp
@@ -8,66 +8,58 @@
 
 #pragma once
 
+#include "Acts/Surfaces/Surface.hpp"
+#include "Acts/Surfaces/SurfaceError.hpp"
+#include "Acts/Utilities/Helpers.hpp"
 #include "ActsFatras/Digitization/DigitizationData.hpp"
-#include <Acts/EventData/ParameterSet.hpp>
-#include <Acts/Surfaces/Surface.hpp>
-#include <Acts/Surfaces/SurfaceError.hpp>
-#include <Acts/Utilities/Helpers.hpp>
-#include <Acts/Utilities/ParameterDefinitions.hpp>
 
 #include <array>
 #include <utility>
 
 namespace ActsFatras {
 
-/// Channel combination to a resulting parameter set.
+/// Generic implementation of a channel merger, currently only additive
+/// channel merging
 ///
-struct ChannelMerger {
-  /// Generic implementation of a channel merger, currently only additive
-  /// channel merging
-  ///
-  /// @tparam kParameters parameter pack describing the parameters to smear
-  ///
-  /// @param channels The channels from one cluster
-  ///
-  /// @return A cluster containing the parameter set and cluster size
-  template <typename signal_t, Acts::BoundIndices... kParameters>
-  const std::vector<Channel<signal_t, kParameters...>> merge(
-      const std::vector<Channel<signal_t, kParameters...>>& channels) const {
-    using StorageSequence = std::make_index_sequence<sizeof...(kParameters)>;
-    using Channel = Channel<signal_t, kParameters...>;
-    using ChannelKey = std::array<unsigned int, sizeof...(kParameters)>;
+/// @tparam signal_t The type of signal, needs operator+= to be defined
+/// @tparam kSize the dimensonality of the object (cluster)
+///
+/// @param channels The channels from one cluster
+///
+/// @return A cluster containing the parameter set and cluster size
+template <typename signal_t, size_t kSize>
+const std::vector<Channel<signal_t, kSize>> mergeChannels(
+    const std::vector<Channel<signal_t, kSize>>& channels) {
+  using Channel = Channel<signal_t, kSize>;
+  using ChannelKey = std::array<unsigned int, kSize>;
 
-    // Fill a channel map
-    std::map<ChannelKey, Channel> channelMap;
-    for (const auto& ch : channels) {
-      ChannelKey key;
-      extractKeys(ch, key, StorageSequence{});
-      auto chItr = channelMap.find(key);
-      if (chItr != channelMap.end()) {
-        chItr->second.value += ch.value;
-        chItr->second.links.insert(ch.links.begin(), ch.links.end());
-      } else {
-        channelMap.insert(std::pair<ChannelKey, Channel>(key, ch));
-      }
+  // Fill a channel map - use the channel identification
+  auto extractChannelKey = [&](const Channel& ch) -> ChannelKey {
+    ChannelKey cKey;
+    for (unsigned int ik = 0; ik < kSize; ++ik) {
+      cKey[ik] = ch.cellId[ik].first;
     }
-    // Unroll the channels after merging
-    std::vector<Channel> mergedChannels;
-    mergedChannels.reserve(channelMap.size());
-    for (auto& [key, value] : channelMap) {
-      mergedChannels.push_back(value);
-    }
-    return mergedChannels;
-  }
+    return cKey;
+  };
 
-  /// Helper method to extract the map keys from the channels
-  ///
-  /// @tparam kStorage is the access sequence to the channel
-  template <typename channel_t, typename key_t, std::size_t... kStorage>
-  static void extractKeys(const channel_t& channel, key_t& key,
-                          std::index_sequence<kStorage...>) {
-    ((key[kStorage] = channel.cellId[kStorage].first), ...);
+  std::map<ChannelKey, Channel> channelMap;
+  for (const auto& ch : channels) {
+    ChannelKey key = extractChannelKey(ch);
+    auto chItr = channelMap.find(key);
+    if (chItr != channelMap.end()) {
+      chItr->second.value += ch.value;
+      chItr->second.links.insert(ch.links.begin(), ch.links.end());
+    } else {
+      channelMap.insert(std::pair<ChannelKey, Channel>(key, ch));
+    }
   }
-};
+  // Unroll the channels after merging
+  std::vector<Channel> mergedChannels;
+  mergedChannels.reserve(channelMap.size());
+  for (auto& [key, value] : channelMap) {
+    mergedChannels.push_back(value);
+  }
+  return mergedChannels;
+}
 
 }  // namespace ActsFatras
--- Tests/UnitTests/Fatras/Digitization/ChannelMergerTests.cpp
+++ Tests/UnitTests/Fatras/Digitization/ChannelMergerTests.cpp
@@ -8,27 +8,22 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "Acts/Definitions/Algebra.hpp"
+#include "Acts/Definitions/TrackParametrization.hpp"
+#include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
+#include "Acts/Utilities/Helpers.hpp"
 #include "ActsFatras/Digitization/ChannelMerger.hpp"
 #include "ActsFatras/Digitization/DigitizationError.hpp"
-
-#include <Acts/Tests/CommonHelpers/FloatComparisons.hpp>
-#include <Acts/Utilities/Definitions.hpp>
-#include <Acts/Utilities/Helpers.hpp>
-#include <Acts/Utilities/ParameterDefinitions.hpp>
-
-namespace ActsFatras {
 
 BOOST_AUTO_TEST_SUITE(Digitization)
 
 BOOST_AUTO_TEST_CASE(ChannelMerger1D) {
-  ChannelMerger cm;
-
   // Some cells
-  Cell cell0(5, 10.5);
-  Cell cell1(6, 11.5);
-  Cell cell2(7, 12.5);
+  ActsFatras::Cell cell0(5, 10.5);
+  ActsFatras::Cell cell1(6, 11.5);
+  ActsFatras::Cell cell2(7, 12.5);
 
-  using Channel1D = Channel<double, Acts::eBoundLoc0>;
+  using Channel1D = ActsFatras::Channel<Acts::ActsScalar, 1>;
 
   // Digital clustering test
   std::vector<Channel1D> channels = {{{cell0}, 1., {5}},
@@ -37,7 +32,7 @@ BOOST_AUTO_TEST_CASE(ChannelMerger1D) {
                                      {{cell0}, 0.5, {6}}};
 
   BOOST_CHECK_EQUAL(channels.size(), 4u);
-  auto mergedChannels = cm.merge<double, Acts::eBoundLoc0>(channels);
+  auto mergedChannels = ActsFatras::mergeChannels(channels);
   BOOST_CHECK_EQUAL(mergedChannels.size(), 3u);
 
   std::unordered_set<unsigned int> mergedLinks = {5, 6};
@@ -54,19 +49,17 @@ BOOST_AUTO_TEST_CASE(ChannelMerger1D) {
 }
 
 BOOST_AUTO_TEST_CASE(ChannelMerger2D) {
-  ChannelMerger cm;
-
   // Some cells in 0 direction
-  Cell cell00(5, 10.5);
-  Cell cell01(6, 11.5);
-  Cell cell02(7, 12.5);
+  ActsFatras::Cell cell00(5, 10.5);
+  ActsFatras::Cell cell01(6, 11.5);
+  ActsFatras::Cell cell02(7, 12.5);
 
   // Some cells in 1 direction
-  Cell cell10(10, 0.5);
-  Cell cell11(10, 0.5);
-  Cell cell12(11, 1.5);
+  ActsFatras::Cell cell10(10, 0.5);
+  ActsFatras::Cell cell11(10, 0.5);
+  ActsFatras::Cell cell12(11, 1.5);
 
-  using Channel2D = Channel<double, Acts::eBoundLoc0, Acts::eBoundLoc1>;
+  using Channel2D = ActsFatras::Channel<Acts::ActsScalar, 2>;
 
   std::vector<Channel2D> channels = {{{cell00, cell10}, 1., {5}},
                                      {{cell01, cell11}, 1., {5}},
@@ -74,8 +67,7 @@ BOOST_AUTO_TEST_CASE(ChannelMerger2D) {
                                      {{cell01, cell11}, 0.5, {6}}};
 
   BOOST_CHECK_EQUAL(channels.size(), 4u);
-  auto mergedChannels =
-      cm.merge<double, Acts::eBoundLoc0, Acts::eBoundLoc1>(channels);
+  auto mergedChannels = ActsFatras::mergeChannels(channels);
   BOOST_CHECK_EQUAL(mergedChannels.size(), 3u);
 
   std::unordered_set<unsigned int> mergedLinks = {5, 6};
@@ -92,5 +84,3 @@ BOOST_AUTO_TEST_CASE(ChannelMerger2D) {
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
-}  // namespace ActsFatras
