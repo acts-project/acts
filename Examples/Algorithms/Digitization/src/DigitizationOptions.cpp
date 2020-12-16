49325c3f3804f5026c303a3e949260fcc2c3b3d6
Moritz Kiehn <msmk@cern.ch>
2020-12-16 14:38:25 +0100

    Examples/Digitization: rework options

--- Examples/Algorithms/Digitization/src/DigitizationOptions.cpp
+++ Examples/Algorithms/Digitization/src/DigitizationOptions.cpp
@@ -19,37 +19,45 @@
 
 #include <boost/program_options.hpp>
 
-void ActsExamples::Options::addDigitizationOptions(
-    ActsExamples::Options::Description& desc) {
+void ActsExamples::Options::addDigitizationOptions(Description& desc) {
   using boost::program_options::bool_switch;
   using boost::program_options::value;
-  using namespace std;
 
+  // each volume configuration is one logical block
+  //
+  //   --digi-smear-volume-id=8
+  //   --digi-smear-indices=0:1:5 # loc0, loc1, and time
+  //   --digi-smear-types=0:0:3   # loc{0,1} uses gaussian, time uses uniform
+  //   # parameter 0: loc0 gaussian width
+  //   # parameter 1: loc1 gaussian width
+  //   # parameter 2-4: time pitch,min,max
+  //   --digi-smear-parameters=10:20:2.5:-25:25
+  //
+  // which can be repeated as often as needed
+  //
+  //   --digi-smear-volume-id=11
+  //   --digi-smear-indices=1       # loc1
+  //   --digi-smear-types=0:0:3     # loc1 uses truncated gaussian
+  //   --digi-smear-parameters=12.5 # loc1 gaussian width
+  //
   auto opt = desc.add_options();
-  opt("digi-config-file", value<string>(),
+  opt("digi-config-file", value<std::string>(),
       "Configuration (.json) file for digitization description, overwrites "
       "options input on command line.");
   opt("digi-geometric-3d", bool_switch(),
       "Geometric: Switching geometric digitisation in 3D on");
-  opt("digi-smearing", bool_switch(), "Smearing: Switching hit smearing on");
-  opt("digi-smear-volume-id",
-      value<read_series>()->multitoken()->default_value({}),
+  opt("digi-smear", bool_switch(), "Smearing: Switching hit smearing on");
+  opt("digi-smear-volume", value<std::vector<int>>(),
       "Smearing Input: sensitive volume identifiers.");
-  opt("digi-smear-dimensions",
-      value<read_series>()->multitoken()->default_value({}),
-      "Smearing Input: smear parameters for this volume.");
-  opt("digi-smear-parIndices",
-      value<read_series>()->multitoken()->default_value({}),
+  opt("digi-smear-indices", value<std::vector<VariableIntegers>>(),
       "Smearing Input: smear parameter indices for this volume.");
-  opt("digi-smear-types", value<read_series>()->multitoken()->default_value({}),
+  opt("digi-smear-types", value<std::vector<VariableIntegers>>(),
       "Smearing Input: smear function types as 0 (gauss), 1 (truncated gauss), "
       "2 (clipped gauss), 3 (uniform), 4 (digital).");
-  opt("digi-smear-parameters",
-      value<read_range>()->multitoken()->default_value({}),
+  opt("digi-smear-parameters", value<std::vector<VariableDoubles>>(),
       "Smearing Input: smear parameters depending on the smearing type, 1 "
-      "parameter for simple gauss, 4 for all others (1 parameter, 2 absolute "
-      "range "
-      "values, 1 range indicator: both neg < 0, neg/pos = 0, both pos > 0).");
+      "parameter for simple gauss, 3 for all others (1 parameter, 2 range "
+      "values.");
 }
 
 namespace {
@@ -64,48 +72,25 @@ enum SmearingTypes : int {
 
 constexpr size_t numConfigParametersForType(int type) {
   // Gaussian smearing requires only a width/standard deviation parameter
-  // Everything else requires a width and a range
-  return (static_cast<SmearingTypes>(type) == eGauss) ? 1u : 4u;
+  // Everything else requires a width/pitch and a range
+  return (static_cast<SmearingTypes>(type) == eGauss) ? 1u : 3u;
 }
 
 ActsFatras::SingleParameterSmearFunction<ActsExamples::RandomEngine>
 makeSmearFunctionForType(SmearingTypes smearingType, const double* parameters) {
   using namespace ActsExamples::Digitization;
 
-  // this is an artifact from the command line parsing. all numbers must be
-  // positive, so we need to determine the signs of the range limits with an
-  // additional parameter. see command line flag documentation above for
-  // details.
-  double signLow = 1;
-  double signHigh = 1;
-  if (smearingType != eGauss) {
-    if (parameters[3u] < 0) {
-      signLow = -1;
-      signHigh = -1;
-    } else if (0 < parameters[3u]) {
-      signLow = 1;
-      signHigh = 1;
-    } else {
-      signLow = -1;
-      signHigh = 1;
-    }
-  }
-
   switch (smearingType) {
     case eGauss:
       return Gauss(parameters[0]);
     case eGaussTruncated:
-      return GaussTrunc(parameters[0],
-                        {signLow * parameters[1u], signHigh * parameters[2u]});
+      return GaussTrunc(parameters[0], {parameters[1u], parameters[2u]});
     case eGaussClipped:
-      return GaussClipped(
-          parameters[0], {signLow * parameters[1u], signHigh * parameters[2u]});
+      return GaussClipped(parameters[0], {parameters[1u], parameters[2u]});
     case eUniform:
-      return Uniform(parameters[0],
-                     {signLow * parameters[1u], signHigh * parameters[2u]});
+      return Uniform(parameters[0], {parameters[1u], parameters[2u]});
     case eDigital:
-      return Digital(parameters[0],
-                     {signLow * parameters[1u], signHigh * parameters[2u]});
+      return Digital(parameters[0], {parameters[1u], parameters[2u]});
   }
   return nullptr;
 }
@@ -113,8 +98,7 @@ makeSmearFunctionForType(SmearingTypes smearingType, const double* parameters) {
 }  // namespace
 
 ActsExamples::SmearingAlgorithm::Config
-ActsExamples::Options::readSmearingConfig(
-    const boost::program_options::variables_map& variables) {
+ActsExamples::Options::readSmearingConfig(const Variables& variables) {
   ACTS_LOCAL_LOGGER(
       Acts::getDefaultLogger("SmearingOptions", Acts::Logging::INFO));
 
@@ -130,42 +114,31 @@ ActsExamples::Options::readSmearingConfig(
   // smearers configuration. this will be caught later on during the algorithm
   // construction.
 
-  auto volumes = variables["digi-smear-volume-id"].as<read_series>();
+  auto volumes = variables["digi-smear-volume"].as<std::vector<int>>();
   if (volumes.empty()) {
     // no configured volumes are not considered an error at this stage
     return smearCfg;
   }
-  auto dimensions = variables["digi-smear-dimensions"].as<read_series>();
-  if (dimensions.size() != volumes.size()) {
-    ACTS_ERROR("Inconsistent digi-smear-dimensions number of entries. Expected "
-               << volumes.size() << ", but received " << dimensions.size());
+
+  auto indices =
+      variables["digi-smear-indices"].as<std::vector<VariableIntegers>>();
+  auto types =
+      variables["digi-smear-types"].as<std::vector<VariableIntegers>>();
+  auto parameters =
+      variables["digi-smear-parameters"].as<std::vector<VariableDoubles>>();
+  if (indices.size() != volumes.size()) {
+    ACTS_ERROR("Inconsistent digi-smear-indices options. Expected "
+               << volumes.size() << ", but received " << indices.size());
     return smearCfg;
   }
-  // count the expected number of parameter indices
-  auto expectedNumIndices =
-      std::accumulate(dimensions.begin(), dimensions.end(), size_t(0u));
-  auto indices = variables["digi-smear-parIndices"].as<read_series>();
-  if (indices.size() != expectedNumIndices) {
-    ACTS_ERROR("Inconsistent digi-smear-parIndices number of entries. Expected "
-               << expectedNumIndices << ", but received " << indices.size());
+  if (types.size() != volumes.size()) {
+    ACTS_ERROR("Inconsistent digi-smear-types options. Expected "
+               << volumes.size() << ", but received " << types.size());
     return smearCfg;
   }
-  auto types = variables["digi-smear-types"].as<read_series>();
-  if (types.size() != indices.size()) {
-    ACTS_ERROR("Inconsistent digi-smear-types number of entries. Expected "
-               << indices.size() << ", but received " << types.size());
-    return smearCfg;
-  }
-  // count the expected number of smearing configuration parameters
-  size_t expectedNumParameters = 0;
-  for (auto smearingType : types) {
-    expectedNumParameters += numConfigParametersForType(smearingType);
-  }
-  auto parameters = variables["digi-smear-parameters"].as<read_range>();
-  if (parameters.size() != expectedNumParameters) {
-    ACTS_ERROR("Inconsistent digi-smear-parameters number of entries. Expected "
-               << expectedNumParameters << ", but received "
-               << parameters.size());
+  if (parameters.size() != volumes.size()) {
+    ACTS_ERROR("Inconsistent digi-smear-parameters options. Expected "
+               << volumes.size() << ", but received " << parameters.size());
     return smearCfg;
   }
 
@@ -173,26 +146,40 @@ ActsExamples::Options::readSmearingConfig(
   std::vector<
       std::pair<Acts::GeometryIdentifier, SmearingAlgorithm::SmearerConfig>>
       smearersInput;
-  // position of the current volume in the input vectors
-  size_t ivol = 0;
-  // position of the current dimension index/type in the input vectors
-  size_t iidx = 0;
-  // position of the current smear function parameter in the input vector
-  size_t ipar = 0;
-  for (; ivol < volumes.size(); ++ivol) {
+  for (size_t ivol = 0; ivol < volumes.size(); ++ivol) {
     Acts::GeometryIdentifier geoId =
         Acts::GeometryIdentifier(0).setVolume(volumes[ivol]);
-    // number of smeared track parameters
-    size_t ndim = dimensions[ivol];
+    const auto& volIndices = indices[ivol].values;
+    const auto& volTypes = types[ivol].values;
+    const auto& volParameters = parameters[ivol].values;
+
+    if (volTypes.size() != volIndices.size()) {
+      ACTS_ERROR("Inconsistent number of digi-smear-types values for volume "
+                 << volumes[ivol] << ". Expected " << indices.size()
+                 << ", but received " << types.size());
+      return smearCfg;
+    }
+    // count the expected number of smearing configuration parameters
+    size_t expectedNumParameters = 0;
+    for (auto smearingType : volTypes) {
+      expectedNumParameters += numConfigParametersForType(smearingType);
+    }
+    if (volParameters.size() != expectedNumParameters) {
+      ACTS_ERROR(
+          "Inconsistent number of digi-smear-parameters values for volume "
+          << volumes[ivol] << ". Expected " << expectedNumParameters
+          << ", but received " << types.size());
+      return smearCfg;
+    }
 
     // create the smearing configuration for this geometry identifier
     SmearingAlgorithm::SmearerConfig geoCfg;
-    geoCfg.reserve(ndim);
+    geoCfg.reserve(volIndices.size());
 
-    for (auto nidx = iidx + ndim; iidx < nidx; ++iidx) {
-      const auto paramIndex = static_cast<Acts::BoundIndices>(indices[iidx]);
-      const auto smearingType = static_cast<SmearingTypes>(types[iidx]);
-      const double* smearingParameters = &parameters[ipar];
+    for (size_t iidx = 0, ipar = 0; iidx < volIndices.size(); ++iidx) {
+      const auto paramIndex = static_cast<Acts::BoundIndices>(volIndices[iidx]);
+      const auto smearingType = static_cast<SmearingTypes>(volTypes[iidx]);
+      const double* smearingParameters = &volParameters[ipar];
       ipar += numConfigParametersForType(smearingType);
 
       SmearingAlgorithm::ParameterSmearerConfig parCfg;
@@ -201,7 +188,8 @@ ActsExamples::Options::readSmearingConfig(
           makeSmearFunctionForType(smearingType, smearingParameters);
       if (not parCfg.smearFunction) {
         ACTS_ERROR("Invalid smearing type for entry "
-                   << iidx << ". Type " << types[iidx] << " is not valid");
+                   << iidx << " for volume " << volumes[ivol] << ". Type "
+                   << volTypes[iidx] << " is not valid");
         return smearCfg;
       }
       geoCfg.emplace_back(std::move(parCfg));
