6d78ddbd0d5b981d11b138601d9923c11e6c679b
Moritz Kiehn <msmk@cern.ch>
2020-12-17 18:34:40 +0100

    Examples/EventData: simplify space point constructor

--- Examples/Framework/include/ActsExamples/EventData/SimSpacePoint.hpp
+++ Examples/Framework/include/ActsExamples/EventData/SimSpacePoint.hpp
@@ -20,33 +20,31 @@ namespace ActsExamples {
 /// Space point representation of a measurement suitable for track seeding.
 class SimSpacePoint {
  public:
-  template <typename position_t, typename covariance_t>
-  SimSpacePoint(const Eigen::MatrixBase<position_t>& pos,
-                const Eigen::MatrixBase<covariance_t>& cov,
-                Index measurementIndex)
+  /// Construct the space point from global position and selected variances.
+  ///
+  /// @tparam position_t Input position type
+  /// @param pos Global position
+  /// @param varRho Measurement variance of the global transverse distance
+  /// @param varZ Measurement variance of the global longitudinal position
+  /// @param measurementIndex Index of the underlying measurement
+  template <typename position_t>
+  SimSpacePoint(const Eigen::MatrixBase<position_t>& pos, float varRho,
+                float varZ, Index measurementIndex)
       : m_x(pos[Acts::ePos0]),
         m_y(pos[Acts::ePos1]),
         m_z(pos[Acts::ePos2]),
-        m_r(std::hypot(m_x, m_y)),
-        //      r = sqrt(x² + y²)
-        //  dr/dx = (1 / sqrt(x² + y²)) * 2 * x = 2 * x / r
-        // var(r) = (dr/dx)² * var(x) + (dr/dy)² * var(y)
-        //        = (4 / r²) * (x² * var(x) + y*2 * var(y))
-        m_varianceR(
-            (4 / (m_r * m_r)) *
-            (m_x * m_x * static_cast<float>(cov(Acts::ePos0, Acts::ePos0)) +
-             m_y * m_y * static_cast<float>(cov(Acts::ePos1, Acts::ePos1)))),
-        m_varianceZ(cov(Acts::ePos2, Acts::ePos2)),
+        m_rho(std::hypot(m_x, m_y)),
+        m_varianceRho(varRho),
+        m_varianceZ(varZ),
         m_measurementIndex(measurementIndex) {
     EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE(position_t, 3);
-    EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE(covariance_t, 3, 3);
   }
 
   constexpr float x() const { return m_x; }
   constexpr float y() const { return m_y; }
   constexpr float z() const { return m_z; }
-  constexpr float r() const { return m_r; }
-  constexpr float varianceR() const { return m_varianceR; }
+  constexpr float r() const { return m_rho; }
+  constexpr float varianceR() const { return m_varianceRho; }
   constexpr float varianceZ() const { return m_varianceZ; }
 
   constexpr Index measurementIndex() const { return m_measurementIndex; }
@@ -56,9 +54,9 @@ class SimSpacePoint {
   float m_x;
   float m_y;
   float m_z;
-  float m_r;
-  // VarianceR/Z of the global position
-  float m_varianceR;
+  float m_rho;
+  // Variance in rho/z of the global coordinates
+  float m_varianceRho;
   float m_varianceZ;
   // Index of the corresponding measurement
   Index m_measurementIndex;
