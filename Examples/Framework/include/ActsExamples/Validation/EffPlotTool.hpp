274ca89fb50ea18b3d062b8e3ce3bc3ba0c1b258
xai <xiaocong.ai@cern.ch>
2021-01-11 01:32:27 +0100

    Change the eff plots title

--- Examples/Framework/include/ActsExamples/Validation/EffPlotTool.hpp
+++ Examples/Framework/include/ActsExamples/Validation/EffPlotTool.hpp
@@ -26,9 +26,9 @@ class EffPlotTool {
   /// @brief The nested configuration struct
   struct Config {
     std::map<std::string, PlotHelpers::Binning> varBinning = {
-        {"Eta", PlotHelpers::Binning("#eta", 40, -4, 4)},
-        {"Phi", PlotHelpers::Binning("#phi", 100, -3.15, 3.15)},
-        {"Pt", PlotHelpers::Binning("pT [GeV/c]", 40, 0, 100)}};
+        {"Eta", PlotHelpers::Binning("Truth #eta", 40, -4, 4)},
+        {"Phi", PlotHelpers::Binning("Truth #phi", 100, -3.15, 3.15)},
+        {"Pt", PlotHelpers::Binning("Truth pT [GeV/c]", 40, 0, 100)}};
   };
 
   /// @brief Nested Cache struct
