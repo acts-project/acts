490943124b338eb4c8e4189b35288c5c01c08db3
xai <xiaocong.ai@cern.ch>
2021-01-12 14:37:10 +0100

    Change the pT binning

--- Examples/Framework/include/ActsExamples/Validation/ResPlotTool.hpp
+++ Examples/Framework/include/ActsExamples/Validation/ResPlotTool.hpp
@@ -34,7 +34,7 @@ class ResPlotTool {
     /// Binning info for variables
     std::map<std::string, PlotHelpers::Binning> varBinning = {
         {"Eta", PlotHelpers::Binning("#eta", 40, -4, 4)},
-        {"Pt", PlotHelpers::Binning("pT [GeV/c]", 40, 0, 100)},
+        {"Pt", PlotHelpers::Binning("pT [GeV/c]", 100, 0, 100)},
         {"Pull", PlotHelpers::Binning("pull", 100, -5, 5)},
         {"Residual_d0", PlotHelpers::Binning("r_{d0} [mm]", 100, -0.5, 0.5)},
         {"Residual_z0", PlotHelpers::Binning("r_{z0} [mm]", 100, -0.5, 0.5)},
