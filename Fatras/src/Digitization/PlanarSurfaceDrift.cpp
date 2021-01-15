4133f09d4f7c831e6b4025f683d27769ac195c62
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2021-01-15 11:24:55 +0100

    feat: introduce surface drift  (#653)
    This PR splits out the addition of the surface merger to the Fatras/Digitzation module.
    
    The PlanarSurfaceDrift struct allows to project an intersection segment to a readout surface,
    it is tested in a new UnitTest.

---
+++ Fatras/src/Digitization/PlanarSurfaceDrift.cpp
@@ -0,0 +1,46 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsFatras/Digitization/PlanarSurfaceDrift.hpp"
+
+ActsFatras::PlanarSurfaceDrift::Segment2D
+ActsFatras::PlanarSurfaceDrift::toReadout(const Acts::GeometryContext& gctx,
+                                          const Acts::Surface& surface,
+                                          double thickness,
+                                          const Acts::Vector3& pos,
+                                          const Acts::Vector3& dir,
+                                          const Acts::Vector3& driftDir) const {
+  // Transform the hit & direction into the local surface frame
+  const auto& invTransform = surface.transform(gctx).inverse();
+  Acts::Vector2 pos2Local = (invTransform * pos).segment<2>(0);
+  Acts::Vector3 seg3Local = invTransform.linear() * dir;
+  // Scale unit direction to the actual segment in the (depletion/drift) zone
+  seg3Local *= thickness / std::cos(Acts::VectorHelpers::theta(seg3Local));
+  // Calulate local entry/exit before drift
+  Acts::Vector2 entry = pos2Local - 0.5 * seg3Local.segment<2>(0);
+  Acts::Vector2 exit = pos2Local + 0.5 * seg3Local.segment<2>(0);
+  // Actually apply a drift
+  // - dirftDir is assumed in local coordinates
+  if (not driftDir.segment<2>(0).isApprox(Acts::Vector2(0., 0.))) {
+    // Apply the scaled drift
+    auto applyDrift = [&](Acts::Vector2& local) {
+      auto scaledDriftDir =
+          driftDir * thickness / std::cos(Acts::VectorHelpers::theta(driftDir));
+      local += scaledDriftDir.segment<2>(0);
+    };
+
+    if (driftDir.z() > 0.) {
+      applyDrift(entry);
+    }
+    if (driftDir.z() < 0.) {
+      applyDrift(exit);
+    }
+  }
+
+  return {entry, exit};
+}
