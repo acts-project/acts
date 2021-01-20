f3f5479d2a3cfdf9fbf629160d211c687532f628
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2021-01-20 17:15:56 +0100

    feat: streamline json plugin (#654)
    This PR introduces the first modules of the updated Json converters that use the nlohmann::json::to_json/from_json interface.

---
+++ Plugins/Json/include/Acts/Plugins/Json/UtilitiesJsonConverter.hpp
@@ -0,0 +1,29 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/Utilities/BinUtility.hpp"
+#include "Acts/Utilities/BinningData.hpp"
+
+#include <nlohmann/json.hpp>
+
+// Custom Json encoder/decoders. Naming is mandated by nlohman::json and thus
+// can not match our naming guidelines.
+
+namespace Acts {
+
+void to_json(nlohmann::json& j, const BinningData& bd);
+
+void from_json(const nlohmann::json& j, BinningData& bd);
+
+void to_json(nlohmann::json& j, const BinUtility& bu);
+
+void from_json(const nlohmann::json& j, BinUtility& bu);
+
+}  // namespace Acts
