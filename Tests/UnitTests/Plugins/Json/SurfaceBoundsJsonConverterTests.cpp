bed4f8b71dc213c082243181302766a6bfd2753a
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2021-01-22 17:28:10 +0100

    feat: introduce Json converter for SurfaceBounds (#660)
    This PR includes the next Json converter, this time for SurfaceBounds.
    
    All SurfaceBounds have a values() streaming method (providing a std::vector) and a constructor by providing a std::array. Hence, only one converter is needed, however, the type has to be given explicitely, no automated conversion is possible, as the Surface objects never swallow SurfaceBounds directly but a class extension of the appropriate type.
    
    UnitTest for one is attached, no need to call the same functions on all inherited objects.

---
+++ Tests/UnitTests/Plugins/Json/SurfaceBoundsJsonConverterTests.cpp
@@ -0,0 +1,51 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Plugins/Json/SurfaceBoundsJsonConverter.hpp"
+#include "Acts/Surfaces/CylinderBounds.hpp"
+#include "Acts/Surfaces/RadialBounds.hpp"
+#include "Acts/Surfaces/RectangleBounds.hpp"
+
+#include <fstream>
+#include <iostream>
+
+#include <nlohmann/json.hpp>
+
+using namespace Acts;
+
+BOOST_AUTO_TEST_SUITE(SurfaceBoundsJsonConverter)
+
+BOOST_AUTO_TEST_CASE(SurfaceBoundsRoundTripTests) {
+  std::ofstream out;
+
+  // As all SurfaceBounds have the same streaming API only a one is
+  // tested here, all others are tests are identical
+
+  auto rectangeRef = std::make_shared<const RectangleBounds>(4., 6.);
+  // Test a rectangle
+  nlohmann::json rectangleOut;
+  to_json(rectangleOut, *rectangeRef);
+  out.open("RectangleBounds.json");
+  out << rectangleOut.dump(2);
+  out.close();
+
+  auto in = std::ifstream("RectangleBounds.json",
+                          std::ifstream::in | std::ifstream::binary);
+  BOOST_CHECK(in.good());
+  nlohmann::json rectangleIn;
+  in >> rectangleIn;
+  in.close();
+
+  auto rectangleTest = surfaceBoundsFromJson<RectangleBounds>(rectangleIn);
+
+  BOOST_CHECK(rectangeRef->values() == rectangleTest->values());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
