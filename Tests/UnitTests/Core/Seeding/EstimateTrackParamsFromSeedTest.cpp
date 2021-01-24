d945321d86f800104aa443ee7e56f5d19216ab1e
xai <xiaocong.ai@cern.ch>
2021-01-24 02:51:27 +0100

    Fix the track parameters estimation test

--- Tests/UnitTests/Core/Seeding/EstimateTrackParamsFromSeedTest.cpp
+++ Tests/UnitTests/Core/Seeding/EstimateTrackParamsFromSeedTest.cpp
@@ -88,19 +88,19 @@ BOOST_AUTO_TEST_CASE(trackparameters_estimation_test) {
 
   ConstantFieldPropagator propagator(std::move(stepper), std::move(navigator));
 
+  std::array<double, 2> pArray = {0.5_GeV, 1.0_GeV};
   std::array<double, 3> phiArray = {20._degree, 0._degree - 20._degree};
   std::array<double, 3> thetaArray = {80._degree, 90.0_degree, 100._degree};
-  std::array<double, 2> pArray = {0.5_GeV, 1.0_GeV};
   std::array<double, 2> qArray = {1, -1};
 
-  for (const auto& phi : phiArray) {
-    for (const auto& theta : thetaArray) {
-      for (const auto& q : qArray) {
-        for (const auto& p : pArray) {
-          BOOST_TEST_INFO("Test track with phi = " << phi << ", theta = "
-                                                   << theta << ", q = " << q
-                                                   << ", p = " << p);
-          auto start = makeParameters(phi, theta, q, p);
+  for (const auto& p : pArray) {
+    for (const auto& phi : phiArray) {
+      for (const auto& theta : thetaArray) {
+        for (const auto& q : qArray) {
+          BOOST_TEST_INFO("Test track with p = " << p << ", phi = " << phi
+                                                 << ", theta = " << theta
+                                                 << ", q = " << q);
+          auto start = makeParameters(phi, theta, p, q);
           auto measurements = createMeasurements(propagator, geoCtx, magCtx,
                                                  start, resolutions, rng);
 
@@ -135,11 +135,15 @@ BOOST_AUTO_TEST_CASE(trackparameters_estimation_test) {
 
           // Check if there is at least 3 space points
           if (spacePoints.size() < 3) {
+            BOOST_TEST_WARN("Number of space points less than 3.");
             continue;
           }
 
           // The truth track parameters at the bottom space point
           const auto& expParams = measurements.truthParameters[0];
+          BOOST_TEST_INFO(
+              "The truth track parameters at the bottom space point: \n"
+              << expParams.transpose());
           // The curvature of track projection on the transverse plane in unit
           // of 1/mm
           double rho = expParams[eBoundQOverP] * 0.3 * 2. / UnitConstants::m;
@@ -155,12 +159,16 @@ BOOST_AUTO_TEST_CASE(trackparameters_estimation_test) {
               spacePointPtrs.begin(), spacePointPtrs.end());
           BOOST_REQUIRE(partialParamsRes.has_value());
           const auto& estPartialParams = partialParamsRes.value();
+          BOOST_TEST_INFO(
+              "The estimated track parameters at the transverse plane: \n"
+              << estPartialParams.transpose());
+
           // The particle starting position is (0, 0, 0). Hence, d0 is zero; the
           // phi at the point of cloest approach is exactly the phi of the truth
           // particle
           CHECK_CLOSE_ABS(estPartialParams[eBoundLoc0], 0., 1e-5);
           CHECK_CLOSE_ABS(estPartialParams[eBoundPhi], phi, 1e-5);
-          CHECK_CLOSE_ABS(estPartialParams[eBoundQOverP], rho, 1e-5);
+          CHECK_CLOSE_ABS(estPartialParams[eBoundQOverP], rho, 1e-4);
           // The loc1, theta and time are set to zero in the estimator
           CHECK_CLOSE_ABS(estPartialParams[eBoundLoc1], 0., 1e-10);
           CHECK_CLOSE_ABS(estPartialParams[eBoundTheta], 0., 1e-10);
@@ -172,18 +180,22 @@ BOOST_AUTO_TEST_CASE(trackparameters_estimation_test) {
               *bottomSurface, Vector3(0, 0, 2._T), 0.1_T);
           BOOST_REQUIRE(fullParamsRes.has_value());
           const auto& estFullParams = fullParamsRes.value();
+          BOOST_TEST_INFO(
+              "The estimated full track parameters at the bottom space point: "
+              "\n"
+              << estFullParams.transpose());
+
           CHECK_CLOSE_ABS(estFullParams[eBoundLoc0], expParams[eBoundLoc0],
                           1e-5);
           CHECK_CLOSE_ABS(estFullParams[eBoundLoc1], expParams[eBoundLoc1],
                           1e-5);
           // @todo Understand why the phi has a limited precision
-          CHECK_CLOSE_ABS(estFullParams[eBoundPhi], expParams[eBoundPhi], 1e-2);
+          CHECK_CLOSE_ABS(estFullParams[eBoundPhi], expParams[eBoundPhi], 1e-1);
           CHECK_CLOSE_ABS(estFullParams[eBoundTheta], expParams[eBoundTheta],
-                          1e-3);
+                          1e-2);
           CHECK_CLOSE_ABS(estFullParams[eBoundQOverP], expParams[eBoundQOverP],
-                          1e-3);
-          CHECK_CLOSE_ABS(estFullParams[eBoundTime], expParams[eBoundTime],
-                          1e-1);
+                          1e-2);
+          CHECK_CLOSE_ABS(estFullParams[eBoundTime], expParams[eBoundTime], 1.);
         }
       }
     }
