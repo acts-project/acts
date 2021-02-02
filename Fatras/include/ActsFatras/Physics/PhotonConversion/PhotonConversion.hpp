7b8ac0581cc8ffe846c52c712c74834d3198ba5f
FabianKlimpel <fabian.klimpel@tum.de>
2021-02-02 08:31:24 +0100

    Adapted API

--- Fatras/include/ActsFatras/Physics/PhotonConversion/PhotonConversion.hpp
+++ Fatras/include/ActsFatras/Physics/PhotonConversion/PhotonConversion.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2020 CERN for the benefit of the Acts project
+// Copyright (C) 2021 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -24,12 +24,13 @@ namespace ActsFatras {
 /// interaction itself.
 class PhotonConversion {
  public:
+  using Scalar = ActsFatras::Particle::Scalar;
   /// Lower energy threshold for produced children
-  double childMomentumMin = 50. * Acts::UnitConstants::MeV;
+  Scalar childMomentumMin = 50. * Acts::UnitConstants::MeV;
   /// Scaling factor of children energy
-  double childEnergyScaleFactor = 2.;
+  Scalar childEnergyScaleFactor = 2.;
   /// Scaling factor for photon conversion probability
-  double conversionProbScaleFactor = 0.98;
+  Scalar conversionProbScaleFactor = 0.98;
 
   /// @brief Method for evaluating the distance after which the photon
   /// conversion will occur
@@ -40,19 +41,19 @@ class PhotonConversion {
   ///
   /// @param The distance in X_0
   template <typename generator_t>
-  void pairProduction(generator_t& generator, Particle& particle) const;
+  std::pair<Scalar, Scalar> generatePathLimits(generator_t& generator, Particle& particle) const;
 
   /// @brief This method evaluates the final state due to the photon conversion
   ///
   /// @tparam generator_t Type of the random number generator
   /// @param [in, out] generator The random number generator
   /// @param [in] particle The interacting photon
+  /// @param [out] generated List of generated particles
   ///
-  /// @return Vector containing the final state leptons
+  /// @return True if the conversion occured, else false
   template <typename generator_t>
-  std::vector<Particle> operator()(generator_t& generator,
-                                   const Acts::MaterialSlab& /*slab*/,
-                                   const Particle& particle) const;
+  bool operator()(generator_t& generator,
+                                   const Particle& particle, std::vector<Particle>& generated) const;
 
  private:
   /// @brief This method constructs and returns the child particles
@@ -95,40 +96,32 @@ class PhotonConversion {
   /// Helper methods for momentum evaluation
   /// @note These methods are taken from the Geant4 class
   /// G4PairProductionRelModel
-  double screenFunction1(double delta) const;
-  double screenFunction2(double delta) const;
+  Scalar screenFunction1(Scalar delta) const;
+  Scalar screenFunction2(Scalar delta) const;
 
-  static constexpr float m_Z = 13.;  // Aluminium
+  static constexpr Scalar m_Z = 13.;  // Aluminium
 
   /// Irrational numbers
-  static constexpr double m_oneOverThree = 1. / 3.;
-  static constexpr double m_nineOverSeven = 9 / 7;
-
-  //~ constexpr double computeCoulombFactor() const;
+  static constexpr Scalar m_oneOverThree = 1. / 3.;
+  static constexpr Scalar m_nineOverSeven = 9. / 7.;
 };
 
-inline double PhotonConversion::screenFunction1(double delta) const {
+inline Scalar PhotonConversion::screenFunction1(Scalar delta) const {
   // Compute the value of the screening function 3*PHI1(delta) - PHI2(delta)
   return (delta > 1.4) ? 42.038 - 8.29 * log(delta + 0.958)
                        : 42.184 - delta * (7.444 - 1.623 * delta);
 }
 
-inline double PhotonConversion::screenFunction2(double delta) const {
+inline Scalar PhotonConversion::screenFunction2(Scalar delta) const {
   // Compute the value of the screening function 1.5*PHI1(delta)
   // +0.5*PHI2(delta)
   return (delta > 1.4) ? 42.038 - 8.29 * log(delta + 0.958)
                        : 41.326 - delta * (5.848 - 0.902 * delta);
 }
-
-//~ inline constexpr double
-//~ PhotonConversion::computeCoulombFactor() const
-//~ {
-
-//~ }
 }  // namespace ActsFatras
 
 template <typename generator_t>
-void ActsFatras::PhotonConversion::pairProduction(generator_t& generator,
+std::pair<Scalar, Scalar> ActsFatras::PhotonConversion::generatePathLimits(generator_t& generator,
                                                   Particle& particle) const {
   /// This method is based upon the Athena class PhotonConversionTool
 
@@ -151,33 +144,19 @@ void ActsFatras::PhotonConversion::pairProduction(generator_t& generator,
   //  1  p0          -7.01612e-03   8.43478e-01   1.62766e-04   1.11914e-05
   //  2  p1           7.69040e-02   1.00059e+00   8.90718e-05  -8.41167e-07
   //  3  p2          -6.07682e-01   5.13256e+00   6.07228e-04  -9.44448e-07
-  constexpr double p0 = -7.01612e-03;
-  constexpr double p1 = 7.69040e-02;
-  constexpr double p2 = -6.07682e-01;
+  constexpr Scalar p0 = -7.01612e-03;
+  constexpr Scalar p1 = 7.69040e-02;
+  constexpr Scalar p2 = -6.07682e-01;
 
   // calculate xi
-  const double xi = p0 + p1 * pow(particle.absMomentum(), p2);
+  const Scalar xi = p0 + p1 * pow(particle.absMomentum(), p2);
 
-  /// Athena
-  //~ double attenuation = exp(
-  //-7.777e-01*pathCorrection*mprop.thicknessInX0()*(1.-xi) ); ~ return
-  //(conversionProbScaleFactor*CLHEP::RandFlat::shoot(m_randomEngine) >
-  // attenuation) ? true : false;
-
-  /// Transformation steps
-  //~ return (CLHEP::RandFlat::shoot(m_randomEngine) < 1 - attenuation /
-  // conversionProbScaleFactor) ? true : false; ~ conversionProbScaleFactor(rnd
-  // - 1) < -attenuation ~ conversionProbScaleFactor(1 - rnd) > attenuation ~
-  // ln(conversionProbScaleFactor(1 - rnd)) >
-  //-7/9*pathCorrection*mprop.thicknessInX0()*(1.-xi)
-
-  std::uniform_real_distribution<double> uniformDistribution{0., 1.};
+  std::uniform_real_distribution<Scalar> uniformDistribution{0., 1.};
   // This is a transformation of eq. 3.75
-  particle.setMaterialLimits(
-      -m_nineOverSeven *
+  return std::make_pair(-m_nineOverSeven *
           ln(conversionProbScaleFactor * (1 - uniformDistribution(generator))) /
           (1. - xi),
-      particle.pathLimitL0());
+      particle.pathLimitL0(), std::numeric_limits<Scalar>::infinity());
 }
 
 template <typename generator_t>
@@ -188,51 +167,50 @@ ActsFatras::Particle::Scalar ActsFatras::PhotonConversion::childEnergyFraction(
   /// @note This method is from the Geant4 class G4Element
   //
   //  Compute Coulomb correction factor (Phys Rev. D50 3-1 (1994) page 1254)
-  constexpr double k1 = 0.0083;
-  constexpr double k2 = 0.20206;
-  constexpr double k3 = 0.0020;  // This term is missing in Athena
-  constexpr double k4 = 0.0369;
-  constexpr double alphaEM = 1. / 137.;
-  constexpr double az2 = (alphaEM * m_Z) * (alphaEM * m_Z);
-  constexpr double az4 = az2 * az2;
-  constexpr double coulombFactor =
+  constexpr Scalar k1 = 0.0083;
+  constexpr Scalar k2 = 0.20206;
+  constexpr Scalar k3 = 0.0020;  // This term is missing in Athena
+  constexpr Scalar k4 = 0.0369;
+  constexpr Scalar alphaEM = 1. / 137.;
+  constexpr Scalar az2 = (alphaEM * m_Z) * (alphaEM * m_Z);
+  constexpr Scalar az4 = az2 * az2;
+  constexpr Scalar coulombFactor =
       (k1 * az4 + k2 + 1. / (1. + az2)) * az2 - (k3 * az4 + k4) * az4;
 
-  constexpr double logZ13 = log(m_Z) * m_oneOverThree;
-  constexpr double FZ = 8. * (logZ13 + coulombFactor);
-  const double deltaMax = exp((42.038 - FZ) * 0.1206) - 0.958;
+  constexpr Scalar logZ13 = log(m_Z) * m_oneOverThree;
+  constexpr Scalar FZ = 8. * (logZ13 + coulombFactor);
+  const Scalar deltaMax = exp((42.038 - FZ) * 0.1206) - 0.958;
 
-  constexpr double deltaPreFactor = 136. / pow(m_Z, m_oneOverThree);
-  const double eps0 = findMass(Acts::eElectron) / gammaMom;
-  const double deltaFactor = deltaPreFactor * eps0;
-  const double deltaMin = 4. * deltaFactor;
+  constexpr Scalar deltaPreFactor = 136. / pow(m_Z, m_oneOverThree);
+  const Scalar eps0 = findMass(Acts::eElectron) / gammaMom;
+  const Scalar deltaFactor = deltaPreFactor * eps0;
+  const Scalar deltaMin = 4. * deltaFactor;
 
   // compute the limits of eps
-  const double epsMin =
+  const Scalar epsMin =
       std::max(eps0, 0.5 - 0.5 * std::sqrt(1. - deltaMin / deltaMax));
-  const double epsRange = 0.5 - epsMin;
+  const Scalar epsRange = 0.5 - epsMin;
 
   // sample the energy rate (eps) of the created electron (or positron)
-  const double F10 = screenFunction1(deltaMin) - FZ;
-  const double F20 = screenFunction2(deltaMin) - FZ;
-  const double NormF1 = F10 * epsRange * epsRange;
-  const double NormF2 = 1.5 * F20;
+  const Scalar F10 = screenFunction1(deltaMin) - FZ;
+  const Scalar F20 = screenFunction2(deltaMin) - FZ;
+  const Scalar NormF1 = F10 * epsRange * epsRange;
+  const Scalar NormF2 = 1.5 * F20;
 
   // we will need 3 uniform random number for each trial of sampling
-  double greject = 0.;
-  double eps;
-  std::uniform_real_distribution<double> rndmEngine;
+  Scalar greject = 0.;
+  Scalar eps;
+  std::uniform_real_distribution<Scalar> rndmEngine;
   do {
     if (NormF1 > rndmEngine(generator) * (NormF1 + NormF2)) {
       eps = 0.5 - epsRange * pow(rndmEngine(generator), m_oneOverThree);
-      const double delta = deltaFactor / (eps * (1. - eps));
+      const Scalar delta = deltaFactor / (eps * (1. - eps));
       greject = (screenFunction1(delta) - FZ) / F10;
     } else {
       eps = epsMin + epsRange * rndmEngine(generator);
-      const double delta = deltaFactor / (eps * (1. - eps));
+      const Scalar delta = deltaFactor / (eps * (1. - eps));
       greject = (screenFunction2(delta) - FZ) / F20;
     }
-    // Loop checking, 03-Aug-2015, Vladimir Ivanchenko
   } while (greject < rndmEngine(generator));
   //  end of eps sampling
   return eps * childEnergyScaleFactor;
@@ -291,7 +269,7 @@ std::vector<ActsFatras::Particle> ActsFatras::PhotonConversion::recordProduct(
     ActsFatras::Particle::Scalar childEnergy,
     const ActsFatras::Particle::Vector3& childDirection) const {
   // Calculate the child momentum
-  const float massChild = findMass(Acts::eElectron);
+  const Scalar massChild = findMass(Acts::eElectron);
   const Particle::Scalar momentum1 =
       sqrt(childEnergy * childEnergy - massChild * massChild);
 
@@ -343,19 +321,22 @@ std::vector<ActsFatras::Particle> ActsFatras::PhotonConversion::recordProduct(
 }
 
 template <typename generator_t>
-std::vector<ActsFatras::Particle> ActsFatras::PhotonConversion::operator()(
-    generator_t& generator, const Acts::MaterialSlab& /*slab*/,
-    const ActsFatras::Particle& particle) const {
-  const double p = particle.absMomentum();
+bool ActsFatras::PhotonConversion::operator()(
+    generator_t& generator, const ActsFatras::Particle& particle, std::vector<ActsFatras::Particle>& generated) const {
+  const Scalar p = particle.absMomentum();
 
   // Get one child energy
   const Particle::Scalar childEnergy = p * childEnergyFraction(generator, p);
 
   // Now get the deflection
-  Particle::Vector3 childDir = childDirection(generator, particle.momentum4());
-
-  // TODO: The parent must die, maybe here?
+  const Particle::Vector3 childDir = childDirection(generator, particle.momentum4());
 
   // Produce the final state
-  return recordProduct(generator, particle, childEnergy, childDir);
+  const std::vector<Particle> finalState = recordProduct(generator, particle, childEnergy, childDir);
+  generated.insert(generated.back(), finalState.begin(), finalState.end());
+  
+  // Kill the photon
+  particle.setAbsoluteMomentum(0.);
+  
+  return true;
 }
