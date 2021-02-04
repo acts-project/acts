cead8ab004b856809ee8f0282c8923437ec8c21e
Moritz Kiehn <msmk@cern.ch>
2021-02-04 17:58:01 +0100

    Examples/Run: update magnetic field options and setup

--- Examples/Run/MagneticField/BFieldExample.cpp
+++ Examples/Run/MagneticField/BFieldExample.cpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2017 CERN for the benefit of the Acts project
+// Copyright (C) 2017-2021 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,8 +8,8 @@
 
 #include "Acts/Utilities/Helpers.hpp"
 #include "ActsExamples/Framework/Sequencer.hpp"
+#include "ActsExamples/MagneticField/MagneticFieldOptions.hpp"
 #include "ActsExamples/Options/CommonOptions.hpp"
-#include "ActsExamples/Plugins/BField/BFieldOptions.hpp"
 
 #include <string>
 
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
 
   // setup and parse options
   auto desc = ActsExamples::Options::makeDefaultOptions();
-  ActsExamples::Options::addBFieldOptions(desc);
+  ActsExamples::Options::addMagneticFieldOptions(desc);
   desc.add_options()("bf-file-out",
                      value<std::string>()->default_value("BFieldOut.root"),
                      "Set this name for an output root file.")(
@@ -69,14 +69,18 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  auto bFieldVar = ActsExamples::Options::readBField(vm);
+  auto bFieldVar = ActsExamples::Options::readMagneticField(vm);
 
   return std::visit(
       [&](auto& bField) -> int {
         using field_type =
             typename std::decay_t<decltype(bField)>::element_type;
-        if constexpr (!std::is_same_v<field_type, InterpolatedBFieldMap2D> &&
-                      !std::is_same_v<field_type, InterpolatedBFieldMap3D>) {
+        if constexpr (!std::is_same_v<
+                          field_type,
+                          ActsExamples::detail::InterpolatedMagneticField2> &&
+                      !std::is_same_v<
+                          field_type,
+                          ActsExamples::detail::InterpolatedMagneticField3>) {
           std::cout << "Bfield map could not be read. Exiting." << std::endl;
           return EXIT_FAILURE;
         } else {
