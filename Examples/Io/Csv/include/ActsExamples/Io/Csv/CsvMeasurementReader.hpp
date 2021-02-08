667cefd8ec41e898f3075dc0f22ed080547d2494
asalzburger <Andreas.Salzburger@cern.ch>
2021-02-08 09:51:05 +0100

    adding non-compiling  measurement reader

---
+++ Examples/Io/Csv/include/ActsExamples/Io/Csv/CsvMeasurementReader.hpp
@@ -0,0 +1,80 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#pragma once
+
+#include "Acts/Geometry/GeometryIdentifier.hpp"
+#include "Acts/Geometry/TrackingGeometry.hpp"
+#include "Acts/Utilities/Logger.hpp"
+#include "ActsExamples/Framework/IReader.hpp"
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace Acts {
+class Surface;
+}
+
+namespace ActsExamples {
+
+/// Read in a measurement cluster collection in comma-separated-value format.
+///
+/// This reads three files per event file in the configured input
+/// directory. By default it reads file in the current working directory.
+/// Files are assumed to be named using the following schema
+///
+///     event000000001-cells.csv
+///     event000000001-measurements.csv
+///     event000000001-truth.csv
+///     event000000002-cells.csv
+///     event000000002-measurements.csv
+///     event000000002-truth.csv
+///
+/// and each line in the file corresponds to one hit/cluster.
+///
+/// One file per fevent: thread-safe for parallel event porcessing.
+class CsvMeasurementReader final : public IReader {
+ public:
+  struct Config {
+    /// Where to read input files from.
+    std::string inputDir;
+    /// Output cluster collection.
+    std::string outputMeasurements;
+    /// For each cluster/ hit index the original hit id stored on file.
+    std::string outputMeasurementIds;
+    /// Output hit-particles mapping collection.
+    std::string outputMeasurementParticlesMap;
+    /// Output simulated (truth) hits collection.
+    std::string outputSimHits;
+    /// Tracking geometry required to access global-to-local transforms.
+    std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry;
+  };
+
+  /// Construct the cluster reader.
+  ///
+  /// @params cfg is the configuration object
+  /// @params lvl is the logging level
+  CsvMeasurementReader(const Config& cfg, Acts::Logging::Level lvl);
+
+  std::string name() const final override;
+
+  /// Return the available events range.
+  std::pair<size_t, size_t> availableEvents() const final override;
+
+  /// Read out data from the input stream.
+  ProcessCode read(const ActsExamples::AlgorithmContext& ctx) final override;
+
+ private:
+  Config m_cfg;
+  std::pair<size_t, size_t> m_eventsRange;
+  std::unique_ptr<const Acts::Logger> m_logger;
+
+  const Acts::Logger& logger() const { return *m_logger; }
+};
+
+}  // namespace ActsExamples
