cc6a6b0ea75dbe0f1561be6e723eed37b8ffda7a
FabianKlimpel <fabian.klimpel@tum.de>
2021-02-09 17:42:58 +0100

    First draft

--- Fatras/include/ActsFatras/Physics/EnergyLoss/BetheHeitler.hpp
+++ Fatras/include/ActsFatras/Physics/EnergyLoss/BetheHeitler.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2018-2020 CERN for the benefit of the Acts project
+// Copyright (C) 2018-2021 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,7 +13,7 @@
 
 #include <array>
 #include <random>
-
+#include <iostream>
 namespace ActsFatras {
 
 /// Simulate electron energy loss using the Bethe-Heitler description.
@@ -30,13 +30,13 @@ struct BetheHeitler {
   /// @param[in]     generator is the random number generator
   /// @param[in]     slab      defines the passed material
   /// @param[in,out] particle  is the particle being updated
-  /// @return Empty secondaries containers.
+  /// @return Produced photon.
   ///
   /// @tparam generator_t is a RandomNumberEngine
   template <typename generator_t>
-  std::array<Particle, 0> operator()(generator_t &generator,
+  std::array<Particle, 1> operator()(generator_t &generator,
                                      const Acts::MaterialSlab &slab,
-                                     Particle &particle) const {
+                                     Particle &particle) const {    
     // Take a random gamma-distributed value - depending on t/X0
     std::gamma_distribution<double> gDist(slab.thicknessInX0() / std::log(2.0),
                                           1.0);
@@ -45,12 +45,17 @@ struct BetheHeitler {
     const auto z = std::exp(-u);
     const auto sampledEnergyLoss =
         std::abs(scaleFactor * particle.energy() * (z - 1.));
-
+    const Particle::Scalar momentum = particle.absoluteMomentum();
+	
     // apply the energy loss
     particle.correctEnergy(-sampledEnergyLoss);
 
-    // TODO return the lost energy as a photon
-    return {};
+	// Build the produced photon
+    Particle photon(particle.particleId().makeDescendant(0), Acts::PdgParticle::eGamma);
+    photon.setProcess(ProcessType::eBremsstrahlung).setPosition4(particle.fourPosition())
+		.setDirection(particle.unitDirection()).setAbsoluteMomentum(std::min(sampledEnergyLoss, momentum));
+	
+    return {photon};
   }
 };
 
--- Tests/UnitTests/Fatras/Physics/EnergyLossTests.cpp
+++ Tests/UnitTests/Fatras/Physics/EnergyLossTests.cpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2018-2020 CERN for the benefit of the Acts project
+// Copyright (C) 2018-2021 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -14,6 +14,7 @@
 #include "ActsFatras/EventData/Particle.hpp"
 #include "ActsFatras/Physics/EnergyLoss/BetheBloch.hpp"
 #include "ActsFatras/Physics/EnergyLoss/BetheHeitler.hpp"
+#include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
 
 #include <random>
 
@@ -40,8 +41,9 @@ BOOST_DATA_TEST_CASE(BetheBloch, Dataset::parameters, pdg, phi, lambda, p,
 
 BOOST_DATA_TEST_CASE(BetheHeitler, Dataset::parameters, pdg, phi, lambda, p,
                      seed) {
+  (void) pdg;
   Generator gen(seed);
-  ActsFatras::Particle before = Dataset::makeParticle(pdg, phi, lambda, p);
+  ActsFatras::Particle before = Dataset::makeParticle(Acts::PdgParticle::eElectron, phi, lambda, p);
   ActsFatras::Particle after = before;
 
   ActsFatras::BetheHeitler process;
@@ -50,7 +52,19 @@ BOOST_DATA_TEST_CASE(BetheHeitler, Dataset::parameters, pdg, phi, lambda, p,
   BOOST_CHECK_LT(after.absoluteMomentum(), before.absoluteMomentum());
   BOOST_CHECK_LT(after.energy(), before.energy());
   // energy loss creates no new particles
-  BOOST_CHECK(outgoing.empty());
+  BOOST_CHECK_EQUAL(outgoing.size(), 1);
+  BOOST_CHECK_GT(outgoing[0].absoluteMomentum(), 0.);
+  
+  // Get the four momenta
+  	Acts::Vector4 p0 = before.fourMomentum();
+  	Acts::Vector4 p1 = after.fourMomentum();
+  	Acts::Vector4 k = outgoing[0].fourMomentum();
+  
+  // Test for similar invariant masses
+  	Acts::Vector4 sum = p1 + k;
+  	double s = sum(Acts::eEnergy) * sum(Acts::eEnergy) - sum.template segment<3>(Acts::eMom0).norm() * sum.template segment<3>(Acts::eMom0).norm();
+  	double s0 = p0(Acts::eEnergy) * p0(Acts::eEnergy) - p0.template segment<3>(Acts::eMom0).norm() * p0.template segment<3>(Acts::eMom0).norm();
+  	CHECK_CLOSE_OR_SMALL(s, s0, 1e-2, 1e-2);
 }
-
+	
 BOOST_AUTO_TEST_SUITE_END()
