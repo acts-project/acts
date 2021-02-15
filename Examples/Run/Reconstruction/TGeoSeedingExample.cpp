14551faee846b42e5eed1fb6bfe72bc4e6fa7b76
Tomohiro Yamazaki <tomohiro.yamazaki@cern.ch>
2021-02-14 23:26:21 -0800

    Fix format

--- Examples/Run/Reconstruction/TGeoSeedingExample.cpp
+++ Examples/Run/Reconstruction/TGeoSeedingExample.cpp
@@ -19,19 +19,19 @@ int main(int argc, char* argv[]) {
       // automatic selection normalization. setting only the volume already
       // selects all layers within it. the explicit layers in the selection
       // should have no effect.
- // ITk barrel
- Acts::GeometryIdentifier().setVolume(9),  // L0, L1
- Acts::GeometryIdentifier().setVolume(16), // L2,L3,L4
- // ITk negative endcap
- Acts::GeometryIdentifier().setVolume(8),  // R0, R1
- Acts::GeometryIdentifier().setVolume(13), // R2,
- Acts::GeometryIdentifier().setVolume(14), // R3,
- Acts::GeometryIdentifier().setVolume(15), // R4,
- // ITk positive endcap
- Acts::GeometryIdentifier().setVolume(10),  // R0, R1
- Acts::GeometryIdentifier().setVolume(18), // R2,
- Acts::GeometryIdentifier().setVolume(19), // R3,
- Acts::GeometryIdentifier().setVolume(20), // R4,
+      // ITk barrel
+      Acts::GeometryIdentifier().setVolume(9),   // L0, L1
+      Acts::GeometryIdentifier().setVolume(16),  // L2,L3,L4
+      // ITk negative endcap
+      Acts::GeometryIdentifier().setVolume(8),   // R0, R1
+      Acts::GeometryIdentifier().setVolume(13),  // R2,
+      Acts::GeometryIdentifier().setVolume(14),  // R3,
+      Acts::GeometryIdentifier().setVolume(15),  // R4,
+      // ITk positive endcap
+      Acts::GeometryIdentifier().setVolume(10),  // R0, R1
+      Acts::GeometryIdentifier().setVolume(18),  // R2,
+      Acts::GeometryIdentifier().setVolume(19),  // R3,
+      Acts::GeometryIdentifier().setVolume(20),  // R4,
 
   };
 
