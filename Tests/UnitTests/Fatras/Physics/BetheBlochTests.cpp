26369ab31de13e62e6b7e35e306a8b4b6ebf8122
Moritz Kiehn <msmk@cern.ch>
2021-03-03 14:57:16 +0100

    refactor(Fatras): Consistent code organization and names (#723)
    All high-level simulation types are named Simulation...
    
    Main event simulation object is named Simulation (previously Simulator). The auxiliary type for single-particle simulations is named SingleParticleSimulation (previously ParticleSimulator).
    Associated error type is named SimulationError (previously SimulatorError).
    The result type was already named SimulationResult.
    The actor is named SimulationActor (previously Interactor) since it handles both interactions and decay.
    The available physics processes are now more coarsely grouped using the following schema
    
    Physics/Decay/...
    Physics/ElectroMagnetic/...
    Physics/Hadronic/... # <- this one does not yet exist
    and only a single public header is provided for all scattering processes.
    
    Documentation and tests are updated accordingly.

---
+++ Tests/UnitTests/Fatras/Physics/BetheBlochTests.cpp
@@ -0,0 +1,35 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2018-2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
+#include "Acts/Tests/CommonHelpers/PredefinedMaterials.hpp"
+#include "ActsFatras/Physics/ElectroMagnetic/BetheBloch.hpp"
+
+#include <random>
+
+#include "Dataset.hpp"
+
+using Generator = std::ranlux48;
+
+BOOST_DATA_TEST_CASE(FatrasBetheBloch, Dataset::parameters, pdg, phi, lambda, p,
+                     seed) {
+  Generator gen(seed);
+  ActsFatras::Particle before = Dataset::makeParticle(pdg, phi, lambda, p);
+  ActsFatras::Particle after = before;
+
+  ActsFatras::BetheBloch process;
+  const auto outgoing = process(gen, Acts::Test::makeUnitSlab(), after);
+  // energy loss changes momentum and energy
+  BOOST_CHECK_LT(after.absoluteMomentum(), before.absoluteMomentum());
+  BOOST_CHECK_LT(after.energy(), before.energy());
+  // energy loss creates no new particles
+  BOOST_CHECK(outgoing.empty());
+}
