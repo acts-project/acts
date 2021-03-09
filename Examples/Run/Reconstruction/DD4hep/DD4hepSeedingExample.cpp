5d8976c88577717e68c4e245970489b24bcc98e0
Tomohiro Yamazaki <tomohiro.yamazaki@cern.ch>
2021-03-08 22:19:41 -0800

    Move layer selection to json config

--- Examples/Run/Reconstruction/DD4hep/DD4hepSeedingExample.cpp
+++ Examples/Run/Reconstruction/DD4hep/DD4hepSeedingExample.cpp
@@ -6,26 +6,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "Acts/Geometry/GeometryIdentifier.hpp"
 #include "ActsExamples/DD4hepDetector/DD4hepDetector.hpp"
 
 #include "SeedingExample.hpp"
 
 int main(int argc, char* argv[]) {
-  /// selection must not have duplicates.
-  std::vector<Acts::GeometryIdentifier> layersForSeeding = {
-      // open detector barrel layers
-      // the selection intentionally contains duplicates to demonstrate the
-      // automatic selection normalization. setting only the volume already
-      // selects all layers within it. the explicit layers in the selection
-      // should have no effect.
-      Acts::GeometryIdentifier().setVolume(13),
-      // open detector positive endcap layers
-      Acts::GeometryIdentifier().setVolume(14),
-      // open detector negative endcap layers
-      Acts::GeometryIdentifier().setVolume(12),
-  };
 
-  return runSeedingExample(argc, argv, std::make_shared<DD4hepDetector>(),
-                           layersForSeeding);
+  return runSeedingExample(argc, argv, std::make_shared<DD4hepDetector>();
 }
