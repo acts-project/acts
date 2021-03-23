cfb00523340a6ea054a2beadd09a77f7184bf99d
hrzhao <hrzhao76@gmail.com>
2021-03-22 21:16:31 -0700

    read the cov from root properly

--- Examples/Io/Root/include/ACTFW/Io/Root/RootVertexAndTracksReader.hpp
+++ Examples/Io/Root/include/ACTFW/Io/Root/RootVertexAndTracksReader.hpp
@@ -76,8 +76,93 @@ class RootVertexAndTracksReader final : public IReader {
   std::vector<double>* m_ptrQP = new std::vector<double>;
   std::vector<double>* m_ptrTime = new std::vector<double>;
   std::vector<int>* m_ptrVtxID = new std::vector<int>;
-  std::vector<std::vector<double>>* m_ptrTrkCov =
-      new std::vector<std::vector<double>>;
+  // std::vector<std::vector<double>>* m_ptrTrkCov =
+  // new std::vector<std::vector<double>>;
+
+  /// The track covariance matrix
+  std::vector<double> m_cov11;
+  std::vector<double> m_cov12;
+  std::vector<double> m_cov13;
+  std::vector<double> m_cov14;
+  std::vector<double> m_cov15;
+  std::vector<double> m_cov16;
+
+  std::vector<double> m_cov21;
+  std::vector<double> m_cov22;
+  std::vector<double> m_cov23;
+  std::vector<double> m_cov24;
+  std::vector<double> m_cov25;
+  std::vector<double> m_cov26;
+
+  std::vector<double> m_cov31;
+  std::vector<double> m_cov32;
+  std::vector<double> m_cov33;
+  std::vector<double> m_cov34;
+  std::vector<double> m_cov35;
+  std::vector<double> m_cov36;
+
+  std::vector<double> m_cov41;
+  std::vector<double> m_cov42;
+  std::vector<double> m_cov43;
+  std::vector<double> m_cov44;
+  std::vector<double> m_cov45;
+  std::vector<double> m_cov46;
+
+  std::vector<double> m_cov51;
+  std::vector<double> m_cov52;
+  std::vector<double> m_cov53;
+  std::vector<double> m_cov54;
+  std::vector<double> m_cov55;
+  std::vector<double> m_cov56;
+
+  std::vector<double> m_cov61;
+  std::vector<double> m_cov62;
+  std::vector<double> m_cov63;
+  std::vector<double> m_cov64;
+  std::vector<double> m_cov65;
+  std::vector<double> m_cov66;
+
+  std::vector<double>* m_ptrTrkCov11 = &m_cov11;
+  std::vector<double>* m_ptrTrkCov12 = &m_cov12;
+  std::vector<double>* m_ptrTrkCov13 = &m_cov13;
+  std::vector<double>* m_ptrTrkCov14 = &m_cov14;
+  std::vector<double>* m_ptrTrkCov15 = &m_cov15;
+  std::vector<double>* m_ptrTrkCov16 = &m_cov16;
+
+  std::vector<double>* m_ptrTrkCov21 = &m_cov21;
+  std::vector<double>* m_ptrTrkCov22 = &m_cov22;
+  std::vector<double>* m_ptrTrkCov23 = &m_cov23;
+  std::vector<double>* m_ptrTrkCov24 = &m_cov24;
+  std::vector<double>* m_ptrTrkCov25 = &m_cov25;
+  std::vector<double>* m_ptrTrkCov26 = &m_cov26;
+
+  std::vector<double>* m_ptrTrkCov31 = &m_cov31;
+  std::vector<double>* m_ptrTrkCov32 = &m_cov32;
+  std::vector<double>* m_ptrTrkCov33 = &m_cov33;
+  std::vector<double>* m_ptrTrkCov34 = &m_cov34;
+  std::vector<double>* m_ptrTrkCov35 = &m_cov35;
+  std::vector<double>* m_ptrTrkCov36 = &m_cov36;
+
+  std::vector<double>* m_ptrTrkCov41 = &m_cov41;
+  std::vector<double>* m_ptrTrkCov42 = &m_cov42;
+  std::vector<double>* m_ptrTrkCov43 = &m_cov43;
+  std::vector<double>* m_ptrTrkCov44 = &m_cov44;
+  std::vector<double>* m_ptrTrkCov45 = &m_cov45;
+  std::vector<double>* m_ptrTrkCov46 = &m_cov46;
+
+  std::vector<double>* m_ptrTrkCov51 = &m_cov51;
+  std::vector<double>* m_ptrTrkCov52 = &m_cov52;
+  std::vector<double>* m_ptrTrkCov53 = &m_cov53;
+  std::vector<double>* m_ptrTrkCov54 = &m_cov54;
+  std::vector<double>* m_ptrTrkCov55 = &m_cov55;
+  std::vector<double>* m_ptrTrkCov56 = &m_cov56;
+
+  std::vector<double>* m_ptrTrkCov61 = &m_cov61;
+  std::vector<double>* m_ptrTrkCov62 = &m_cov62;
+  std::vector<double>* m_ptrTrkCov63 = &m_cov63;
+  std::vector<double>* m_ptrTrkCov64 = &m_cov64;
+  std::vector<double>* m_ptrTrkCov65 = &m_cov65;
+  std::vector<double>* m_ptrTrkCov66 = &m_cov66;
 
   std::unique_ptr<const Acts::Logger> m_logger;
 
--- Examples/Io/Root/src/RootVertexAndTracksReader.cpp
+++ Examples/Io/Root/src/RootVertexAndTracksReader.cpp
@@ -36,7 +36,49 @@ FW::RootVertexAndTracksReader::RootVertexAndTracksReader(
   m_inputChain->SetBranchAddress("qp", &m_ptrQP);
   m_inputChain->SetBranchAddress("time", &m_ptrTime);
   m_inputChain->SetBranchAddress("vtxID", &m_ptrVtxID);
-  m_inputChain->SetBranchAddress("trkCov", &m_ptrTrkCov);
+  // m_inputChain->SetBranchAddress("trkCov", &m_ptrTrkCov);
+
+  m_inputChain->SetBranchAddress("trkCov11", &m_ptrTrkCov11);
+  m_inputChain->SetBranchAddress("trkCov12", &m_ptrTrkCov12);
+  m_inputChain->SetBranchAddress("trkCov13", &m_ptrTrkCov13);
+  m_inputChain->SetBranchAddress("trkCov14", &m_ptrTrkCov14);
+  m_inputChain->SetBranchAddress("trkCov15", &m_ptrTrkCov15);
+  m_inputChain->SetBranchAddress("trkCov16", &m_ptrTrkCov16);
+
+  m_inputChain->SetBranchAddress("trkCov21", &m_ptrTrkCov21);
+  m_inputChain->SetBranchAddress("trkCov22", &m_ptrTrkCov22);
+  m_inputChain->SetBranchAddress("trkCov23", &m_ptrTrkCov23);
+  m_inputChain->SetBranchAddress("trkCov24", &m_ptrTrkCov24);
+  m_inputChain->SetBranchAddress("trkCov25", &m_ptrTrkCov25);
+  m_inputChain->SetBranchAddress("trkCov26", &m_ptrTrkCov26);
+
+  m_inputChain->SetBranchAddress("trkCov31", &m_ptrTrkCov31);
+  m_inputChain->SetBranchAddress("trkCov32", &m_ptrTrkCov32);
+  m_inputChain->SetBranchAddress("trkCov33", &m_ptrTrkCov33);
+  m_inputChain->SetBranchAddress("trkCov34", &m_ptrTrkCov34);
+  m_inputChain->SetBranchAddress("trkCov35", &m_ptrTrkCov35);
+  m_inputChain->SetBranchAddress("trkCov36", &m_ptrTrkCov36);
+
+  m_inputChain->SetBranchAddress("trkCov41", &m_ptrTrkCov41);
+  m_inputChain->SetBranchAddress("trkCov42", &m_ptrTrkCov42);
+  m_inputChain->SetBranchAddress("trkCov43", &m_ptrTrkCov43);
+  m_inputChain->SetBranchAddress("trkCov44", &m_ptrTrkCov44);
+  m_inputChain->SetBranchAddress("trkCov45", &m_ptrTrkCov45);
+  m_inputChain->SetBranchAddress("trkCov46", &m_ptrTrkCov46);
+
+  m_inputChain->SetBranchAddress("trkCov51", &m_ptrTrkCov51);
+  m_inputChain->SetBranchAddress("trkCov52", &m_ptrTrkCov52);
+  m_inputChain->SetBranchAddress("trkCov53", &m_ptrTrkCov53);
+  m_inputChain->SetBranchAddress("trkCov54", &m_ptrTrkCov54);
+  m_inputChain->SetBranchAddress("trkCov55", &m_ptrTrkCov55);
+  m_inputChain->SetBranchAddress("trkCov56", &m_ptrTrkCov56);
+
+  m_inputChain->SetBranchAddress("trkCov61", &m_ptrTrkCov61);
+  m_inputChain->SetBranchAddress("trkCov62", &m_ptrTrkCov62);
+  m_inputChain->SetBranchAddress("trkCov63", &m_ptrTrkCov63);
+  m_inputChain->SetBranchAddress("trkCov64", &m_ptrTrkCov64);
+  m_inputChain->SetBranchAddress("trkCov65", &m_ptrTrkCov65);
+  m_inputChain->SetBranchAddress("trkCov66", &m_ptrTrkCov66);
 
   // loop over the input files
   for (auto inputFile : m_cfg.fileList) {
@@ -61,7 +103,7 @@ FW::RootVertexAndTracksReader::~RootVertexAndTracksReader() {
   delete m_ptrQP;
   delete m_ptrTime;
   delete m_ptrVtxID;
-  delete m_ptrTrkCov;
+  // delete m_ptrTrkCov;
 }
 
 std::string FW::RootVertexAndTracksReader::name() const {
@@ -110,11 +152,23 @@ FW::ProcessCode FW::RootVertexAndTracksReader::read(
                 (*m_ptrTime)[trkId];
 
             // Get track covariance vector
-            std::vector<double> trkCovVec = (*m_ptrTrkCov)[trkId];
+            // std::vector<double> trkCovVec = (*m_ptrTrkCov)[trkId];
 
             // Construct track covariance
-            Acts::BoundSymMatrix covMat =
-                Eigen::Map<Acts::BoundSymMatrix>(trkCovVec.data());
+            Acts::BoundSymMatrix covMat;
+
+            covMat << m_cov11[trkId], m_cov12[trkId], m_cov13[trkId],
+                m_cov14[trkId], m_cov15[trkId], m_cov16[trkId], m_cov21[trkId],
+                m_cov22[trkId], m_cov23[trkId], m_cov24[trkId], m_cov25[trkId],
+                m_cov26[trkId], m_cov31[trkId], m_cov32[trkId], m_cov33[trkId],
+                m_cov34[trkId], m_cov35[trkId], m_cov36[trkId], m_cov41[trkId],
+                m_cov42[trkId], m_cov43[trkId], m_cov44[trkId], m_cov45[trkId],
+                m_cov46[trkId], m_cov51[trkId], m_cov52[trkId], m_cov53[trkId],
+                m_cov54[trkId], m_cov55[trkId], m_cov56[trkId], m_cov61[trkId],
+                m_cov62[trkId], m_cov63[trkId], m_cov64[trkId], m_cov65[trkId],
+                m_cov66[trkId];
+
+            // std::cout << covMat <<std::endl;
 
             // Create track parameters and add to track list
             std::shared_ptr<Acts::PerigeeSurface> perigeeSurface =
