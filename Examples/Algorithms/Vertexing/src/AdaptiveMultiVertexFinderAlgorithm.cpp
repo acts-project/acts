f7d2308934e7dd20e663709fc69380b7973753a7
Bastian Schlag <bastian.schlag@cern.ch>
2021-04-14 01:16:05 +0200

    make amvf algorithm output also list of Acts::Vertex objects

--- Examples/Algorithms/Vertexing/src/AdaptiveMultiVertexFinderAlgorithm.cpp
+++ Examples/Algorithms/Vertexing/src/AdaptiveMultiVertexFinderAlgorithm.cpp
@@ -42,6 +42,9 @@ ActsExamples::AdaptiveMultiVertexFinderAlgorithm::
   if (m_cfg.outputProtoVertices.empty()) {
     throw std::invalid_argument("Missing output proto vertices collection");
   }
+  if (m_cfg.outputVertices.empty()) {
+    throw std::invalid_argument("Missing output vertices collection");
+  }
 }
 
 ActsExamples::ProcessCode
@@ -127,6 +130,12 @@ ActsExamples::AdaptiveMultiVertexFinderAlgorithm::execute(
   // store proto vertices extracted from the found vertices
   ctx.eventStore.add(m_cfg.outputProtoVertices,
                      makeProtoVertices(inputTrackParameters, vertices));
+
+  std::vector<Acts::Vertex<Acts::BoundTrackParameters>> verticesOut = vertices;
+
+  std::cout << "storing n vertices now.. : " << verticesOut.size() << std::endl;
+  // store found vertices
+  ctx.eventStore.add(m_cfg.outputVertices, std::move(vertices));
 
   return ActsExamples::ProcessCode::SUCCESS;
 }
