21d00704148b6180b0a73f178bf844a73b274583
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2021-04-13 09:32:00 -0700

    EigenStepper: remove FIXME comment

--- Core/include/Acts/Propagator/EigenStepper.hpp
+++ Core/include/Acts/Propagator/EigenStepper.hpp
@@ -96,7 +96,7 @@ class EigenStepper {
         const auto& surface = par.referenceSurface();
         // set the covariance transport flag to true and copy
         covTransport = true;
-        cov = BoundSymMatrix(*par.covariance());  // FIXME (std::optional)
+        cov = BoundSymMatrix(*par.covariance());
         jacToGlobal = surface.jacobianLocalToGlobal(gctx, par.parameters());
       }
     }
