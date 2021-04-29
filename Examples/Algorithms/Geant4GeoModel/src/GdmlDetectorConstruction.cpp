e35c14f6f2ede7297dc5d62e1737303a0210eb5b
FabianKlimpel <fabian.klimpel@tum.de>
2021-04-29 14:13:30 +0200

    Making GeoModel optional and moving some files

--- Examples/Algorithms/Geant4GeoModel/src/GdmlDetectorConstruction.cpp
+++ Examples/Algorithms/Geant4GeoModel/src/GdmlDetectorConstruction.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "ActsExamples/Geant4/GdmlDetectorConstruction.hpp"
+#include "ActsExamples/Geant4GeoModel/GdmlDetectorConstruction.hpp"
 
 #include <G4GDMLParser.hh>
 #include <G4VisAttributes.hh>
