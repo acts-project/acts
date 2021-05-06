13ce710bb59953cda1d5cfcf59c956a64647e999
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2021-05-06 16:09:04 +0200

    clang-format

--- Examples/Scripts/TrackingPerformance/ResidualsAndPulls.cpp
+++ Examples/Scripts/TrackingPerformance/ResidualsAndPulls.cpp
@@ -6,32 +6,30 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "boundParamResolution.C"
-
-#include <TApplication.h>
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-  
-  if (argc > 1){
+#include <TApplication.h>
 
-      std::string farg = argv[1];
-      if (farg == "-h" or farg.find("help") != std::string::npos ){
-          std::cout << "*** ACTS Residual and Pull plotting " << std::endl;
-          std::cout << "*** Usage: ./ActsExampleResidualAndPulls input_file input_tree output_file <save_extension>" << std::endl;
-      }
-      if (argc >= 4){
+#include "boundParamResolution.C"
 
-          TApplication theApp("ResidualAndPulls",0,0);
-          std::string inputFile = farg;
-          std::string inputTree = argv[2];
-          std::string outputFile = argv[3];
-          std::string saveAs = (argc > 4) ? std::string(argv[4]) : std::string("");
-          boundParamResolution(inputFile, inputTree, outputFile, saveAs);
-          theApp.Run();
-
-      }
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    std::string farg = argv[1];
+    if (farg == "-h" or farg.find("help") != std::string::npos) {
+      std::cout << "*** ACTS Residual and Pull plotting " << std::endl;
+      std::cout << "*** Usage: ./ActsExampleResidualAndPulls input_file "
+                   "input_tree output_file <save_extension>"
+                << std::endl;
+    }
+    if (argc >= 4) {
+      TApplication theApp("ResidualAndPulls", 0, 0);
+      std::string inputFile = farg;
+      std::string inputTree = argv[2];
+      std::string outputFile = argv[3];
+      std::string saveAs = (argc > 4) ? std::string(argv[4]) : std::string("");
+      boundParamResolution(inputFile, inputTree, outputFile, saveAs);
+      theApp.Run();
+    }
   }
   return 1;
 }
