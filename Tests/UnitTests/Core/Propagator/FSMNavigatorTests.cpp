a4917aec8246093c1fef3e4ba92a0bb9b591d225
Paul Gessinger <hello@paulgessinger.com>
2021-05-21 10:14:34 +0200

    test works for default navigator

--- Tests/UnitTests/Core/Propagator/FSMNavigatorTests.cpp
+++ Tests/UnitTests/Core/Propagator/FSMNavigatorTests.cpp
@@ -15,6 +15,7 @@
 #include "Acts/Geometry/GeometryContext.hpp"
 #include "Acts/Propagator/ConstrainedStep.hpp"
 #include "Acts/Propagator/FSMNavigator.hpp"
+#include "Acts/Propagator/Navigator.hpp"
 #include "Acts/Propagator/StepperConcept.hpp"
 #include "Acts/Propagator/detail/SteppingHelper.hpp"
 #include "Acts/Surfaces/CylinderSurface.hpp"
@@ -236,7 +237,7 @@ struct PropagatorState {
   PseudoStepper::State stepping;
 
   /// Navigation state - internal state of the Navigator
-  FSMNavigator::State navigation;
+  Navigator::State navigation;
 
   // The context cache for this propagation
   GeometryContext geoContext = GeometryContext();
@@ -246,245 +247,123 @@ CylindricalTrackingGeometry cGeometry(tgContext);
 auto tGeometry = cGeometry();
 
 BOOST_AUTO_TEST_CASE(Navigation) {
-  FSMNavigator::Config cfg;
-  cfg.trackingGeometry = tGeometry;
-  cfg.resolveSensitive = true;
-  cfg.resolveMaterial = true;
-  cfg.resolvePassive = true;
-  FSMNavigator navigator(cfg,
-                         getDefaultLogger("FSMNavigator", Logging::VERBOSE));
+  // FSMNavigator::Config cfg;
+  // cfg.trackingGeometry = tGeometry;
+  // cfg.resolveSensitive = true;
+  // cfg.resolveMaterial = true;
+  // cfg.resolvePassive = true;
+  Navigator navigator(tGeometry);
 
   Vector4 position(0., 0., 0, 0);
   Vector3 momentum(1., 1., 0);
 
+  CurvilinearTrackParameters params{position, momentum.normalized(),
+                                    momentum.norm(), 1};
+
+  const auto* startSurface = &params.referenceSurface();
+
   PseudoStepper stepper;
   PropagatorState state;
 
+  state.navigation.startSurface = startSurface;
+
+  auto logger = getDefaultLogger("Navigator", Logging::Level::VERBOSE);
+  state.options.logger = LoggerWrapper{*logger};
+
   auto step = [&](double fraction = 1.) {
     stepper.step(state.stepping, fraction);
+    // navigator.status(state, stepper);
+    // std::cout << "ACTORS / ABORTERS" << std::endl;
+    // navigator.target(state, stepper);
+  };
+
+  auto status = [&]() {
+    std::cout << "STATUS" << std::endl;
     navigator.status(state, stepper);
-    std::cout << "ACTORS / ABORTERS" << std::endl;
+  };
+
+  auto target = [&]() {
+    std::cout << "TARGET" << std::endl;
     navigator.target(state, stepper);
   };
 
   state.stepping.pos4 = position;
   state.stepping.dir = momentum.normalized();
 
-  navigator.status(state, stepper);
+  status();
 
   // currentVolume has been set
   BOOST_CHECK_NE(state.navigation.currentVolume, nullptr);
   BOOST_CHECK_EQUAL(state.navigation.currentVolume,
                     state.navigation.startVolume);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
+  BOOST_CHECK_EQUAL(state.navigation.currentSurface, startSurface);
+  BOOST_CHECK_EQUAL(state.navigation.startSurface, startSurface);
 
   BOOST_CHECK_EQUAL(state.navigation.currentVolume->volumeName(),
                     "BeamPipe::Barrel");
 
   // status is done, we should still be in initial state
   // propagator now calls target
-  navigator.target(state, stepper);
+  target();
 
-  // do the first step
-  step(0.5);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
-  step(1.0);
-  // we're on the beampipe layer now
+  std::vector<GeometryIdentifier> surfaceSequence{
+      GeometryIdentifier{}.setVolume(2).setLayer(2),
+      GeometryIdentifier{}.setVolume(3).setBoundary(4),
+      GeometryIdentifier{}.setVolume(3).setLayer(2).setApproach(1),
+      GeometryIdentifier{}.setVolume(3).setLayer(2).setSensitive(122),
+      GeometryIdentifier{}.setVolume(3).setLayer(2).setSensitive(123),
+      GeometryIdentifier{}.setVolume(3).setLayer(2).setSensitive(106),
+      GeometryIdentifier{}.setVolume(3).setLayer(2).setSensitive(107),
+      GeometryIdentifier{}.setVolume(3).setLayer(2).setApproach(2),
+      GeometryIdentifier{}.setVolume(3).setLayer(4).setApproach(1),
+      GeometryIdentifier{}.setVolume(3).setLayer(4).setSensitive(244),
+      GeometryIdentifier{}.setVolume(3).setLayer(4).setSensitive(212),
+      GeometryIdentifier{}.setVolume(3).setLayer(4).setSensitive(245),
+      GeometryIdentifier{}.setVolume(3).setLayer(4).setSensitive(213),
+      GeometryIdentifier{}.setVolume(3).setLayer(4).setApproach(2),
+      GeometryIdentifier{}.setVolume(3).setLayer(6).setApproach(1),
+      GeometryIdentifier{}.setVolume(3).setLayer(6).setSensitive(397),
+      GeometryIdentifier{}.setVolume(3).setLayer(6).setSensitive(345),
+      GeometryIdentifier{}.setVolume(3).setLayer(6).setApproach(2),
+      GeometryIdentifier{}.setVolume(3).setLayer(8).setApproach(1),
+      GeometryIdentifier{}.setVolume(3).setLayer(8).setSensitive(595),
+      GeometryIdentifier{}.setVolume(3).setLayer(8).setSensitive(517),
+      GeometryIdentifier{}.setVolume(3).setLayer(8).setApproach(2),
+      GeometryIdentifier{}.setVolume(3).setBoundary(3),
+  };
+
+  for (auto targetSurface : surfaceSequence) {
+    step(0.5);
+    status();
+    BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
+    BOOST_CHECK_NE(state.navigation.currentVolume, nullptr);
+    if (targetSurface.boundary() == 0) {
+      BOOST_CHECK_EQUAL(targetSurface.volume(),
+                        state.navigation.currentVolume->geometryId().volume());
+    }
+    target();
+
+    step(1.0);
+    status();
+    BOOST_CHECK_NE(state.navigation.currentSurface, nullptr);
+    if (!state.navigation.navigationBreak) {
+      BOOST_CHECK_NE(state.navigation.currentVolume, nullptr);
+    }
+    BOOST_CHECK_EQUAL(state.navigation.currentSurface->geometryId(),
+                      targetSurface);
+
+    if (targetSurface.boundary() == 0) {
+      BOOST_CHECK_EQUAL(targetSurface.volume(),
+                        state.navigation.currentVolume->geometryId().volume());
+    }
+    target();
+  }
+
+  BOOST_CHECK_EQUAL(state.navigation.navigationBreak, true);
+  BOOST_CHECK_EQUAL(state.navigation.currentVolume, nullptr);
   BOOST_CHECK_EQUAL(state.navigation.currentSurface->geometryId(),
-                    GeometryIdentifier{}.setVolume(2).setLayer(2));
-
-  step(0.5);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
-  step(1.0);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface->geometryId(),
-                    GeometryIdentifier{}.setVolume(3).setBoundary(4));
-
-  step(0.5);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
-  step(1.0);
-  BOOST_CHECK_EQUAL(
-      state.navigation.currentSurface->geometryId(),
-      GeometryIdentifier{}.setVolume(3).setLayer(2).setApproach(1));
-
-  step(0.5);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
-  step(1.0);
-
-  // // we're on the first surface now
-  BOOST_CHECK_EQUAL(
-      state.navigation.currentSurface->geometryId(),
-      GeometryIdentifier{}.setVolume(3).setLayer(2).setSensitive(122));
-
-  step(0.5);
-  BOOST_CHECK_EQUAL(state.navigation.currentSurface, nullptr);
-  step(1.0);
-
-  // we're on the second surface now
-  BOOST_CHECK_EQUAL(
-      state.navigation.currentSurface->geometryId(),
-      GeometryIdentifier{}.setVolume(3).setLayer(2).setSensitive(123));
+                    surfaceSequence.back());
 }
-
-// the debug boolean
-bool debug = true;
-
-/// @brief Method for testing vectors in @c Navigator::State
-///
-/// @param [in] state Navigator state
-/// @param [in] navSurf Number of navigation surfaces
-/// @param [in] navLay Number of navigation layers
-/// @param [in] navBound Number of navigation boundaries
-/// @param [in] extSurf Number of external surfaces
-bool testNavigatorStateVectors(FSMNavigator::State& state, size_t navSurf,
-                               size_t navLay, size_t navBound, size_t extSurf) {
-  return ((state.navSurfaces.size() == navSurf) &&
-          (state.navLayers.size() == navLay) &&
-          (state.navBoundaries.size() == navBound) &&
-          (state.externalSurfaces.size() == extSurf));
-}
-
-/// @brief Method for testing pointers in @c Navigator::State
-///
-/// @param [in] state Navigation state
-/// @param [in] worldVol World volume
-/// @param [in] startVol Start volume
-/// @param [in] startLay Start layer
-/// @param [in] startSurf Start surface
-/// @param [in] currSurf Current surface
-/// @param [in] currVol Current volume
-/// @param [in] targetVol Target volume
-/// @param [in] targetLay Target layer
-/// @param [in] targetSurf Target surface
-bool testNavigatorStatePointers(
-    FSMNavigator::State& state, const TrackingVolume* worldVol,
-    const TrackingVolume* startVol, const Layer* startLay,
-    const Surface* startSurf, const Surface* currSurf,
-    const TrackingVolume* currVol, const TrackingVolume* targetVol,
-    const Layer* targetLay, const Surface* targetSurf) {
-  return (
-      (state.worldVolume == worldVol) && (state.startVolume == startVol) &&
-      (state.startLayer == startLay) && (state.startSurface == startSurf) &&
-      (state.currentSurface == currSurf) && (state.currentVolume == currVol) &&
-      (state.targetVolume == targetVol) && (state.targetLayer == targetLay) &&
-      (state.targetSurface == targetSurf));
-}
-
-// BOOST_AUTO_TEST_CASE(FSMNavigator_status_methods) {
-//   // create a navigator
-//   FSMNavigator::Config cfg;
-//   cfg.resolveSensitive = false;
-//   cfg.resolveMaterial = false;
-//   cfg.resolvePassive = false;
-//   FSMNavigator navigator{cfg};
-
-//   // position and direction vector
-//   Vector4 position4(0., 0., 0, 0);
-//   Vector3 momentum(1., 1., 0);
-
-//   // the propagator cache
-//   PropagatorState state;
-//   state.options.debug = debug;
-
-//   // the stepper cache
-//   state.stepping.pos4 = position4;
-//   state.stepping.dir = momentum.normalized();
-
-//   // Stepper
-//   PseudoStepper stepper;
-
-//   //
-//   // (1) Test for inactivity
-//   //
-//   // Run without anything present
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr));
-
-//   // Run with geometry but without resolving
-//   cfg.trackingGeometry = tGeometry;
-//   navigator = FSMNavigator{cfg};
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr));
-
-//   // Run with geometry and resolving but broken navigation for various
-//   reasons cfg.resolveSensitive = true; cfg.resolveMaterial = true;
-//   cfg.resolvePassive = true;
-//   navigator = FSMNavigator{cfg};
-//   state.navigation.navigationBreak = true;
-//   // a) Because target is reached
-//   state.navigation.targetReached = true;
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr));
-//   // b) Beacause of no target surface
-//   state.navigation.targetReached = false;
-//   state.navigation.targetSurface = nullptr;
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr, nullptr,
-//                                          nullptr, nullptr, nullptr));
-//   // c) Because the target surface is reached
-//   const Surface* startSurf = tGeometry->getBeamline();
-//   state.stepping.pos4.segment<3>(Acts::ePos0) =
-//       startSurf->center(state.geoContext);
-//   const Surface* targetSurf = startSurf;
-//   state.navigation.targetSurface = targetSurf;
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, nullptr, nullptr,
-//                                          nullptr, nullptr, targetSurf,
-//                                          nullptr, nullptr, nullptr,
-//                                          targetSurf));
-
-//   //
-//   // (2) Test the initialisation
-//   //
-//   // a) Initialise without additional information
-//   state.navigation = FSMNavigator::State();
-//   state.stepping.pos4 << 0., 0., 0., 0.;
-//   const TrackingVolume* worldVol = tGeometry->highestTrackingVolume();
-//   const TrackingVolume* startVol = tGeometry->lowestTrackingVolume(
-//       state.geoContext, stepper.position(state.stepping));
-//   const Layer* startLay = startVol->associatedLayer(
-//       state.geoContext, stepper.position(state.stepping));
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, worldVol,
-//   startVol,
-//                                          startLay, nullptr, nullptr,
-//                                          startVol, nullptr, nullptr,
-//                                          nullptr));
-
-//   // b) Initialise having a start surface
-//   state.navigation = FSMNavigator::State();
-//   state.navigation.startSurface = startSurf;
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, worldVol,
-//   startVol,
-//                                          startLay, startSurf, startSurf,
-//                                          startVol, nullptr, nullptr,
-//                                          nullptr));
-
-//   // c) Initialise having a start volume
-//   state.navigation = FSMNavigator::State();
-//   state.navigation.startVolume = startVol;
-//   navigator.status(state, stepper);
-//   BOOST_CHECK(testNavigatorStateVectors(state.navigation, 0u, 0u, 0u, 0u));
-//   BOOST_CHECK(testNavigatorStatePointers(state.navigation, worldVol,
-//   startVol,
-//                                          startLay, nullptr, nullptr,
-//                                          startVol, nullptr, nullptr,
-//                                          nullptr));
-// }
 
 }  // namespace Test
 }  // namespace Acts
