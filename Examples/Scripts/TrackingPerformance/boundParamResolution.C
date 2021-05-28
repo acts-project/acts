d72e80cc4955fa1aeac0fd6583ec4cb10ad18d06
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2021-05-28 15:37:03 +0200

    feat: update performance plotting scripts (#814)
    Performance plotting script is updated.

--- Examples/Scripts/TrackingPerformance/boundParamResolution.C
+++ Examples/Scripts/TrackingPerformance/boundParamResolution.C
@@ -25,47 +25,9 @@
 #include <TStyle.h>
 #include <TTree.h>
 
+#include "CommonUtils.h"
+
 using namespace ROOT;
-
-// Helper function:
-// function to set up the histgram style
-template <typename hist_t>
-void setHistStyle(hist_t* hist, short color = 1) {
-  hist->GetXaxis()->SetTitleSize(0.05);
-  hist->GetYaxis()->SetTitleSize(0.05);
-  hist->GetXaxis()->SetLabelSize(0.05);
-  hist->GetYaxis()->SetLabelSize(0.05);
-  hist->GetXaxis()->SetTitleOffset(1.);
-  hist->GetYaxis()->SetTitleOffset(1.8);
-  hist->GetXaxis()->SetNdivisions(505);
-  hist->SetMarkerStyle(20);
-  hist->SetMarkerSize(0.8);
-  hist->SetLineWidth(2);
-  // hist->SetTitle("");
-  hist->SetLineColor(color);
-  hist->SetMarkerColor(color);
-}
-
-// Helper function:
-// set color pallette
-template <typename hist_t>
-void adaptColorPalette(hist_t* h, float rmin, float rmax, float rgood,
-                       float rwindow, int n) {
-  // min - max is the range of the axis
-  float rel_good = (rgood - rmin) / (rmax - rmin);
-  float rel_window = rwindow / (rmax - rmin);
-
-  // Stops are
-  const int number = 5;
-  double red[number] = {0., 0., 0., 1., 1.};
-  double green[number] = {0., 1., 1., 1., 0.};
-  double blue[number] = {1., 1., 0., 0., 0.};
-  double stops[number] = {0., rel_good - rel_window, rel_good,
-                          rel_good + rel_window, 1.};
-  h->SetContour(n);
-
-  TColor::CreateGradientColorTable(number, stops, red, green, blue, n);
-}
 
 // (loc1, phi, theta, q/p, t) at all track states from root file produced by the
 // RootTrajectoryStatesWriter
