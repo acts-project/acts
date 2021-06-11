8ca86454855269893ca33c1e83d4a77944c13989
xai <xiaocong.ai@cern.ch>
2021-06-11 23:48:56 +0200

    simplify and fix the scripts

---
+++ Examples/Scripts/TrackingPerformance/TreeAccessors.h
@@ -0,0 +1,180 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include <TCanvas.h>
+#include <TEfficiency.h>
+#include <TFile.h>
+#include <TTree.h>
+
+
+struct RecoTrackInfo {
+  double eta;
+  double pt;
+  unsigned int nMajorityHits;
+  unsigned int nMeasurements;
+};
+
+struct ParticleInfo {
+  ULong64_t particleId = 0;
+  double eta = 0;
+  double p = 0;
+  double pt = 0;
+  UShort_t nHits = 0;
+};
+
+
+struct TreeVars {
+  // The constructor
+  TreeVars(TTree* tree_) : tree(tree_){};
+
+  // The tree being read
+  TTree* tree = nullptr;
+
+  /// The entry numbers for accessing events in increased order (there could be
+  /// multiple entries corresponding to one event number)
+  std::vector<long long> entryNumbers = {};
+};
+
+struct TrackVars : public TreeVars {
+  // Delete the default constructor
+  TrackVars() = delete;
+
+  // The constructor
+  TrackVars(TTree* tree_) : TreeVars(tree_) {
+    tree->SetBranchAddress("event_nr", &eventId);
+    tree->SetBranchAddress("nStates", &nStates);
+    tree->SetBranchAddress("nMeasurements", &nMeasurements);
+    tree->SetBranchAddress("nOutliers", &nOutliers);
+    tree->SetBranchAddress("nHoles", &nHoles);
+    tree->SetBranchAddress("chi2Sum", &chi2Sum);
+    tree->SetBranchAddress("chi2OnMeasurements", &chi2OnMeasurements);
+    tree->SetBranchAddress("nMajorityHits", &nMajorityHits);
+    tree->SetBranchAddress("majorityParticleId", &majorityParticleId);
+    tree->SetBranchAddress("NDF", &NDF);
+
+    tree->SetBranchAddress("hasFittedParams", &hasFittedParams);
+    tree->SetBranchAddress("eLOC0_fit", &eLOC0_fit);
+    tree->SetBranchAddress("eLOC1_fit", &eLOC1_fit);
+    tree->SetBranchAddress("ePHI_fit", &ePHI_fit);
+    tree->SetBranchAddress("eTHETA_fit", &eTHETA_fit);
+    tree->SetBranchAddress("eQOP_fit", &eQOP_fit);
+    tree->SetBranchAddress("eT_fit", &eT_fit);
+    tree->SetBranchAddress("err_eLOC0_fit", &err_eLOC0_fit);
+    tree->SetBranchAddress("err_eLOC1_fit", &err_eLOC1_fit);
+    tree->SetBranchAddress("err_ePHI_fit", &err_ePHI_fit);
+    tree->SetBranchAddress("err_eTHETA_fit", &err_eTHETA_fit);
+    tree->SetBranchAddress("err_eQOP_fit", &err_eQOP_fit);
+    tree->SetBranchAddress("err_eT_fit", &err_eT_fit);
+
+    // It might not be necessary, but we still do it to be sure
+    entryNumbers.resize(tree->GetEntries());
+    tree->Draw("event_nr", "", "goff");
+    // Sort to get the entry numbers of the ordered events
+    TMath::Sort(tree->GetEntries(), tree->GetV1(), entryNumbers.data(), false);
+  }
+
+  // The variables
+  uint32_t eventId;
+  std::vector<unsigned int>* nStates = new std::vector<unsigned int>;
+  std::vector<unsigned int>* nMeasurements = new std::vector<unsigned int>;
+  std::vector<unsigned int>* nOutliers = new std::vector<unsigned int>;
+  std::vector<unsigned int>* nHoles = new std::vector<unsigned int>;
+  std::vector<float>* chi2Sum = new std::vector<float>;
+  std::vector<std::vector<double>>* chi2OnMeasurements =
+      new std::vector<std::vector<double>>;
+  std::vector<unsigned int>* NDF = new std::vector<unsigned int>;
+  std::vector<unsigned int>* nMajorityHits = new std::vector<unsigned int>;
+  std::vector<uint64_t>* majorityParticleId = new std::vector<uint64_t>;
+
+  std::vector<bool>* hasFittedParams = new std::vector<bool>;
+  std::vector<float>* eLOC0_fit = new std::vector<float>;
+  std::vector<float>* eLOC1_fit = new std::vector<float>;
+  std::vector<float>* ePHI_fit = new std::vector<float>;
+  std::vector<float>* eTHETA_fit = new std::vector<float>;
+  std::vector<float>* eQOP_fit = new std::vector<float>;
+  std::vector<float>* eT_fit = new std::vector<float>;
+  std::vector<float>* err_eLOC0_fit = new std::vector<float>;
+  std::vector<float>* err_eLOC1_fit = new std::vector<float>;
+  std::vector<float>* err_ePHI_fit = new std::vector<float>;
+  std::vector<float>* err_eTHETA_fit = new std::vector<float>;
+  std::vector<float>* err_eQOP_fit = new std::vector<float>;
+  std::vector<float>* err_eT_fit = new std::vector<float>;
+};
+
+struct ParticleVars : public TreeVars {
+  // Delete the default constructor
+  ParticleVars() = delete;
+
+  // The constructor
+  ParticleVars(TTree* tree_) : TreeVars(tree_) {
+    tree->SetBranchAddress("event_id", &eventId);
+    tree->SetBranchAddress("particle_id", &particleId);
+    tree->SetBranchAddress("particle_type", &particleType);
+    tree->SetBranchAddress("vx", &vx);
+    tree->SetBranchAddress("vy", &vy);
+    tree->SetBranchAddress("vz", &vz);
+    tree->SetBranchAddress("vt", &vt);
+    tree->SetBranchAddress("px", &px);
+    tree->SetBranchAddress("py", &py);
+    tree->SetBranchAddress("pz", &pz);
+    tree->SetBranchAddress("m", &m);
+    tree->SetBranchAddress("q", &q);
+    tree->SetBranchAddress("nhits", &nHits);
+    tree->SetBranchAddress("ntracks", &nTracks);
+    tree->SetBranchAddress("ntracks_majority", &nTracksMajority);
+
+    // It might not be necessary, but we still do it to be sure
+    entryNumbers.resize(tree->GetEntries());
+    tree->Draw("event_id", "", "goff");
+    // Sort to get the entry numbers of the ordered events
+    TMath::Sort(tree->GetEntries(), tree->GetV1(), entryNumbers.data(), false);
+  }
+
+  // Get all the particles with requested event id
+  std::vector<ParticleInfo> getParticles(const uint32_t& eventNumber) const {
+    // Find the start entry and the batch size for this event
+    std::string eventNumberStr = std::to_string(eventNumber);
+    std::string findStartEntry = "event_id<" + eventNumberStr;
+    std::string findParticlesSize = "event_id==" + eventNumberStr;
+    size_t startEntry = tree->GetEntries(findStartEntry.c_str());
+    size_t nParticles = tree->GetEntries(findParticlesSize.c_str());
+    if (nParticles == 0) {
+      throw std::invalid_argument(
+          "No particles found. Please check the input file.");
+    }
+    std::vector<ParticleInfo> particles;
+    particles.reserve(nParticles);
+    for (unsigned int i = 0; i < nParticles; ++i) {
+      tree->GetEvent(entryNumbers[startEntry + i]);
+      auto pt = std::hypot(px, py);
+      auto p = std::hypot(pt, pz);
+      auto eta = std::atanh(pz / p * 1.);
+      particles.push_back({particleId, eta, p, pt, nHits});
+    }
+    return particles;
+  }
+
+  // The variables
+  ULong64_t eventId = 0;
+  ULong64_t particleId = 0;
+  Int_t particleType = 0;
+  float vx = 0, vy = 0, vz = 0;
+  float vt = 0;
+  float px = 0, py = 0, pz = 0;
+  float m = 0;
+  float q = 0;
+  UShort_t nHits = 0;
+  UShort_t nTracks = 0;
+  UShort_t nTracksMajority = 0;
+};
--- Examples/Scripts/TrackingPerformance/defineReconstructionPerformance.cpp
+++ Examples/Scripts/TrackingPerformance/defineReconstructionPerformance.cpp
@@ -17,201 +17,58 @@
 #include <TFile.h>
 #include <TProfile.h>
 
+#include "TreeAccessors.h"
 #include "CommonUtils.h"
 
-struct TreeVars {
-  TTree* tree = nullptr;
-
-  /// Event identifier.
-  uint32_t eventId;
-
-  /// The entry numbers for accessing events in increased order (there could be
-  /// multiple entries corresponding to one event number)
-  std::vector<long long> entryNumbers = {};
-
-  virtual void setBranch() = 0;
-
-  virtual void setEntryNumbers() = 0;
-};
-
-struct TrackVars : public TreeVars {
-  std::vector<unsigned int>* nStates = new std::vector<unsigned int>;
-  std::vector<unsigned int>* nMeasurements = new std::vector<unsigned int>;
-  std::vector<unsigned int>* nOutliers = new std::vector<unsigned int>;
-  std::vector<unsigned int>* nHoles = new std::vector<unsigned int>;
-  std::vector<float>* chi2Sum = new std::vector<float>;
-  std::vector<std::vector<double>>* chi2OnMeasurements = new std::vector<std::vector<double>>;
-  std::vector<unsigned int>* NDF = new std::vector<unsigned int>;
-  std::vector<unsigned int>* nMajorityHits = new std::vector<unsigned int>;
-  std::vector<uint64_t>* majorityParticleId = new std::vector<uint64_t>;
-
-  std::vector<bool>* hasFittedParams = new std::vector<bool>;
-  std::vector<float>* eLOC0_fit = new std::vector<float>;
-  std::vector<float>* eLOC1_fit = new std::vector<float>;
-  std::vector<float>* ePHI_fit = new std::vector<float>;
-  std::vector<float>* eTHETA_fit = new std::vector<float>;
-  std::vector<float>* eQOP_fit = new std::vector<float>;
-  std::vector<float>* eT_fit = new std::vector<float>;
-  std::vector<float>* err_eLOC0_fit = new std::vector<float>;
-  std::vector<float>* err_eLOC1_fit = new std::vector<float>;
-  std::vector<float>* err_ePHI_fit = new std::vector<float>;
-  std::vector<float>* err_eTHETA_fit = new std::vector<float>;
-  std::vector<float>* err_eQOP_fit = new std::vector<float>;
-  std::vector<float>* err_eT_fit = new std::vector<float>;
-
-  void setBranch() {
-    tree->SetBranchAddress("event_nr", &eventId);
-    tree->SetBranchAddress("nStates", &nStates);
-    tree->SetBranchAddress("nMeasurements", &nMeasurements);
-    tree->SetBranchAddress("nOutliers", &nOutliers);
-    tree->SetBranchAddress("nHoles", &nHoles);
-    tree->SetBranchAddress("chi2Sum", &chi2Sum);
-    tree->SetBranchAddress("chi2OnMeasurements", &chi2OnMeasurements);
-    tree->SetBranchAddress("nMajorityHits", &nMajorityHits);
-    tree->SetBranchAddress("majorityParticleId", &majorityParticleId);
-    tree->SetBranchAddress("NDF", &NDF);
-
-    tree->SetBranchAddress("hasFittedParams", &hasFittedParams);
-    tree->SetBranchAddress("eLOC0_fit", &eLOC0_fit);
-    tree->SetBranchAddress("eLOC1_fit", &eLOC1_fit);
-    tree->SetBranchAddress("ePHI_fit", &ePHI_fit);
-    tree->SetBranchAddress("eTHETA_fit", &eTHETA_fit);
-    tree->SetBranchAddress("eQOP_fit", &eQOP_fit);
-    tree->SetBranchAddress("eT_fit", &eT_fit);
-    tree->SetBranchAddress("err_eLOC0_fit", &err_eLOC0_fit);
-    tree->SetBranchAddress("err_eLOC1_fit", &err_eLOC1_fit);
-    tree->SetBranchAddress("err_ePHI_fit", &err_ePHI_fit);
-    tree->SetBranchAddress("err_eTHETA_fit", &err_eTHETA_fit);
-    tree->SetBranchAddress("err_eQOP_fit", &err_eQOP_fit);
-    tree->SetBranchAddress("err_eT_fit", &err_eT_fit);
-  }
-
-  void setEntryNumbers() {
-    entryNumbers.resize(tree->GetEntries());
-    tree->Draw("event_nr", "", "goff");
-    // Sort to get the entry numbers of the ordered events
-    TMath::Sort(tree->GetEntries(), tree->GetV1(), entryNumbers.data(), false);
-  }
-};
-
-struct ParticleVars : public TreeVars {
-  std::vector<uint64_t>* particleId = new std::vector<uint64_t>;
-  std::vector<int32_t>* particleType = new std::vector<int32_t>;
-  std::vector<uint32_t>* process = new std::vector<uint32_t>;
-  std::vector<float>* vx = new std::vector<float>;
-  std::vector<float>* vy = new std::vector<float>;
-  std::vector<float>* vz = new std::vector<float>;
-  std::vector<float>* vt = new std::vector<float>;
-  std::vector<float>* px = new std::vector<float>;
-  std::vector<float>* py = new std::vector<float>;
-  std::vector<float>* pz = new std::vector<float>;
-  std::vector<float>* m = new std::vector<float>;
-  std::vector<float>* q = new std::vector<float>;
-  std::vector<float>* eta = new std::vector<float>;
-  std::vector<float>* phi = new std::vector<float>;
-  std::vector<float>* pt = new std::vector<float>;
-  std::vector<float>* p = new std::vector<float>;
-  std::vector<uint32_t>* vertexPrimary = new std::vector<uint32_t>;
-  std::vector<uint32_t>* vertexSecondary = new std::vector<uint32_t>;
-  std::vector<uint32_t>* particle = new std::vector<uint32_t>;
-  std::vector<uint32_t>* generation = new std::vector<uint32_t>;
-  std::vector<uint32_t>* subParticle = new std::vector<uint32_t>;
-  std::vector<uint32_t>* nHits = new std::vector<uint32_t>;
-
-  void setBranch() {
-    // Set the branches
-    tree->SetBranchAddress("event_id", &eventId);
-    tree->SetBranchAddress("particle_id", &particleId);
-    tree->SetBranchAddress("particle_type", &particleType);
-    tree->SetBranchAddress("process", &process);
-    tree->SetBranchAddress("vx", &vx);
-    tree->SetBranchAddress("vy", &vy);
-    tree->SetBranchAddress("vz", &vz);
-    tree->SetBranchAddress("vt", &vt);
-    tree->SetBranchAddress("p", &p);
-    tree->SetBranchAddress("px", &px);
-    tree->SetBranchAddress("py", &py);
-    tree->SetBranchAddress("pz", &pz);
-    tree->SetBranchAddress("m", &m);
-    tree->SetBranchAddress("q", &q);
-    tree->SetBranchAddress("eta", &eta);
-    tree->SetBranchAddress("phi", &phi);
-    tree->SetBranchAddress("pt", &pt);
-    tree->SetBranchAddress("vertex_primary", &vertexPrimary);
-    tree->SetBranchAddress("vertex_secondary", &vertexSecondary);
-    tree->SetBranchAddress("particle", &particle);
-    tree->SetBranchAddress("generation", &generation);
-    tree->SetBranchAddress("sub_particle", &subParticle);
-    tree->SetBranchAddress("nHits", &nHits);
-  }
-
-  void setEntryNumbers() {
-    entryNumbers.resize(tree->GetEntries());
-    tree->Draw("event_id", "", "goff");
-    // Sort to get the entry numbers of the ordered events
-    TMath::Sort(tree->GetEntries(), tree->GetV1(), entryNumbers.data(), false);
-  }
-};
-
-/// This script reads all the reconstructed tracks from e.g.
-/// 'tracksummary_ckf.root' and the truth particles from e.g.
-/// 'particles_initial.root' and defines the efficiency, fake rate and
-/// duplicaiton rate. It aims to make custom definition and tuning of the
-/// reconstruction performance easier.
+/// This script/function reads all the reconstructed tracks from e.g.
+/// 'tracksummary_ckf.root' and the (possibly selected) truth particles from
+/// e.g. 'track_finder_particles.root' (which contains the info of 'nHits'), and
+/// defines the efficiency, fake rate and duplicaiton rate. It aims to make
+/// custom definition and tuning of the reconstruction performance easier.
+///
+/// NB: It's very likely that fiducal cuts are already imposed on the truth
+/// particles. Please check the selection criteria in the truth fitting example
+/// which writes out the 'track_finder_particles.root'. For instance, if the
+/// truth particles are already required to have pT > 1 GeV, it does not make
+/// sense to have ptMin = 0.5 GeV here.
 ///
 void defineReconstructionPerformance(
-    const std::string& inputTrackSummaryFileName =
-        "tracksummary_ckf.root",
-    const std::string& inputTruthParticleFileName =
-        "particles_initial.root",
+    const std::string& inputTrackSummaryFileName = "tracksummary_ckf.root",
+    const std::string& inputSimParticleFileName =
+        "performance_track_finder.root",
     const std::string& trackSummaryTreeName = "tracksummary_ckf",
-    const std::string& truthParticleTreeName = "particles",
+    const std::string& simParticleTreeName = "track_finder_particles",
     unsigned int nHitsMin = 9, unsigned int nMeasurementsMin = 6,
-    double ptMin = 1., double truthMatchProbMin = 0.5) {
+    double ptMin = 0.5, double truthMatchProbMin = 0.5) {
   // Open the files for reading
   TFile* trackFile = TFile::Open(inputTrackSummaryFileName.c_str(), "read");
-  TFile* particleFile = TFile::Open(inputTruthParticleFileName.c_str(), "read");
+  TFile* particleFile = TFile::Open(inputSimParticleFileName.c_str(), "read");
 
-  // Define variables for tree reading
-  TrackVars tracks;
-  ParticleVars particles;
-  tracks.tree = (TTree*)trackFile->Get(trackSummaryTreeName.c_str());
-  particles.tree = (TTree*)particleFile->Get(truthParticleTreeName.c_str());
+  // Define variables for tree reading (setBranchAddress etc. is done in the
+  // contructor)
+  TrackVars tVars((TTree*)trackFile->Get(trackSummaryTreeName.c_str()));
+  ParticleVars pVars((TTree*)particleFile->Get(simParticleTreeName.c_str()));
 
-  // Check if there are exactly the same number of events in the two files
-  size_t nEvents = tracks.tree->GetEntries();
-  if (nEvents != particles.tree->GetEntries()) {
-    throw std::invalid_argument(
-        "The number of events are not consistent in the track summary and "
-        "truth particles files");
-  }
-  std::cout << "There are " << nEvents << " events in total." << std::endl;;
-
-  // Set the branch address for variables
-  tracks.setBranch();
-  particles.setBranch();
-
-  // Get the entry numbers for events in increasing order (this is to
-  // 'synchronize' the events in the two root files)
-  tracks.setEntryNumbers();
-  particles.setEntryNumbers();
-
-  const auto& tEntryNumbers = tracks.entryNumbers;
-  const auto& pEntryNumbers = particles.entryNumbers;
+  size_t nEvents = tVars.tree->GetEntries();
+  std::cout << "There are " << nEvents << " events in total." << std::endl;
 
   // Define the efficiency plots
   TEfficiency* trackEff_vs_eta = new TEfficiency(
-      "trackeff_vs_eta", "Tracking efficiency;Truth #eta [GeV/c];Efficiency", 40, -4, 4);
+      "trackeff_vs_eta", "Tracking efficiency;Truth #eta [GeV/c];Efficiency",
+      40, -4, 4);
   TEfficiency* fakeRate_vs_eta = new TEfficiency(
       "fakerate_vs_eta", "Fake rate;#eta [GeV/c];fake rate", 40, -4, 4);
-  TEfficiency* duplicateRate_vs_eta = new TEfficiency(
-      "duplicaterate_vs_eta", "Duplicate rate;#eta [GeV/c];Duplicate rate", 40, -4, 4);
+  TEfficiency* duplicateRate_vs_eta =
+      new TEfficiency("duplicaterate_vs_eta",
+                      "Duplicate rate;#eta [GeV/c];Duplicate rate", 40, -4, 4);
   TEfficiency* trackEff_vs_pt = new TEfficiency(
-      "trackeff_vs_pt", "Tracking efficiency;Truth pt [GeV/c];Efficiency", 40, 0, 100);
+      "trackeff_vs_pt", "Tracking efficiency;Truth pt [GeV/c];Efficiency", 40,
+      0, 100);
   TEfficiency* fakeRate_vs_pt = new TEfficiency(
       "fakerate_vs_pt", "Fake rate;pt [GeV/c];fake rate", 40, 0, 100);
-  TEfficiency* duplicateRate_vs_pt = new TEfficiency(
-      "duplicaterate_vs_pt", "Duplicate rate;pt [GeV/c];Duplicate rate", 40, 0, 100);
+  TEfficiency* duplicateRate_vs_pt =
+      new TEfficiency("duplicaterate_vs_pt",
+                      "Duplicate rate;pt [GeV/c];Duplicate rate", 40, 0, 100);
 
   // Set styles
   setEffStyle(trackEff_vs_eta, 1);
@@ -221,42 +78,45 @@ void defineReconstructionPerformance(
   setEffStyle(fakeRate_vs_pt, 1);
   setEffStyle(duplicateRate_vs_pt, 1);
 
-  struct RecoTrackInfo {
-    double eta;
-    double pt;
-    unsigned int nMajorityHits;
-    unsigned int nMeasurements;
-  };
-
+  // The container from track-particle matching info (Flushed per event)
   std::map<uint64_t, std::vector<RecoTrackInfo>> matchedParticles;
+
+  // Get the sorted entry numbers
+  const auto& tEntryNumbers = tVars.entryNumbers;
+
   // Loop over the events to fill plots
   for (size_t i = 0; i < nEvents; ++i) {
     if (i % 10 == 0) {
       std::cout << "Processing event: " << i << std::endl;
     }
-    auto tEntry = tEntryNumbers[i];
-    auto pEntry = pEntryNumbers[i];
 
-    tracks.tree->GetEvent(tEntry);
-    particles.tree->GetEvent(pEntry);
+    // Get the tracks
+    auto tEntry = tEntryNumbers[i];
+    tVars.tree->GetEvent(tEntry);
+
+    // Get the particles
+    auto particles = pVars.getParticles(i);
+    //    std::cout << "There are " << particles.size()
+    //              << " truth particles in event: " << i << std::endl;
 
     // Loop over the tracks
     // The fake rate is defined as the ratio of selected truth-matched tracks
     // over all selected tracks
-    for (size_t j = 0; j < tracks.nStates->size(); ++j) {
-      auto nMeasurements = tracks.nMeasurements->at(j);
-      auto nOutliers = tracks.nOutliers->at(j);
-      auto nHoles = tracks.nHoles->at(j);
-      auto theta = tracks.eTHETA_fit->at(j);
-      auto qop = tracks.eQOP_fit->at(j);
+    for (size_t j = 0; j < tVars.nStates->size(); ++j) {
+      bool hasFittedParameters = tVars.hasFittedParams->at(j);
+      auto nMeasurements = tVars.nMeasurements->at(j);
+      auto nOutliers = tVars.nOutliers->at(j);
+      auto nHoles = tVars.nHoles->at(j);
+      auto theta = tVars.eTHETA_fit->at(j);
+      auto qop = tVars.eQOP_fit->at(j);
       auto pt = std::abs(1 / qop) * std::sin(theta);
       auto eta = std::atanh(std::cos(theta));
-      auto nMajorityHits = tracks.nMajorityHits->at(j);
-      auto majorityParticleId = tracks.majorityParticleId->at(j);
+      auto nMajorityHits = tVars.nMajorityHits->at(j);
+      auto majorityParticleId = tVars.majorityParticleId->at(j);
 
-      // Select the tracks, e.g. you might also want to add cuts on the
+      // Select the tVars, e.g. you might also want to add cuts on the
       // nOutliers, nHoles
-      if ( (!tracks.hasFittedParams->at(j)) or nMeasurements < nMeasurementsMin or
+      if ((!hasFittedParameters) or nMeasurements < nMeasurementsMin or
           pt < ptMin) {
         continue;
       }
@@ -310,22 +170,23 @@ void defineReconstructionPerformance(
     // Loop over all truth particles in this event
     // The effiency is define as the ratio of selected particles that have been
     // matched with reco
-    for (size_t j = 0; j < particles.particleId->size(); ++j) {
-      auto nHits = particles.nHits->at(j);
-      auto teta = particles.eta->at(j);
-      auto tpt = particles.pt->at(j);
-      if (nHits < nHitsMin or tpt < ptMin) {
+    for (const auto& particle : particles) {
+      auto nHits = particle.nHits;
+      auto eta = particle.eta;
+      auto pt = particle.pt;
+      if (nHits < nHitsMin or pt < ptMin) {
         continue;
       }
+      uint64_t id = particle.particleId;
 
       // Fill the efficiency plots
-      auto ip = matchedParticles.find(particles.particleId->at(j));
+      auto ip = matchedParticles.find(id);
       if (ip != matchedParticles.end()) {
-        trackEff_vs_eta->Fill(true, teta);
-        trackEff_vs_pt->Fill(true, tpt);
+        trackEff_vs_eta->Fill(true, eta);
+        trackEff_vs_pt->Fill(true, pt);
       } else {
-        trackEff_vs_eta->Fill(false, teta);
-        trackEff_vs_pt->Fill(false, tpt);
+        trackEff_vs_eta->Fill(false, eta);
+        trackEff_vs_pt->Fill(false, pt);
       }
     }  // end of all particles
 
@@ -352,5 +213,4 @@ void defineReconstructionPerformance(
 
   c1->cd(6);
   duplicateRate_vs_pt->Draw();
-
 }
