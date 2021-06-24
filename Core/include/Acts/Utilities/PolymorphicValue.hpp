a899541ef551fbcb5b4c0a1b04cacd2a7b6b7f52
Paul Gessinger <hello@paulgessinger.com>
2021-06-24 11:58:41 +0200

    pv: conversion construction and assignment works

--- Core/include/Acts/Utilities/PolymorphicValue.hpp
+++ Core/include/Acts/Utilities/PolymorphicValue.hpp
@@ -25,11 +25,13 @@ struct ControlBlockBase {
 
 template <typename T, typename U>
 struct ControlBlock : public ControlBlockBase<T> {
+  static_assert(std::is_convertible_v<U*, T*>, "Types must be convertible");
+
   explicit ControlBlock(std::unique_ptr<U> u) : m_value{std::move(u)} {}
 
   std::unique_ptr<ControlBlockBase<T>> clone() const override {
     // return std::make_unique<ControlBlock<U>>(*this);
-    return std::make_unique<ControlBlock<T, U>>(std::make_unique<U>(*m_value));
+    return std::make_unique<ControlBlock>(std::make_unique<U>(*m_value));
   }
 
   T* pointer() override { return m_value.get(); }
@@ -40,29 +42,60 @@ struct ControlBlock : public ControlBlockBase<T> {
   std::unique_ptr<U> m_value;
 };
 
+template <typename T, typename U>
+struct DelegatingControlBlock : public ControlBlockBase<T> {
+  DelegatingControlBlock(std::unique_ptr<ControlBlockBase<U>> delegate)
+      : m_delegate{std::move(delegate)} {}
+
+  std::unique_ptr<ControlBlockBase<T>> clone() const override {
+    return std::make_unique<DelegatingControlBlock>(m_delegate->clone());
+  }
+
+  T* pointer() override { return m_delegate->pointer(); }
+  const T* pointer() const override { return m_delegate->pointer(); }
+
+  T* releaseValue() override { return m_delegate->releaseValue(); }
+
+  std::unique_ptr<ControlBlockBase<U>> m_delegate;
+};
+
 }  // namespace detail
+
+template <class T>
+class PolymorphicValue;
+
+template <class T>
+struct IsPolymorphicValue : std::false_type {};
+
+template <class T>
+struct IsPolymorphicValue<PolymorphicValue<T>> : std::true_type {};
 
 template <typename T>
 class PolymorphicValue {
+  template <typename U>
+  friend class PolymorphicValue;
+
  public:
   PolymorphicValue() {}
 
   //   template <typename U>
   //   explicit PolymorphicValue(U value) {}
 
-  template <typename U, typename _U = U, typename _T = T,
-            typename = std::enable_if<!std::is_same_v<_U, _T> &&
-                                      std::is_copy_constructible_v<_U> &&
-                                      std::is_convertible_v<_U*, _T*>>>
+  template <
+      typename U, typename _U = U, typename _T = T,
+      typename = std::enable_if<
+          !std::is_same_v<_U, _T> && std::is_copy_constructible_v<_U> &&
+          std::is_convertible_v<_U*, _T*> && !IsPolymorphicValue<_U>::value>>
   explicit PolymorphicValue(U&& u)
       : m_controlBlock{std::make_unique<detail::ControlBlock<T, U>>(
             std::make_unique<U>(u))} {}
 
-  template <typename U, typename _U = U, typename _T = T,
-            typename = std::enable_if<!std::is_same_v<_U, _T> &&
-                                      std::is_copy_constructible_v<_U> &&
-                                      std::is_convertible_v<_U*, _T*>>,
-            typename... Args>
+  template <
+      typename U, typename _U = U, typename _T = T,
+      typename = std::enable_if<
+          !std::is_same_v<_U, _T> && std::is_copy_constructible_v<_U> &&
+          std::is_convertible_v<_U*, _T*> && !IsPolymorphicValue<_U>::value>,
+      typename... Args>
   explicit PolymorphicValue(std::in_place_type_t<U>, Args&&... args)
       : m_controlBlock{std::make_unique<detail::ControlBlock<T, U>>(
             std::make_unique<U>(std::forward<Args>(args)...))} {}
@@ -71,6 +104,27 @@ class PolymorphicValue {
             typename = std::enable_if<!std::is_same_v<_U, _T> &&
                                       std::is_copy_constructible_v<_U> &&
                                       std::is_convertible_v<_U*, _T*>>>
+  explicit PolymorphicValue(const PolymorphicValue<U>& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = std::make_unique<detail::DelegatingControlBlock<T, U>>(
+        other.m_controlBlock->clone());
+  }
+
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<!std::is_same_v<_U, _T> &&
+                                      std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  explicit PolymorphicValue(PolymorphicValue<U>&& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = std::make_unique<detail::DelegatingControlBlock<T, U>>(
+        std::move(other.m_controlBlock));
+  }
+
+  template <
+      typename U, typename _U = U, typename _T = T,
+      typename = std::enable_if<
+          !std::is_same_v<_U, _T> && std::is_copy_constructible_v<_U> &&
+          std::is_convertible_v<_U*, _T*> && !IsPolymorphicValue<_U>::value>>
   PolymorphicValue& operator=(const U& u) {
     auto cbTmp =
         std::move(m_controlBlock);  // extend lifetime until after operation
@@ -81,12 +135,57 @@ class PolymorphicValue {
 
   template <typename U, typename _U = U, typename _T = T,
             typename = std::enable_if<std::is_copy_constructible_v<_U> &&
-                                      std::is_convertible_v<_U*, _T*>>>
+                                      std::is_convertible_v<_U*, _T*> &&
+                                      !IsPolymorphicValue<_U>::value>>
   PolymorphicValue& operator=(U&& u) {
     auto cbTmp =
         std::move(m_controlBlock);  // extend lifetime until after operation
     m_controlBlock = std::make_unique<detail::ControlBlock<T, U>>(
         std::make_unique<U>(std::move(u)));
+    return *this;
+  }
+
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<!std::is_same_v<_U, _T> &&
+                                      std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  PolymorphicValue& operator=(const PolymorphicValue<U>& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = std::make_unique<detail::DelegatingControlBlock<T, U>>(
+        other.m_controlBlock->clone());
+    return *this;
+  }
+
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<!std::is_same_v<_U, _T> &&
+                                      std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  PolymorphicValue& operator=(PolymorphicValue<U>&& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = std::make_unique<detail::DelegatingControlBlock<T, U>>(
+        std::move(other.m_controlBlock));
+    return *this;
+  }
+
+  PolymorphicValue(const PolymorphicValue& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = other.m_controlBlock->clone();
+  }
+
+  PolymorphicValue& operator=(const PolymorphicValue& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = other.m_controlBlock->clone();
+    return *this;
+  }
+
+  PolymorphicValue(PolymorphicValue&& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = std::move(other.m_controlBlock);
+  }
+
+  PolymorphicValue& operator=(PolymorphicValue&& other) {
+    auto cbTmp = std::move(m_controlBlock);
+    m_controlBlock = std::move(other.m_controlBlock);
     return *this;
   }
 
@@ -119,6 +218,19 @@ class PolymorphicValue {
 
   const T* operator->() const {
     assert(m_controlBlock);
+    return m_controlBlock->pointer();
+  }
+
+  T* pointer() {
+    if (!m_controlBlock) {
+      return nullptr;
+    }
+    return m_controlBlock->pointer();
+  }
+  const T* pointer() const {
+    if (!m_controlBlock) {
+      return nullptr;
+    }
     return m_controlBlock->pointer();
   }
 
--- Tests/UnitTests/Core/Utilities/PolymorphicValueTest.cpp
+++ Tests/UnitTests/Core/Utilities/PolymorphicValueTest.cpp
@@ -34,6 +34,7 @@ BOOST_AUTO_TEST_CASE(TestConstruction) {
   {
     PolymorphicValue<Base> pv;
     BOOST_CHECK(!pv);
+    BOOST_CHECK_EQUAL(pv.pointer(), nullptr);
     // BOOST_CHECK_THROW(PolymorphicValue<Base>{D{}}, std::invalid_argument);
     // BOOST_CHECK_THROW(pv = D{}, std::invalid_argument);
   }
@@ -43,16 +44,19 @@ BOOST_AUTO_TEST_CASE(TestConstruction) {
     BOOST_CHECK(!pv);
     pv = A{6};
     BOOST_CHECK(!!pv);
+    BOOST_CHECK_NE(pv.pointer(), nullptr);
     BOOST_CHECK_EQUAL(pv->value(), 6);
   }
 
   {
     PolymorphicValue<Base> pv{A{8}};
     BOOST_CHECK(!!pv);
+    BOOST_CHECK_NE(pv.pointer(), nullptr);
     BOOST_CHECK_EQUAL(pv->value(), 8);
 
     pv = A{9};
     BOOST_CHECK(!!pv);
+    BOOST_CHECK_NE(pv.pointer(), nullptr);
     BOOST_CHECK_EQUAL(pv->value(), 9);
   }
 }
@@ -66,32 +70,272 @@ struct Destruct : public Base {
 };
 
 BOOST_AUTO_TEST_CASE(TestDestruction) {
-  {
-    bool destroyed = false;
+  // {
+  //   bool destroyed = false;
 
-    std::optional<PolymorphicValue<Base>> pvOpt =
-        PolymorphicValue<Base>{std::in_place_type_t<Destruct>(), &destroyed};
+  //   std::optional<PolymorphicValue<Base>> pvOpt =
+  //       std::make_optional<PolymorphicValue<Base>>(
+  //           std::in_place_type_t<Destruct>(), &destroyed);
 
-    BOOST_CHECK(!destroyed);
-    BOOST_CHECK_EQUAL((*pvOpt)->value(), 0);
-    pvOpt = std::nullopt;
-    BOOST_CHECK(!!destroyed);
+  //   BOOST_CHECK(!destroyed);
+  //   BOOST_CHECK_EQUAL((*pvOpt)->value(), 0);
+  //   pvOpt = std::nullopt;
+  //   BOOST_CHECK(!!destroyed);
+  // }
+
+  // {
+  //   bool destroyed = false;
+
+  //   PolymorphicValue<Base> pv{std::in_place_type_t<Destruct>(), &destroyed};
+
+  //   BOOST_CHECK(!destroyed);
+  //   BOOST_CHECK_EQUAL(pv->value(), 0);
+  //   pv.reset();
+  //   BOOST_CHECK(!!destroyed);
+  //   BOOST_CHECK(!pv);
+  // }
+}
+
+struct Base1 {
+  virtual int value() = 0;
+};
+
+struct E : public Base1 {
+  E(int v) : m_value(v) {}
+  int value() override { return m_value; }
+  int m_value;
+};
+
+struct F : public Base1 {
+  F(int v) : m_value(v) {}
+  int value() override { return m_value; }
+  int m_value;
+};
+
+BOOST_AUTO_TEST_CASE(TestAssignmentSameBase) {
+  {  // COPY
+    PolymorphicValue<Base1> pv1{E{8}};
+    PolymorphicValue<Base1> pv2{F{2}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<F*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 2);
+    BOOST_CHECK_NE(dynamic_cast<F*>(pv2.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+
+    Base1* pre = pv2.pointer();
+
+    pv1 = pv2;
+
+    BOOST_CHECK(pv1);
+    BOOST_CHECK(pv2);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 2);
+    BOOST_CHECK_NE(dynamic_cast<F*>(pv2.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_EQUAL(pv1->value(), 2);
+    BOOST_CHECK_NE(dynamic_cast<F*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_NE(pv1.pointer(), nullptr);
+    BOOST_CHECK_NE(pv2.pointer(), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pre);
   }
 
-  {
-    bool destroyed = false;
+  {  // MOVE
+    PolymorphicValue<Base1> pv1{E{8}};
+    PolymorphicValue<Base1> pv2{F{2}};
 
-    PolymorphicValue<Base> pv{std::in_place_type_t<Destruct>(), &destroyed};
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<F*>(pv1.pointer()), nullptr);
 
-    BOOST_CHECK(!destroyed);
-    BOOST_CHECK_EQUAL(pv->value(), 0);
-    pv.reset();
-    BOOST_CHECK(!!destroyed);
-    BOOST_CHECK(!pv);
+    BOOST_CHECK_EQUAL(pv2->value(), 2);
+    BOOST_CHECK_NE(dynamic_cast<F*>(pv2.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+
+    Base1* pre = pv2.pointer();
+
+    pv1 = std::move(pv2);
+
+    BOOST_CHECK(pv1);
+    BOOST_CHECK(!pv2);
+
+    BOOST_CHECK_EQUAL(pv1->value(), 2);
+    BOOST_CHECK_NE(dynamic_cast<F*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_NE(pv1.pointer(), nullptr);
+    BOOST_CHECK_EQUAL(pv2.pointer(), nullptr);
+
+    BOOST_CHECK_EQUAL(pv1.pointer(), pre);
   }
 }
 
-// @TODO: Conversion / assignment between PV
+BOOST_AUTO_TEST_CASE(TestConstructionSameBase) {
+  {  // COPY
+    PolymorphicValue<Base1> pv1{E{8}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv1.pointer()), nullptr);
+
+    PolymorphicValue<Base1> pv2{pv1};
+
+    BOOST_CHECK(pv1);
+    BOOST_CHECK(pv2);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_NE(pv1.pointer(), nullptr);
+    BOOST_CHECK_NE(pv2.pointer(), nullptr);
+  }
+
+  {  // MOVE
+    PolymorphicValue<Base1> pv1{E{8}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv1.pointer()), nullptr);
+
+    PolymorphicValue<Base1> pv2{std::move(pv1)};
+
+    BOOST_CHECK(!pv1);
+    BOOST_CHECK(pv2);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_EQUAL(pv1.pointer(), nullptr);
+    BOOST_CHECK_NE(pv2.pointer(), nullptr);
+  }
+}
+
+struct Base2 : public Base1 {};
+struct G : public Base2 {
+  G(int v) : m_value(v) {}
+  int value() override { return m_value; }
+  int m_value;
+};
+
+BOOST_AUTO_TEST_CASE(TestAssignmentDifferentBase) {
+  {  // COPY
+    PolymorphicValue<Base1> pv1{E{8}};
+    PolymorphicValue<Base2> pv2{G{3}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<G*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 3);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv2.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+
+    Base1* pre = pv2.pointer();
+    pv1 = pv2;
+
+    BOOST_CHECK(pv1);
+    BOOST_CHECK(pv2);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 3);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv2.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_EQUAL(pv1->value(), 3);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_NE(pv1.pointer(), nullptr);
+    BOOST_CHECK_NE(pv2.pointer(), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pre);
+  }
+
+  {  // MOVE
+    PolymorphicValue<Base1> pv1{E{8}};
+    PolymorphicValue<Base2> pv2{G{3}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<E*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<G*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 3);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv2.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+
+    Base1* pre = pv2.pointer();
+    pv1 = std::move(pv2);
+
+    BOOST_CHECK(pv1);
+    BOOST_CHECK(!pv2);
+
+    BOOST_CHECK_EQUAL(pv1->value(), 3);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv1.pointer()), nullptr);
+    BOOST_CHECK_EQUAL(dynamic_cast<E*>(pv1.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_NE(pv1.pointer(), nullptr);
+    BOOST_CHECK_EQUAL(pv2.pointer(), nullptr);
+
+    BOOST_CHECK_EQUAL(pv1.pointer(), pre);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(TestConstructionDifferentBase) {
+  {  // COPY
+    PolymorphicValue<Base2> pv1{G{8}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv1.pointer()), nullptr);
+
+    PolymorphicValue<Base1> pv2{pv1};
+
+    BOOST_CHECK(pv1);
+    BOOST_CHECK(pv2);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_NE(pv1.pointer(), nullptr);
+    BOOST_CHECK_NE(pv2.pointer(), nullptr);
+  }
+
+  {  // MOVE
+    PolymorphicValue<Base2> pv1{G{8}};
+
+    BOOST_CHECK_EQUAL(pv1->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv1.pointer()), nullptr);
+
+    PolymorphicValue<Base1> pv2{std::move(pv1)};
+
+    BOOST_CHECK(!pv1);
+    BOOST_CHECK(pv2);
+
+    BOOST_CHECK_EQUAL(pv2->value(), 8);
+    BOOST_CHECK_NE(dynamic_cast<G*>(pv2.pointer()), nullptr);
+
+    BOOST_CHECK_NE(pv1.pointer(), pv2.pointer());
+    BOOST_CHECK_EQUAL(pv1.pointer(), nullptr);
+    BOOST_CHECK_NE(pv2.pointer(), nullptr);
+  }
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 }  // namespace Test
