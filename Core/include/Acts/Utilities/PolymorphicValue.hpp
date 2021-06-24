427d44b208bb32e9cce05826b3200c878205f972
Paul Gessinger <hello@paulgessinger.com>
2021-06-24 10:10:06 +0200

    pv: basic functionality

--- Core/include/Acts/Utilities/PolymorphicValue.hpp
+++ Core/include/Acts/Utilities/PolymorphicValue.hpp
@@ -18,18 +18,24 @@ struct ControlBlockBase {
   virtual std::unique_ptr<ControlBlockBase<T>> clone() const = 0;
 
   virtual T* pointer() = 0;
+  virtual const T* pointer() const = 0;
+
+  virtual T* releaseValue() = 0;
 };
 
-template <typename T, typename U = T>
-struct ControlBlock {
-  explicit ControlBlock(std::unique_ptr<U> value) : m_value{std::move(value)} {}
+template <typename T, typename U>
+struct ControlBlock : public ControlBlockBase<T> {
+  explicit ControlBlock(std::unique_ptr<U> u) : m_value{std::move(u)} {}
 
   std::unique_ptr<ControlBlockBase<T>> clone() const override {
     // return std::make_unique<ControlBlock<U>>(*this);
-    return std::make_unique<ControlBlock<U>>(*m_value);
+    return std::make_unique<ControlBlock<T, U>>(std::make_unique<U>(*m_value));
   }
 
-  T* pointer() { return &m_value; }
+  T* pointer() override { return m_value.get(); }
+  const T* pointer() const override { return m_value.get(); }
+
+  T* releaseValue() override { return m_value.release(); }
 
   std::unique_ptr<U> m_value;
 };
@@ -44,35 +50,77 @@ class PolymorphicValue {
   //   template <typename U>
   //   explicit PolymorphicValue(U value) {}
 
-  template <typename U>
-  explicit PolymorphicValue(U&& value)
-      : m_controlBlock{
-            std::make_unique<detail::ControlBlock<U>>(std::move(value))} {}
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  explicit PolymorphicValue(U&& u)
+      : m_controlBlock{std::make_unique<detail::ControlBlock<T, U>>(
+            std::make_unique<U>(u))} {}
 
-  template <typename U>
-  PolymorphicValue& operator=(const U& other) {
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>,
+            typename... Args>
+  explicit PolymorphicValue(std::in_place_type_t<U>, Args&&... args)
+      : m_controlBlock{std::make_unique<detail::ControlBlock<T, U>>(
+            std::make_unique<U>(std::forward<Args>(args)...))} {}
+
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  PolymorphicValue& operator=(const U& u) {
+    auto cbTmp =
+        std::move(m_controlBlock);  // extend lifetime until after operation
+    m_controlBlock =
+        std::make_unique<detail::ControlBlock<T, U>>(std::make_unique<U>(u));
     return *this;
   }
 
-  template <typename U>
-  PolymorphicValue& operator=(U&& other) {
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  PolymorphicValue& operator=(U&& u) {
+    auto cbTmp =
+        std::move(m_controlBlock);  // extend lifetime until after operation
+    m_controlBlock = std::make_unique<detail::ControlBlock<T, U>>(
+        std::make_unique<U>(std::move(u)));
     return *this;
   }
 
   operator bool() const { return !!m_controlBlock; }
 
+  T* release() {
+    // release the value itself first
+    T* value = m_controlBlock->releaseValue();
+    // now release the control block
+    m_controlBlock.reset();
+    return value;
+  }
+
+  template <typename U, typename _U = U, typename _T = T,
+            typename = std::enable_if<std::is_copy_constructible_v<_U> &&
+                                      std::is_convertible_v<_U*, _T*>>>
+  void reset(U* u) {
+    auto cbTmp =
+        std::move(m_controlBlock);  // extend lifetime until after operation
+    m_controlBlock =
+        std::make_unique<detail::ControlBlock<T, U>>(std::unique_ptr<U>(u));
+  }
+
+  void reset() { m_controlBlock.reset(); }
+
   T* operator->() {
     assert(m_controlBlock);
-    return *m_controlBlock->pointer();
+    return m_controlBlock->pointer();
   }
 
   const T* operator->() const {
     assert(m_controlBlock);
-    return *m_controlBlock->pointer();
+    return m_controlBlock->pointer();
   }
 
  private:
-  std::unique_ptr<detail::ControlBlock<T>> m_controlBlock{nullptr};
+  std::unique_ptr<detail::ControlBlockBase<T>> m_controlBlock{nullptr};
 };
 
 }  // namespace Acts
--- Tests/UnitTests/Core/Utilities/PolymorphicValueTest.cpp
+++ Tests/UnitTests/Core/Utilities/PolymorphicValueTest.cpp
@@ -34,21 +34,26 @@ BOOST_AUTO_TEST_CASE(TestConstruction) {
   {
     PolymorphicValue<Base> pv;
     BOOST_CHECK(!pv);
-    BOOST_CHECK_THROW(PolymorphicValue<Base>{D{}}, std::invalid_argument);
-    BOOST_CHECK_THROW(pv = D{}, std::invalid_argument);
+    // BOOST_CHECK_THROW(PolymorphicValue<Base>{D{}}, std::invalid_argument);
+    // BOOST_CHECK_THROW(pv = D{}, std::invalid_argument);
   }
 
   {
     PolymorphicValue<Base> pv;
+    BOOST_CHECK(!pv);
     pv = A{6};
     BOOST_CHECK(!!pv);
-    BOOST_CHECK_EQUAL(pv->value(), 8);
+    BOOST_CHECK_EQUAL(pv->value(), 6);
   }
 
   {
     PolymorphicValue<Base> pv{A{8}};
     BOOST_CHECK(!!pv);
     BOOST_CHECK_EQUAL(pv->value(), 8);
+
+    pv = A{9};
+    BOOST_CHECK(!!pv);
+    BOOST_CHECK_EQUAL(pv->value(), 9);
   }
 }
 
@@ -61,12 +66,27 @@ struct Destruct : public Base {
 };
 
 BOOST_AUTO_TEST_CASE(TestDestruction) {
-  bool destroyed = false;
-  std::optional<PolymorphicValue<Base>> pvOpt =
-      PolymorphicValue<Base>{Destruct{&destroyed}};
-  BOOST_CHECK(!destroyed);
-  pvOpt = std::nullopt;
-  BOOST_CHECK(!!destroyed);
+  {
+    bool destroyed = false;
+
+    std::optional<PolymorphicValue<Base>> pvOpt =
+        PolymorphicValue<Base>{std::in_place_type_t<Destruct>(), &destroyed};
+
+    BOOST_CHECK(!destroyed);
+    pvOpt = std::nullopt;
+    BOOST_CHECK(!!destroyed);
+  }
+
+  {
+    bool destroyed = false;
+
+    PolymorphicValue<Base> pv{std::in_place_type_t<Destruct>(), &destroyed};
+
+    BOOST_CHECK(!destroyed);
+    pv.reset();
+    BOOST_CHECK(!!destroyed);
+    BOOST_CHECK(!pv);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
