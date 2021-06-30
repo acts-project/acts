384531889458a1bc5162cfcd456c6bdb729a6676
Paul Gessinger <hello@paulgessinger.com>
2021-06-30 13:18:23 +0200

    fix: AnnulusBoundsTests uses deprecated boost header
    Fixes #180

--- Tests/UnitTests/Core/Surfaces/AnnulusBoundsTests.cpp
+++ Tests/UnitTests/Core/Surfaces/AnnulusBoundsTests.cpp
@@ -9,7 +9,7 @@
 // clang-format off
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
-#include <boost/test/output_test_stream.hpp>
+#include <boost/test/tools/output_test_stream.hpp>
 // clang-format on
 
 #include <limits>
