af3db3df2979772573d58107868f3f9529081e87
Gabriel Alfredo Carmona Tabja <gcarmona@ui01.hpc.utfsm.cl>
2021-07-01 11:27:20 -0400

    repairing format of names of functions

--- Plugins/Cuda/include/Acts/Plugins/Cuda/Utilities/NewMemoryManager/detail/Aligned.hpp
+++ Plugins/Cuda/include/Acts/Plugins/Cuda/Utilities/NewMemoryManager/detail/Aligned.hpp
@@ -14,6 +14,7 @@
 #include <cstdint>
 #include <memory>
 #include <new>
+#include <iostream>
 
 namespace Acts {
 namespace Cuda {
@@ -27,7 +28,7 @@ static constexpr std::size_t NMM_DEFAULT_HOST_ALIGNMENT{alignof(std::max_align_t
 constexpr bool is_pow2(std::size_t n) { return (0 == (n & (n - 1)));} 
 
 //@return if `alignment` is a valid memory alignment
-constexpr bool is_suppored_aligment(std::size_t alignment) { return is_pow2(alignment);}
+constexpr bool is_supported_alignment(std::size_t alignment) { return is_pow2(alignment);}
 
 //@param[in] v value to align
 //@param[in] alignment amount, in bytes, must be a power of 2
@@ -35,7 +36,7 @@ constexpr bool is_suppored_aligment(std::size_t alignment) { return is_pow2(alig
 //@return the aligned value, as one would except
 constexpr std::size_t align_up(std::size_t v, std::size_t align_bytes) noexcept {
 	//if the alignment is not support, the program will end
-	assert(is_suppored_aligment(align_bytes));
+	assert(is_supported_alignment(align_bytes));
 	return (v + (align_bytes - 1)) & ~(align_bytes - 1);
 }
 
@@ -45,7 +46,7 @@ constexpr std::size_t align_up(std::size_t v, std::size_t align_bytes) noexcept 
 //@return the aligned value, as one would except
 constexpr std::size_t align_down(std::size_t v, std::size_t align_bytes) noexcept {
 	//if the alignment is not support, the program will end
-	assert(is_suppored_aligment(align_bytes));
+	assert(is_supported_alignment(align_bytes));
 	return v & ~(align_bytes - 1);
 }
 
@@ -55,7 +56,7 @@ constexpr std::size_t align_down(std::size_t v, std::size_t align_bytes) noexcep
 //@return true if aligned
 constexpr bool is_aligned(std::size_t v, std::size_t align_bytes) noexcept {
 	//if the alignment is not support, the program will end
-	assert(is_suppored_aligment(align_bytes));
+	assert(is_supported_alignment(align_bytes));
 	return v == align_down(v, align_bytes);
 }
 
@@ -81,13 +82,9 @@ void *aligned_allocate(std::size_t bytes, std::size_t alignment, Alloc alloc) {
 	assert(is_pow2(alignment));
 
 	std::size_t padded_allocation_size{bytes + alignment + sizeof(std::ptrdiff_t)};
-
 	char *const original = static_cast<char *>(alloc(padded_allocation_size));
-
 	void *aligned{original + sizeof(std::ptrdiff_t)};
-
 	std::align(alignment, bytes, aligned, padded_allocation_size);
-
 	std::ptrdiff_t offset = static_cast<char *>(aligned) - original;
 
 	*(static_cast<std::ptrdiff_t *>(aligned) - 1) = offset;
@@ -101,7 +98,7 @@ void *aligned_allocate(std::size_t bytes, std::size_t alignment, Alloc alloc) {
 //
 // @tparam Dealloc a unary callable type that deallocates memory
 // @param[in] p the aligned pointer to deallocate
-// @param[in] bytes the number of bytes requestedfrom `aligned_allocate`
+// @param[in] bytes the number of bytes requested from `aligned_allocate`
 // @param[in] alignment the alignment required from `aligned_allocate`
 // @param[in] dealloc a unvary callable capable of freeing memory returned from
 // `alloc` in `aligned_allocate` 
