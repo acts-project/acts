81f48ff52aaf219b7f587712d246181c91f0c0f1
Gabriel Alfredo Carmona Tabja <gcarmona@ui01.hpc.utfsm.cl>
2021-07-01 11:25:55 -0400

    same as arena memory resource

--- Plugins/Cuda/include/Acts/Plugins/Cuda/Utilities/NewMemoryManager/MemoryResource/Device/CudaMemoryResource.hpp
+++ Plugins/Cuda/include/Acts/Plugins/Cuda/Utilities/NewMemoryManager/MemoryResource/Device/CudaMemoryResource.hpp
@@ -17,43 +17,7 @@ namespace Cuda {
 namespace Nmm {
 namespace MemoryResource {
 
-class CudaMemoryResource final : public DeviceMemoryResource {
-	public:
-		CudaMemoryResource() = default;
-		~CudaMemoryResource() = default;
-		CudaMemoryResource(CudaMemoryResource const&) = default;
-		CudaMemoryResource(CudaMemoryResource&&) = default;
-		CudaMemoryResource& operator=(CudaMemoryResource const&) = default;
-		CudaMemoryResource& operator=(CudaMemoryResource&&) = default;
 
-		bool supportsStreams() const noexcept override { return false; }
-
-		bool supportsGetMemInfo() const noexcept override { return true; }
-
-	private:
-		void* doAllocate(std::size_t bytes, CudaStreamView) override {
-			void* p{nullptr};
-			cudaMalloc(&p, bytes);
-			return p;
-		}
-
-		void doDeallocate(void* p, std::size_t, CudaStreamView) override {
-			cudaFree(p);
-		}
-
-		bool doIsEqual(DeviceMemoryResource const& other) const noexcept override {
-			return dynamic_cast<CudaMemoryResource const*>(&other) != nullptr;
-		}
-
-		std::pair<size_t, size_t> doGetMemInfo(CudaStreamView) const override {
-			std::size_t freeSize;
-			std::size_t totalSize;
-
-			cudaMemGetInfo(&freeSize, &totalSize);
-			return std::make_pair(freeSize, totalSize);
-		}
-
-};// class CudaMemoryResource
 
 } // namespace MemoryResource
 } // namespace Nmm
