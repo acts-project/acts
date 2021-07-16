150cffb02ec0bd5de1959cc9023f6a384b431eb6
Joana Niermann <joana.niermann@cern.ch>
2021-07-16 17:50:25 +0200

    Basic json config for tgeodetector and cylinder/disk splitter (#884)
    The options handling of the TGeoDetector is moved from boost to json
    based configuration. It builds upon the recent reordering of the
    detector options in #923. New functionality is introduces in
    Examples/Io/Json. Since it needs information from the TGeo plugin,
    the latter is only linked in the example io when the plugin is required.

---
+++ Examples/Io/Json/include/ActsExamples/Io/Json/JsonTGeoDetectorConfig.hpp
@@ -0,0 +1,39 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2018-2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/Geometry/PassiveLayerBuilder.hpp"
+#include "Acts/Plugins/Json/ActsJson.hpp"
+#include "Acts/Plugins/TGeo/TGeoCylinderDiscSplitter.hpp"
+#include "Acts/Plugins/TGeo/TGeoLayerBuilder.hpp"
+#include "ActsExamples/Utilities/OptionsFwd.hpp"
+
+namespace ActsExamples {
+
+namespace Options {
+/// Add TGeo detector options prefixed with geo-tgeo.
+void addTGeoGeometryOptions(Description& desc);
+}  // namespace Options
+
+/// Read the BeamPipe configuration from the user configuration.
+std::vector<double> readBeampipeBuilderParam(const std::string& path);
+
+/// Read config for cylinder/disc module splitter
+void from_json(const nlohmann::json& j,
+               Acts::TGeoCylinderDiscSplitter::Config& msc);
+
+/// Read nested layer config for TGeoLayerbuilder
+void from_json(const nlohmann::json& j,
+               Acts::TGeoLayerBuilder::LayerConfig& psc);
+
+/// Read the TGeo layer builder configurations from the user configuration.
+std::vector<Acts::TGeoLayerBuilder::Config> readTGeoLayerBuilderConfigs(
+    const std::string& path);
+
+}  // namespace ActsExamples
