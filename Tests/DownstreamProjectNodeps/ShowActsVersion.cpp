eaba3a8d68e91398bc7ea2191cac1d45ab65e234
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2021-07-20 15:30:35 -0700

    clang-format

--- Tests/DownstreamProjectNodeps/ShowActsVersion.cpp
+++ Tests/DownstreamProjectNodeps/ShowActsVersion.cpp
@@ -11,15 +11,15 @@
 #include <cstdio>
 #include <cstdlib>
 
-#include <boost/version.hpp>
 #include <Eigen/Core>
+#include <boost/version.hpp>
 
 int main(void) {
   printf("Using Acts version %u.%u.%u commit %s\n", Acts::VersionMajor,
          Acts::VersionMinor, Acts::VersionPatch, Acts::CommitHash);
   printf("Using Boost version %u.%u.%u\n", BOOST_VERSION / 100000,
-	 BOOST_VERSION / 100 % 1000, BOOST_VERSION % 100);
+         BOOST_VERSION / 100 % 1000, BOOST_VERSION % 100);
   printf("Using Eigen version %u.%u.%u\n", EIGEN_WORLD_VERSION,
-	 EIGEN_MAJOR_VERSION, EIGEN_MINOR_VERSION);
+         EIGEN_MAJOR_VERSION, EIGEN_MINOR_VERSION);
   return EXIT_SUCCESS;
 }
