600f4032bf8aad1b779e9df046a678585813f3af
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2021-07-20 14:14:48 -0700

    Allow boost & eigen headers to be used by downstream users after install

---
+++ Tests/DownstreamProjectNodeps/ShowActsVersion.cpp
@@ -0,0 +1,25 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2020 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <Acts/ActsVersion.hpp>
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <boost/version.hpp>
+#include <Eigen/Core>
+
+int main(void) {
+  printf("Using Acts version %u.%u.%u commit %s\n", Acts::VersionMajor,
+         Acts::VersionMinor, Acts::VersionPatch, Acts::CommitHash);
+  printf("Using Boost version %u.%u.%u\n", BOOST_VERSION / 100000,
+	 BOOST_VERSION / 100 % 1000, BOOST_VERSION % 100);
+  printf("Using Eigen version %u.%u.%u\n", EIGEN_WORLD_VERSION,
+	 EIGEN_MAJOR_VERSION, EIGEN_MINOR_VERSION);
+  return EXIT_SUCCESS;
+}
