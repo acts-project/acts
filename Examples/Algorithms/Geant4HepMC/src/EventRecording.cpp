66b4de74801d740ac06bb0e31cbd3bd61a212731
andriish <andriish@users.noreply.github.com>
2021-08-11 00:04:15 +0200

    Update EventRecording.cpp

--- Examples/Algorithms/Geant4HepMC/src/EventRecording.cpp
+++ Examples/Algorithms/Geant4HepMC/src/EventRecording.cpp
@@ -135,14 +135,14 @@ ActsExamples::ProcessCode ActsExamples::EventRecording::execute(
       // Remove vertices w/o outgoing particles and particles w/o production vertices
         while (true) {
           bool sane = true;
-          for (auto v: m_event.vertices()) {
+          for (auto v: event.vertices()) {
             if (!v) continue;
             if (v->particles_out().empty()) {
               event.remove_vertex(v);
               sane = false;
             }
           }
-          for (auto p: m_event.particles()) {
+          for (auto p: event.particles()) {
             if (!p) continue;
             if (!p->production_vertex()) {
               event.remove_particle(p);
