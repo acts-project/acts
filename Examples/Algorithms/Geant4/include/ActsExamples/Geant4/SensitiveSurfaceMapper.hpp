58720295db19eb08072e765c4bda63b4223176bf
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2021-09-06 16:15:53 +0200

    feat: rework Geant4 simulation (2/X) (#977)
    This PR is the second pull request for the Geant4 simulation and introduces the full simulation for single particles.
    It uses the same Geant4Simulation algorithm and establishes a symmetric setup between Fatras and Geant4 simulation.
    
    e.g. Fatras:
    
    ./ActsExamplesFatrasDD4hep <args>
    and Geant4:
    
    ./ActsExamplesGeant4DD4hep <args>
    Can now be run with the same argument list.

---
+++ Examples/Algorithms/Geant4/include/ActsExamples/Geant4/SensitiveSurfaceMapper.hpp
@@ -0,0 +1,77 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2021 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/Definitions/Algebra.hpp"
+#include "Acts/Utilities/Logger.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class G4VPhysicalVolume;
+
+namespace Acts {
+class TrackingGeometry;
+}
+
+namespace ActsExamples {
+
+/// This Mapper takes a (non-const) Geant4 geometry and maps
+/// it such that name will be containing the mapping prefix
+/// and the Acts::GeometryIdentifier of the surface.
+///
+/// This allows to directly associate Geant4 hits to the sensitive
+/// elements of the Acts::TrackingGeoemtry w/o map lookup.
+class SensitiveSurfaceMapper {
+ public:
+  static constexpr const char* mappingPrefix = "ActsGeoID#";
+
+  /// Configuration struct for the surface mapper
+  struct Config {
+    std::vector<std::string> materialMappings = {"Silicon"};
+    std::vector<std::string> volumeMappings = {};
+
+    std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry = nullptr;
+  };
+
+  /// Constructor with:
+  ///
+  /// @param cfg the configuration struct
+  /// @param logger the logging instance
+  SensitiveSurfaceMapper(const Config& cfg,
+                         std::unique_ptr<const Acts::Logger> logger =
+                             Acts::getDefaultLogger("SensitiveSurfaceMapper",
+                                                    Acts::Logging::INFO));
+  ~SensitiveSurfaceMapper() = default;
+
+  /// Recursive mapping function that walks through the Geant4
+  /// hierarchy and applies name remapping to the Physical volumes
+  /// of the Geant4 geometry.
+  ///
+  /// @param g4PhysicalVolume the current physical volume in process
+  /// @param motherPosition the absolute position of the mother
+  /// @param sCounter  a counter of how many volumes have been remapped
+  void remapSensitiveNames(G4VPhysicalVolume* g4PhysicalVolume,
+                           const Acts::Vector3 motherPosition,
+                           int& sCounter) const;
+
+ protected:
+  /// Configuration object
+  Config m_cfg;
+
+ private:
+  /// Private access method to the logging instance
+  const Acts::Logger& logger() const { return *m_logger; }
+
+  /// The looging instance
+  std::unique_ptr<const Acts::Logger> m_logger;
+};
+
+}  // namespace ActsExamples
