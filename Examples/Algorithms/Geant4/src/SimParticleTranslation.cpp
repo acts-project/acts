8bc6ab01c554a25e54c8677c450d8c253908d977
Paul Gessinger <hello@paulgessinger.com>
2021-09-27 11:50:12 +0200

    fix: SimParticleTranslation does not correctly translate momentum

--- Examples/Algorithms/Geant4/src/SimParticleTranslation.cpp
+++ Examples/Algorithms/Geant4/src/SimParticleTranslation.cpp
@@ -89,7 +89,7 @@ void ActsExamples::SimParticleTranslation::GeneratePrimaries(G4Event* anEvent) {
           currentVertex[2] * convertLength, currentVertex[3]);
     }
     // Add a new primary to the vertex
-    auto mom4 = part.fourMomentum() * convertEnergy;
+    auto mom4 = (part.fourMomentum() * convertEnergy).eval();
 
     // Particle properties, may be forced to specific value
     G4int particlePdgCode =
@@ -121,7 +121,11 @@ void ActsExamples::SimParticleTranslation::GeneratePrimaries(G4Event* anEvent) {
     }
 
     ACTS_VERBOSE("Adding particle with name '"
-                 << particleDefinition->GetParticleName() << "'.");
+                 << particleDefinition->GetParticleName()
+                 << "' and properties:");
+    ACTS_VERBOSE(" -> mass: " << particleMass);
+    ACTS_VERBOSE(" -> momentum: " << mom4.transpose());
+    ACTS_VERBOSE(" -> charge: " << part.charge());
 
     G4PrimaryParticle* particle = new G4PrimaryParticle(particleDefinition);
 
