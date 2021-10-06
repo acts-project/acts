169921a6d54373b080917fd045c9989841865944
Paul Gessinger <paul.gessinger@cern.ch>
2021-10-06 11:26:17 +0200

    apply black python formatting

--- Examples/Scripts/MaterialMaping/configureMap.py
+++ Examples/Scripts/MaterialMaping/configureMap.py
@@ -17,86 +17,138 @@ import sys
 # It can also be used to define the binning for volume mapping
 
 if sys.version_info[0] < 3:
-    print('Using Python 2')
-    print('To obtain the proper ordering in the Json files Python 3 is recomanded')
+    print("Using Python 2")
+    print("To obtain the proper ordering in the Json files Python 3 is recomanded")
 
-if len(sys.argv) < 2 :
-    inFileName = 'geometry-maps.json'
-    confFileName = 'config-map.json'
-    
-if len(sys.argv) < 3 :
-    confFileName = 'config-map.json'
-    
-else :
+if len(sys.argv) < 2:
+    inFileName = "geometry-maps.json"
+    confFileName = "config-map.json"
+
+if len(sys.argv) < 3:
+    confFileName = "config-map.json"
+
+else:
     inFileName = sys.argv[1]
     confFileName = sys.argv[2]
 
-    
-with open(inFileName,'r+') as json_file:
-    with open(confFileName,'r') as config_file:
+
+with open(inFileName, "r+") as json_file:
+    with open(confFileName, "r") as config_file:
 
         config = json.load(config_file)
         data = json.load(json_file)
 
-        for entry in data['Surfaces']['entries']:
+        for entry in data["Surfaces"]["entries"]:
 
-            if 'type' not in entry['value']['bounds']:
-                entry['value']['bounds']['type'] = ''
+            if "type" not in entry["value"]["bounds"]:
+                entry["value"]["bounds"]["type"] = ""
 
-            if 'layer' in entry:  
-                if 'approach' not in entry:
-                    if 'sensitive' not in entry:
-                        for conf in config['Surfaces'][str(entry['volume'])]:
-                            if 'layer' in conf and conf['layer'] == 'X' and conf['value']['bounds']['type'] == entry['value']['bounds']['type']:
-                                entry['value']['material']['mapMaterial'] = conf['value']['material']['mapMaterial']
-                                entry['value']['material']['mappingType'] = conf['value']['material']['mappingType']
+            if "layer" in entry:
+                if "approach" not in entry:
+                    if "sensitive" not in entry:
+                        for conf in config["Surfaces"][str(entry["volume"])]:
+                            if (
+                                "layer" in conf
+                                and conf["layer"] == "X"
+                                and conf["value"]["bounds"]["type"]
+                                == entry["value"]["bounds"]["type"]
+                            ):
+                                entry["value"]["material"]["mapMaterial"] = conf[
+                                    "value"
+                                ]["material"]["mapMaterial"]
+                                entry["value"]["material"]["mappingType"] = conf[
+                                    "value"
+                                ]["material"]["mappingType"]
                                 ibin = 0
-                                for bin in entry['value']['material']['binUtility']['binningdata']:                                  
-                                    bin['bins'] = conf['value']['material']['binUtility']['binningdata'][ibin]['bins']
-                                    ibin = ibin+1
+                                for bin in entry["value"]["material"]["binUtility"][
+                                    "binningdata"
+                                ]:
+                                    bin["bins"] = conf["value"]["material"][
+                                        "binUtility"
+                                    ]["binningdata"][ibin]["bins"]
+                                    ibin = ibin + 1
                                 continue
                         continue
 
-            if 'boundary' in entry:    
-                if 'layer' not in entry:
-                    for conf in config['Surfaces'][str(entry['volume'])]:
-                        if 'boundary' in conf and conf['boundary'] == entry['boundary'] and conf['value']['bounds']['type'] == entry['value']['bounds']['type']:
-                            entry['value']['material']['mapMaterial'] = conf['value']['material']['mapMaterial']
-                            entry['value']['material']['mappingType'] = conf['value']['material']['mappingType']
+            if "boundary" in entry:
+                if "layer" not in entry:
+                    for conf in config["Surfaces"][str(entry["volume"])]:
+                        if (
+                            "boundary" in conf
+                            and conf["boundary"] == entry["boundary"]
+                            and conf["value"]["bounds"]["type"]
+                            == entry["value"]["bounds"]["type"]
+                        ):
+                            entry["value"]["material"]["mapMaterial"] = conf["value"][
+                                "material"
+                            ]["mapMaterial"]
+                            entry["value"]["material"]["mappingType"] = conf["value"][
+                                "material"
+                            ]["mappingType"]
                             ibin = 0
-                            for bin in entry['value']['material']['binUtility']['binningdata']:
-                                bin['bins'] = conf['value']['material']['binUtility']['binningdata'][ibin]['bins']
-                                ibin = ibin+1
+                            for bin in entry["value"]["material"]["binUtility"][
+                                "binningdata"
+                            ]:
+                                bin["bins"] = conf["value"]["material"]["binUtility"][
+                                    "binningdata"
+                                ][ibin]["bins"]
+                                ibin = ibin + 1
                             continue
                     continue
-                 
-            if 'approach' in entry:
-                if 'sensitive' not in entry:
-                    for conf in config['Surfaces'][str(entry['volume'])]:
-                        if 'approach' in conf and conf['approach'] == entry['approach'] and conf['value']['bounds']['type'] == entry['value']['bounds']['type']:
-                            entry['value']['material']['mapMaterial'] = conf['value']['material']['mapMaterial']
-                            entry['value']['material']['mappingType'] = conf['value']['material']['mappingType']
+
+            if "approach" in entry:
+                if "sensitive" not in entry:
+                    for conf in config["Surfaces"][str(entry["volume"])]:
+                        if (
+                            "approach" in conf
+                            and conf["approach"] == entry["approach"]
+                            and conf["value"]["bounds"]["type"]
+                            == entry["value"]["bounds"]["type"]
+                        ):
+                            entry["value"]["material"]["mapMaterial"] = conf["value"][
+                                "material"
+                            ]["mapMaterial"]
+                            entry["value"]["material"]["mappingType"] = conf["value"][
+                                "material"
+                            ]["mappingType"]
                             ibin = 0
-                            for bin in entry['value']['material']['binUtility']['binningdata']:
-                                bin['bins'] = conf['value']['material']['binUtility']['binningdata'][ibin]['bins']
-                                ibin = ibin+1
+                            for bin in entry["value"]["material"]["binUtility"][
+                                "binningdata"
+                            ]:
+                                bin["bins"] = conf["value"]["material"]["binUtility"][
+                                    "binningdata"
+                                ][ibin]["bins"]
+                                ibin = ibin + 1
                             continue
                     continue
-                 
-            if 'sensitive' in entry:  
-                if 'approach' not in entry:
-                    for conf in config['Surfaces'][str(entry['volume'])]:
-                        if 'sensitive' in conf and conf['sensitive'] == 'X' and conf['layer'] == entry['layer'] and conf['value']['bounds']['type'] == entry['value']['bounds']['type']:
-                            entry['value']['material']['mapMaterial'] = conf['value']['material']['mapMaterial']
-                            entry['value']['material']['mappingType'] = conf['value']['material']['mappingType']
+
+            if "sensitive" in entry:
+                if "approach" not in entry:
+                    for conf in config["Surfaces"][str(entry["volume"])]:
+                        if (
+                            "sensitive" in conf
+                            and conf["sensitive"] == "X"
+                            and conf["layer"] == entry["layer"]
+                            and conf["value"]["bounds"]["type"]
+                            == entry["value"]["bounds"]["type"]
+                        ):
+                            entry["value"]["material"]["mapMaterial"] = conf["value"][
+                                "material"
+                            ]["mapMaterial"]
+                            entry["value"]["material"]["mappingType"] = conf["value"][
+                                "material"
+                            ]["mappingType"]
                             ibin = 0
-                            for bin in entry['value']['material']['binUtility']['binningdata']:
-                                bin['bins'] = conf['value']['material']['binUtility']['binningdata'][ibin]['bins']
-                                ibin = ibin+1
+                            for bin in entry["value"]["material"]["binUtility"][
+                                "binningdata"
+                            ]:
+                                bin["bins"] = conf["value"]["material"]["binUtility"][
+                                    "binningdata"
+                                ][ibin]["bins"]
+                                ibin = ibin + 1
                             continue
-                    continue  
-        data['Volumes'] = config['Volumes']        
-    json_file.seek(0) 
+                    continue
+        data["Volumes"] = config["Volumes"]
+    json_file.seek(0)
     json.dump(data, json_file, indent=4)
     json_file.truncate()
-    
