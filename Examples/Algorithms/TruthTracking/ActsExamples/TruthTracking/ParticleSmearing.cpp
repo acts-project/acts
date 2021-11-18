2ee8b700b687acf16a3ebc5caf4bc3dd127d0845
beomki-yeo <beomki.yeo@gmail.com>
2021-11-18 01:57:11 -0800

    double brace

--- Examples/Algorithms/TruthTracking/ActsExamples/TruthTracking/ParticleSmearing.cpp
+++ Examples/Algorithms/TruthTracking/ActsExamples/TruthTracking/ParticleSmearing.cpp
@@ -26,15 +26,15 @@ void ActsExamples::ParticleSmearing::addOptions(Options::Description& desc) {
   using Options::Reals;
 
   auto opt = desc.add_options();
-  opt("smear-sigma-D0", value<Reals<3>>()->default_value({20, 30, 0.3}),
+  opt("smear-sigma-D0", value<Reals<3>>()->default_value({{20, 30, 0.3}}),
       "Smear the initial Pt-dependent d0 in perigee frame with a_0[um] + "
       "a_1[um]*exp(-1.*abs(a_2[1/GeV])*pt)");
-  opt("smear-sigma-Z0", value<Reals<3>>()->default_value({20, 30, 0.3}),
+  opt("smear-sigma-Z0", value<Reals<3>>()->default_value({{20, 30, 0.3}}),
       "Smear the initial Pt-dependent z0 in perigee frame with a_0[um] + "
       "a_1[um]*exp(-1.*abs(a_2[1/GeV])*pt)");
   opt("smear-sigma-T0", value<double>()->default_value(1),
       "Smear the initial time in ns");
-  opt("smear-sigma-momentum", value<Reals<3>>()->default_value({1, 1, 0.1}),
+  opt("smear-sigma-momentum", value<Reals<3>>()->default_value({{1, 1, 0.1}}),
       "Smear the initial phi (degree), theta (degree) and momentum (relative)");
 }
 
