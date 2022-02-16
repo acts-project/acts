e6f9724ecae93532f1be32b38c502d2b6bb306b4
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2022-02-16 12:16:12 +0100

    update python script

--- Examples/Scripts/Python/ExaTrkX.py
+++ Examples/Scripts/Python/ExaTrkX.py
@@ -1,105 +1,19 @@
 #!/usr/bin/env python3
 from pathlib import Path
-from typing import Optional
-
-from acts.examples import Sequencer, GenericDetector, RootParticleReader
+from typing import Optional, Union
 
 import acts.examples
-
 import acts
-
 from acts import UnitConstants as u
 
 
-def runExaTrkX(
-    trackingGeometry,
-    decorators,
-    geometrySelection: Path,
-    digiConfigFile: Path,
-    field,
-    outputDir: Path,
-    outputCsv=True,
-    inputParticlePath: Optional[Path] = None,
-    s=None,
-):
-    s = s or Sequencer(events=1, numThreads=1)
-
-    logger = acts.logging.getLogger("ExaTrkXExample")
-
-    for d in decorators:
-        s.addContextDecorator(d)
-
-    rnd = acts.examples.RandomNumbers(seed=42)
-
-    if inputParticlePath is None:
-        logger.info("Generating particles using particle gun")
-
-        evGen = acts.examples.EventGenerator(
-            level=acts.logging.INFO,
-            generators=[
-                acts.examples.EventGenerator.Generator(
-                    multiplicity=acts.examples.FixedMultiplicityGenerator(n=2),
-                    vertex=acts.examples.GaussianVertexGenerator(
-                        stddev=acts.Vector4(0, 0, 0, 0), mean=acts.Vector4(0, 0, 0, 0)
-                    ),
-                    particles=acts.examples.ParametricParticleGenerator(
-                        p=(1 * u.GeV, 10 * u.GeV),
-                        eta=(-2, 2),
-                        phi=(0, 360 * u.degree),
-                        randomizeCharge=True,
-                        numParticles=4,
-                    ),
-                )
-            ],
-            outputParticles="particles_input",
-            randomNumbers=rnd,
-        )
-        s.addReader(evGen)
-        inputParticles = evGen.config.outputParticles
-    else:
-        logger.info("Reading particles from %s", inputParticlePath.resolve())
-        assert inputParticlePath.exists()
-        inputParticles = "particles_read"
-        s.addReader(
-            RootParticleReader(
-                level=acts.logging.INFO,
-                filePath=str(inputParticlePath.resolve()),
-                particleCollection=inputParticles,
-                orderedEvents=False,
-            )
-        )
-
-    # Selector
-    selector = acts.examples.ParticleSelector(
-        level=acts.logging.INFO,
-        inputParticles=inputParticles,
-        outputParticles="particles_selected",
-    )
-    s.addAlgorithm(selector)
-
-    # Simulation
-    simAlg = acts.examples.FatrasSimulation(
-        level=acts.logging.INFO,
-        inputParticles=selector.config.outputParticles,
-        outputParticlesInitial="particles_initial",
-        outputParticlesFinal="particles_final",
-        outputSimHits="simhits",
-        randomNumbers=rnd,
-        trackingGeometry=trackingGeometry,
-        magneticField=field,
-        generateHitsOnSensitive=True,
-    )
-    s.addAlgorithm(simAlg)
-
-    # Run the sim hits smearing
-    digiCfg = acts.examples.DigitizationConfig(
-        acts.examples.readDigiConfigFromJson(str(digiConfigFile)),
-        trackingGeometry=trackingGeometry,
-        randomNumbers=rnd,
-        inputSimHits=simAlg.config.outputSimHits,
-    )
-    digiAlg = acts.examples.DigitizationAlgorithm(digiCfg, acts.logging.INFO)
-    s.addAlgorithm(digiAlg)
+def addExaTrkx(
+    s: acts.examples.Sequencer,
+    trackingGeometry: acts.TrackingGeometry,
+    geometrySelection: Optional[Union[Path, str]],
+    onnxModelDir: Optional[Union[Path, str]],
+    outputDirRoot: Optional[Union[Path, str]] = None,
+) -> acts.examples.Sequencer:
 
     # Run the particle selection
     # The pre-selection will select truth particles satisfying provided criteria
@@ -109,8 +23,8 @@ def runExaTrkX(
         level=acts.logging.INFO,
         ptMin=500 * u.MeV,
         nHitsMin=9,
-        inputParticles=simAlg.config.outputParticlesInitial,
-        inputMeasurementParticlesMap=digiCfg.outputMeasurementParticlesMap,
+        inputParticles="particles_initial",
+        inputMeasurementParticlesMap="measurement_particle_map",
         outputParticles="particles_seed_selected",
     )
     s.addAlgorithm(selAlg)
@@ -120,8 +34,8 @@ def runExaTrkX(
     # Create space points
     spAlg = acts.examples.SpacePointMaker(
         level=acts.logging.INFO,
-        inputSourceLinks=digiAlg.config.outputSourceLinks,
-        inputMeasurements=digiAlg.config.outputMeasurements,
+        inputSourceLinks="source_links",
+        inputMeasurements="measurements",
         outputSpacePoints="spacepoints",
         trackingGeometry=trackingGeometry,
         geometrySelection=acts.examples.readJsonGeometryList(str(geometrySelection)),
@@ -132,7 +46,7 @@ def runExaTrkX(
     # It takes all the source links created from truth hit smearing, seeds from
     # truth particle smearing and source link selection config
     exaTrkxFinding = acts.examples.ExaTrkXTrackFinding(
-        inputMLModuleDir="/home/benjamin/Documents/acts_project/gnn_integration/run/onnx_models",
+        inputMLModuleDir=onnxModelDir,
         spacepointFeatures=3,
         embeddingDim=8,
         rVal=1.6,
@@ -140,7 +54,7 @@ def runExaTrkX(
         filterCut=0.21,
     )
 
-    trackFinderAlg = acts.examples.TrackFindingAlgorithmExaTrkX(
+    trackFinderAlg = acts.examples.TrackFindingMLBasedAlgorithm(
         level=acts.logging.INFO,
         inputSpacePoints="spacepoints",
         outputProtoTracks="protoTracks",
@@ -154,7 +68,7 @@ def runExaTrkX(
         inputProtoTracks="protoTracks",
         inputParticles=inputParticles,  # the original selected particles after digitization
         inputMeasurementParticlesMap=digiAlg.config.outputMeasurementParticlesMap,
-        filePath=str(outputDir / "performance_seeding_trees.root"),
+        filePath=str(outputDirRoot / "performance_seeding_trees.root"),
     )
     s.addWriter(trackFinderPerformanceWriter)
 
@@ -162,9 +76,12 @@ def runExaTrkX(
 
 
 if "__main__" == __name__:
+    import os
+    from digitization import configureDigitization
+
     srcdir = Path(__file__).resolve().parent.parent.parent.parent
 
-    detector, trackingGeometry, decorators = GenericDetector.create()
+    detector, trackingGeometry, decorators = acts.examples.GenericDetector.create()
 
     field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
 
@@ -172,13 +89,35 @@ if "__main__" == __name__:
     if not inputParticlePath.exists():
         inputParticlePath = None
 
-    runExaTrkX(
+    srcdir = Path(__file__).resolve().parent.parent.parent.parent
+    geometrySelection = srcdir / "Examples/Algorithms/TrackFinding/share/geoSelection-genericDetector.json"
+    assert geometrySelection.exists()
+
+    onnxdir = Path(os.getcwd()) / "onnx_models"
+    assert onnxdir.exists()
+
+    s = acts.examples.Sequencer(events=100, numThreads=-1)
+    s.config.logLevel = acts.logging.INFO
+
+    rnd = acts.examples.RandomNumbers()
+    outputDir = Path(os.getcwd())
+
+    s = configureDigitization(
         trackingGeometry,
-        decorators,
-        field=field,
-        geometrySelection=srcdir
-        / "Examples/Algorithms/TrackFinding/share/geoSelection-genericDetector.json",
-        digiConfigFile=srcdir
-        / "Examples/Algorithms/Digitization/share/default-smearing-config-generic.json",
-        outputDir=Path.cwd(),
-    ).run()
+        field,
+        outputDir,
+        inputParticlePath,
+        outputRoot=True,
+        outputCsv=True,
+        s=s
+    )
+
+    s = addExaTrkx(
+        s,
+        trackingGeometry,
+        geometrySelection,
+        onnxdir,
+        outputDir
+    )
+
+    s.run()
