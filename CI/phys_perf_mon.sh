99274e4295224751938c542a5b2fb01ac831bacd
Paul Gessinger <hello@paulgessinger.com>
2022-03-08 13:34:18 +0100

    gha output

--- CI/phys_perf_mon.sh
+++ CI/phys_perf_mon.sh
@@ -11,20 +11,32 @@ set +e
 
 ec=0
 
-histcmp \
+function run() {
+    a=$1
+    b=$2
+
+    echo "::group::Comparing $a vs. $b"
+
+    histcmp $@
+
+    ec=$(($ec | $?))
+
+    echo "::endgroup::"
+}
+
+
+
+run \
     histcmp_demo/performance_ckf.root \
     histcmp_demo/performance_ckf_main.root \
     -o $outdir/ckf.html \
 
-ec=$(($ec | $?))
 
-histcmp \
+run \
     histcmp_demo/performance_track_fitter_a.root \
     histcmp_demo/performance_track_fitter_b.root \
     -c track_fitter.yml \
     -o $outdir/track_fitter.html \
-
-ec=$(($ec | $?))
 
 
 exit $ec
