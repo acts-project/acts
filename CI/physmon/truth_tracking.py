a23e102512046ad182fad07af1e6e73c39b8d149
Paul Gessinger <hello@paulgessinger.com>
2022-03-08 18:24:18 +0100

    chain

---
+++ CI/physmon/truth_tracking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from truth_tracking import runTruthTracking
+
+#  srcdir = Path(__file__).resolve().parent.parent.parent.parent
+
+#  # detector, trackingGeometry, _ = getOpenDataDetector()
+#  detector, trackingGeometry, decorators = acts.examples.GenericDetector.create()
+
+#  field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
+
+#  runTruthTracking(
+#  trackingGeometry,
+#  field,
+#  digiConfigFile=srcdir
+#  / "Examples/Algorithms/Digitization/share/default-smearing-config-generic.json",
+#  # "thirdparty/OpenDataDetector/config/odd-digi-smearing-config.json",
+#  outputDir=Path.cwd(),
+#  ).run()
