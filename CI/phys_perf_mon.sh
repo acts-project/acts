75eb12a673ec813cbf2b6eb5f691a050bf60e9b5
Paul Gessinger <hello@paulgessinger.com>
2022-03-08 11:35:20 +0100

    accumulate ec

--- CI/phys_perf_mon.sh
+++ CI/phys_perf_mon.sh
@@ -12,16 +12,19 @@ set +e
 ec=0
 
 histcmp \
+    histcmp_demo/performance_ckf.root \
+    histcmp_demo/performance_ckf_main.root \
+    -o $outdir/ckf.html \
+
+ec=$(($ec | $?))
+
+histcmp \
     histcmp_demo/performance_track_fitter_a.root \
     histcmp_demo/performance_track_fitter_b.root \
     -c track_fitter.yml \
     -o $outdir/track_fitter.html \
-ec=((ec | $?))
 
-histcmp \
-    histcmp_demo/performance_ckf.root \
-    histcmp_demo/performance_ckf_main.root \
-    -o $outdir/ckf.html \
-ec=((ec | $?))
+ec=$(($ec | $?))
+
 
 exit $ec
