749f4c392839cbb1934b365ca96124230bc97975
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2022-03-10 14:52:03 +0100

    add test event data

--- Examples/Scripts/Python/itk_seeding.py
+++ Examples/Scripts/Python/itk_seeding.py
@@ -19,7 +19,7 @@ def runITkSeeding(field, outputDir, s=None):
         level=acts.logging.INFO,
         inputStem="spacepoints",
         inputCollection="pixel",
-        inputDir="/Users/luiscoelho/Public/Documents/lcoelho/acts/data/CsvSpacePointsOutput_singleMu_100GeV_300evnts/",
+        inputDir="/acts/Examples/Scripts/Python/CsvSpacePointsOutput_singleMu_100GeV_300evnts/",
         outputSpacePoints="PixelSpacePoints",
     )
 
