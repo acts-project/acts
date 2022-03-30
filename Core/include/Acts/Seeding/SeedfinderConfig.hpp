80061cc2dd2ef3623847cbf7254253bbb63b549f
Luis Falda Coelho <56648068+LuisFelipeCoelho@users.noreply.github.com>
2022-03-30 16:31:28 +0200

    feat: Custom z bin search order (#1169)
    This PR introduces a custom z looping feature, 
    which allows us to change the order in which `BinnedSPGroup` search for the Space Points.
    
    In the Main branch the groups are sorted in **phi** and **z** in ascending order. But in Athena **z** follows the order within the vector `{1, 2, 3, 4, 11, 10, 9, 8, 6, 5, 7}`
    
    This PR introduces this same custom looping approach with the vector `zBinsCustomLooping`. If it is not defined, the previous behaviour is preserved.
    
    @noemina @paulgessinger @robertlangenberg

--- Core/include/Acts/Seeding/SeedfinderConfig.hpp
+++ Core/include/Acts/Seeding/SeedfinderConfig.hpp
@@ -119,6 +119,8 @@ struct SeedfinderConfig {
   Acts::Vector2 beamPos{0 * Acts::UnitConstants::mm,
                         0 * Acts::UnitConstants::mm};
 
+  boost::container::small_vector<size_t, 20> zBinsCustomLooping = {};
+
   // average radiation lengths of material on the length of a seed. used for
   // scattering.
   // default is 5%
--- Examples/Python/src/TrackFinding.cpp
+++ Examples/Python/src/TrackFinding.cpp
@@ -109,6 +109,7 @@ void addTrackFinding(Context& ctx) {
     ACTS_PYTHON_MEMBER(enableCutsForSortedSP);
     ACTS_PYTHON_MEMBER(interactionPointCut);
     ACTS_PYTHON_MEMBER(zBinEdges);
+    ACTS_PYTHON_MEMBER(zBinsCustomLooping);
     ACTS_PYTHON_MEMBER(rRangeMiddleSP);
     ACTS_PYTHON_MEMBER(useVariableMiddleSPRange);
     ACTS_PYTHON_MEMBER(deltaRMiddleSPRange);
