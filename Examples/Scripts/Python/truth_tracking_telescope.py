4276277587067384deb97bdc061de82a55c4a1aa
RFarkas <r4lv@peaxels.com>
2022-04-06 12:49:36 +0200

    FIX: python formatting…

--- Examples/Scripts/Python/truth_tracking_telescope.py
+++ Examples/Scripts/Python/truth_tracking_telescope.py
@@ -16,8 +16,7 @@ if "__main__" == __name__:
     # detector, trackingGeometry, _ = getOpenDataDetector()
     # detector, trackingGeometry, decorators = acts.examples.GenericDetector.create()
     detector, trackingGeometry, decorators = acts.examples.TelescopeDetector.create(
-        bounds=[200, 200],
-        positions=[30, 60, 90, 120, 150, 180, 210, 240, 270]
+        bounds=[200, 200], positions=[30, 60, 90, 120, 150, 180, 210, 240, 270]
     )
 
     field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
