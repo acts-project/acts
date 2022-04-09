73cdf4e7dea7f83cd37994ecd224f5879382b49c
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2022-04-09 10:02:54 +0200

    set default values in SpacePoint

--- Tests/UnitTests/Plugins/Cuda/Seeding/SpacePoint.hpp
+++ Tests/UnitTests/Plugins/Cuda/Seeding/SpacePoint.hpp
@@ -16,10 +16,30 @@ struct SpacePoint {
   int surface;
   float varianceR;
   float varianceZ;
+  float m_topHalfStripLength = 0;
+  float m_bottomHalfStripLength = 0;
+  Acts::Vector3 m_topStripDirection = {0, 0, 0};
+  Acts::Vector3 m_bottomStripDirection = {0, 0, 0};
+  Acts::Vector3 m_stripCenterDistance = {0, 0, 0};
+  Acts::Vector3 m_bottomStripCenterPosition = {0, 0, 0};
+  bool m_validDoubleMeasurementDetails = false;
   float x() const { return m_x; }
   float y() const { return m_y; }
   float z() const { return m_z; }
   float r() const { return m_r; }
+  constexpr float topHalfStripLength() const { return m_topHalfStripLength; }
+  constexpr float bottomHalfStripLength() const {
+    return m_bottomHalfStripLength;
+  }
+  Acts::Vector3 topStripDirection() const { return m_topStripDirection; }
+  Acts::Vector3 bottomStripDirection() const { return m_bottomStripDirection; }
+  Acts::Vector3 stripCenterDistance() const { return m_stripCenterDistance; }
+  Acts::Vector3 bottomStripCenterPosition() const {
+    return m_bottomStripCenterPosition;
+  }
+  constexpr bool validDoubleMeasurementDetails() const {
+    return m_validDoubleMeasurementDetails;
+  }
 };
 
 bool operator==(SpacePoint a, SpacePoint b) {
