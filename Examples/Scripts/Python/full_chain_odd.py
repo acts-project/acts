0d8d349c875f3c69d3a1a27198f2e3404b231f1c
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2022-04-11 13:12:35 +0200

    adding full_chain example for the ODD

---
+++ Examples/Scripts/Python/full_chain_odd.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import pathlib, acts, acts.examples
+import acts.examples.dd4hep
+from common import getOpenDataDetector, getOpenDataDetectorDirectory
+
+u = acts.UnitConstants
+outputDir = pathlib.Path.cwd()
+
+oddDir = str(getOpenDataDetectorDirectory())
+
+oddMaterialMap = oddDir + '/data/odd-material-maps.root'
+oddDigiConfig = oddDir + '/config/odd-digi-smearing-config.json'
+oddSeedingSel = oddDir + '/config/odd-seeding-config.json'
+oddMaterialDeco = acts.IMaterialDecorator.fromFile(oddMaterialMap)
+
+detector, trackingGeometry, decorators = getOpenDataDetector(mdecorator=oddMaterialDeco)
+field = acts.ConstantBField(acts.Vector3(0.0, 0.0, 2.0 * u.T))
+rnd = acts.examples.RandomNumbers(seed=42)
+
+from particle_gun import addParticleGun, MomentumConfig, EtaConfig, ParticleConfig
+from fatras import addFatras
+from digitization import addDigitization
+from seeding import addSeeding, SeedingAlgorithm, TruthSeedRanges
+from ckf_tracks import addCKFTracks
+
+s = acts.examples.Sequencer(events=100, numThreads=-1)
+s = addParticleGun(
+    s,
+    MomentumConfig(1.0 * u.GeV, 10.0 * u.GeV, True),
+    EtaConfig(-4.0, 4.0, True),
+    ParticleConfig(1, acts.PdgParticle.eMuon, True),
+    rnd=rnd,
+)
+s = addFatras(
+    s,
+    trackingGeometry,
+    field,
+    outputDirRoot=outputDir,
+    rnd=rnd,
+)
+s = addDigitization(
+    s,
+    trackingGeometry,
+    field,
+    digiConfigFile=oddDigiConfig,
+    outputDirRoot=outputDir,
+    rnd=rnd,
+)
+s = addSeeding(
+    s,
+    trackingGeometry,
+    field,
+    TruthSeedRanges(pt=(1.0 * u.GeV, None), eta=(-4.0, 4.0), nHits=(9, None)),
+    geoSelectionConfigFile=oddSeedingSel,
+    outputDirRoot=outputDir,
+)
+s = addCKFTracks(
+    s,
+    trackingGeometry,
+    field,
+    TruthSeedRanges(pt=(400.0 * u.MeV, None), nHits=(6, None)),
+    outputDirRoot=outputDir,
+)
+
+s.run()
