bc795749da580a65205755fa91720a884406b947
Tomohiro Yamazaki <tomohiro.yamazaki@cern.ch>
2022-04-21 17:55:59 +0900

    revert identification changes

--- Plugins/TGeo/include/Acts/Plugins/TGeo/ITGeoIdentifierProvider.hpp
+++ Plugins/TGeo/include/Acts/Plugins/TGeo/ITGeoIdentifierProvider.hpp
@@ -7,7 +7,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "Acts/Geometry/GeometryContext.hpp"
-#include "Acts/Identification/Identifier.hpp"
+#include "Acts/Plugins/Identification/Identifier.hpp"
 
 class TGeoNode;
 
