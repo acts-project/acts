e8d18793b9e8ef8fed8fdf4b93d09a4131541b02
xai <xiaocong.ai@cern.ch>
2022-04-22 00:11:26 +0200

    update KF

--- Core/include/Acts/TrackFitting/KalmanFitter.hpp
+++ Core/include/Acts/TrackFitting/KalmanFitter.hpp
@@ -107,6 +107,7 @@ struct KalmanFitterOptions {
   /// @param eLoss Whether to include energy loss
   /// @param rFiltering Whether to run filtering in reversed direction as smoothing
   /// @param rfScaling Scale factor for the covariance matrix before the backward filtering
+  /// @param gToLCorrection Whether to include non-linear correction during global to local transformation
   KalmanFitterOptions(const GeometryContext& gctx,
                       const MagneticFieldContext& mctx,
                       std::reference_wrapper<const CalibrationContext> cctx,
@@ -114,7 +115,8 @@ struct KalmanFitterOptions {
                       const PropagatorPlainOptions& pOptions,
                       const Surface* rSurface = nullptr,
                       bool mScattering = true, bool eLoss = true,
-                      bool rFiltering = false, double rfScaling = 1.0)
+                      bool rFiltering = false, double rfScaling = 1.0,
+                      bool gToLCorrection = false)
       : geoContext(gctx),
         magFieldContext(mctx),
         calibrationContext(cctx),
@@ -125,6 +127,8 @@ struct KalmanFitterOptions {
         energyLoss(eLoss),
         reversedFiltering(rFiltering),
         reversedFilteringCovarianceScaling(rfScaling),
+        globalToLocalCorrection(gToLCorrection),
+
         logger(logger_) {}
   /// Contexts are required and the options must not be default-constructible.
   KalmanFitterOptions() = delete;
@@ -159,6 +163,10 @@ struct KalmanFitterOptions {
   /// is true or if the ReverseFilteringLogic return true for the track of
   /// interest)
   double reversedFilteringCovarianceScaling = 1.0;
+
+  /// Whether to include non-linear correction during global to local
+  /// transformation
+  bool globalToLocalCorrection = false;
 
   /// Logger
   LoggerWrapper logger;
@@ -284,6 +292,10 @@ class KalmanFitter {
 
     // Scale the covariance before the reversed filtering
     double reversedFilteringCovarianceScaling = 1.0;
+
+    /// Whether to include non-linear correction during global to local
+    /// transformation
+    bool globalToLocalCorrection = false;
 
     /// @brief Kalman actor operation
     ///
@@ -412,7 +424,8 @@ class KalmanFitter {
         } else if (targetReached(state, stepper, *targetSurface)) {
           ACTS_VERBOSE("Completing with fitted track parameter");
           // Transport & bind the parameter to the final surface
-          auto res = stepper.boundState(state.stepping, *targetSurface);
+          auto res = stepper.boundState(state.stepping, *targetSurface, true,
+                                        globalToLocalCorrection);
           if (!res.ok()) {
             ACTS_ERROR("Error in " << direction << " filter: " << res.error());
             result.result = res.error();
@@ -529,7 +542,8 @@ class KalmanFitter {
         ACTS_VERBOSE("Measurement surface " << surface->geometryId()
                                             << " detected.");
         // Transport the covariance to the surface
-        stepper.transportCovarianceToBound(state.stepping, *surface);
+        stepper.transportCovarianceToBound(state.stepping, *surface,
+                                           globalToLocalCorrection);
 
         // Update state and stepper with pre material effects
         materialInteractor(surface, state, stepper,
@@ -640,7 +654,8 @@ class KalmanFitter {
         }
 
         // Transport the covariance to the surface
-        stepper.transportCovarianceToBound(state.stepping, *surface);
+        stepper.transportCovarianceToBound(state.stepping, *surface,
+                                           globalToLocalCorrection);
 
         // Update state and stepper with pre material effects
         materialInteractor(surface, state, stepper,
@@ -1018,6 +1033,8 @@ class KalmanFitter {
     kalmanActor.reversedFiltering = kfOptions.reversedFiltering;
     kalmanActor.reversedFilteringCovarianceScaling =
         kfOptions.reversedFilteringCovarianceScaling;
+    kalmanActor.globalToLocalCorrection =
+        std::move(kfOptions.globalToLocalCorrection);
     kalmanActor.extensions = std::move(kfOptions.extensions);
 
     // Run the fitter
