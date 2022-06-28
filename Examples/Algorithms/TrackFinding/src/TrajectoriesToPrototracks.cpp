ddb18b7296a9949682e35da7d3073010ca59c488
Benjamin Huth <Benjamin-Huth@t-online.de>
2022-06-28 10:59:43 +0200

    Add TrajectoriesToPrototracks Algorithm

---
+++ Examples/Algorithms/TrackFinding/src/TrajectoriesToPrototracks.cpp
@@ -0,0 +1,39 @@
+#include "ActsExamples/TrackFinding/TrajectoriesToPrototracks.hpp"
+
+#include "ActsExamples/EventData/IndexSourceLink.hpp"
+#include "ActsExamples/EventData/ProtoTrack.hpp"
+#include "ActsExamples/EventData/Trajectories.hpp"
+#include "ActsExamples/Framework/WhiteBoard.hpp"
+
+namespace ActsExamples {
+ProcessCode TrajectoriesToPrototracks::execute(
+    const AlgorithmContext& ctx) const {
+  const auto trajectories = ctx.eventStore.get<TrajectoriesContainer>(m_cfg.inputTrajectories);
+  
+  ProtoTrackContainer tracks;
+
+  for (const auto& trajectory : trajectories) {
+    for (const auto tip : trajectory.tips()) {
+      ProtoTrack track;
+      
+      trajectory.multiTrajectory().visitBackwards(tip, [&](const auto& state) {
+        if (not state.typeFlags().test(Acts::TrackStateFlag::MeasurementFlag)) {
+          return true;
+        }
+        
+        const auto& source_link =
+            static_cast<const IndexSourceLink&>(state.uncalibrated());
+        track.push_back(source_link.index());
+        
+        return true;
+      });
+      
+      tracks.push_back(track);
+    }
+  }
+  
+  ctx.eventStore.add(m_cfg.outputPrototracks, std::move(tracks));
+  
+  return ProcessCode::SUCCESS;
+}
+}  // namespace ActsExamples
