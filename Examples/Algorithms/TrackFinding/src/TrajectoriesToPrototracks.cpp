269ff9e2b8719f6ef1a9407d4efb624282954d01
Benjamin Huth <Benjamin-Huth@t-online.de>
2022-06-28 11:35:43 +0200

    clang-format

--- Examples/Algorithms/TrackFinding/src/TrajectoriesToPrototracks.cpp
+++ Examples/Algorithms/TrackFinding/src/TrajectoriesToPrototracks.cpp
@@ -6,34 +6,36 @@
 #include "ActsExamples/Framework/WhiteBoard.hpp"
 
 namespace ActsExamples {
+  
 ProcessCode TrajectoriesToPrototracks::execute(
     const AlgorithmContext& ctx) const {
-  const auto trajectories = ctx.eventStore.get<TrajectoriesContainer>(m_cfg.inputTrajectories);
-  
+  const auto trajectories =
+      ctx.eventStore.get<TrajectoriesContainer>(m_cfg.inputTrajectories);
+
   ProtoTrackContainer tracks;
 
   for (const auto& trajectory : trajectories) {
     for (const auto tip : trajectory.tips()) {
       ProtoTrack track;
-      
+
       trajectory.multiTrajectory().visitBackwards(tip, [&](const auto& state) {
         if (not state.typeFlags().test(Acts::TrackStateFlag::MeasurementFlag)) {
           return true;
         }
-        
+
         const auto& source_link =
             static_cast<const IndexSourceLink&>(state.uncalibrated());
         track.push_back(source_link.index());
-        
+
         return true;
       });
-      
+
       tracks.push_back(track);
     }
   }
-  
+
   ctx.eventStore.add(m_cfg.outputPrototracks, std::move(tracks));
-  
+
   return ProcessCode::SUCCESS;
 }
 }  // namespace ActsExamples
