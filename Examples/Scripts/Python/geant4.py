3baa16c61eddded3e8d77e6be09a138f1dcfa3de
andiwand <stefl.andreas@gmail.com>
2022-07-21 19:27:47 +0200

    add geant4 python example

---
+++ Examples/Scripts/Python/geant4.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+from typing import Optional, Union
+from pathlib import Path
+
+import acts
+import acts.examples
+import acts.examples.dd4hep
+from acts.examples.geant4 import Geant4Simulation, geant4SimulationConfig
+from acts.examples.geant4.dd4hep import DDG4DetectorConstruction
+from common import getOpenDataDetector
+
+u = acts.UnitConstants
+
+
+def addGeant4(
+    s: acts.examples.Sequencer,
+    geometryService: acts.examples.dd4hep.DD4hepGeometryService,
+    trackingGeometry: acts.TrackingGeometry,
+    field: acts.MagneticFieldProvider,
+    seed: Optional[int] = None,
+    preselectParticles: bool = True,
+) -> acts.examples.Sequencer:
+    """This function steers the detector simulation using Geant4
+
+    Parameters
+    ----------
+    s: Sequencer
+        the sequencer module to which we add the Geant4 steps (returned from addGeant4)
+    trackingGeometry : tracking geometry
+    field : magnetic field
+    seed : int, None
+        random number generator seed
+    """
+
+    if int(s.config.logLevel) <= int(acts.logging.DEBUG):
+        acts.examples.dump_args_calls(locals())
+
+    # Selector
+    if preselectParticles:
+        particles_selected = "particles_selected"
+        s.addAlgorithm(
+            acts.examples.ParticleSelector(
+                level=s.config.logLevel,
+                inputParticles="particles_input",
+                outputParticles=particles_selected,
+            )
+        )
+    else:
+        particles_selected = "particles_input"
+
+    g4detector = DDG4DetectorConstruction(geometryService)
+    g4conf = geant4SimulationConfig(
+        level=s.config.logLevel,
+        detector=g4detector,
+        inputParticles="particles_input",
+        trackingGeometry=trackingGeometry,
+        magneticField=field,
+    )
+    g4conf.outputSimHits = "simhits"
+    g4conf.outputParticlesInitial = "particles_initial"
+    g4conf.outputParticlesFinal = "particles_final"
+    g4conf.seed = seed
+
+    # Simulation
+    alg = Geant4Simulation(
+        level=s.config.logLevel,
+        config=g4conf,
+    )
+
+    # Sequencer
+    s.addAlgorithm(alg)
+
+    return s
+
+
+def runGeant4(
+    geometryService, trackingGeometry, field, s: acts.examples.Sequencer = None
+):
+    from particle_gun import addParticleGun, EtaConfig
+
+    s = s or acts.examples.Sequencer(events=100, numThreads=1)
+    s.config.logLevel = acts.logging.INFO
+    rnd = acts.examples.RandomNumbers()
+    seed = 42
+    s = addParticleGun(
+        s,
+        EtaConfig(-2.0, 2.0),
+        rnd=rnd,
+    )
+    return addGeant4(
+        s,
+        geometryService,
+        trackingGeometry,
+        field,
+        seed=seed,
+    )
+
+
+if "__main__" == __name__:
+    detector, trackingGeometry, decorators = getOpenDataDetector()
+
+    field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
+
+    runGeant4(detector.geometryService, trackingGeometry, field).run()
