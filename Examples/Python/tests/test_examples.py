57c25eb4a5dc642f97aaefa816e21b834213f42e
andiwand <stefl.andreas@gmail.com>
2022-07-28 17:09:13 +0200

    add python test

--- Examples/Python/tests/test_examples.py
+++ Examples/Python/tests/test_examples.py
@@ -135,6 +135,57 @@ def test_fatras(trk_geo, tmp_path, field, assert_root_hash):
         assert_root_hash(f, rfp)
 
 
+def test_geant4(tmp_path, field, assert_root_hash):
+    from geant4 import runGeant4
+
+    detector, trackingGeometry, decorators = getOpenDataDetector(
+        getOpenDataDetectorDirectory()
+    )
+
+    csv = tmp_path / "csv"
+    csv.mkdir()
+
+    nevents = 10
+
+    root_files = [
+        (
+            "fatras_particles_final.root",
+            "particles",
+            nevents,
+        ),
+        (
+            "fatras_particles_initial.root",
+            "particles",
+            nevents,
+        ),
+        (
+            "hits.root",
+            "hits",
+            115,
+        ),
+    ]
+
+    assert len(list(csv.iterdir())) == 0
+    for rf, _, _ in root_files:
+        assert not (tmp_path / rf).exists()
+
+    seq = Sequencer(events=nevents)
+    runGeant4(detector.geometryService, trackingGeometry, field, str(tmp_path), s=seq).run()
+
+    del seq
+
+    assert_csv_output(csv, "particles_final")
+    assert_csv_output(csv, "particles_initial")
+    assert_csv_output(csv, "hits")
+    for f, tn, exp_entries in root_files:
+        rfp = tmp_path / f
+        assert rfp.exists()
+        assert rfp.stat().st_size > 2**10 * 10
+
+        assert_entries(rfp, tn, exp_entries)
+        assert_root_hash(f, rfp)
+
+
 def test_seeding(tmp_path, trk_geo, field, assert_root_hash):
     from seeding import runSeeding
 
