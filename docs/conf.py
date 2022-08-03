5bf6fb83f05bcdc0e69193684f2b58d99738a016
Paul Gessinger <paul.gessinger@cern.ch>
2022-08-03 16:20:03 +0200

    update docs setup

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 
 import os
-
-from m2r import MdInclude
-from recommonmark.transform import AutoStructify
+import sys
+import subprocess
 
 # check if we are running on readthedocs.org
 on_readthedocs = os.environ.get("READTHEDOCS", None) == "True"
@@ -12,7 +11,7 @@ on_readthedocs = os.environ.get("READTHEDOCS", None) == "True"
 
 project = "Acts"
 author = "The Acts authors"
-copyright = "2014–2020 CERN for the benefit of the Acts project"
+copyright = "2014–2022 CERN for the benefit of the Acts project"
 # version = '@PROJECT_VERSION@'
 # release = '@PROJECT_VERSION@'
 
@@ -20,9 +19,8 @@ copyright = "2014–2020 CERN for the benefit of the Acts project"
 
 extensions = [
     "breathe",
-    "recommonmark",
+    "myst_parser",
     "sphinx.ext.mathjax",
-    "sphinx_markdown_tables",
 ]
 source_suffix = {
     ".rst": "restructuredtext",
@@ -39,20 +37,19 @@ highlight_language = "cpp"
 smartquotes = True
 numfig = True
 
+myst_enable_extensions = ["dollarmath", "colon_fence"]
+myst_heading_anchors = 3
+
 # -- Options for HTML output --------------------------------------------------
 
 # ensure we use the RTD them when building locally
 if not on_readthedocs:
-    import sphinx_rtd_theme
-
     html_theme = "sphinx_rtd_theme"
-    html_theme_path = [
-        sphinx_rtd_theme.get_html_theme_path(),
-    ]
+    extensions.append("sphinx_rtd_theme")
 
 html_theme_options = {
     "collapse_navigation": False,
-    "navigation_depth": 2,
+    "navigation_depth": 3,
     "prev_next_buttons_location": None,  # no prev/next links
     "style_external_links": True,
 }
@@ -78,50 +75,40 @@ breathe_domain_by_extension = {
     "hpp": "cpp",
     "ipp": "cpp",
 }
-breathe_default_members = [
+breathe_default_members = (
     "members",
     "undoc-members",
-]
+)
 
-# -- Automatic API documentation generation with Exhale -----------------------
+# -- Automatic API documentation ---------------------------------------------
 
-exhale_args = {
-    "containmentFolder": "api",
-    "rootFileName": "api.rst",
-    "rootFileTitle": "API",
-    "createTreeView": True,
-    "exhaleUseDoxyfile": True,
-    # note: OUTPUT_DIRECTORY in Doxyfile must match breathe default project path
-    # note: this must match STRIP_FROM_PATH in Doxyfile
-    "doxygenStripFromPath": "..",
-}
+env = os.environ.copy()
+env["DOXYGEN_WARN_AS_ERROR"] = "NO"
+cwd = os.path.dirname(__file__)
 
-if on_readthedocs:
+if on_readthedocs or tags.has("run_doxygen"):
     # if we are running on RTD Doxygen must be run as part of the build
-    # let exhale handle the doxygen execution
-    extensions.append("exhale")
-    exhale_args["exhaleExecutesDoxygen"] = True
-elif tags.has("use_exhale"):
-    # if exhale is requested manually, we expect the Doxygen has been run for us
-    extensions.append("exhale")
-    exhale_args["exhaleExecutesDoxygen"] = False
+    print("Executing doxygen in", cwd)
+    print(
+        "Doxygen version:",
+        subprocess.check_output(["doxygen", "--version"], encoding="utf-8"),
+    )
+    sys.stdout.flush()
+    subprocess.check_call(
+        ["doxygen", "Doxyfile"], stdout=subprocess.PIPE, cwd=cwd, env=env
+    )
+
+if on_readthedocs or tags.has("run_apidoc"):
+    print("Executing breathe apidoc in", cwd)
+    subprocess.check_call(
+        [sys.executable, "-m", "breathe.apidoc", "_build/doxygen-xml", "-o", "api"],
+        stdout=subprocess.PIPE,
+        cwd=cwd,
+        env=env,
+    )
 
 # -- Markdown bridge setup hook (must come last, not sure why) ----------------
 
 
 def setup(app):
-    app.add_config_value(
-        "recommonmark_config",
-        {
-            "enable_math": True,
-            "enable_inline_math": True,
-        },
-        True,
-    )
-    app.add_transform(AutoStructify)
-
-    app.add_config_value("no_underscore_emphasis", False, "env")
-    app.add_config_value("m2r_parse_relative_links", False, "env")
-    app.add_config_value("m2r_anonymous_references", False, "env")
-    app.add_config_value("m2r_disable_inline_math", False, "env")
-    app.add_directive("mdinclude", MdInclude)
+    pass
