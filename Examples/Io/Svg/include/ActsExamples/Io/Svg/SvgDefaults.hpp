dc39ccda692a99259907f9246ffc3260b391698c
Andreas Salzburger <Andreas.Salzburger@cern.ch>
2022-08-10 11:05:09 +0200

    adding event data converter

--- Examples/Io/Svg/include/ActsExamples/Io/Svg/SvgDefaults.hpp
+++ Examples/Io/Svg/include/ActsExamples/Io/Svg/SvgDefaults.hpp
@@ -27,6 +27,30 @@ static inline Acts::Svg::Style layerStyle() {
   return defaultStyle;
 };
 
+static inline Acts::Svg::Style backgroundStyle() {
+  Acts::Svg::Style bgStyle;
+  bgStyle.fillColor = {55, 55, 55};
+  bgStyle.fillOpacity = 0.50;
+  bgStyle.highlights = {};
+  bgStyle.strokeColor = {25, 25, 25};
+  bgStyle.strokeWidth = 0.5;
+  bgStyle.nSegments = 72u;
+  return bgStyle;
+};
+
+static inline Acts::Svg::Style pointStyle() {
+  Acts::Svg::Style pStyle;
+  pStyle.fillColor = {200, 0, 0};
+  pStyle.fillOpacity = 1.0;
+  pStyle.highlightColor = {0, 200, 0};
+  pStyle.highlights = {"mouseover", "mouseout"};
+  pStyle.strokeColor = {0, 0, 0};
+  pStyle.strokeWidth = 0.5;
+  pStyle.nSegments = 72u;
+
+  return pStyle;
+};
+
 static inline Acts::Svg::TrackingGeometryConverter::Options
 trackingGeometryOptions() {
   Acts::GeometryIdentifier geoID(0);
@@ -46,12 +70,41 @@ trackingGeometryOptions() {
   return tgOptions;
 };
 
+static inline Acts::Svg::TrackingGeometryConverter::Options
+backgroundGeometryOptions() {
+  Acts::GeometryIdentifier geoID(0);
+
+  Acts::Svg::LayerConverter::Options lOptions;
+
+  lOptions.name = "layer";
+  lOptions.surfaceStyles = Acts::GeometryHierarchyMap<Acts::Svg::Style>(
+      {{geoID, backgroundStyle()}});
+  lOptions.moduleInfo = false;
+  lOptions.gridInfo = false;
+  lOptions.zRange = {-100, 100};
+  lOptions.phiRange = {-0.2, 0.2};
+
+  Acts::Svg::TrackingGeometryConverter::Options tgOptions;
+  tgOptions.prefix = "";
+  tgOptions.layerOptions =
+      Acts::GeometryHierarchyMap<Acts::Svg::LayerConverter::Options>(
+          {{geoID, lOptions}});
+
+  return tgOptions;
+};
+
 }  // namespace
 
 namespace ActsExamples {
 
 static Acts::Svg::Style s_defaultLayerStyle = layerStyle();
 
+static Acts::Svg::Style s_pointStyle = pointStyle();
+
 static Acts::Svg::TrackingGeometryConverter::Options
     s_defaultTrackingGeometryOptions = trackingGeometryOptions();
+
+static Acts::Svg::TrackingGeometryConverter::Options
+    s_backgroundTrackingGeometryOptions = backgroundGeometryOptions();
+
 }  // namespace ActsExamples
