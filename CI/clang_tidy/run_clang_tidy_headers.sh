2113e035c0a1665af491b679695c606adaa05709
Paul Gessinger <paul.gessinger@cern.ch>
2022-08-15 13:26:06 +0200

    improve header mode

--- CI/clang_tidy/run_clang_tidy_headers.sh
+++ CI/clang_tidy/run_clang_tidy_headers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -name "*.hpp" -print0 | parallel -0 clang-tidy -p build {} 2>/dev/null
+find . -type f \( -name "*.hpp" -or -name "*.ipp" \) -and -not -path "*build*" -and -not -path "*install*" -print0 | parallel -0 clang-tidy -p build {} 2>/dev/null
