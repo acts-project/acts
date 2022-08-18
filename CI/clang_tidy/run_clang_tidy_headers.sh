039e59fbe9bda045c900bccabc0541e0e0dab43e
Paul Gessinger <hello@paulgessinger.com>
2022-08-18 11:19:57 +0200

    ignore failed clang-tidy executions

--- CI/clang_tidy/run_clang_tidy_headers.sh
+++ CI/clang_tidy/run_clang_tidy_headers.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-find . -type f \( -name "*.hpp" -or -name "*.ipp" \) -and -not -path "*build*" -and -not -path "*install*" -print0 | parallel -0 clang-tidy -p build {} 2>/dev/null
+function execute() {
+    clang-tidy "$@" || true
+}
+
+export -f execute
+
+find . -type f \( -name "*.hpp" -or -name "*.ipp" \) -and -not -path "*build*" -and -not -path "*install*" -print0 | parallel -0 execute -p build {} 2>/dev/null
