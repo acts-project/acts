ce8bf76cd0503055361925b9519cb7716cc88791
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2022-09-16 12:21:55 +0200

    feat: Exa.TrkX with torchscript backend (#1473)
    This PR is a big update to the Exa.TrkX plugin:
    
    * It now provides both a TorchScript-based and a ONNX-based backend (the ONNX one doesn't really work at the moment due to missing operators in Onnx, however, we should'nt throw away the code for this I think)
    * Partial integration of Acts logging instead of `std::cout` based things
    * The `TrajectoriesToProtoTracks` algorithm to be able to compare results of the CKF and the Exa.TrkkX track finding
    * Update python to support all of this
    * The dependency to FRNN is now managed by CMake's `FetchContent` mechanism
    * The CI routines are restrucutured
      * no build in the github workflows anymore
      * Build and check both backends in the CI bridge
    
    Co-authored-by: Andreas Stefl <487211+andiwand@users.noreply.github.com>

--- Plugins/ExaTrkX/include/Acts/Plugins/ExaTrkX/ExaTrkXTrackFindingOnnx.hpp
+++ Plugins/ExaTrkX/include/Acts/Plugins/ExaTrkX/ExaTrkXTrackFindingOnnx.hpp
@@ -8,22 +8,29 @@
 
 #pragma once
 
+#include "Acts/Plugins/ExaTrkX/ExaTrkXTrackFindingBase.hpp"
+
 #include <memory>
 #include <string>
 #include <vector>
 
-#include <core/session/onnxruntime_cxx_api.h>
+namespace Ort {
+class Env;
+class Session;
+class Value;
+}  // namespace Ort
 
 namespace Acts {
 
-/// Class implementing the Exa.TrkX track finding algorithm.
-/// It holds the required ONNX objects.
-class ExaTrkXTrackFinding {
+/// @class ExaTrkXTrackFindingOnnx
+///
+/// Class implementing the Exa.TrkX track finding algorithm based on ONNX
+class ExaTrkXTrackFindingOnnx final : public ExaTrkXTrackFindingBase {
  public:
   /// Configuration struct for the track finding.
   struct Config {
     // input model directory
-    std::string inputMLModuleDir;
+    std::string modelDir;
 
     // hyperparameters in the pipeline.
     int64_t spacepointFeatures = 3;
@@ -35,23 +42,27 @@ class ExaTrkXTrackFinding {
 
   /// Constructor of the track finding module
   ///
-  /// @param cfg is the config struct to configure the module
-  ExaTrkXTrackFinding(const Config& config);
+  /// @param config is the config struct to configure the module
+  ExaTrkXTrackFindingOnnx(const Config& config);
 
-  virtual ~ExaTrkXTrackFinding() {}
+  /// Destructor
+  ~ExaTrkXTrackFindingOnnx();
 
-  /// Do the track finding
+  /// Run the inference
   ///
-  /// @param [in] input_values Packed spacepoints in the form
+  /// @param inputValues tPacked spacepoints in the form
   /// [ r1, phi1, z1, r2, phi2, z2, ... ]
-  /// @param [in] spacepointIDs corresponding spacepoint IDs to the input_values.
-  /// @param [out] trackCandidates nested vector containing the spacepoint ids
-  /// of the found tracks
-  /// @note The input values are not const, because the underlying ONNX API
+  /// @param spacepointIDs The corresponding spacepoint spacepoint spacepointIDs
+  /// @param trackCandidates This vector is filled with the tracks as vectors of spacepoint spacepoint IDs
+  /// @param logger If provided, logging is enabled
+  /// @param recordTiming If enabled, returns a @class ExaTrkXTime object with measured timings
+  /// @note The input values are not const, because the ONNX API
   /// takes only non-const pointers.
-  void getTracks(std::vector<float>& input_values,
-                 std::vector<uint32_t>& spacepointIDs,
-                 std::vector<std::vector<uint32_t> >& trackCandidates) const;
+  std::optional<ExaTrkXTime> getTracks(
+      std::vector<float>& inputValues, std::vector<int>& spacepointIDs,
+      std::vector<std::vector<int> >& trackCandidates,
+      LoggerWrapper logger = getDummyLogger(),
+      bool recordTiming = false) const override;
 
   /// Return the configuration object of the track finding module
   const Config& config() const { return m_cfg; }
@@ -69,9 +80,9 @@ class ExaTrkXTrackFinding {
  private:
   Config m_cfg;
   std::unique_ptr<Ort::Env> m_env;
-  std::unique_ptr<Ort::Session> e_sess;
-  std::unique_ptr<Ort::Session> f_sess;
-  std::unique_ptr<Ort::Session> g_sess;
+  std::unique_ptr<Ort::Session> m_embeddingSession;
+  std::unique_ptr<Ort::Session> m_filterSession;
+  std::unique_ptr<Ort::Session> m_gnnSession;
 };
 
 }  // namespace Acts
