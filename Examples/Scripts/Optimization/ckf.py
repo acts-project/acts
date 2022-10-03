bb7c6496d304da65c49319ec73bd6f6b980adbb7
Paul Gessinger <paul.gessinger@cern.ch>
2022-10-03 10:13:40 +0200

    feat: Added Optimization Scripts [backport of #1541 to develop/v19.x] (#1563)
    Backport 8fbf7de9e82022c6fd1415427ca98a7c952e9c1d from #1541
    ---
    The Optuna and Orion optimization algorithms are used to auto-tune some parameters of seeding algorithm and performance has been checked on the CKF algorithm.
    
    Also includes #1560

---
+++ Examples/Scripts/Optimization/ckf.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from typing import Optional, Union
+from collections import namedtuple
+import argparse
+import sys
+import os
+
+from acts.examples import Sequencer, GenericDetector, RootParticleReader
+
+import acts
+
+from acts import UnitConstants as u
+
+
+def getArgumentParser():
+    """Get arguments from command line"""
+    parser = argparse.ArgumentParser(description="Command line arguments for CKF")
+    parser.add_argument(
+        "-i",
+        "--indir",
+        dest="indir",
+        help="Directory with input root files",
+        default="./",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="outdir",
+        help="Output directory for new ntuples",
+        default="./",
+    )
+    parser.add_argument(
+        "-n", "--nEvents", dest="nEvts", help="Number of events to run over", default=1
+    )
+    parser.add_argument(
+        "--sf_maxSeedsPerSpM",
+        dest="sf_maxSeedsPerSpM",
+        help="Number of compatible seeds considered for middle seed",
+        type=int,
+        default=1,
+    )
+    parser.add_argument(
+        "--sf_cotThetaMax",
+        dest="sf_cotThetaMax",
+        help="cot of maximum theta angle",
+        type=float,
+        default=7.40627,
+    )
+    parser.add_argument(
+        "--sf_sigmaScattering",
+        dest="sf_sigmaScattering",
+        help="How many sigmas of scattering to include in seeds",
+        type=float,
+        default=5,
+    )
+    parser.add_argument(
+        "--sf_radLengthPerSeed",
+        dest="sf_radLengthPerSeed",
+        help="Average Radiation Length",
+        type=float,
+        default=0.1,
+    )
+    parser.add_argument(
+        "--sf_impactMax",
+        dest="sf_impactMax",
+        help="max impact parameter in mm",
+        type=float,
+        default=3.0,
+    )
+    parser.add_argument(
+        "--sf_maxPtScattering",
+        dest="sf_maxPtScattering",
+        help="maximum Pt for scattering cut in GeV",
+        type=float,
+        default=10.0,
+    )
+    parser.add_argument(
+        "--sf_deltaRMin",
+        dest="sf_deltaRMin",
+        help="minimum value for deltaR separation in mm",
+        type=float,
+        default=1.0,
+    )
+    parser.add_argument(
+        "--sf_deltaRMax",
+        dest="sf_deltaRMax",
+        help="maximum value for deltaR separation in mm",
+        type=float,
+        default=60.0,
+    )
+
+    return parser
+
+
+def runCKFTracks(
+    trackingGeometry,
+    decorators,
+    geometrySelection: Path,
+    digiConfigFile: Path,
+    field,
+    outputDir: Path,
+    NumEvents=1,
+    truthSmearedSeeded=False,
+    truthEstimatedSeeded=False,
+    outputCsv=True,
+    inputParticlePath: Optional[Path] = None,
+    s=None,
+    MaxSeedsPerSpM=1,
+    CotThetaMax=7.40627,
+    SigmaScattering=5,
+    RadLengthPerSeed=0.1,
+    ImpactMax=3.0,
+    MaxPtScattering=10.0,
+    DeltaRMin=1.0,
+    DeltaRMax=60.0,
+):
+
+    from acts.examples.simulation import (
+        addParticleGun,
+        EtaConfig,
+        PhiConfig,
+        ParticleConfig,
+        addFatras,
+        addDigitization,
+    )
+
+    from acts.examples.reconstruction import (
+        addSeeding,
+        TruthSeedRanges,
+        ParticleSmearingSigmas,
+        SeedfinderConfigArg,
+        SeedingAlgorithm,
+        TrackParamsEstimationConfig,
+        CKFPerformanceConfig,
+        addCKFTracks,
+    )
+
+    s = s or acts.examples.Sequencer(
+        events=int(NumEvents),
+        numThreads=-1,
+        logLevel=acts.logging.INFO,
+        outputDir=outputDir,
+    )
+    for d in decorators:
+        s.addContextDecorator(d)
+    rnd = acts.examples.RandomNumbers(seed=42)
+    outputDir = Path(outputDir)
+
+    if inputParticlePath is None:
+        addParticleGun(
+            s,
+            EtaConfig(-2.0, 2.0),
+            ParticleConfig(4, acts.PdgParticle.eMuon, True),
+            PhiConfig(0.0, 360.0 * u.degree),
+            multiplicity=2,
+            rnd=rnd,
+        )
+    else:
+        acts.logging.getLogger("CKFExample").info(
+            "Reading particles from %s", inputParticlePath.resolve()
+        )
+        assert inputParticlePath.exists()
+        s.addReader(
+            RootParticleReader(
+                level=acts.logging.INFO,
+                filePath=str(inputParticlePath.resolve()),
+                particleCollection="particles_input",
+                orderedEvents=False,
+            )
+        )
+
+    addFatras(
+        s,
+        trackingGeometry,
+        field,
+        rnd=rnd,
+    )
+
+    addDigitization(
+        s,
+        trackingGeometry,
+        field,
+        digiConfigFile=digiConfigFile,
+        rnd=rnd,
+    )
+
+    addSeeding(
+        s,
+        trackingGeometry,
+        field,
+        TruthSeedRanges(pt=(500.0 * u.MeV, None), nHits=(9, None)),
+        ParticleSmearingSigmas(pRel=0.01),  # only used by SeedingAlgorithm.TruthSmeared
+        SeedfinderConfigArg(
+            r=(None, 200 * u.mm),  # rMin=default, 33mm
+            deltaR=(DeltaRMin * u.mm, DeltaRMax * u.mm),
+            collisionRegion=(-250 * u.mm, 250 * u.mm),
+            z=(-2000 * u.mm, 2000 * u.mm),
+            maxSeedsPerSpM=MaxSeedsPerSpM,
+            cotThetaMax=CotThetaMax,
+            sigmaScattering=SigmaScattering,
+            radLengthPerSeed=RadLengthPerSeed,
+            maxPtScattering=MaxPtScattering * u.GeV,
+            minPt=500 * u.MeV,
+            bFieldInZ=1.99724 * u.T,
+            impactMax=ImpactMax * u.mm,
+        ),
+        TrackParamsEstimationConfig(deltaR=(10.0 * u.mm, None)),
+        seedingAlgorithm=SeedingAlgorithm.TruthSmeared
+        if truthSmearedSeeded
+        else SeedingAlgorithm.TruthEstimated
+        if truthEstimatedSeeded
+        else SeedingAlgorithm.Default,
+        geoSelectionConfigFile=geometrySelection,
+        outputDirRoot=outputDir,
+        rnd=rnd,  # only used by SeedingAlgorithm.TruthSmeared
+    )
+
+    addCKFTracks(
+        s,
+        trackingGeometry,
+        field,
+        CKFPerformanceConfig(ptMin=400.0 * u.MeV, nMeasurementsMin=6),
+        outputDirRoot=outputDir,
+        outputDirCsv=outputDir / "csv" if outputCsv else None,
+    )
+
+    return s
+
+
+if "__main__" == __name__:
+    options = getArgumentParser().parse_args()
+
+    Inputdir = options.indir
+    Outputdir = options.outdir
+
+    srcdir = Path(__file__).resolve().parent.parent.parent.parent
+
+    detector, trackingGeometry, decorators = GenericDetector.create()
+
+    field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
+
+    inputParticlePath = Path(Inputdir) / "pythia8_particles.root"
+    if not inputParticlePath.exists():
+        inputParticlePath = None
+
+    runCKFTracks(
+        trackingGeometry,
+        decorators,
+        field=field,
+        geometrySelection=srcdir
+        / "Examples/Algorithms/TrackFinding/share/geoSelection-genericDetector.json",
+        digiConfigFile=srcdir
+        / "Examples/Algorithms/Digitization/share/default-smearing-config-generic.json",
+        outputCsv=True,
+        truthSmearedSeeded=False,
+        truthEstimatedSeeded=False,
+        inputParticlePath=inputParticlePath,
+        outputDir=Outputdir,
+        NumEvents=options.nEvts,
+        MaxSeedsPerSpM=options.sf_maxSeedsPerSpM,
+        CotThetaMax=options.sf_cotThetaMax,
+        SigmaScattering=options.sf_sigmaScattering,
+        RadLengthPerSeed=options.sf_radLengthPerSeed,
+        ImpactMax=options.sf_impactMax,
+        MaxPtScattering=options.sf_maxPtScattering,
+        DeltaRMin=options.sf_deltaRMin,
+        DeltaRMax=options.sf_deltaRMax,
+    ).run()
