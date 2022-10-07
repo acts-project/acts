232efd0f85f54ed025a18ac69fe7b25032eb8215
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2022-10-07 09:28:38 +0200

    move extent

--- Core/include/Acts/Seeding/BinnedSPGroup.hpp
+++ Core/include/Acts/Seeding/BinnedSPGroup.hpp
@@ -272,6 +272,7 @@ class BinnedSPGroup {
       std::shared_ptr<Acts::BinFinder<external_spacepoint_t>> botBinFinder,
       std::shared_ptr<Acts::BinFinder<external_spacepoint_t>> tBinFinder,
       std::unique_ptr<SpacePointGrid<external_spacepoint_t>> grid,
+      Acts::Extent rRangeSPExtent,
       const SeedfinderConfig<external_spacepoint_t>& _config);
 
   size_t size() { return m_binnedSP->size(); }
