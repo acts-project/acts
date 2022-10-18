56a1e05963f8b59e018efbf75ffe514a2a046cb1
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2022-10-19 01:21:53 +0200

    refactor: Add macro to simplify algorithm binding (#1510)
    While working with the python bindings, I thought its quite a lot of repetive boiler plate code to add an Algorithm to the python bindings. With the help of `Boost.Preprocessor` this can be simplified quite a lot:
    
    ```c++ 
    ACTS_PYTHON_DECLARE_ALGORITHM(
        ActsExamples::FooAlgorithm,
        "FooAlgorithm", 
        inputA, inputB, output)
    ```
    
    expands to 
    
    ```c++
    {
      using Alg = ActsExamples::FooAlgorithm;
      using Config = Alg::Config;
      auto alg = py::class_<Alg, ActsExamples::BareAlgorithm, std::shared_ptr<Alg>>(
                     mex, "FooAlgorithm")
                     .def(py::init<const Config &, Acts::Logging::Level>(),
                          py::arg("config"), py::arg("level"))
                     .def_property_readonly("config", &Alg::config);
      auto c = py::class_<Config>(alg, "Config").def(py::init<>());
      ACTS_PYTHON_STRUCT_BEGIN(c, Config);
      ACTS_PYTHON_MEMBER(inputA);
      ACTS_PYTHON_MEMBER(inputB);
      ACTS_PYTHON_MEMBER(output);
      ACTS_PYTHON_STRUCT_END();
    }
    ```
    
    This supports any number of arguments due to the preprocessor loops of Boost. Since we use macros anyways here to simplify declaration, I think this could be an acceptable use case of another macro.
    
    I have this already appied for the Exa.TrkX algorithm, but wanted to wait for feedback (@paulgessinger) before doing the rest of the algorithms.

--- Examples/Io/Csv/include/ActsExamples/Io/Csv/CsvMeasurementWriter.hpp
+++ Examples/Io/Csv/include/ActsExamples/Io/Csv/CsvMeasurementWriter.hpp
@@ -65,6 +65,9 @@ class CsvMeasurementWriter final : public WriterT<MeasurementContainer> {
   /// End-of-run hook
   ProcessCode endRun() final override;
 
+  /// Get readonly access to the config parameters
+  const Config& config() const { return m_cfg; }
+
  protected:
   /// This implementation holds the actual writing method
   /// and is called by the WriterT<>::write interface
