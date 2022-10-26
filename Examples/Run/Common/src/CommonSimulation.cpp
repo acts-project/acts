44cb24675ad278c458b6daca8eaffcf39624f4da
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2022-10-26 11:13:02 +0200

    refactor: Remove dependency on Boost::program_options from Algorithms/Writers/Readers (#1610)
    This aims to be the first step into making the binaries optional to build by removing the dependencies on `Boost::program_options` from the Algorithms/Writers/Readers.
    
    The second step would be to also unwind this from the Detectors, but this is a bit more complicated, and thus I would do this step by step.
    
    A long term goal would be that only the Analysis Apps depend on Boost program options, so that just building the Examples does not need any Boost components that require binaries (since we also use `std::filesystem` now).

--- Examples/Run/Common/src/CommonSimulation.cpp
+++ Examples/Run/Common/src/CommonSimulation.cpp
@@ -15,6 +15,7 @@
 #include "ActsExamples/Io/Root/RootSimHitWriter.hpp"
 #include "ActsExamples/Options/CommonOptions.hpp"
 #include "ActsExamples/Options/ParticleGunOptions.hpp"
+#include "ActsExamples/Options/ParticleSelectorOptions.hpp"
 #include "ActsExamples/TruthTracking/ParticleSelector.hpp"
 #include "ActsExamples/Utilities/Paths.hpp"
 
@@ -34,7 +35,7 @@ void addInputOptions(ActsExamples::Options::Description& desc) {
       "Read particle input from CSV files in the given directory. If not "
       "given, particles are generated with the particle gun");
   ActsExamples::Options::addParticleGunOptions(desc);
-  ActsExamples::ParticleSelector::addOptions(desc);
+  ActsExamples::Options::addParticleSelectorOptions(desc);
 }
 
 void setupInput(
@@ -61,7 +62,7 @@ void setupInput(
   }
 
   // add additional particle selection
-  auto select = ActsExamples::ParticleSelector::readConfig(vars);
+  auto select = ActsExamples::Options::readParticleSelectorConfig(vars);
   select.inputParticles = kParticlesInput;
   select.outputParticles = kParticlesSelection;
   sequencer.addAlgorithm(
