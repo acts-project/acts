5d65b734b8a0dcc55ed792a62caab7b70896b40e
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2022-10-26 11:13:02 +0200

    refactor: Remove dependency on Boost::program_options from Algorithms/Writers/Readers (#1610)
    This aims to be the first step into making the binaries optional to build by removing the dependencies on `Boost::program_options` from the Algorithms/Writers/Readers.
    
    The second step would be to also unwind this from the Detectors, but this is a bit more complicated, and thus I would do this step by step.
    
    A long term goal would be that only the Analysis Apps depend on Boost program options, so that just building the Examples does not need any Boost components that require binaries (since we also use `std::filesystem` now).

--- Examples/Run/Common/src/TrackFindingOptions.cpp
+++ Examples/Run/Common/src/TrackFindingOptions.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "ActsExamples/TrackFinding/TrackFindingOptions.hpp"
+#include "ActsExamples/Options/TrackFindingOptions.hpp"
 
 #include "Acts/Geometry/GeometryIdentifier.hpp"
 #include "ActsExamples/Utilities/Options.hpp"
