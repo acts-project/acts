355d42a0c1354547378c4d4210a0d17852ee948a
Paul Gessinger <hello@paulgessinger.com>
2022-11-03 15:08:14 +0100

    update the template

--- CI/physmon/generate_comment.py
+++ CI/physmon/generate_comment.py
@@ -1,6 +1,97 @@
 #!/usr/bin/env python3
+
+expected_reports = {""}
+
+### CHANGE BELOW WITH CAUTION ###
+
 import sys
+import os
+from pathlib import Path, PurePath
+
+import jinja2
+
+template_source = (Path(__file__).parent / "comment_template.md").read_text()
+
+macro_source = """
+{% macro detail_block(title, check) %}
+{% if exists(check) %}
+<details>
+  <summary><b>{{ title }}</b></summary>
+  {{ caller() }}
+</details>
+{% else %}
+<b>:x: {{ title }}</b>
+{% endif %}
+{% endmacro %}
+"""
+
+template_source = macro_source + template_source
+
+template = jinja2.Template(template_source)
 
 _, artifact_dir, outfile = sys.argv
+artifact_dir = Path(artifact_dir)
+outfile = Path(outfile)
+
+artifact_url = PurePath(os.environ["ARTIFACT_URL"])
+pr_sha = os.environ["PR_SHA"]
+
 
 print("Hi from python")
+
+has_errors = False
+
+
+def exists(arg):
+    result = (artifact_dir / arg).exists()
+    global has_errors
+    if not result:
+        has_errors = True
+    return result
+
+
+def all_exist(*args):
+    result = all(exists(a) for a in args)
+    global has_errors
+    if not result:
+        has_errors = True
+    return result
+
+
+def make_url(title, target):
+    if (artifact_dir / target).exists():
+        return f"[{title}]({artifact_url/target})"
+    else:
+        global has_errors
+        has_errors = True
+        return f":x: {title}"
+
+
+def make_image(target, width):
+    file = target
+    if "?" in target:
+        file = target[: target.index("?")]
+    if exists(file):
+        return f'<img src="{artifact_url/file}?to_png=1" width="{width}"/>'
+    else:
+        global has_errors
+        has_errors = True
+        return f":framed_picture: {target} :x:"
+
+
+kwargs = dict(
+    url=artifact_url,
+    commit=pr_sha,
+    exists=exists,
+    all_exist=all_exist,
+    make_url=make_url,
+    make_image=make_image,
+    has_errors=has_errors,
+)
+
+# render once to fill `has_errors`
+template.render(**kwargs)
+
+kwargs["has_errors"] = has_errors
+
+outfile.write_text(template.render(**kwargs))
