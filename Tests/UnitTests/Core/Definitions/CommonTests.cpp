ddeb4ec29a46a2dd2da4bd3a4b64fccbeda1e737
Andreas Salzburger <asalzburger@gmail.com>
2022-11-10 13:50:41 +0100

    removing BOOST_TEST

--- Tests/UnitTests/Core/Definitions/CommonTests.cpp
+++ Tests/UnitTests/Core/Definitions/CommonTests.cpp
@@ -16,45 +16,45 @@ using namespace Acts;
 BOOST_AUTO_TEST_SUITE(CommonDefinitions)
 
 BOOST_AUTO_TEST_CASE(NavigationDirectionTests) {
-  BOOST_TEST(indexFromDirection(NavigationDirection::Backward) == 0u);
-  BOOST_TEST(indexFromDirection(NavigationDirection::Forward) == 1u);
+  BOOST_CHECK(indexFromDirection(NavigationDirection::Backward) == 0u);
+  BOOST_CHECK(indexFromDirection(NavigationDirection::Forward) == 1u);
 
-  BOOST_TEST(directionFromStepSize(-1.) == NavigationDirection::Backward);
-  BOOST_TEST(directionFromStepSize(1.) == NavigationDirection::Forward);
+  BOOST_CHECK(directionFromStepSize(-1.) == NavigationDirection::Backward);
+  BOOST_CHECK(directionFromStepSize(1.) == NavigationDirection::Forward);
 
-  BOOST_TEST(invertDirection(NavigationDirection::Backward) ==
-             NavigationDirection::Forward);
-  BOOST_TEST(invertDirection(NavigationDirection::Forward) ==
-             NavigationDirection::Backward);
+  BOOST_CHECK(invertDirection(NavigationDirection::Backward) ==
+              NavigationDirection::Forward);
+  BOOST_CHECK(invertDirection(NavigationDirection::Forward) ==
+              NavigationDirection::Backward);
 
   NavigationDirection nFwd = NavigationDirection::Forward;
   NavigationDirection nBwd = NavigationDirection::Backward;
 
-  BOOST_TEST(2. * nFwd == 2.);
-  BOOST_TEST(7 * nFwd == 7);
-  BOOST_TEST(Vector3(1., 1., 1.) * nFwd == Vector3(1., 1., 1.));
+  BOOST_CHECK(2. * nFwd == 2.);
+  BOOST_CHECK(7 * nFwd == 7);
+  BOOST_CHECK(Vector3(1., 1., 1.) * nFwd == Vector3(1., 1., 1.));
 
-  BOOST_TEST(2. * nBwd == -2.);
-  BOOST_TEST(7 * nBwd == -7);
-  BOOST_TEST(Vector3(1., 1., 1.) * nBwd == Vector3(-1., -1., -1.));
+  BOOST_CHECK(2. * nBwd == -2.);
+  BOOST_CHECK(7 * nBwd == -7);
+  BOOST_CHECK(Vector3(1., 1., 1.) * nBwd == Vector3(-1., -1., -1.));
 
   double a = 7.;
   a *= nFwd;
-  BOOST_TEST(a == 7.);
+  BOOST_CHECK(a == 7.);
   a *= nBwd;
-  BOOST_TEST(a == -7.);
+  BOOST_CHECK(a == -7.);
 
   float b = 8.;
   b *= nFwd;
-  BOOST_TEST(b == 8.);
+  BOOST_CHECK(b == 8.);
   b *= nBwd;
-  BOOST_TEST(b == -8.);
+  BOOST_CHECK(b == -8.);
 
   Vector3 c(9., 9., 9.);
   c *= nFwd;
-  BOOST_TEST(c == Vector3(9., 9., 9.));
+  BOOST_CHECK(c == Vector3(9., 9., 9.));
   c *= nBwd;
-  BOOST_TEST(c == Vector3(-9., -9., -9.));
+  BOOST_CHECK(c == Vector3(-9., -9., -9.));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
