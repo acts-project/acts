8a37a259ad72fc3a702f5f60fab2c8f6f44f7c2b
andiwand <stefl.andreas@gmail.com>
2022-11-11 15:23:05 +0100

    clean

--- CI/clang_tidy/run_clang_tidy_headers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function execute() {
-    clang-tidy --config .clang-tidy-headers "$@" || true
-}
-
-export -f execute
-
-find . -type f \( -name "*.hpp" -or -name "*.ipp" \) -and -not -path "*build*" -and -not -path "*install*" -print0 | parallel -0 execute -p build {} 2>/dev/null
