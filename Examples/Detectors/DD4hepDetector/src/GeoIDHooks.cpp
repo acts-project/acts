55970d0a33b6264e74e7e1aa3c9e09439c011090
Corentin Allaire <corentin.allaire@cern.ch>
2022-11-25 16:19:33 +0100

    ODD hook function

---
+++ Examples/Detectors/DD4hepDetector/src/GeoIDHooks.cpp
@@ -0,0 +1,31 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2017 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/DD4hepDetector/GeoIDHooks.h"
+
+namespace det {
+namespace geoIDHook {
+
+Acts::GeometryIdentifier stripEndcapODD(Acts::GeometryIdentifier identifier, const Acts::Surface& surface){
+
+  if(identifier.volume() == 28 || identifier.volume() == 30){
+    Acts::Vector3 center = surface.center(Acts::GeometryContext());
+    double radius = sqrt(center[0]*center[0]+center[1]*center[1]);
+    if (radius < 850) {
+      identifier.setExtra(1);
+    } else {
+      identifier.setExtra(2);
+    }
+    std::cout << identifier << std::endl;
+  }
+  return identifier;
+}
+
+}
+}
+
