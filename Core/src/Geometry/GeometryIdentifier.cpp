1358e2b8e96f5c5d5dffbef2b7ddec18719d9fef
Corentin Allaire <corentin.allaire@cern.ch>
2022-11-30 16:48:14 +0100

    fix cout

--- Core/src/Geometry/GeometryIdentifier.cpp
+++ Core/src/Geometry/GeometryIdentifier.cpp
@@ -24,7 +24,7 @@ std::ostream& Acts::operator<<(std::ostream& os, Acts::GeometryIdentifier id) {
                                               id.sensitive(), id.extra()};
 
   bool writeSeparator = false;
-  for (auto i = 0u; i < sizeof(levels); ++i) {
+  for (auto i = 0u; i < (sizeof(levels) / sizeof(levels[0])); ++i) {
     if (levels[i] != 0u) {
       if (writeSeparator) {
         os << '|';
