f453469204861cea539653de904466237590f4ca
Corentin Allaire <corentin.allaire@cern.ch>
2022-12-06 18:31:02 +0100

    fix

--- Examples/Detectors/DD4hepDetector/src/GeometryIdentifierHooks.cpp
+++ Examples/Detectors/DD4hepDetector/src/GeometryIdentifierHooks.cpp
@@ -19,8 +19,12 @@ Acts::GeometryIdentifier stripEndcapODD(Acts::GeometryIdentifier identifier,
 
   // Define the spliting parameters for the ODD.
   // In case of geometry change please edit those accordingly
+
+  // volume where the spliting should be performed (strip endcaps)
   std::vector<int> volumes = {28, 30};
+  // list of the radial cutoff point (smallest to largest)
   std::vector<double> radialCuts = {580};
+  // list of the radial cutoff point for each volumes
   std::vector<std::vector<double>> cut = {radialCuts, radialCuts};
 
   auto it = std::find(volumes.begin(), volumes.end(), identifier.volume());
@@ -28,9 +32,10 @@ Acts::GeometryIdentifier stripEndcapODD(Acts::GeometryIdentifier identifier,
     std::vector<double> layercut = cut[it - volumes.begin()];
     Acts::Vector3 center = surface.center(Acts::GeometryContext());
     double radius = sqrt(center[0] * center[0] + center[1] * center[1]);
+    identifier.setExtra(1);
     for (unsigned i = 0; i < layercut.size(); i++) {
-      if (radius < layercut[i]) {
-        identifier.setExtra(i + 1);
+      if (radius > layercut[i]) {
+        identifier.setExtra(i + 2);
       }
     }
   }
