5cab50d75dc892b31fb76a45e230c41f66e8429d
Paul Gessinger <paul.gessinger@cern.ch>
2022-12-12 19:56:36 +0100

    refactor!: Switch SourceLink to container (#1616)
    This PR changes the `SourceLink` back from a base class to a concrete type without inheritance. It now contains a type erased upstream sourcelink (right now implement by `std::any`, but we can change this to use something with small buffer optimization).
    
    Contains #1512 at the moment.

--- Tests/UnitTests/Core/TrackFinding/CombinatorialKalmanFilterTests.cpp
+++ Tests/UnitTests/Core/TrackFinding/CombinatorialKalmanFilterTests.cpp
@@ -10,6 +10,7 @@
 
 #include "Acts/Definitions/Algebra.hpp"
 #include "Acts/EventData/Measurement.hpp"
+#include "Acts/EventData/SourceLink.hpp"
 #include "Acts/EventData/TrackParameters.hpp"
 #include "Acts/EventData/VectorMultiTrajectory.hpp"
 #include "Acts/Geometry/GeometryContext.hpp"
@@ -98,7 +99,10 @@ struct TestContainerAccessor {
 
     bool operator!=(const Iterator& other) const { return !(*this == other); }
 
-    const Value& operator*() const { return m_iterator->second; }
+    Acts::SourceLink operator*() const {
+      const auto& sl = m_iterator->second;
+      return Acts::SourceLink{sl};
+    }
 
     BaseIterator m_iterator;
   };
@@ -321,7 +325,7 @@ BOOST_AUTO_TEST_CASE(ZeroFieldForward) {
         val.lastMeasurementIndices.front(), [&](const auto& trackState) {
           numHits += 1u;
           const auto& sl =
-              static_cast<const TestSourceLink&>(trackState.uncalibrated());
+              trackState.uncalibrated().template get<TestSourceLink>();
           nummismatchedHits += (trackId != sl.sourceId);
         });
 
@@ -383,8 +387,8 @@ BOOST_AUTO_TEST_CASE(ZeroFieldBackward) {
     val.fittedStates->visitBackwards(
         val.lastMeasurementIndices.front(), [&](const auto& trackState) {
           numHits += 1u;
-          nummismatchedHits += (trackId != static_cast<const TestSourceLink&>(
-                                               trackState.uncalibrated())
+          nummismatchedHits += (trackId != trackState.uncalibrated()
+                                               .template get<TestSourceLink>()
                                                .sourceId);
         });
     BOOST_CHECK_EQUAL(numHits, f.detector.numMeasurements);
