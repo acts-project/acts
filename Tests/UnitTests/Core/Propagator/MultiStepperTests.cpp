fcb719f967c07b8fdfb48d148ce560eb0d65019d
Paul Gessinger <paul.gessinger@cern.ch>
2022-12-13 10:58:52 +0100

    'fix' the unit test

--- Tests/UnitTests/Core/Propagator/MultiStepperTests.cpp
+++ Tests/UnitTests/Core/Propagator/MultiStepperTests.cpp
@@ -328,7 +328,7 @@ void test_components_modifying_accessors() {
       [](auto &cmp) -> decltype(auto) { return cmp.jacToGlobal(); });
 
   std::apply(
-      [&](const auto &... projs) {
+      [&](const auto &...projs) {
         // clang-format off
         ( [&]() { modify(projs); check(projs); }(), ...);
         // clang-format on
@@ -713,13 +713,17 @@ void propagator_instatiation_test_function() {
                BoundSymMatrix::Identity().eval()});
   MultiComponentBoundTrackParameters<SinglyCharged> pars(surface, cmps);
 
+  // This only checks that this compiles, not that it runs without errors
+  // @TODO: Add test that checks the target aborter works corretly
+
   // Instantiate with target
-  propagator.template propagate<decltype(pars), decltype(options),
-                                MultiStepperSurfaceReached>(pars, *surface,
-                                                            options);
+  using type_a =
+      decltype(propagator.template propagate<decltype(pars), decltype(options),
+                                             MultiStepperSurfaceReached>(
+          pars, *surface, options));
 
   // Instantiate without target
-  propagator.propagate(pars, options);
+  using tybe_b = decltype(propagator.propagate(pars, options));
 }
 
 BOOST_AUTO_TEST_CASE(propagator_instatiation_test) {
