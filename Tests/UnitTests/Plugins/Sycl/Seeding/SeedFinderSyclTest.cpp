8fba3cb51e8e0e9f804f284e6051ee4c305ae586
Carlo Varni <cvarni@MBP-di-Carlo-2.lan>
2023-01-03 18:38:00 +0100

    format

--- Tests/UnitTests/Plugins/Sycl/Seeding/SeedFinderSyclTest.cpp
+++ Tests/UnitTests/Plugins/Sycl/Seeding/SeedFinderSyclTest.cpp
@@ -121,7 +121,8 @@ template <typename external_spacepoint_t>
 std::pair<Acts::SpacePointGridConfig, Acts::SpacePointGridOptions>
 setupSpacePointGridConfig(
     const Acts::SeedFinderConfig<external_spacepoint_t>& config,
-    const Acts::SeedFinderOptions& options) -> std::pair<Acts::SpacePointGridConfig, Acts::SpacePointGridOptions> {
+    const Acts::SeedFinderOptions& options)
+    ->std::pair<Acts::SpacePointGridConfig, Acts::SpacePointGridOptions> {
   Acts::SpacePointGridConfig gridConf{};
   gridConf.minPt = config.minPt;
   gridConf.rMax = config.rMax;
