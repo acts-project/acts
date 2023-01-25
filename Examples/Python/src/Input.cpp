1df320e7b768284d2f81d7121a3f620265ff5cd2
andiwand <stefl.andreas@gmail.com>
2023-01-25 10:27:55 +0100

    add python binding

--- Examples/Python/src/Input.cpp
+++ Examples/Python/src/Input.cpp
@@ -12,6 +12,7 @@
 #include "ActsExamples/Io/Csv/CsvPlanarClusterReader.hpp"
 #include "ActsExamples/Io/Csv/CsvSimHitReader.hpp"
 #include "ActsExamples/Io/Csv/CsvSpacePointReader.hpp"
+#include "ActsExamples/Io/Root/RootAthenaNTupleReader.hpp"
 #include "ActsExamples/Io/Root/RootMaterialTrackReader.hpp"
 #include "ActsExamples/Io/Root/RootParticleReader.hpp"
 #include "ActsExamples/Io/Root/RootTrajectorySummaryReader.hpp"
@@ -68,5 +69,11 @@ void addInput(Context& ctx) {
   ACTS_PYTHON_DECLARE_READER(
       ActsExamples::CsvSpacePointReader, mex, "CsvSpacePointReader", inputDir,
       inputStem, inputCollection, outputSpacePoints, extendCollection);
+
+  ACTS_PYTHON_DECLARE_READER(ActsExamples::RootAthenaNTupleReader, mex,
+                             "RootAthenaNTupleReader", inputTreeName,
+                             inputFilePath, outputTrackParameters,
+                             outputTruthVtxParameters, outputRecoVtxParameters,
+                             outputBeamspotConstraint);
 }
 }  // namespace Acts::Python
