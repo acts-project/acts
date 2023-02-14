678925c1d150cded63189d93170f937068dae161
Andreas Stefl <stefl.andreas@gmail.com>
2023-02-14 13:16:00 +0100

    refactor: print numeric value for multi index (#1859)
    was useful for me for debugging as it gets easier to compare numeric particle barcode values
    
    also aligns the way we print these values in the root writer

--- Examples/Io/Root/src/RootTrajectoryStatesWriter.cpp
+++ Examples/Io/Root/src/RootTrajectoryStatesWriter.cpp
@@ -294,17 +294,18 @@ ActsExamples::ProcessCode ActsExamples::RootTrajectoryStatesWriter::writeT(
                                     particleHitCounts);
       if (not particleHitCounts.empty()) {
         // Get the barcode of the majority truth particle
-        auto barcode = particleHitCounts.front().particleId.value();
+        auto barcode = particleHitCounts.front().particleId;
         // Find the truth particle via the barcode
         auto ip = particles.find(barcode);
         if (ip != particles.end()) {
           const auto& particle = *ip;
-          ACTS_DEBUG("Find the truth particle with barcode = " << barcode);
+          ACTS_DEBUG("Find the truth particle with barcode "
+                     << barcode << "=" << barcode.value());
           // Get the truth particle charge
           truthQ = static_cast<int>(particle.charge());
         } else {
-          ACTS_WARNING("Truth particle with barcode = " << barcode
-                                                        << " not found!");
+          ACTS_WARNING("Truth particle with barcode "
+                       << barcode << "=" << barcode.value() << " not found!");
         }
       }
 
