5e323a2bf1538052352ed51269613900675258b8
Benjamin Huth <Benjamin-Huth@t-online.de>
2023-02-15 12:03:26 +0100

    cosmetic update

--- CI/check_pragma_once.sh
+++ CI/check_pragma_once.sh
@@ -2,11 +2,11 @@
 
 ec=0
 
-for file in $(find ./Core Examples Tests Plugins -name "*.hpp"); do
+for file in $(find Core Examples Tests Plugins -name "*.hpp"); do
     res=$(grep "#pragma once" $file)
     if [[ "$res" != "#pragma once" ]]; then
         ec=1
-        echo "Missing #pragma once in '$file'"
+        echo "'#pragma once' missing in '$file'"
     fi
 done
 
