b8655a3e0c18f6dd1e8d452e5061196e626511ff
Luis Falda Coelho <56648068+LuisFelipeCoelho@users.noreply.github.com>
2023-02-15 17:29:53 +0100

    cange duplet capacity

--- Core/include/Acts/Seeding/SeedFinderConfig.hpp
+++ Core/include/Acts/Seeding/SeedFinderConfig.hpp
@@ -48,7 +48,7 @@ struct SeedFinderConfig {
 
   // maximum capacity of SP duplets to avoid reallocating memory at each
   // iteration
-  int bottomDupletCapacity = 210;
+  int bottomDupletCapacity = 200;
   int topDupletCapacity = 450;
 
   // force sorting of middle SPs in radius
