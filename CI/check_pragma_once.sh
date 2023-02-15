bbf3d56464a0d4c6e07c1bcb483d3f20c3b88ff4
Benjamin Huth <Benjamin-Huth@t-online.de>
2023-02-15 19:48:11 +0100

    fix and review

--- CI/check_pragma_once.sh
+++ CI/check_pragma_once.sh
@@ -3,7 +3,7 @@
 ec=0
 
 for file in $(find Core Examples Tests Plugins -name "*.hpp"); do
-    res=$(grep "#pragma once" $file)
+    res=$(grep -e "^[[:space:]]*#pragma once" $file)
     if [[ "$res" != "#pragma once" ]]; then
         ec=1
         echo "'#pragma once' missing in '$file'"
