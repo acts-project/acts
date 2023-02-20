11e5cb90e46f486c70dd2b7973802581c1644e7c
Benjamin Huth <Benjamin-Huth@t-online.de>
2023-02-20 14:21:39 +0100

    apply review

--- Examples/Scripts/Python/geant4_parallel.py
+++ Examples/Scripts/Python/geant4_parallel.py
@@ -3,20 +3,26 @@ from pathlib import Path
 from multiprocessing import Pool
 from functools import partial
 
-import numpy as np
-
 # This script runs a Geant4 simulation in parallel by passing chunks of events to subprocesses.
 # This is a workaround to achieve parallel processing even though Geant4 is not thread-save
 # and thus the internal parallelism of the ACTS examples framework cannot be used.
 #
 # Note that:
+# ==========
+#
 # * This should give equivalent results to a sequential run if the RNG is initialized with
 #   the same seed in all runs
-# * This works only for csv outputs, since they write the results in one file per event
+#
+# * So far this works only for csv outputs, since they write the results in one file per event
 #   (the naming of the csv-files should be equivalent to a sequential run)
+#
+# * In principle it is not difficult to extend this for ROOT files as well. One would need to
+#   write the root-files into seperate directory per chunk, and then use ROOT's hadd to combine
+#   the output files.
+#
 
 
-def runGeant4EventRange(events, outputDir):
+def runGeant4EventRange(beginEvent, endEvent, outputDir):
     import acts
     import acts.examples
     from acts.examples.simulation import addParticleGun, addGeant4, EtaConfig
@@ -32,7 +38,9 @@ def runGeant4EventRange(events, outputDir):
     field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
     rnd = acts.examples.RandomNumbers(seed=42)
 
-    s = acts.examples.Sequencer(events=len(events), skip=events[0], numThreads=1)
+    s = acts.examples.Sequencer(
+        events=endEvent - beginEvent, skip=beginEvent, numThreads=1
+    )
 
     outputDir = Path(outputDir)
     addParticleGun(
@@ -60,10 +68,11 @@ if "__main__" == __name__:
     n_events = 100
     n_jobs = 8
 
+    chunksize = n_events // (n_jobs - 1)
+    begins = range(0, n_events, chunksize)
+    ends = [min(b + chunksize, n_events) for b in begins]
+
     outputDir = Path.cwd()
 
     with Pool(n_jobs) as p:
-        p.map(
-            partial(runGeant4EventRange, outputDir=outputDir),
-            np.array_split(np.arange(n_events), n_jobs),
-        )
+        p.starmap(partial(runGeant4EventRange, outputDir=outputDir), zip(begins, ends))
