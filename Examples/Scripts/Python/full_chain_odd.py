a0ec21f9e3c85f88634871f909d9a90058c04592
Corentin Allaire <corentin.allaire@cern.ch>
2023-02-21 16:45:24 +0100

    explicit nMeasurementsMin

--- Examples/Scripts/Python/full_chain_odd.py
+++ Examples/Scripts/Python/full_chain_odd.py
@@ -192,7 +192,7 @@ with acts.FpeMonitor() if not g4_simulation else contextlib.nullcontext():
     else:
         addAmbiguityResolution(
             s,
-            AmbiguityResolutionConfig(maximumSharedHits=3),
+            AmbiguityResolutionConfig(maximumSharedHits=3, nMeasurementsMin=7),
             CKFPerformanceConfig(
                 ptMin=1.0 * u.GeV if ttbar_pu200 else 0.0,
                 nMeasurementsMin=7,
