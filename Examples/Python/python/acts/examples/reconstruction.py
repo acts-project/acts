b4cd7fdcafb65760dba4e044d10666991e656576
Andreas Stefl <stefl.andreas@gmail.com>
2023-02-22 11:03:12 +0100

    Update Examples/Python/python/acts/examples/reconstruction.py
    Co-authored-by: Paul Gessinger <hello@paulgessinger.com>

--- Examples/Python/python/acts/examples/reconstruction.py
+++ Examples/Python/python/acts/examples/reconstruction.py
@@ -368,7 +368,7 @@ def addTruthSmearing(
     rnd: acts.examples.RandomNumbers,
     selectedParticles: str,
     particleSmearingSigmas: ParticleSmearingSigmas,
-    initialVarInflation: list[float],
+    initialVarInflation: List[float],
     logLevel: acts.logging.Level = None,
 ):
     """adds algorithm that would mimic detector response uncertainties for truth seeding
