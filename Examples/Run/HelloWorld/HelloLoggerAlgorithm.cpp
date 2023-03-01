8027940d5f549f54da676cee5b46850b0cb8c83f
Paul Gessinger <hello@paulgessinger.com>
2023-03-01 12:06:44 +0100

    remove block

--- Examples/Run/HelloWorld/HelloLoggerAlgorithm.cpp
+++ Examples/Run/HelloWorld/HelloLoggerAlgorithm.cpp
@@ -18,11 +18,7 @@ ActsExamples::HelloLoggerAlgorithm::HelloLoggerAlgorithm(
 
 ActsExamples::ProcessCode ActsExamples::HelloLoggerAlgorithm::execute(
     const AlgorithmContext& ctx) const {
-  // using hard-coded data name should be avoided, but i'm a bit lazy tonight.
-  auto block = ctx.eventStore.get<std::size_t>("eventBlock");
-
-  ACTS_INFO(" Hello World! (from event=" << ctx.eventNumber
-                                         << ", block=" << block << ")");
+  ACTS_INFO(" Hello World! (from event=" << ctx.eventNumber);
   ACTS_DEBUG("  - that's an ACTS_DEBUG message");
   ACTS_VERBOSE("  - that's an ACTS_VERBOSE message");
   return ActsExamples::ProcessCode::SUCCESS;
