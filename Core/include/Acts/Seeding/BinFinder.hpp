ebbc28f634ece7da742760d67757d06ab24569c8
Carlo Varni <75478407+CarloVarni@users.noreply.github.com>
2023-03-12 18:41:31 +0100

    refactor!: Rewrite Binned SP Group (#1919)
    All credits go to @LuisFelipeCoelho 
    
    I have integrated Luis' code changes to the seeding algorithm. The result should be a considerable speed up of the seeding execution. I've added a few changes w.r.t. Luis' implementation
    
    Summary of the main changes:
    - Neighbourhood (and its iterator) class has been removed
    - BinnedSPGroup (and its iterator) class has been changed
    - Minor additional changes for code optimization (effect is very small, or even null)
    
    The code now runs on the grid bins. When it finds a not empty bin it stops and returns three vectors. These vectors contain indexes to grid bins and they represent the bottom, middle and top space point candidates. Seeding is run on these candidates (you run on the indexes and then ask the grid to retrieve the elements in that specific bin). Once done, we move to the next not-empty bin.
    
    Note: input collections to `createSeedsForGroup` are now `boost::container::small_vector<size_t, 9>` instead of Neightbourhood objects. Much simpler, cleaner and faster.
    
    /cc @noemina

--- Core/include/Acts/Seeding/BinFinder.hpp
+++ Core/include/Acts/Seeding/BinFinder.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2018 CERN for the benefit of the Acts project
+// Copyright (C) 2023 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "Acts/Seeding/SpacePointGrid.hpp"
+#include "Acts/Utilities/Holders.hpp"
 
 #include <vector>
 
@@ -25,9 +26,9 @@ template <typename external_spacepoint_t>
 class BinFinder {
  public:
   /// constructor
-  BinFinder();
+  BinFinder() = delete;
 
-  BinFinder(std::vector<std::pair<int, int> > zBinNeighbors,
+  BinFinder(const std::vector<std::pair<int, int>>& zBinNeighbors,
             int numPhiNeighbors);
 
   /// Return all bins that could contain space points that can be used with the
@@ -35,12 +36,15 @@ class BinFinder {
   /// @param phiBin phi index of bin with middle space points
   /// @param zBin z index of bin with middle space points
   /// @param binnedSP phi-z grid containing all bins
-  boost::container::small_vector<size_t, 10> findBins(
+  boost::container::small_vector<size_t, 9> findBins(
       size_t phiBin, size_t zBin,
       const SpacePointGrid<external_spacepoint_t>* binnedSP) const;
 
  private:
-  std::vector<std::pair<int, int> > m_zBinNeighbors;
+  // This vector is provided by the user and is supposed to be a contant for
+  // all events. No point in making a copy
+  Acts::detail::RefHolder<const std::vector<std::pair<int, int>>>
+      m_zBinNeighbors;
   int m_numPhiNeighbors = 1;
 };
 }  // namespace Acts
