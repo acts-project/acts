e60dec987fa20306b92baaa702791bd82130be75
andiwand <stefl.andreas@gmail.com>
2023-03-15 13:59:05 +0100

    remove dead code

--- Core/include/Acts/Propagator/VolumeCollector.hpp
+++ Core/include/Acts/Propagator/VolumeCollector.hpp
@@ -117,14 +117,6 @@ struct VolumeCollector {
       }
     }
   }
-
-  /// Pure observer interface
-  /// - this does not apply to the volume collector
-  template <typename propagator_state_t, typename stepper_t,
-            typename navigator_t>
-  void operator()(propagator_state_t& /*state*/, const stepper_t& /*unused*/,
-                  const navigator_t& /*navigator*/,
-                  const Logger& /*logger*/) const {}
 };
 
 }  // namespace Acts
