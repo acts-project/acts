0c8ba38e6aa1f07d851db381e11375460c37de8e
andiwand <stefl.andreas@gmail.com>
2023-03-23 15:24:05 +0100

    formatting

--- Core/include/Acts/Propagator/DirectNavigator.hpp
+++ Core/include/Acts/Propagator/DirectNavigator.hpp
@@ -148,8 +148,7 @@ class DirectNavigator {
   /// @param [in,out] state is the propagation state object
   /// @param [in] stepper Stepper in use
   template <typename propagator_state_t, typename stepper_t>
-  void initialize(propagator_state_t& state,
-                  const stepper_t& stepper) const {
+  void initialize(propagator_state_t& state, const stepper_t& stepper) const {
     (void)state;
     (void)stepper;
   }
