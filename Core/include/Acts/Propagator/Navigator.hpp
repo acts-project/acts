27f482919fd8f608a36c1c6ab60f23603a467258
andiwand <stefl.andreas@gmail.com>
2023-03-23 15:21:50 +0100

    remove todos

--- Core/include/Acts/Propagator/Navigator.hpp
+++ Core/include/Acts/Propagator/Navigator.hpp
@@ -293,38 +293,28 @@ class Navigator {
     return state.startSurface;
   }
 
-  // TODO sounds like an aborters job?
   const Surface* targetSurface(const State& state) const {
     return state.targetSurface;
   }
 
-  // TODO sounds like an aborters job?
   bool targetReached(const State& state) const { return state.targetReached; }
 
-  // TODO not sure why the navigation has to break - propergator has control
-  // over that
   bool navigationBreak(const State& state) const {
     return state.navigationBreak;
   }
 
-  // TODO why would anybody set this? sounds dangerous
   void currentSurface(State& state, const Surface* surface) const {
     state.currentSurface = surface;
   }
 
-  // TODO sounds like an aborters job? shouldnt we tell the propagator that we
-  // are done?
   void targetReached(State& state, bool targetReached) const {
     state.targetReached = targetReached;
   }
 
-  // TODO not sure why the navigation has to break - propergator has control
-  // over that
   void navigationBreak(State& state, bool navigationBreak) const {
     state.navigationBreak = navigationBreak;
   }
 
-  // TODO why this
   void insertExternalSurface(State& state, GeometryIdentifier geoid) const {
     state.externalSurfaces.insert(
         std::pair<uint64_t, GeometryIdentifier>(geoid.layer(), geoid));
--- Core/include/Acts/Propagator/detail/VoidPropagatorComponents.hpp
+++ Core/include/Acts/Propagator/detail/VoidPropagatorComponents.hpp
@@ -54,33 +54,24 @@ struct VoidNavigator {
     return state.startSurface;
   }
 
-  // TODO sounds like an aborters job?
   const Surface* targetSurface(const State& state) const {
     return state.targetSurface;
   }
 
-  // TODO sounds like an aborters job?
   bool targetReached(const State& state) const { return state.targetReached; }
 
-  // TODO not sure why the navigation has to break - propergator has control
-  // over that
   bool navigationBreak(const State& state) const {
     return state.navigationBreak;
   }
 
-  // TODO why would anybody set this? sounds dangerous
   void currentSurface(State& state, const Surface* surface) const {
     state.currentSurface = surface;
   }
 
-  // TODO sounds like an aborters job? shouldnt we tell the propagator that we
-  // are done?
   void targetReached(State& state, bool targetReached) const {
     state.targetReached = targetReached;
   }
 
-  // TODO not sure why the navigation has to break - propergator has control
-  // over that
   void navigationBreak(State& state, bool navigationBreak) const {
     state.navigationBreak = navigationBreak;
   }
