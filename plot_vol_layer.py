1e7cac044bd33db6b26fe77a74b1ce42a2b6fc4e
Rosanne-Hasan <rosannehasan2@gmail.com>
2023-03-24 17:42:26 +0000

    got plots ready for slides

--- plot_vol_layer.py
+++ plot_vol_layer.py
@@ -7,11 +7,12 @@ import ROOT
 from ROOT import TFile, TTree, gStyle , TColor 
 import numpy as np 
 import csv 
+import array as arr 
 
 
 #open csv 
 
-df = pd.read_csv('vol_layer.csv')
+df = pd.read_csv('vol_layer_morestats.csv', skiprows=1)
 array = df.to_numpy()
 
 acts_vol = array[:,0]
@@ -23,30 +24,33 @@ FTF_id= array[:,4]
 
 #make histogram 
 c1 = ROOT.TCanvas("c1","c1",800,600)
-h1 = ROOT.TH2D("h1","vol_layer;z;r",10000,-3000,3000,10000,-3000,3000) 
+h1 = ROOT.TH2D("h1","ACTS volume & layers in terms of FTF volume id;z;r",100,-3000,3000,100,0,1000) 
 
+# #ACTS: 
+# for i in range(0,len(acts_vol)): 
 
+#     h1.Fill(z[i],r[i],acts_vol[i])
+
+#FTF
 for i in range(0,len(acts_vol)): 
     if FTF_id[i] != 0: 
         h1.Fill(z[i],r[i],FTF_id[i])
-    # else: 
-    #     h1.Fill(z[i],r[i])
+        
 
+#zcontours = [70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,90,91,92,93,94,95,96,97,98]
+zcontours = [70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98]
+zcountour = arr.array('d', zcontours)
 
+h1.SetContour(len(zcontours),zcountour)
 
-stops = [0.00, 0.34, 0.61, 0.84, 1.00]
-red   = [0.00, 0.00, 0.87, 1.00, 0.51]
-green = [0.00, 0.81, 1.00, 0.20, 0.00]
-blue  = [0.51, 1.00, 0.12, 0.00, 0.00]
+h1.SetMinimum(70)
+h1.SetMaximum(98) 
 
-TColor.CreateGradientColorTable(7, stops, red, green, blue, 999)
-gStyle.SetNumberContours(999)
+        
 
-
-
-#gStyle.SetPalette(80)
+gStyle.SetPalette(91)
 
 c1.cd()
 h1.SetStats(0)
-h1.Draw("COLZ") 
-c1.SaveAs("vol_layer_better.pdf")
+h1.Draw("COLZ") #text if want numbers 
+c1.SaveAs("vol_layer_FTF.pdf")
