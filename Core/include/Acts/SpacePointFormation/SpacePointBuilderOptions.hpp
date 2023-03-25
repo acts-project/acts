401fb2b5ed44dc211bcdafa93500ac42d05dbb28
Carlo Varni <carlovarni@pccvarni.dyndns.cern.ch>
2023-03-25 15:16:39 +0100

    pass by const ref

--- Core/include/Acts/SpacePointFormation/SpacePointBuilderOptions.hpp
+++ Core/include/Acts/SpacePointFormation/SpacePointBuilderOptions.hpp
@@ -18,7 +18,7 @@ struct SpacePointBuilderOptions {
             const std::pair<Vector3, Vector3>>
       stripEndsPair;
   // accessor of local position and covariance from soruce link
-  std::function<std::pair<const BoundVector, const BoundSymMatrix>(SourceLink)>
+  std::function<std::pair<const BoundVector, const BoundSymMatrix>(const SourceLink&)>
       paramCovAccessor;
   /// vertex position
   Vector3 vertex = {0., 0., 0.};
@@ -31,7 +31,7 @@ struct SpacePointBuilderOptions {
 
 struct StripPairOptions {
   // accessor of local position and covariance from soruce link
-  std::function<std::pair<const BoundVector, const BoundSymMatrix>(SourceLink)>
+  std::function<std::pair<const BoundVector, const BoundSymMatrix>(const SourceLink&)>
       paramCovAccessor;
   /// vertex position
   Vector3 vertex = {0., 0., 0.};
