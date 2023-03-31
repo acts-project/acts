08b19efd7c465ab9831814d425158a33268d2502
dimitra97 <dimitraamp.1997@gmail.com>
2023-04-01 00:38:38 +0200

    feat: Build mockup geometry with the chambers

---
+++ Examples/Run/Geant4/TestMockupBuilder.cpp
@@ -0,0 +1,66 @@
+
+
+#include "Acts/Visualization/GeometryView3D.hpp"
+#include "Acts/Visualization/IVisualization3D.hpp"
+#include "Acts/Visualization/ObjVisualization3D.hpp"
+#include "ActsExamples/Geant4Detector/Geant4Detector.hpp"
+#include "ActsExamples/Geant4Detector/MockupSectorBuilder.hpp"
+
+using namespace Acts;
+using namespace ActsExamples;
+
+int main() {
+  auto mockup_config = MockupSectorBuilder::Config();
+
+  auto mockup_chamberConfig_inner = MockupSectorBuilder::ChamberConfig();
+  auto mockup_chamberConfig_middle = MockupSectorBuilder::ChamberConfig();
+  auto mockup_chamberConfig_outer = MockupSectorBuilder::ChamberConfig();
+
+  mockup_config.gdml_path = "./MuonChamber.gdml";
+  mockup_config.NumberOfSectors = 8;
+
+  mockup_chamberConfig_inner.name = "Inner_Detector_Chamber";
+  mockup_chamberConfig_inner.SensitiveNames = {"Inner_Skin"};
+  mockup_chamberConfig_inner.PassiveNames = {"xx"};
+
+  mockup_chamberConfig_middle.name = "Middle_Detector_Chamber";
+  mockup_chamberConfig_middle.SensitiveNames = {"Middle_Skin"};
+  mockup_chamberConfig_middle.PassiveNames = {"xx"};
+
+  mockup_chamberConfig_outer.name = "Outer_Detector_Chamber";
+  mockup_chamberConfig_outer.SensitiveNames = {"Outer_Skin"};
+  mockup_chamberConfig_outer.PassiveNames = {"xx"};
+
+  MockupSectorBuilder mockup_builder(mockup_config);
+
+  GeometryContext gctx;
+  auto detectorVolume_inner_chamber =
+      mockup_builder.BuildChamber(gctx, mockup_chamberConfig_inner);
+
+  auto detectorVolume_middle_chamber =
+      mockup_builder.BuildChamber(gctx, mockup_chamberConfig_middle);
+
+  auto detectorVolume_outer_chamber =
+      mockup_builder.BuildChamber(gctx, mockup_chamberConfig_outer);
+
+  std::vector<std::shared_ptr<Acts::Experimental::DetectorVolume>>
+      detector_volumes = {};
+
+  detector_volumes.push_back(detectorVolume_middle_chamber);
+  detector_volumes.push_back(detectorVolume_inner_chamber);
+  detector_volumes.push_back(detectorVolume_outer_chamber);
+
+  auto detectorVolume_sector =
+      mockup_builder.BuildSector(detector_volumes, gctx);
+
+  Acts::ObjVisualization3D obj_testMockUp;
+  Acts::ViewConfig sConfig = Acts::s_viewSensitive;
+
+  Acts::ObjVisualization3D obj_Sector;
+
+  Acts::GeometryView3D::drawDetectorVolume(
+      obj_Sector, *detectorVolume_sector, Acts::GeometryContext(), true,
+      Acts::Transform3::Identity(), sConfig);
+
+  obj_Sector.write("test_sector.obj");
+}
