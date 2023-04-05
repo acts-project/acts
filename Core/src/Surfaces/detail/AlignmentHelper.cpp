d86b5a482058b0daa5af4bfc73606bc55ca3ec63
Xiaocong Ai <xiaocong.ai@desy.de>
2023-04-05 15:51:22 +0800

    change the definitiom of the rotation parameters

--- Core/src/Surfaces/detail/AlignmentHelper.cpp
+++ Core/src/Surfaces/detail/AlignmentHelper.cpp
@@ -13,41 +13,51 @@
 #include <utility>
 
 Acts::detail::RotationToAxes Acts::detail::rotationToLocalAxesDerivative(
-    const RotationMatrix3& rotation) {
+    const RotationMatrix3& compositeRotation,
+    const RotationMatrix3& relRotation) {
   // Get Euler angles for rotation represented by rotZ * rotY * rotX, i.e.
   // first rotation around x axis, then y axis, last z axis
   // The elements stored in rotAngles is (rotZ, rotY, rotX)
-  const Vector3 rotAngles = rotation.eulerAngles(2, 1, 0);
-  double sx = std::sin(rotAngles(2));
-  double cx = std::cos(rotAngles(2));
-  double sy = std::sin(rotAngles(1));
-  double cy = std::cos(rotAngles(1));
-  double sz = std::sin(rotAngles(0));
-  double cz = std::cos(rotAngles(0));
+  // const Vector3 rotAngles = rotation.eulerAngles(2, 1, 0);
+  // double sx = std::sin(rotAngles(2));
+  // double cx = std::cos(rotAngles(2));
+  // double sy = std::sin(rotAngles(1));
+  // double cy = std::cos(rotAngles(1));
+  // double sz = std::sin(rotAngles(0));
+  // double cz = std::cos(rotAngles(0));
   // rotZ * rotY * rotX =
   // [ cz*cy  cz*sy*sx-cx*sz  sz*sx+cz*cx*sy ]
   // [ cy*sz  cz*cx+sz*sy*sx  cx*sz*sy-cz*sx ]
   // [ -sy   cy*sx         cy*cx        ]
 
   // Derivative of local x axis w.r.t. (rotX, rotY, rotZ)
-  RotationMatrix3 rotToLocalXAxis = RotationMatrix3::Zero();
-  rotToLocalXAxis.col(0) = Vector3(0, 0, 0);
-  rotToLocalXAxis.col(1) = Vector3(-cz * sy, -sz * sy, -cy);
-  rotToLocalXAxis.col(2) = Vector3(-sz * cy, cz * cy, 0);
+  RotationMatrix3 rotToCompositeLocalXAxis = RotationMatrix3::Zero();
+  rotToCompositeLocalXAxis.col(0) = compositeRotation * Vector3(0, 0, 0);
+  rotToCompositeLocalXAxis.col(1) = compositeRotation * Vector3(0, 0, -1);
+  rotToCompositeLocalXAxis.col(2) = compositeRotation * Vector3(0, 1, 0);
   // Derivative of local y axis w.r.t. (rotX, rotY, rotZ)
-  RotationMatrix3 rotToLocalYAxis = RotationMatrix3::Zero();
-  rotToLocalYAxis.col(0) =
-      Vector3(cz * sy * cx + sz * sx, sz * sy * cx - cz * sx, cy * cx);
-  rotToLocalYAxis.col(1) = Vector3(cz * cy * sx, sz * cy * sx, -sy * sx);
-  rotToLocalYAxis.col(2) =
-      Vector3(-sz * sy * sx - cz * cx, cz * sy * sx - sz * cx, 0);
+  RotationMatrix3 rotToCompositeLocalYAxis = RotationMatrix3::Zero();
+  rotToCompositeLocalYAxis.col(0) = compositeRotation * Vector3(0, 0, 1);
+  rotToCompositeLocalYAxis.col(1) = compositeRotation * Vector3(0, 0, 0);
+  rotToCompositeLocalYAxis.col(2) = compositeRotation * Vector3(-1, 0, 0);
   // Derivative of local z axis w.r.t. (rotX, rotY, rotZ)
+  RotationMatrix3 rotToCompositeLocalZAxis = RotationMatrix3::Zero();
+  rotToCompositeLocalZAxis.col(0) = compositeRotation * Vector3(0, -1, 0);
+  rotToCompositeLocalZAxis.col(1) = compositeRotation * Vector3(1, 0, 0);
+  rotToCompositeLocalZAxis.col(2) = compositeRotation * Vector3(0, 0, 0);
+
+  RotationMatrix3 rotToLocalXAxis = RotationMatrix3::Zero();
+  RotationMatrix3 rotToLocalYAxis = RotationMatrix3::Zero();
   RotationMatrix3 rotToLocalZAxis = RotationMatrix3::Zero();
-  rotToLocalZAxis.col(0) =
-      Vector3(sz * cx - cz * sy * sx, -sz * sy * sx - cz * cx, -cy * sx);
-  rotToLocalZAxis.col(1) = Vector3(cz * cy * cx, sz * cy * cx, -sy * cx);
-  rotToLocalZAxis.col(2) =
-      Vector3(cz * sx - sz * sy * cx, cz * sy * cx + sz * sx, 0);
+  rotToLocalXAxis = rotToCompositeLocalXAxis * relRotation(0, 0) +
+                    rotToCompositeLocalYAxis * relRotation(1, 0) +
+                    rotToCompositeLocalZAxis * relRotation(2, 0);
+  rotToLocalYAxis = rotToCompositeLocalXAxis * relRotation(0, 1) +
+                    rotToCompositeLocalYAxis * relRotation(1, 1) +
+                    rotToCompositeLocalZAxis * relRotation(2, 1);
+  rotToLocalZAxis = rotToCompositeLocalXAxis * relRotation(0, 2) +
+                    rotToCompositeLocalYAxis * relRotation(1, 2) +
+                    rotToCompositeLocalZAxis * relRotation(2, 2);
 
   return std::make_tuple(std::move(rotToLocalXAxis), std::move(rotToLocalYAxis),
                          std::move(rotToLocalZAxis));
