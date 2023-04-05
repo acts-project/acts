959730499395b82ebd71769c14d3b86ec14f33fd
Andreas Salzburger <asalzburger@gmail.com>
2023-04-05 13:54:26 +0200

    remove BOOST_TEST

--- Tests/UnitTests/Core/Detector/IndexedSurfaceGridFillerTests.cpp
+++ Tests/UnitTests/Core/Detector/IndexedSurfaceGridFillerTests.cpp
@@ -52,48 +52,48 @@ BOOST_AUTO_TEST_CASE(BinSequence) {
   // Test standard bound local bin sequence
   auto seq48e0b10B = binSequence({4u, 8u}, 0u, 10u, AxisBoundaryType::Bound);
   std::vector<std::size_t> reference = {4u, 5u, 6u, 7u, 8u};
-  BOOST_TEST(seq48e0b10B == reference);
+  BOOST_CHECK(seq48e0b10B == reference);
 
   // Test bound local bin sequence with expansion 1u
   auto seq48e1b10B = binSequence({4u, 8u}, 1u, 10u, AxisBoundaryType::Bound);
   reference = {3u, 4u, 5u, 6u, 7u, 8u, 9u};
-  BOOST_TEST(seq48e1b10B == reference);
+  BOOST_CHECK(seq48e1b10B == reference);
 
   // Test bound local bin sequence with expansion 3u - clipped to max bin 10u
   auto seq48e3b10B = binSequence({4u, 8u}, 3u, 10u, AxisBoundaryType::Bound);
   reference = {1u, 2u, 3u, 4u, 5u, 6u, 7u, 8u, 9u, 10u};
-  BOOST_TEST(seq48e3b10B == reference);
+  BOOST_CHECK(seq48e3b10B == reference);
 
   // Test open bin sequence with overflow filling
   auto seq48e3b10O = binSequence({4u, 8u}, 3u, 10u, AxisBoundaryType::Open);
   reference = {1u, 2u, 3u, 4u, 5u, 6u, 7u, 8u, 9u, 10u, 11u};
-  BOOST_TEST(seq48e3b10O == reference);
+  BOOST_CHECK(seq48e3b10O == reference);
 
   // Test standard closed local bins
   auto seq48e0b10C = binSequence({4u, 8u}, 0u, 20u, AxisBoundaryType::Closed);
   reference = {4u, 5u, 6u, 7u, 8u};
-  BOOST_TEST(seq48e0b10C == reference);
+  BOOST_CHECK(seq48e0b10C == reference);
 
   // Test closed local bins with expansion
   auto seq48e1b10C = binSequence({4u, 8u}, 1u, 20u, AxisBoundaryType::Closed);
   reference = {3u, 4u, 5u, 6u, 7u, 8u, 9u};
-  BOOST_TEST(seq48e1b10C == reference);
+  BOOST_CHECK(seq48e1b10C == reference);
 
   // Test closed local bins with expansion over bin boundary
   auto seq1029e1b20C =
       binSequence({19u, 20u}, 1u, 20u, AxisBoundaryType::Closed);
   reference = {1u, 18u, 19u, 20u};
-  BOOST_TEST(seq1029e1b20C == reference);
+  BOOST_CHECK(seq1029e1b20C == reference);
 
   // Test closed local bins with bin boundary jump
   auto seq218e0b20C = binSequence({2u, 18u}, 0u, 20u, AxisBoundaryType::Closed);
   reference = {1u, 2u, 18u, 19u, 20u};
-  BOOST_TEST(seq218e0b20C == reference);
+  BOOST_CHECK(seq218e0b20C == reference);
 
   // Test closed local bins with bin boundary jump with extension
   auto seq218e2b20C = binSequence({2u, 18u}, 2u, 20u, AxisBoundaryType::Closed);
   reference = {1u, 2u, 3u, 4u, 16u, 17u, 18u, 19u, 20u};
-  BOOST_TEST(seq218e2b20C == reference);
+  BOOST_CHECK(seq218e2b20C == reference);
 }
 
 BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfaceCenter) {
@@ -127,7 +127,7 @@ BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfaceCenter) {
   std::size_t nonEmptyBins = countBins<decltype(indexedGridXY)>(indexedGridXY);
   // Check the correct number of filled bins
   ACTS_INFO("- filled " << nonEmptyBins << " bins of the grid.");
-  BOOST_TEST(nonEmptyBins == 1u);
+  BOOST_CHECK(nonEmptyBins == 1u);
 }
 
 BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfaceBinValue) {
@@ -161,7 +161,7 @@ BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfaceBinValue) {
 
   std::size_t nonEmptyBins = countBins<decltype(indexedGridXY)>(indexedGridXY);
   ACTS_INFO("- filled " << nonEmptyBins << " bins of the grid.");
-  BOOST_TEST(nonEmptyBins == 1u);
+  BOOST_CHECK(nonEmptyBins == 1u);
 }
 
 BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfacePolyhedron) {
@@ -196,7 +196,7 @@ BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfacePolyhedron) {
 
   std::size_t nonEmptyBins = countBins<decltype(indexedGridXY)>(indexedGridXY);
   ACTS_INFO("- filled " << nonEmptyBins << " bins of the grid.");
-  BOOST_TEST(nonEmptyBins == 25u);
+  BOOST_CHECK(nonEmptyBins == 25u);
 }
 
 BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfacePolyhedronBinExpansion) {
@@ -231,7 +231,7 @@ BOOST_AUTO_TEST_CASE(IndexGridXYOneSurfacePolyhedronBinExpansion) {
 
   std::size_t nonEmptyBins = countBins<decltype(indexedGridXY)>(indexedGridXY);
   ACTS_INFO("- filled " << nonEmptyBins << " bins of the grid.");
-  BOOST_TEST(nonEmptyBins == 49u);
+  BOOST_CHECK(nonEmptyBins == 49u);
 }
 
 BOOST_AUTO_TEST_CASE(IndexGridZPhiYOneSurfacePolyhedronBinExpansion) {
@@ -267,7 +267,7 @@ BOOST_AUTO_TEST_CASE(IndexGridZPhiYOneSurfacePolyhedronBinExpansion) {
   std::size_t nonEmptyBins =
       countBins<decltype(indexedGridZPhi)>(indexedGridZPhi);
   ACTS_INFO("- filled " << nonEmptyBins << " bins of the grid.");
-  BOOST_TEST(nonEmptyBins == 6u);
+  BOOST_CHECK(nonEmptyBins == 6u);
 }
 
 BOOST_AUTO_TEST_CASE(IndexGridZPhiYOneSurfaceMPIPolyhedronBinExpansion) {
@@ -301,7 +301,7 @@ BOOST_AUTO_TEST_CASE(IndexGridZPhiYOneSurfaceMPIPolyhedronBinExpansion) {
   std::size_t nonEmptyBins =
       countBins<decltype(indexedGridZPhi)>(indexedGridZPhi);
   ACTS_INFO("- filled " << nonEmptyBins << " bins of the grid.");
-  BOOST_TEST(nonEmptyBins == 9u);
+  BOOST_CHECK(nonEmptyBins == 9u);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
