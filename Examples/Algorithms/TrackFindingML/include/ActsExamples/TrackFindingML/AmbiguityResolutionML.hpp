a73b2c2bc8c84195cd32aeda524f00a202cd0340
Corentin-Allaire <62873125+Corentin-Allaire@users.noreply.github.com>
2023-04-11 21:06:49 +0200

    feat: Add Mlpack DBScan clustering to the ML Ambiguity solver (#2005)
    This PR allow us to use the mlpack library inside Acts. This is a cpp library implementing many different ML algorithm. After this change we now have two implementation of the ML Solver, one using the hit based clustering and one based on the DBScan+hit based

---
+++ Examples/Algorithms/TrackFindingML/include/ActsExamples/TrackFindingML/AmbiguityResolutionML.hpp
@@ -0,0 +1,47 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2023 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "ActsExamples/EventData/Track.hpp"
+#include "ActsExamples/Framework/IAlgorithm.hpp"
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace ActsExamples {
+
+/// Generic implementation of the machine learning ambiguity resolution
+/// Conatains method for data preparations
+class AmbiguityResolutionML : public IAlgorithm {
+ public:
+  /// Construct the ambiguity resolution algorithm.
+  ///
+  /// @param name name of the algorithm
+  /// @param lvl is the logging level
+  AmbiguityResolutionML(std::string name, Acts::Logging::Level lvl);
+
+ protected:
+  /// Associated measurements ID to Tracks ID
+  ///
+  /// @param tracks is the input track container
+  /// @param nMeasurementsMin minimum number of measurment per track
+  /// @return an ordered list containing pairs of track ID and associated measurement ID
+  std::multimap<int, std::pair<int, std::vector<int>>> mapTrackHits(
+      const ConstTrackContainer& tracks, int nMeasurementsMin) const;
+
+  /// Prepare the output track container to be written
+  ///
+  /// @param tracks is the input track container
+  /// @param goodTracks is list of the IDs of all the tracks we want to keep
+  ConstTrackContainer prepareOutputTrack(const ConstTrackContainer& tracks,
+                                         std::vector<int>& goodTracks) const;
+};
+
+}  // namespace ActsExamples
