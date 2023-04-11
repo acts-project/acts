306566f7ea12fb676bb9a25167c4da71e94211e0
Paul Gessinger <paul.gessinger@cern.ch>
2023-04-11 11:28:40 +0200

    move functions to cpp where possible

--- Plugins/EDM4hep/src/EDM4hepUtil.cpp
+++ Plugins/EDM4hep/src/EDM4hepUtil.cpp
@@ -15,9 +15,99 @@
 
 #include "edm4hep/TrackState.h"
 
-namespace ActsExamples {
+namespace Acts {
+namespace EDM4hepUtil {
+namespace detail {
 
- void EDM4hepUtil::writeTrack(TrackContainer::ConstTrackProxy track,
-                              edm4hep::MutableTrack to) {}
+ActsSymMatrix<6> jacobianToEdm4hep(double theta, double qOverP, double Bz) {
+  // Calculate jacobian from our internal parametrization (d0, z0, phi, theta,
+  // q/p) to the LCIO / edm4hep (see:
+  // https://bib-pubdb1.desy.de/record/81214/files/LC-DET-2006-004%5B1%5D.pdf)
+  // one (d0, z0, phi, tan(lambda), omega). Top left 3x3 matrix in the
+  // jacobian is 1. Bottom right 2x2 matrix is:
+  //
+  // [  d                                 ]
+  // [------(cot(theta))         0        ]
+  // [dtheta                              ]
+  // [                                    ]
+  // [  d   /  B*q/p   \   d  /  B*q/p   \]
+  // [------|----------|  ----|----------|]
+  // [dtheta\sin(theta)/  dq/p\sin(theta)/]
+  //
+  // =
+  //
+  // [     2                        ]
+  // [- cot (theta) - 1       0     ]
+  // [                              ]
+  // [-B*q/p*cos(theta)       B     ]
+  // [------------------  ----------]
+  // [      2             sin(theta)]
+  // [   sin (theta)                ]
 
-}  // namespace ActsExamples
+  ActsSymMatrix<6> J;
+  J.setIdentity();
+  double cotTheta = std::tan(M_PI_2 + theta);
+  J(3, 3) = -cotTheta * cotTheta - 1;  // d(tanLambda) / dTheta
+  J(4, 4) = Bz / std::sin(theta);      // dOmega / d(qop)
+  double sinTheta = std::sin(theta);
+  J(4, 3) = -Bz * qOverP * std::cos(theta) /
+            (sinTheta * sinTheta);  // dOmega / dTheta
+  return J;
+}
+
+ActsSymMatrix<6> jacobianFromEdm4hep(double tanLambda, double omega,
+                                     double Bz) {
+  // [     d      /                     pi\                                  ]
+  // [------------|-atan(\tan\lambda) + --|                 0                ]
+  // [d\tan\lambda\                     2 /                                  ]
+  // [                                                                       ]
+  // [     d      /         \Omega        \     d   /         \Omega        \]
+  // [------------|-----------------------|  -------|-----------------------|]
+  // [d\tan\lambda|     __________________|  d\Omega|     __________________|]
+  // [            |    /            2     |         |    /            2     |]
+  // [            \B*\/  \tan\lambda  + 1 /         \B*\/  \tan\lambda  + 1 /]
+  //
+  // =
+  //
+  // [         -1                                     ]
+  // [   ----------------                 0           ]
+  // [              2                                 ]
+  // [   \tan\lambda  + 1                             ]
+  // [                                                ]
+  // [  -\Omega*\tan\lambda               1           ]
+  // [-----------------------  -----------------------]
+  // [                    3/2       __________________]
+  // [  /           2    \         /            2     ]
+  // [B*\\tan\lambda  + 1/     B*\/  \tan\lambda  + 1 ]
+
+  ActsSymMatrix<6> J;
+  J.setIdentity();
+  J(3, 3) = -1 / (tanLambda * tanLambda + 1);
+  J(4, 3) = -1 * omega * tanLambda /
+            (Bz * std::pow(tanLambda * tanLambda + 1, 3. / 2.));
+  J(4, 4) = 1 / (Bz * std::sqrt(tanLambda * tanLambda + 1));
+
+  return J;
+}
+
+void packCovariance(const ActsSymMatrix<6>& from, float* to) {
+  for (int i = 0; i < from.rows(); i++) {
+    for (int j = 0; j <= i; j++) {
+      size_t k = (i + 1) * i / 2 + j;
+      to[k] = from(i, j);
+    }
+  }
+}
+
+void unpackCovariance(const float* from, ActsSymMatrix<6>& to) {
+  auto k = [](size_t i, size_t j) { return (i + 1) * i / 2 + j; };
+  for (int i = 0; i < to.rows(); i++) {
+    for (int j = 0; j < to.cols(); j++) {
+      to(i, j) = from[j <= i ? k(i, j) : k(j, i)];
+    }
+  }
+}
+
+}  // namespace detail
+}  // namespace EDM4hepUtil
+}  // namespace Acts
