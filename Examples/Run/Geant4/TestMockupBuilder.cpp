5cd841b10215f352357a2060d48a799cc2fdd981
dimitra97 <dimitraamp.1997@gmail.com>
2023-04-13 10:37:00 +0200

    Place the files that create the mockup geometry in another folder

--- Examples/Run/Geant4/TestMockupBuilder.cpp
+++ Examples/Run/Geant4/TestMockupBuilder.cpp
@@ -6,13 +6,38 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "Acts/Detector/Detector.hpp"
+#include "Acts/Detector/DetectorVolume.hpp"
+#include "Acts/Detector/Portal.hpp"
+#include "Acts/Detector/PortalHelper.hpp"
+#include "Acts/MagneticField/ConstantBField.hpp"
+#include "Acts/MagneticField/MagneticFieldContext.hpp"
+#include "Acts/Navigation/DetectorVolumeFinders.hpp"
+#include "Acts/Navigation/NavigationState.hpp"
+#include "Acts/Navigation/NextNavigator.hpp"
+#include "Acts/Navigation/SurfaceCandidatesUpdators.hpp"
+#include "Acts/Propagator/AbortList.hpp"
+#include "Acts/Propagator/EigenStepper.hpp"
+#include "Acts/Propagator/Propagator.hpp"
+#include "Acts/Propagator/SurfaceCollector.hpp"
 #include "Acts/Visualization/GeometryView3D.hpp"
 #include "Acts/Visualization/IVisualization3D.hpp"
 #include "Acts/Visualization/ObjVisualization3D.hpp"
 #include "ActsExamples/Geant4Detector/Geant4Detector.hpp"
-#include "ActsExamples/Geant4Detector/MockupSectorBuilder.hpp"
+#include "ActsExamples/MuonSpectrometerMockupDetector/MockupSectorBuilder.hpp"
+
+#include <memory>
+
 using namespace Acts;
 using namespace ActsExamples;
+
+struct StrawSelector {
+  /// Call operator
+  /// @param sf The input surface to be checked
+  bool operator()(const Surface& sf) const {
+    return (sf.type() == Surface::Straw);
+  }
+};
 
 int main() {
   auto mockup_config = MockupSectorBuilder::Config();
@@ -51,11 +76,17 @@ int main() {
   std::vector<std::shared_ptr<Acts::Experimental::DetectorVolume>>
       detector_volumes = {};
 
-  detector_volumes.push_back(detectorVolume_middle_chamber);
   detector_volumes.push_back(detectorVolume_inner_chamber);
+  detector_volumes.push_back(detectorVolume_middle_chamber);
   detector_volumes.push_back(detectorVolume_outer_chamber);
 
   auto detectorVolume_sector = mockup_builder.buildSector(detector_volumes);
 
   mockup_builder.drawSector(detectorVolume_sector, "sector_test.obj");
+
+  auto detector_sector = Acts::Experimental::Detector::makeShared(
+      "Detector",
+      std::vector<std::shared_ptr<Acts::Experimental::DetectorVolume>>(
+          {detectorVolume_sector}),
+      Acts::Experimental::tryAllVolumes());
 }
