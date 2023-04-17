6ee459188493f7e6e89327ec28c3db1ad89a77c8
Amperiadou Dimitra <63058524+dimitra97@users.noreply.github.com>
2023-04-17 17:33:15 +0200

    refactor: Mockup builder files location (#2029)
    This PR locates the files that hold the class for the construction of the muon spectrometer mock up into another folder.

--- Examples/Run/Geant4/TestMockupBuilder.cpp
+++ Examples/Run/Geant4/TestMockupBuilder.cpp
@@ -6,13 +6,38 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "Acts/Detector/Detector.hpp"
+#include "Acts/Detector/DetectorVolume.hpp"
+#include "Acts/Detector/Portal.hpp"
+#include "Acts/Detector/PortalHelper.hpp"
+#include "Acts/MagneticField/ConstantBField.hpp"
+#include "Acts/MagneticField/MagneticFieldContext.hpp"
+#include "Acts/Navigation/DetectorVolumeFinders.hpp"
+#include "Acts/Navigation/NavigationState.hpp"
+#include "Acts/Navigation/NextNavigator.hpp"
+#include "Acts/Navigation/SurfaceCandidatesUpdators.hpp"
+#include "Acts/Propagator/AbortList.hpp"
+#include "Acts/Propagator/EigenStepper.hpp"
+#include "Acts/Propagator/Propagator.hpp"
+#include "Acts/Propagator/SurfaceCollector.hpp"
 #include "Acts/Visualization/GeometryView3D.hpp"
 #include "Acts/Visualization/IVisualization3D.hpp"
 #include "Acts/Visualization/ObjVisualization3D.hpp"
 #include "ActsExamples/Geant4Detector/Geant4Detector.hpp"
-#include "ActsExamples/Geant4Detector/MockupSectorBuilder.hpp"
+#include "ActsExamples/MuonSpectrometerMockupDetector/MockupSectorBuilder.hpp"
+
+#include <memory>
+
 using namespace Acts;
 using namespace ActsExamples;
+
+struct StrawSelector {
+  /// Call operator
+  /// @param sf The input surface to be checked
+  bool operator()(const Surface& sf) const {
+    return (sf.type() == Surface::Straw);
+  }
+};
 
 int main() {
   auto mockup_config = MockupSectorBuilder::Config();
@@ -21,7 +46,9 @@ int main() {
   auto mockup_chamberConfig_middle = MockupSectorBuilder::ChamberConfig();
   auto mockup_chamberConfig_outer = MockupSectorBuilder::ChamberConfig();
 
-  mockup_config.gdmlPath = "./MuonChamber.gdml";
+  mockup_config.gdmlPath =
+      " ../../../../acts/Examples/Detectors/MuonSpectrometerMockupDetector/"
+      "MuonChamber.gdml";
   mockup_config.NumberOfSectors = 8;
 
   mockup_chamberConfig_inner.name = "Inner_Detector_Chamber";
@@ -51,11 +78,17 @@ int main() {
   std::vector<std::shared_ptr<Acts::Experimental::DetectorVolume>>
       detector_volumes = {};
 
-  detector_volumes.push_back(detectorVolume_middle_chamber);
   detector_volumes.push_back(detectorVolume_inner_chamber);
+  detector_volumes.push_back(detectorVolume_middle_chamber);
   detector_volumes.push_back(detectorVolume_outer_chamber);
 
   auto detectorVolume_sector = mockup_builder.buildSector(detector_volumes);
 
   mockup_builder.drawSector(detectorVolume_sector, "sector_test.obj");
+
+  auto detector_sector = Acts::Experimental::Detector::makeShared(
+      "Detector",
+      std::vector<std::shared_ptr<Acts::Experimental::DetectorVolume>>(
+          {detectorVolume_sector}),
+      Acts::Experimental::tryAllVolumes());
 }
