8975c2b89359c147b563afa4d95edc52ecf4050a
Paul Gessinger <hello@paulgessinger.com>
2023-04-17 15:55:58 +0200

    actually add the tests

---
+++ Tests/CommonHelpers/Acts/Tests/CommonHelpers/NonCompileTestHelpers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define ACTS_DOES_NOT_COMPILE_SUITE_BEGIN(name) int main() {
+#define ACTS_DOES_NOT_COMPILE_SUITE_END() }
+
+#define ACTS_DOES_NOT_COMPILE_BEGIN(name) {
+#define ACTS_DOES_NOT_COMPILE_END() }
