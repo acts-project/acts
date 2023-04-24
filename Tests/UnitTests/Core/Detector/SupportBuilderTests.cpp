320be7976df227d12dac0c95b5d6838b7880a2d4
Andreas Salzburger <asalzburger@gmail.com>
2023-04-24 14:00:53 +0200

    feat:  adding layer building infrastructure for DetectorVolume objects (#2043)
    This PR adds the infrastructure to build layer structures to be filled
    into DetectorVoluems, it relies on the IndexeSurfaces infrastructure.
    
    ### In **Core**:
    - a new Interfaces ```IInternalStructureBuilder``` is defined, which
    provides the internal objects and updators of a `DetectorVolume`
    - `Acts::Surface` objects are provided to this, e.g through a new
    `KDTree` provider
    - The `LayerStructureBuilder` class (which extends
    IInternalStructueBuilder) then creates
      - the binned structure of the surfaces
      - additional support surfaces (via a new Helper `SupportBuilder`)
    
    *Note*: if configured the support surfaces can be approximated by `N`
    planar surfaces in order to allow intersection vectorisation.
    
    ### In **Tests**:
    
    - All of the classes are unit tested extensively
    
    ### As **Visualizations**:
    
    - An indexed Disc from the UnitTests:
    ![Screenshot 2023-04-14 at 13 27
    29](https://user-images.githubusercontent.com/26623879/233130277-1cc87b43-4794-4529-bd57-bba176ad41d0.png)
    
    - A cylindrical support surfrace modelled as planes:
    
    ![Screenshot 2023-04-18 at 14 21
    44](https://user-images.githubusercontent.com/26623879/233129927-19b400b1-1945-4a61-b404-40a809845101.png)
    
    - A sectoral disk-like support surface modelled as trapezoids:
    
    ![Screenshot 2023-04-18 at 14 28
    27](https://user-images.githubusercontent.com/26623879/233130161-09d8e480-1205-42ec-b75f-39687fc83172.png)
    
    ###  In **Examples**:
    
    In order to enable visual inspection of the surface binning generated, a
    new `SurfaceIndexing` inspector in
    `Examples/Detectors/DetectorInspectors` with appropriate python bindings
    is added, here is an example for the OpenDataDetector negative short
    strip endcap.
    
    ```python
    >>> import acts
    >>> from acts import examples as aex
    >>> ci = aex.CylindricalDetectorIndexing("odd-sensitives.json")
    ```
    
    Now inspect a possible indexing:
    ```python
    ci.inspect("ss_outermost_disk_n", [[-3100., -2900. ], [200., 700.]] , [["binR", "open", "equidistant", 4, [200., 700.], 0], ["binPhi", "closed", "equidistant", 24, [ -3.142, 3.142 ], 1] ], [] )
    ```
    
    With the following results:
    ![Screenshot 2023-04-19 at 17 38
    37](https://user-images.githubusercontent.com/26623879/233131669-ff981bfe-2874-4199-a125-1a327a8b4eb4.png)
    
    Now, the same with adapted inner radius to 240 and overcommitting the
    phi binning:
    
    ```python
    >>> ci.inspect("ss_outermost_disk_n", [[-3100., -2900. ], [200., 700.]] , [["binR", "open", "equidistant", 4, [240., 700.], 0], ["binPhi", "closed", "equidistant", 48, [ -3.142, 3.142 ], 1] ], [] )
    ```
    
    ![Screenshot 2023-04-19 at 17 55
    03](https://user-images.githubusercontent.com/26623879/233132671-ffad9ceb-e9c2-4eb8-866f-12ce4d5dbe60.png)

---
+++ Tests/UnitTests/Core/Detector/SupportBuilderTests.cpp
@@ -0,0 +1,142 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2022 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Detector/SupportBuilder.hpp"
+#include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
+
+Acts::GeometryContext tContext;
+
+BOOST_AUTO_TEST_SUITE(Detector)
+
+BOOST_AUTO_TEST_CASE(CylindricalSupport) {
+  // As a single cylinder
+  auto singleSupport = Acts::Experimental::SupportBuilder::cylindricalSupport(
+      Acts::Transform3::Identity(), {100., 400., M_PI, 0., 0., 0.}, 1u);
+  BOOST_CHECK(singleSupport.size() == 1u);
+  BOOST_CHECK(singleSupport[0u]->type() ==
+              Acts::Surface::SurfaceType::Cylinder);
+
+  // As a split cylinder
+  auto splitSupport = Acts::Experimental::SupportBuilder::cylindricalSupport(
+      Acts::Transform3::Identity(), {100., 400., M_PI, 0., 0., 0.}, 32u);
+  BOOST_CHECK(splitSupport.size() == 32u);
+  for (const auto& ss : splitSupport) {
+    BOOST_CHECK(ss->type() == Acts::Surface::SurfaceType::Plane);
+  }
+
+  // As a split cylinder - sectoral
+  auto splitSectoralSupport =
+      Acts::Experimental::SupportBuilder::cylindricalSupport(
+          Acts::Transform3::Identity(),
+          {100., 400., 0.25 * M_PI, 0.75 * M_PI, 0., 0.}, 128u);
+  BOOST_CHECK(splitSectoralSupport.size() == 128u);
+  for (const auto& ss : splitSectoralSupport) {
+    BOOST_CHECK(ss->type() == Acts::Surface::SurfaceType::Plane);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(DiscSupport) {
+  // As a single disc
+  auto singleSupport = Acts::Experimental::SupportBuilder::discSupport(
+      Acts::Transform3::Identity(), {100., 400., M_PI, 0.}, 1u);
+  BOOST_CHECK(singleSupport.size() == 1u);
+  BOOST_CHECK(singleSupport[0u]->type() == Acts::Surface::SurfaceType::Disc);
+
+  // As a split disc
+  auto splitSupport = Acts::Experimental::SupportBuilder::discSupport(
+      Acts::Transform3::Identity(), {100., 400., M_PI, 0.}, 32u);
+  BOOST_CHECK(splitSupport.size() == 32u);
+  for (const auto& ss : splitSupport) {
+    BOOST_CHECK(ss->type() == Acts::Surface::SurfaceType::Plane);
+  }
+
+  // As a split disc - sectoral
+  auto splitSectoralSupport = Acts::Experimental::SupportBuilder::discSupport(
+      Acts::Transform3::Identity(), {100., 400., 0.5 * M_PI, 0.}, 16u);
+  BOOST_CHECK(splitSectoralSupport.size() == 16u);
+  for (const auto& ss : splitSectoralSupport) {
+    BOOST_CHECK(ss->type() == Acts::Surface::SurfaceType::Plane);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(addCylinderSupport) {
+  std::vector<std::shared_ptr<Acts::Surface>> lSurfaces;
+  std::vector<size_t> assignToAll;
+
+  // The Extent
+  Acts::Extent lExtent;
+  lExtent.set(Acts::binR, 100., 110.);
+  lExtent.set(Acts::binZ, -400., -400.);
+
+  // Get the main support parameters:
+  // - doff .. offset (in r.z)
+  // - demin, demax .. envelop min, max (in z,r)
+  // - dphimin, dphimin .. envelop min, max (in phi)
+  std::array<Acts::ActsScalar, 5u> sValues = {10, 5, 5, 0., 0.};
+  // Add a single
+  Acts::Experimental::SupportBuilder::addSupport(
+      lSurfaces, assignToAll, lExtent, Acts::Surface::SurfaceType::Cylinder,
+      sValues, std::nullopt, 1u);
+  BOOST_CHECK(lSurfaces.size() == 1u);
+  BOOST_CHECK(assignToAll.size() == 1u);
+  BOOST_CHECK(assignToAll[0u] == 0u);
+  // The radius of the newly created suport surface should be 10 out of the
+  // maximum
+  CHECK_CLOSE_ABS(lSurfaces[0u]->bounds().values()[0u], 120, 1e-3);
+
+  // Clear up for the next test
+  lSurfaces.clear();
+  assignToAll.clear();
+  // Add split surfaces
+  Acts::Experimental::SupportBuilder::addSupport(
+      lSurfaces, assignToAll, lExtent, Acts::Surface::SurfaceType::Cylinder,
+      sValues, std::nullopt, 16u);
+  BOOST_CHECK(lSurfaces.size() == 16u);
+  BOOST_CHECK(assignToAll.empty());
+}
+
+BOOST_AUTO_TEST_CASE(addDiscSupport) {
+  std::vector<std::shared_ptr<Acts::Surface>> lSurfaces;
+  std::vector<size_t> assignToAll;
+
+  // The Extent
+  Acts::Extent lExtent;
+  lExtent.set(Acts::binR, 100., 400.);
+  lExtent.set(Acts::binZ, -110., -100.);
+
+  // Get the main support parameters:
+  // - doff .. offset (in r.z)
+  // - demin, demax .. envelop min, max (in z,r)
+  // - dphimin, dphimin .. envelop min, max (in phi)
+  std::array<Acts::ActsScalar, 5u> sValues = {-10, 10, 20, 0., 0.};
+  // Add a single
+  Acts::Experimental::SupportBuilder::addSupport(
+      lSurfaces, assignToAll, lExtent, Acts::Surface::SurfaceType::Disc,
+      sValues, std::nullopt, 1u);
+  BOOST_CHECK(lSurfaces.size() == 1u);
+  BOOST_CHECK(assignToAll.size() == 1u);
+  BOOST_CHECK(assignToAll[0u] == 0u);
+  // The radius of the newly created suport surface should be 10 out of the
+  // minimum
+  CHECK_CLOSE_ABS(lSurfaces[0u]->transform(tContext).translation().z(), -120,
+                  1e-3);
+
+  // Clear up for the next test
+  lSurfaces.clear();
+  assignToAll.clear();
+  // Add split surfaces
+  Acts::Experimental::SupportBuilder::addSupport(
+      lSurfaces, assignToAll, lExtent, Acts::Surface::SurfaceType::Disc,
+      sValues, std::nullopt, 16u);
+  BOOST_CHECK(lSurfaces.size() == 16u);
+  BOOST_CHECK(assignToAll.empty());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
