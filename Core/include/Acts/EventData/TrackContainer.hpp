4ca3e3389f6bd8a438f2a84d04bb26dca3846343
Paul Gessinger <paul.gessinger@cern.ch>
2023-04-26 10:32:01 +0200

    chore: Bump clang-format to version 14 (#2060)
    This should give us better formatting with new language constructs like
    concepts in C++20.

--- Core/include/Acts/EventData/TrackContainer.hpp
+++ Core/include/Acts/EventData/TrackContainer.hpp
@@ -97,7 +97,9 @@ class TrackContainer {
   /// Get a const track proxy for a track index
   /// @param itrack the track index in the container
   /// @return A const track proxy for the index
-  ConstTrackProxy getTrack(IndexType itrack) const { return {*this, itrack}; }
+  ConstTrackProxy getTrack(IndexType itrack) const {
+    return {*this, itrack};
+  }
 
   /// Get a mutable track proxy for a track index
   /// @param itrack the track index in the container
@@ -109,7 +111,9 @@ class TrackContainer {
 
   /// Get the size of the track container
   /// @return the sixe
-  constexpr IndexType size() const { return m_container->size_impl(); }
+  constexpr IndexType size() const {
+    return m_container->size_impl();
+  }
 
   /// Add a track to the container. Note this only creates the logical track and
   /// allocates memory. You can combine this with @c getTrack to obtain a track proxy
@@ -157,7 +161,9 @@ class TrackContainer {
 
   /// Get a const reference to the track container backend
   /// @return a const reference to the backend
-  const auto& container() const { return *m_container; }
+  const auto& container() const {
+    return *m_container;
+  }
 
   /// Get a mutable reference to the track state container backend
   /// @return a mutable reference to the backend
@@ -175,11 +181,15 @@ class TrackContainer {
 
   /// Get a const reference to the track state container backend
   /// @return a const reference to the backend
-  const auto& trackStateContainer() const { return *m_traj; }
+  const auto& trackStateContainer() const {
+    return *m_traj;
+  }
 
   /// Retrieve the holder of the track state container
   /// @return The track state container including it's holder
-  const auto& trackStateContainerHolder() const { return m_traj; }
+  const auto& trackStateContainerHolder() const {
+    return m_traj;
+  }
 
   /// Get a mutable iterator to the first track in the container
   /// @return a mutable iterator to the first track
