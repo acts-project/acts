bc5a1f69f89b4f1b5585c142b8e8ff0893073497
Lara <lara.calic@hep.lu.se>
2023-05-04 16:24:58 +0200

    adding hpp and cpp files-xternal alignment

--- Examples/Detectors/ContextualDetector/include/ActsExamples/ContextualDetector/ExternallyAlignedDetectorElement.hpp
+++ Examples/Detectors/ContextualDetector/include/ActsExamples/ContextualDetector/ExternallyAlignedDetectorElement.hpp
@@ -1,43 +1,136 @@
-// This file is part of the Acts project.
-//
-// Copyright (C) 2019 CERN for the benefit of the Acts project
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 #pragma once
 
 #include "Acts/Definitions/Algebra.hpp"
-#include "Acts/Geometry/GeometryContext.hpp"
 #include "Acts/Geometry/GeometryIdentifier.hpp"
+#include "Acts/Geometry/GeometryContext.hpp"
+#include "Acts/Utilities/Logger.hpp"
+#include "ActsExamples/ContextualDetector/AlignmentDecorator.hpp"
+#include "ActsExamples/ContextualDetector/ExternallyAlignedDetectorElement.hpp"
+#include "ActsExamples/Framework/AlgorithmContext.hpp"
+#include "ActsExamples/Framework/IContextDecorator.hpp"
+#include "ActsExamples/Framework/IContextDecorator.hpp"
 #include "Acts/Plugins/Identification/IdentifiedDetectorElement.hpp"
 #include "Acts/Plugins/Identification/Identifier.hpp"
 #include "Acts/Surfaces/Surface.hpp"
 #include "ActsExamples/GenericDetector/GenericDetectorElement.hpp"
 
 #include <map>
+#include <vector>
+
+namespace Acts {
+class TrackingGeometry;
+}
 
 namespace ActsExamples {
 
 namespace Contextual {
 
-/// @class ExternallyAlignedDetectorElement extends GenericDetectorElement
+/// @brief A mockup service that rotates the modules in a
+/// simple tracking geometry
 ///
-/// This is a lightweight type of detector element,
-/// it simply implements the base class.
-///
-/// The ExternallyAlignedDetectorElement demonstrates how a GeometryContext
-/// can be used if it carries the entire set of Transforms through
-/// the program flow.
-///
-/// The nominal transform is only used to once create the alignment
-/// store and then in a contextual call the actual detector element
-/// position is taken from the alignment Store.
-///
-/// In this simple implementation, it does rely on the Identifier
-/// to be orderded from 0 to N-1, as the identifier is simply taken
-/// as a vector index for the alignment store
+/// It acts on the PayloadDetectorElement, i.e. the
+/// geometry context carries the full transform store (payload)
+class ExternalAlignmentDecorator : public AlignmentDecorator {
+ public:
+  /// @brief nested configuration struct
+  struct Config : public AlignmentDecorator::Config {
+    /// The trackng geometry
+    std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry = nullptr;
+  };
+
+  /// Constructor
+  ///
+  /// @param cfg Configuration struct
+  /// @param logger The logging framework
+  ExternalAlignmentDecorator(
+      const Config& cfg,
+      std::unique_ptr<const Acts::Logger> logger = Acts::getDefaultLogger(
+          "ExternalAlignmentDecorator", Acts::Logging::INFO));
+  ExternalAlignmentDecorator::ExternalAlignmentDecorator(const Config& cfg,
+                                                       const std::string& logFileName,
+                                                       std::unique_ptr<const Acts::Logger> logger)
+    : AlignmentDecorator(cfg, std::move(logger)), m_cfg(cfg) {
+  auto fileLogger = std::make_unique<Acts::FileLogger>(logFileName);
+  logger().addLogger(std::move(fileLogger));
+  }
+
+  /// Virtual destructor
+  ~ExternalAlignmentDecorator() override = default;
+
+  /// @brief decorates (adds, modifies) the AlgorithmContext
+  /// with a geometric rotation per event
+  ///
+  /// @note If decorators depend on each other, they have to be
+  /// added in order.
+  ///
+  /// @param context the bare (or at least non-const) Event context
+  ProcessCode decorate(AlgorithmContext& context) override;
+
+  /// @brief decorator name() for screen output
+  const std::string& name() const override { return m_name; }
+
+ private:
+  Config m_cfg;                                  ///< the configuration class
+  std::unique_ptr<const Acts::Logger> m_logger;  ///!< the logging instance
+  std::string m_name = "ExternalAlignmentDecorator";
+
+  /// Map of nominal transforms
+  std::vector<Acts::Transform3> m_nominalStore;
+
+  std::unordered_map<
+      unsigned int,
+      std::shared_ptr<ExternallyAlignedDetectorElement::AlignmentStore>>
+      m_activeIovs;
+
+  std::mutex m_iovMutex;
+
+  size_t m_eventsSeen{0};
+
+  /// Private access to the logging instance
+  const Acts::Logger& logger() const { return *m_logger; }
+
+  /// Populate the nominal transforms
+  /// this parses the TrackingGeometry and fills the nominal store
+  ///
+  /// @param tGeometry the tracking geometry
+  void parseGeometry(const Acts::TrackingGeometry& tGeometry);
+};
+}  // namespace Contextual
+
+
+template <typename SurfaceT>
+void fillTransforms(std::map<size_t, Acts::Transform3>& aStore, 
+		     const Acts::TrackingGeometry& trackingGeometry, 
+		     const SurfaceT* surface, 
+		     const Acts::GeometryContext& nominalCtx, 
+		     std::vector<ResultList>& resultList)
+{
+	int layer = surface -> layer(); 
+	int volume = surface -> volume(); 
+	
+	Acts::Transform3 result=surface->transform(nominalCtx); 
+	
+	struct ResultList{
+		int layer; 
+		int volume; 
+		Acts::Transform3 result; 
+	};
+	
+	
+	ResultList list; 
+	list.layer = layer; 
+	list.volume = volume; 
+	list.result = result; 
+	resultList.push_back(list); 
+	
+	
+	const auto* detectorElement = surface -> associatedDetectorElement(); 
+	auto alignableElement = dynamic_cast<const Acts::ExternallyAlignedDetectorElement*>(detectorElement); 
+	if(alignableElement) { 
+	     aStore[alignableElement->identifier()] = result;
+	 }
+}  
+
 class ExternallyAlignedDetectorElement
     : public Generic::GenericDetectorElement {
  public:
@@ -86,3 +179,4 @@ inline const Acts::Transform3& ExternallyAlignedDetectorElement::transform(
 
 }  // end of namespace Contextual
 }  // end of namespace ActsExamples
+
--- Examples/Detectors/ContextualDetector/src/ExternalAlignmentDecorator.cpp
+++ Examples/Detectors/ContextualDetector/src/ExternalAlignmentDecorator.cpp
@@ -1,14 +1,9 @@
-// This file is part of the Acts project.
-//
-// Copyright (C) 2019 CERN for the benefit of the Acts project
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 #include "ActsExamples/ContextualDetector/ExternalAlignmentDecorator.hpp"
+<<<<<<< HEAD
 
 #include "Acts/Geometry/GeometryContext.hpp"
+=======
+>>>>>>> 2c318123d (adding hpp and cpp files-xternal alignment)
 #include "Acts/Geometry/TrackingGeometry.hpp"
 #include "Acts/Surfaces/SurfaceArray.hpp"
 #include "ActsExamples/ContextualDetector/ExternallyAlignedDetectorElement.hpp"
@@ -20,6 +15,14 @@
 #include <thread>
 #include <utility>
 
+
+// ExternalAlignmentDecorator -- class that is responsible for applying the external alignment output to trackingGeometry
+//                            -- basically based on the external alignment measurements this class can modify on the geometry of detector 
+// TrackingGeometry  - geometry of the detector 
+
+// this constructor ExternalAlignmentDecorator takes 2 parameters: Config and pointer to Logger object
+// initalizing the variables: m_cfg  && m_logger (respect to some arguments)
+// inside the if statement we're checking if m_cfg is not null, then we will parseGeometry function that will populates the member variable m_nominalStore  with transforms for the surfaces in the tracking geometry
 ActsExamples::Contextual::ExternalAlignmentDecorator::
     ExternalAlignmentDecorator(const Config& cfg,
                                std::unique_ptr<const Acts::Logger> logger)
@@ -30,6 +33,9 @@ ActsExamples::Contextual::ExternalAlignmentDecorator::
   }
 }
 
+
+// decorate() - takes object AlgorithmContext as argument and returns an ActsExamples::ProcessCode enum
+// m_iovMutex() - 
 ActsExamples::ProcessCode
 ActsExamples::Contextual::ExternalAlignmentDecorator::decorate(
     AlgorithmContext& context) {
@@ -37,6 +43,8 @@ ActsExamples::Contextual::ExternalAlignmentDecorator::decorate(
   std::lock_guard lock{m_iovMutex};
 
   // In which iov batch are we?
+  // IOV - Interval of Validity
+
   unsigned int iov = context.eventNumber / m_cfg.iovSize;
   ACTS_VERBOSE("IOV handling in thread " << std::this_thread::get_id() << ".");
   ACTS_VERBOSE("IOV resolved to " << iov << " - from event "
@@ -44,6 +52,8 @@ ActsExamples::Contextual::ExternalAlignmentDecorator::decorate(
 
   m_eventsSeen++;
 
+//  checking if m_cfg.randomNumberSvc is not null, we are doing check in if the current batch iov can be find in m_activeIovs map
+// basically, if the IOV can be find 
   if (m_cfg.randomNumberSvc != nullptr) {
     if (auto it = m_activeIovs.find(iov); it != m_activeIovs.end()) {
       // Iov is already present, update last accessed
@@ -60,8 +70,11 @@ ActsExamples::Contextual::ExternalAlignmentDecorator::decorate(
                               << context.eventNumber
                               << ", emulate new alignment.");
 
+     
+
       // Create an algorithm local random number generator
       RandomEngine rng = m_cfg.randomNumberSvc->spawnGenerator(context);
+
 
       alignmentStore->transforms = m_nominalStore;  // copy nominal alignment
       for (auto& tForm : alignmentStore->transforms) {
@@ -102,19 +115,64 @@ void ActsExamples::Contextual::ExternalAlignmentDecorator::parseGeometry(
   size_t nTransforms = 0;
   tGeometry.visitSurfaces([&nTransforms](const auto*) { ++nTransforms; });
 
+
+// this is where initialization is done - object nominalCtx (type Acts::GeometryContext) with 
+// with an empty context type ExternallyAlignedDetectorElement::ContextType
+
+// GeometryContext - object where we can find an info about geometry -- basically the nominalCtx holds the context geomtry without applying external alignment 
   Acts::GeometryContext nominalCtx{
       ExternallyAlignedDetectorElement::ContextType{}};
-
-  // Collect the surfacas into the nominal store
+//L111: creating a 'empty context type' based on the geometry that has been used 
+  
+  // Collect the surfacas into the nominal store -- > or in other words, vector that stores info about nominal alignment for detectors elements
+  // typical declaration of a  std::vector object aStore with paramateres (nTransforms - number of elements, type Acts::Transform3)
+  // for each element, we have init identity transformation
   std::vector<Acts::Transform3> aStore(nTransforms,
                                        Acts::Transform3::Identity());
 
+// function that takes pointer to a surface object, and return void 
+// aStore - vector of Acts::Transform3 objects and nominalCtx passed by reference because it will change 
   auto fillTransforms = [&aStore, &nominalCtx](const auto* surface) -> void {
-    auto alignableElement =
+    // it first casts the associatedDetectorElement() of the surface pointer to a const ExternallyAlignedDetectorElement*, and then uses the identifier of that element to index into the aStore vector and set the corresponding Transform3 object to the result of calling transform() on the surface object with the nominalCtx context. Essentially, this lambda function is used to populate the m_nominalStore vector with the nominal alignment of the surfaces in the tracking geometry.
         dynamic_cast<const ExternallyAlignedDetectorElement*>(
             surface->associatedDetectorElement());
     aStore[alignableElement->identifier()] = surface->transform(nominalCtx);
   };
+
+
+// TOdo task: figure out how identifier() can access to specific layer and volume, an safe that information to new list where can access it whenever we want 
+// first attemp: 
+ auto fillTransforms = [&aStore, &nominalCtx, &resultList](const auto* surface) -> void {
+    
+    // we have to somehow retrieve the vol + sur info from surface/volume object
+    int layer = surface->layer();
+    int volume = surface->volume();
+
+    // setting up the result of calling a method transform() on the surface object 
+    Transform3 result = surface->transform(nominalCtx);
+
+    // defining the structure in which we could store the all the relevant informations (surface, volume, etc)
+    struct ResultList {
+        int layer;
+        int volume;
+        Transform3 result;
+    };
+
+    // all the important informations regarding vol, layers, transformation resutls can be save into the list 
+   ResultList list;
+    list.layer = layer;
+    list.volume = volume;
+    list.result = result;
+
+    // Adding new results to the list
+    resultList.push_back(list);
+
+    // Index into the aStore vector and set the corresponding Transform3 object.
+    dynamic_cast<const ExternallyAlignedDetectorElement*>(
+            surface->associatedDetectorElement());
+    aStore[alignableElement->identifier()] = result;
+};
+
 
   tGeometry.visitSurfaces(fillTransforms);
   m_nominalStore = std::move(aStore);
