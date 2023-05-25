afe979a2a9d817a5b84de53c2b5ceec7c328c41f
Andreas Salzburger <asalzburger@gmail.com>
2023-05-25 09:27:41 +0200

    Update Core/include/Acts/Detector/VolumeStructureBuilder.hpp
    Co-authored-by: russofel <72298366+russofel@users.noreply.github.com>

--- Core/include/Acts/Detector/VolumeStructureBuilder.hpp
+++ Core/include/Acts/Detector/VolumeStructureBuilder.hpp
@@ -58,7 +58,7 @@ class VolumeStructureBuilder : public IExternalStructureBuilder {
   /// configuration object
   Config m_cfg;
 
-  /// Private acces method to the logger
+  /// Private access method to the logger
   const Logger& logger() const { return *m_logger; }
 
   /// logging instance
