9e78f4fcbac8f6d2c4b5d745d4010377c065fe1f
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2023-05-30 16:29:19 +0200

    add fastSeeding == True to full_chain_itk

--- Examples/Scripts/Python/full_chain_itk.py
+++ Examples/Scripts/Python/full_chain_itk.py
@@ -87,7 +87,7 @@ addSeeding(
     else TruthSeedRanges(),
     seedingAlgorithm=SeedingAlgorithm.Default,
     *acts.examples.itk.itkSeedingAlgConfig(
-        acts.examples.itk.InputSpacePointsType.PixelSpacePoints
+        acts.examples.itk.InputSpacePointsType.PixelSpacePoints, fastSeeding=True
     ),
     geoSelectionConfigFile=geo_dir / "itk-hgtd/geoSelection-ITk.json",
     outputDirRoot=outputDir,
