63fb9bea5272f1521dea6656b5455e8d8366d355
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2023-06-12 10:20:06 +0200

    rest mass selection and pythia printing

--- Examples/Algorithms/GeneratorsPythia8/ActsExamples/Generators/Pythia8ProcessGenerator.hpp
+++ Examples/Algorithms/GeneratorsPythia8/ActsExamples/Generators/Pythia8ProcessGenerator.hpp
@@ -35,6 +35,10 @@ class Pythia8Generator : public EventGenerator::ParticlesGenerator {
     double cmsEnergy = 14 * Acts::UnitConstants::TeV;
     /// Additional Pythia8 settings.
     std::vector<std::string> settings = {{"HardQCD:all = on"}};
+    /// Let pythia print summarized event info
+    bool printShortEventListing = false;
+    /// Let pythia print detailed event info
+    bool printLongEventListing = false;
   };
 
   Pythia8Generator(const Config& cfg, Acts::Logging::Level lvl);
