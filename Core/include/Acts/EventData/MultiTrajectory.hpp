de665e0b041205b659f6d0f5324d623830fe3c19
Paul Gessinger <paul.gessinger@cern.ch>
2023-06-14 15:27:23 +0200

    refactor: TrackStateType safety, bitsize, conversion (#2201)
    This makes TrackStateType check it's internal pointer, fixes the size of the bitset used for manipulation, and allow auto-conversion from mutable to const.

--- Core/include/Acts/EventData/MultiTrajectory.hpp
+++ Core/include/Acts/EventData/MultiTrajectory.hpp
@@ -53,6 +53,8 @@ class ConstTrackStateType;
 class TrackStateType {
  public:
   using raw_type = std::uint64_t;
+  static constexpr std::size_t kRawBits =
+      std::numeric_limits<std::make_unsigned<raw_type>::type>::digits;
   /// Constructor from a reference to the underlying value container
   /// @param raw the value container
   TrackStateType(raw_type& raw) : m_raw{&raw} {}
@@ -61,6 +63,7 @@ class TrackStateType {
   /// @param other the other set of flags to assign
   /// @return this object
   TrackStateType& operator=(const TrackStateType& other) {
+    assert(other.m_raw != nullptr);
     *m_raw = *other.m_raw;
     return *this;
   }
@@ -70,11 +73,15 @@ class TrackStateType {
   /// @return this object
   TrackStateType& operator=(const ConstTrackStateType& other);
 
+  /// Automatically convert to const track state type
+  operator ConstTrackStateType();
+
   /// Return if the bit at position @p pos is 1
   /// @param pos the bit position
   /// @return if the bit at @p pos is one or not
   bool test(std::size_t pos) const {
-    std::bitset<sizeof(raw_type)> bs{*m_raw};
+    assert(m_raw != nullptr);
+    std::bitset<kRawBits> bs{*m_raw};
     return bs.test(pos);
   }
 
@@ -82,7 +89,8 @@ class TrackStateType {
   /// @param pos the position of the bit to change
   /// @param value the value to change the bit to
   void set(std::size_t pos, bool value = true) {
-    std::bitset<sizeof(raw_type)> bs{*m_raw};
+    assert(m_raw != nullptr);
+    std::bitset<kRawBits> bs{*m_raw};
     bs.set(pos, value);
     *m_raw = bs.to_ullong();
   }
@@ -100,6 +108,8 @@ class TrackStateType {
 class ConstTrackStateType {
  public:
   using raw_type = std::uint64_t;
+  static constexpr std::size_t kRawBits =
+      std::numeric_limits<std::make_unsigned<raw_type>::type>::digits;
 
   /// Constructor from a reference to the underlying value container
   /// @param raw the value container
@@ -109,8 +119,20 @@ class ConstTrackStateType {
   /// @param pos the bit position
   /// @return if the bit at @p pos is one or not
   bool test(std::size_t pos) const {
-    std::bitset<sizeof(raw_type)> bs{*m_raw};
+    assert(m_raw != nullptr);
+    std::bitset<kRawBits> bs{*m_raw};
     return bs.test(pos);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, ConstTrackStateType t) {
+    assert(t.m_raw != nullptr);
+    std::bitset<kRawBits> bs{*t.m_raw};
+    std::bitset<TrackStateFlag::NumTrackStateFlags> trunc;
+    for (size_t i = 0; i < TrackStateFlag::NumTrackStateFlags; i++) {
+      trunc[i] = bs[i];
+    }
+    os << "MPOHMS=" << trunc;
+    return os;
   }
 
  private:
@@ -120,8 +142,12 @@ class ConstTrackStateType {
 
 inline TrackStateType& TrackStateType::operator=(
     const ConstTrackStateType& other) {
+  assert(other.m_raw != nullptr);
   *m_raw = *other.m_raw;
   return *this;
+}
+inline TrackStateType::operator ConstTrackStateType() {
+  return {*m_raw};
 }
 
 // using TrackStateType = std::bitset<TrackStateFlag::NumTrackStateFlags>;
