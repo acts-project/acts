5e2d6d088e3fdffc997f1c239b09ee4e9e6435f4
andiwand <stefl.andreas@gmail.com>
2023-06-16 15:40:33 +0200

    fix hepmc

--- Examples/Python/src/Geant4HepMC3.cpp
+++ Examples/Python/src/Geant4HepMC3.cpp
@@ -7,6 +7,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "Acts/Plugins/Python/Utilities.hpp"
+#include "ActsExamples/Geant4/DetectorConstructionFactory.hpp"
 #include "ActsExamples/Geant4HepMC/EventRecording.hpp"
 
 #include <memory>
