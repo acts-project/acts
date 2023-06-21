a2eb1f362652fba0aeb84d2a7307d4ed3ec6e13b
Paul Gessinger <paul.gessinger@cern.ch>
2023-06-21 09:47:47 +0200

    docs: Ignore tavianator.com in linkcheck (#2225)

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,11 @@ myst_enable_extensions = ["dollarmath", "colon_fence", "amsmath"]
 myst_heading_anchors = 3
 
 linkcheck_retries = 5
-linkcheck_ignore = [r"https://doi.org/.*", r"https://cernvm.cern.ch/.*"]
+linkcheck_ignore = [
+    r"https://doi.org/.*",
+    r"https://cernvm.cern.ch/.*",
+    r"https://tavianator.com/.*",
+]
 
 # -- Options for HTML output --------------------------------------------------
 
