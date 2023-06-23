d938c2e95ee84964f3e0e3d50cdd1567b92ab9be
andiwand <stefl.andreas@gmail.com>
2023-06-23 11:02:27 +0200

    test non overlapping clusters

--- Tests/UnitTests/Examples/Algorithms/Digitization/ModuleClustersTests.cpp
+++ Tests/UnitTests/Examples/Algorithms/Digitization/ModuleClustersTests.cpp
@@ -18,7 +18,33 @@ using namespace ActsExamples;
 
 namespace {
 
-auto testDigitizedParameters(bool merge) {
+DigitizedParameters makeDigitizationParameters(const Vector2 &position,
+                                               const Vector2 &variance,
+                                               const BinUtility &binUtility) {
+  auto [binX, binY, _] =
+      binUtility.binTriple((Vector3() << position, 0).finished());
+  Channelizer::Bin2D bin = {(Channelizer::Bin2D::value_type)binX,
+                            (Channelizer::Bin2D::value_type)binY};
+  Channelizer::Segment2D segment = {position, position};
+  double activation = 1;
+  Cluster::Cell cell = {bin, segment, activation};
+
+  Cluster cluster;
+  cluster.sizeLoc0 = 1;
+  cluster.sizeLoc1 = 1;
+  cluster.channels = {cell};
+
+  DigitizedParameters params;
+  params.indices = {eBoundLoc0, eBoundLoc1};
+  params.values = {position.x(), position.y()};
+  params.variances = {variance.x(), variance.y()};
+  params.cluster = {cluster};
+
+  return params;
+}
+
+auto testDigitizedParametersWithTwoClusters(bool merge, const Vector2 &firstHit,
+                                            const Vector2 &secondHit) {
   BinUtility binUtility;
   binUtility +=
       BinningData(BinningOption::open, BinningValue::binX, 20, -10.0f, 10.0f);
@@ -31,20 +57,10 @@ auto testDigitizedParameters(bool merge) {
   ModuleClusters moduleClusters(binUtility, boundIndices, merge, nsigma,
                                 commonCorner);
 
-  Cluster cluster;
-  cluster.sizeLoc0 = 1;
-  cluster.sizeLoc1 = 1;
-  cluster.channels = {Cluster::Cell(
-      {0, 0}, Channelizer::Segment2D({Vector2(0, 0), Vector2(0, 0)}), 1)};
-
-  DigitizedParameters params;
-  params.indices = {eBoundLoc0, eBoundLoc1};
-  params.values = {0, 0};
-  params.variances = {1, 1};
-  params.cluster = {cluster};
-
-  moduleClusters.add(params, 0);
-  moduleClusters.add(params, 1);
+  moduleClusters.add(makeDigitizationParameters(firstHit, {1, 1}, binUtility),
+                     0);
+  moduleClusters.add(makeDigitizationParameters(secondHit, {1, 1}, binUtility),
+                     1);
 
   return moduleClusters.digitizedParameters();
 }
@@ -54,11 +70,23 @@ auto testDigitizedParameters(bool merge) {
 BOOST_AUTO_TEST_SUITE(DigitizationModuleClustersTests)
 
 BOOST_AUTO_TEST_CASE(digitizedParameters_merging) {
-  auto result = testDigitizedParameters(true);
-  BOOST_CHECK_EQUAL(result.size(), 1);
+  // overlapping hits are expected to be merged if turned on
+  {
+    auto result = testDigitizedParametersWithTwoClusters(true, {0, 0}, {0, 0});
+    BOOST_CHECK_EQUAL(result.size(), 1);
 
-  result = testDigitizedParameters(false);
-  BOOST_CHECK_EQUAL(result.size(), 2);
+    result = testDigitizedParametersWithTwoClusters(false, {0, 0}, {0, 0});
+    BOOST_CHECK_EQUAL(result.size(), 2);
+  }
+
+  // non overlapping hits are not expected to be merged
+  {
+    auto result = testDigitizedParametersWithTwoClusters(true, {0, 0}, {5, 0});
+    BOOST_CHECK_EQUAL(result.size(), 2);
+
+    result = testDigitizedParametersWithTwoClusters(false, {0, 0}, {5, 0});
+    BOOST_CHECK_EQUAL(result.size(), 2);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
