b2785a3b96791f1154eb29623f800ba90f7fc4c1
Paul Gessinger <paul.gessinger@cern.ch>
2023-06-30 17:05:49 +0200

    refactor: Move TrackSelector algorithm

--- Examples/Python/src/ExampleAlgorithms.cpp
+++ Examples/Python/src/ExampleAlgorithms.cpp
@@ -14,6 +14,7 @@
 #include "ActsExamples/Printers/ParticlesPrinter.hpp"
 #include "ActsExamples/Printers/TrackParametersPrinter.hpp"
 #include "ActsExamples/Utilities/Range.hpp"
+#include "ActsExamples/Utilities/TrackSelector.hpp"
 
 #include <vector>
 
@@ -50,6 +51,47 @@ void addExampleAlgorithms(Context& ctx) {
 
   ACTS_PYTHON_DECLARE_ALGORITHM(ActsExamples::TrackParametersPrinter, mex,
                                 "TrackParametersPrinter", inputTrackParameters);
+
+  {
+    using Alg = ActsExamples::TrackSelector;
+    using Config = Alg::Config;
+
+    auto alg =
+        py::class_<Alg, IAlgorithm, std::shared_ptr<Alg>>(mex, "TrackSelector")
+            .def(py::init<const Alg::Config&, Acts::Logging::Level>(),
+                 py::arg("config"), py::arg("level"))
+            .def_property_readonly("config", &Alg::config);
+
+    auto c = py::class_<Config>(alg, "Config").def(py::init<>());
+
+    ACTS_PYTHON_STRUCT_BEGIN(c, Config);
+    ACTS_PYTHON_MEMBER(inputTracks);
+    ACTS_PYTHON_MEMBER(outputTracks);
+    ACTS_PYTHON_MEMBER(loc0Min);
+    ACTS_PYTHON_MEMBER(loc0Max);
+    ACTS_PYTHON_MEMBER(loc1Min);
+    ACTS_PYTHON_MEMBER(loc1Max);
+    ACTS_PYTHON_MEMBER(timeMin);
+    ACTS_PYTHON_MEMBER(timeMax);
+    ACTS_PYTHON_MEMBER(phiMin);
+    ACTS_PYTHON_MEMBER(phiMax);
+    ACTS_PYTHON_MEMBER(etaMin);
+    ACTS_PYTHON_MEMBER(etaMax);
+    ACTS_PYTHON_MEMBER(absEtaMin);
+    ACTS_PYTHON_MEMBER(absEtaMax);
+    ACTS_PYTHON_MEMBER(ptMin);
+    ACTS_PYTHON_MEMBER(ptMax);
+    ACTS_PYTHON_MEMBER(minMeasurements);
+    ACTS_PYTHON_STRUCT_END();
+
+    pythonRangeProperty(c, "loc0", &Config::loc0Min, &Config::loc0Max);
+    pythonRangeProperty(c, "loc1", &Config::loc1Min, &Config::loc1Max);
+    pythonRangeProperty(c, "time", &Config::timeMin, &Config::timeMax);
+    pythonRangeProperty(c, "phi", &Config::phiMin, &Config::phiMax);
+    pythonRangeProperty(c, "eta", &Config::etaMin, &Config::etaMax);
+    pythonRangeProperty(c, "absEta", &Config::absEtaMin, &Config::absEtaMax);
+    pythonRangeProperty(c, "pt", &Config::ptMin, &Config::ptMax);
+  }
 }
 
 }  // namespace Acts::Python
