04d52d87b09aa84388e8420d9d7125c853ab4000
Andreas Stefl <stefl.andreas@gmail.com>
2023-07-03 13:51:55 +0200

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ linkcheck_ignore = [
     r"https://doi.org/.*",
     r"https://cernvm.cern.ch/.*",
     r"https://tavianator.com/.*",
-    r"https://eigen.tuxfamily.org/.*",
+    r"http://eigen.tuxfamily.org/.*",
 ]
 
 # -- Options for HTML output --------------------------------------------------
