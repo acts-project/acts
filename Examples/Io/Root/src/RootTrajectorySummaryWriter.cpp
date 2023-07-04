030936d409fc003df76d3220a8dd5c0886e0b451
andiwand <stefl.andreas@gmail.com>
2023-07-04 08:55:01 +0200

    fixes a potential FPE in RootTrajectorySummaryWriter

--- Examples/Io/Root/src/RootTrajectorySummaryWriter.cpp
+++ Examples/Io/Root/src/RootTrajectorySummaryWriter.cpp
@@ -238,6 +238,7 @@ ActsExamples::ProcessCode ActsExamples::RootTrajectorySummaryWriter::writeT(
       float t_pT = NaNfloat;
       float t_d0 = NaNfloat;
       float t_z0 = NaNfloat;
+      float t_qop = NaNfloat;
 
       // Get the perigee surface
       Acts::Surface* pSurface = nullptr;
@@ -279,6 +280,7 @@ ActsExamples::ProcessCode ActsExamples::RootTrajectorySummaryWriter::writeT(
           t_phi = phi(particle.unitDirection());
           t_eta = eta(particle.unitDirection());
           t_pT = t_p * perp(particle.unitDirection());
+          t_qop = particle.qop();
 
           if (pSurface != nullptr) {
             auto intersection =
@@ -345,22 +347,21 @@ ActsExamples::ProcessCode ActsExamples::RootTrajectorySummaryWriter::writeT(
           param[i] = parameter[i];
         }
 
-        res = {param[Acts::eBoundLoc0] - t_d0,
-               param[Acts::eBoundLoc1] - t_z0,
-               Acts::detail::difference_periodic(param[Acts::eBoundPhi], t_phi,
-                                                 static_cast<float>(2 * M_PI)),
-               param[Acts::eBoundTheta] - t_theta,
-               param[Acts::eBoundQOverP] - t_charge / t_p,
-               param[Acts::eBoundTime] - t_time};
+        if (foundMajorityParticle) {
+          res = {
+              param[Acts::eBoundLoc0] - t_d0,
+              param[Acts::eBoundLoc1] - t_z0,
+              Acts::detail::difference_periodic(param[Acts::eBoundPhi], t_phi,
+                                                static_cast<float>(2 * M_PI)),
+              param[Acts::eBoundTheta] - t_theta,
+              param[Acts::eBoundQOverP] - t_qop,
+              param[Acts::eBoundTime] - t_time};
 
-        if (boundParam.covariance().has_value()) {
-          const auto& covariance = *boundParam.covariance();
-          for (unsigned int i = 0; i < Acts::eBoundSize; ++i) {
-            error[i] = std::sqrt(covariance(i, i));
-            if (error[i] != 0.0) {
+          if (boundParam.covariance().has_value()) {
+            const auto& covariance = *boundParam.covariance();
+            for (unsigned int i = 0; i < Acts::eBoundSize; ++i) {
+              error[i] = std::sqrt(covariance(i, i));
               pull[i] = res[i] / error[i];
-            } else {
-              pull[i] = std::numeric_limits<double>::quiet_NaN();
             }
           }
         }
