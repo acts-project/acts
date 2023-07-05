ccbed15e997d3fd4acf04452431a6eee531eadb2
Andreas Stefl <stefl.andreas@gmail.com>
2023-07-05 12:10:00 +0200

    feat: Add ability to remove secondaries in `ParticleSelector` (#2265)
    Allows to filter secondary particles with `ParticleSelector`
    
    blocked by
    - https://github.com/acts-project/acts/pull/2266

--- Examples/Python/src/TruthTracking.cpp
+++ Examples/Python/src/TruthTracking.cpp
@@ -126,6 +126,7 @@ void addTruthTracking(Context& ctx) {
     ACTS_PYTHON_MEMBER(ptMax);
     ACTS_PYTHON_MEMBER(removeCharged);
     ACTS_PYTHON_MEMBER(removeNeutral);
+    ACTS_PYTHON_MEMBER(removeSecondaries);
     ACTS_PYTHON_STRUCT_END();
 
     pythonRangeProperty(c, "rho", &Config::rhoMin, &Config::rhoMax);
