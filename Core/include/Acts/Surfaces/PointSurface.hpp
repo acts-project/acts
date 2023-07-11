4ecfb9443aba5ba4cd0005e0f544a5ce1498c75c
andiwand <stefl.andreas@gmail.com>
2023-07-11 14:26:45 +0200

    draft

--- Core/include/Acts/Surfaces/PointSurface.hpp
+++ Core/include/Acts/Surfaces/PointSurface.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2016-2020 CERN for the benefit of the Acts project
+// Copyright (C) 2023 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,7 +16,7 @@
 #include "Acts/Geometry/Polyhedron.hpp"
 #include "Acts/Surfaces/BoundaryCheck.hpp"
 #include "Acts/Surfaces/InfiniteBounds.hpp"
-#include "Acts/Surfaces/LineBounds.hpp"
+#include "Acts/Surfaces/PointBounds.hpp"
 #include "Acts/Surfaces/Surface.hpp"
 #include "Acts/Utilities/BinningType.hpp"
 #include "Acts/Utilities/Result.hpp"
@@ -26,70 +26,67 @@
 
 namespace Acts {
 
-class LineBounds;
+class PointBounds;
 class DetectorElementBase;
 class SurfaceBounds;
 
-///  @class LineSurface
+/// @class PointSurface
 ///
-///  Base class for a linear surfaces in the TrackingGeometry
-///  to describe dirft tube, straw like detectors or the Perigee
-///  It inherits from Surface.
+/// Base class for a point surfaces in the TrackingGeometry to allow propagation
+/// to the closest point of approach of a 3D point. It inherits from Surface.
 ///
-///  @note It leaves the type() method virtual, so it can not be instantiated
-///
-/// @image html figures/LineSurface.png
-class LineSurface : public Surface {
+/// @note It leaves the type() method virtual, so it can not be instantiated
+class PointSurface : public Surface {
 #ifndef DOXYGEN
   friend Surface;
 #endif
 
  protected:
-  /// Constructor from Transform3 and bounds
+  /// Constructor from Translation3 and bounds
   ///
   /// @param transform The transform that positions the surface in the global
   /// frame
   /// @param radius The straw radius
   /// @param halez The half length in z
-  LineSurface(const Transform3& transform, double radius, double halez);
+  PointSurface(const Translation3& transform, double radius);
 
-  /// Constructor from Transform3 and a shared bounds object
+  /// Constructor from Translation3 and a shared bounds object
   ///
   /// @param transform The transform that positions the surface in the global
   /// frame
-  /// @param lbounds The bounds describing the straw dimensions, can be
+  /// @param pbounds The bounds describing the straw dimensions, can be
   /// optionally nullptr
-  LineSurface(const Transform3& transform,
-              std::shared_ptr<const LineBounds> lbounds = nullptr);
+  PointSurface(const Translation3& transform,
+               std::shared_ptr<const PointBounds> pbounds = nullptr);
 
   /// Constructor from DetectorElementBase : Element proxy
   ///
-  /// @param lbounds The bounds describing the straw dimensions
+  /// @param pbounds The bounds describing the straw dimensions
   /// @param detelement for which this surface is (at least) one representation
-  LineSurface(std::shared_ptr<const LineBounds> lbounds,
-              const DetectorElementBase& detelement);
+  PointSurface(std::shared_ptr<const PointBounds> pbounds,
+               const DetectorElementBase& detelement);
 
   /// Copy constructor
   ///
   /// @param other The source surface for copying
-  LineSurface(const LineSurface& other);
+  PointSurface(const PointSurface& other);
 
   /// Copy constructor - with shift
   ///
   /// @param gctx The current geometry context object, e.g. alignment
   /// @param other is the source cone surface
   /// @param shift is the additional transform applied after copying
-  LineSurface(const GeometryContext& gctx, const LineSurface& other,
-              const Transform3& shift);
+  PointSurface(const GeometryContext& gctx, const PointSurface& other,
+               const Transform3& shift);
 
  public:
-  ~LineSurface() override = default;
-  LineSurface() = delete;
+  ~PointSurface() override = default;
+  PointSurface() = delete;
 
   /// Assignment operator
   ///
   /// @param other is the source surface dor copying
-  LineSurface& operator=(const LineSurface& other);
+  PointSurface& operator=(const PointSurface& other);
 
   /// Normal vector return
   ///
@@ -113,8 +110,7 @@ class LineSurface : public Surface {
   Vector3 binningPosition(const GeometryContext& gctx,
                           BinningValue bValue) const final;
 
-  /// Return the measurement frame - this is needed for alignment, in particular
-  /// for StraightLine and Perigee Surface.
+  /// Return the measurement frame
   ///
   /// @param gctx The current geometry context object, e.g. alignment
   /// @param position is the global position where the measurement frame is
@@ -149,9 +145,6 @@ class LineSurface : public Surface {
 
   /// Local to global transformation
   ///
-  /// @note for line surfaces the momentum direction is used in order to interpret the
-  /// drift radius
-  ///
   /// @param gctx The current geometry context object, e.g. alignment
   /// @param lposition is the local position to be transformed
   /// @param direction is the global momentum direction (used to sign the closest approach)
@@ -160,34 +153,10 @@ class LineSurface : public Surface {
   Vector3 localToGlobal(const GeometryContext& gctx, const Vector2& lposition,
                         const Vector3& direction) const final;
 
-  /// Specified for LineSurface: global to local method without dynamic
-  /// memory allocation
+  /// This method is the true global->local transformation.
   ///
-  /// This method is the true global->local transformation.<br>
-  /// makes use of globalToLocal and indicates the sign of the Acts::eBoundLoc0
-  /// by the given momentum direction
-  ///
-  /// The calculation of the sign of the radius (or \f$ d_0 \f$) can be done as
-  /// follows:<br>
-  /// May \f$ \vec d = \vec m - \vec c \f$ denote the difference between the
-  /// center of the line and the global position of the measurement/predicted
-  /// state,
-  /// then \f$ \vec d
-  /// \f$
-  /// lies within the so
-  /// called measurement plane.
-  /// The measurement plane is determined by the two orthogonal vectors \f$
-  /// \vec{measY}= \vec{Acts::eBoundLoc1} \f$
-  /// and \f$ \vec{measX} = \vec{measY} \times \frac{\vec{p}}{|\vec{p}|}
-  /// \f$.<br>
-  ///
-  /// The sign of the radius (\f$ d_{0} \f$ ) is then defined by the projection
-  /// of
-  /// \f$ \vec{d} \f$
-  /// onto \f$ \vec{measX} \f$:<br>
-  /// \f$ sign = -sign(\vec{d} \cdot \vec{measX}) \f$
-  ///
-  /// @image html figures/SignOfDriftCircleD0.gif
+  /// Makes use of globalToLocal and indicates the sign of the Acts::eBoundLoc0
+  /// by the given momentum direction.
   ///
   /// @param gctx The current geometry context object, e.g. alignment
   /// @param position global 3D position - considered to be on surface but not
@@ -210,44 +179,17 @@ class LineSurface : public Surface {
   /// @param bcheck The boundary check directive for the estimate
   /// @param tolerance the tolerance used for the intersection
   ///
-  ///   <b>mathematical motivation:</b>
-  ///   Given two lines in parameteric form:<br>
-  ///   - @f$ \vec l_{a}(\lambda) = \vec m_a + \lambda \cdot \vec e_{a} @f$ <br>
-  ///   - @f$ \vec l_{b}(\mu) = \vec m_b + \mu \cdot \vec e_{b} @f$ <br>
-  ///   the vector between any two points on the two lines is given by:
-  ///   - @f$ \vec s(\lambda, \mu) = \vec l_{b} - l_{a} = \vec m_{ab} + \mu
-  ///   \cdot
-  ///  \vec e_{b} - \lambda \cdot \vec e_{a} @f$, <br>
-  ///   when @f$ \vec m_{ab} = \vec m_{b} - \vec m_{a} @f$.<br>
-  ///   @f$ \vec s(u, \mu_0) @f$  denotes the vector between the two
-  ///  closest points <br>
-  ///   @f$ \vec l_{a,0} = l_{a}(u) @f$ and @f$ \vec l_{b,0} =
-  ///  l_{b}(\mu_0) @f$ <br>
-  ///   and is perpendicular to both, @f$ \vec e_{a} @f$ and @f$ \vec e_{b} @f$.
-  ///
-  ///   This results in a system of two linear equations:<br>
-  ///   - (i) @f$ 0 = \vec s(u, \mu_0) \cdot \vec e_a = \vec m_ab \cdot
-  ///  \vec e_a + \mu_0 \vec e_a \cdot \vec e_b - u @f$ <br>
-  ///   - (ii) @f$ 0 = \vec s(u, \mu_0) \cdot \vec e_b = \vec m_ab \cdot
-  ///  \vec e_b + \mu_0  - u \vec e_b \cdot \vec e_a @f$ <br>
-  ///
-  ///   Solving (i), (ii) for @f$ u @f$ and @f$ \mu_0 @f$ yields:
-  ///   - @f$ u = \frac{(\vec m_ab \cdot \vec e_a)-(\vec m_ab \cdot \vec
-  ///  e_b)(\vec e_a \cdot \vec e_b)}{1-(\vec e_a \cdot \vec e_b)^2} @f$ <br>
-  ///   - @f$ \mu_0 = - \frac{(\vec m_ab \cdot \vec e_b)-(\vec m_ab \cdot \vec
-  ///  e_a)(\vec e_a \cdot \vec e_b)}{1-(\vec e_a \cdot \vec e_b)^2} @f$ <br>
-  ///
   /// @return is the intersection object
   SurfaceIntersection intersect(
       const GeometryContext& gctx, const Vector3& position,
       const Vector3& direction, const BoundaryCheck& bcheck = false,
       ActsScalar tolerance = s_onSurfaceTolerance) const final;
 
-  /// the pathCorrection for derived classes with thickness
-  /// is by definition 1 for LineSurfaces
+  /// The pathCorrection for derived classes with thickness is by definition 1
+  /// for PointSurface.
   ///
   /// @note input parameters are ignored
-  /// @note there's no material associated to the line surface
+  /// @note there's no material associated to the point surface
   double pathCorrection(const GeometryContext& gctx, const Vector3& position,
                         const Vector3& direction) const override;
 
@@ -281,7 +223,7 @@ class LineSurface : public Surface {
       const GeometryContext& gctx, const Vector3& position) const final;
 
  protected:
-  std::shared_ptr<const LineBounds> m_bounds;  ///< bounds (shared)
+  std::shared_ptr<const PointBounds> m_bounds;  ///< bounds (shared)
 
  private:
   /// helper function to apply the globalToLocal with out transform
