f6aed18bb5d8cb856fff89c4894db9c3c3210292
andiwand <stefl.andreas@gmail.com>
2023-07-11 16:25:31 +0200

    revert unintended changes

--- Examples/Scripts/Python/full_chain_itk.py
+++ Examples/Scripts/Python/full_chain_itk.py
@@ -22,9 +22,9 @@ from acts.examples.reconstruction import (
     VertexFinder,
 )
 
-ttbar_pu200 = True
+ttbar_pu200 = False
 u = acts.UnitConstants
-geo_dir = pathlib.Path("../../acts-itk")
+geo_dir = pathlib.Path("acts-itk")
 outputDir = pathlib.Path.cwd() / "itk_output"
 # acts.examples.dump_args_calls(locals())  # show acts.examples python binding calls
 
