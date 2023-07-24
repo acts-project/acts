432e6ad302da9bf387fb1b39c55e812fa1d614fa
Paul Gessinger <hello@paulgessinger.com>
2023-07-24 14:47:13 +0200

    change comparison, setup

--- Core/src/ActsVersion.cpp
+++ Core/src/ActsVersion.cpp
@@ -8,13 +8,29 @@
 
 #include "Acts/ActsVersion.hpp"
 
+#include <ostream>
+#include <string_view>
+
 namespace Acts {
 
-VersionInfo::VersionInfo()
-    : major{VersionMajor},
-      minor{VersionMinor},
-      patch{VersionPatch},
-      commitHash{CommitHash},
-      commitHashShort{CommitHashShort} {}
+VersionInfo::VersionInfo(unsigned int major, unsigned int minor,
+                         unsigned int patch, const char* const commitHash)
+    : major(major), minor(minor), patch(patch), commitHash(commitHash) {}
 
+VersionInfo VersionInfo::fromLibrary() {
+  // this is filled by the Core shared library
+  // while the constants below depend on the include
+  return VersionInfo{VersionMajor, VersionMinor, VersionPatch, CommitHash};
+}
+
+bool VersionInfo::operator==(const VersionInfo& other) const {
+  return major == other.major && minor == other.minor && patch == other.patch &&
+         std::string_view{commitHash} == std::string_view{other.commitHash};
+}
+
+std::ostream& operator<<(std::ostream& os, const VersionInfo& vi) {
+  os << vi.major << "." << vi.minor << "." << vi.patch << " (commit "
+     << vi.commitHash << ")";
+  return os;
+}
 }  // namespace Acts
