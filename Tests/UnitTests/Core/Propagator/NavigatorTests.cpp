3c69bf252e40caf835ab4ef8e288d513b906284c
Andreas Stefl <stefl.andreas@gmail.com>
2023-08-04 18:12:43 +0200

    refactor: Remove direction from stepper (#2312)
    The propagation direction is currently stored in multiple places which makes it tedious to change it consistently during propagation which can easily result in bugs.
    
    In this PR I propose to remove the `navDir` from the stepper interface and replace it consistently with the propagation option for the direction.
    
    This should not effect any physics performance as it is a code refactor of how we pass down the propagation direction to the stepper.
    
    In a follow up PR I want to do the same thing for the navigation.

--- Tests/UnitTests/Core/Propagator/NavigatorTests.cpp
+++ Tests/UnitTests/Core/Propagator/NavigatorTests.cpp
@@ -91,9 +91,6 @@ struct PropagatorState {
       /// Charge
       double q = 0;
 
-      /// the navigation direction
-      Direction navDir = Direction::Forward;
-
       // accummulated path length cache
       double pathAccumulated = 0.;
 
@@ -112,7 +109,6 @@ struct PropagatorState {
     /// State resetter
     void resetState(State& /*state*/, const BoundVector& /*boundParams*/,
                     const BoundSymMatrix& /*cov*/, const Surface& /*surface*/,
-                    const Direction /*navDir*/,
                     const double /*stepSize*/) const {}
 
     /// Global particle position accessor
@@ -145,11 +141,14 @@ struct PropagatorState {
       return s_onSurfaceTolerance;
     }
 
-    Intersection3D::Status updateSurfaceStatus(
-        State& state, const Surface& surface, const BoundaryCheck& bcheck,
-        const Logger& logger, ActsScalar surfaceTolerance) const {
+    Intersection3D::Status updateSurfaceStatus(State& state,
+                                               const Surface& surface,
+                                               Direction navDir,
+                                               const BoundaryCheck& bcheck,
+                                               ActsScalar surfaceTolerance,
+                                               const Logger& logger) const {
       return detail::updateSingleSurfaceStatus<Stepper>(
-          *this, state, surface, bcheck, logger, surfaceTolerance);
+          *this, state, surface, navDir, bcheck, surfaceTolerance, logger);
     }
 
     template <typename object_intersection_t>
@@ -235,6 +234,8 @@ struct PropagatorState {
     /// buffer & formatting for consistent output
     size_t debugPfxWidth = 30;
     size_t debugMsgWidth = 50;
+
+    Direction direction = Direction::Forward;
 
     const Acts::Logger& logger = Acts::getDummyLogger();
 
