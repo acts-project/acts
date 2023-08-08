12ad6e47f2fa88ec652f71a72752ef0a4ac3a3b1
Paul Gessinger <hello@paulgessinger.com>
2023-08-08 09:17:43 +0200

    fix compilation failure

--- Examples/Framework/ML/src/NeuralCalibrator.cpp
+++ Examples/Framework/ML/src/NeuralCalibrator.cpp
@@ -98,63 +98,64 @@ void ActsExamples::NeuralCalibrator::calibrate(
 
   std::visit(
       [&](const auto& measurement) {
-    auto E = measurement.expander();
-    auto P = measurement.projector();
-    Acts::ActsVector<Acts::eBoundSize> fpar = E * measurement.parameters();
-    Acts::ActsSymMatrix<Acts::eBoundSize> fcov =
-        E * measurement.covariance() * E.transpose();
+        auto E = measurement.expander();
+        auto P = measurement.projector();
+        Acts::ActsVector<Acts::eBoundSize> fpar = E * measurement.parameters();
+        Acts::ActsSymMatrix<Acts::eBoundSize> fcov =
+            E * measurement.covariance() * E.transpose();
 
-    input[iInput++] = fpar[Acts::eBoundLoc0];
-    input[iInput++] = fpar[Acts::eBoundLoc1];
-    input[iInput++] = fcov(Acts::eBoundLoc0, Acts::eBoundLoc0);
-    input[iInput++] = fcov(Acts::eBoundLoc1, Acts::eBoundLoc1);
-    if (iInput != m_nInputs) {
-      throw std::runtime_error("Expected input size of " +
-                               std::to_string(m_nInputs) +
-                               ", got: " + std::to_string(iInput));
-    }
+        input[iInput++] = fpar[Acts::eBoundLoc0];
+        input[iInput++] = fpar[Acts::eBoundLoc1];
+        input[iInput++] = fcov(Acts::eBoundLoc0, Acts::eBoundLoc0);
+        input[iInput++] = fcov(Acts::eBoundLoc1, Acts::eBoundLoc1);
+        if (iInput != m_nInputs) {
+          throw std::runtime_error("Expected input size of " +
+                                   std::to_string(m_nInputs) +
+                                   ", got: " + std::to_string(iInput));
+        }
 
-    // Input is a single row, hence .front()
-    std::vector<float> output = m_model.runONNXInference(inputBatch).front();
-    // Assuming 2-D measurements, the expected params structure is:
-    // [           0,    nComponent[ --> priors
-    // [  nComponent,  3*nComponent[ --> means
-    // [3*nComponent,  5*nComponent[ --> variances
-    size_t nParams = 5 * m_nComponents;
-    if (output.size() != nParams) {
-      throw std::runtime_error("Got output vector of size " +
-                               std::to_string(output.size()) +
-                               ", expected size " + std::to_string(nParams));
-    }
+        // Input is a single row, hence .front()
+        std::vector<float> output =
+            m_model.runONNXInference(inputBatch).front();
+        // Assuming 2-D measurements, the expected params structure is:
+        // [           0,    nComponent[ --> priors
+        // [  nComponent,  3*nComponent[ --> means
+        // [3*nComponent,  5*nComponent[ --> variances
+        size_t nParams = 5 * m_nComponents;
+        if (output.size() != nParams) {
+          throw std::runtime_error(
+              "Got output vector of size " + std::to_string(output.size()) +
+              ", expected size " + std::to_string(nParams));
+        }
 
-    // Most probable value computation of mixture density
-    size_t iMax = 0;
-    if (m_nComponents > 1) {
-      iMax = std::distance(
-          output.begin(),
-          std::max_element(output.begin(), output.begin() + m_nComponents));
-    }
-    size_t iLoc0 = m_nComponents + iMax * 2;
-    size_t iVar0 = 3 * m_nComponents + iMax * 2;
+        // Most probable value computation of mixture density
+        size_t iMax = 0;
+        if (m_nComponents > 1) {
+          iMax = std::distance(
+              output.begin(),
+              std::max_element(output.begin(), output.begin() + m_nComponents));
+        }
+        size_t iLoc0 = m_nComponents + iMax * 2;
+        size_t iVar0 = 3 * m_nComponents + iMax * 2;
 
-    fpar[Acts::eBoundLoc0] = output[iLoc0];
-    fpar[Acts::eBoundLoc1] = output[iLoc0 + 1];
-    fcov(Acts::eBoundLoc0, Acts::eBoundLoc0) = output[iVar0];
-    fcov(Acts::eBoundLoc1, Acts::eBoundLoc1) = output[iVar0 + 1];
+        fpar[Acts::eBoundLoc0] = output[iLoc0];
+        fpar[Acts::eBoundLoc1] = output[iLoc0 + 1];
+        fcov(Acts::eBoundLoc0, Acts::eBoundLoc0) = output[iVar0];
+        fcov(Acts::eBoundLoc1, Acts::eBoundLoc1) = output[iVar0 + 1];
 
-    constexpr size_t kSize =
-        std::remove_reference_t<decltype(measurement)>::size();
-    std::array<Acts::BoundIndices, kSize> indices = measurement.indices();
-    Acts::ActsVector<kSize> cpar = P * fpar;
-    Acts::ActsSymMatrix<kSize> ccov = P * fcov * P.transpose();
+        constexpr size_t kSize =
+            std::remove_reference_t<decltype(measurement)>::size();
+        std::array<Acts::BoundIndices, kSize> indices = measurement.indices();
+        Acts::ActsVector<kSize> cpar = P * fpar;
+        Acts::ActsSymMatrix<kSize> ccov = P * fcov * P.transpose();
 
-    Acts::SourceLink sl{idxSourceLink};
+        Acts::SourceLink sl{idxSourceLink};
 
-    Acts::Measurement<Acts::BoundIndices, kSize> calibrated(
-        std::move(sl), indices, cpar, ccov);
+        Acts::Measurement<Acts::BoundIndices, kSize> calibrated(
+            std::move(sl), indices, cpar, ccov);
 
-    trackState.allocateCalibrated(calibrated.size());
-    trackState.setCalibrated(calibrated);
+        trackState.allocateCalibrated(calibrated.size());
+        trackState.setCalibrated(calibrated);
       },
-      (measurements)idxSsourceLink.index()]);
+      measurements[idxSsourceLink.index()]);
 }
