d4de69de53cda5b351c64a05761d33682782503f
Benjamin Huth <Benjamin-Huth@t-online.de>
2023-08-13 14:49:25 +0200

    finalize test

--- Tests/UnitTests/Plugins/ExaTrkX/ExaTrkXBoostTrackBuildingTests.cpp
+++ Tests/UnitTests/Plugins/ExaTrkX/ExaTrkXBoostTrackBuildingTests.cpp
@@ -9,31 +9,54 @@
 #include <boost/test/unit_test.hpp>
 
 #include "Acts/Plugins/ExaTrkX/BoostTrackBuilding.hpp"
+#include "Acts/Plugins/ExaTrkX/detail/TensorVectorConversion.hpp"
 
 #include <algorithm>
-#include <random>
 
 BOOST_AUTO_TEST_CASE(test_track_building) {
-    std::mt19937 gen{42};
-    
-    // Make some spacepoint IDs
-    std::vector<int> spacepointIds(16);
-    std::iota(spacepointIds.begin(), spacepointIds.end(), 0);
-    
-    // Build 4 tracks with 4 hits
-    std::vector<std::vector<int>> refTracks;
-    for(auto t=0ul; t < 4; ++t) {
-        refTracks.emplace_back(spacepointIds.begin()+4*t, spacepointIds.begin()+4*(t+1));
+  // Make some spacepoint IDs
+  // The spacepoint ids are [100, 101, 102, ...]
+  // They should not be zero based to check if the thing also works if the
+  // spacepoint IDs do not match the node IDs used for the edges
+  std::vector<int> spacepointIds(16);
+  std::iota(spacepointIds.begin(), spacepointIds.end(), 100);
+
+  // Build 4 tracks with 4 hits
+  std::vector<std::vector<int>> refTracks;
+  for (auto t = 0ul; t < 4; ++t) {
+    refTracks.emplace_back(spacepointIds.begin() + 4 * t,
+                           spacepointIds.begin() + 4 * (t + 1));
+  }
+
+  // Make edges
+  std::vector<int64_t> edges;
+  for (const auto &track : refTracks) {
+    for (auto it = track.begin(); it != track.end() - 1; ++it) {
+      // edges must be 0 based, so subtract 100 again
+      edges.push_back(*it - 100);
+      edges.push_back(*std::next(it) - 100);
     }
-    
-    // Make edges
-    std::vector<int> edges;
-    for(const auto &track : refTracks) {
-        for(auto it=track.begin(); it != track.end()-1; ++it) {
-            edges.push_back(*it);
-            edges.push_back(*std::next(it));
-        }
-    }
-    
-    
+  }
+
+  auto edgeTensor =
+      Acts::detail::vectorToTensor2D(edges, 2).t().contiguous().clone();
+  auto dummyWeights = torch::ones(edges.size() / 2, torch::kFloat32);
+
+  // Run Track building
+  auto logger = Acts::getDefaultLogger("TestLogger", Acts::Logging::ERROR);
+  Acts::BoostTrackBuilding trackBuilder(std::move(logger));
+
+  auto testTracks = trackBuilder({}, edgeTensor, dummyWeights, spacepointIds);
+
+  // Sort tracks, so we can find them
+  std::for_each(testTracks.begin(), testTracks.end(),
+                [](auto &t) { std::sort(t.begin(), t.end()); });
+  std::for_each(refTracks.begin(), refTracks.end(),
+                [](auto &t) { std::sort(t.begin(), t.end()); });
+
+  // Check what we have here
+  for (const auto &refTrack : refTracks) {
+    auto found = std::find(testTracks.begin(), testTracks.end(), refTrack);
+    BOOST_CHECK(found != testTracks.end());
+  }
 }
