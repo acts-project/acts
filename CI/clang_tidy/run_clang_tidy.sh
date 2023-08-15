516349012f25f413c4f56c0dcc3370b050c932c9
andiwand <stefl.andreas@gmail.com>
2023-08-15 20:40:07 +0200

    debug

--- CI/clang_tidy/run_clang_tidy.sh
+++ CI/clang_tidy/run_clang_tidy.sh
@@ -11,6 +11,12 @@ mkdir -p $output_dir
 output_dir=$(realpath $output_dir)
 
 pushd $build_dir
+print "why?"
 NINJA_STATUS="[ninja][%f/%t] " ninja $@ | tee $output_dir/ninja.log
+print "what?"
 popd
+print "hi!"
 cat $output_dir/ninja.log | grep -v '\[ninja\]' > $output_dir/clang-tidy.log
+
+print "hello?"
+exit 0
