dcfd3dc3e18086a23005f7c81f8ebdacd15c0aa1
andiwand <stefl.andreas@gmail.com>
2023-08-15 17:04:46 +0200

    improve logs and output

--- CI/clang_tidy/run_clang_tidy.sh
+++ CI/clang_tidy/run_clang_tidy.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+output_dir=$1
+shift
 build_dir=$1
 shift
 
-export NINJA_STATUS="[ninja][%f/%t] "
+mkdir -p $output_dir
+output_dir=$(realpath $output_dir)
 
 pushd $build_dir
-ninja $@ | grep -v '\[ninja\]'
+NINJA_STATUS="[ninja][%f/%t] " ninja $@ | tee $output_dir/ninja.log
 popd
+cat $output_dir/ninja.log | grep -v '\[ninja\]' > $output_dir/clang-tidy.log
