acts-project/acts

commit 20cf882b02597d01f93f76881036facea51081fc

ci: Monitor grid seeding (#2355)
Since we will extend the adaptive grid seeder to accommodate time seeding, I propose to add it to the physmon.

CI/physmon/phys_perf_mon.sh
@@ -172,6 +172,26 @@ function full_chain() {
         "AMVF ${suffix}" \
         amvf_${suffix}
 
+    if [ $suffix == seeded ]; then
+	    Examples/Scripts/generic_plotter.py \
+            $outdir/performance_amvf_gridseeder_${suffix}.root \
+            vertexing \
+            $outdir/performance_amvf_gridseeder_${suffix}_hist.root \
+            --silent \
+            --config CI/physmon/vertexing_config.yml
+        ec=$(($ec | $?))
+
+        # remove ntuple file because it's large
+        rm $outdir/performance_amvf_gridseeder_${suffix}.root
+
+        run \
+            $outdir/performance_amvf_gridseeder_${suffix}_hist.root \
+            $refdir/performance_amvf_gridseeder_${suffix}_hist.root \
+            --title "AMVF (+grid seeder) ${suffix}" \
+            -o $outdir/amvf_gridseeder_${suffix}.html \
+            -p $outdir/amvf_gridseeder_${suffix}_plots
+    fi
+
     Examples/Scripts/generic_plotter.py \
         $outdir/tracksummary_ckf_${suffix}.root \
         tracksummary \
@@ -285,6 +305,23 @@ if [[ "$mode" == "all" || "$mode" == "fullchains" ]]; then
         $refdir/performance_amvf_ttbar_hist.root \
         "AMVF ttbar" \
         amvf_ttbar
+
+    Examples/Scripts/generic_plotter.py \
+        $outdir/performance_amvf_gridseeder_ttbar.root \
+        vertexing \
+        $outdir/performance_amvf_gridseeder_ttbar_hist.root \
+        --silent \
+        --config CI/physmon/vertexing_config.yml
+    ec=$(($ec | $?))
+
+    # remove ntuple file because it's large
+    rm $outdir/performance_amvf_gridseeder_ttbar.root
+
+    run \
+        $outdir/performance_amvf_gridseeder_ttbar_hist.root \
+        $refdir/performance_amvf_gridseeder_ttbar_hist.root \
+        "AMVF (+grid seeder) ttbar" \
+        amvf_gridseeder_ttbar
 fi
 
 if [[ "$mode" == "all" || "$mode" == "gsf" ]]; then

CI/physmon/workflows/physmon_ckf_tracking.py
@@ -142,6 +142,7 @@ def run_ckf_tracking(truthSmearedSeeded, truthEstimatedSeeded, label):
         addVertexFitting(
             s,
             setup.field,
+            seeder=acts.VertexSeedFinder.GaussianSeeder,
             associatedParticles=None
             if label in ["seeded", "orthogonal"]
             else "particles_input",
@@ -154,6 +155,7 @@ def run_ckf_tracking(truthSmearedSeeded, truthEstimatedSeeded, label):
         addVertexFitting(
             s,
             setup.field,
+            seeder=acts.VertexSeedFinder.GaussianSeeder,
             associatedParticles=None
             if label in ["seeded", "orthogonal"]
             else "particles_input",
@@ -162,6 +164,20 @@ def run_ckf_tracking(truthSmearedSeeded, truthEstimatedSeeded, label):
             vertexFinder=VertexFinder.AMVF,
             outputDirRoot=tp / "amvf",
         )
+
+        # Use the adaptive grid vertex seeder in combination with the AMVF
+        # To avoid having too many physmon cases, we only do this for the label "seeded"
+        if label == "seeded":
+            addVertexFitting(
+                s,
+                setup.field,
+                seeder=acts.VertexSeedFinder.AdaptiveGridSeeder,
+                associatedParticles=None,
+                outputProtoVertices="amvf_gridseeder_protovertices",
+                outputVertices="amvf_gridseeder_fittedVertices",
+                vertexFinder=VertexFinder.AMVF,
+                outputDirRoot=tp / "amvf_gridseeder",
+            )
 
         s.run()
         del s
@@ -172,17 +188,28 @@ def run_ckf_tracking(truthSmearedSeeded, truthEstimatedSeeded, label):
                 tp / f"performance_{vertexing}.root",
             )
 
-        for stem in [
-            "performance_ckf",
-            "tracksummary_ckf",
-            "performance_ivf",
-            "performance_amvf",
-        ] + (
-            ["performance_seeding", "performance_ambi"]
-            if label in ["seeded", "orthogonal"]
-            else ["performance_seeding"]
-            if label == "truth_estimated"
-            else []
+        if label == "seeded":
+            vertexing = "amvf_gridseeder"
+            shutil.move(
+                tp / f"{vertexing}/performance_vertexing.root",
+                tp / f"performance_{vertexing}.root",
+            )
+
+        for stem in (
+            [
+                "performance_ckf",
+                "tracksummary_ckf",
+                "performance_ivf",
+                "performance_amvf",
+            ]
+            + (["performance_amvf_gridseeder"] if label == "seeded" else [])
+            + (
+                ["performance_seeding", "performance_ambi"]
+                if label in ["seeded", "orthogonal"]
+                else ["performance_seeding"]
+                if label == "truth_estimated"
+                else []
+            )
         ):
             perf_file = tp / f"{stem}.root"
             assert perf_file.exists(), "Performance file not found"

CI/physmon/workflows/physmon_track_finding_ttbar.py
@@ -154,6 +154,7 @@ with tempfile.TemporaryDirectory() as temp:
     addVertexFitting(
         s,
         setup.field,
+        seeder=acts.VertexSeedFinder.GaussianSeeder,
         associatedParticles=None,
         outputProtoVertices="amvf_protovertices",
         outputVertices="amvf_fittedVertices",
@@ -161,19 +162,31 @@ with tempfile.TemporaryDirectory() as temp:
         outputDirRoot=tp / "amvf",
     )
 
+    addVertexFitting(
+        s,
+        setup.field,
+        seeder=acts.VertexSeedFinder.AdaptiveGridSeeder,
+        associatedParticles=None,
+        outputProtoVertices="amvf_gridseeder_protovertices",
+        outputVertices="amvf_gridseeder_fittedVertices",
+        vertexFinder=VertexFinder.AMVF,
+        outputDirRoot=tp / "amvf_gridseeder",
+    )
+
     s.run()
     del s
 
-    vertexing = "amvf"
-    shutil.move(
-        tp / f"{vertexing}/performance_vertexing.root",
-        tp / f"performance_{vertexing}.root",
-    )
+    for vertexing in ["amvf", "amvf_gridseeder"]:
+        shutil.move(
+            tp / f"{vertexing}/performance_vertexing.root",
+            tp / f"performance_{vertexing}.root",
+        )
 
     for stem in [
         "performance_ckf",
         "tracksummary_ckf",
         "performance_amvf",
+        "performance_amvf_gridseeder",
     ] + (["performance_seeding", "performance_ambi"]):
         perf_file = tp / f"{stem}.root"
         assert perf_file.exists(), "Performance file not found"

Core/include/Acts/Utilities/AlgebraHelpers.hpp
@@ -167,14 +167,16 @@ inline ActsMatrix<A::RowsAtCompileTime, B::ColsAtCompileTime> blockedMult(
   }
 }
 
-/// Calculate the inverse of an Eigen matrix after checking if it can be
-/// numerically inverted. This allows to catch potential FPEs before they occur.
+/// FPE "safe" functions
 ///
 /// Our main motivation for this is that users might have a strict FPE policy
 /// which would flag every single occurrence as a failure and then sombody has
 /// to investigate. Since we are processing a high number of events and floating
 /// point numbers sometimes work in mysterious ways the caller of this function
 /// might want to hide FPEs and handle them in a more controlled way.
+
+/// Calculate the inverse of an Eigen matrix after checking if it can be
+/// numerically inverted. This allows to catch potential FPEs before they occur.
 ///
 /// @tparam Derived Eigen derived concrete type
 /// @tparam Result Eigen result type defaulted to input type
@@ -194,6 +196,27 @@ std::optional<ResultType> safeInverse(const MatrixType& m) noexcept {
   }
 
   return std::nullopt;
+}
+
+/// Calculate the exponential function while avoiding FPEs.
+/// @note The boundary values of -50.0 and 50.0 might need to be adapted when
+/// using this function with doubles
+///
+/// @param val argument for which the exponential function should be evaluated.
+///
+/// @return 0 in the case of underflow, std::numeric_limits<T>::infinity in the
+/// case of overflow, std::exp(val) else
+template <typename T>
+T safeExp(T val) noexcept {
+  if (val < -50.0) {
+    return 0.0;
+  }
+
+  if (val > 50.0) {
+    return std::numeric_limits<T>::infinity();
+  }
+
+  return std::exp(val);
 }
 
 }  // namespace Acts

Core/include/Acts/Vertexing/AdaptiveGridTrackDensity.ipp
@@ -5,6 +5,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#include "Acts/Utilities/AlgebraHelpers.hpp"
 #include "Acts/Vertexing/VertexingError.hpp"
 
 #include <algorithm>
@@ -197,7 +198,7 @@ float Acts::AdaptiveGridTrackDensity<trkGridSize>::normal2D(
   float expo =
       -1 / (2 * det) *
       (cov(1, 1) * d * d - d * z * (cov(0, 1) + cov(1, 0)) + cov(0, 0) * z * z);
-  return coef * std::exp(expo);
+  return coef * safeExp(expo);
 }
 
 template <int trkGridSize>

Core/include/Acts/Vertexing/GaussianGridTrackDensity.ipp
@@ -5,6 +5,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#include "Acts/Utilities/AlgebraHelpers.hpp"
 #include "Acts/Vertexing/VertexingError.hpp"
 
 #include <algorithm>
@@ -207,7 +208,7 @@ float Acts::GaussianGridTrackDensity<mainGridSize, trkGridSize>::normal2D(
   float expo =
       -1 / (2 * det) *
       (cov(1, 1) * d * d - d * z * (cov(0, 1) + cov(1, 0)) + cov(0, 0) * z * z);
-  return coef * std::exp(expo);
+  return coef * safeExp(expo);
 }
 
 template <int mainGridSize, int trkGridSize>

commit 21883197368141149a3f22f85323164b233f10c3

fix: grid seeder physmon not shown (#2386)
Remove bug introduced in #2355, where updates in `phys_perf_mon.sh` were not correctly considered

CI/physmon/phys_perf_mon.sh
@@ -184,12 +184,11 @@ function full_chain() {
         # remove ntuple file because it's large
         rm $outdir/performance_amvf_gridseeder_${suffix}.root
 
-        run \
+        run_histcmp \
             $outdir/performance_amvf_gridseeder_${suffix}_hist.root \
             $refdir/performance_amvf_gridseeder_${suffix}_hist.root \
-            --title "AMVF (+grid seeder) ${suffix}" \
-            -o $outdir/amvf_gridseeder_${suffix}.html \
-            -p $outdir/amvf_gridseeder_${suffix}_plots
+            "AMVF (+grid seeder) ${suffix}" \
+            amvf_gridseeder_${suffix}
     fi
 
     Examples/Scripts/generic_plotter.py \
@@ -317,7 +316,7 @@ if [[ "$mode" == "all" || "$mode" == "fullchains" ]]; then
     # remove ntuple file because it's large
     rm $outdir/performance_amvf_gridseeder_ttbar.root
 
-    run \
+    run_histcmp \
         $outdir/performance_amvf_gridseeder_ttbar_hist.root \
         $refdir/performance_amvf_gridseeder_ttbar_hist.root \
         "AMVF (+grid seeder) ttbar" \