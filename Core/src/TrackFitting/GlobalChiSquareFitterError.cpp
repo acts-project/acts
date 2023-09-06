e99370744d9f13046df52add5ee9713dfb20582a
Alexander J. Pfleger <70842573+AJPfleger@users.noreply.github.com>
2023-09-06 09:23:07 +0200

    feat: GlobalChiSquareFitterError framework for Gx2f (#2417)
    This mostly adds the framework for the fitter specific error handling. Currently it only contains a single error case. During the next steps in development we can figure out which other errors might occur and they can be added easily.

---
+++ Core/src/TrackFitting/GlobalChiSquareFitterError.cpp
@@ -0,0 +1,41 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2023 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "Acts/TrackFitting/GlobalChiSquareFitterError.hpp"
+
+#include <string>
+
+namespace {
+
+class GlobalChiSquareFitterErrorCategory : public std::error_category {
+ public:
+  // Return a short descriptive name for the category.
+  const char* name() const noexcept final {
+    return "GlobalChiSquareFitterError";
+  }
+
+  // Return what each enum means in text.
+  std::string message(int c) const final {
+    using Acts::Experimental::GlobalChiSquareFitterError;
+
+    switch (static_cast<GlobalChiSquareFitterError>(c)) {
+      case GlobalChiSquareFitterError::DetAIsZero:
+        return "Gx2f has det(a) == 0 during update.";
+      default:
+        return "unknown";
+    }
+  }
+};
+
+}  // namespace
+
+std::error_code Acts::Experimental::make_error_code(
+    Acts::Experimental::GlobalChiSquareFitterError e) {
+  static GlobalChiSquareFitterErrorCategory c;
+  return {static_cast<int>(e), c};
+}
