71d83406fa542230b4b96c19a4c75dcc06f57542
Amperiadou Dimitra <63058524+dimitra97@users.noreply.github.com>
2023-09-26 21:30:15 +0200

    Update NavigationStateUpdators.hpp

--- Core/include/Acts/Navigation/NavigationStateUpdators.hpp
+++ Core/include/Acts/Navigation/NavigationStateUpdators.hpp
@@ -133,7 +133,6 @@ class IndexedUpdatorImpl : public INavigationDelegate {
   /// @param igrid the grid that is moved into this attacher
   /// @param icasts is the cast values array
   /// @param itr a transform applied to the global position
-  /// @param isort a flag for sorting the candidates or not
   IndexedUpdatorImpl(grid_type&& igrid,
                      const std::array<BinningValue, grid_type::DIM>& icasts,
                      const Transform3& itr = Transform3::Identity())
