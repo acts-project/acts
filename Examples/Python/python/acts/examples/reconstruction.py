7b2ef5df95eed1508d1d3ed57c7778e9f0abbb77
andiwand <stefl.andreas@gmail.com>
2023-09-28 13:22:09 +0200

    debug: bump chi2 cutoff

--- Examples/Python/python/acts/examples/reconstruction.py
+++ Examples/Python/python/acts/examples/reconstruction.py
@@ -122,7 +122,7 @@ TrackSelectorConfig = namedtuple(
 TrackFindingConfig = namedtuple(
     "TrackFindingConfig",
     ["chi2CutOff", "numMeasurementsCutOff"],
-    defaults=[5.0, 10],
+    defaults=[50.0, 10],
 )
 
 AmbiguityResolutionConfig = namedtuple(
