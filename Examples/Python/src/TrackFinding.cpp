1eddd2a9e9d539783a5c6f80c35f169217bd34b5
Andreas Stefl <stefl.andreas@gmail.com>
2023-09-29 20:40:19 +0200

    refactor: Align initial track param covariance (#2491)
    Currently the initial track param covariance will depend on the chosen seeder. Truth smeared will use the configured sigmas to cook up a cov while to other seeder take a directly configured one.
    
    Here I align the truth smeared to the others which also allows to experiment with the covariance separately from the smearing.

--- Examples/Python/src/TrackFinding.cpp
+++ Examples/Python/src/TrackFinding.cpp
@@ -261,8 +261,8 @@ void addTrackFinding(Context& ctx) {
       ActsExamples::TrackParamsEstimationAlgorithm, mex,
       "TrackParamsEstimationAlgorithm", inputSeeds, inputProtoTracks,
       outputTrackParameters, outputSeeds, outputProtoTracks, trackingGeometry,
-      magneticField, bFieldMin, sigmaLoc0, sigmaLoc1, sigmaPhi, sigmaTheta,
-      sigmaQOverP, sigmaT0, initialVarInflation, particleHypothesis);
+      magneticField, bFieldMin, initialSigmas, initialVarInflation,
+      particleHypothesis);
 
   {
     using Alg = ActsExamples::TrackFindingAlgorithm;
