03c88b586a45042d075b7fe1092d8c61a710ba1f
Andreas Stefl <stefl.andreas@gmail.com>
2023-09-29 13:25:23 +0200

    Update Examples/Python/python/acts/examples/reconstruction.py

--- Examples/Python/python/acts/examples/reconstruction.py
+++ Examples/Python/python/acts/examples/reconstruction.py
@@ -122,7 +122,7 @@ TrackSelectorConfig = namedtuple(
 TrackFindingConfig = namedtuple(
     "TrackFindingConfig",
     ["chi2CutOff", "numMeasurementsCutOff"],
-    defaults=[50.0, 10],
+    defaults=[15.0, 10],
 )
 
 AmbiguityResolutionConfig = namedtuple(
