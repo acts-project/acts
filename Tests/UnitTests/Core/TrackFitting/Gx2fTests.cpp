c6c17e779bd7d6fd6da1ad7c2a1215fbceb83a1c
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-10-04 16:07:21 +0200

    add Trackstate filling

--- Tests/UnitTests/Core/TrackFitting/Gx2fTests.cpp
+++ Tests/UnitTests/Core/TrackFitting/Gx2fTests.cpp
@@ -246,7 +246,7 @@ BOOST_AUTO_TEST_CASE(NoFit) {
 
   auto& track = *res;
   BOOST_CHECK_EQUAL(track.tipIndex(), Acts::MultiTrajectoryTraits::kInvalid);
-  BOOST_CHECK(!track.hasReferenceSurface());
+  BOOST_CHECK(track.hasReferenceSurface());
   BOOST_CHECK_EQUAL(track.nMeasurements(), 0u);
   BOOST_CHECK_EQUAL(track.nHoles(), 0u);
   BOOST_CHECK_EQUAL(track.parameters(), startParametersFit.parameters());
@@ -341,9 +341,9 @@ BOOST_AUTO_TEST_CASE(Fit5Iterations) {
   BOOST_REQUIRE(res.ok());
 
   auto& track = *res;
-  BOOST_CHECK_EQUAL(track.tipIndex(), Acts::MultiTrajectoryTraits::kInvalid);
-  BOOST_CHECK(!track.hasReferenceSurface());
-  BOOST_CHECK_EQUAL(track.nMeasurements(), 0u);
+  BOOST_CHECK_EQUAL(track.tipIndex(), nSurfaces - 1);
+  BOOST_CHECK(track.hasReferenceSurface());
+  BOOST_CHECK_EQUAL(track.nMeasurements(), nSurfaces);
   BOOST_CHECK_EQUAL(track.nHoles(), 0u);
   // We need quite coarse checks here, since on different builds
   // the created measurements differ in the randomness
@@ -451,9 +451,9 @@ BOOST_AUTO_TEST_CASE(MixedDetector) {
   BOOST_REQUIRE(res.ok());
 
   auto& track = *res;
-  BOOST_CHECK_EQUAL(track.tipIndex(), Acts::MultiTrajectoryTraits::kInvalid);
-  BOOST_CHECK(!track.hasReferenceSurface());
-  BOOST_CHECK_EQUAL(track.nMeasurements(), 0u);
+  BOOST_CHECK_EQUAL(track.tipIndex(), nSurfaces - 1);
+  BOOST_CHECK(track.hasReferenceSurface());
+  BOOST_CHECK_EQUAL(track.nMeasurements(), nSurfaces);
   BOOST_CHECK_EQUAL(track.nHoles(), 0u);
   // We need quite coarse checks here, since on different builds
   // the created measurements differ in the randomness
