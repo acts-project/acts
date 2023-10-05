5f637250a8695c44d19a6ce8b061cb3820fd710d
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2023-10-05 13:43:11 +0200

    Apply suggestions from code review
    Co-authored-by: Andreas Stefl <stefl.andreas@gmail.com>

--- Examples/Io/Csv/include/ActsExamples/Io/Csv/CsvProtoTrackWriter.hpp
+++ Examples/Io/Csv/include/ActsExamples/Io/Csv/CsvProtoTrackWriter.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2021 CERN for the benefit of the Acts project
+// Copyright (C) 2023 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -28,10 +28,11 @@ namespace ActsExamples {
 class CsvProtoTrackWriter final : public WriterT<ProtoTrackContainer> {
  public:
   struct Config {
+    /// Which prototracks to write
     std::string inputPrototracks;
-    /// Which measurement collection to write.
+    /// Spacepoint collection
     std::string inputSpacepoints;
-    /// Which cluster collection to write (optional)
+    /// Output directory
     std::string outputDir;
     /// Number of decimal digits for floating point precision in output.
     int outputPrecision = std::numeric_limits<float>::max_digits10;
