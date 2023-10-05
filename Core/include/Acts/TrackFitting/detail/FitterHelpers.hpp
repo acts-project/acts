23c02ef5bb25a2c1b6b75e6248555b7d86c6c505
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-10-05 10:21:33 +0200

    generaalise for gx2f

--- Core/include/Acts/TrackFitting/detail/FitterHelpers.hpp
+++ Core/include/Acts/TrackFitting/detail/FitterHelpers.hpp
@@ -25,9 +25,9 @@ namespace detail {
 /// @param surface The current surface
 /// @param fittedStates The Multitrajectory to that we add the state
 /// @param lastTrackIndex The parent index for the new state in the MT
-/// @param doCovTransport Whether to perform a covariance transport when
-/// computing the bound state or not
+/// @param doCovTransport Whether to perform a covariance transport when computing the bound state or not
 /// @param freeToBoundCorrection Correction for non-linearity effect during transform from free to bound (only corrected when performing CovTransport)
+/// @param getOnLastTrackIndex If set, no new TrackState will be added
 template <typename propagator_state_t, typename stepper_t, typename traj_t>
 auto getTrackStateProxy(propagator_state_t &state, const stepper_t &stepper,
                         const Surface &surface, traj_t &fittedStates,
@@ -35,15 +35,28 @@ auto getTrackStateProxy(propagator_state_t &state, const stepper_t &stepper,
                         const Logger &logger,
                         const FreeToBoundCorrection &freeToBoundCorrection =
                             FreeToBoundCorrection(false),
-                        TrackStatePropMask mask = TrackStatePropMask::All)
+                        TrackStatePropMask mask = TrackStatePropMask::All,
+                        bool getOnLastTrackIndex = false)
     -> Result<typename traj_t::TrackStateProxy> {
-  // add a full TrackState entry multi trajectory
-  // (this allocates storage for all components, we will set them later)
-  const auto newTrackIndex = fittedStates.addTrackState(mask, lastTrackIndex);
+  size_t currentTrackIndex = Acts::MultiTrajectoryTraits::kInvalid;
+
+  if (getOnLastTrackIndex) {
+    if (lastTrackIndex < fittedStates.size() - 1 ||
+        lastTrackIndex == Acts::MultiTrajectoryTraits::kInvalid) {
+      ACTS_WARNING("No TrackState with index "
+                   << lastTrackIndex << " found. Add new TrackState.");
+      currentTrackIndex = fittedStates.addTrackState(mask, lastTrackIndex);
+    }
+    currentTrackIndex = lastTrackIndex;
+  } else {
+    // add a full TrackState entry multi trajectory
+    // (this allocates storage for all components, we will set them later)
+    currentTrackIndex = fittedStates.addTrackState(mask, lastTrackIndex);
+  }
 
   // now get track state proxy back
   typename traj_t::TrackStateProxy trackStateProxy =
-      fittedStates.getTrackState(newTrackIndex);
+      fittedStates.getTrackState(currentTrackIndex);
 
   trackStateProxy.setReferenceSurface(surface.getSharedPtr());
 
