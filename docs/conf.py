76eb9da4e7c16514f7754ea5df3cd36a6f4056f5
Paul Gessinger <hello@paulgessinger.com>
2023-11-02 09:16:25 +0100

    fix lcginfo

--- docs/conf.py
+++ docs/conf.py
@@ -54,9 +54,9 @@ linkcheck_retries = 5
 linkcheck_ignore = [
     r"https://doi.org/.*",
     r"https://cernvm.cern.ch/.*",
-    r"https://tavianator.com/.*",
     r"http://eigen.tuxfamily.org.*",
     r"https://pythia.org.*",
+    r"https://lcginfo.cern.ch/.*",
 ]
 
 # -- Options for HTML output --------------------------------------------------
