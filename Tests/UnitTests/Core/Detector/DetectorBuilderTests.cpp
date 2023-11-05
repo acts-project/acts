0c2dbd93d0364f78a6b7c1694a484ffcdd1906eb
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-11-05 11:58:12 +0100

    part

--- Tests/UnitTests/Core/Detector/DetectorBuilderTests.cpp
+++ Tests/UnitTests/Core/Detector/DetectorBuilderTests.cpp
@@ -140,7 +140,7 @@ BOOST_AUTO_TEST_CASE(DetectorBuilder_test) {
   auto detector = Acts::Experimental::DetectorBuilder(dCfg).construct(tContext);
 
   BOOST_CHECK_EQUAL(detector->name(), "TestDetector");
-  BOOST_CHECK(detector->rootVolumes().size() == 1);
+  BOOST_CHECK_EQUAL(detector->rootVolumes().size(), 1);
 }
 
 BOOST_AUTO_TEST_CASE(DetectorBuilder_testWithSurfaces) {
@@ -166,9 +166,9 @@ BOOST_AUTO_TEST_CASE(DetectorBuilder_testWithSurfaces) {
 
   auto detector = Acts::Experimental::DetectorBuilder(dCfg).construct(tContext);
   BOOST_CHECK_EQUAL(detector->name(), "TestDetectorWithSurfaces");
-  BOOST_CHECK(detector->volumes()[0]->surfaces()[0]->geometryId().sensitive() ==
+  BOOST_CHECK_EQUAL(detector->volumes()[0]->surfaces()[0]->geometryId().sensitive(),
               1);
-  BOOST_CHECK(detector->volumes()[0]->surfaces()[1]->geometryId().sensitive() ==
+  BOOST_CHECK_EQUAL(detector->volumes()[0]->surfaces()[1]->geometryId().sensitive(),
               2);
 }
 
