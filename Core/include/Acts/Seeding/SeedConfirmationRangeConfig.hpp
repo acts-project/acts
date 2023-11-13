d1086a80ffefa2e95392f7cad957f41034b3acaa
LuisFelipeCoelho <luisfelipefuc@gmail.com>
2023-11-13 13:07:47 +0100

    change seed confRange config

--- Core/include/Acts/Seeding/SeedConfirmationRangeConfig.hpp
+++ Core/include/Acts/Seeding/SeedConfirmationRangeConfig.hpp
@@ -17,29 +17,41 @@ namespace Acts {
 
 /// @brief contains parameters for  seed confirmation
 struct SeedConfirmationRangeConfig {
-  // z minimum and maximum of middle component of the seed used to define the
-  // region of the detector for seed confirmation
+  // This is used for quality seed confirmation. Where requirements on the
+  // number of compatible space-points and impact parameters can also be defined
+  // for different (r, z) regions of the detector (e.g. forward or central
+  // region) by SeedConfirmationRange. Seeds are classied as "high-quality"
+  // seeds and normal quality seeds. Normal quality seeds are only selected if
+  // no other "high-quality" seeds has been found for that inner-middle doublet.
+  // For optimization reasons, the algorithm only calls the seed confirmation
+  // for a certain inner-middle doublet in case a configurable minimum number of
+  // inner-middle-outer triplets have been found.
+
+  // Minimum and maximum z position of middle component of the seed used to
+  // split the region of the detector for seed confirmation
   float zMinSeedConf =
       std::numeric_limits<float>::lowest();  // Acts::UnitConstants::mm
-  float zMaxSeedConf =
-      std::numeric_limits<float>::max();  // Acts::UnitConstants::mm
-  // radius of bottom component of seed that is used to define the number of
-  // compatible top required
+  float zMaxSeedConf = std::numeric_limits<
+      float>::max();  // Acts::UnitConstants::mm
+                      // Radius position of inner seed component that is used to
+                      // split the region of the detector for seed confirmation
   float rMaxSeedConf =
       std::numeric_limits<float>::max();  // Acts::UnitConstants::mm
 
-  // number of compatible top SPs of seed if bottom radius is larger than
-  // rMaxSeedConf
+  // Minimum number of compatible outer space-points required in quality seed
+  // confirmation if inner space-points radius is larger than rMaxSeedConf
   std::size_t nTopForLargeR = 0;
-  // number of compatible top SPs of seed if bottom radius is smaller than
-  // rMaxSeedConf
+  // Minimum number of compatible outer space-points required in quality seed
+  // confirmation if inner space-points radius is smaller than rMaxSeedConf
   std::size_t nTopForSmallR = 0;
 
-  // minimum radius for bottom SP in seed confirmation
+  // Minimum radius for inner seed component required in quality in seed
+  // confirmation
   float seedConfMinBottomRadius = 60. * Acts::UnitConstants::mm;
-  // maximum zOrigin in seed confirmation
+  // Maximum longitudinal impact parameter of seed required in quality seed
+  // confirmation
   float seedConfMaxZOrigin = 150. * Acts::UnitConstants::mm;
-  // minimum impact parameter for seed confirmation
+  // Minimum impact parameter of seed required in quality seed confirmation
   float minImpactSeedConf = 1. * Acts::UnitConstants::mm;
 };
 
