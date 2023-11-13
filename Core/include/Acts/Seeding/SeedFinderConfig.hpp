66faf6d105ec23291f1d300027a323530f7dc8bf
Luis Falda Coelho <56648068+LuisFelipeCoelho@users.noreply.github.com>
2023-11-13 15:28:28 +0100

    Update Core/include/Acts/Seeding/SeedFinderConfig.hpp
    Co-authored-by: Alexander J. Pfleger <70842573+AJPfleger@users.noreply.github.com>

--- Core/include/Acts/Seeding/SeedFinderConfig.hpp
+++ Core/include/Acts/Seeding/SeedFinderConfig.hpp
@@ -132,7 +132,7 @@ struct SeedFinderConfig {
   // Number of sigmas of scattering angle to be considered in the minimum pT
   // scattering term
   float sigmaScattering = 5;
-  // Term that accounts for the thickness of scattering medium in radiation lengths in the Lynch \& Dahl correction to the Highland equation
+  // Term that accounts for the thickness of scattering medium in radiation lengths in the Lynch & Dahl correction to the Highland equation
   // default is 5%
   // TODO: necessary to make amount of material dependent on detector region?
   float radLengthPerSeed = 0.05;
