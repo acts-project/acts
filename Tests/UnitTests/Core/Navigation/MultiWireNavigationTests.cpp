e25adf899200463af036390495ef47121a77b40c
andiwand <stefl.andreas@gmail.com>
2023-11-16 13:09:44 +0100

    fix more new geo stuff

--- Tests/UnitTests/Core/Navigation/MultiWireNavigationTests.cpp
+++ Tests/UnitTests/Core/Navigation/MultiWireNavigationTests.cpp
@@ -94,8 +94,9 @@ BOOST_AUTO_TEST_CASE(Navigation_in_Indexed_Surfaces) {
   nState.currentVolume = volumes.front().get();
   nState.currentVolume->updateNavigationState(tContext, nState);
 
-  // check the surface candidates after update (12 surfaces + 6 portals)
-  BOOST_CHECK_EQUAL(nState.surfaceCandidates.size(), 18u);
+  // check the surface candidates after update (12 surfaces + 6 portals but only
+  // 3 are reachable)
+  BOOST_CHECK_EQUAL(nState.surfaceCandidates.size(), 3u);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
