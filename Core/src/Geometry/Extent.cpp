87f9cb4e197ba20299f931d6921033ad756fbcf5
Andreas Salzburger <asalzburger@gmail.com>
2023-11-18 13:05:21 +0100

    Update Extent.cpp

--- Core/src/Geometry/Extent.cpp
+++ Core/src/Geometry/Extent.cpp
@@ -79,10 +79,6 @@ void Acts::Extent::extend(const Extent& rhs,
   }
 }
 
-void Acts::Extent::setConstrain(BinningValue bValue) {
-  m_constrains.set(bValue);
-}
-
 void Acts::Extent::addConstrain(const Acts::Extent& rhs,
                                 const ExtentEnvelope& envelope) {
   for (const auto& bValue : s_binningValues) {
