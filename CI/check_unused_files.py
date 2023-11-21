95eb047dbd1def60fb4426e4d1b6dbe1b75876b2
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-11-21 13:53:43 +0100

    progress in output

--- CI/check_unused_files.py
+++ CI/check_unused_files.py
@@ -14,6 +14,16 @@ def file_can_be_removed(searchstring, scope):
     return output == b""
 
 
+def count_files(path=".", exclude_dirs=(), exclude_files=()):
+    count = 0
+    for root, dirs, files in os.walk(path):
+        dirs[:] = [d for d in dirs if d not in exclude_dirs]
+        files[:] = [f for f in files if f not in exclude_files]
+        count += len(files)
+
+    return count
+
+
 def main():
     print("\033[32mINFO\033[0m Start check_unused_files.py ...")
     exclude_dirs = (
@@ -104,27 +114,37 @@ def main():
     unused_files = ()
 
     # walk over all files
-    for root, dirs, files in os.walk("."):  # topdown=True
+    for root, dirs, files in os.walk("."):
         dirs[:] = [d for d in dirs if d not in exclude_dirs]
         files[:] = [f for f in files if f not in exclude_files]
+
+        # Skip base-directory
+        if str(Path(root)) == ".":
+            continue
+
+        # Print progress
+        if root[2:] in dirs_base:
+            processed_files = 0
+            current_base_dir = root
+            number_files = count_files(root, exclude_dirs, exclude_files)
+            # print empty to start a new line
+            print("")
 
         # Skip "white-paper-figures"
         # TODO Find a more elegant way
         if str(root).find("white_papers/figures") != -1:
+            processed_files += count_files(root, exclude_dirs, exclude_files)
             continue
 
         # Skip "DD4hep-tests" since their cmake looks a bit different
         # TODO Find a more elegant way
         if str(root).find("Tests/UnitTests/Plugins/DD4hep") != -1:
+            processed_files += count_files(root, exclude_dirs, exclude_files)
             continue
 
         root = Path(root)
-
-        # Skip base-directory
-        if str(root) == ".":
-            continue
-
         for filename in files:
+            processed_files += 1
             # get the full path of the file
             filepath = root / filename
 
@@ -158,9 +178,18 @@ def main():
                     remove_cmd = "rm " + str(filepath)
                     os.system(remove_cmd)
 
+        # Print the progress in place
+        progress = int(20 * processed_files / number_files)
+        sys.stdout.write("\r")
+        sys.stdout.write(
+            "Checked [%-20s] %d/%d files in %s"
+            % ("=" * progress, processed_files, number_files, current_base_dir)
+        )
+        sys.stdout.flush()
+
     if len(wrong_extension) != 0:
         print(
-            "\033[31mERROR\033[0m "
+            "\n\n\033[31mERROR\033[0m "
             + f"The following {len(wrong_extension)} files have an unsupported extension:\n\n"
             + "\033[31m"
             + "\n".join(wrong_extension)
@@ -174,7 +203,7 @@ def main():
 
     if len(unused_files) != 0:
         print(
-            "\033[31mERROR\033[0m "
+            "\n\n\033[31mERROR\033[0m "
             + f"The following {len(unused_files)} files seem to be unused:\n"
             + "\033[31m"
             + "\n".join(unused_files)
@@ -188,7 +217,9 @@ def main():
         exit += 1
 
     if exit == 0:
-        print("\033[32mINFO\033[0m Finished check_unused_files.py without any errors.")
+        print(
+            "\n\n\033[32mINFO\033[0m Finished check_unused_files.py without any errors."
+        )
 
     return exit
 
