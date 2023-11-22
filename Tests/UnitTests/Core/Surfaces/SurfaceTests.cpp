a6a34f50779c1c471503d1bb67a8fe475136760e
AJPfleger <alexander.julian.pfleger@cern.ch>
2023-11-22 21:55:03 +0100

    revert MockTrack

--- Tests/UnitTests/Core/Surfaces/SurfaceTests.cpp
+++ Tests/UnitTests/Core/Surfaces/SurfaceTests.cpp
@@ -25,6 +25,22 @@
 #include "SurfaceStub.hpp"
 
 namespace Acts {
+/// Mock track object with minimal methods implemented for compilation
+class MockTrack {
+ public:
+  MockTrack(const Vector3& mom, const Vector3& pos) : m_mom(mom), m_pos(pos) {
+    // nop
+  }
+
+  Vector3 momentum() const { return m_mom; }
+
+  Vector3 position() const { return m_pos; }
+
+ private:
+  Vector3 m_mom;
+  Vector3 m_pos;
+};
+
 namespace Test {
 
 // Create a test context
