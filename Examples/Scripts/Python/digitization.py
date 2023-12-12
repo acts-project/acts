02fcaa48e4fa78dce1fa123f92b00a4fa09465b9
Andreas Salzburger <asalzburger@gmail.com>
2023-12-12 16:29:10 +0100

    allow for variance setting

--- Examples/Scripts/Python/digitization.py
+++ Examples/Scripts/Python/digitization.py
@@ -4,7 +4,8 @@ from typing import Optional, Union
 
 import acts
 import acts.examples
-
+from acts.examples.odd import getOpenDataDetector
+from common import getOpenDataDetectorDirectory
 
 u = acts.UnitConstants
 
@@ -25,6 +26,7 @@ def runDigitization(
         addParticleGun,
         EtaConfig,
         PhiConfig,
+        MomentumConfig,
         ParticleConfig,
         addFatras,
         addDigitization,
@@ -38,8 +40,9 @@ def runDigitization(
     if particlesInput is None:
         addParticleGun(
             s,
-            EtaConfig(-2.0, 2.0),
-            ParticleConfig(4, acts.PdgParticle.eMuon, True),
+            MomentumConfig(0.25 * u.GeV, 100.0 * u.GeV, transverse=True),
+            EtaConfig(-3.2, 3.2),
+            ParticleConfig(1000, acts.PdgParticle.eMuon, True),
             PhiConfig(0.0, 360.0 * u.degree),
             multiplicity=2,
             rnd=rnd,
@@ -77,14 +80,21 @@ def runDigitization(
 
 
 if "__main__" == __name__:
-    detector, trackingGeometry, _ = acts.examples.GenericDetector.create()
 
-    digiConfigFile = (
-        Path(__file__).resolve().parent.parent.parent.parent
-        / "Examples/Algorithms/Digitization/share/default-smearing-config-generic.json"
+    geoDir = getOpenDataDetectorDirectory()
+
+    detector, trackingGeometry, decorators = getOpenDataDetector(
+        geoDir
     )
+
+    #digiConfigFile = geoDir / "config/odd-digi-smearing-config.json"
+    #digiConfigFile = geoDir / "config/odd-digi-geometric-config.json"
+    #digiConfigFile = geoDir / "config/odd-digi-geometric-new-config.json"
+ 
+    digiConfigFile = Path.cwd() / "odd-digi-geometric-config.json"
+
     assert digiConfigFile.exists()
 
     field = acts.ConstantBField(acts.Vector3(0, 0, 2 * u.T))
 
-    runDigitization(trackingGeometry, field, outputDir=Path.cwd()).run()
+    runDigitization(trackingGeometry, field, digiConfigFile=digiConfigFile, outputDir=Path.cwd()).run()
