ebc9779b8a06db77d0ab2c72aebdb11ecd81ea2e
Paul Gessinger <hello@paulgessinger.com>
2023-12-12 16:51:33 +0100

    add makeTrackState function to MTJ

--- Core/include/Acts/EventData/MultiTrajectory.hpp
+++ Core/include/Acts/EventData/MultiTrajectory.hpp
@@ -246,6 +246,15 @@ class MultiTrajectory {
     return self().addTrackState_impl(mask, iprevious);
   }
 
+  /// Add a track state to the container and return a track state proxy to it
+  /// This effectively calls @c addTrackState and @c getTrackState
+  /// @note Only available if the track state container is not read-only
+  /// @return a track state proxy to the newly added track state
+  template <bool RO = ReadOnly, typename = std::enable_if_t<!RO>>
+  TrackStateProxy makeTrackState(IndexType istate) {
+    return getTrackState(addTrackState(istate));
+  }
+
   /// Visit all previous states starting at a given endpoint.
   ///
   /// @param iendpoint  index of the last state
