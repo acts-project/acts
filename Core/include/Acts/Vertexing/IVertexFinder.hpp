6a291de1d29a59a2e3bcf5434178f0895c5074a4
Paul Gessinger <hello@paulgessinger.com>
2023-12-21 09:22:28 +0100

    Add virtual destructor to IVertexFinder

--- Core/include/Acts/Vertexing/IVertexFinder.hpp
+++ Core/include/Acts/Vertexing/IVertexFinder.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2020 CERN for the benefit of the Acts project
+// Copyright (C) 2023 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -34,5 +34,7 @@ class IVertexFinder {
 
   virtual void setTracksToRemove(
       State& state, const std::vector<InputTrack>& removedTracks) const = 0;
+
+  virtual ~IVertexFinder() = default;
 };
 }  // namespace Acts
