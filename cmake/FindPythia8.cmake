fa5271fe4b6e6d3c8ad1634aaab6110c8204a728
Corentin Allaire <corentin.allaire@cern.ch>
2024-01-12 13:18:51 +0100

    small fixes

--- cmake/FindPythia8.cmake
+++ cmake/FindPythia8.cmake
@@ -5,20 +5,18 @@
 
 find_library(Pythia8_LIBRARY
   NAMES Pythia8 pythia8
-  HINTS $ENV{PYTHIA8_DIR}/lib PYTHIA8/lib
+  HINTS ENV PYTHIA8_DIR PYTHIA8
   PATHS /opt/pythia8 /usr/local
   DOC "The Pythia8 library")
 find_path(Pythia8_INCLUDE_DIR
   NAMES Pythia8/Pythia.h
-  HINTS $ENV{PYTHIA8_DIR}/include PYTHIA8/include
+  HINTS ENV PYTHIA8_DIR PYTHIA8
   PATHS /opt/pythia8 /usr/local
   DOC "The Pythia8 include directory")
 
 find_package_handle_standard_args(Pythia8
   REQUIRED_VARS Pythia8_LIBRARY Pythia8_INCLUDE_DIR)
-
 add_library(Pythia8 SHARED IMPORTED)
 set_property(TARGET Pythia8 PROPERTY IMPORTED_LOCATION ${Pythia8_LIBRARY})
 set_property(TARGET Pythia8 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Pythia8_INCLUDE_DIR})
-
 mark_as_advanced(Pythia8_FOUND Pythia8_INCLUDE_DIR Pythia8_LIBRARY)
