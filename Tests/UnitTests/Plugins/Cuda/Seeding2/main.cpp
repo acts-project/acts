8bb1baef5550f6e2dbadb4acf426c6239a32150b
Carlo Varni <75478407+CarloVarni@users.noreply.github.com>
2024-01-16 20:53:03 +0100

    feat!: Generic grid bin finder (#2838)
    This will complete the adaptation of the grid to an N-dimentional case. The user is now able to iterate on any axis as they like without the need to rotate their geometry. 
    
    Next step is to see how the seed finding routine can be adapted to be more general
    
    This PR is also a bug-fix since in the past we had:
    ```cpp
    template <typename external_spacepoint_t>
    boost::container::small_vector<std::size_t, 9>
    Acts::BinFinder<external_spacepoint_t>::findBins(
        std::size_t phiBin, std::size_t zBin,
        const Acts::SpacePointGrid<external_spacepoint_t>* binnedSP) const {
      // if zBinNeighbors is not defined, get the indices using
      // neighborHoodIndices
      if (m_zBinNeighbors->empty()) {
        return binnedSP->neighborHoodIndices({phiBin, zBin}).collect();
      }
      // if the zBinNeighbors is defined, get the indices from there
      std::array<std::pair<int, int>, 2> sizePerAxis;
      sizePerAxis.at(0) = std::make_pair(-m_numPhiNeighbors, m_numPhiNeighbors);
      sizePerAxis.at(1) = (*m_zBinNeighbors)[zBin - 1];
      return binnedSP->neighborHoodIndices({phiBin, zBin}, sizePerAxis).collect();
    }
    ```
    
    In the case `m_zBinNeighbors` is empty, the code is instructed to search neighbours in the range `{-1, 1}` FOR EVERY AXIS (this is what `binnedSP->neighborHoodIndices({phiBin, zBin}).collect();` does), thus overwriting the value set by the user for `m_numPhiNeighbors`. 
    
    Now, the axes are treated independently
    
    This requires: https://github.com/acts-project/acts/pull/2835

--- Tests/UnitTests/Plugins/Cuda/Seeding2/main.cpp
+++ Tests/UnitTests/Plugins/Cuda/Seeding2/main.cpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2023 CERN for the benefit of the Acts project
+// Copyright (C) 2024 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -20,11 +20,11 @@
 
 // Acts include(s).
 #include "Acts/EventData/SpacePointData.hpp"
-#include "Acts/Seeding/BinFinder.hpp"
 #include "Acts/Seeding/BinnedSPGroup.hpp"
 #include "Acts/Seeding/SeedFilterConfig.hpp"
 #include "Acts/Seeding/SeedFinder.hpp"
 #include "Acts/Seeding/SeedFinderConfig.hpp"
+#include "Acts/Utilities/GridBinFinder.hpp"
 
 // System include(s).
 #include <cassert>
@@ -61,10 +61,10 @@ int main(int argc, char* argv[]) {
   std::vector<std::pair<int, int>> zBinNeighborsBottom;
 
   // Create binned groups of these spacepoints.
-  auto bottomBinFinder = std::make_shared<Acts::BinFinder<TestSpacePoint>>(
-      zBinNeighborsBottom, numPhiNeighbors);
-  auto topBinFinder = std::make_shared<Acts::BinFinder<TestSpacePoint>>(
-      zBinNeighborsTop, numPhiNeighbors);
+  auto bottomBinFinder = std::make_shared<Acts::GridBinFinder<2ul>>(
+      numPhiNeighbors, zBinNeighborsBottom);
+  auto topBinFinder = std::make_shared<Acts::GridBinFinder<2ul>>(
+      numPhiNeighbors, zBinNeighborsTop);
 
   // Set up the seedFinder configuration.
   Acts::SeedFinderConfig<TestSpacePoint> sfConfig;
--- Tests/UnitTests/Plugins/Sycl/Seeding/SeedFinderSyclTest.cpp
+++ Tests/UnitTests/Plugins/Sycl/Seeding/SeedFinderSyclTest.cpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2023 CERN for the benefit of the Acts project
+// Copyright (C) 2024 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,7 +9,6 @@
 #include "Acts/EventData/SpacePointData.hpp"
 #include "Acts/Plugins/Sycl/Seeding/SeedFinder.hpp"
 #include "Acts/Plugins/Sycl/Utilities/QueueWrapper.hpp"
-#include "Acts/Seeding/BinFinder.hpp"
 #include "Acts/Seeding/BinnedSPGroup.hpp"
 #include "Acts/Seeding/InternalSeed.hpp"
 #include "Acts/Seeding/InternalSpacePoint.hpp"
@@ -18,6 +17,7 @@
 #include "Acts/Seeding/SeedFinder.hpp"
 #include "Acts/Seeding/SeedFinderConfig.hpp"
 #include "Acts/Seeding/SpacePointGrid.hpp"
+#include "Acts/Utilities/GridBinFinder.hpp"
 #include "Acts/Utilities/Logger.hpp"
 
 #include <chrono>
@@ -160,10 +160,10 @@ auto main(int argc, char** argv) -> int {
   std::vector<std::pair<int, int>> zBinNeighborsTop;
   std::vector<std::pair<int, int>> zBinNeighborsBottom;
 
-  auto bottomBinFinder = std::make_shared<Acts::BinFinder<SpacePoint>>(
-      Acts::BinFinder<SpacePoint>(zBinNeighborsBottom, numPhiNeighbors));
-  auto topBinFinder = std::make_shared<Acts::BinFinder<SpacePoint>>(
-      Acts::BinFinder<SpacePoint>(zBinNeighborsTop, numPhiNeighbors));
+  auto bottomBinFinder = std::make_shared<Acts::GridBinFinder<2ul>>(
+      Acts::GridBinFinder<2ul>(numPhiNeighbors, zBinNeighborsBottom));
+  auto topBinFinder = std::make_shared<Acts::GridBinFinder<2ul>>(
+      Acts::GridBinFinder<2ul>(numPhiNeighbors, zBinNeighborsTop));
   auto config = setupSeedFinderConfiguration<SpacePoint>();
   config = config.toInternalUnits().calculateDerivedQuantities();
   auto options = setupSeedFinderOptions();
