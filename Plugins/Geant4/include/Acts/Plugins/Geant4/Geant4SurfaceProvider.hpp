65a1f3431e068f07c9ad343a12c429a469919438
ssdetlab <ssdetlab@gmail.com>
2024-01-25 17:26:38 +0100

    Format

--- Plugins/Geant4/include/Acts/Plugins/Geant4/Geant4SurfaceProvider.hpp
+++ Plugins/Geant4/include/Acts/Plugins/Geant4/Geant4SurfaceProvider.hpp
@@ -8,9 +8,9 @@
 
 #pragma once
 
+#include "Acts/Detector/KdtSurfacesProvider.hpp"
 #include "Acts/Detector/interface/ISurfacesProvider.hpp"
 #include "Acts/Plugins/Geant4/Geant4DetectorSurfaceFactory.hpp"
-#include "Acts/Detector/KdtSurfacesProvider.hpp" 
 
 #include "G4GDMLParser.hh"
 #include "G4LogicalVolume.hh"
@@ -20,27 +20,27 @@ namespace Acts {
 namespace Experimental {
 
 /// @brief A surface provider that extracts surfaces from a gdml file
-/// 
-/// This provider extracts volumes from a gdml file based on 
-/// the names and returns them as a vector of shared pointers 
-/// to surfaces. 
 ///
-/// Additionally, it can be configured to return a range-based 
+/// This provider extracts volumes from a gdml file based on
+/// the names and returns them as a vector of shared pointers
+/// to surfaces.
+///
+/// Additionally, it can be configured to return a range-based
 /// subset of all the surfaces selected by the name.
-/// The latter is achieved by using a KdtSurfacesProvider 
+/// The latter is achieved by using a KdtSurfacesProvider
 /// internally.
-/// 
+///
 /// @note If the names of the volumes to be extracted are unique,
 /// and the range-based selection is not needed, instantialization of
-/// the KdtSurfacesProvider can be skipped. In this case, the 
+/// the KdtSurfacesProvider can be skipped. In this case, the
 /// template parameters do not affect the behaviour of the provider.
 ///
 /// @tparam kDim The number of dimensions for the KDTree
 /// @tparam bSize The maximum number of surfaces per KDTree leaf
 /// @tparam reference_generator The reference generator for the KDTree
 template <std::size_t kDim = 2u, std::size_t bSize = 100u,
-  typename reference_generator =
-    detail::PolyhedronReferenceGenerator<1u, false>>
+          typename reference_generator =
+              detail::PolyhedronReferenceGenerator<1u, false>>
 class Geant4SurfaceProvider : public Acts::Experimental::ISurfacesProvider {
  public:
   /// Nested configuration struct
@@ -59,78 +59,79 @@ class Geant4SurfaceProvider : public Acts::Experimental::ISurfacesProvider {
   /// Optional configuration for the KdtSurfaces
   struct kdtOptions {
     /// A set of ranges to separate the surfaces
-    Acts::RangeXD<kDim,Acts::ActsScalar> range;
+    Acts::RangeXD<kDim, Acts::ActsScalar> range;
 
-    std::array<Acts::BinningValue,kDim> binningValues;
+    std::array<Acts::BinningValue, kDim> binningValues;
 
     std::size_t leafSize = bSize;
-    
+
     reference_generator rgen;
   };
 
   /// Constructor
   ///@param config The configuration struct
   Geant4SurfaceProvider(const Config& config,
-    const kdtOptions& options = kdtOptions()) {
-      if (config.gdmlPath.empty()) {
-        throw std::invalid_argument("Geant4SurfaceProvider: no gdml file provided");
-      }
-      if (config.surfaceNames.empty()) {
-        throw std::invalid_argument(
-            "Geant4SurfaceProvider: no surface names provided");
-      }
-  
-      m_cfg = config;
-      m_kdtOptions = options;
+                        const kdtOptions& options = kdtOptions()) {
+    if (config.gdmlPath.empty()) {
+      throw std::invalid_argument(
+          "Geant4SurfaceProvider: no gdml file provided");
+    }
+    if (config.surfaceNames.empty()) {
+      throw std::invalid_argument(
+          "Geant4SurfaceProvider: no surface names provided");
+    }
 
-      /// Read the gdml file and get the world volume
-      G4GDMLParser parser;
-      parser.Read(m_cfg.gdmlPath);
-      m_g4World = parser.GetWorldVolume();
-  
-      if (m_g4World == nullptr) {
-        throw std::invalid_argument(
-            "Geant4SurfaceProvider: No g4World initialized");
-      }
-    };
+    m_cfg = config;
+    m_kdtOptions = options;
+
+    /// Read the gdml file and get the world volume
+    G4GDMLParser parser;
+    parser.Read(m_cfg.gdmlPath);
+    m_g4World = parser.GetWorldVolume();
+
+    if (m_g4World == nullptr) {
+      throw std::invalid_argument(
+          "Geant4SurfaceProvider: No g4World initialized");
+    }
+  };
 
   /// Destructor
   ~Geant4SurfaceProvider() = default;
 
   std::vector<std::shared_ptr<Acts::Surface>> surfaces(
-    [[maybe_unused]] const Acts::GeometryContext& gctx) const override {
-      /// Surface selector to look in the G4 world
-      auto surfaceSelector =
-          std::make_shared<Acts::Geant4PhysicalVolumeSelectors::NameSelector>(
-              m_cfg.surfaceNames, m_cfg.exactMatch);
+      [[maybe_unused]] const Acts::GeometryContext& gctx) const override {
+    /// Surface selector to look in the G4 world
+    auto surfaceSelector =
+        std::make_shared<Acts::Geant4PhysicalVolumeSelectors::NameSelector>(
+            m_cfg.surfaceNames, m_cfg.exactMatch);
 
-      /// Surface factory options
-      Acts::Geant4DetectorSurfaceFactory::Options g4SurfaceOptions;
+    /// Surface factory options
+    Acts::Geant4DetectorSurfaceFactory::Options g4SurfaceOptions;
 
-      /// It doesn't matter which selector is used here
-      g4SurfaceOptions.passiveSurfaceSelector = surfaceSelector;
-    
-      /// Generate the surface cache
-      Acts::Geant4DetectorSurfaceFactory::Cache g4SurfaceCache;
-      G4Transform3D g4ToWorld;
+    /// It doesn't matter which selector is used here
+    g4SurfaceOptions.passiveSurfaceSelector = surfaceSelector;
 
-      /// Find and store surfaces in the cache object
-      Acts::Geant4DetectorSurfaceFactory{}.construct(
+    /// Generate the surface cache
+    Acts::Geant4DetectorSurfaceFactory::Cache g4SurfaceCache;
+    G4Transform3D g4ToWorld;
+
+    /// Find and store surfaces in the cache object
+    Acts::Geant4DetectorSurfaceFactory{}.construct(
         g4SurfaceCache, g4ToWorld, *m_g4World, g4SurfaceOptions);
 
-      auto surfaces = g4SurfaceCache.passiveSurfaces;
+    auto surfaces = g4SurfaceCache.passiveSurfaces;
 
-      /// If range is not set, return all surfaces
-      if (m_kdtOptions.range.degenerate()) {
-        return surfaces;
-      }
+    /// If range is not set, return all surfaces
+    if (m_kdtOptions.range.degenerate()) {
+      return surfaces;
+    }
 
-      /// Otherwise, select the surfaces based on the range
-      auto kdtSurfaces = 
-        Acts::Experimental::KdtSurfaces<kDim,bSize,reference_generator>
-          (gctx,surfaces,m_kdtOptions.binningValues);
+    /// Otherwise, select the surfaces based on the range
+    auto kdtSurfaces =
+        Acts::Experimental::KdtSurfaces<kDim, bSize, reference_generator>(
+            gctx, surfaces, m_kdtOptions.binningValues);
 
-      return kdtSurfaces.surfaces(m_kdtOptions.range);
+    return kdtSurfaces.surfaces(m_kdtOptions.range);
   };
 
  private:
--- Tests/UnitTests/Plugins/Geant4/Geant4SurfaceProviderTests.cpp
+++ Tests/UnitTests/Plugins/Geant4/Geant4SurfaceProviderTests.cpp
@@ -8,10 +8,9 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include "Acts/Plugins/Geant4/Geant4SurfaceProvider.hpp"
-
-#include "Acts/Geometry/GeometryContext.hpp"
 #include "Acts/Detector/LayerStructureBuilder.hpp"
+#include "Acts/Geometry/GeometryContext.hpp"
+#include "Acts/Plugins/Geant4/Geant4SurfaceProvider.hpp"
 
 #include <filesystem>
 #include <memory>
@@ -101,184 +100,163 @@ ConstructGeant4World() {
 auto gctx = Acts::GeometryContext();
 
 BOOST_AUTO_TEST_CASE(Geant4SurfaceProviderNames) {
-    // Construct the world
-    auto [physWorld, names] = ConstructGeant4World();
+  // Construct the world
+  auto [physWorld, names] = ConstructGeant4World();
 
-    // Write GDML file
-    std::filesystem::path gdmlPath = "test.gdml";
-    G4GDMLParser parser;
-    parser.Write(gdmlPath.string(), physWorld);
+  // Write GDML file
+  std::filesystem::path gdmlPath = "test.gdml";
+  G4GDMLParser parser;
+  parser.Write(gdmlPath.string(), physWorld);
 
-    // Default template parameters are fine
-    // when using names as identifiers 
-    auto spFullCfg = 
-        Acts::Experimental::Geant4SurfaceProvider<>::Config();
-    spFullCfg.gdmlPath = gdmlPath.string();
-    spFullCfg.surfaceNames = names;
-    spFullCfg.exactMatch = true;
+  // Default template parameters are fine
+  // when using names as identifiers
+  auto spFullCfg = Acts::Experimental::Geant4SurfaceProvider<>::Config();
+  spFullCfg.gdmlPath = gdmlPath.string();
+  spFullCfg.surfaceNames = names;
+  spFullCfg.exactMatch = true;
 
-    auto spFull =
-        std::make_shared<Acts::Experimental::Geant4SurfaceProvider<>>(
-            spFullCfg);
+  auto spFull =
+      std::make_shared<Acts::Experimental::Geant4SurfaceProvider<>>(spFullCfg);
 
-    auto lbFullCfg = Acts::Experimental::LayerStructureBuilder::Config();
-    lbFullCfg.surfacesProvider = spFull;
+  auto lbFullCfg = Acts::Experimental::LayerStructureBuilder::Config();
+  lbFullCfg.surfacesProvider = spFull;
 
-    auto lbFull =
-        std::make_shared<Acts::Experimental::LayerStructureBuilder>(
-            lbFullCfg);
+  auto lbFull =
+      std::make_shared<Acts::Experimental::LayerStructureBuilder>(lbFullCfg);
 
-    auto [sFull, vFull, suFull, vuFull] =
-        lbFull->construct(gctx);
+  auto [sFull, vFull, suFull, vuFull] = lbFull->construct(gctx);
 
-    BOOST_CHECK_EQUAL(sFull.size(), names.size());
-    for (int nArm = 0; nArm < nArms; nArm++) {
-        for (int nLayer = 0; nLayer < nLayers; nLayer++) {
-            for (int nChip = 0; nChip < nChips; nChip++) {
-                int sign = (nArm == 0) ? 1 : -1;
-                double posX = sign * (armOffset + nChip * cm);
-                double posY = 0;
-                double posZ = nLayer * cm;
-                Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
-        
-                BOOST_CHECK_EQUAL(
-                    sFull.at(nChips * nLayers * nArm + nChips * nLayer + nChip)
-                        ->center(gctx),
-                    pos);
-            }
-        }
-    }
-
-    // Now check that we can extract only 
-    // a subset of the surfaces
-    std::vector<std::string> leftArmNames(names.begin(), names.begin() + names.size()/2);
-
-    auto spLeftArmCfg = 
-    Acts::Experimental::Geant4SurfaceProvider<>::Config();
-    spLeftArmCfg.gdmlPath = gdmlPath.string();
-    spLeftArmCfg.surfaceNames = leftArmNames;
-    spLeftArmCfg.exactMatch = true;
-
-    auto spLeftArm =
-        std::make_shared<Acts::Experimental::Geant4SurfaceProvider<>>(
-            spLeftArmCfg);
-
-    auto lbCfg = Acts::Experimental::LayerStructureBuilder::Config();
-    lbCfg.surfacesProvider = spLeftArm;
-
-    auto lbLeftArm =
-        std::make_shared<Acts::Experimental::LayerStructureBuilder>(
-            lbCfg);
-
-    auto [sLeftArm, vLeftArm, suLeftArm, vuLeftArm] =
-        lbLeftArm->construct(gctx);
-
-    BOOST_CHECK_EQUAL(sLeftArm.size(), leftArmNames.size());
+  BOOST_CHECK_EQUAL(sFull.size(), names.size());
+  for (int nArm = 0; nArm < nArms; nArm++) {
     for (int nLayer = 0; nLayer < nLayers; nLayer++) {
-        for (int nChip = 0; nChip < nChips; nChip++) {
-            double posX = armOffset + nChip * cm;
-            double posY = 0;
-            double posZ = nLayer * cm;
-            Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
-    
-            BOOST_CHECK_EQUAL(
-                sLeftArm.at(nChips * nLayer + nChip)->center(gctx),
-                pos);
-        }
-    }
+      for (int nChip = 0; nChip < nChips; nChip++) {
+        int sign = (nArm == 0) ? 1 : -1;
+        double posX = sign * (armOffset + nChip * cm);
+        double posY = 0;
+        double posZ = nLayer * cm;
+        Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
 
-    std::filesystem::remove(gdmlPath);
+        BOOST_CHECK_EQUAL(
+            sFull.at(nChips * nLayers * nArm + nChips * nLayer + nChip)
+                ->center(gctx),
+            pos);
+      }
+    }
+  }
+
+  // Now check that we can extract only
+  // a subset of the surfaces
+  std::vector<std::string> leftArmNames(names.begin(),
+                                        names.begin() + names.size() / 2);
+
+  auto spLeftArmCfg = Acts::Experimental::Geant4SurfaceProvider<>::Config();
+  spLeftArmCfg.gdmlPath = gdmlPath.string();
+  spLeftArmCfg.surfaceNames = leftArmNames;
+  spLeftArmCfg.exactMatch = true;
+
+  auto spLeftArm =
+      std::make_shared<Acts::Experimental::Geant4SurfaceProvider<>>(
+          spLeftArmCfg);
+
+  auto lbCfg = Acts::Experimental::LayerStructureBuilder::Config();
+  lbCfg.surfacesProvider = spLeftArm;
+
+  auto lbLeftArm =
+      std::make_shared<Acts::Experimental::LayerStructureBuilder>(lbCfg);
+
+  auto [sLeftArm, vLeftArm, suLeftArm, vuLeftArm] = lbLeftArm->construct(gctx);
+
+  BOOST_CHECK_EQUAL(sLeftArm.size(), leftArmNames.size());
+  for (int nLayer = 0; nLayer < nLayers; nLayer++) {
+    for (int nChip = 0; nChip < nChips; nChip++) {
+      double posX = armOffset + nChip * cm;
+      double posY = 0;
+      double posZ = nLayer * cm;
+      Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
+
+      BOOST_CHECK_EQUAL(sLeftArm.at(nChips * nLayer + nChip)->center(gctx),
+                        pos);
+    }
+  }
+
+  std::filesystem::remove(gdmlPath);
 }
 
 BOOST_AUTO_TEST_CASE(Geant4SurfaceProviderRanges) {
-    // Construct the world
-    auto [physWorld, names] = ConstructGeant4World();
+  // Construct the world
+  auto [physWorld, names] = ConstructGeant4World();
 
-    // Write GDML file
-    std::filesystem::path gdmlPath = "test.gdml";
-    G4GDMLParser parser;
-    parser.Write(gdmlPath.string(), physWorld);
+  // Write GDML file
+  std::filesystem::path gdmlPath = "test.gdml";
+  G4GDMLParser parser;
+  parser.Write(gdmlPath.string(), physWorld);
 
-    // 1D selection -- select only the first row
-    auto sp1DCfg = 
-        Acts::Experimental::Geant4SurfaceProvider<1>::Config();
-    sp1DCfg.gdmlPath = gdmlPath.string();
-    sp1DCfg.surfaceNames = names;
-    
-    auto kdt1DOpt = 
-        Acts::Experimental::Geant4SurfaceProvider<1>::kdtOptions();
-    kdt1DOpt.range = Acts::RangeXD<1,Acts::ActsScalar>();
-    kdt1DOpt.range[0].set(-5, 5);
-    kdt1DOpt.binningValues = {Acts::BinningValue::binZ};
+  // 1D selection -- select only the first row
+  auto sp1DCfg = Acts::Experimental::Geant4SurfaceProvider<1>::Config();
+  sp1DCfg.gdmlPath = gdmlPath.string();
+  sp1DCfg.surfaceNames = names;
 
-    auto sp1D =
-        std::make_shared<Acts::Experimental::Geant4SurfaceProvider<1>>(
-            sp1DCfg, kdt1DOpt);
+  auto kdt1DOpt = Acts::Experimental::Geant4SurfaceProvider<1>::kdtOptions();
+  kdt1DOpt.range = Acts::RangeXD<1, Acts::ActsScalar>();
+  kdt1DOpt.range[0].set(-5, 5);
+  kdt1DOpt.binningValues = {Acts::BinningValue::binZ};
 
-    auto lb1DCfg = Acts::Experimental::LayerStructureBuilder::Config();
-    lb1DCfg.surfacesProvider = sp1D;
+  auto sp1D = std::make_shared<Acts::Experimental::Geant4SurfaceProvider<1>>(
+      sp1DCfg, kdt1DOpt);
 
-    auto lb1D =
-        std::make_shared<Acts::Experimental::LayerStructureBuilder>(
-            lb1DCfg);
+  auto lb1DCfg = Acts::Experimental::LayerStructureBuilder::Config();
+  lb1DCfg.surfacesProvider = sp1D;
 
-    auto [s1D, v1D, su1D, vu1D] =
-        lb1D->construct(gctx);
+  auto lb1D =
+      std::make_shared<Acts::Experimental::LayerStructureBuilder>(lb1DCfg);
 
-    BOOST_CHECK_EQUAL(s1D.size(), nChips*nArms);
-    for (int nArm = 0; nArm < nArms; nArm++) {
-        for (int nChip = 0; nChip < nChips; nChip++) {
-            int sign = (nArm == 0) ? 1 : -1;
-            double posX = sign * (armOffset + nChip * cm);
-            double posY = 0, posZ = 0;
-            Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
-        
-            BOOST_CHECK_EQUAL(
-                s1D.at(nChips * nArm + nChip)->center(gctx),
-                pos);
-        }
-    }
+  auto [s1D, v1D, su1D, vu1D] = lb1D->construct(gctx);
 
-    // 2D selection -- select only the first row
-    // of the left arm
-    auto sp2DCfg = 
-        Acts::Experimental::Geant4SurfaceProvider<2>::Config();
-    sp2DCfg.gdmlPath = gdmlPath.string();
-    sp2DCfg.surfaceNames = names;
-    
-    auto kdt2DOpt = 
-        Acts::Experimental::Geant4SurfaceProvider<2>::kdtOptions();
-    kdt2DOpt.range = Acts::RangeXD<2,Acts::ActsScalar>();
-    kdt2DOpt.range[0].set(-5, 5);
-    kdt2DOpt.range[1].set(armOffset-5, armOffset+100);
-    kdt2DOpt.binningValues = {Acts::BinningValue::binZ};
-
-    auto sp2D =
-        std::make_shared<Acts::Experimental::Geant4SurfaceProvider<2>>(
-            sp2DCfg, kdt2DOpt);
-
-    auto lb2DCfg = Acts::Experimental::LayerStructureBuilder::Config();
-    lb2DCfg.surfacesProvider = sp2D;
-
-    auto lb2D =
-        std::make_shared<Acts::Experimental::LayerStructureBuilder>(
-            lb2DCfg);
-
-    auto [s2D, v2D, su2D, vu2D] =
-        lb2D->construct(gctx);
-
-    BOOST_CHECK_EQUAL(s2D.size(), nChips);
+  BOOST_CHECK_EQUAL(s1D.size(), nChips * nArms);
+  for (int nArm = 0; nArm < nArms; nArm++) {
     for (int nChip = 0; nChip < nChips; nChip++) {
-        double posX = armOffset + nChip * cm;
-        double posY = 0, posZ = 0;
-        Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
-    
-        BOOST_CHECK_EQUAL(
-            s2D.at(nChip)->center(gctx),pos);
+      int sign = (nArm == 0) ? 1 : -1;
+      double posX = sign * (armOffset + nChip * cm);
+      double posY = 0, posZ = 0;
+      Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
+
+      BOOST_CHECK_EQUAL(s1D.at(nChips * nArm + nChip)->center(gctx), pos);
     }
+  }
 
-    std::filesystem::remove(gdmlPath);
+  // 2D selection -- select only the first row
+  // of the left arm
+  auto sp2DCfg = Acts::Experimental::Geant4SurfaceProvider<2>::Config();
+  sp2DCfg.gdmlPath = gdmlPath.string();
+  sp2DCfg.surfaceNames = names;
+
+  auto kdt2DOpt = Acts::Experimental::Geant4SurfaceProvider<2>::kdtOptions();
+  kdt2DOpt.range = Acts::RangeXD<2, Acts::ActsScalar>();
+  kdt2DOpt.range[0].set(-5, 5);
+  kdt2DOpt.range[1].set(armOffset - 5, armOffset + 100);
+  kdt2DOpt.binningValues = {Acts::BinningValue::binZ};
+
+  auto sp2D = std::make_shared<Acts::Experimental::Geant4SurfaceProvider<2>>(
+      sp2DCfg, kdt2DOpt);
+
+  auto lb2DCfg = Acts::Experimental::LayerStructureBuilder::Config();
+  lb2DCfg.surfacesProvider = sp2D;
+
+  auto lb2D =
+      std::make_shared<Acts::Experimental::LayerStructureBuilder>(lb2DCfg);
+
+  auto [s2D, v2D, su2D, vu2D] = lb2D->construct(gctx);
+
+  BOOST_CHECK_EQUAL(s2D.size(), nChips);
+  for (int nChip = 0; nChip < nChips; nChip++) {
+    double posX = armOffset + nChip * cm;
+    double posY = 0, posZ = 0;
+    Acts::Vector3 pos = Acts::Vector3(posX, posY, posZ);
+
+    BOOST_CHECK_EQUAL(s2D.at(nChip)->center(gctx), pos);
+  }
+
+  std::filesystem::remove(gdmlPath);
 }
-
 
 BOOST_AUTO_TEST_SUITE_END()
