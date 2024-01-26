c233109dc7eb3ca8336b8ba8a1b654ef896ffe5e
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-01-26 12:57:47 +0100

    boost_test

--- Tests/UnitTests/Core/TrackFitting/Gx2fTests.cpp
+++ Tests/UnitTests/Core/TrackFitting/Gx2fTests.cpp
@@ -725,7 +725,7 @@ BOOST_AUTO_TEST_CASE(DidNotConverge) {
                               startParametersFit, gx2fOptions, tracks);
 
   BOOST_REQUIRE(!res.ok());
-  BOOST_TEST((res.error() ==
+  BOOST_CHECK((res.error() ==
               Acts::Experimental::GlobalChiSquareFitterError::DidNotConverge));
 
   ACTS_INFO("*** Test: DidNotConverge -- Finish");
