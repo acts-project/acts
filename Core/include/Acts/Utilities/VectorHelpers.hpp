d893294d1c437d236afe33d903f9857911204f55
ssdetlab <ssdetlab@gmail.com>
2024-02-21 19:38:44 +0200

    Revert "Adding zero sine safety check"
    This reverts commit 04ee6fcf9bc5782e19abb10eb5b03981830d1ef9.

--- Core/include/Acts/Utilities/VectorHelpers.hpp
+++ Core/include/Acts/Utilities/VectorHelpers.hpp
@@ -140,8 +140,8 @@ inline std::array<ActsScalar, 4> evaluateTrigonomics(const Vector3& direction) {
   const ActsScalar cosTheta = z;
   const ActsScalar sinTheta = std::sqrt(1 - z * z);
   const ActsScalar invSinTheta = 1. / sinTheta;
-  const ActsScalar cosPhi = (sinTheta != 0) ? x * invSinTheta : 0;
-  const ActsScalar sinPhi = (sinTheta != 0) ? y * invSinTheta : 0;
+  const ActsScalar cosPhi = x * invSinTheta;
+  const ActsScalar sinPhi = y * invSinTheta;
 
   return {cosPhi, sinPhi, cosTheta, sinTheta};
 }
