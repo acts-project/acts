9fd5d1589d8a7881ae1b13e957cf12b55ab9efb2
Andreas Stefl <stefl.andreas@gmail.com>
2024-02-29 14:39:23 +0100

    fix fix

--- Examples/Io/Root/LinkDef.hpp
+++ Examples/Io/Root/LinkDef.hpp
@@ -1,18 +1,18 @@
 #ifdef __CLING__
 
-#pragma link C++ class vector<vector<int>>+;
-#pragma link C++ class vector<vector<long>>+;
+#pragma link C++ class vector < vector < int>> + ;
+#pragma link C++ class vector < vector < long>> + ;
 
-#pragma link C++ class vector<vector<unsigned int>>+;
-#pragma link C++ class vector<vector<unsigned long>>+;
+#pragma link C++ class vector < vector < unsigned int>> + ;
+#pragma link C++ class vector < vector < unsigned long>> + ;
 
-#pragma link C++ class vector<vector<std::int32_t>>+;
-#pragma link C++ class vector<vector<std::int64_t>>+;
+#pragma link C++ class vector < vector < std::int32_t>> + ;
+#pragma link C++ class vector < vector < std::int64_t>> + ;
 
-#pragma link C++ class vector<vector<std::uint32_t>>+;
-#pragma link C++ class vector<vector<std::uint64_t>>+;
+#pragma link C++ class vector < vector < std::uint32_t>> + ;
+#pragma link C++ class vector < vector < std::uint64_t>> + ;
 
-#pragma link C++ class vector<vector<float>>+;
-#pragma link C++ class vector<vector<double>>+;
+#pragma link C++ class vector < vector < float>> + ;
+#pragma link C++ class vector < vector < double>> + ;
 
 #endif
