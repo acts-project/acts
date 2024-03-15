63460765d418f9edd4197fdae1190e0e763b0adf
Paul Gessinger <paul.gessinger@cern.ch>
2024-03-15 10:52:01 +0100

    refactor: OrientedSurface improvements (#3026)
    This PR:
    
    1. Makes `OrientedSurface` a struct with member names
    2. Remove the `OrientedSurfaces` typedef over a vector
    3. Add aliases `AlongNormal` and `OppositeNormal` to the `Direction` type
    
    I don't think this is breaking.

--- Plugins/Json/src/PortalJsonConverter.cpp
+++ Plugins/Json/src/PortalJsonConverter.cpp
@@ -72,7 +72,7 @@ nlohmann::json Acts::PortalJsonConverter::toJson(
 std::vector<nlohmann::json> Acts::PortalJsonConverter::toJsonDetray(
     const GeometryContext& gctx, const Experimental::Portal& portal,
     std::size_t ip, const Experimental::DetectorVolume& volume,
-    const OrientedSurfaces& orientedSurfaces,
+    const std::vector<OrientedSurface>& orientedSurfaces,
     const std::vector<const Experimental::DetectorVolume*>& detectorVolumes,
     const Options& option) {
   // The overall return object
