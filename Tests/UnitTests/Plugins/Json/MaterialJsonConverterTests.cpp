db76459051b4dfed99cbe8b7222043f3150c8a57
Andreas Salzburger <asalzburger@gmail.com>
2024-03-26 15:27:10 +0100

    test of grid material writing

--- Tests/UnitTests/Plugins/Json/MaterialJsonConverterTests.cpp
+++ Tests/UnitTests/Plugins/Json/MaterialJsonConverterTests.cpp
@@ -25,7 +25,7 @@
 
 BOOST_AUTO_TEST_SUITE(MaterialJsonIO)
 
-BOOST_AUTO_TEST_CASE(IndexedSurfaceMaterialTests) {
+BOOST_AUTO_TEST_CASE(IndexedSurfaceMaterial1DTests) {
   std::vector<Acts::MaterialSlab> material;
   material.emplace_back(Acts::Material(), 0.0);  // vacuum
   material.emplace_back(
@@ -65,10 +65,89 @@ BOOST_AUTO_TEST_CASE(IndexedSurfaceMaterialTests) {
 
   nlohmann::json jMaterial = &ism;
 
-  std::cout << jMaterial.dump(2) << std::endl;
+  // Run a few tests
+  BOOST_REQUIRE(jMaterial.find("material") != jMaterial.end());
+
+  // Read it back in
+  const Acts::ISurfaceMaterial* ismRead = nullptr;
+  Acts::from_json(jMaterial, ismRead);
+  BOOST_REQUIRE(ismRead != nullptr);
+
+  // Check if it's the right type
+  const Acts::IndexedSurfaceMaterial<EqGrid>* ismReadTyped =
+      dynamic_cast<const Acts::IndexedSurfaceMaterial<EqGrid>*>(ismRead);
+  BOOST_REQUIRE(ismReadTyped != nullptr);
+
+  auto gridRead = ismReadTyped->grid();
+  BOOST_CHECK(gridRead.atPosition(Point{0.5}) == 1u);  // material 1
+  BOOST_CHECK(gridRead.atPosition(Point{1.5}) == 0u);  // vacuum
+  BOOST_CHECK(gridRead.atPosition(Point{2.5}) == 2u);  // material 2
+  BOOST_CHECK(gridRead.atPosition(Point{3.5}) == 2u);  // material 2
+  BOOST_CHECK(gridRead.atPosition(Point{4.5}) == 3u);  // material 3
+
+  // Check the accessor is ther
+  auto accessorRead = ismReadTyped->materialAccessor();
+  CHECK_CLOSE_ABS(accessorRead.material[0].thickness(), 0.0, 1e-5);
+  CHECK_CLOSE_ABS(accessorRead.material[1].thickness(), 1.0, 1e-5);
+  CHECK_CLOSE_ABS(accessorRead.material[2].thickness(), 2.0, 1e-5);
+  CHECK_CLOSE_ABS(accessorRead.material[3].thickness(), 3.0, 1e-5);
+}
+
+BOOST_AUTO_TEST_CASE(IndexedSurfaceMaterial2DTests) {
+  std::vector<Acts::MaterialSlab> material;
+  material.emplace_back(Acts::Material(), 1.0);  // vacuum
+  material.emplace_back(
+      Acts::Material::fromMolarDensity(1.0, 2.0, 3.0, 4.0, 5.0), 1.0);
+  material.emplace_back(
+      Acts::Material::fromMolarDensity(11.0, 12.0, 13.0, 14.0, 15.0), 1.0);
+  material.emplace_back(
+      Acts::Material::fromMolarDensity(21.0, 22.0, 23.0, 24.0, 25.0), 1.0);
+
+  using EqBoundEqClosed = Acts::GridAxisGenerators::EqBoundEqClosed;
+  using EqEqGrid = EqBoundEqClosed::grid_type<std::size_t>;
+  using Point = EqEqGrid::point_t;
+
+  EqBoundEqClosed eqeqBound{{-1., 1.}, 2, {-M_PI, M_PI}, 4};
+  EqEqGrid eqeqGrid{eqeqBound()};
+
+  eqeqGrid.atPosition(Point{-0.5, -M_PI * 0.75}) = 1u;  // material 1
+  eqeqGrid.atPosition(Point{-0.5, -M_PI * 0.25}) = 1u;  // material 1
+  eqeqGrid.atPosition(Point{-0.5, M_PI * 0.25}) = 0u;   // vacuum
+  eqeqGrid.atPosition(Point{-0.5, M_PI * 0.75}) = 2u;   // material 2
+
+  eqeqGrid.atPosition(Point{0.5, -M_PI * 0.75}) = 0u;  // vacuum
+  eqeqGrid.atPosition(Point{0.5, -M_PI * 0.25}) = 3u;  // material 3
+  eqeqGrid.atPosition(Point{0.5, M_PI * 0.25}) = 3u;   // material 3
+  eqeqGrid.atPosition(Point{0.5, M_PI * 0.75}) = 0u;   // vacuum
+
+  auto boundToGrid =
+      std::make_unique<const Acts::GridAccess::LocalSubspace<0u, 1u>>();
+  Acts::IndexedSurfaceMaterial<EqEqGrid>::BoundToGridLocalDelegate bToZPhi;
+  bToZPhi.connect<&Acts::GridAccess::LocalSubspace<0u, 1u>::toGridLocal>(
+      std::move(boundToGrid));
+
+  // With z shift 10
+  auto globalToGrid = std::make_unique<
+      const Acts::GridAccess::GlobalSubspace<Acts::binZ, Acts::binPhi>>();
+  Acts::IndexedSurfaceMaterial<EqEqGrid>::GlobalToGridLocalDelegate gToZphi;
+  gToZphi.connect<
+      &Acts::GridAccess::GlobalSubspace<Acts::binZ, Acts::binPhi>::toGridLocal>(
+      std::move(globalToGrid));
+
+  // Create the indexed material grid
+  Acts::IndexedSurfaceMaterial<EqEqGrid> ism(
+      std::move(eqeqGrid), Acts::IndexedMaterialAccessor{std::move(material)},
+      std::move(bToZPhi), std::move(gToZphi));
+
+  nlohmann::json jMaterial = &ism;
 
   // Run a few tests
   BOOST_REQUIRE(jMaterial.find("material") != jMaterial.end());
+
+  // Read it back in
+  const Acts::ISurfaceMaterial* ismRead = nullptr;
+  Acts::from_json(jMaterial, ismRead);
+  BOOST_REQUIRE(ismRead != nullptr);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
