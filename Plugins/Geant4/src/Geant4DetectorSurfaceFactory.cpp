fea2f618d1d11615d0e93540e6468dec45efca7d
ssdetlab <ssdetlab@gmail.com>
2024-04-02 13:23:12 +0300

    Memory and cleanup

--- Plugins/Geant4/src/Geant4DetectorSurfaceFactory.cpp
+++ Plugins/Geant4/src/Geant4DetectorSurfaceFactory.cpp
@@ -26,13 +26,13 @@ void Acts::Geant4DetectorSurfaceFactory::construct(
   auto g4Translation = g4PhysVol.GetTranslation();
   auto g4Rotation = g4PhysVol.GetRotation();
 
-  if (g4Rotation == nullptr) {
-    g4Rotation = new CLHEP::HepRotation();
-  }
+  auto newTranslation =
+      g4ToGlobal.getTranslation() + g4ToGlobal.getRotation() * g4Translation;
+  auto newRotation = (g4Rotation == nullptr)
+                         ? g4ToGlobal.getRotation() * CLHEP::HepRotation()
+                         : g4ToGlobal.getRotation() * g4Rotation->inverse();
 
-  G4Transform3D newToGlobal(
-      g4ToGlobal.getRotation() * g4Rotation->inverse(),
-      g4ToGlobal.getTranslation() + g4ToGlobal.getRotation() * g4Translation);
+  G4Transform3D newToGlobal(newRotation, newTranslation);
 
   // Get the logical volume
   auto g4LogicalVolume = g4PhysVol.GetLogicalVolume();
--- Tests/UnitTests/Plugins/Geant4/Geant4DetectorSurfaceFactoryTests.cpp
+++ Tests/UnitTests/Plugins/Geant4/Geant4DetectorSurfaceFactoryTests.cpp
@@ -172,19 +172,19 @@ BOOST_AUTO_TEST_CASE(Geant4DetecturSurfaceFactory_Transforms) {
   Acts::GeometryView3D::drawArrowForward(
       obj, surface->center(gctx), surface->center(gctx) + 100 * normal, 1000,
       10, Acts::ViewConfig({0, 255, 0}));
-  int k = 0;
-  for (auto& [el, surf] : cache.sensitiveSurfaces) {
+  auto surfaces = cache.sensitiveSurfaces;
+  for (auto [k, val] : Acts::enumerate(cache.sensitiveSurfaces)) {
+    auto [el, surf] = val;
     Acts::ViewConfig vCfg;
-    if (k == 2) {
-      vCfg.color = Acts::ColorRGB({0, 255, 255});
+    if (k == 0) {
+      vCfg.color = Acts::ColorRGB({0, 255, 0});
     } else if (k == 1) {
       vCfg.color = Acts::ColorRGB({255, 0, 0});
-    } else if (k == 0) {
-      vCfg.color = Acts::ColorRGB({0, 255, 0});
+    } else if (k == 2) {
+      vCfg.color = Acts::ColorRGB({0, 255, 255});
     }
     Acts::GeometryView3D::drawSurface(obj, *surf, gctx,
                                       Acts::Transform3::Identity(), vCfg);
-    k++;
   }
 
   obj.write("RotatedSurface.obj");
