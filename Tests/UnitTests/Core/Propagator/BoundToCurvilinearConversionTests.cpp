5ccabfd1645cd917dc5a2b1687855d10a2708512
Andreas Stefl <stefl.andreas@gmail.com>
2024-04-03 22:11:55 +0200

    refactor!: Replace `Surface` `FreeVector` input with position and direction (#2933)
    We do not depend on free parameters for `Surface` so this should not be part of the interface. I consistently replace this by position and direction vectors here.
    
    blocked by
    - https://github.com/acts-project/acts/pull/2932
    - https://github.com/acts-project/acts/pull/2811

--- Tests/UnitTests/Core/Propagator/BoundToCurvilinearConversionTests.cpp
+++ Tests/UnitTests/Core/Propagator/BoundToCurvilinearConversionTests.cpp
@@ -197,9 +197,6 @@ void test_bound_to_curvilinear(const std::vector<TestData> &test_data_list,
       }
     }
 
-    FreeVector free_param_vec = Acts::detail::transformBoundToFreeParameters(
-        *surface, geoCtx, param_vec);
-
     Vector3 direction{cos(param_vec[2]) * sin(param_vec[3]),
                       sin(param_vec[2]) * sin(param_vec[3]), cos(param_vec[3])};
     Vector3 position(surface->localToGlobal(
@@ -225,7 +222,7 @@ void test_bound_to_curvilinear(const std::vector<TestData> &test_data_list,
       // compute Jacobian for bound to curvilinear covariance transformation
       Acts::BoundMatrix b2c;
       Acts::detail::boundToCurvilinearTransportJacobian(
-          direction, surface->boundToFreeJacobian(geoCtx, free_param_vec),
+          direction, surface->boundToFreeJacobian(geoCtx, position, direction),
           Acts::FreeMatrix::Identity(),
           computeFreeToPathDerivatives(
               direction, params.parameters()[eBoundQOverP],
