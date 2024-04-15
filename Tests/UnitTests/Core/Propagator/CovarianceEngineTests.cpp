d786d64e05cbc7da7f43b5af384c2adfd7d80cf3
Andreas Stefl <stefl.andreas@gmail.com>
2024-04-15 18:58:56 +0200

    adapt unit test

--- Tests/UnitTests/Core/Propagator/CovarianceEngineTests.cpp
+++ Tests/UnitTests/Core/Propagator/CovarianceEngineTests.cpp
@@ -114,8 +114,7 @@ BOOST_AUTO_TEST_CASE(covariance_engine_test) {
   auto curvResult = detail::curvilinearState(
       covariance, jacobian, transportJacobian, derivatives, boundToFreeJacobian,
       parameters, particleHypothesis, false, 1337.);
-  BOOST_CHECK(std::get<0>(curvResult).covariance().has_value());
-  BOOST_CHECK_EQUAL(*(std::get<0>(curvResult).covariance()), covarianceBefore);
+  BOOST_CHECK(!std::get<0>(curvResult).covariance().has_value());
   BOOST_CHECK_EQUAL(std::get<2>(curvResult), 1337.);
 
   // Reset
