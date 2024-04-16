80c4981257c19a6a9456abd0b056ce98254597de
Alexander J. Pfleger <70842573+AJPfleger@users.noreply.github.com>
2024-04-16 13:39:27 +0200

    docs: remove old-style file comments (#3105)
    We do not really do this kind of comments in ACTS. Especially date and author is clear from git.

--- Examples/Algorithms/Generators/ActsExamples/Generators/MultiplicityGenerators.hpp
+++ Examples/Algorithms/Generators/ActsExamples/Generators/MultiplicityGenerators.hpp
@@ -6,10 +6,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-/// @file
-/// @date 2018-03-13
-/// @author Moritz Kiehn <msmk@cern.ch>
-
 #pragma once
 
 #include "ActsExamples/Framework/RandomNumbers.hpp"
