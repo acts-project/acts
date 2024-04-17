e6cd8b5efad6fa701cc14c7917f1c23ff6188d52
Andreas Stefl <stefl.andreas@gmail.com>
2024-04-17 10:17:07 +0200

    refactor: Refactor how the bound state covariance is handled without transport (#3104)
    Refactors the internal wirings of `boundState` if no covariance transport is requested. Currently the function might still return a covariance depending on the state of the stepper. This is a workaround to access the covariance of the stepper in case it was transported before.
    
    I refactored this and left the responsibility to the caller to get a covariance in case it was transported before hand.
    
    While doing this I also removed some unnecessary initializations and removed `std::move`s from eigen objects because they do not have an effect.

--- Core/include/Acts/TrackFitting/detail/KalmanUpdateHelpers.hpp
+++ Core/include/Acts/TrackFitting/detail/KalmanUpdateHelpers.hpp
@@ -10,6 +10,7 @@
 
 #include "Acts/EventData/MultiTrajectory.hpp"
 #include "Acts/EventData/SourceLink.hpp"
+#include "Acts/EventData/TrackParameters.hpp"
 #include "Acts/EventData/detail/CorrectedTransformationFreeToBound.hpp"
 #include "Acts/Surfaces/Surface.hpp"
 #include "Acts/Utilities/CalibrationContext.hpp"
@@ -59,17 +60,14 @@ auto kalmanHandleMeasurement(
                                          << " failed: " << res.error());
       return res.error();
     }
-    auto &[boundParams, jacobian, pathLength] = *res;
+    const auto &[boundParams, jacobian, pathLength] = *res;
 
     // Fill the track state
-    trackStateProxy.predicted() = std::move(boundParams.parameters());
-    if (boundParams.covariance().has_value()) {
-      trackStateProxy.predictedCovariance() =
-          std::move(*boundParams.covariance());
-    }
+    trackStateProxy.predicted() = boundParams.parameters();
+    trackStateProxy.predictedCovariance() = state.stepping.cov;
 
-    trackStateProxy.jacobian() = std::move(jacobian);
-    trackStateProxy.pathLength() = std::move(pathLength);
+    trackStateProxy.jacobian() = jacobian;
+    trackStateProxy.pathLength() = pathLength;
   }
 
   // We have predicted parameters, so calibrate the uncalibrated input
@@ -154,17 +152,14 @@ auto kalmanHandleNoMeasurement(
     if (!res.ok()) {
       return res.error();
     }
-    auto &[boundParams, jacobian, pathLength] = *res;
+    const auto &[boundParams, jacobian, pathLength] = *res;
 
     // Fill the track state
-    trackStateProxy.predicted() = std::move(boundParams.parameters());
-    if (boundParams.covariance().has_value()) {
-      trackStateProxy.predictedCovariance() =
-          std::move(*boundParams.covariance());
-    }
+    trackStateProxy.predicted() = boundParams.parameters();
+    trackStateProxy.predictedCovariance() = state.stepping.cov;
 
-    trackStateProxy.jacobian() = std::move(jacobian);
-    trackStateProxy.pathLength() = std::move(pathLength);
+    trackStateProxy.jacobian() = jacobian;
+    trackStateProxy.pathLength() = pathLength;
 
     // Set the filtered parameter index to be the same with predicted
     // parameter
