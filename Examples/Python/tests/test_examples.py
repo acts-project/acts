2bffed60fa5f7c20298de565de4fd2a1394485eb
ragansu <ragansu@gmail.com>
2024-04-19 18:43:21 +0200

    test_examples from ragansu_ambi

--- Examples/Python/tests/test_examples.py
+++ Examples/Python/tests/test_examples.py
@@ -1223,7 +1223,7 @@ def test_ML_Ambiguity_Solver(tmp_path, assert_root_hash):
     env["ACTS_LOG_FAILURE_THRESHOLD"] = "ERROR"
     try:
         subprocess.check_call(
-            [sys.executable, str(script), "-n1", "--MLSolver"],
+            [sys.executable, str(script), "-n5", "--SolverType", "ML"],
             cwd=tmp_path,
             env=env,
             stderr=subprocess.STDOUT,
