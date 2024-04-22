616a383244740e0a5f0ba6220e2bcbcf5ac7e843
Jeremy Couthures <jeremy.couthures@lapp.in2p3.fr>
2024-04-22 15:15:46 +0200

    Add csv bucket writer

---
+++ Examples/Io/Csv/include/ActsExamples/Io/Csv/CsvBucketWriter.hpp
@@ -0,0 +1,79 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/Definitions/TrackParametrization.hpp"
+#include "Acts/Geometry/GeometryContext.hpp"
+#include "Acts/Geometry/GeometryHierarchyMap.hpp"
+#include "Acts/Geometry/GeometryIdentifier.hpp"
+#include "Acts/Utilities/Helpers.hpp"
+#include "ActsExamples/Digitization/DigitizationConfig.hpp"
+#include "ActsExamples/EventData/Cluster.hpp"
+#include "ActsExamples/EventData/Index.hpp"
+#include "ActsExamples/EventData/Measurement.hpp"
+#include "ActsExamples/EventData/SimHit.hpp"
+#include "ActsExamples/EventData/SimSeed.hpp"
+#include "ActsExamples/EventData/SimSpacePoint.hpp"
+#include "ActsExamples/Framework/WriterT.hpp"
+
+#include <string>
+
+namespace ActsExamples {
+
+/// @class CsvBucketWriter
+///
+/// This writes one file per event containing information about the
+/// buckets
+///
+///     event000000001-buckets.csv
+///     event000000002-buckets.csv
+///     ...
+///
+/// Intrinsically thread-safe as one file per event.
+class CsvBucketWriter final
+    : public WriterT<std::vector<SimSpacePointContainer>> {
+ public:
+  struct Config {
+    /// Which measurement collection to write.
+    std::string inputBuckets;
+    /// Where to place output files
+    std::string outputDir;
+    /// Number of decimal digits for floating point precision in output.
+    int outputPrecision = std::numeric_limits<float>::max_digits10;
+  };
+
+  /// Constructor with
+  /// @param config configuration struct
+  /// @param level logging level
+  CsvBucketWriter(const Config& config, Acts::Logging::Level level);
+
+  /// Virtual destructor
+  ~CsvBucketWriter() override;
+
+  /// End-of-run hook
+  ProcessCode finalize() override;
+
+  /// Get readonly access to the config parameters
+  const Config& config() const { return m_cfg; }
+
+ protected:
+  /// This implementation holds the actual writing method
+  /// and is called by the WriterT<>::write interface
+  ///
+  /// @param ctx The Algorithm context with per event information
+  /// @param buckets is the data to be written out
+  ProcessCode writeT(
+      const AlgorithmContext& ctx,
+      const std::vector<SimSpacePointContainer>& buckets) override;
+
+ private:
+  Config m_cfg;
+};
+
+}  // namespace ActsExamples
