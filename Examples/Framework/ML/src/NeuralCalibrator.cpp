ad56a02dad3c86396eb5f720c73b15f90d0ed0ea
Andreas Stefl <stefl.andreas@gmail.com>
2024-04-24 18:12:30 +0200

    refactor NeuralCalibrator

--- Examples/Framework/ML/src/NeuralCalibrator.cpp
+++ Examples/Framework/ML/src/NeuralCalibrator.cpp
@@ -6,6 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "Acts/Definitions/TrackParametrization.hpp"
 #include "Acts/EventData/SourceLink.hpp"
 #include "Acts/Utilities/CalibrationContext.hpp"
 #include "Acts/Utilities/UnitVectors.hpp"
@@ -100,84 +101,67 @@ void ActsExamples::NeuralCalibrator::calibrate(
 
   const Acts::Surface& referenceSurface = trackState.referenceSurface();
 
-  std::visit(
-      [&](const auto& measurement) {
-        auto E = measurement.expander();
-        auto P = measurement.projector();
-        Acts::ActsVector<Acts::eBoundSize> fpar = E * measurement.parameters();
-        Acts::ActsSquareMatrix<Acts::eBoundSize> fcov =
-            E * measurement.covariance() * E.transpose();
+  const auto& measurement = measurements[idxSourceLink.index()];
 
-        Acts::Vector3 dir = Acts::makeDirectionFromPhiTheta(
-            fpar[Acts::eBoundPhi], fpar[Acts::eBoundTheta]);
-        Acts::Vector3 globalPosition = referenceSurface.localToGlobal(
-            gctx, fpar.segment<2>(Acts::eBoundLoc0), dir);
+  const Acts::BoundVector& fpar = measurement.fullParameters();
+  const Acts::BoundMatrix& fcov = measurement.fullCovariance();
 
-        // Rotation matrix. When applied to global coordinates, they
-        // are rotated into the local reference frame of the
-        // surface. Note that this such a rotation can be found by
-        // inverting a matrix whose columns correspond to the
-        // coordinate axes of the local coordinate system.
-        Acts::RotationMatrix3 rot =
-            referenceSurface.referenceFrame(gctx, globalPosition, dir)
-                .inverse();
-        std::pair<double, double> angles =
-            Acts::VectorHelpers::incidentAngles(dir, rot);
+  Acts::Vector3 dir = Acts::makeDirectionFromPhiTheta(fpar[Acts::eBoundPhi],
+                                                      fpar[Acts::eBoundTheta]);
+  Acts::Vector3 globalPosition = referenceSurface.localToGlobal(
+      gctx, fpar.segment<2>(Acts::eBoundLoc0), dir);
 
-        input[iInput++] = angles.first;
-        input[iInput++] = angles.second;
-        input[iInput++] = fpar[Acts::eBoundLoc0];
-        input[iInput++] = fpar[Acts::eBoundLoc1];
-        input[iInput++] = fcov(Acts::eBoundLoc0, Acts::eBoundLoc0);
-        input[iInput++] = fcov(Acts::eBoundLoc1, Acts::eBoundLoc1);
-        if (iInput != m_nInputs) {
-          throw std::runtime_error("Expected input size of " +
-                                   std::to_string(m_nInputs) +
-                                   ", got: " + std::to_string(iInput));
-        }
+  // Rotation matrix. When applied to global coordinates, they
+  // are rotated into the local reference frame of the
+  // surface. Note that this such a rotation can be found by
+  // inverting a matrix whose columns correspond to the
+  // coordinate axes of the local coordinate system.
+  Acts::RotationMatrix3 rot =
+      referenceSurface.referenceFrame(gctx, globalPosition, dir).inverse();
+  std::pair<double, double> angles =
+      Acts::VectorHelpers::incidentAngles(dir, rot);
 
-        // Input is a single row, hence .front()
-        std::vector<float> output =
-            m_model.runONNXInference(inputBatch).front();
-        // Assuming 2-D measurements, the expected params structure is:
-        // [           0,    nComponent[ --> priors
-        // [  nComponent,  3*nComponent[ --> means
-        // [3*nComponent,  5*nComponent[ --> variances
-        std::size_t nParams = 5 * m_nComponents;
-        if (output.size() != nParams) {
-          throw std::runtime_error(
-              "Got output vector of size " + std::to_string(output.size()) +
-              ", expected size " + std::to_string(nParams));
-        }
+  input[iInput++] = angles.first;
+  input[iInput++] = angles.second;
+  input[iInput++] = fpar[Acts::eBoundLoc0];
+  input[iInput++] = fpar[Acts::eBoundLoc1];
+  input[iInput++] = fcov(Acts::eBoundLoc0, Acts::eBoundLoc0);
+  input[iInput++] = fcov(Acts::eBoundLoc1, Acts::eBoundLoc1);
+  if (iInput != m_nInputs) {
+    throw std::runtime_error("Expected input size of " +
+                             std::to_string(m_nInputs) +
+                             ", got: " + std::to_string(iInput));
+  }
 
-        // Most probable value computation of mixture density
-        std::size_t iMax = 0;
-        if (m_nComponents > 1) {
-          iMax = std::distance(
-              output.begin(),
-              std::max_element(output.begin(), output.begin() + m_nComponents));
-        }
-        std::size_t iLoc0 = m_nComponents + iMax * 2;
-        std::size_t iVar0 = 3 * m_nComponents + iMax * 2;
+  // Input is a single row, hence .front()
+  std::vector<float> output = m_model.runONNXInference(inputBatch).front();
+  // Assuming 2-D measurements, the expected params structure is:
+  // [           0,    nComponent[ --> priors
+  // [  nComponent,  3*nComponent[ --> means
+  // [3*nComponent,  5*nComponent[ --> variances
+  std::size_t nParams = 5 * m_nComponents;
+  if (output.size() != nParams) {
+    throw std::runtime_error("Got output vector of size " +
+                             std::to_string(output.size()) +
+                             ", expected size " + std::to_string(nParams));
+  }
 
-        fpar[Acts::eBoundLoc0] = output[iLoc0];
-        fpar[Acts::eBoundLoc1] = output[iLoc0 + 1];
-        fcov(Acts::eBoundLoc0, Acts::eBoundLoc0) = output[iVar0];
-        fcov(Acts::eBoundLoc1, Acts::eBoundLoc1) = output[iVar0 + 1];
+  // Most probable value computation of mixture density
+  std::size_t iMax = 0;
+  if (m_nComponents > 1) {
+    iMax = std::distance(
+        output.begin(),
+        std::max_element(output.begin(), output.begin() + m_nComponents));
+  }
+  std::size_t iLoc0 = m_nComponents + iMax * 2;
+  std::size_t iVar0 = 3 * m_nComponents + iMax * 2;
 
-        constexpr std::size_t kSize =
-            std::remove_reference_t<decltype(measurement)>::size();
-        std::array<Acts::BoundIndices, kSize> indices = measurement.indices();
-        Acts::ActsVector<kSize> cpar = P * fpar;
-        Acts::ActsSquareMatrix<kSize> ccov = P * fcov * P.transpose();
-
-        Acts::SourceLink sl{idxSourceLink};
-
-        Acts::Measurement<Acts::BoundIndices, kSize> calibrated(
-            std::move(sl), indices, cpar, ccov);
-
-        trackState.allocateCalibrated(calibrated.size());
-        trackState.setCalibrated(calibrated);
-      },
-      measurements[idxSourceLink.index()]);
+  Measurement measurementCopy = measurement;
+  measurementCopy.parameters()[Acts::eBoundLoc0] = output[iLoc0];
+  measurementCopy.parameters()[Acts::eBoundLoc1] = output[iLoc0 + 1];
+  measurementCopy.covariance()(Acts::eBoundLoc0, Acts::eBoundLoc0) =
+      output[iVar0];
+  measurementCopy.covariance()(Acts::eBoundLoc1, Acts::eBoundLoc1) =
+      output[iVar0 + 1];
+  trackState.setCalibrated(measCopy);
 }
