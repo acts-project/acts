c1e1ce29e90fa65a1d9da43b6ec63c99b72f4315
ragansu <ragansu@gmail.com>
2024-05-02 15:09:38 +0200

    bug fixed

--- Examples/Scripts/Python/full_chain_odd.py
+++ Examples/Scripts/Python/full_chain_odd.py
@@ -286,6 +286,7 @@ addCKFTracks(
         loc0=(-4.0 * u.mm, 4.0 * u.mm),
         nMeasurementsMin=7,
         maxHoles=2,
+        maxOutliers=2,
     ),
     CkfConfig(
         seedDeduplication=True,
