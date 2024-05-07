926f4f56442227cb9dccc7d24dd272bb35c0f942
Andreas Salzburger <asalzburger@gmail.com>
2024-05-07 23:01:48 +0200

    switch writer to map for config

--- Examples/Python/tests/test_writer.py
+++ Examples/Python/tests/test_writer.py
@@ -370,8 +370,8 @@ def test_root_writer_interface(writer, conf_const, tmp_path, trk_geo):
     for k, _ in inspect.getmembers(config):
         if k.startswith("input"):
             kw[k] = "collection"
-        if k == "trackingGeometry":
-            kw[k] = trk_geo
+        if k == "surfaceByIdentifier":
+            kw[k] = trk_geo.geoIdSurfaceMap()
 
     assert conf_const(writer, **kw)
 
