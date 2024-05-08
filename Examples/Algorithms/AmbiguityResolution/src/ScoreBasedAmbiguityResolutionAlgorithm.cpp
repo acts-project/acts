acts-project/acts

commit 201dec0b9b70e0b47fa19eb24b7d38fdb5973ef7

feat: Athena Ambiguity Resolution Algorithm (#3139)
This is the PR for Ambiguity Resolution algorithm inspired from ATLAS-Athena

In this PR we are implementing a score based Ambiguity resolution which takes inspiration from ATLAS Ambiguity resolution.

Along with the core Functionality, I have added Algorithm function in Examples, made necessary modification for Pybind and run_full_chain_odd.py and made unit tests.

To replace the detector dependent hard-coding a config file in *.json format can be used. Hence a Json Config Reader is also included in this PR.


This Project was guided by Corentin, Carlo and David.

Regards
Ragansu.

Examples/Algorithms/AmbiguityResolution/src/ScoreBasedAmbiguityResolutionAlgorithm.cpp
@@ -1,0 +1,151 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "ActsExamples/AmbiguityResolution/ScoreBasedAmbiguityResolutionAlgorithm.hpp"
+
+#include "Acts/AmbiguityResolution/ScoreBasedAmbiguityResolution.hpp"
+#include "Acts/EventData/MultiTrajectoryHelpers.hpp"
+#include "Acts/Plugins/Json/AmbiguityConfigJsonConverter.hpp"
+#include "Acts/Utilities/Logger.hpp"
+#include "ActsExamples/EventData/IndexSourceLink.hpp"
+#include "ActsExamples/EventData/Measurement.hpp"
+#include "ActsExamples/Framework/ProcessCode.hpp"
+#include "ActsExamples/Framework/WhiteBoard.hpp"
+
+#include <fstream>
+
+namespace {
+
+Acts::ScoreBasedAmbiguityResolution::Config transformConfig(
+    const ActsExamples::ScoreBasedAmbiguityResolutionAlgorithm::Config& cfg,
+    const std::string& configFile) {
+  Acts::ScoreBasedAmbiguityResolution::Config result;
+
+  Acts::ConfigPair configPair;
+  nlohmann::json json_file;
+  std::ifstream file(configFile);
+  if (!file.is_open()) {
+    std::cerr << "Error opening file: " << configFile << std::endl;
+    return {};
+  }
+  file >> json_file;
+  file.close();
+
+  from_json(json_file, configPair);
+
+  result.volumeMap = configPair.first;
+  result.detectorConfigs = configPair.second;
+  result.minScore = cfg.minScore;
+  result.minScoreSharedTracks = cfg.minScoreSharedTracks;
+  result.maxSharedTracksPerMeasurement = cfg.maxSharedTracksPerMeasurement;
+  result.maxShared = cfg.maxShared;
+  result.pTMin = cfg.pTMin;
+  result.pTMax = cfg.pTMax;
+  result.phiMin = cfg.phiMin;
+  result.phiMax = cfg.phiMax;
+  result.etaMin = cfg.etaMin;
+  result.etaMax = cfg.etaMax;
+  result.useAmbiguityFunction = cfg.useAmbiguityFunction;
+  return result;
+}
+
+std::size_t sourceLinkHash(const Acts::SourceLink& a) {
+  return static_cast<std::size_t>(
+      a.get<ActsExamples::IndexSourceLink>().index());
+}
+
+bool sourceLinkEquality(const Acts::SourceLink& a, const Acts::SourceLink& b) {
+  return a.get<ActsExamples::IndexSourceLink>().index() ==
+         b.get<ActsExamples::IndexSourceLink>().index();
+}
+
+bool doubleHolesFilter(const Acts::TrackProxy<Acts::ConstVectorTrackContainer,
+                                              Acts::ConstVectorMultiTrajectory,
+                                              std::shared_ptr, true>& track) {
+  bool doubleFlag = false;
+  int counter = 0;
+  for (const auto& ts : track.trackStatesReversed()) {
+    auto iTypeFlags = ts.typeFlags();
+    if (!iTypeFlags.test(Acts::TrackStateFlag::HoleFlag)) {
+      doubleFlag = false;
+    }
+
+    if (iTypeFlags.test(Acts::TrackStateFlag::HoleFlag)) {
+      if (doubleFlag) {
+        counter++;
+        doubleFlag = false;
+      } else {
+        doubleFlag = true;
+      };
+    }
+  }
+  if (counter > 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+}  // namespace
+
+ActsExamples::ScoreBasedAmbiguityResolutionAlgorithm::
+    ScoreBasedAmbiguityResolutionAlgorithm(
+        ActsExamples::ScoreBasedAmbiguityResolutionAlgorithm::Config cfg,
+        Acts::Logging::Level lvl)
+    : ActsExamples::IAlgorithm("ScoreBasedAmbiguityResolutionAlgorithm", lvl),
+      m_cfg(std::move(cfg)),
+      m_ambi(transformConfig(cfg, m_cfg.configFile), logger().clone()) {
+  if (m_cfg.inputTracks.empty()) {
+    throw std::invalid_argument("Missing trajectories input collection");
+  }
+  if (m_cfg.outputTracks.empty()) {
+    throw std::invalid_argument("Missing trajectories output collection");
+  }
+  m_inputTracks.initialize(m_cfg.inputTracks);
+  m_outputTracks.initialize(m_cfg.outputTracks);
+}
+
+ActsExamples::ProcessCode
+ActsExamples::ScoreBasedAmbiguityResolutionAlgorithm::execute(
+    const AlgorithmContext& ctx) const {
+  const auto& tracks = m_inputTracks(ctx);  // Read input data
+  ACTS_VERBOSE("Number of input tracks: " << tracks.size());
+
+  std::vector<std::vector<Acts::ScoreBasedAmbiguityResolution::MeasurementInfo>>
+      measurementsPerTracks;
+
+  std::vector<std::vector<Acts::ScoreBasedAmbiguityResolution::TrackFeatures>>
+      trackFeaturesVectors;
+  measurementsPerTracks = m_ambi.computeInitialState(
+      tracks, &sourceLinkHash, &sourceLinkEquality, trackFeaturesVectors);
+
+  Acts::ScoreBasedAmbiguityResolution::OptionalCuts<
+      Acts::ConstVectorTrackContainer, Acts::ConstVectorMultiTrajectory,
+      std::shared_ptr, true>
+      optionalCuts;
+  optionalCuts.cuts.push_back(doubleHolesFilter);
+  std::vector<int> goodTracks = m_ambi.solveAmbiguity(
+      tracks, measurementsPerTracks, trackFeaturesVectors, optionalCuts);
+  // Prepare the output track collection from the IDs
+  TrackContainer solvedTracks{std::make_shared<Acts::VectorTrackContainer>(),
+                              std::make_shared<Acts::VectorMultiTrajectory>()};
+  solvedTracks.ensureDynamicColumns(tracks);
+  for (auto iTrack : goodTracks) {
+    auto destProxy = solvedTracks.makeTrack();
+    auto srcProxy = tracks.getTrack(iTrack);
+    destProxy.copyFrom(srcProxy, false);
+    destProxy.tipIndex() = srcProxy.tipIndex();
+  }
+
+  ActsExamples::ConstTrackContainer outputTracks{
+      std::make_shared<Acts::ConstVectorTrackContainer>(
+          std::move(solvedTracks.container())),
+      tracks.trackStateContainerHolder()};
+
+  m_outputTracks(ctx, std::move(outputTracks));
+  return ActsExamples::ProcessCode::SUCCESS;
+}

Plugins/Json/include/Acts/Plugins/Json/AmbiguityConfigJsonConverter.hpp
@@ -1,0 +1,23 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2023-2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/AmbiguityResolution/ScoreBasedAmbiguityResolution.hpp"
+#include "Acts/Plugins/Json/ActsJson.hpp"
+
+namespace Acts {
+
+using DetectorConfig = ScoreBasedAmbiguityResolution::DetectorConfig;
+using ConfigPair =
+    std::pair<std::map<std::size_t, std::size_t>, std::vector<DetectorConfig>>;
+/// This function is used to convert the ScoreBasedAmbiguityResolutionCongig
+/// from JSON to C++
+void from_json(const nlohmann::json& j, ConfigPair& p);
+
+}  // namespace Acts

Core/include/Acts/AmbiguityResolution/ScoreBasedAmbiguityResolution.hpp
@@ -1,0 +1,229 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/Definitions/Units.hpp"
+#include "Acts/EventData/TrackContainer.hpp"
+#include "Acts/Utilities/Delegate.hpp"
+#include "Acts/Utilities/Logger.hpp"
+
+#include <cstddef>
+#include <map>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <boost/container/flat_map.hpp>
+#include <boost/container/flat_set.hpp>
+
+namespace Acts {
+
+/// Generic implementation of the score based ambiguity resolution.
+/// The alhorithm is based on the following steps:
+/// 1) Compute the initial state of the tracks
+/// 2) Compute the score of each track
+/// 3) Removes hits that are not good enough for each track
+/// 4) Remove tracks that have a score below a certain threshold or not have
+/// enough hits
+/// 5) Remove tracks that are not good enough based on cuts Contains method for
+/// data preparations
+class ScoreBasedAmbiguityResolution {
+ public:
+  /// @brief Detector configuration struct : contains the configuration for each detector
+  ///
+  /// The configuration can be saved in a json file and loaded from there.
+  ///
+  struct DetectorConfig {
+    int hitsScoreWeight = 0;
+    int holesScoreWeight = 0;
+    int outliersScoreWeight = 0;
+    int otherScoreWeight = 0;
+
+    std::size_t minHits = 0;
+    std::size_t maxHits = 0;
+    std::size_t maxHoles = 0;
+    std::size_t maxOutliers = 0;
+    std::size_t maxSharedHits = 0;
+
+    /// if true, the shared hits are considered as bad hits for this detector
+    bool sharedHitsFlag = false;
+
+    std::size_t detectorId = 0;
+
+    /// a list of values from  0 to 1, the higher number of hits, higher value
+    /// in the list is multiplied to ambuiguity score applied only if
+    /// useAmbiguityFunction is true
+    std::vector<double> factorHits = {1.0};
+
+    /// a list of values from  0 to 1, the higher number of holes, lower value
+    /// in the list is multiplied to ambuiguity score applied only if
+    /// useAmbiguityFunction is true
+    std::vector<double> factorHoles = {1.0};
+  };
+
+  /// @brief  TrackFeatures struct : contains the features that are counted for each track.
+  ///
+  /// The trackFeatures is used to compute the score of each track
+  struct TrackFeatures {
+    std::size_t nHits = 0;
+    std::size_t nHoles = 0;
+    std::size_t nOutliers = 0;
+    std::size_t nSharedHits = 0;
+  };
+
+  /// @brief MeasurementInfo : contains the measurement ID and the detector ID
+  struct MeasurementInfo {
+    std::size_t iMeasurement = 0;
+    std::size_t detectorId = 0;
+    bool isOutlier = false;
+  };
+
+  /// @brief Configuration struct : contains the configuration for the ambiguity resolution.
+  struct Config {
+    std::map<std::size_t, std::size_t> volumeMap = {{0, 0}};
+    std::vector<DetectorConfig> detectorConfigs;
+    /// minimum score for any track
+    double minScore = 0;
+    /// minimum score for shared tracks
+    double minScoreSharedTracks = 0;
+    /// maximum number of shared tracks per measurement
+    std::size_t maxSharedTracksPerMeasurement = 10;
+    /// maximum number of shared hit per track
+    std::size_t maxShared = 5;
+
+    double pTMin = 0 * UnitConstants::GeV;
+    double pTMax = 1e5 * UnitConstants::GeV;
+
+    double phiMin = -M_PI * UnitConstants::rad;
+    double phiMax = M_PI * UnitConstants::rad;
+
+    double etaMin = -5;
+    double etaMax = 5;
+
+    // if true, the ambiguity score is computed based on a different function.
+    bool useAmbiguityFunction = false;
+  };
+
+  /// @brief OptionalCuts struct : contains the optional cuts to be applied.
+  ///
+  /// The optional cuts,weights and score are used to remove tracks that are not
+  /// good enough, based on some criteria. Users are free to add their own cuts
+  /// with the help of this struct.
+  template <typename track_container_t, typename traj_t,
+            template <typename> class holder_t, bool ReadOnly>
+  struct OptionalCuts {
+    using OptionalFilter =
+        std::function<bool(const Acts::TrackProxy<track_container_t, traj_t,
+                                                  holder_t, ReadOnly>&)>;
+
+    using OptionalScoreModifier = std::function<void(
+        const Acts::TrackProxy<track_container_t, traj_t, holder_t, ReadOnly>&,
+        double&)>;
+    std::vector<OptionalFilter> cuts = {};
+    std::vector<OptionalScoreModifier> weights = {};
+
+    /// applied only if useAmbiguityFunction is true
+    std::vector<OptionalScoreModifier> scores = {};
+  };
+
+  ScoreBasedAmbiguityResolution(
+      const Config& cfg,
+      std::unique_ptr<const Logger> logger =
+          getDefaultLogger("ScoreBasedAmbiguityResolution", Logging::INFO))
+      : m_cfg{cfg}, m_logger{std::move(logger)} {}
+
+  /// Compute the initial state of the tracks.
+  ///
+  /// @param tracks is the input track container
+  /// @param sourceLinkHash is the  source links
+  /// @param sourceLinkEquality is the equality function for the source links
+  /// @param trackFeaturesVectors is the trackFeatures map from detector ID to trackFeatures
+  /// @return a vector of the initial state of the tracks
+  template <typename track_container_t, typename traj_t,
+            template <typename> class holder_t, typename source_link_hash_t,
+            typename source_link_equality_t>
+  std::vector<std::vector<MeasurementInfo>> computeInitialState(
+      const TrackContainer<track_container_t, traj_t, holder_t>& tracks,
+      source_link_hash_t sourceLinkHash,
+      source_link_equality_t sourceLinkEquality,
+      std::vector<std::vector<TrackFeatures>>& trackFeaturesVectors) const;
+
+  /// Compute the score of each track.
+  ///
+  /// @param tracks is the input track container
+  /// @param trackFeaturesVectors is the trackFeatures map from detector ID to trackFeatures
+  /// @param optionalCuts is the user defined optional cuts to be applied.
+  /// @return a vector of scores for each track
+  template <typename track_container_t, typename traj_t,
+            template <typename> class holder_t, bool ReadOnly>
+  std::vector<double> simpleScore(
+      const TrackContainer<track_container_t, traj_t, holder_t>& tracks,
+      const std::vector<std::vector<TrackFeatures>>& trackFeaturesVectors,
+      const OptionalCuts<track_container_t, traj_t, holder_t, ReadOnly>&
+          optionalCuts = {}) const;
+
+  /// Compute the score of each track based on the ambiguity function.
+  ///
+  /// @param tracks is the input track container
+  /// @param trackFeaturesVectors is the trackFeatures map from detector ID to trackFeatures
+  /// @param optionalCuts is the user defined optional cuts to be applied.
+  /// @return a vector of scores for each track
+  template <typename track_container_t, typename traj_t,
+            template <typename> class holder_t, bool ReadOnly>
+  std::vector<double> ambiguityScore(
+      const TrackContainer<track_container_t, traj_t, holder_t>& tracks,
+      const std::vector<std::vector<TrackFeatures>>& trackFeaturesVectors,
+      const OptionalCuts<track_container_t, traj_t, holder_t, ReadOnly>&
+          optionalCuts = {}) const;
+
+  /// Remove hits that are not good enough for each track and removes tracks
+  /// that have a score below a certain threshold or not enough hits.
+  ///
+  /// @brief Remove tracks that are not good enough based on cuts
+  /// @param trackScore is the score of each track
+  /// @param trackFeaturesVectors is the trackFeatures map for each track
+  /// @param measurementsPerTrack is the list of measurements for each track
+  /// @return a vector of IDs of the tracks we want to keep
+  std::vector<bool> getCleanedOutTracks(
+      const std::vector<double>& trackScore,
+      const std::vector<std::vector<TrackFeatures>>& trackFeaturesVectors,
+      const std::vector<std::vector<MeasurementInfo>>& measurementsPerTrack)
+      const;
+
+  /// Remove tracks that are bad based on cuts and weighted scores.
+  ///
+  /// @brief Remove tracks that are not good enough
+  /// @param tracks is the input track container
+  /// @param measurementsPerTrack is the list of measurements for each track
+  /// @param trackFeaturesVectors is the map of detector id to trackFeatures for each track
+  /// @param optionalCuts is the optional cuts to be applied
+  /// @return a vector of IDs of the tracks we want to keep
+  template <typename track_container_t, typename traj_t,
+            template <typename> class holder_t, bool ReadOnly>
+  std::vector<int> solveAmbiguity(
+      const TrackContainer<track_container_t, traj_t, holder_t>& tracks,
+      const std::vector<std::vector<MeasurementInfo>>& measurementsPerTrack,
+      const std::vector<std::vector<TrackFeatures>>& trackFeaturesVectors,
+      const OptionalCuts<track_container_t, traj_t, holder_t, ReadOnly>&
+          optionalCuts = {}) const;
+
+ private:
+  Config m_cfg;
+
+  /// Logging instance
+  std::unique_ptr<const Logger> m_logger = nullptr;
+
+  /// Private access to logging instance
+  const Logger& logger() const;
+};
+
+}  // namespace Acts
+
+#include "Acts/AmbiguityResolution/ScoreBasedAmbiguityResolution.ipp"

commit 5947cb0e2f8654ddf01b4bf33d97685edbc8f851

fix: Fix my local build after #3139 (#3178)
My local builds fails after https://github.com/acts-project/acts/pull/3139 which I think is caused by a more recent json version that includes `std::pair` `from_json` implementations which clash with win the overload resolution.

Examples/Algorithms/AmbiguityResolution/src/ScoreBasedAmbiguityResolutionAlgorithm.cpp
@@ -36,7 +36,7 @@ Acts::ScoreBasedAmbiguityResolution::Config transformConfig(
   file >> json_file;
   file.close();
 
-  from_json(json_file, configPair);
+  Acts::from_json(json_file, configPair);
 
   result.volumeMap = configPair.first;
   result.detectorConfigs = configPair.second;
@@ -90,6 +90,7 @@ bool doubleHolesFilter(const Acts::TrackProxy<Acts::ConstVectorTrackContainer,
     return false;
   }
 }
+
 }  // namespace
 
 ActsExamples::ScoreBasedAmbiguityResolutionAlgorithm::

Plugins/Json/include/Acts/Plugins/Json/AmbiguityConfigJsonConverter.hpp
@@ -9,7 +9,12 @@
 #pragma once
 
 #include "Acts/AmbiguityResolution/ScoreBasedAmbiguityResolution.hpp"
-#include "Acts/Plugins/Json/ActsJson.hpp"
+
+#include <map>
+#include <utility>
+#include <vector>
+
+#include <nlohmann/json.hpp>
 
 namespace Acts {
 