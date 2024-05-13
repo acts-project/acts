524cc22f968b9e7f225fd200c18ce3c3e678bfd7
Andreas Salzburger <asalzburger@gmail.com>
2024-05-13 23:16:25 +0200

    undo rename

--- Examples/Io/Obj/src/ObjTrackingGeometryWriter.cpp
+++ Examples/Io/Obj/src/ObjTrackingGeometryWriter.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "ActsExamples/Io/Obj/ObjTrackingGeometryWriter.hpp"
+#include "ActsExamples/Io/Plugins/ObjTrackingGeometryWriter.hpp"
 
 #include "Acts/Utilities/Logger.hpp"
 #include "ActsExamples/Framework/AlgorithmContext.hpp"
