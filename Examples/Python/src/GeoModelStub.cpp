92cdcf2ffc26de5af95b897d42543651f7f5f954
Andreas Salzburger <asalzburger@gmail.com>
2024-05-22 14:36:08 +0200

    Update Examples/Python/src/GeoModelStub.cpp
    Co-authored-by: Alexander J. Pfleger <70842573+AJPfleger@users.noreply.github.com>

--- Examples/Python/src/GeoModelStub.cpp
+++ Examples/Python/src/GeoModelStub.cpp
@@ -6,12 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-namespace Acts {
-namespace Python {
-struct Context;
-}  // namespace Python
-}  // namespace Acts
-
 namespace Acts::Python {
+struct Context;
 void addGeoModel(Context& /*ctx*/) {}
 }  // namespace Acts::Python
