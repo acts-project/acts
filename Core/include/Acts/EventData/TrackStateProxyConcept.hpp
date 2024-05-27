22558d2df165a146448c1c2e7b848d5fdbbc0e45
Andreas Stefl <stefl.andreas@gmail.com>
2024-05-27 16:55:49 +0200

    try fix concept

--- Core/include/Acts/EventData/TrackStateProxyConcept.hpp
+++ Core/include/Acts/EventData/TrackStateProxyConcept.hpp
@@ -52,7 +52,7 @@ using ConstDynamicMeasurementCovariance = Eigen::Map<
     const Eigen::Matrix<Covariance::Scalar, Eigen::Dynamic, Eigen::Dynamic,
                         Eigen::ColMajor | Eigen::AutoAlign>>;
 
-constexpr static auto ProjectorFlags = Eigen::RowMajor | Eigen::AutoAlign;
+constexpr static auto ProjectorFlags = Eigen::ColMajor | Eigen::AutoAlign;
 using Projector = Eigen::Matrix<typename Covariance::Scalar, eBoundSize,
                                 eBoundSize, ProjectorFlags>;
 
