e4f239898d4c5d0b1b7b2a1f95acb9040d01d4db
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-06-02 22:16:39 +0200

    always 5

--- Core/include/Acts/TrackFitting/GlobalChiSquareFitter.hpp
+++ Core/include/Acts/TrackFitting/GlobalChiSquareFitter.hpp
@@ -1099,11 +1099,11 @@ class Gx2Fitter {
     std::cout << "F_P_E_GX2F track.nMeasurements(): " << track.nMeasurements()
               << std::endl;
 
-    if ((track.nMeasurements() < 2) || (track.nMeasurements() > 100)) {
+//    if (((track.nMeasurements() < 2) || (track.nMeasurements() > 100)) && (nUpdate > 0)) {
       track.nMeasurements() = 5;
-      std::cout << "F_P_E_GX2F_CHANGE track.nMeasurements() " << track.nMeasurements()
-                << std::endl;
-    }
+//      std::cout << "F_P_E_GX2F_CHANGE track.nMeasurements() " << track.nMeasurements()
+//                << std::endl;
+//    }
 
     // Return the converted Track
     return track;
