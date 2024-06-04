f2f2410492e6aa6154a40741a5ab023526ec8611
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-06-04 08:13:54 +0200

    track fpes again [skip ci]

--- CI/physmon/workflows/physmon_truth_tracking_gx2f.py
+++ CI/physmon/workflows/physmon_truth_tracking_gx2f.py
@@ -15,7 +15,7 @@ with tempfile.TemporaryDirectory() as temp:
         events=10000,
         numThreads=-1,
         logLevel=acts.logging.INFO,
-        trackFpes=False
+        trackFpes=True,
     )
 
     tp = Path(temp)
