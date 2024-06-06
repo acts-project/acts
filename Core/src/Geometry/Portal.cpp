ba11b510f74bd32ad5fd151b5309815dbef28d86
Paul Gessinger <hello@paulgessinger.com>
2024-06-06 18:07:09 +0200

    checkpoint w/o double-dispatch

--- Core/src/Geometry/Portal.cpp
+++ Core/src/Geometry/Portal.cpp
@@ -11,6 +11,7 @@
 #include "Acts/Utilities/detail/Axis.hpp"
 #include "Acts/Utilities/detail/AxisFwd.hpp"
 
+#include <stdexcept>
 #include <type_traits>
 
 namespace Acts {
@@ -39,6 +40,18 @@ const Volume* Portal::resolveVolume(const GeometryContext& gctx,
   }
 }
 
+std::ostream& operator<<(std::ostream& os, PortalDirection direction) {
+  switch (direction) {
+    case PortalDirection::loc0:
+      os << "loc0";
+      break;
+    case PortalDirection::loc1:
+      os << "loc1";
+      break;
+  }
+  return os;
+}
+
 // MARK: - PortalLinkBase
 
 std::ostream& operator<<(std::ostream& os, const PortalLinkBase& link) {
@@ -60,66 +73,203 @@ std::ostream& operator<<(std::ostream& os, const PortalLinkBase& link) {
 //   return nullptr;
 // }
 
-namespace {
-std::unique_ptr<GridPortalLink1> merge(const GridPortalLink1& a,
-                                       const GridPortalLink1& b) {}
+std::unique_ptr<GridPortalLink1> PortalLinkBase::merge1d(
+    const GridPortalLink1& a, const GridPortalLink1& b, const Vector2& offset,
+    const Logger& logger) {
+  ACTS_DEBUG("Merge GridPortalLink1 + GridPortalLink1 with offset: "
+             << offset.transpose());
 
-std::unique_ptr<GridPortalLink2> merge(const GridPortalLink2& a,
-                                       const GridPortalLink2& b) {}
-}  // namespace
+  assert(a.grid().axes().size() == 1);
+  assert(b.grid().axes().size() == 1);
+  ACTS_VERBOSE("Axis counts are good");
+
+  if (a.direction() != b.direction()) {
+    // 1D axes are not aligned, we cannot merge them
+    throw std::logic_error{"Cannot merge 1D grids with different directions"};
+  }
+
+  const auto commonDirection = a.direction();
+  ACTS_VERBOSE("Directions are consistent: " << commonDirection);
+
+  const IAxis& axisA = *a.grid().axes().at(0);
+  const IAxis& axisB = *b.grid().axes().at(0);
+
+  const auto bdtA = axisA.getBoundaryType();
+  const auto bdtB = axisB.getBoundaryType();
+
+  if (bdtA == AxisBoundaryType::Open || bdtB == AxisBoundaryType::Open) {
+    // Rejecting Open axes outright, so we don't have to handle overflow bins
+    // for now
+    ACTS_ERROR("Cannot merge 1D grids with Open axes");
+    throw std::logic_error{"Cannot merge 1D grids with Open axes"};
+  }
+
+  ACTS_VERBOSE("Boundary types are consistent: " << bdtA << " + " << bdtB)
+
+  ACTS_VERBOSE("- grid offset is " << offset.transpose());
+  PortalDirection direction = PortalDirection::loc0;
+  if (offset[0] != 0 && offset[1] != 0) {
+    ACTS_ERROR("Offset is not aligned with either loc0 or loc1");
+    throw std::logic_error{"Cannot merge 1D grids with diagonal offsets"};
+  } else if (offset[0] != 0) {
+    direction = PortalDirection::loc0;
+  } else if (offset[1] != 0) {
+    direction = PortalDirection::loc1;
+  } else {
+    ACTS_ERROR("Offset is zero");
+    throw std::logic_error{"Cannot merge 1D grids with zero offset"};
+  }
+  ACTS_VERBOSE("=> merging along " << direction);
+  ActsScalar localOffset = offset[0] != 0 ? offset[0] : offset[1];
+
+  if (commonDirection == direction) {
+    ACTS_VERBOSE("Merging along the common direction (" << commonDirection
+                                                        << ") was requested");
+    // Merging axes along the single binning direction, so we extend binnings
+    if (bdtA != AxisBoundaryType::Bound || bdtB != AxisBoundaryType::Bound) {
+      // one of the axes is not bound, cannot merge
+      ACTS_ERROR("Axes are not bound, refusing to merge them");
+      throw std::logic_error{
+          "Cannot merge 1D grids with axes != Bound along common direction"};
+    }
+
+    // NOTE: We don't currently know which order the axes are in, really.
+    // We're assuming a relationship between the grids and the offset
+
+    ACTS_VERBOSE("Are they both equidistant and have same bin width?");
+    if (axisA.isEquidistant() == axisB.isEquidistant()) {
+      ActsScalar binWidthA =
+          (axisA.getMax() - axisA.getMin()) / axisA.getNBins();
+      ActsScalar binWidthB =
+          (axisB.getMax() - axisB.getMin()) / axisB.getNBins();
+
+      ActsScalar aMin = axisA.getMin();
+      ActsScalar aMax = axisA.getMax();
+      ActsScalar bMin = axisB.getMin() + localOffset;
+      ActsScalar bMax = axisB.getMax() + localOffset;
+
+      ACTS_VERBOSE("- axis a: [" << aMin << " -> " << aMax << "] with "
+                                 << axisA.getNBins() << " bins of "
+                                 << binWidthA);
+      ACTS_VERBOSE("- axis b: [" << bMin << " -> " << bMax << "] with "
+                                 << axisB.getNBins() << " bins of "
+                                 << binWidthB);
+
+      if (binWidthA == binWidthB) {
+        ACTS_VERBOSE("=> yes!");
+
+        ACTS_VERBOSE("Do their edges line up?");
+
+        if (bMin < aMax) {
+          ACTS_ERROR("=> no! Axes edges overlap, refusing to merge");
+          throw std::logic_error{
+              "Cannot merge 1D grids with non-matching "
+              "axes along common direction"};
+        }
+        ACTS_VERBOSE("=> yes!")
+
+        ActsScalar min = axisA.getMin();
+        // ActsScalar width =
+        //     axisA.getMax() - axisA.getMin() + axisB.getMax() -
+        //     axisB.getMin();
+        ActsScalar max = axisB.getMax() + localOffset;
+
+        ACTS_VERBOSE("New axis will be [" << min << " -> " << max << "]");
+      } else {
+        ACTS_VERBOSE("=> no!");
+      }
+    }
+  } else {
+    ACTS_VERBOSE("Merging across the common direction (" << commonDirection
+                                                         << ") was requested");
+    // Merging axes across the common direction, we don't care about the
+    // boundary type, as long as they're the same
+    throw std::domain_error{"NotImplemented"};
+  }
+
+  return nullptr;
+}
 
 // MARK: - GridPortalLink1
 
-std::unique_ptr<PortalLinkBase> GridPortalLink1::merge(
-    const PortalLinkBase& other) const {
-  std::cout << "Merge GridPortalLink1 + PortalLinkBase" << std::endl;
-  return other.merge(*this);
-}
-std::unique_ptr<PortalLinkBase> GridPortalLink1::merge(
-    const GridPortalLink1& other) const {
-  std::cout << "Merge GridPortalLink1 + GridPortalLink1" << std::endl;
+// std::unique_ptr<PortalLinkBase> GridPortalLink1::merge(
+//     const PortalLinkBase& other, const Vector2& offset,
+//     const Logger& logger) const {
+//   ACTS_DEBUG("Merge GridPortalLink1 + PortalLinkBase > " <<
+//   offset.transpose()); return other.merge(*this, -1 * offset, logger);
+// }
+
+std::unique_ptr<PortalLinkBase> PortalLinkBase::merge(
+    const PortalLinkBase& other, const Vector2& offset,
+    const Logger& logger) const {
+  ACTS_DEBUG("Merge PortalLinkBase + PortalLinkBase with offset: "
+             << offset.transpose());
+
+  if (const auto* a1 = dynamic_cast<const GridPortalLink1*>(this);
+      a1 != nullptr) {
+    if (const auto* b1 = dynamic_cast<const GridPortalLink1*>(&other);
+        b1 != nullptr) {
+      ACTS_DEBUG("Merging GridPortalLink1 + GridPortalLink1");
+      merge1d(*a1, *b1, offset, logger);
+
+    } else if (const auto* b2 = dynamic_cast<const GridPortalLink2*>(&other);
+               b2 != nullptr) {
+      ACTS_DEBUG("Merging GridPortalLink1 + GridPortalLink2");
+
+    } else {
+      ACTS_DEBUG("Merging GridPortalLink1 + unknown");
+    }
+  } else if (const auto* a2 = dynamic_cast<const GridPortalLink2*>(this);
+             a2 != nullptr) {
+    if (const auto* b1 = dynamic_cast<const GridPortalLink1*>(&other);
+        b1 != nullptr) {
+      ACTS_DEBUG("Merging GridPortalLink2 + GridPortalLink1");
+
+    } else if (const auto* b2 = dynamic_cast<const GridPortalLink2*>(&other);
+               b2 != nullptr) {
+      ACTS_DEBUG("Merging GridPortalLink2 + GridPortalLink2");
+
+    } else {
+      ACTS_DEBUG("Merging GridPortalLink2 + unknown");
+    }
+  } else {
+    ACTS_DEBUG("Merging unknown + unknown");
+  }
+
   return nullptr;
 }
 
-std::unique_ptr<PortalLinkBase> GridPortalLink1::merge(
-    const GridPortalLink2& other) const {
-  std::cout << "Merge GridPortalLink1 + GridPortalLink2" << std::endl;
-  return nullptr;
-}
+// std::unique_ptr<PortalLinkBase> GridPortalLink1::merge(
+//     const GridPortalLink2& other, const Vector2& offset,
+//     const Logger& logger) const {
+//   ACTS_DEBUG("Merge GridPortalLink1 + GridPortalLink2 > "
+//              << offset.transpose());
+//   return mergeImpl(*this, other, offset, logger);
+// }
 
 // MARK: - GridPortalLink2
 
-std::unique_ptr<PortalLinkBase> GridPortalLink2::merge(
-    const PortalLinkBase& other) const {
-  std::cout << "Merge GridPortalLink2 + PortalLinkBase" << std::endl;
-  return other.merge(*this);
-}
-
-std::unique_ptr<PortalLinkBase> GridPortalLink2::merge(
-    const GridPortalLink2& other) const {
-  std::cout << "Merge GridPortalLink2 + GridPortalLink2" << std::endl;
-  return nullptr;
-}
-
-std::unique_ptr<PortalLinkBase> GridPortalLink2::merge(
-    const GridPortalLink1& other) const {
-  std::cout << "Merge GridPortalLink2 + GridPortalLink1" << std::endl;
-  return nullptr;
-}
-
-// template <typename axis_0_t, typename axis_1_t>
-// class GridPortalLink2 : public GridPortalLink {
-//   void toStream(std::ostream& os) const override {
-//     os << "GridPortalLink2<" << axis_0_t::type << ", " << axis_1_t::type <<
-//     ">";
-//   }
-// };
-
-// std::unique_ptr<GridPortalLink> GridPortalLink::make(ActsScalar min,
-//                                                      ActsScalar max,
-//                                                      std::size_t bins) {
-//   return std::make_unique<GridPortalLinkNT<detail::Axis<
-//       detail::AxisType::Equidistant, detail::AxisBoundaryType::Closed>>>();
+// std::unique_ptr<PortalLinkBase> GridPortalLink2::merge(
+//     const PortalLinkBase& other, const Vector2& offset,
+//     const Logger& logger) const {
+//   ACTS_DEBUG("Merge GridPortalLink2 + PortalLinkBase > " <<
+//   offset.transpose()); return other.merge(*this, -1 * offset, logger);
+// }
+//
+// std::unique_ptr<PortalLinkBase> GridPortalLink2::merge(
+//     const GridPortalLink2& other, const Vector2& offset,
+//     const Logger& logger) const {
+//   ACTS_DEBUG("Merge GridPortalLink2 + GridPortalLink2 > "
+//              << offset.transpose());
+//   return nullptr;
+// }
+//
+// std::unique_ptr<PortalLinkBase> GridPortalLink2::merge(
+//     const GridPortalLink1& other, const Vector2& offset,
+//     const Logger& logger) const {
+//   ACTS_DEBUG("Merge GridPortalLink2 + GridPortalLink1 > "
+//              << offset.transpose());
+//   return mergeImpl(other, *this, offset, logger);
 // }
 
 }  // namespace Acts
---
+++ Tests/UnitTests/Core/Geometry/PortalTests.cpp
@@ -0,0 +1,79 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_suite.hpp>
+
+#include "Acts/Definitions/Units.hpp"
+#include "Acts/Geometry/Portal.hpp"
+#include "Acts/Utilities/detail/AxisFwd.hpp"
+
+#include <iostream>
+
+using namespace Acts::UnitLiterals;
+
+namespace Acts::Test {
+
+auto logger = Acts::getDefaultLogger("UnitTests", Acts::Logging::VERBOSE);
+
+struct Fixture {
+  Logging::Level m_level;
+  Fixture() {
+    m_level = Acts::Logging::getFailureThreshold();
+    Acts::Logging::setFailureThreshold(Acts::Logging::FATAL);
+  }
+
+  ~Fixture() { Acts::Logging::setFailureThreshold(m_level); }
+};
+
+BOOST_FIXTURE_TEST_SUITE(Geometry, Fixture)
+
+BOOST_AUTO_TEST_SUITE(GridMerging)
+
+BOOST_AUTO_TEST_CASE(Merging1d) {
+  std::unique_ptr<PortalLinkBase> grid1d1 =
+      GridPortalLink::make(Axis{AxisBound{}, 0, 5, 5});
+  std::unique_ptr<PortalLinkBase> grid1d2 =
+      GridPortalLink::make(Axis{AxisBound{}, -1, 5, 6});
+
+  // @TODO Equidistance different bin widths to variable
+  // @TODO Diagonal offset gives error
+  // @TODO Zero offset
+
+  {
+    auto mergedPtr = grid1d1->merge(*grid1d2, Vector2{6, 0}, *logger);
+    auto* merged = dynamic_cast<GridPortalLink*>(mergedPtr.get());
+    // BOOST_REQUIRE_NE(merged, nullptr);
+    // merged->grid().axes();
+  }
+
+  {
+    auto mergedPtr = grid1d2->merge(*grid1d1, Vector2{6, 0}, *logger);
+    auto* merged = dynamic_cast<GridPortalLink*>(mergedPtr.get());
+    // BOOST_REQUIRE_NE(merged, nullptr);
+    // merged->grid().axes();
+  }
+
+  // @TODO Check merge loc0 (closed + bound)
+  // @TODO Check merge loc1 (bound only)
+  // @TODO Check non-bound for same direction (error)
+  // @TODO: Check inconsistent directions (error)
+
+  // std::unique_ptr<PortalLinkBase> grid2d1 = GridPortalLink::make(
+  //     Axis{AxisBound{}, 0, 5, 5}, Axis{AxisBound{}, 0, 5, 5});
+  //
+  // grid1d1->merge(*grid2d1);
+  // grid2d1->merge(*grid1d1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace Acts::Test
