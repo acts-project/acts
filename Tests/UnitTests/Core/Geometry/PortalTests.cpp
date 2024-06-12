7c5cf1f1877f2c899e5090d8eb8e01d6809fbab8
Paul Gessinger <hello@paulgessinger.com>
2024-06-12 14:04:50 +0200

    portal testing

--- Tests/UnitTests/Core/Geometry/PortalTests.cpp
+++ Tests/UnitTests/Core/Geometry/PortalTests.cpp
@@ -14,9 +14,11 @@
 #include "Acts/Geometry/Portal.hpp"
 #include "Acts/Surfaces/CylinderBounds.hpp"
 #include "Acts/Surfaces/CylinderSurface.hpp"
+#include "Acts/Surfaces/LineSurface.hpp"
 #include "Acts/Utilities/AxisFwd.hpp"
 
 #include <iostream>
+#include <stdexcept>
 
 using namespace Acts::UnitLiterals;
 
@@ -42,18 +44,61 @@ BOOST_AUTO_TEST_CASE(Merging1dCylinder) {
   auto cyl = Surface::makeShared<CylinderSurface>(Transform3::Identity(), 30_mm,
                                                   100_mm);
 
-  std::unique_ptr<GridPortalLink> grid1dCyl = GridPortalLink::make(
-      *cyl, GridPortalLink::Direction::loc1, Axis{AxisBound, 0, 5, 5});
+  BOOST_TEST_CONTEXT("z Binning") {
+    BOOST_CHECK_THROW(
+        GridPortalLink::make(*cyl, GridPortalLink::Direction::loc1,
+                             Axis{AxisBound, 0, 5, 5}),
+        std::invalid_argument);
 
-  std::cout << *grid1dCyl << std::endl;
+    std::unique_ptr<GridPortalLink> grid1dCyl =
+        GridPortalLink::make(*cyl, GridPortalLink::Direction::loc1,
+                             Axis{AxisBound, -100_mm, 100_mm, 5});
+
+    // Another cylinder, shifted in z
+    auto cyl2 = Surface::makeShared<CylinderSurface>(
+
+        Transform3{Translation3{Vector3::UnitZ() * 200_mm}}, 30_mm, 100_mm);
+
+    std::unique_ptr<GridPortalLink> grid1dCyl2 =
+        GridPortalLink::make(*cyl, GridPortalLink::Direction::loc1,
+                             Axis{AxisBound, -100_mm, 100_mm, 5});
+  }
+
+  BOOST_TEST_CONTEXT("rPhi Binning") {
+    BOOST_CHECK_THROW(
+        GridPortalLink::make(*cyl, GridPortalLink::Direction::loc0,
+                             Axis{AxisBound, 0, 5, 5}),
+        std::invalid_argument);
+
+    auto grid1dCyl =
+        GridPortalLink::make(*cyl, GridPortalLink::Direction::loc0,
+                             Axis{AxisBound, -M_PI * 30_mm, M_PI * 30_mm, 5});
+  }
 }
 
 BOOST_AUTO_TEST_CASE(Merging2dCylinder) {
   auto cyl = Surface::makeShared<CylinderSurface>(Transform3::Identity(), 30_mm,
                                                   100_mm);
 
-  std::unique_ptr<GridPortalLink> grid2dCyl = GridPortalLink::make(
-      *cyl, Axis{AxisBound, 0, 5, 5}, Axis{AxisBound, 0, 5, 5});
+  // z bad, rphi bad
+  BOOST_CHECK_THROW(GridPortalLink::make(*cyl, Axis{AxisBound, 1, 2, 5},
+                                         Axis{AxisBound, 3_mm, 4_mm, 5}),
+                    std::invalid_argument);
+
+  // z good, rphi bad
+  BOOST_CHECK_THROW(
+      GridPortalLink::make(*cyl, Axis{AxisBound, 0, 5, 5},
+                           Axis{AxisBound, -M_PI * 30_mm, M_PI * 30_mm, 5}),
+      std::invalid_argument);
+
+  // z bad, rphi good
+  BOOST_CHECK_THROW(GridPortalLink::make(*cyl, Axis{AxisBound, 1, 2, 5},
+                                         Axis{AxisBound, -100_mm, 100_mm, 5}),
+                    std::invalid_argument);
+
+  auto grid2dCyl = GridPortalLink::make(
+      *cyl, Axis{AxisBound, -M_PI * 30_mm, M_PI * 30_mm, 5},
+      Axis{AxisBound, -100_mm, 100_mm, 5});
 
   std::cout << *grid2dCyl << std::endl;
 }
