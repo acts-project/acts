efad3f503077d44c39cb5960abbf71732839258b
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-06-18 00:50:12 +0200

    add other types

--- CI/check_size_t.py
+++ CI/check_size_t.py
@@ -7,7 +7,20 @@ from fnmatch import fnmatch
 import re
 import sys
 
-ex = re.compile(r"(\b(?<!std::)size_t)\b")
+type_list = [
+    "size_t",
+    "ptrdiff_t",
+    "nullptr_t",
+    "int8_t",
+    "int16_t",
+    "int32_t",
+    "int64_t",
+    "uint8_t",
+    "uint16_t",
+    "uint32_t",
+    "uint64_t",
+    "max_align_t",
+]
 
 github = "GITHUB_ACTIONS" in os.environ
 
@@ -15,15 +28,14 @@ github = "GITHUB_ACTIONS" in os.environ
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("input")
-    p.add_argument(
-        "--fix", action="store_true", help="Attempt to fix any license issues found."
-    )
+    p.add_argument("--fix", action="store_true", help="Attempt to fix C-style types.")
     p.add_argument("--exclude", "-e", action="append", default=[])
 
     args = p.parse_args()
 
+    exit_code = 0
+
     # walk over all files
-    exit = 0
     for root, _, files in os.walk("."):
         root = Path(root)
         for filename in files:
@@ -44,30 +56,34 @@ def main():
             if any([fnmatch(str(filepath), e) for e in args.exclude]):
                 continue
 
-            changed_lines = handle_file(filepath, fix=args.fix)
-            if len(changed_lines) > 0:
-                exit = 1
-                print()
-                print(filepath)
-                for i, oline in changed_lines:
-                    print(f"{i}: {oline}")
+            for c_type in type_list:
+                changed_lines = handle_file(file=filepath, fix=args.fix, c_type=c_type)
+                if len(changed_lines) > 0:
+                    exit_code = 1
+                    print()
+                    print(filepath)
+                    for i, oline in changed_lines:
+                        print(f"{i}: {oline}")
 
-                    if github:
-                        print(
-                            f"::error file={filepath},line={i+1},title=Do not use C-style size_t::Replace size_t with std::size_t"
-                        )
+                        if github:
+                            print(
+                                f"::error file={filepath},line={i+1},title=Do not use C-style {c_type}::Replace {c_type} with std::{c_type}"
+                            )
 
-    return exit
+    return exit_code
 
 
-def handle_file(file: Path, fix: bool) -> list[tuple[int, str]]:
+def handle_file(file: Path, fix: bool, c_type: str) -> list[tuple[int, str]]:
+
+    ex = re.compile(rf"\b(?<!std::){c_type}\b")
+
     content = file.read_text()
     lines = content.splitlines()
 
     changed_lines = []
 
     for i, oline in enumerate(lines):
-        line, n_subs = ex.subn(r"std::size_t", oline)
+        line, n_subs = ex.subn(rf"std::{c_type}", oline)
         lines[i] = line
         if n_subs > 0:
             changed_lines.append((i, oline))
