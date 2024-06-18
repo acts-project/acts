33ab10f68515b1966cdc37e95cca7ed7c64fdf8d
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-06-18 14:44:46 +0200

    add comments

--- docs/conf.py
+++ docs/conf.py
@@ -60,8 +60,8 @@ myst_dmath_allow_labels = True
 
 linkcheck_retries = 5
 linkcheck_ignore = [
-    r"https://cbea.ms/.*",
-    r"https://eigen.tuxfamily.org.*",
+    r"https://cbea.ms/.*",  # no certificate on 2024-06-18
+    r"https://eigen.tuxfamily.org.*",  # frequently down
 ]
 
 # -- Options for HTML output --------------------------------------------------
