29574e1d261ea179f1d8015a34cb969e414da7ea
Paul Gessinger <hello@paulgessinger.com>
2024-06-20 16:54:05 +0200

    slight improvement to milestone getting

--- CI/get_next_milestone.py
+++ CI/get_next_milestone.py
@@ -3,6 +3,7 @@
 import os
 import urllib.request
 import json
+import sys
 
 GITHUB_TOKEN = os.environ["GH_TOKEN"]
 GITHUB_REPO = os.environ["GH_REPO"]
@@ -16,7 +17,15 @@ with urllib.request.urlopen(
     milestones = json.loads(response.read())
 
 
+sys.stderr.write(f"Found {len(milestones)} milestones\n")
+for m in milestones:
+    sys.stderr.write(f"- {m['title']}\n")
+
+
 titles = [m["title"] for m in milestones if m["title"] != "next"]
 titles.sort()
+
+if len(titles) == 0:
+    raise RuntimeError("No eligible milestone found")
 
 print(titles[0])
