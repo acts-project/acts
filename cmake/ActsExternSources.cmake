24b822241ae40ff3767dffdae24afc2606be983b
Frederik <frederik.barba@hotmail.com>
2024-06-21 18:41:41 +0200

    new traccc commit

--- cmake/ActsExternSources.cmake
+++ cmake/ActsExternSources.cmake
@@ -19,7 +19,7 @@ set( ACTS_DETRAY_SOURCE
 mark_as_advanced( ACTS_DETRAY_SOURCE )
 
 set( ACTS_TRACCC_SOURCE
-   "GIT_REPOSITORY;https://github.com/fredevb/traccc;GIT_TAG;28ed9e3c1e0b05aad391bed2b32275c7d6a3cd4c" CACHE STRING "Source to take TRACCC from")
+   "GIT_REPOSITORY;https://github.com/fredevb/traccc;GIT_TAG;5f39fc6a85fa1796ea0dbd41ae8c0d45247021ee" CACHE STRING "Source to take TRACCC from")
 mark_as_advanced( ACTS_TRACCC_SOURCE )
 
 set( ACTS_DFELIBS_SOURCE
