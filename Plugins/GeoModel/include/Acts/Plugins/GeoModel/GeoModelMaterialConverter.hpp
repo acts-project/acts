47547d98e60637b1398dbd532f0a7b386188af8c
Matthew Leary Harris <maharris@atlmuonsw1.dyndns.cern.ch>
2024-07-04 11:19:50 +0200

    bump GeoModel to 6.0.0 and add GeoMaterial converter

---
+++ Plugins/GeoModel/include/Acts/Plugins/GeoModel/GeoModelMaterialConverter.hpp
@@ -0,0 +1,33 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#pragma once
+
+#include "Acts/Material/Material.hpp"
+
+class GeoMaterial;
+
+namespace Acts{
+
+namespace GeoModel {
+
+class GeoMaterialConverter {
+    public:
+    // Remove default constructor
+    GeoMaterialConverter() = delete;
+
+    ~GeoMaterialConverter() = default;
+
+    /// @brief Convert GeoMaterial to Acts::Material
+    ///
+    /// @param gm The GeoMaterial to be converted
+    /// @return the Acts::Material
+    static Material convert(const GeoMaterial* gm, bool molarDensity = true);
+
+}; // class GeoMaterialConverter
+} // namespace GeoModel
+} // namespace Acts
