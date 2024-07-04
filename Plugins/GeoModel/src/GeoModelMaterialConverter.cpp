e6c8ac0c0973be7e63b1d4a10760ca36e91e99ff
Matt Harris <maharris@matts-macbook-pro-7.home>
2024-07-04 11:34:17 +0200

    format fix

--- Plugins/GeoModel/src/GeoModelMaterialConverter.cpp
+++ Plugins/GeoModel/src/GeoModelMaterialConverter.cpp
@@ -13,26 +13,27 @@
 #include "GeoModelKernel/GeoMaterial.h"
 #include "GeoModelKernel/Units.h"
 namespace {
-    constexpr double s_densityCnvFactor = 1./ GeoModelKernelUnits::gram;
+constexpr double s_densityCnvFactor = 1. / GeoModelKernelUnits::gram;
 }
-Acts::Material Acts::GeoModel::GeoMaterialConverter::convert(const GeoMaterial* gm, bool useMolarDensity) {
-    double x0 = gm->getRadLength();
-    double l0 = gm->getIntLength();
-    double density = gm->getDensity() * s_densityCnvFactor;
-    double A = 0.;
-    double Z = 0.;
-    //Get number elements
-    int numberOfElements = gm->getNumElements();
-    //Loop
-    for(int iEl = 0; iEl < numberOfElements; ++iEl) {
-        const GeoElement* geoEl = gm->getElement(iEl);
-        double fraction = gm->getFraction(iEl);
-        A += fraction * (geoEl->getA() / GeoModelKernelUnits::gram);
-        Z += fraction * geoEl->getZ();
-    }
-    if(molarDensity) {
-        return Material::fromMolarDensity(x0, l0, A, Z, density);
-    } else{
-        return Material::fromMassDensity(x0, l0, A, Z, density);
-    }
+Acts::Material Acts::GeoModel::GeoMaterialConverter::convert(
+    const GeoMaterial* gm, bool useMolarDensity) {
+  double x0 = gm->getRadLength();
+  double l0 = gm->getIntLength();
+  double density = gm->getDensity() * s_densityCnvFactor;
+  double A = 0.;
+  double Z = 0.;
+  // Get number elements
+  int numberOfElements = gm->getNumElements();
+  // Loop
+  for (int iEl = 0; iEl < numberOfElements; ++iEl) {
+    const GeoElement* geoEl = gm->getElement(iEl);
+    double fraction = gm->getFraction(iEl);
+    A += fraction * (geoEl->getA() / GeoModelKernelUnits::gram);
+    Z += fraction * geoEl->getZ();
+  }
+  if (molarDensity) {
+    return Material::fromMolarDensity(x0, l0, A, Z, density);
+  } else {
+    return Material::fromMassDensity(x0, l0, A, Z, density);
+  }
 }
