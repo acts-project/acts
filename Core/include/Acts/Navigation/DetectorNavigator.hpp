3798cf6cbcb0eb086deb47cbc77f4946c5cc23bc
Andreas Salzburger <asalzburger@gmail.com>
2024-07-12 11:44:59 +0200

    feat: gen2 navigation debugged (#3365)
    This PR establishes the Gen-2 navigation for the ODD and ITk detector.
    
    I also deletes left-over dead code.
    
    x-y view of central ITk:
    
    ![Screenshot 2024-07-11 at 15 21 49](https://github.com/acts-project/acts/assets/26623879/cfe012d7-bdad-4521-8308-d1395c532d0e)
    
    R-z view of ITK:
    
    ![Screenshot 2024-07-11 at 15 22 12](https://github.com/acts-project/acts/assets/26623879/49df8c0c-42b0-44ea-a370-c2fcf949bc8a)
    
    
    
    **Attention**:
    there is one change to the Propagagtor so physmom needs to be checked carefully

--- Core/include/Acts/Navigation/DetectorNavigator.hpp
+++ Core/include/Acts/Navigation/DetectorNavigator.hpp
@@ -190,27 +190,8 @@ class DetectorNavigator {
     if (nState.currentSurface != nullptr) {
       ACTS_VERBOSE(volInfo(state)
                    << posInfo(state, stepper) << "stepping through surface");
-    } else if (nState.currentPortal != nullptr) {
-      ACTS_VERBOSE(volInfo(state)
-                   << posInfo(state, stepper) << "stepping through portal");
-
-      nState.surfaceCandidates.clear();
-      nState.surfaceCandidateIndex = 0;
-
-      nState.currentPortal->updateDetectorVolume(state.geoContext, nState);
-
-      // If no Volume is found, we are at the end of the world
-      if (nState.currentVolume == nullptr) {
-        ACTS_VERBOSE(volInfo(state) << posInfo(state, stepper)
-                                    << "no volume after Portal update");
-        nState.navigationBreak = true;
-        return;
-      }
-
-      // Switched to a new volume
-      // Update candidate surfaces
-      updateCandidateSurfaces(state, stepper);
     }
+
     for (; nState.surfaceCandidateIndex != nState.surfaceCandidates.size();
          ++nState.surfaceCandidateIndex) {
       // Screen output how much is left to try
@@ -313,14 +294,33 @@ class DetectorNavigator {
                    << posInfo(state, stepper) << "landed on surface");
 
       if (isPortal) {
-        ACTS_VERBOSE(volInfo(state) << posInfo(state, stepper)
-                                    << "this is a portal, storing it.");
-
+        ACTS_VERBOSE(volInfo(state)
+                     << posInfo(state, stepper)
+                     << "this is a portal, updating to new volume.");
         nState.currentPortal = nextPortal;
+
+        nState.surfaceCandidates.clear();
+        nState.surfaceCandidateIndex = 0;
+
+        nState.currentPortal->updateDetectorVolume(state.geoContext, nState);
+
+        // If no Volume is found, we are at the end of the world
+        if (nState.currentVolume == nullptr) {
+          ACTS_VERBOSE(volInfo(state)
+                       << posInfo(state, stepper)
+                       << "no volume after Portal update, end of world.");
+          nState.navigationBreak = true;
+          return;
+        }
+
+        // Switched to a new volume
+        // Update candidate surfaces
+        updateCandidateSurfaces(state, stepper);
 
         ACTS_VERBOSE(volInfo(state)
                      << posInfo(state, stepper) << "current portal set to "
                      << nState.currentPortal->surface().geometryId());
+
       } else {
         ACTS_VERBOSE(volInfo(state) << posInfo(state, stepper)
                                     << "this is a surface, storing it.");
@@ -404,8 +404,6 @@ class DetectorNavigator {
 
     // Here we get the candidate surfaces
     nState.currentVolume->updateNavigationState(state.geoContext, nState);
-
-    ACTS_VERBOSE("SURFACE CANDIDATES: " << nState.surfaceCandidates.size());
 
     // Sort properly the surface candidates
     auto& nCandidates = nState.surfaceCandidates;
