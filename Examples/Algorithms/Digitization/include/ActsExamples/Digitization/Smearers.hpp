b4c888d828d70cb003f24efcca596cd18569c244
Andreas Stefl <stefl.andreas@gmail.com>
2024-07-16 09:39:28 +0200

    fix: Fix and refactor digitization parametrization in Examples (#3386)
    - `Exact` was not serialized correctly
    - Add sigma to `Exact` as `0` breaks reconstruction
    - Configure `GaussTrunc` and `GaussClipped` range in terms of `sigma`

--- Examples/Algorithms/Digitization/include/ActsExamples/Digitization/Smearers.hpp
+++ Examples/Algorithms/Digitization/include/ActsExamples/Digitization/Smearers.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2020 CERN for the benefit of the Acts project
+// Copyright (C) 2020-2024 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -26,6 +26,11 @@ namespace ActsExamples::Digitization {
 /// Exact smearing of a single parameter.
 ///
 struct Exact {
+  double sigma;
+
+  /// Construct with a @param sigma standard deviation
+  explicit Exact(double sigma_) : sigma{sigma_} {}
+
   /// Call operator for the SmearFunction caller interface.
   ///
   /// @param value parameter to be smeared
@@ -35,7 +40,7 @@ struct Exact {
   /// the value and a stddev of 0.0
   Acts::Result<std::pair<double, double>> operator()(
       double value, RandomEngine& /*rnd*/) const {
-    return std::pair{value, 0.0};
+    return std::pair{value, sigma};
   }
 };
 
@@ -47,7 +52,7 @@ struct Gauss {
   double sigma;
 
   /// Construct with a @param sigma standard deviation
-  Gauss(double sigma_) : sigma{sigma_} {}
+  explicit Gauss(double sigma_) : sigma{sigma_} {}
 
   /// Call operator for the SmearFunction caller interface.
   ///
@@ -58,7 +63,7 @@ struct Gauss {
   Acts::Result<std::pair<double, double>> operator()(double value,
                                                      RandomEngine& rnd) const {
     std::normal_distribution<> dist{0, sigma};
-    return std::pair{value + dist(rnd), dist.stddev()};
+    return std::pair{value + dist(rnd), sigma};
   }
 };
 
@@ -83,12 +88,13 @@ struct GaussTrunc {
   /// @return a Result that is ok() when inside range, other DigitizationError
   Acts::Result<std::pair<double, double>> operator()(double value,
                                                      RandomEngine& rnd) const {
-    std::normal_distribution<> dist{0., sigma};
-    double svalue = value + dist(rnd);
-    if (svalue >= range.first && svalue <= range.second) {
-      return std::pair{svalue, dist.stddev()};
+    std::normal_distribution<> dist{0., 1};
+    double x = dist(rnd);
+    if (x < range.first || x > range.second) {
+      return ActsFatras::DigitizationError::SmearingOutOfRange;
     }
-    return ActsFatras::DigitizationError::SmearingOutOfRange;
+    double svalue = value + sigma * x;
+    return std::pair{svalue, sigma};
   }
 };
 
@@ -118,11 +124,12 @@ struct GaussClipped {
   /// @return a Result that is ok() when inside range, other DigitizationError
   Acts::Result<std::pair<double, double>> operator()(double value,
                                                      RandomEngine& rnd) const {
-    std::normal_distribution<> dist{0., sigma};
+    std::normal_distribution<> dist{0., 1};
     for (std::size_t attempt = 0; attempt < maxAttemps; ++attempt) {
-      double svalue = value + dist(rnd);
-      if (svalue >= range.first && svalue <= range.second) {
-        return std::pair{svalue, dist.stddev()};
+      double x = dist(rnd);
+      if (x >= range.first && x <= range.second) {
+        double svalue = value + sigma * x;
+        return std::pair{svalue, sigma};
       }
     }
     return ActsFatras::DigitizationError::SmearingError;
@@ -145,7 +152,7 @@ struct Uniform {
   /// Constructor with a binning data in order to get the bin borders.
   ///
   /// @param bu the binning data
-  Uniform(Acts::BinningData&& bd) : binningData(bd) {}
+  explicit Uniform(const Acts::BinningData& bd) : binningData(bd) {}
 
   /// Call operator for the SmearFunction caller interface.
   ///
@@ -183,7 +190,7 @@ struct Digital {
   /// Constructor with a bin utility in order to get the bin borders.
   ///
   /// @param bu the bin utility within hich the parameter is allowed
-  Digital(Acts::BinningData&& bd) : binningData(bd) {}
+  explicit Digital(const Acts::BinningData& bd) : binningData(bd) {}
 
   /// Call operator for the SmearFunction caller interface.
   ///
