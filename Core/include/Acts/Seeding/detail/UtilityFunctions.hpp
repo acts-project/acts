f1592a355d107e772645721a18d09ddad9c44166
Carlo Varni <75478407+CarloVarni@users.noreply.github.com>
2024-07-16 12:03:38 +0200

    refactor!: use c++20 concept to define insert function for different types of seed collections (#3367)
    Taking from @<!---->CouthuresJeremy 's PR I'm here proposing a possible alternative that would not even be a breaking change (unless I'm missing something). This will use c++20 concepts to define different versions of an `insert` function that will add a seed to the output collection.
    
    This will work with 'back_insert_operator' (as we currently do) as well as collection that support `push_back` and/or `insert`
    
    blocked by
    
    - https://github.com/acts-project/acts/pull/3345
    - https://github.com/acts-project/acts/pull/3377
    
    edit: Now this is breaking since we do not support back_insert_operator option anymore

---
+++ Core/include/Acts/Seeding/detail/UtilityFunctions.hpp
@@ -0,0 +1,48 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <ranges>
+
+namespace Acts::detail {
+
+// Define some concepts
+template <typename external_t>
+concept isCollectionThatSupportsPushBack =
+    std::ranges::range<external_t> && requires {
+  typename external_t::value_type;
+} && requires(external_t coll, typename external_t::value_type val) {
+  coll.push_back(val);
+};
+
+template <typename external_t>
+concept isCollectionThatSupportsInsert =
+    std::ranges::range<external_t> && requires {
+  typename external_t::value_type;
+} && requires(external_t coll, typename external_t::value_type val) {
+  coll.insert(std::ranges::end(coll), val);
+};
+
+// Define some functions
+template <typename value_t>
+void pushBackOrInsertAtEnd(
+    Acts::detail::isCollectionThatSupportsPushBack auto& storage,
+    value_t&& value) {
+  storage.push_back(std::forward<value_t>(value));
+}
+
+template <std::ranges::range storage_t, typename value_t>
+requires(!Acts::detail::isCollectionThatSupportsPushBack<storage_t> &&
+         Acts::detail::isCollectionThatSupportsInsert<
+             storage_t>) void pushBackOrInsertAtEnd(storage_t& storage,
+                                                    value_t&& value) {
+  storage.insert(std::ranges::end(storage), std::forward<value_t>(value));
+}
+
+}  // namespace Acts::detail
