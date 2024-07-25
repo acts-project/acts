ed5379705803243732383599d66c1f0ef3366711
Andreas Stefl <stefl.andreas@gmail.com>
2024-07-25 10:48:18 +0200

    deprecate header

--- Core/include/Acts/EventData/TrackHelpers.hpp
+++ Core/include/Acts/EventData/TrackHelpers.hpp
@@ -8,4 +8,20 @@
 
 #pragma once
 
+// This header is deprecated and will be removed in the future.
+// Please use the new header instead:
 #include "Acts/Utilities/TrackHelpers.hpp"
+
+namespace Acts {
+
+// Funny header deprecation strategy
+namespace {
+[[deprecated(
+    "This header is deprecated - use "
+    "Acts/Utilities/TrackHelpers.hpp")]] constexpr static int
+    utilities_trackhelpers_hpp_is_deprecated = 0;
+constexpr static int please_dont_use_utilities_trackhelpers_hpp =
+    utilities_trackhelpers_hpp_is_deprecated;
+}  // namespace
+
+}  // namespace Acts
