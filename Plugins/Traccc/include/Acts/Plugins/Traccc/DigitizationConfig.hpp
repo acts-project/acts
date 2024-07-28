a2628101515949dfcf86143fcb3b7a895b9f2428
Frederik <frederik.barba@hotmail.com>
2024-07-28 16:25:33 +0200

    formatting

--- Plugins/Traccc/include/Acts/Plugins/Traccc/DigitizationConfig.hpp
+++ Plugins/Traccc/include/Acts/Plugins/Traccc/DigitizationConfig.hpp
@@ -6,6 +6,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#pragma once
+
 // Acts include(s)
 #include "Acts/Geometry/GeometryHierarchyMap.hpp"
 #include "Acts/Utilities/BinUtility.hpp"
