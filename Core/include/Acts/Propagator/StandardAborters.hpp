b81d8aaaf49ea13851ee5c266a6d94dfa21bdd68
Andreas Stefl <stefl.andreas@gmail.com>
2024-08-01 18:09:23 +0200

    fix docs

--- Core/include/Acts/Propagator/StandardAborters.hpp
+++ Core/include/Acts/Propagator/StandardAborters.hpp
@@ -173,6 +173,7 @@ struct EndOfWorldReached {
   ///
   /// @param [in,out] state The propagation state object
   /// @param [in] navigator The navigator object
+  /// @param logger a logger instance
   template <typename propagator_state_t, typename stepper_t,
             typename navigator_t>
   bool operator()(propagator_state_t& state, const stepper_t& /*stepper*/,
