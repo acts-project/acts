5cb6b7857e4d19ab3506c4a1f7cd50c38c6d7225
Andreas Stefl <stefl.andreas@gmail.com>
2024-08-03 07:52:34 +0200

    format python

--- Examples/Scripts/Python/full_chain_odd.py
+++ Examples/Scripts/Python/full_chain_odd.py
@@ -357,13 +357,21 @@ if args.reco:
             maxPixelHoles=1,
             maxStripHoles=2,
             constrainToVolumes=[
-                2, # beam pipe
-                32, 4, # beam pip gap
-                16, 17, 18, # pixel
-                20, # PST
-                23, 24, 25, # short strip
-                26, 8, # long strip gap
-                28, 29, 30, # long strip
+                2,  # beam pipe
+                32,
+                4,  # beam pip gap
+                16,
+                17,
+                18,  # pixel
+                20,  # PST
+                23,
+                24,
+                25,  # short strip
+                26,
+                8,  # long strip gap
+                28,
+                29,
+                30,  # long strip
             ],
         ),
         outputDirRoot=outputDir if args.output_root else None,
