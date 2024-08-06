d501de8d63d9cd6336d92cd42481fe49242461f2
Paul Gessinger <hello@paulgessinger.com>
2024-08-06 15:44:32 +0200

    add missing doc comment

--- Core/include/Acts/EventData/MeasurementHelpers.hpp
+++ Core/include/Acts/EventData/MeasurementHelpers.hpp
@@ -63,6 +63,7 @@ auto visit_measurement(A&& param, B&& cov, std::size_t dim, L&& lambda) {
 /// @tparam L The generic lambda type to call
 /// @param dim The runtime dimension of the measurement
 /// @param lambda The generic lambda instance to call
+/// @param args Additional arguments passed to @p lambda
 /// @return Returns the lambda return value
 template <typename L, typename... Args>
 auto visit_measurement(std::size_t dim, L&& lambda, Args&&... args) {
