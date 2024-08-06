d5d6875705f1f7f1b83a41d05f16f34df5c7ff42
Paul Gessinger <hello@paulgessinger.com>
2024-08-06 15:35:51 +0200

    refactor: `visit_measurement` can pipe through arguments

--- Core/include/Acts/EventData/MeasurementHelpers.hpp
+++ Core/include/Acts/EventData/MeasurementHelpers.hpp
@@ -64,9 +64,10 @@ auto visit_measurement(A&& param, B&& cov, std::size_t dim, L&& lambda) {
 /// @param dim The runtime dimension of the measurement
 /// @param lambda The generic lambda instance to call
 /// @return Returns the lambda return value
-template <typename L>
-auto visit_measurement(std::size_t dim, L&& lambda) {
-  return template_switch_lambda<1, eBoundSize>(dim, lambda);
+template <typename L, typename... Args>
+auto visit_measurement(std::size_t dim, L&& lambda, Args&&... args) {
+  return template_switch_lambda<1, eBoundSize>(dim, lambda,
+                                               std::forward<Args>(args)...);
 }
 
 }  // namespace Acts
