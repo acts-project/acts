3050e22f7e420dd54866ede1f0e982c95cc6240f
Paul Gessinger <paul.gessinger@cern.ch>
2024-08-09 15:53:12 +0200

    chore: Bump clang-format to v18.1.8 (#3496)
    This PR bumps clang-format in the CI to version 18, and reapplies the formatting to all files.

--- Examples/Algorithms/TrackFinding/src/GbtsSeedingAlgorithm.cpp
+++ Examples/Algorithms/TrackFinding/src/GbtsSeedingAlgorithm.cpp
@@ -106,15 +106,15 @@ ActsExamples::ProcessCode ActsExamples::GbtsSeedingAlgorithm::execute(
   // }
 
   for (auto sp : GbtsSpacePoints) {
-    ACTS_DEBUG("Gbts space points: "
-               << " Gbts_id: " << sp.gbtsID << " z: " << sp.SP->z()
-               << " r: " << sp.SP->r() << " ACTS volume:  "
-               << sp.SP->sourceLinks()
-                      .front()
-                      .get<IndexSourceLink>()
-                      .geometryId()
-                      .volume()
-               << "\n");
+    ACTS_DEBUG("Gbts space points: " << " Gbts_id: " << sp.gbtsID << " z: "
+                                     << sp.SP->z() << " r: " << sp.SP->r()
+                                     << " ACTS volume:  "
+                                     << sp.SP->sourceLinks()
+                                            .front()
+                                            .get<IndexSourceLink>()
+                                            .geometryId()
+                                            .volume()
+                                     << "\n");
   }
 
   // this is now calling on a core algorithm
