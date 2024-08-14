975bc52e5e6513b89217d977e0e710374f06694d
Andreas Stefl <stefl.andreas@gmail.com>
2024-08-14 14:50:35 +0200

    revert sequencer

--- Examples/Framework/src/Framework/Sequencer.cpp
+++ Examples/Framework/src/Framework/Sequencer.cpp
@@ -400,9 +400,9 @@ void storeTiming(const std::vector<std::string>& identifiers,
     const auto time_total_s =
         std::chrono::duration_cast<Seconds>(durations[i]).count();
     file << identifiers[i] << "," << time_total_s << ","
-         << time_total_s / numEvents << "\n";
+         << time_total_s / numEvents;
   }
-  file << "\n";
+  file << std::endl;
 }
 }  // namespace
 
