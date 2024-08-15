e911c999f9f071ec2b86a253333ee977e8098dc5
Carl Jonas Berggren <cberggre@atlmuonsw1.dyndns.cern.ch>
2024-08-15 11:24:28 +0200

    CI fix

--- Tests/UnitTests/Plugins/GeoModel/GeoDetectorVolTest.cpp
+++ Tests/UnitTests/Plugins/GeoModel/GeoDetectorVolTest.cpp
@@ -1,3 +1,11 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include <boost/test/unit_test.hpp>
 #include "Acts/Plugins/GeoModel/GeoModelDetectorObjectFactory.hpp"
 #include "Acts/Surfaces/PlaneSurface.hpp"
@@ -43,7 +51,7 @@ BOOST_AUTO_TEST_CASE(GeoModelDetectorObjectFactory) {
   auto physTube = new GeoFullPhysVol(logTube);
   auto physSurface = new GeoFullPhysVol(logSurface);
 
-  //build hiarchy
+  //build hierarchy
   fphysXY->add(physTube);
   fphysXY->add(physSurface);
 
