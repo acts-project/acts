89f156badb7951fa6051d9798546ec1298ca91dc
Carl Jonas Berggren <cberggre@atlmuonsw1.dyndns.cern.ch>
2024-08-16 11:37:00 +0200

    pre-commit

--- Plugins/GeoModel/src/GeoModelDetectorObjectFactory.cpp
+++ Plugins/GeoModel/src/GeoModelDetectorObjectFactory.cpp
@@ -143,7 +143,7 @@ bool Acts::GeoModelDetectorObjectFactory::convertBox(std::string name) {
   auto convB = std::any_of(
       m_cfg.convertBox.begin(), m_cfg.convertBox.end(),
       [&](const auto &n) { return name.find(n) != std::string::npos; });
-  //return (name.find(m_cfg.convertBox) != std::string::npos);
+  // return (name.find(m_cfg.convertBox) != std::string::npos);
   return convB;
 }
 void Acts::GeoModelDetectorObjectFactory::convertFpv(
