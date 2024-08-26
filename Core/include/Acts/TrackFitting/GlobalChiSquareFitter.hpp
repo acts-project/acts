1b383386bcd62c2ebafe97579057a65e91ff9ac3
Alexander J. Pfleger <70842573+AJPfleger@users.noreply.github.com>
2024-08-26 20:10:24 +0200

    refactor(gx2f): remove outdated navigation abort conditions (#3552)
    Removes some unnecessary navigation limits from the GX2F.
    
    ## `if (result.processedMeasurements >= inputMeasurements->size()) {`
    Not necessary anymore, since we have the same limit already in the beginning of the actor.
    
    ## `if (result.surfaceCount > 900) {`
    This limit was set to abort the navigation, even if not all measurements have been found. The navigation in the GX2F is now stable enough, that we don't need this limit anymore

--- Core/include/Acts/TrackFitting/GlobalChiSquareFitter.hpp
+++ Core/include/Acts/TrackFitting/GlobalChiSquareFitter.hpp
@@ -635,19 +635,6 @@ class Gx2Fitter {
               "    The surface contains no measurement/(material)/hole.");
         }
       }
-      ACTS_VERBOSE("result.processedMeasurements: "
-                   << result.processedMeasurements << "\n"
-                   << "inputMeasurements.size(): "
-                   << inputMeasurements->size());
-      if (result.processedMeasurements >= inputMeasurements->size()) {
-        ACTS_INFO("Actor: finish: all measurements found.");
-        result.finished = true;
-      }
-
-      if (result.surfaceCount > 900) {
-        ACTS_INFO("Actor: finish due to limit. Result might be garbage.");
-        result.finished = true;
-      }
     }
   };
 
