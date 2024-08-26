051aefe04cc3f7910a1ced25eb6f1a42894b65cf
Paul Gessinger <hello@paulgessinger.com>
2024-08-26 09:13:36 +0200

    debug which python physmon runs

--- CI/physmon/phys_perf_mon.sh
+++ CI/physmon/phys_perf_mon.sh
@@ -15,7 +15,7 @@ function run() {
 
 export run
 
-
+run which python3
 shopt -s extglob
 
 
@@ -222,6 +222,7 @@ function trackfinding() {
         $path/performance_ckf.html \
         $path/performance_ckf_plots \
         --config $default_config
+
 
     run Examples/Scripts/generic_plotter.py \
         $outdir/data/$path/tracksummary_ckf.root \
