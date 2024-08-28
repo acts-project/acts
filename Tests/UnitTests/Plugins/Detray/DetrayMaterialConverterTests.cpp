2366a097c43c8d4be4469ab4533c9f65e72d6c1d
Andreas Salzburger <asalzburger@gmail.com>
2024-08-28 11:50:29 +0200

    import first UnitTest code for Detray plugin

---
+++ Tests/UnitTests/Plugins/Detray/DetrayMaterialConverterTests.cpp
@@ -0,0 +1,44 @@
+// This file is part of the Acts project.
+//
+// Copyright (C) 2024 CERN for the benefit of the Acts project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Plugins/Detray/DetrayConversionUtils.hpp"
+
+BOOST_AUTO_TEST_SUITE(DetrayConversion)
+
+// These tests check the conversion to the payload objects, the full test
+
+BOOST_AUTO_TEST_CASE(DetrayHomogeneousMaterialConversion) {
+}
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionX) {
+}
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionY) {
+}
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionXY) {
+}
+
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionR) {
+}
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionRPhi) {
+}
+
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionZ) {
+}
+
+
+BOOST_AUTO_TEST_CASE(DetrayBinnedMaterialConversionZPhi) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
