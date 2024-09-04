3f7169d6e16718beb03c40986f887ad6ca4c65b0
Benjamin Huth <benjamin.huth@cern.ch>
2024-09-04 09:40:15 +0200

    fixes to ITk Module Splitting

--- Examples/Detectors/ITkModuleSplitting/include/ActsExamples/ITkModuleSplitting/ITkModuleSplitting.hpp
+++ Examples/Detectors/ITkModuleSplitting/include/ActsExamples/ITkModuleSplitting/ITkModuleSplitting.hpp
@@ -104,12 +104,11 @@ inline std::vector<std::shared_ptr<detector_element_t>> splitDiscModule(
   auto nSegments = splitRanges.size();
 
   // Output container for the submodules
-  std::vector<std::shared_ptr<const Acts::TGeoDetectorElement>> detElements =
+  std::vector<std::shared_ptr<const detector_element_t>> detElements =
       {};
   detElements.reserve(nSegments);
 
   // Get the geometric information
-  double thickness = detElement->thickness();
   const Acts::Transform3& transform = surface.transform(gctx);
   const std::vector<double> boundsValues = bounds.values();
   std::array<double, Acts::AnnulusBounds::eSize> values{};
@@ -117,8 +116,8 @@ inline std::vector<std::shared_ptr<detector_element_t>> splitDiscModule(
   std::copy_n(boundsValues.begin(), Acts::AnnulusBounds::eSize, values.begin());
 
   for (std::size_t i = 0; i < nSegments; i++) {
-    if (boundsValues[Acts::AnnulusBounds::eMinR] < splitRanges[i].first ||
-        boundsValues[Acts::AnnulusBounds::eMaxR] > splitRanges[i].second) {
+    if (boundsValues[Acts::AnnulusBounds::eMinR] > splitRanges[i].first ||
+        boundsValues[Acts::AnnulusBounds::eMaxR] < splitRanges[i].second) {
       ACTS_WARNING(
           "Radius pattern not within the original bounds, node will not be "
           "split!");
