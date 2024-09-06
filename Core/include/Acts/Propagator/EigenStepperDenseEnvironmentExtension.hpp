6d682dec890ff29ffd0f88338c1f49cddd1d7aef
Andreas Stefl <stefl.andreas@gmail.com>
2024-09-06 09:43:17 +0200

    fix docs

--- Core/include/Acts/Propagator/EigenStepperDenseEnvironmentExtension.hpp
+++ Core/include/Acts/Propagator/EigenStepperDenseEnvironmentExtension.hpp
@@ -143,6 +143,7 @@ struct EigenStepperDenseEnvironmentExtension
   ///
   /// @param [in] state State of the propagator
   /// @param [in] stepper Stepper of the propagator
+  /// @param [in] navigator Navigator of the propagator
   /// @param [in] h Step size
   ///
   /// @return Boolean flag if the calculation is valid
