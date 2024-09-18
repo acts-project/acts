2dc73be03cf8b686f95c6358b6074d5ef4c63283
Paul Gessinger <paul.gessinger@cern.ch>
2024-09-18 13:01:08 +0200

    format fix

--- Examples/Framework/src/Framework/Sequencer.cpp
+++ Examples/Framework/src/Framework/Sequencer.cpp
@@ -199,8 +199,10 @@ void Sequencer::addElement(const std::shared_ptr<SequenceElement>& element) {
         ACTS_ERROR("Adding "
                    << elementType << " " << element->name() << ":"
                    << "\n-> white board will contain key '" << handle->key()
-                   << "'" << "\nat this point in the sequence (source: "
-                   << source.fullName() << ")," << "\nbut the type will be\n"
+                   << "'"
+                   << "\nat this point in the sequence (source: "
+                   << source.fullName() << "),"
+                   << "\nbut the type will be\n"
                    << "'" << demangleAndShorten(source.typeInfo().name()) << "'"
                    << "\nand not\n"
                    << "'" << demangleAndShorten(handle->typeInfo().name())
@@ -209,8 +211,8 @@ void Sequencer::addElement(const std::shared_ptr<SequenceElement>& element) {
       }
     } else {
       ACTS_ERROR("Adding " << elementType << " " << element->name() << ":"
-                           << "\n-> white board will not contain key" << " '"
-                           << handle->key()
+                           << "\n-> white board will not contain key"
+                           << " '" << handle->key()
                            << "' at this point in the sequence."
                            << "\n   Needed for read data handle '"
                            << handle->name() << "'");
