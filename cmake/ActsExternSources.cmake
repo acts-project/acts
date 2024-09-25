9c7cdfaa0a852e783ea7c88416a972fdbf1eb815
Andreas Salzburger <asalzburger@gmail.com>
2024-09-25 06:39:56 +0200

    chore: update to detray version v0.75.3 (#3646)
    This PR updates to the detray version v0.75.3 which should have another
    attempt to fix the CI failure for the surface grid conversion PR.

--- cmake/ActsExternSources.cmake
+++ cmake/ActsExternSources.cmake
@@ -27,7 +27,7 @@ set(ACTS_COVFIE_SOURCE
 mark_as_advanced(ACTS_COVFIE_SOURCE)
 
 set(ACTS_DETRAY_SOURCE
-    "URL;https://github.com/acts-project/detray/archive/refs/tags/v${_acts_detray_version}.tar.gz;URL_HASH;SHA256=249066c138eac4114032e8d558f3a05885140a809332a347c7667978dbff54ee"
+    "URL;https://github.com/acts-project/detray/archive/refs/tags/v${_acts_detray_version}.tar.gz;URL_HASH;SHA256=1249d7398d1e534bd36b6f5a7d06a5e67adf6adeb8bca188d7e35490a675de7a"
     CACHE STRING
     "Source to take DETRAY from"
 )
