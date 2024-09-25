aa4d52cb8f20bb6f1b6c44012bdd3a91fc539284
Andreas Stefl <stefl.andreas@gmail.com>
2024-09-25 19:49:17 +0200

    revert path

--- Examples/Scripts/Python/full_chain_itk.py
+++ Examples/Scripts/Python/full_chain_itk.py
@@ -25,7 +25,7 @@ from acts.examples.reconstruction import (
 
 ttbar_pu200 = False
 u = acts.UnitConstants
-geo_dir = pathlib.Path("../../acts-itk")
+geo_dir = pathlib.Path("acts-itk")
 outputDir = pathlib.Path.cwd() / "itk_output"
 # acts.examples.dump_args_calls(locals())  # show acts.examples python binding calls
 
