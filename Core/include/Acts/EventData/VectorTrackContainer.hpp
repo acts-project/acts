a8ad0b6860756f6b0dd53d0a52f1b0c5bcadfb77
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-09-29 21:25:11 +0200

    constexpr2

--- Core/include/Acts/EventData/VectorTrackContainer.hpp
+++ Core/include/Acts/EventData/VectorTrackContainer.hpp
@@ -144,7 +144,11 @@ class VectorTrackContainerBase {
 
  public:
   constexpr bool hasColumn_impl(HashedString key) const {
-    return m_dynamic.find(key) != m_dynamic.end();
+    using namespace Acts::HashedStringLiteral;
+    switch (key) {
+      default:
+        return m_dynamic.contains(key);
+    }
   }
 
   const Surface* referenceSurface_impl(IndexType itrack) const {
