346e84425f97cb85f8ea1fcfe7f6c8904c64d8a6
Carlo Varni <75478407+CarloVarni@users.noreply.github.com>
2024-09-30 17:38:21 +0200

    feat: Support seed from N space points (#3645)
    Future support for seeds created from `N` space points, with `N >= 3`.
    The seed EDM naturally support the possibility of requiring more than 3 space points, so the Seed class is now templated on both the external space point type and the required number of space points: `Seed<external_spacepoint_t, N>`.
    
    By default `N == 3`, and there is a requirement that `N >= 3`.
    The default values is to avoid a breaking change in the code.
    
    This PR requires https://github.com/acts-project/acts/pull/3432

--- Core/include/Acts/EventData/Seed.hpp
+++ Core/include/Acts/EventData/Seed.hpp
@@ -13,21 +13,27 @@
 
 namespace Acts {
 
-template <typename external_spacepoint_t>
+template <typename external_spacepoint_t, std::size_t N = 3ul>
+  requires(N >= 3)
 class Seed {
  public:
-  Seed(const external_spacepoint_t& b, const external_spacepoint_t& m,
-       const external_spacepoint_t& u);
+  using value_type = external_spacepoint_t;
+  static constexpr std::size_t DIM = N;
+
+  template <typename... args_t>
+    requires(sizeof...(args_t) == N) &&
+            (std::same_as<external_spacepoint_t, args_t> && ...)
+  explicit Seed(const args_t&... points);
 
   void setVertexZ(float vertex);
   void setQuality(float seedQuality);
 
-  const std::array<const external_spacepoint_t*, 3>& sp() const;
+  const std::array<const external_spacepoint_t*, N>& sp() const;
   float z() const;
   float seedQuality() const;
 
  private:
-  std::array<const external_spacepoint_t*, 3> m_spacepoints;
+  std::array<const external_spacepoint_t*, N> m_spacepoints{};
   float m_vertexZ{0.f};
   float m_seedQuality{-std::numeric_limits<float>::infinity()};
 };
