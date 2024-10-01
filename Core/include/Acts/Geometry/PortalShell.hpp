93b50fe1319b41d13e55604f41ebda2457610160
Paul Gessinger <paul.gessinger@cern.ch>
2024-10-01 11:29:15 +0200

    shell: Add doc comments

--- Core/include/Acts/Geometry/PortalShell.hpp
+++ Core/include/Acts/Geometry/PortalShell.hpp
@@ -21,28 +21,56 @@ class Portal;
 class TrackingVolume;
 class GeometryContext;
 
+/// @class PortalShellBase
+/// The portal shell of a volume is the set of portals that describes
+/// connections "outside" of the volume. Which boundary surfaces of a volume are
+/// included in the shell depends on the volume. Portal shells are only used
+/// during geometry construction, and are not part of the final geometry
+/// description.
+///
+/// This class is the base class for all portal shells
 class PortalShellBase {
  public:
-  // virtual void apply(TrackingVolume& volume) const = 0;
-
+  /// Virtusl destructor
   virtual ~PortalShellBase() = default;
 
+  /// Connect a volume to the outer side of all portal shells. Which "side" is
+  /// "outer" depends on the volume type.
+  /// This method essentially creates a @c TrivialPortalLink on the unconnected
+  /// side of each portal that is part of the chell
+  /// @param volume The volume to connect
   virtual void connectOuter(TrackingVolume& volume) = 0;
 
+  /// Get the number of portals in the shell. This number depends on the volume
+  /// type
+  /// @return The number of portals in the shell
   virtual std::size_t size() const = 0;
 
-  // @TODO: Revisit, I'm not super happy with this interface.
+  /// Instruct the shell to register the portals with the volume, handing over
+  /// shared ownership in the process.
+  /// @note The target volume depends on the shell type, e.g. composite shells
+  ///       like the @c CylinerStackPortalShell register portals to the *correct*
+  ///       volumes.
   virtual void applyToVolume() = 0;
 
+  /// Check if a portal is *valid*, e.g. if non of the portals has two
+  /// unconnected sides.
+  /// @return True if the shell is valid, false otherwise
   virtual bool isValid() const = 0;
 
+  /// Get a label for the portal shell for debugging purposes
+  /// @return A label for the portal shell
   virtual std::string label() const = 0;
 };
 
+/// @class CylinderPortalShell
+/// Base class for cylinder shaped portal shells, e.g. shells for cylinder
+/// volumes
 class CylinderPortalShell : public PortalShellBase {
  public:
-  // These values are synchronized with the BoundarySurfaceFace enum.
-  // Once Gen1 is removed, this can be changed.
+  /// Enum describing the possible faces of a cylinder portal shell
+  /// @note These values are synchronized with the BoundarySurfaceFace enum.
+  ///       Once Gen1 is removed, this can be changed.
   enum class Face : unsigned int {
     PositiveDisc = BoundarySurfaceFace::positiveFaceXY,
     NegativeDisc = BoundarySurfaceFace::negativeFaceXY,
@@ -54,30 +82,63 @@ class CylinderPortalShell : public PortalShellBase {
 
   using enum Face;
 
+  /// Retrieve the portal associated to the given face. Can be nullptr if unset.
+  /// @param face The face to retrieve the portal for
+  /// @return The portal associated to the face
   virtual Portal* portal(Face face) = 0;
+
+  /// Retrieve a shared_ptr for the portal associated to the given face. Can be
+  /// nullptr if unset.
+  /// @param face The face to retrieve the portal for
+  /// @return The portal associated to the face
   virtual std::shared_ptr<Portal> portalPtr(Face face) = 0;
 
+  /// Set the portal associated to the given face.
+  /// @param portal The portal to set
+  /// @param face The face to set the portal
   virtual void setPortal(std::shared_ptr<Portal> portal, Face face) = 0;
 
+  /// @copydoc PortalShellBase::connectOuter
   void connectOuter(TrackingVolume& volume) override;
 };
 
+/// Output stream operator for the CylinderPortalShell::Face enum
+/// @param os The output stream
+/// @param face The face to output
+/// @return The output stream
 std::ostream& operator<<(std::ostream& os, CylinderPortalShell::Face face);
 
+/// @class SingleCylinderPortalShell
+/// This class describes a cylinder shell containing a single volume. The
+/// available faces depend on the configuration of the cylinder volume bounds.
+/// If a phi sector is configured, the shell will have corresponding portal
+/// slots. If the inner radius is non-zero, the shell will have an inner
+/// cylinder portal slot.
 class SingleCylinderPortalShell : public CylinderPortalShell {
  public:
+  /// Construct a single cylinder portal shell for the given volume
+  /// @param volume The volume to create the shell for
   explicit SingleCylinderPortalShell(TrackingVolume& volume);
 
+  /// @copydoc PortalShellBase::size
   std::size_t size() const final;
 
+  /// @copydoc CylinderPortalShell::portal
   Portal* portal(Face face) final;
+
+  /// @copydoc CylinderPortalShell::portalPtr
   std::shared_ptr<Portal> portalPtr(Face face) final;
+
+  /// @copydoc CylinderPortalShell::setPortal
   void setPortal(std::shared_ptr<Portal> portal, Face face) final;
 
+  /// @copydoc PortalShellBase::applyToVolume
   void applyToVolume() override;
 
+  /// @copydoc PortalShellBase::isValid
   bool isValid() const override;
 
+  /// @copydoc PortalShellBase::label
   std::string label() const override;
 
  private:
@@ -86,24 +147,72 @@ class SingleCylinderPortalShell : public CylinderPortalShell {
   TrackingVolume* m_volume;
 };
 
+/// @class CylinderStackPortalShell
+/// This class describes a cylinder shell containing multiple volumes. The
+/// available faces depend on the configuration of the cylinder volume bounds.
+/// @note The stack shell currently does not support phi sectors
+/// The stack can be oriented along the (local) z or r direction, which drives
+/// the stacking. Depending on the direction, portals on the shells of children
+/// are merged or fused. Subsequently, portal access respects shared portals
+/// between shells. Below is an illustration of a stack in the r direction:
+///
+///  Fused         ------------------+
+/// portals ----+  |                 |
+///   |         |  v           OuterCylinder
+///   |  +------+------+
+///   |  |      |      |
+///   |  |      |      |<--+
+///   +--+---+  v      |   |
+///      +---+---------+   |
+///      |   |         |   |      Shared portal
+///      |   |         |<--+---      (grid)
+///      |   v         |   |      PositiveDisc
+///      +-------------+   |
+/// r ^  |             |   |
+///   |  |             |<--+
+///   |  |             |
+///   |  +-------------+       InnerCylinder
+///  -+----->      ^            (if rMin>0)
+///          z     |                 |
+///                ------------------+
+///
+/// @note The shells must be ordered in the given direction
+/// Depending on the stack direction, the portal lookup will return different
+/// portals. In the illustration above, the `PositiveDisc` portal is shared
+/// among all shells, while the `OuterCylinder` and `InnerCylinder` portals are
+/// looked up from the innermost and outermost shell in the r direction.
 class CylinderStackPortalShell : public CylinderPortalShell {
  public:
+  /// Construct the portal shell stack from the given shells
+  /// @param gctx The geometry context
+  /// @param shells The shells to stack
   /// @note The shells must be ordered in the given direction
+  /// @param direction The stacking direction
+  /// @param logger A logging instance for debugging
   CylinderStackPortalShell(const GeometryContext& gctx,
                            std::vector<CylinderPortalShell*> shells,
                            BinningValue direction,
                            const Logger& logger = getDummyLogger());
 
+  /// @copydoc PortalShellBase::connectOuter
   std::size_t size() const final;
+
+  /// @copydoc CylinderPortalShell::portal
   Portal* portal(Face face) final;
+
+  /// @copydoc CylinderPortalShell::portalPtr
   std::shared_ptr<Portal> portalPtr(Face face) final;
+
+  /// @copydoc CylinderPortalShell::setPortal
   void setPortal(std::shared_ptr<Portal> portal, Face face) final;
 
-  // No-op, because it's a composite portal shell
+  /// No-op, because it's a composite portal shell
   void applyToVolume() override {}
 
+  /// @copydoc PortalShellBase::isValid
   bool isValid() const override;
 
+  /// @copydoc PortalShellBase::label
   std::string label() const override;
 
  private:
