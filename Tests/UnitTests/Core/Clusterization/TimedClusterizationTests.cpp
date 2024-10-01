e5c2fe7334633522c99fb1a06001848ba7749cf5
cvarni <carlovarni@pccvarni.dyndns.cern.ch>
2024-10-01 14:08:06 +0200

    add empty unit test

---
+++ Tests/UnitTests/Core/Clusterization/TimedClusterizationTests.cpp
@@ -0,0 +1,18 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016-2024 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Clusterization/TimedClusterization.hpp"
+
+namespace Acts::Test {
+
+BOOST_AUTO_TEST_CASE(TimedGrid_2D_notime) {
+}
+
+}
