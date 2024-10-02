480ceb8adc0feb933dbb6c0fd7ad15b607cf79d7
Andreas Stefl <stefl.andreas@gmail.com>
2024-10-02 21:18:10 +0200

    move impl to cpp

--- Core/include/Acts/Seeding/EstimateTrackParamsFromSeed.hpp
+++ Core/include/Acts/Seeding/EstimateTrackParamsFromSeed.hpp
@@ -304,33 +304,6 @@ struct EstimateTrackParamCovarianceConfig {
 
 BoundMatrix estimateTrackParamCovariance(
     const BoundVector& params,
-    const EstimateTrackParamCovarianceConfig& config) {
-  BoundSquareMatrix result = BoundSquareMatrix::Zero();
-
-  for (std::size_t i = eBoundLoc0; i < eBoundSize; ++i) {
-    double sigma = config.initialSigmas[i];
-    double variance = sigma * sigma;
-
-    if (i == eBoundQOverP) {
-      // note that we rely on the fact that sigma theta is already computed
-      double varianceTheta = result(eBoundTheta, eBoundTheta);
-
-      // transverse momentum contribution
-      variance += std::pow(config.initialSigmaPtRel * params[eBoundQOverP], 2);
-
-      // theta contribution
-      variance +=
-          varianceTheta *
-          std::pow(params[eBoundQOverP] / std::tan(params[eBoundTheta]), 2);
-    }
-
-    // Inflate the initial covariance
-    variance *= config.initialVarInflation[i];
-
-    result(i, i) = variance;
-  }
-
-  return result;
-}
+    const EstimateTrackParamCovarianceConfig& config);
 
 }  // namespace Acts
