a96d7d97932ee01f50e2e67ecac6e8dd456726cc
Andreas Stefl <stefl.andreas@gmail.com>
2024-10-02 21:38:00 +0200

    more periodic treatment

--- Core/src/TrackFitting/GainMatrixSmoother.cpp
+++ Core/src/TrackFitting/GainMatrixSmoother.cpp
@@ -57,8 +57,14 @@ Result<void> GainMatrixSmoother::calculate(
   ACTS_VERBOSE(
       "Prev. predicted parameters: " << predicted(prev_ts).transpose());
 
+  BoundVector smoothedMinusPredicted = smoothed(prev_ts) - predicted(prev_ts);
+  smoothedMinusPredicted[eBoundPhi] = detail::difference_periodic(
+      smoothed(prev_ts)[eBoundPhi], predicted(prev_ts)[eBoundPhi], 2 * M_PI);
+  smoothedMinusPredicted[eBoundTheta] = detail::difference_periodic(
+      smoothed(prev_ts)[eBoundTheta], predicted(prev_ts)[eBoundTheta], M_PI);
+
   // Calculate the smoothed parameters
-  smoothed(ts) = filtered(ts) + G * (smoothed(prev_ts) - predicted(prev_ts));
+  smoothed(ts) = filtered(ts) + G * smoothedMinusPredicted;
   // Normalize phi and theta
   detail::normalizePhiThetaInplace(smoothed(ts)[eBoundPhi],
                                    smoothed(ts)[eBoundTheta]);
