f13806846a8337a91df5472cd9a9ac8bc91097a8
cvarni <carlovarni@MacBook-Pro-di-Carlo-5.local>
2024-10-04 00:08:59 +0200

    format

--- Tests/UnitTests/Core/EventData/SpacePointContainerEdmTests.cpp
+++ Tests/UnitTests/Core/EventData/SpacePointContainerEdmTests.cpp
@@ -15,17 +15,18 @@
 
 #include <any>
 #include <cmath>
-#include <vector>
-#include <math.h>
-#include <ranges>
 #include <iterator>
+#include <ranges>
+#include <vector>
+
+#include <math.h>
 
 namespace Acts::Test {
 
 struct SpacePoint {
   SpacePoint() = default;
   SpacePoint(float ix, float iy, float iz, float ivarR, float ivarZ)
-    : x(ix), y(iy), z(iz), varR(ivarR), varZ(ivarZ) {}
+      : x(ix), y(iy), z(iz), varR(ivarR), varZ(ivarZ) {}
   float x{};
   float y{};
   float z{};
@@ -34,32 +35,31 @@ struct SpacePoint {
 };
 using SpacePointCollection = std::vector<SpacePoint>;
 
-  class Adapter {
-  public:
-    friend Acts::SpacePointContainer<Adapter, Acts::detail::RefHolder>;
-    friend Acts::SpacePointContainer<Adapter, Acts::detail::ValueHolder>;
-    using value_type = SpacePoint;
-    using ValueType = value_type;
-    
-    Adapter(SpacePointCollection&&) = delete;
-    Adapter(const SpacePointCollection& externalCollection)
-      : m_storage(&externalCollection)
-    {}
-    
-  private:
-    std::size_t size_impl() const { return storage().size(); }
-    
-    float x_impl(std::size_t idx) const { return storage()[idx].x; };
-    float y_impl(std::size_t idx) const { return storage()[idx].y; };
-    float z_impl(std::size_t idx) const { return storage()[idx].z; };
-    float varianceR_impl(std::size_t idx) const { return storage()[idx].varR; }
-    float varianceZ_impl(std::size_t idx) const { return storage()[idx].varZ; }
+class Adapter {
+ public:
+  friend Acts::SpacePointContainer<Adapter, Acts::detail::RefHolder>;
+  friend Acts::SpacePointContainer<Adapter, Acts::detail::ValueHolder>;
+  using value_type = SpacePoint;
+  using ValueType = value_type;
 
-    const SpacePoint& get_impl(std::size_t idx) const { return storage()[idx]; }
+  Adapter(SpacePointCollection&&) = delete;
+  Adapter(const SpacePointCollection& externalCollection)
+      : m_storage(&externalCollection) {}
 
-    std::any component_impl(Acts::HashedString key, std::size_t /*n*/) const {
-      using namespace Acts::HashedStringLiteral;
-      switch (key) {
+ private:
+  std::size_t size_impl() const { return storage().size(); }
+
+  float x_impl(std::size_t idx) const { return storage()[idx].x; };
+  float y_impl(std::size_t idx) const { return storage()[idx].y; };
+  float z_impl(std::size_t idx) const { return storage()[idx].z; };
+  float varianceR_impl(std::size_t idx) const { return storage()[idx].varR; }
+  float varianceZ_impl(std::size_t idx) const { return storage()[idx].varZ; }
+
+  const SpacePoint& get_impl(std::size_t idx) const { return storage()[idx]; }
+
+  std::any component_impl(Acts::HashedString key, std::size_t /*n*/) const {
+    using namespace Acts::HashedStringLiteral;
+    switch (key) {
       case "TopStripVector"_hash:
       case "BottomStripVector"_hash:
       case "StripCenterDistance"_hash:
@@ -67,28 +67,30 @@ using SpacePointCollection = std::vector<SpacePoint>;
         return Acts::Vector3(0, 0, 0);
       default:
         throw std::runtime_error("no such component " + std::to_string(key));
-      }
     }
+  }
 
-    const SpacePointCollection& storage() const
-    { return *m_storage; }
-    
+  const SpacePointCollection& storage() const { return *m_storage; }
+
  private:
-    const SpacePointCollection* m_storage{};
-  };
-
+  const SpacePointCollection* m_storage{};
+};
 
 BOOST_AUTO_TEST_CASE(spacepoint_container_edm_traits) {
   using adapter_t = Acts::Test::Adapter;
-  using container_t = Acts::SpacePointContainer<adapter_t, Acts::detail::RefHolder>;
+  using container_t =
+      Acts::SpacePointContainer<adapter_t, Acts::detail::RefHolder>;
   using proxy_t = Acts::SpacePointProxy<container_t>;
   using iterator_t = Acts::ContainerIndexIterator<container_t, proxy_t&, false>;
 
   static_assert(std::ranges::range<container_t>);
-  static_assert(std::same_as<typename iterator_t::iterator_category, std::random_access_iterator_tag>);
-  static_assert(std::same_as<typename std::iterator_traits<iterator_t>::iterator_category, std::random_access_iterator_tag>);
+  static_assert(std::same_as<typename iterator_t::iterator_category,
+                             std::random_access_iterator_tag>);
+  static_assert(
+      std::same_as<typename std::iterator_traits<iterator_t>::iterator_category,
+                   std::random_access_iterator_tag>);
 }
-  
+
 BOOST_AUTO_TEST_CASE(spacepoint_container_edm_constructors) {
   std::size_t nExternalPoints = 10;
   SpacePointCollection externalCollection(nExternalPoints);
@@ -97,49 +99,68 @@ BOOST_AUTO_TEST_CASE(spacepoint_container_edm_constructors) {
   Acts::SpacePointContainerOptions spOptions;
 
   Acts::Test::Adapter adapterForRef(externalCollection);
-  Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::RefHolder> spContainerRef(spConfig, spOptions, adapterForRef);
-    
+  Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::RefHolder>
+      spContainerRef(spConfig, spOptions, adapterForRef);
+
   Acts::Test::Adapter adapterForVal(externalCollection);
-  Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::ValueHolder> spContainerVal(spConfig, spOptions, std::move(adapterForVal));
+  Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::ValueHolder>
+      spContainerVal(spConfig, spOptions, std::move(adapterForVal));
 
   BOOST_CHECK_EQUAL(spContainerRef.size(), nExternalPoints);
   BOOST_CHECK_EQUAL(spContainerVal.size(), nExternalPoints);
 }
-  
+
 BOOST_AUTO_TEST_CASE(spacepoint_container_edm_functionalities) {
   std::size_t nExternalPoints = 100;
   SpacePointCollection externalCollection;
   externalCollection.reserve(nExternalPoints);
-  for (std::size_t i(0); i<nExternalPoints; ++i) {
-    externalCollection.emplace_back(1.f * i, 1.5f * i, 2.f * i, 2.5f * i, 3.f * i);
+  for (std::size_t i(0); i < nExternalPoints; ++i) {
+    externalCollection.emplace_back(1.f * i, 1.5f * i, 2.f * i, 2.5f * i,
+                                    3.f * i);
   }
-    
+
   Acts::SpacePointContainerConfig spConfig;
   spConfig.useDetailedDoubleMeasurementInfo = true;
   Acts::SpacePointContainerOptions spOptions;
 
   Acts::Test::Adapter adapter(externalCollection);
-  Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::RefHolder> spContainer(spConfig, spOptions, adapter);
+  Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::RefHolder>
+      spContainer(spConfig, spOptions, adapter);
 
   BOOST_CHECK_EQUAL(spContainer.size(), nExternalPoints);
   BOOST_CHECK_EQUAL(spContainer.size(), externalCollection.size());
   BOOST_CHECK_EQUAL(spContainer.end() - spContainer.begin(), nExternalPoints);
-  BOOST_CHECK_EQUAL(std::distance(spContainer.begin(), spContainer.end()), nExternalPoints);
-  BOOST_CHECK_EQUAL(std::distance(std::ranges::begin(spContainer), std::ranges::end(spContainer)), nExternalPoints);
-  
-  using proxy_t = Acts::SpacePointProxy<Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::RefHolder>>;
-  static_assert(std::same_as<typename decltype(spContainer)::ValueType, Acts::Test::SpacePoint>);
-  static_assert(std::same_as<typename decltype(spContainer)::ProxyType, proxy_t>);
-  static_assert(std::same_as<typename decltype(spContainer)::value_type, proxy_t>);
-  static_assert(std::same_as<typename proxy_t::ContainerType, decltype(spContainer)>);
-  static_assert(std::same_as<typename proxy_t::ValueType, Acts::Test::SpacePoint>);
+  BOOST_CHECK_EQUAL(std::distance(spContainer.begin(), spContainer.end()),
+                    nExternalPoints);
+  BOOST_CHECK_EQUAL(std::distance(std::ranges::begin(spContainer),
+                                  std::ranges::end(spContainer)),
+                    nExternalPoints);
 
-  using iterator_t = Acts::ContainerIndexIterator<decltype(spContainer), proxy_t&, false>;
-  using const_iterator_t = Acts::ContainerIndexIterator<decltype(spContainer), const proxy_t&, true>;
-  static_assert(std::same_as<iterator_t, typename decltype(spContainer)::iterator>);
-  static_assert(std::same_as<const_iterator_t, typename decltype(spContainer)::const_iterator>);
-  static_assert(std::same_as<iterator_t, decltype(std::ranges::begin(spContainer))>);
-  static_assert(std::same_as<const_iterator_t, decltype(std::ranges::cbegin(spContainer))>);
+  using proxy_t = Acts::SpacePointProxy<
+      Acts::SpacePointContainer<Acts::Test::Adapter, Acts::detail::RefHolder>>;
+  static_assert(std::same_as<typename decltype(spContainer)::ValueType,
+                             Acts::Test::SpacePoint>);
+  static_assert(
+      std::same_as<typename decltype(spContainer)::ProxyType, proxy_t>);
+  static_assert(
+      std::same_as<typename decltype(spContainer)::value_type, proxy_t>);
+  static_assert(
+      std::same_as<typename proxy_t::ContainerType, decltype(spContainer)>);
+  static_assert(
+      std::same_as<typename proxy_t::ValueType, Acts::Test::SpacePoint>);
+
+  using iterator_t =
+      Acts::ContainerIndexIterator<decltype(spContainer), proxy_t&, false>;
+  using const_iterator_t =
+      Acts::ContainerIndexIterator<decltype(spContainer), const proxy_t&, true>;
+  static_assert(
+      std::same_as<iterator_t, typename decltype(spContainer)::iterator>);
+  static_assert(std::same_as<const_iterator_t,
+                             typename decltype(spContainer)::const_iterator>);
+  static_assert(
+      std::same_as<iterator_t, decltype(std::ranges::begin(spContainer))>);
+  static_assert(std::same_as<const_iterator_t,
+                             decltype(std::ranges::cbegin(spContainer))>);
 
   std::size_t n = 0ul;
   for (const proxy_t& proxy : spContainer) {
@@ -163,9 +184,10 @@ BOOST_AUTO_TEST_CASE(spacepoint_container_edm_functionalities) {
     const Acts::Vector3& topStripVector = proxy.topStripVector();
     const Acts::Vector3& bottomStripVector = proxy.bottomStripVector();
     const Acts::Vector3& stripCenterDistance = proxy.stripCenterDistance();
-    const Acts::Vector3& topStripCenterPosition = proxy.topStripCenterPosition();
+    const Acts::Vector3& topStripCenterPosition =
+        proxy.topStripCenterPosition();
 
-    for (std::size_t i(0); i<3ul; ++i) {
+    for (std::size_t i(0); i < 3ul; ++i) {
       BOOST_CHECK_EQUAL(topStripVector[i], 0.);
       BOOST_CHECK_EQUAL(bottomStripVector[i], 0.);
       BOOST_CHECK_EQUAL(stripCenterDistance[i], 0.);
@@ -177,7 +199,7 @@ BOOST_AUTO_TEST_CASE(spacepoint_container_edm_functionalities) {
 
     ++n;
   }
-  BOOST_CHECK_EQUAL(n, nExternalPoints);  
+  BOOST_CHECK_EQUAL(n, nExternalPoints);
 }
 
 }  // namespace Acts::Test
