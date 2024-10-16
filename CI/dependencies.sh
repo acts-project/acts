f3f25e42b054179cc6ae90dba048d7a1d1124c9e
Paul Gessinger <paul.gessinger@cern.ch>
2024-10-16 08:10:50 +0200

    dep script exits, but unclear why

--- CI/dependencies.sh
+++ CI/dependencies.sh
@@ -12,7 +12,7 @@ function set_env {
   key="$1"
   value="$2"
 
-  echo "Setting ${key}=${value}"
+  echo "=> ${key}=${value}"
 
   if [ -n "${GITHUB_ACTIONS:-}" ]; then
     echo "${key}=${value}" >> $GITHUB_ENV
@@ -60,9 +60,9 @@ if [ -z "$TAR" ]; then
     exit 1
 fi
 
-run mkdir -p "${destination}"
+mkdir -p "${destination}"
 
-run $CURL \
+$CURL \
   --retry 5 \
   --connect-timeout 2 \
   --location $url \
@@ -76,7 +76,7 @@ run $CURL \
 orig_share=$(${destination}/bin/geant4-config --datasets|head -n1|perl -pe 's|.*?(\/.*)\/share.*|\1|')
 orig_share_escaped=$(echo $orig_share|perl -pe 's|/|\\/|g')
 destination_escaped=$(echo "$destination"|perl -pe 's|/|\\/|g')
-run perl -pi.bak -e "s/$orig_share_escaped/$destination_escaped/g" dependencies/bin/geant4-config
+run perl -pi.bak -e "s/$orig_share_escaped/$destination_escaped/g" ${destination}/bin/geant4-config
 
 if [ -n "${GITHUB_ACTIONS:-}" ]; then
   echo "Running in GitHub Actions"
