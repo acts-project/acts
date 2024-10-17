32ba8f1f606107a8841764009f4abd8190dade1f
Andreas Stefl <stefl.andreas@gmail.com>
2024-10-17 10:38:06 +0200

    refactor: Group all ROOT writers together

--- Examples/Io/Root/src/TrackFitterPerformanceWriter.cpp
+++ Examples/Io/Root/src/TrackFitterPerformanceWriter.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "ActsExamples/Io/Performance/TrackFitterPerformanceWriter.hpp"
+#include "ActsExamples/Io/Root/TrackFitterPerformanceWriter.hpp"
 
 #include "Acts/EventData/MultiTrajectoryHelpers.hpp"
 #include "Acts/EventData/TrackParameters.hpp"
