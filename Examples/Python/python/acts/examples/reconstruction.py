c7075afff81384d691d1b48661514d3201ef85fd
Andreas Stefl <stefl.andreas@gmail.com>
2024-10-19 12:10:20 +0200

    rename outputs and labels in physmon; rename refs

--- Examples/Python/python/acts/examples/reconstruction.py
+++ Examples/Python/python/acts/examples/reconstruction.py
@@ -1662,7 +1662,7 @@ def addTrackWriters(
                 inputParticles="particles_selected",
                 inputTrackParticleMatching="track_particle_matching",
                 inputParticleTrackMatching="particle_track_matching",
-                filePath=str(outputDirRoot / f"performance_{name}.root"),
+                filePath=str(outputDirRoot / f"performance_finding_{name}.root"),
             )
             s.addWriter(trackFinderPerfWriter)
 
