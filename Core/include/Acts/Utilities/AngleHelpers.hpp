94ddba51c30722b80b27376511bf0800453b9b96
Andreas Stefl <stefl.andreas@gmail.com>
2024-10-28 09:20:26 +0100

    revert angle helper changes

--- Core/include/Acts/Utilities/AngleHelpers.hpp
+++ Core/include/Acts/Utilities/AngleHelpers.hpp
@@ -9,36 +9,8 @@
 #pragma once
 
 #include <cmath>
-#include <numbers>
 
 namespace Acts::AngleHelpers {
-
-template <typename Scalar>
-struct EtaThetaConversionTraits {
-  static constexpr Scalar minTheta = 0;
-  static constexpr Scalar maxTheta = std::numbers::pi_v<Scalar>;
-
-  static constexpr Scalar minEta = -std::numeric_limits<Scalar>::infinity();
-  static constexpr Scalar maxEta = std::numeric_limits<Scalar>::infinity();
-};
-
-template <>
-struct EtaThetaConversionTraits<float> {
-  static constexpr float minTheta = 0;
-  static constexpr float maxTheta = std::numbers::pi_v<float> - 1e-6f;
-
-  static constexpr float minEta = -std::numeric_limits<float>::infinity();
-  static constexpr float maxEta = std::numeric_limits<float>::infinity();
-};
-
-template <>
-struct EtaThetaConversionTraits<double> {
-  static constexpr double minTheta = 0;
-  static constexpr double maxTheta = std::numbers::pi;
-
-  static constexpr double minEta = -std::numeric_limits<double>::infinity();
-  static constexpr double maxEta = std::numeric_limits<double>::infinity();
-};
 
 /// Calculate the pseudorapidity from the polar angle theta.
 ///
@@ -47,13 +19,6 @@ struct EtaThetaConversionTraits<double> {
 /// @return the pseudorapidity towards the z-axis.
 template <typename Scalar>
 Scalar etaFromTheta(Scalar theta) {
-  if (theta < EtaThetaConversionTraits<Scalar>::minTheta) {
-    return std::numeric_limits<Scalar>::infinity();
-  }
-  if (theta > EtaThetaConversionTraits<Scalar>::maxTheta) {
-    return -std::numeric_limits<Scalar>::infinity();
-  }
-
   return -std::log(std::tan(0.5 * theta));
 }
 
@@ -64,13 +29,6 @@ Scalar etaFromTheta(Scalar theta) {
 /// @return the polar angle in radian towards the z-axis.
 template <typename Scalar>
 Scalar thetaFromEta(Scalar eta) {
-  if (eta < EtaThetaConversionTraits<Scalar>::minEta) {
-    return std::numbers::pi_v<Scalar>;
-  }
-  if (eta > EtaThetaConversionTraits<Scalar>::maxEta) {
-    return 0;
-  }
-
   return 2 * std::atan(std::exp(-eta));
 }
 
