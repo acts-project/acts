e17e48f0885d992ce71ba9d49e20c5b68a3cf482
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-10-30 11:45:31 +0100

    stable 4

--- Core/include/Acts/Geometry/CylinderVolumeBounds.hpp
+++ Core/include/Acts/Geometry/CylinderVolumeBounds.hpp
@@ -111,7 +111,7 @@ class CylinderVolumeBounds : public VolumeBounds {
   /// Constructor - from a fixed size array
   ///
   /// @param values The bound values
-  CylinderVolumeBounds(const std::array<ActsScalar, eSize>& values);
+  explicit CylinderVolumeBounds(const std::array<ActsScalar, eSize>& values);
 
   /// Constructor - extruded from cylinder bounds and thickness
   ///
