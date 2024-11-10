e3e108212dd4a6da5167cde0537a91b6076a4bea
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-11-10 16:38:13 +0100

    make function

--- Core/src/TrackFitting/GlobalChiSquareFitter.cpp
+++ Core/src/TrackFitting/GlobalChiSquareFitter.cpp
@@ -10,6 +10,30 @@
 
 #include "Acts/Definitions/TrackParametrization.hpp"
 
+void Acts::Experimental::updateGx2fParams(
+    BoundTrackParameters& params, const Eigen::VectorXd& deltaParamsExtended,
+    const std::size_t nMaterialSurfaces,
+    std::unordered_map<GeometryIdentifier, ScatteringProperties>& scatteringMap,
+    const std::vector<GeometryIdentifier>& geoIdVector) {
+  // update params
+  params.parameters() +=
+      deltaParamsExtended.topLeftCorner<eBoundSize, 1>().eval();
+
+  // update the scattering angles.
+  for (std::size_t matSurface = 0; matSurface < nMaterialSurfaces;
+       matSurface++) {
+    const std::size_t deltaPosition = eBoundSize + 2 * matSurface;
+    const GeometryIdentifier geoId = geoIdVector[matSurface];
+    const auto scatteringMapId = scatteringMap.find(geoId);
+    assert(scatteringMapId != scatteringMap.end() &&
+           "No scattering angles found for material surface.");
+    scatteringMapId->second.scatteringAngles().block<2, 1>(2, 0) +=
+        deltaParamsExtended.block<2, 1>(deltaPosition, 0).eval();
+  }
+
+  return;
+}
+
 void Acts::Experimental::updateGx2fCovarianceParams(
     BoundMatrix& fullCovariancePredicted, Gx2fSystem& extendedSystem) {
   // make invertible
