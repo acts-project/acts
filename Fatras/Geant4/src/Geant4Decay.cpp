ba4e47a6b662fb4d7679ce95719ac2fcf4cce543
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-11-22 17:37:46 +0100

    fixes

--- Fatras/Geant4/src/Geant4Decay.cpp
+++ Fatras/Geant4/src/Geant4Decay.cpp
@@ -64,7 +64,7 @@ std::vector<ActsFatras::Particle> ActsFatras::Geant4Decay::decayParticle(
 
     // Convert the decay product from Geant4 to Acts
     const G4ThreeVector& mom = prod->GetMomentum();
-    constexpr Scalar convertEnergy = Acts::UnitConstants::GeV / CLHEP::GeV;
+    constexpr double convertEnergy = Acts::UnitConstants::GeV / CLHEP::GeV;
     Acts::Vector3 amgMom(mom.x(), mom.y(), mom.z());
     amgMom *= convertEnergy;
     const std::int32_t pdg = prod->GetPDGcode();
