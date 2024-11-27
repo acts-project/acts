820142db6185bc2ae8350831afa108e5527bea9a
Andreas Stefl <stefl.andreas@gmail.com>
2024-11-27 17:23:05 +0100

    Revert "revert initial sigmas"
    This reverts commit 531440f6f94385d63ecad1e1f396845db89f0136.

--- Examples/Scripts/Python/truth_tracking_gx2f.py
+++ Examples/Scripts/Python/truth_tracking_gx2f.py
@@ -97,16 +97,6 @@ def runTruthTrackingGx2f(
         rnd=rnd,
         inputParticles="particles_input",
         seedingAlgorithm=SeedingAlgorithm.TruthSmeared,
-        initialSigmas=[
-            1 * u.mm,
-            1 * u.mm,
-            1 * u.degree,
-            1 * u.degree,
-            0.1 * u.e / u.GeV,
-            1 * u.ns,
-        ],
-        initialSigmaPtRel=0.01,
-        initialVarInflation=[1.0] * 6,
         particleHypothesis=acts.ParticleHypothesis.muon,
     )
 
