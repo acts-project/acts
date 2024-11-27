7c19694152108f1ec1370dbf1230a20c3125e46b
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2024-11-27 18:52:07 +0100

    feat: Add JSON detector element (#3851)
    Add a json detector element, which is explicitly constructed from a `nlohmann::json` object in order to ensure consistency. Move json surface reading utilities from examples to Plugins.

--- Plugins/Json/include/Acts/Plugins/Json/JsonSurfacesReader.hpp
+++ Plugins/Json/include/Acts/Plugins/Json/JsonSurfacesReader.hpp
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "Acts/Geometry/GeometryHierarchyMap.hpp"
+#include "Acts/Plugins/Json/JsonDetectorElement.hpp"
 
 #include <memory>
 #include <string>
@@ -18,9 +19,11 @@ namespace Acts {
 class Surface;
 }
 
-namespace ActsExamples::JsonSurfacesReader {
+namespace Acts::JsonSurfacesReader {
 
 /// @brief Options specification for surface reading
+/// The file should contain an array of json surfaces
+/// as produced by the SurfaceJsonConverter tools
 struct Options {
   /// @brief  Which input file to read from
   std::string inputFile = "";
@@ -29,6 +32,7 @@ struct Options {
 };
 
 /// @brief Read the surfaces from the input file
+/// For details on the file format see the options struct
 ///
 /// @param options specifies which file and what to read
 ///
@@ -37,10 +41,22 @@ Acts::GeometryHierarchyMap<std::shared_ptr<Acts::Surface>> readHierarchyMap(
     const Options& options);
 
 /// @brief Read the flat surfaces from the input file
+/// For details on the file format see the options struct
 ///
-/// @param inputFile is the input file to read from
+/// @param options options for surface reading
 ///
 /// @return  a vector of surfaces
 std::vector<std::shared_ptr<Acts::Surface>> readVector(const Options& options);
 
-}  // namespace ActsExamples::JsonSurfacesReader
+/// @brief Read the surfaces from the input file and create
+/// detector elements
+/// For details on the file format see the options struct
+///
+/// @param options options for surface reading
+/// @param thickness the thickness used to construct the detector element
+///
+/// @return  a vector of surfaces
+std::vector<std::shared_ptr<Acts::JsonDetectorElement>> readDetectorElements(
+    const Options& options, double thickness);
+
+}  // namespace Acts::JsonSurfacesReader
