bab2d1e1c7f1f685afc16eec4721230f516d4ca7
Andreas Salzburger <asalzburger@gmail.com>
2024-12-05 09:26:35 +0100

    add a bit more tests

--- Tests/UnitTests/Core/Visualization/Interpolation3DTests.cpp
+++ Tests/UnitTests/Core/Visualization/Interpolation3DTests.cpp
@@ -7,43 +7,50 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include <boost/test/unit_test.hpp>
-#include <numbers>
 
-#include "Acts/Visualization/Interpolation3D.hpp"
 #include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
+#include "Acts/Visualization/Interpolation3D.hpp"
+
+#include <numbers>
 
 namespace Acts::Test {
 
 BOOST_AUTO_TEST_SUITE(Visualization)
 
 BOOST_AUTO_TEST_CASE(SplineInterpolation) {
+  /// Define the input vector
+  double R = 10.;
+  std::vector<Acts::Vector3> inputs;
 
-    /// Define the input vector
-    double R = 10.;
-    std::vector<Acts::Vector3> inputs;
-    for (double phi = 0; phi < 2 * std::numbers::pi; phi += std::numbers::pi / 4) {
-        inputs.push_back(Acts::Vector3(R * cos(phi), R * sin(phi), 0.));
-    }
+  // Interpolate the points options
+  std::vector<Acts::Vector3> trajectory;
 
-    // Interpolate the points options
-    std::vector<Acts::Vector3> trajectory;
+  // Empty in empty out check
+  trajectory = Acts::Interpolation3D::spline(inputs, 10);
+  BOOST_CHECK(trajectory.empty());
 
-    // (0) - nothing happens
-    trajectory = Acts::Interpolation3D::spline(inputs, 1);
-    // Check input and output size are the same
-    BOOST_CHECK_EQUAL(trajectory.size(), inputs.size());
+  for (double phi = 0; phi < 2 * std::numbers::pi;
+       phi += std::numbers::pi / 4) {
+    inputs.push_back(Acts::Vector3(R * cos(phi), R * sin(phi), 0.));
+  }
 
-    // (1) - interpolate between the points with 12 points in total
-    trajectory = Acts::Interpolation3D::spline(inputs, 12);
-    // Check the output size is correct
-    BOOST_CHECK_EQUAL(trajectory.size(), 12);
+  // (0) - nothing happens
+  trajectory = Acts::Interpolation3D::spline(inputs, 1);
+  // Check input and output size are the same
+  BOOST_CHECK_EQUAL(trajectory.size(), inputs.size());
 
-    for (const auto& point : trajectory) {
-        // Check the interpolated points are on the circle
-        // with a tolerance of course
-        CHECK_CLOSE_ABS(point.norm(), R, 0.1);
-    }
+  // (1) - interpolate between the points with 12 points in total
+  trajectory = Acts::Interpolation3D::spline(inputs, 12);
+  // Check the output size is correct
+  BOOST_CHECK_EQUAL(trajectory.size(), 12);
 
+  for (const auto& point : trajectory) {
+    // Check the interpolated points are on the circle
+    // with a tolerance of course
+    CHECK_CLOSE_ABS(point.norm(), R, 0.1);
+    // Verify points remain in the XY plane
+    CHECK_CLOSE_ABS(point.z(), 0., 0.1);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
