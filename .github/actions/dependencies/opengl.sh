209298a962ed2cd711212e941cac6b7d160b61b1
Paul Gessinger <hello@paulgessinger.com>
2024-12-11 16:59:21 +0100

    test

---
+++ .github/actions/dependencies/opengl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -u
+set -e
+
+source detect_os.sh
+
+packages_file=${GITHUB_WORKSPACE}/spack/etc/spack/packages.yaml
+
+if [ "$os" == "ubuntu" ]; then
+  sudo apt-get update
+  sudo apt-get install -y libgl1-mesa-dev
+cat <<EOF > "$packages_file"
+packages:
+  opengl:
+    buildable: false
+    externals:
+    - prefix: /usr/
+      spec: opengl@4.5
+EOF
+cat "$packages_file"
+elif [ "$os" == "almalinux" ]; then
+  dnf install -y mesa-libGLU
+cat <<EOF > "$packages_file"
+packages:
+  opengl:
+    buildable: false
+    externals:
+    - prefix: /usr/
+      spec: opengl@4.6
+EOF
+cat "$packages_file"
+fi
