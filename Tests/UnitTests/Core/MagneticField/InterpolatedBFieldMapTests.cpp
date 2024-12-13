9a2b74def57154015e846797923f611cfcc21281
Andreas Stefl <stefl.andreas@gmail.com>
2024-12-13 10:55:21 +0100

    chore: Some magnetic field code cleanup (#3982)
    - cleanup includes
    - cleanup some `using namespace`
    
    pulled out of https://github.com/acts-project/acts/pull/3951
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    	- Introduced new classes for enhanced magnetic field handling in Python.
    	- Added methods for creating magnetic field maps from files.
    
    - **Bug Fixes**
    	- Improved error handling in `getField` function to provide clearer feedback on failures.
    
    - **Documentation**
    	- Enhanced comments for clarity in several files.
    
    - **Chores**
    	- Removed unused include directives across multiple files to streamline code.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- Tests/UnitTests/Core/MagneticField/InterpolatedBFieldMapTests.cpp
+++ Tests/UnitTests/Core/MagneticField/InterpolatedBFieldMapTests.cpp
@@ -11,22 +11,17 @@
 #include "Acts/Definitions/Algebra.hpp"
 #include "Acts/MagneticField/InterpolatedBFieldMap.hpp"
 #include "Acts/MagneticField/MagneticFieldContext.hpp"
-#include "Acts/MagneticField/MagneticFieldProvider.hpp"
 #include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
 #include "Acts/Utilities/Axis.hpp"
 #include "Acts/Utilities/AxisFwd.hpp"
 #include "Acts/Utilities/Grid.hpp"
 #include "Acts/Utilities/Result.hpp"
 #include "Acts/Utilities/VectorHelpers.hpp"
-#include "Acts/Utilities/detail/grid_helper.hpp"
 
 #include <array>
 #include <cstddef>
-#include <functional>
 #include <optional>
-#include <tuple>
 #include <utility>
-#include <vector>
 
 using Acts::VectorHelpers::perp;
 
