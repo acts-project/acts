f0f2572d30c8f42ceea26db0b54c80c023732271
Paul Gessinger <hello@paulgessinger.com>
2024-12-16 11:32:59 +0100

    opengl

--- CI/dependencies/opengl.sh
+++ CI/dependencies/opengl.sh
@@ -3,7 +3,9 @@
 set -u
 set -e
 
-packages_file=$(spack location -r)/spack/etc/spack/packages.yaml
+packages_file=$(spack location -r)/etc/spack/packages.yaml
+echo "Packages file: $packages_file"
+stat "$packages_file" || true
 
 if ! command -v sudo &> /dev/null
 then
@@ -14,16 +16,28 @@ fi
 
 os=$(spack arch --family)
 
+echo "OS: $os"
+
 if [[ "$os" == *ubuntu* ]]; then
   ${SUDO} apt-get update
   ${SUDO} apt-get install -y libgl1-mesa-dev
+
+if [[ "$os" == *ubuntu24* ]]; then
+  version="4.6"
+elif [[ "$os" == *ubuntu20* ]]; then
+  version="4.5"
+else
+  echo "Unknown OS version, default OpenGL version"
+  version="4.5"
+fi
+
 cat <<EOF > "$packages_file"
 packages:
   opengl:
     buildable: false
     externals:
     - prefix: /usr/
-      spec: opengl@4.5
+      spec: opengl@${version}
 EOF
 cat "$packages_file"
 elif [[ "$os" == *almalinux* ]]; then
