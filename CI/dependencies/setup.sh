53daaef70f3c147e1a68f2269cd6601467234a15
Paul Gessinger <hello@paulgessinger.com>
2024-12-19 17:43:51 +0100

    need to do it after we did the environment

--- CI/dependencies/setup.sh
+++ CI/dependencies/setup.sh
@@ -99,7 +99,25 @@ mkdir -p ${destination}
 
 
 start_section "Install spack if not already installed"
-source "${SCRIPT_DIR}/install_spack.sh"
+if ! command -v spack &> /dev/null; then
+    if [ ! -d "spack" ]; then
+        echo "Cloning spack"
+        git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+        pushd spack > /dev/null
+        git config user.name github-actions[bot]
+        git config user.email 41898282+github-actions[bot]@users.noreply.github.com
+        # Apply patch for spack improvements
+        curl https://patch-diff.githubusercontent.com/raw/spack/spack/pull/47370.patch | git am
+        popd > /dev/null
+    else
+        echo "Updating spack"
+        pushd spack > /dev/null
+        git pull --rebase
+        popd > /dev/null
+    fi
+
+    source "$(pwd)/spack/share/spack/setup-env.sh"
+fi
 end_section
 
 if [ -n "${CI:-}" ]; then
