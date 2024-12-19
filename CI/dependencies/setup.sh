0d01f2f7e95b8cb45faa46539b72c9d61774c033
Paul Gessinger <hello@paulgessinger.com>
2024-12-19 17:26:17 +0100

    let's try to cache the spack checkout

--- CI/dependencies/setup.sh
+++ CI/dependencies/setup.sh
@@ -34,26 +34,6 @@ function end_section() {
     fi
 }
 
-# if [ -n "${1:-${DEPENDENCY_TAG:-}}" ]; then
-#     tag=${1:-${DEPENDENCY_TAG}}
-# else
-#     echo "Usage: $0 <tag> <destination>"
-#     exit 1
-# fi
-
-# if [ -n "${GITHUB_ACTIONS:-}" ]; then
-#     destination="${GITHUB_WORKSPACE}/dependencies"
-# elif [ -n "${GITLAB_CI:-}" ];then
-#     destination="${CI_PROJECT_DIR}/dependencies"
-# else
-#     if [ -n "${2:-}" ]; then
-#         destination=${2}
-#     else
-#         echo "Usage: $0 <tag> <destination>"
-#         exit 1
-#     fi
-# fi
-
 # Parse command line arguments
 while getopts "c:t:d:h" opt; do
   case ${opt} in
@@ -120,14 +100,23 @@ mkdir -p ${destination}
 
 start_section "Install spack if not already installed"
 if ! command -v spack &> /dev/null; then
-    git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-    pushd spack > /dev/null
-    git config user.name github-actions[bot]
-    git config user.email 41898282+github-actions[bot]@users.noreply.github.com
-    # Apply patch for spack improvements
-    curl https://patch-diff.githubusercontent.com/raw/spack/spack/pull/47370.patch | git am
-    source "$(pwd)/share/spack/setup-env.sh"
-    popd > /dev/null
+    if [ ! -d "spack" ]; then
+        echo "Cloning spack"
+        git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+        pushd spack > /dev/null
+        git config user.name github-actions[bot]
+        git config user.email 41898282+github-actions[bot]@users.noreply.github.com
+        # Apply patch for spack improvements
+        curl https://patch-diff.githubusercontent.com/raw/spack/spack/pull/47370.patch | git am
+        popd > /dev/null
+    else
+        echo "Updating spack"
+        pushd spack > /dev/null
+        git pull --rebase
+        popd > /dev/null
+    fi
+
+    source "$(pwd)/spack/share/spack/setup-env.sh"
 fi
 end_section
 
