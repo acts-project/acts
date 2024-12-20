ba00215ad1bba1aab560f253d1fe9e2e28d4acd9
Paul Gessinger <hello@paulgessinger.com>
2024-12-20 13:40:24 +0100

    switch repo, no patches

--- CI/dependencies/setup_spack.sh
+++ CI/dependencies/setup_spack.sh
@@ -6,13 +6,10 @@ _spack_version=${SPACK_VERSION:-develop}
 
 if [ ! -d "spack" ]; then
     echo "Cloning spack"
-    git clone --branch ${_spack_version} -c feature.manyFiles=true https://github.com/spack/spack.git
+    git clone --branch ${_spack_version} -c feature.manyFiles=true https://github.com/acts-project/spack.git
     pushd spack > /dev/null
     git config user.name 'CI'
     git config user.email '<>'
-    echo "Applying patch for spack improvements"
-    curl https://patch-diff.githubusercontent.com/raw/spack/spack/pull/47370.patch | git am
-    curl https://patch-diff.githubusercontent.com/raw/spack/spack/pull/48236.patch | git am
 
     rm -rf .git
 
