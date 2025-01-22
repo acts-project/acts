72f2fb50e86863bf4bf7a601c51561fcd40535db
Andreas Stefl <stefl.andreas@gmail.com>
2025-01-22 18:44:18 +0100

    revert

--- Tests/UnitTests/Core/Surfaces/AnnulusBoundsTests.cpp
+++ Tests/UnitTests/Core/Surfaces/AnnulusBoundsTests.cpp
@@ -11,7 +11,6 @@
 #include "Acts/Definitions/Algebra.hpp"
 #include "Acts/Surfaces/AnnulusBounds.hpp"
 #include "Acts/Surfaces/BoundaryTolerance.hpp"
-#include "Acts/Surfaces/DiscSurface.hpp"
 #include "Acts/Surfaces/SurfaceBounds.hpp"
 #include "Acts/Utilities/VectorHelpers.hpp"
 
