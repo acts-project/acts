719337d04db14980c14ca491c00b7bcfe64191d5
Paul Gessinger <hello@paulgessinger.com>
2025-01-30 14:53:43 +0100

    fix invokation maybe?

--- cmake/ActsStaticAnalysis.cmake
+++ cmake/ActsStaticAnalysis.cmake
@@ -12,7 +12,7 @@ if(ACTS_RUN_CLANG_TIDY)
         set(CLANG_TIDY_HEADER_FILTER ".*")
 
         set(CMAKE_CXX_CLANG_TIDY
-            "${CLANG_TIDY_COMMAND};-header-filter=${CLANG_TIDY_HEADER_FILTER};"
+            "${CLANG_TIDY_COMMAND};-header-filter=${CLANG_TIDY_HEADER_FILTER};-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy"
         )
     endif()
 endif()
