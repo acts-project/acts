fca8b1aa186601f20100abbaa45e46e7c5455af0
Paul Gessinger <paul.gessinger@cern.ch>
2025-02-24 11:37:56 +0100

    chore: Add pre-commit hook to test dependency consistency

---
+++ CI/check_codegen_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -u
+
+PYTHON_VERSION=3.13
+
+input=$1
+input_abs=$(realpath "$input")
+input_rel=$(basename "$input_abs")
+dir=$(dirname "$input_abs")
+output=requirements.txt
+
+pushd $dir
+uv pip compile -p $PYTHON_VERSION "$input_rel" -o "$output"
+popd
