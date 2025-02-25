eff8cee1ef2f514c478be91d0f598cfb989a2738
Paul Gessinger <paul.gessinger@cern.ch>
2025-02-25 22:11:06 +0100

    feat: Codegen as sources from CMake (#4104)

---
+++ CI/check_codegen_dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -u
+
+PYTHON_VERSION=3.13
+
+input=$1
+input_abs=$(realpath "$input")
+input_rel=$(basename "$input_abs")
+dir=$(dirname "$input_abs")
+output=requirements.txt
+
+pushd "$dir"
+uv pip compile \
+  --python-version $PYTHON_VERSION \
+  "$input_rel" \
+  -o "$output"
+popd
