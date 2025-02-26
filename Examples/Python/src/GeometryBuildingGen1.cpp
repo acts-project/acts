a22685bbd05d6bd8e8ee25185d10c1ce0789353e
Benjamin Huth <benjamin.huth@cern.ch>
2025-02-26 17:22:15 +0100

    add function to check sensitive overlaps in geometries to bindings

--- Examples/Python/src/GeometryBuildingGen1.cpp
+++ Examples/Python/src/GeometryBuildingGen1.cpp
@@ -11,19 +11,98 @@
 #include "Acts/Geometry/LayerArrayCreator.hpp"
 #include "Acts/Geometry/LayerCreator.hpp"
 #include "Acts/Geometry/SurfaceArrayCreator.hpp"
+#include "Acts/Geometry/TrackingGeometry.hpp"
 #include "Acts/Geometry/TrackingVolume.hpp"
 #include "Acts/Geometry/TrackingVolumeArrayCreator.hpp"
 #include "Acts/Geometry/VolumeBounds.hpp"
+#include "Acts/Geometry/detail/PolyhedronIntersection.hpp"
 #include "Acts/Plugins/Python/Utilities.hpp"
+#include "Acts/Surfaces/SurfaceBounds.hpp"
 
+#include <algorithm>
 #include <memory>
 #include <vector>
 
+#include <boost/container/flat_set.hpp>
+#include <boost/geometry.hpp>
+#include <boost/timer/progress_display.hpp>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
 using namespace pybind11::literals;
+
+namespace Acts::detail {
+
+namespace bg = boost::geometry;
+
+std::vector<std::array<GeometryIdentifier, 2>> checkSensitiveOverlaps(
+    std::shared_ptr<const TrackingGeometry> tg) {
+  std::vector<std::pair<Polyhedron, GeometryIdentifier>> polyhedrons;
+  tg->visitSurfaces(
+      [&](const Surface *s) {
+        auto nPoints = 0;
+        if (s->type() == Surface::Plane &&
+            s->bounds().type() == SurfaceBounds::BoundsType::eRectangle) {
+          nPoints = 4;
+        }
+        auto polyhedron = s->polyhedronRepresentation({}, nPoints);
+
+        polyhedrons.emplace_back(polyhedron, s->geometryId());
+      },
+      true);
+
+  std::vector<std::array<GeometryIdentifier, 2>> overlaps;
+
+  auto nComparisons = polyhedrons.size() * (polyhedrons.size() - 1) / 2;
+  boost::timer::progress_display show_progress(nComparisons);
+
+  auto &polygons = polyhedrons;
+  std::vector<Vector3> set;
+  std::size_t skipShared = 0;
+  for (std::size_t i = 0; i < polygons.size(); ++i) {
+    for (std::size_t j = i + 1; j < polygons.size(); ++j) {
+      const auto &p1 = polygons[i].first;
+      const auto &p2 = polygons[j].first;
+      set.clear();
+      std::ranges::copy(p1.vertices, std::back_inserter(set));
+      std::ranges::copy(p2.vertices, std::back_inserter(set));
+      std::sort(set.begin(), set.end(), [](const auto &a, const auto &b) {
+        return a.x() < b.x() || (a.x() == b.x() && a.y() < b.y()) ||
+               (a.x() == b.x() && a.y() == b.y() && a.z() < b.z());
+      });
+      set.erase(std::unique(set.begin(), set.end(),
+                            [](const auto &a, const auto &b) {
+                              return a.isApprox(b, 1e-4);
+                            }),
+                set.end());
+      if (set.size() < p1.vertices.size() + p2.vertices.size()) {
+        ++skipShared;
+      } else if (polyhedronIntersection(p2, p1, 1e-4)) {
+        overlaps.push_back({polygons[i].second, polygons[j].second});
+      }
+      ++show_progress;
+    }
+  }
+
+  std::cout << "skipped because of shared vertices: " << skipShared
+            << std::endl;
+  std::cout << "found overlaps: " << overlaps.size() << std::endl;
+  std::cout << "total checks: " << nComparisons << std::endl;
+
+  std::set<GeometryIdentifier> uniqueIds;
+  for (const auto &[a, b] : overlaps) {
+    uniqueIds.insert(a);
+    uniqueIds.insert(b);
+  }
+
+  std::cout << "involved surfaces: " << uniqueIds.size() << " / "
+            << polyhedrons.size() << std::endl;
+
+  return overlaps;
+}
+
+}  // namespace Acts::detail
 
 namespace Acts::Python {
 void addGeometryBuildingGen1(Context &ctx) {
@@ -41,19 +120,24 @@ void addGeometryBuildingGen1(Context &ctx) {
               return Acts::LayerCreator(
                   cfg, Acts::getDefaultLogger("LayerCreator", level));
             }))
-            .def("cylinderLayer",
-                 [](const Acts::LayerCreator &self, const GeometryContext &gctx,
-                    SurfacePtrVector surfaces, std::size_t binsPhi,
-                    std::size_t binsZ) {
-                   return self.cylinderLayer(gctx, std::move(surfaces), binsPhi,
-                                             binsZ);
-                 })
-            .def("discLayer", [](const Acts::LayerCreator &self,
-                                 const GeometryContext &gctx,
-                                 SurfacePtrVector surfaces, std::size_t binsR,
-                                 std::size_t binsPhi) {
-              return self.discLayer(gctx, std::move(surfaces), binsR, binsPhi);
-            });
+            .def(
+                "cylinderLayer",
+                [](const Acts::LayerCreator &self, const GeometryContext &gctx,
+                   SurfacePtrVector surfaces, std::size_t binsPhi,
+                   std::size_t binsZ) {
+                  return self.cylinderLayer(gctx, std::move(surfaces), binsPhi,
+                                            binsZ);
+                },
+                "gctx"_a, "surfaces"_a, "binsPhi"_a, "binsZ"_a)
+            .def(
+                "discLayer",
+                [](const Acts::LayerCreator &self, const GeometryContext &gctx,
+                   SurfacePtrVector surfaces, std::size_t binsR,
+                   std::size_t binsPhi) {
+                  return self.discLayer(gctx, std::move(surfaces), binsR,
+                                        binsPhi);
+                },
+                "gctx"_a, "surfaces"_a, "binsR"_a, "binsPhi"_a);
 
     auto config =
         py::class_<LayerCreator::Config>(creator, "Config").def(py::init<>());
@@ -137,6 +221,8 @@ void addGeometryBuildingGen1(Context &ctx) {
     ACTS_PYTHON_MEMBER(passiveLayerRzBins);
     ACTS_PYTHON_STRUCT_END();
   }
+
+  { m.def("checkSensitiveOverlaps", &Acts::detail::checkSensitiveOverlaps); }
 }
 
 }  // namespace Acts::Python
