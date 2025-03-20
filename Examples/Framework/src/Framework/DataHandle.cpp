de5087b034365b95f62e5d292308b7d61602abb7
Paul Gessinger <paul.gessinger@cern.ch>
2025-03-20 16:37:09 +0100

    fix compilation error

--- Examples/Framework/src/Framework/DataHandle.cpp
+++ Examples/Framework/src/Framework/DataHandle.cpp
@@ -49,8 +49,7 @@ std::string symbol(const char* in) {
   std::string s = demangleAndShorten(in);
   std::size_t pos = 0;
   while (pos + 80 < s.size()) {
--    ss << "   " + s.substr(pos, pos + 80);
-+    ss << "   " + s.substr(pos, 80);
+    ss << "   " + s.substr(pos, 80);
     pos += 80;
   }
   ss << "   " + s.substr(pos);
