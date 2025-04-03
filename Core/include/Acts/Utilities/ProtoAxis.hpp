0eb6d9210be84e9ffb537961275f311395aaaab7
Andreas Salzburger <asalzburger@gmail.com>
2025-04-03 15:11:41 +0200

    fix self declaration

--- Core/include/Acts/Utilities/ProtoAxis.hpp
+++ Core/include/Acts/Utilities/ProtoAxis.hpp
@@ -159,7 +159,7 @@ std::unique_ptr<IGrid> makeGrid(const ProtoAxis& a, const ProtoAxis& b) {
   });
 }
 
-using DirectedProtoAxis = DirectedProtoAxis;
+using DirectedProtoAxis = std::tuple<ProtoAxis, AxisDirection>;
 
 std::ostream& operator<<(std::ostream& os, const std::vector<ProtoAxis>& a);
 
