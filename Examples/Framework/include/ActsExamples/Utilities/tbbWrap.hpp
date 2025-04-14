f70920ce24d0bf6e012801549af0dd381ec02331
Paul Gessinger <paul.gessinger@cern.ch>
2025-04-14 15:56:42 +0200

    remove unused tbbwrap code

--- Examples/Framework/include/ActsExamples/Utilities/tbbWrap.hpp
+++ Examples/Framework/include/ActsExamples/Utilities/tbbWrap.hpp
@@ -41,46 +41,6 @@ static bool enableTBB(int nthreads = -99) {
   return setting;
 }
 
-/// Small wrapper for tbb::task_arena.
-/// Note that the tbbWrap::task_arena constructor is not thread-safe.
-/// That should be fine because the task_arena is initialised before spawning
-/// any threads.
-class task_arena {
-  std::optional<tbb::task_arena> tbb;
-
- public:
-  explicit task_arena(int nthreads = tbb::task_arena::automatic,
-                      unsigned res = 1) {
-    if (enableTBB(nthreads)) {
-      tbb.emplace(nthreads, res);
-    }
-  }
-
-  template <typename F>
-  void execute(const F& f) {
-    if (tbb) {
-      tbb->execute(f);
-    } else {
-      f();
-    }
-  }
-};
-
-/// Small wrapper for tbb::parallel_for.
-class parallel_for {
- public:
-  template <typename R, typename F>
-  parallel_for(const R& r, const F& f) {
-    if (enableTBB()) {
-      tbb::parallel_for(r, f);
-    } else {
-      for (auto i = r.begin(); i != r.end(); ++i) {  // use default grainsize=1
-        f(R(i, i + 1));
-      }
-    }
-  }
-};
-
 /// Small wrapper for tbb::queuing_mutex and tbb::queuing_mutex::scoped_lock.
 class queuing_mutex {
   std::optional<tbb::queuing_mutex> tbb;
