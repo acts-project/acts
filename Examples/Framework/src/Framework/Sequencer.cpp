bcf12c954047be5215a4dae9874b50c9f0b5bca8
Paul Gessinger <paul.gessinger@cern.ch>
2025-04-25 10:45:38 +0200

    add threadId to algo context

--- Examples/Framework/src/Framework/Sequencer.cpp
+++ Examples/Framework/src/Framework/Sequencer.cpp
@@ -36,6 +36,7 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <thread>
 #include <typeinfo>
 
 #include <TROOT.h>
@@ -391,7 +392,7 @@ int Sequencer::run() {
             ACTS_VERBOSE("Thread about to pick next event");
 
             for (const auto& writer : m_writers) {
-              if (writer->beginEvent() != ProcessCode::SUCCESS) {
+              if (writer->beginEvent(threadId) != ProcessCode::SUCCESS) {
                 throw std::runtime_error("Failed to process event data");
               }
             }
@@ -408,7 +409,7 @@ int Sequencer::run() {
                 m_whiteboardObjectAliases);
             // If we ever wanted to run algorithms in parallel, this needs to
             // be changed to Algorithm context copies
-            AlgorithmContext context(0, event, eventStore);
+            AlgorithmContext context(0, event, eventStore, threadId);
             std::size_t ialgo = 0;
 
             /// Decorate the context
