4d0209aeab17bc6a2389c55f13d6c5a62dec3d69
Paul Gessinger <paul.gessinger@cern.ch>
2025-04-25 10:47:21 +0200

    test: Label typo fix

--- Examples/Python/tests/conftest.py
+++ Examples/Python/tests/conftest.py
@@ -152,7 +152,7 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
             terminalreporter.line(f"{e.key}: {e.act_hash}")
 
     if not helpers.doHashChecks:
-        terminalreporter.section("Root file has checks", sep="-", blue=True, bold=True)
+        terminalreporter.section("Root file hash checks", sep="-", blue=True, bold=True)
         terminalreporter.line(
             "NOTE: Root file hash checks were skipped, enable with ROOT_HASH_CHECKS=on"
         )
