ce50b0c22b5bb36c8a7581642dc209f6c0c03886
Andreas Salzburger <asalzburger@gmail.com>
2025-04-29 16:23:47 +0200

    update nlohmann version

--- cmake/ActsExternSources.cmake
+++ cmake/ActsExternSources.cmake
@@ -48,7 +48,7 @@ set(ACTS_FRNN_SOURCE
 mark_as_advanced(ACTS_FRNN_SOURCE)
 
 set(ACTS_NLOHMANNJSON_SOURCE
-    "URL;https://github.com/nlohmann/json/archive/refs/tags/v${_acts_nlohmanjson_version}.tar.gz;URL_HASH;SHA256=5daca6ca216495edf89d167f808d1d03c4a4d929cef7da5e10f135ae1540c7e4"
+    "URL;https://github.com/nlohmann/json/archive/refs/tags/v${_acts_nlohmanjson_version}.tar.gz;URL_HASH;SHA256=4b92eb0c06d10683f7447ce9406cb97cd4b453be18d7279320f7b2f025c10187"
     CACHE STRING
     "Source to take nlohmann_json from"
 )
