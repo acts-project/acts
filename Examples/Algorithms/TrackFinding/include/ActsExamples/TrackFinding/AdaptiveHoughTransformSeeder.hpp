a4d23e6bfdbb71d44995af9e69120842f13af01b
tbold <tomasz.bold@cern.ch>
2025-05-05 17:29:51 +0200

    Linter fixes

--- Examples/Algorithms/TrackFinding/include/ActsExamples/TrackFinding/AdaptiveHoughTransformSeeder.hpp
+++ Examples/Algorithms/TrackFinding/include/ActsExamples/TrackFinding/AdaptiveHoughTransformSeeder.hpp
@@ -13,12 +13,12 @@
 
 #pragma once
 
+#include "Acts/EventData/SourceLink.hpp"
 #include "Acts/Geometry/GeometryIdentifier.hpp"
 #include "Acts/Utilities/Delegate.hpp"
 #include "Acts/Utilities/Grid.hpp"
 #include "Acts/Utilities/Logger.hpp"
 #include "Acts/Utilities/Result.hpp"
-#include "Acts/EventData/SourceLink.hpp"
 #include "ActsExamples/EventData/Index.hpp"
 #include "ActsExamples/EventData/Measurement.hpp"
 #include "ActsExamples/EventData/ProtoTrack.hpp"
@@ -39,20 +39,20 @@
 
 namespace ActsExamples {
 struct AlgorithmContext;
-}  // namespace ActsExamples
+} // namespace ActsExamples
 
 using ResultDouble = Acts::Result<double>;
 using ResultBool = Acts::Result<bool>;
 using ResultUnsigned = Acts::Result<unsigned>;
 
 using FieldCorrector = Acts::Delegate<ResultDouble(
-    unsigned, double, double)>;  // (unsigned region, double y, double r)
+    unsigned, double, double)>; // (unsigned region, double y, double r)
 using LayerIDFinder = Acts::Delegate<ResultUnsigned(
-    double)>;  // (double r) this function will map the r of a measurement to a
-               // layer.
+    double)>; // (double r) this function will map the r of a measurement to a
+              // layer.
 using SliceTester = Acts::Delegate<ResultBool(
-    double, unsigned, int)>;  // (double z,unsigned layer, int slice) returns
-                              // true if measurement in slice
+    double, unsigned, int)>; // (double z,unsigned layer, int slice) returns
+                             // true if measurement in slice
 
 namespace Acts {
 class TrackingGeometry;
@@ -61,15 +61,15 @@ class TrackingGeometry;
 namespace ActsExamples {
 // Helper class describing one section of the accumulator space
 class AccumulatorSection {
- public:
+public:
   AccumulatorSection() = default;
 
   AccumulatorSection(double xw, double yw, double xBegin, double yBegin,
-                     int div = 0, const std::vector<unsigned>& indices = {});
+                     int div = 0, const std::vector<unsigned> &indices = {});
 
   inline unsigned count() const { return m_indices.size(); }
-  inline const std::vector<unsigned>& indices() const { return m_indices; }
-  inline std::vector<unsigned>& indices() { return m_indices; }
+  inline const std::vector<unsigned> &indices() const { return m_indices; }
+  inline std::vector<unsigned> &indices() { return m_indices; }
 
   // create section that is bottom part this this one
   // +------+
@@ -123,20 +123,20 @@ class AccumulatorSection {
   double xBegin() const { return m_xBegin; }
   double yBegin() const { return m_yBegin; }
 
- private:
+private:
   double m_xSize;
   double m_ySize;
   double m_xBegin;
   double m_yBegin;
   unsigned m_divisionLevel =
-      0;  // number of times the starting section was already divided
+      0; // number of times the starting section was already divided
   std::vector<unsigned>
-      m_indices;  // indices of measurements contributing to this section
+      m_indices; // indices of measurements contributing to this section
 };
 
 // Construct track seeds from space points.
 class AdaptiveHoughTransformSeeder final : public IAlgorithm {
- public:
+public:
   struct Config {
     /// Input space point collections.
     ///
@@ -153,31 +153,31 @@ class AdaptiveHoughTransformSeeder final : public IAlgorithm {
     /// Tracking geometry required to access global-to-local transforms.
     std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry;
 
-    float qOverPtMin = 1.0;  // min q/pt, -1/1 GeV
+    float qOverPtMin = 1.0; // min q/pt, -1/1 GeV
 
-    float qOverPtMinBinSize = 0.01;  // minimal size of pT bin that the
-                                     // algorithm should not go beyond (in GeV)
-    float phiMinBinSize = 0.01;  // minimal size of phi bin that the algorithm
-                                 // should not go beyond
+    float qOverPtMinBinSize = 0.01; // minimal size of pT bin that the
+                                    // algorithm should not go beyond (in GeV)
+    float phiMinBinSize = 0.01; // minimal size of phi bin that the algorithm
+                                // should not go beyond
 
     unsigned threshold =
-        8;  // number of lines passing section for it to be still considered
+        8; // number of lines passing section for it to be still considered
 
-    bool deduplicate = true;  // when adding solutions try avoiding duplicates
+    bool deduplicate = true; // when adding solutions try avoiding duplicates
 
     bool requireIntersections =
-        true;  // require that lines passing section need to cross inside
-               // the count is required to be at lease threshold*(threshold-1):
+        true; // require that lines passing section need to cross inside
+              // the count is required to be at lease threshold*(threshold-1):
     unsigned intersectionsThreshold =
         threshold * (threshold - 1) /
-        2;  // the number of lines in section should be at most this to enable
-            // intersection test
+        2; // the number of lines in section should be at most this to enable
+           // intersection test
 
     double inverseA =
-        1.0 / 3.0e-4;  // Assume B = 2T constant. Can apply corrections to
-                       // this with fieldCorrection function
-                       // This 3e-4 comes from the 2T field when converted to
-                       // units of GeV / (c*mm*e)
+        1.0 / 3.0e-4; // Assume B = 2T constant. Can apply corrections to
+                      // this with fieldCorrection function
+                      // This 3e-4 comes from the 2T field when converted to
+                      // units of GeV / (c*mm*e)
 
     // it's up to the user to connect these to the functions they want to use
     FieldCorrector fieldCorrector;
@@ -197,7 +197,7 @@ class AdaptiveHoughTransformSeeder final : public IAlgorithm {
     double invr;
     double phi;
     Acts::SourceLink link;
-    };
+  };
 
   /// Construct the seeding algorithm.
   ///
@@ -209,15 +209,15 @@ class AdaptiveHoughTransformSeeder final : public IAlgorithm {
   ///
   /// @param txt is the algorithm context with event information
   /// @return a process code indication success or failure
-  ProcessCode execute(const AlgorithmContext& ctx) const final;
+  ProcessCode execute(const AlgorithmContext &ctx) const final;
 
   /// Const access to the config
-  const Config& config() const { return m_cfg; }
+  const Config &config() const { return m_cfg; }
 
- private:
+private:
   Config m_cfg;
   std::unique_ptr<const Acts::Logger> m_logger;
-  const Acts::Logger& logger() const { return *m_logger; }
+  const Acts::Logger &logger() const { return *m_logger; }
 
   WriteDataHandle<ProtoTrackContainer> m_outputProtoTracks{this,
                                                            "OutputProtoTracks"};
@@ -236,32 +236,35 @@ class AdaptiveHoughTransformSeeder final : public IAlgorithm {
   /// @param solutions is the output set of sections
   /// @param measurements are input measurements
   void processStackHead(
-      std::stack<AccumulatorSection>& sections,
-      std::vector<AccumulatorSection>& solutions,
-      const std::vector<PreprocessedMeasurement>& measurements) const;
+      std::stack<AccumulatorSection> &sections,
+      std::vector<AccumulatorSection> &solutions,
+      const std::vector<PreprocessedMeasurement> &measurements) const;
 
   /// @brief assign measurements to the section
-  /// @warning the sections needs to have already indices of measurements to consider
+  /// @warning the sections needs to have already indices of measurements to
+  /// consider
   /// @warning from previous iteration
   /// @param sections section to be processed
-  /// @param vector of measurements - indices in the section need to point to this vector
-  void updateSection(AccumulatorSection& section,
-                     const std::vector<PreprocessedMeasurement>& input) const;
+  /// @param vector of measurements - indices in the section need to point to
+  /// this vector
+  void updateSection(AccumulatorSection &section,
+                     const std::vector<PreprocessedMeasurement> &input) const;
 
   /// @brief check if lines intersect in the section
   /// modifies the section leaving only indices of measurements that do so
   /// @param section - the section to check
-  /// @param measurements - the measurements that are pointed to by indices in section
+  /// @param measurements - the measurements that are pointed to by indices in
+  /// section
   bool passIntersectionsCheck(
-      const AccumulatorSection& section,
-      const std::vector<PreprocessedMeasurement>& measurements) const;
+      const AccumulatorSection &section,
+      const std::vector<PreprocessedMeasurement> &measurements) const;
 
   /// @brief  add solution to the solutions vector
   /// depending on options it may eliminate trivial duplicates
   /// @param s - the solution to be potentially added
   /// @param solutions - the output solutions set
-  void addSolution(AccumulatorSection&& s,
-                   std::vector<AccumulatorSection>& output) const;
+  void addSolution(AccumulatorSection &&s,
+                   std::vector<AccumulatorSection> &output) const;
 };
 
-}  // namespace ActsExamples
+} // namespace ActsExamples
