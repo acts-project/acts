5865d72250b066079c8a60758152ca5da4cb313a
tbold <tomasz.bold@cern.ch>
2025-05-06 05:58:25 +0200

    Lint

--- Examples/Algorithms/TrackFinding/src/AdaptiveHoughTransformSeeder.cpp
+++ Examples/Algorithms/TrackFinding/src/AdaptiveHoughTransformSeeder.cpp
@@ -144,7 +144,7 @@ ProcessCode AdaptiveHoughTransformSeeder::execute(
             std::end(sectionsStack.top().indices()), 0);
 
   std::vector<AccumulatorSection> solutions;
-  while (! sectionsStack.empty()) {
+  while (!sectionsStack.empty()) {
     ACTS_VERBOSE("Processing AccumulatorSection "
                  << sectionsStack.top().xBegin() << " "
                  << sectionsStack.top().xSize() << " "
