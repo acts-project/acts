9ad97e54e5c5812371965e0819ab86c9de1f348b
Paul Gessinger <paul.gessinger@cern.ch>
2025-05-15 13:26:18 -0700

    add odd construction test + add gen3 flag to odd

--- Examples/Python/tests/test_geometry.py
+++ Examples/Python/tests/test_geometry.py
@@ -72,10 +72,8 @@ def test_geometry_example(detectorFactory, aligned, nobj, tmp_path):
     runGeometry(outputJson=False, **kwargs)
 
     assert len(list(obj_dir.iterdir())) == nobj
-    assert all(f.stat().st_size > 200 for f in obj_dir.iterdir())
 
     assert len(list(csv_dir.iterdir())) == 3 * events
-    assert all(f.stat().st_size > 200 for f in csv_dir.iterdir())
 
     detector_files = [csv_dir / f"event{i:>09}-detectors.csv" for i in range(events)]
     for detector_file in detector_files:
@@ -101,31 +99,70 @@ def test_geometry_example(detectorFactory, aligned, nobj, tmp_path):
         assert material_file.stat().st_size > 200
 
 
+class CountingVisitor(acts.TrackingGeometryMutableVisitor):
+    def __init__(self):
+        super().__init__()
+        self.num_surfaces = 0
+        self.num_layers = 0
+        self.num_volumes = 0
+        self.num_portals = 0
+        self.num_boundary_surfaces = 0
+
+    def visitSurface(self, surface: acts.Surface):
+        self.num_surfaces += 1
+
+    def visitLayer(self, layer: acts.Layer):
+        self.num_layers += 1
+
+    def visitVolume(self, volume: acts.Volume):
+        self.num_volumes += 1
+
+    def visitPortal(self, portal: acts.Portal):
+        self.num_portals += 1
+
+    def visitBoundarySurface(self, boundary: acts.BoundarySurfaceT_TrackingVolume):
+        self.num_boundary_surfaces += 1
+
+
 def test_geometry_visitor(trk_geo):
-    class Visitor(acts.TrackingGeometryMutableVisitor):
-        def __init__(self):
-            super().__init__()
-            self.num_surfaces = 0
-            self.num_layers = 0
-            self.num_volumes = 0
-            self.num_portals = 0
-
-        def visitSurface(self, surface: acts.Surface):
-            self.num_surfaces += 1
-
-        def visitLayer(self, layer: acts.Layer):
-            self.num_layers += 1
-
-        def visitVolume(self, volume: acts.Volume):
-            self.num_volumes += 1
-
-        def visitPortal(self, portal: acts.Portal):
-            self.num_portals += 1
-
-    visitor = Visitor()
+    visitor = CountingVisitor()
     trk_geo.apply(visitor)
 
     assert visitor.num_surfaces == 19078
     assert visitor.num_layers == 111
     assert visitor.num_volumes == 18
     assert visitor.num_portals == 0
+    assert visitor.num_boundary_surfaces == 67
+
+
+@pytest.mark.skipif(not dd4hepEnabled, reason="DD4hep not set up")
+@pytest.mark.odd
+def test_odd_gen1():
+    with getOpenDataDetector(gen3=False) as detector:
+        trackingGeometry = detector.trackingGeometry()
+
+        visitor = CountingVisitor()
+        trackingGeometry.apply(visitor)
+
+        assert visitor.num_surfaces == 19264
+        assert visitor.num_layers == 142
+        assert visitor.num_volumes == 32
+        assert visitor.num_portals == 0  # Gen1: will have no portals
+        assert visitor.num_boundary_surfaces == 126  # Gen1: will have boundary surfaces
+
+
+@pytest.mark.skipif(not dd4hepEnabled, reason="DD4hep not set up")
+@pytest.mark.odd
+@pytest.mark.xfail(reason="Gen3 building for ODD not yet implemented")
+def test_odd_gen3():
+    with getOpenDataDetector(gen3=True) as detector:
+        trackingGeometry = detector.trackingGeometry()
+
+        visitor = CountingVisitor()
+        trackingGeometry.apply(visitor)
+
+        assert visitor.num_surfaces == 19264
+        assert visitor.num_layers == 0  # Gen3: no layers
+        assert visitor.num_volumes == 32
+        assert visitor.num_portals == 123  # Gen3: will have portals
+        assert visitor.num_boundary_surfaces == 0  # Gen3: no boundary surfaces
