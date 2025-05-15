58717150aebf470606a5e21ab303bd47d9ea2690
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2025-05-15 15:51:05 +0200

    Update Core/include/Acts/Geometry/LayerCreator.hpp
    Co-authored-by: Andreas Stefl <stefl.andreas@gmail.com>

--- Core/include/Acts/Geometry/LayerCreator.hpp
+++ Core/include/Acts/Geometry/LayerCreator.hpp
@@ -65,10 +65,6 @@ class LayerCreator {
                         std::unique_ptr<const Logger> logger =
                             getDefaultLogger("LayerCreator", Logging::INFO));
 
-  LayerCreator(LayerCreator&&) = default;
-
-  /// Destructor
-  ~LayerCreator() = default;
 
   /// returning a cylindrical layer
   ///
