5ece1b7b5d73504940291aa9d2394d80e95a19d3
Paul Gessinger <paul.gessinger@cern.ch>
2025-05-19 13:40:08 +0200

    feat(detray): Add partial payload converter, test conversions

---
+++ Plugins/Detray/src/DetrayPayloadConverter.cpp
@@ -0,0 +1,209 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "Acts/Plugins/Detray/DetrayPayloadConverter.hpp"
+
+#include "Acts/Definitions/Algebra.hpp"
+#include "Acts/Geometry/GeometryContext.hpp"
+#include "Acts/Geometry/TrackingGeometry.hpp"
+#include "Acts/Surfaces/AnnulusBounds.hpp"
+#include "Acts/Surfaces/CylinderBounds.hpp"
+#include "Acts/Surfaces/RadialBounds.hpp"
+#include "Acts/Surfaces/RectangleBounds.hpp"
+#include "Acts/Surfaces/Surface.hpp"
+#include "Acts/Surfaces/SurfaceBounds.hpp"
+#include "Acts/Surfaces/TrapezoidBounds.hpp"
+
+#include <detray/geometry/shapes/annulus2D.hpp>
+#include <detray/geometry/shapes/concentric_cylinder2D.hpp>
+#include <detray/geometry/shapes/cylinder2D.hpp>
+#include <detray/geometry/shapes/rectangle2D.hpp>
+#include <detray/geometry/shapes/ring2D.hpp>
+#include <detray/geometry/shapes/trapezoid2D.hpp>
+#include <detray/io/frontend/definitions.hpp>
+#include <detray/io/frontend/payloads.hpp>
+
+namespace Acts {
+
+detray::io::detector_payload DetrayPayloadConverter::convert(
+    const GeometryContext& gctx, const TrackingGeometry& trackingGeometry) {
+  DetrayPayloadConverter converter(gctx, trackingGeometry);
+  converter.convertPayload();
+  return std::move(*converter.m_detectorPayload);
+}
+
+DetrayPayloadConverter::DetrayPayloadConverter(
+    const GeometryContext& /*gctx*/,
+    const TrackingGeometry& /*trackingGeometry*/) {}
+
+DetrayPayloadConverter::~DetrayPayloadConverter() = default;
+
+void DetrayPayloadConverter::convertPayload() {}
+
+detray::io::transform_payload DetrayPayloadConverter::convertTransform(
+    const Transform3& transform) {
+  detray::io::transform_payload tfPayload;
+
+  Eigen::Map<Vector3> tr{tfPayload.tr.data()};
+  tr = transform.translation();
+
+  Eigen::Map<SquareMatrix3> rot{tfPayload.rot.data()};
+  rot = transform.linear();
+
+  return tfPayload;
+}
+
+namespace {
+using enum detray::io::shape_id;
+
+detray::io::mask_payload convertBounds(const AnnulusBounds& annulus) {
+  using enum detray::annulus2D::boundaries;
+  using enum AnnulusBounds::BoundValues;
+
+  detray::io::mask_payload payload;
+  payload.shape = annulus2;
+  payload.boundaries.resize(e_size);
+  payload.boundaries.at(e_min_r) = annulus.get(eMinR);
+  payload.boundaries.at(e_max_r) = annulus.get(eMaxR);
+  payload.boundaries.at(e_min_phi_rel) = annulus.get(eMinPhiRel);
+  payload.boundaries.at(e_max_phi_rel) = annulus.get(eMaxPhiRel);
+  payload.boundaries.at(e_average_phi) = annulus.get(eAveragePhi);
+  payload.boundaries.at(e_shift_x) = annulus.get(eOriginX);
+  payload.boundaries.at(e_shift_y) = annulus.get(eOriginY);
+
+  return payload;
+}
+
+detray::io::mask_payload convertBounds(const RectangleBounds& rectangle) {
+  using enum RectangleBounds::BoundValues;
+  using enum detray::rectangle2D::boundaries;
+
+  detray::io::mask_payload payload;
+  payload.shape = rectangle2;
+  payload.boundaries.resize(e_size);
+
+  double minX = rectangle.get(eMinX);
+  double maxX = rectangle.get(eMaxX);
+  double minY = rectangle.get(eMinY);
+  double maxY = rectangle.get(eMaxY);
+
+  if (minX != -maxX || minY != -maxY) {
+    throw std::runtime_error(
+        "Rectangle bounds are not symmetric, detray cannot handle this");
+  }
+
+  payload.boundaries.at(e_half_x) = maxX;
+  payload.boundaries.at(e_half_y) = maxY;
+
+  return payload;
+}
+
+detray::io::mask_payload convertBounds(const CylinderBounds& cylinder,
+                                       detray::io::shape_id shape) {
+  using enum CylinderBounds::BoundValues;
+
+  detray::io::mask_payload payload;
+  payload.shape = shape;
+  if (shape == portal_cylinder2) {
+    using enum detray::concentric_cylinder2D::boundaries;
+    payload.boundaries.resize(e_size);
+    payload.boundaries.at(e_r) = cylinder.get(eR);
+    double hlZ = cylinder.get(eHalfLengthZ);
+    payload.boundaries.at(e_lower_z) = -hlZ;
+    payload.boundaries.at(e_upper_z) = hlZ;
+  } else {
+    using enum detray::cylinder2D::boundaries;
+    payload.boundaries.resize(e_size);
+    payload.boundaries.at(e_r) = cylinder.get(eR);
+    double hlZ = cylinder.get(eHalfLengthZ);
+    payload.boundaries.at(e_lower_z) = -hlZ;
+    payload.boundaries.at(e_upper_z) = hlZ;
+  }
+  return payload;
+}
+
+detray::io::mask_payload convertBounds(const TrapezoidBounds& trapezoid) {
+  using enum TrapezoidBounds::BoundValues;
+  using enum detray::trapezoid2D::boundaries;
+
+  detray::io::mask_payload payload;
+  payload.shape = trapezoid2;
+
+  payload.boundaries.resize(e_size);
+  payload.boundaries.at(e_half_length_0) = trapezoid.get(eHalfLengthXnegY);
+  payload.boundaries.at(e_half_length_1) = trapezoid.get(eHalfLengthXposY);
+  payload.boundaries.at(e_half_length_2) = trapezoid.get(eHalfLengthY);
+  payload.boundaries.at(e_divisor) = 1 / (2 * trapezoid.get(eHalfLengthY));
+
+  return payload;
+}
+
+detray::io::mask_payload convertBounds(const RadialBounds& radial) {
+  using enum RadialBounds::BoundValues;
+  using enum detray::ring2D::boundaries;
+
+  if (!radial.coversFullAzimuth()) {
+    throw std::runtime_error(
+        "Radial bounds do not cover full azimuth, detray cannot handle this");
+  }
+
+  if (radial.get(eAveragePhi) != 0.) {
+    throw std::runtime_error(
+        "Radial bounds have an average phi, detray cannot handle this");
+  }
+
+  detray::io::mask_payload payload;
+  payload.shape = ring2;
+
+  payload.boundaries.resize(e_size);
+  payload.boundaries.at(e_inner_r) = radial.get(eMinR);
+  payload.boundaries.at(e_outer_r) = radial.get(eMaxR);
+
+  return payload;
+}
+
+}  // namespace
+
+detray::io::mask_payload DetrayPayloadConverter::convertMask(
+    const SurfaceBounds& bounds, bool forPortal) {
+  detray::io::mask_payload payload;
+
+  switch (bounds.type()) {
+    using enum SurfaceBounds::BoundsType;
+    using enum detray::io::shape_id;
+    case eAnnulus:
+      payload = convertBounds(dynamic_cast<const AnnulusBounds&>(bounds));
+      break;
+    case eRectangle:
+      payload = convertBounds(dynamic_cast<const RectangleBounds&>(bounds));
+      break;
+    case eCylinder:
+      payload = convertBounds(dynamic_cast<const CylinderBounds&>(bounds),
+                              forPortal ? portal_cylinder2 : cylinder2);
+      break;
+    case eTrapezoid:
+      payload = convertBounds(dynamic_cast<const TrapezoidBounds&>(bounds));
+      break;
+    case eDisc:
+      if (auto* radial = dynamic_cast<const RadialBounds*>(&bounds);
+          radial != nullptr) {
+        payload = convertBounds(*radial);
+      } else {
+        throw std::runtime_error(
+            "Disc bounds type but not radial bounds currently unsupported");
+      }
+      break;
+    default:
+      payload.shape = unknown;
+      break;
+  }
+
+  return payload;
+}
+
+}  // namespace Acts
