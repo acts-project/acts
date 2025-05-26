deb8c286b8d93d7f15160324018865d31a2e582a
Doga Elitez <108287101+delitez@users.noreply.github.com>
2025-05-26 09:47:03 +0200

    Apply pre-commit corrections

--- Examples/Python/python/acts/examples/simulation.py
+++ Examples/Python/python/acts/examples/simulation.py
@@ -858,7 +858,7 @@ def addTruthJetAlg(
     loglevel: Optional[acts.logging.Level] = None,
 ) -> None:
     from acts.examples import TruthJetAlgorithm
-    
+
     customLogLevel = acts.examples.defaultLogging(s, loglevel)
     truthJetAlg = acts.examples.TruthJetAlgorithm(
         **acts.examples.defaultKWArgs(**_getTruthJetKWargs(config)),
