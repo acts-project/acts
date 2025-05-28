3f7f889c0fe99489d0ef445ead83b4d6a86a541e
Andreas Salzburger <asalzburger@gmail.com>
2025-05-28 13:55:37 +0200

    remove Contextual Detector

--- Examples/Python/tests/test_detectors.py
+++ Examples/Python/tests/test_detectors.py
@@ -67,20 +67,6 @@ def test_odd():
         assert count_surfaces(trackingGeometry) == 18824
 
 
-def test_aligned_detector():
-    detector = acts.examples.AlignedDetector()
-    trackingGeometry = detector.trackingGeometry()
-    decorators = detector.contextDecorators()
-
-    assert detector is not None
-    assert trackingGeometry is not None
-    assert decorators is not None
-
-    assert count_surfaces(trackingGeometry) == 18728
-
-
-import itertools
-
 
 def test_tgeo_config_triplet(monkeypatch):
     from acts.examples import TGeoDetector, Interval
