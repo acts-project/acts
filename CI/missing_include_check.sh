854cec8e8fd3434635a547072b27d3f73dba8691
Benjamin Huth <benjamin.huth@cern.ch>
2025-05-30 17:31:46 +0200

    do it

--- CI/missing_include_check.sh
+++ CI/missing_include_check.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-RET=0
-ERRORS=0
+set -e
 
-FILES=$(find  Core/include/ -name "*.hpp" | grep -v "/detail/")
-N_FILES=$(echo "$FILES" | wc -l)
-echo "Check $N_FILES files"
-
-ITER=0
-
-for file in $(find  Core/include/ -name "*.hpp" | grep -v "/detail/"); do
-    ITER=$((ITER+1))
-    echo "$(date +%H:%M:%S)    $((100*ITER/N_FILES))%   check $file"
+function check_includes {
+    file=$1
     out=$(printf "#include <${file:13}>\nint main() { return 0; }" | clang++ -std=c++20 -O0 -c -I "Core/include" -I "/usr/include/eigen3" -x c++ - 2>&1)
     if [[ "$?" -ne "0" ]]; then
-        echo "------------------------------------"
+        echo "$(date +%H:%M:%S)    Failed: $file"
         echo "$out"
         echo "------------------------------------"
-        RET=1
-        ERRORS=$((ERRORS+1))
+    else
+      # echo "$(date +%H:%M:%S)    Success: $file"
+      :
     fi
-done
+}
 
-echo "Total errors: $ERRORS"
-exit $RET
+export -f check_includes
+
+find Core/include/ -name "*.hpp" | grep -v "/detail/" | parallel check_includes
+find Core/include/ -name "*.ipp" | parallel check_includes
