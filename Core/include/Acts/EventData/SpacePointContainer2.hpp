1ea8c23514acdd88afd71e492eddf0c50fa379a6
Andreas Stefl <stefl.andreas@gmail.com>
2025-06-27 19:58:36 +0200

    pr feedback

--- Core/include/Acts/EventData/SpacePointContainer2.hpp
+++ Core/include/Acts/EventData/SpacePointContainer2.hpp
@@ -673,15 +673,15 @@ class SpacePointContainer2 {
     explicit ExtraColumnHolder(ValueType defaultValue = {})
         : m_default(std::move(defaultValue)) {}
 
-    std::unique_ptr<ColumnHolderBase> copy() const final {
+    std::unique_ptr<ColumnHolderBase> copy() const override {
       return std::make_unique<ExtraColumnHolder<T>>(*this);
     }
     ProxyType proxy() const { return ProxyType(m_data); }
 
-    void reserve(std::size_t size) final { m_data.reserve(size); }
-    void clear() final { m_data.clear(); }
-    void resize(std::size_t size) final { m_data.resize(size, m_default); }
-    void emplace_back() final { m_data.emplace_back(m_default); }
+    void reserve(std::size_t size) override { m_data.reserve(size); }
+    void clear() override { m_data.clear(); }
+    void resize(std::size_t size) override { m_data.resize(size, m_default); }
+    void emplace_back() override { m_data.emplace_back(m_default); }
 
    private:
     ValueType m_default;
