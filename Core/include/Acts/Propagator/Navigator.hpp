f08b07e4621d9849c71e18e270205590fd73ee2a
Andreas Stefl <stefl.andreas@gmail.com>
2025-07-07 21:37:58 +0200

    add logging

--- Core/include/Acts/Propagator/Navigator.hpp
+++ Core/include/Acts/Propagator/Navigator.hpp
@@ -718,6 +718,12 @@ class Navigator {
             return std::abs(a.pathLength() - b.pathLength()) <
                    state.options.surfaceTolerance;
           });
+      if (toBeRemoved.begin() != toBeRemoved.end()) {
+        ACTS_VERBOSE(volInfo(state)
+                     << "Removing "
+                     << std::distance(toBeRemoved.begin(), toBeRemoved.end())
+                     << " overlapping surfaces.");
+      }
       state.navSurfaces.erase(toBeRemoved.begin(), toBeRemoved.end());
     } else {
       // @TODO: What to do with external surfaces?
