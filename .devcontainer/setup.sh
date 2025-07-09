1cca959ef9393c90c2c764ed36728d9c4dd2d456
Paul Gessinger <paul.gessinger@cern.ch>
2025-07-09 17:41:48 +0200

    chore: Add devcontainer configuration

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+WORKSPACE=$1
+
+git config --global --add safe.directory $WORKSPACE
+
+pushd $WORKSPACE
+pre-commit install
+popd
+
+echo "export WORKSPACE=$WORKSPACE" >> $HOME/.bashrc
+
+cat >> $HOME/.bashrc <<EOF
+function configure_acts() {
+    echo "+ cmake -S $WORKSPACE -B $WORKSPACE/build_devcontainer --preset github-ci"
+    cmake -S $WORKSPACE -B $WORKSPACE/build_devcontainer --preset github-ci
+}
+export configure_acts
+
+function build_acts() {
+    echo "+ cmake --build $WORKSPACE/build_devcontainer"
+    cmake --build $WORKSPACE/build_devcontainer
+}
+export build_acts
+EOF
+
+
+cat > /etc/motd <<EOF
+=============== ACTS dev container with dependencies ===============
+- Configure:
+    configure_acts
+- Build:
+    build_acts
+  OR
+    cmake --build build_devcontainer
+- Run:
+    source build_devcontainer/this_acts_withdeps.sh
+    acts/Examples/Scripts/Python/full_chain_odd.py -n1
+EOF
