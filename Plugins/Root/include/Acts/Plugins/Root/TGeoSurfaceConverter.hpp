6d47103ba6ff132f2d81b26683b2717e931af59e
Pavel Larionov <pavel.larionov@cern.ch>
2025-07-15 12:24:26 +0200

    Make use of UnitConstants and Helpers

--- Plugins/Root/include/Acts/Plugins/Root/TGeoSurfaceConverter.hpp
+++ Plugins/Root/include/Acts/Plugins/Root/TGeoSurfaceConverter.hpp
@@ -9,6 +9,8 @@
 #pragma once
 
 #include "Acts/Definitions/Algebra.hpp"
+#include "Acts/Definitions/Units.hpp"
+#include "Acts/Utilities/detail/periodic.hpp"
 
 #include <cmath>
 #include <memory>
@@ -93,13 +95,7 @@ struct TGeoSurfaceConverter {
   /// @param degree The input in degree
   /// @return angle in radians
   static double toRadian(double degree) {
-    // Normalize degree to [-180, 180)
-    degree = fmod(degree + 180.0, 360.0);
-    if (degree < 0)
-      degree += 360.0;
-    degree -= 180.0;
-
-    return degree / 180.0 * std::numbers::pi;
+    return detail::radian_sym(degree * UnitConstants::degree);
   }
 };
 
