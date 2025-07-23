d1063bf118861131644f2d84caee8bbe6fae7c33
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2025-07-23 11:50:11 +0200

    Conflict

--- Examples/Algorithms/Digitization/include/ActsExamples/Digitization/MuonSpacePointDigitizer.hpp
+++ Examples/Algorithms/Digitization/include/ActsExamples/Digitization/MuonSpacePointDigitizer.hpp
@@ -8,59 +8,54 @@
 
 #pragma once
 
-
+#include "Acts/Geometry/TrackingGeometry.hpp"
 #include "Acts/Utilities/Logger.hpp"
-
+#include "ActsExamples/EventData/MuonSpacePoint.hpp"
 #include "ActsExamples/EventData/SimHit.hpp"
 #include "ActsExamples/EventData/SimParticle.hpp"
-#include "ActsExamples/EventData/MuonSpacePoint.hpp"
 #include "ActsExamples/Framework/DataHandle.hpp"
 #include "ActsExamples/Framework/IAlgorithm.hpp"
 #include "ActsExamples/Framework/ProcessCode.hpp"
 #include "ActsExamples/Framework/RandomNumbers.hpp"
-
-#include "Acts/Geometry/TrackingGeometry.hpp"
-
 
 namespace ActsExamples {
 
 /// Algorithm that turns simulated hits into measurements by truth smearing.
 class MuonSpacePointDigitizer final : public IAlgorithm {
  public:
-    struct Config{
-      /// @brief Pointer to the tracking geometry to fetch the surfaces
-      std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry{};
-      /// @brief Name of the input simulated hits collection
-      std::string inputSimHits{"simHits"};
-      /// @brief Name of the input simulated particles collection
-      std::string inputParticles{"particles_simulated"};
-      /// @brief Name of the output space points collection
-      std::string outputSpacePoints{"space_points"};
-      /// @brief Random number generator service
-      std::shared_ptr<const RandomNumbers> randomNumbers{};
+  struct Config {
+    /// @brief Pointer to the tracking geometry to fetch the surfaces
+    std::shared_ptr<const Acts::TrackingGeometry> trackingGeometry{};
+    /// @brief Name of the input simulated hits collection
+    std::string inputSimHits{"simHits"};
+    /// @brief Name of the input simulated particles collection
+    std::string inputParticles{"particles_simulated"};
+    /// @brief Name of the output space points collection
+    std::string outputSpacePoints{"space_points"};
+    /// @brief Random number generator service
+    std::shared_ptr<const RandomNumbers> randomNumbers{};
+  };
+  /// @brief Constructor
+  MuonSpacePointDigitizer(const Config& cfg, Acts::Logging::Level lvl);
+  /// @brief Destructor
+  ~MuonSpacePointDigitizer() = default;
 
-    };
-    /// @brief Constructor  
-    MuonSpacePointDigitizer(const Config& cfg, Acts::Logging::Level lvl);
-    /// @brief Destructor
-    ~MuonSpacePointDigitizer() = default;
-    
-    /// @brief Initialize the digitizer
-    ProcessCode initialize() override;
-    /// @brief Execute the digitization 
-    ProcessCode execute(const AlgorithmContext& ctx) const override;
-    
+  /// @brief Initialize the digitizer
+  ProcessCode initialize() override;
+  /// @brief Execute the digitization
+  ProcessCode execute(const AlgorithmContext& ctx) const override;
+
  private:
-    /// @brief Configuration of the digitizer
-    Config m_cfg;
-    /// @brief Logger for the digitizer
-    std::unique_ptr<const Acts::Logger> m_logger;
-    /// @brief Data handle for the input simulated hits
-   ReadDataHandle<SimHitContainer> m_inputSimHits{this, "InputSimHits"};
+  /// @brief Configuration of the digitizer
+  Config m_cfg;
+  /// @brief Logger for the digitizer
+  std::unique_ptr<const Acts::Logger> m_logger;
+  /// @brief Data handle for the input simulated hits
+  ReadDataHandle<SimHitContainer> m_inputSimHits{this, "InputSimHits"};
 
-       ReadDataHandle<SimParticleContainer> m_inputParticles{this,
-                                                          "G4Particles"};
-      /// @brief Data handle for the output space points
-      WriteDataHandle<MuonSpacePointContainer> m_outputSpacePoints{this,"SpacePoints"};
+  ReadDataHandle<SimParticleContainer> m_inputParticles{this, "G4Particles"};
+  /// @brief Data handle for the output space points
+  WriteDataHandle<MuonSpacePointContainer> m_outputSpacePoints{this,
+                                                               "SpacePoints"};
 };
-}
+}  // namespace ActsExamples
--- Examples/Algorithms/Digitization/src/MuonSpacePointDigitizer.cpp
+++ Examples/Algorithms/Digitization/src/MuonSpacePointDigitizer.cpp
@@ -1,17 +1,24 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #include "ActsExamples/Digitization/MuonSpacePointDigitizer.hpp"
-    
+
 #include "ActsExamples/EventData/MuonSpacePoint.hpp"
 
 namespace ActsExamples {
-MuonSpacePointDigitizer::MuonSpacePointDigitizer(
-    const Config& cfg, Acts::Logging::Level lvl)
+MuonSpacePointDigitizer::MuonSpacePointDigitizer(const Config& cfg,
+                                                 Acts::Logging::Level lvl)
     : IAlgorithm("MuonSpacePointDigitizer", lvl),
       m_cfg(cfg),
       m_logger(Acts::getDefaultLogger("MuonSpacePointDigitizer", lvl)) {}
 
-
 ProcessCode MuonSpacePointDigitizer::initialize() {
-    return ProcessCode::SUCCESS;
+  return ProcessCode::SUCCESS;
 }
 
 ProcessCode MuonSpacePointDigitizer::execute(
@@ -19,4 +26,4 @@ ProcessCode MuonSpacePointDigitizer::execute(
   return ProcessCode::SUCCESS;
 }
 
-}
+}  // namespace ActsExamples
