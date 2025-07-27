ce35bdfc2adadd411d189a91bd570910878cd015
cvarni <cvarni@MacBook-Pro-di-Carlo-2.local>
2025-07-27 18:38:30 +0200

    implementation of spaceship operator

--- Core/include/Acts/EventData/SpacePointContainer2.hpp
+++ Core/include/Acts/EventData/SpacePointContainer2.hpp
@@ -924,10 +924,13 @@ class SpacePointContainer2 {
         return lhs.m_iterator - rhs.m_iterator;
       }
 
-      friend constexpr auto operator<=>(const Iterator &a,
-                                        const Iterator &b) noexcept {
-        return a.m_iterator <=> b.m_iterator;
+      friend constexpr std::strong_ordering operator<=>(const Iterator &a,
+							const Iterator &b) noexcept {
+	if (a.m_iterator < b.m_iterator) return std::strong_ordering::less;
+        if (a.m_iterator > b.m_iterator) return std::strong_ordering::greater;
+        return std::strong_ordering::equal;
       }
+      
       friend constexpr bool operator==(const Iterator &a,
                                        const Iterator &b) noexcept {
         return a.m_iterator == b.m_iterator;
