7041d79b5de8d73907ef460d27e9be34898a005f
Paul Gessinger <hello@paulgessinger.com>
2025-07-29 17:41:17 +0200

    ci: Configure spack repo location as safe git repository

--- CI/dependencies/setup.sh
+++ CI/dependencies/setup.sh
@@ -127,8 +127,11 @@ if ! command -v spack &> /dev/null; then
 fi
 
 _spack_repo_version=${SPACK_REPO_VERSION:-develop}
+_spack_repo_directory=$(spack location --repo builtin)
 
 echo "Ensure repo is synced with version ${_spack_repo_version}"
+
+git config --global --add safe.directory "${_spack_repo_directory}"
 spack repo update builtin --tag "${_spack_repo_version}"
 
 end_section
