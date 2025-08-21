f12e7a8ccb2da08aa44bacc5cd4b63dcac69ab9d
Benjamin Huth <37871400+benjaminhuth@users.noreply.github.com>
2025-08-21 20:14:31 +0200

    chore: Rename ExaTrkX... to Gnn... (#4560)
    Since time has envolved, ExaTrkX seems not the correct label anymore for the GNN pipeline implementation.

--- Examples/Python/python/acts/examples/reconstruction.py
+++ Examples/Python/python/acts/examples/reconstruction.py
@@ -1926,16 +1926,16 @@ def addTrackSelection(
     return trackSelector
 
 
-ExaTrkXBackend = Enum("ExaTrkXBackend", "Torch Onnx")
+GnnBackend = Enum("GnnBackend", "Torch Onnx")
 
 
-def addExaTrkX(
+def addGnn(
     s: acts.examples.Sequencer,
     trackingGeometry: acts.TrackingGeometry,
     geometrySelection: Union[Path, str],
     modelDir: Union[Path, str],
     outputDirRoot: Optional[Union[Path, str]] = None,
-    backend: Optional[ExaTrkXBackend] = ExaTrkXBackend.Torch,
+    backend: Optional[GnnBackend] = GnnBackend.Torch,
     logLevel: Optional[acts.logging.Level] = None,
 ) -> None:
     customLogLevel = acts.examples.defaultLogging(s, logLevel)
@@ -1972,7 +1972,7 @@ def addExaTrkX(
         "cut": 0.5,
     }
 
-    if backend == ExaTrkXBackend.Torch:
+    if backend == GnnBackend.Torch:
         filterConfig["modelPath"] = str(modelDir / "filter.pt")
         filterConfig["selectedFeatures"] = [0, 1, 2]
         gnnConfig["modelPath"] = str(modelDir / "gnn.pt")
@@ -1985,7 +1985,7 @@ def addExaTrkX(
             acts.examples.TorchEdgeClassifier(**gnnConfig),
         ]
         trackBuilder = acts.examples.BoostTrackBuilding(customLogLevel())
-    elif backend == ExaTrkXBackend.Onnx:
+    elif backend == GnnBackend.Onnx:
         filterConfig["modelPath"] = str(modelDir / "filtering.onnx")
         gnnConfig["modelPath"] = str(modelDir / "gnn.onnx")
 
@@ -1997,10 +1997,10 @@ def addExaTrkX(
         ]
         trackBuilder = acts.examples.BoostTrackBuilding(customLogLevel())
 
-    findingAlg = acts.examples.TrackFindingAlgorithmExaTrkX(
+    findingAlg = acts.examples.TrackFindingAlgorithmGnn(
         level=customLogLevel(),
         inputSpacePoints="spacepoints",
-        outputProtoTracks="exatrkx_prototracks",
+        outputProtoTracks="gnn_prototracks",
         graphConstructor=graphConstructor,
         edgeClassifiers=edgeClassifiers,
         trackBuilder=trackBuilder,
@@ -2022,8 +2022,8 @@ def addExaTrkX(
         inputTracks="tracks",
         inputParticles="particles",
         inputMeasurementParticlesMap="measurement_particles_map",
-        outputTrackParticleMatching="exatrkx_track_particle_matching",
-        outputParticleTrackMatching="exatrkx_particle_track_matching",
+        outputTrackParticleMatching="gnn_track_particle_matching",
+        outputParticleTrackMatching="gnn_particle_track_matching",
         doubleMatching=True,
     )
     s.addAlgorithm(matchAlg)
