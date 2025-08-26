0ce84882585f25b9e1b267e03c989a0326dcc8fc
Benjamin Huth <benjamin.huth@cern.ch>
2025-08-26 11:53:00 +0200

    update

--- Examples/Algorithms/Digitization/include/ActsExamples/Digitization/MeasurementCreation.hpp
+++ Examples/Algorithms/Digitization/include/ActsExamples/Digitization/MeasurementCreation.hpp
@@ -69,25 +69,29 @@ measurementConstituents(const DigitizedParameters& dParams) {
 }
 
 /// Function that computes a global position for a measurement.
-/// For 1D measurements, 0 is assumed for the missing dimension.
-/// This is reliable for Rectangle bounds and Trapezoid bounds.
+/// For 1D measurements, the center of the module is used for the missing dimension.
 inline Acts::Vector3 measurementGlobalPosition(
     const DigitizedParameters& digitizedParameters,
     const Acts::Surface& surface, const Acts::GeometryContext& gctx) {
-  if (surface.bounds().type() != Acts::SurfaceBounds::eRectangle &&
-      surface.bounds().type() != Acts::SurfaceBounds::eTrapezoid) {
-    throw std::runtime_error(
-        "Currently can only compute global position of Rectangle and Trapezoid "
-        "bounds. Consider disabling the option to compute the global "
-        "position.");
-  }
-  Acts::Vector2 locPos = Acts::Vector2::Zero();
-  for (auto idx : digitizedParameters.indices) {
-    if (idx == Acts::eBoundLoc0 || idx == Acts::eBoundLoc1) {
-      locPos[idx] = digitizedParameters.values.at(idx);
+    // we need a regular surface to perform the local to global transformation
+    // without direction input. the direction could be obtained from truth
+    // information but we can leave it out here.
+    const Acts::RegularSurface* regularSurface =
+        dynamic_cast<const Acts::RegularSurface*>(&surface);
+    if (regularSurface == nullptr) {
+      throw std::invalid_argument("Expected a regular surface");
     }
+
+    Acts::Vector2 locPos = Acts::Vector2::Zero();
+    for (auto idx : digitizedParameters.indices) {
+      if (idx == Acts::eBoundLoc0 || idx == Acts::eBoundLoc1) {
+        locPos[idx] = digitizedParameters.values.at(idx);
+      } else {
+        locPos[idx] = regularSurface->bounds().center()[idx];
+    }
+
+    return regularSurface->localToGlobal(gctx, locPos);
   }
-  return surface.localToGlobal(gctx, locPos, Acts::Vector3::Zero());
 }
 
 }  // namespace ActsExamples
