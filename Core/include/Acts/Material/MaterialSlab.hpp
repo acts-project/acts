f5a2883535d5a9304fafd8505d858083457a20e1
Paul Gessinger <hello@paulgessinger.com>
2025-08-28 11:02:24 +0200

    material

--- Core/include/Acts/Material/MaterialSlab.hpp
+++ Core/include/Acts/Material/MaterialSlab.hpp
@@ -25,10 +25,15 @@ namespace Acts {
 /// @see Material for a description of the available parameters.
 class MaterialSlab {
  public:
+  /// Create a material slab with no material content
+  /// @return Empty material slab with zero thickness and no material
   static constexpr MaterialSlab Nothing() {
     return MaterialSlab(Material::Vacuum(), 0, false);
   }
 
+  /// Create a vacuum material slab with specified thickness
+  /// @param thickness The thickness of the vacuum region
+  /// @return Vacuum material slab with the given thickness
   static constexpr MaterialSlab Vacuum(float thickness) {
     return MaterialSlab(Material::Vacuum(), thickness, false);
   }
@@ -78,18 +83,24 @@ class MaterialSlab {
   MaterialSlab(const Material& material, float thickness);
 
   /// Scale the material thickness by the given factor.
+  /// @param scale Factor by which to scale the thickness
   void scaleThickness(float scale);
 
   /// Check if the material is vacuum.
+  /// @return True if the material is vacuum or thickness is zero/negative
   bool isVacuum() const { return m_material.isVacuum() || m_thickness <= 0; }
 
   /// Access the (average) material parameters.
+  /// @return Reference to the material properties
   constexpr const Material& material() const { return m_material; }
   /// Return the thickness.
+  /// @return Material thickness in millimeters
   constexpr float thickness() const { return m_thickness; }
   /// Return the radiation length fraction.
+  /// @return Thickness as a fraction of radiation length
   constexpr float thicknessInX0() const { return m_thicknessInX0; }
   /// Return the nuclear interaction length fraction.
+  /// @return Thickness as a fraction of nuclear interaction length
   constexpr float thicknessInL0() const { return m_thicknessInL0; }
 
  private:
@@ -126,10 +137,18 @@ class MaterialSlab {
   }
 };
 
+/// Stream operator for MaterialSlab
+/// @param os Output stream
+/// @param materialSlab MaterialSlab to output
+/// @return Reference to output stream
 std::ostream& operator<<(std::ostream& os, const MaterialSlab& materialSlab);
 
-// Useful typedefs
+/// @brief Type alias for a vector of material slabs
+/// @details Used to store a collection of material slabs in sequence
 using MaterialSlabVector = std::vector<MaterialSlab>;
+
+/// @brief Type alias for a matrix of material slabs
+/// @details Used to store a 2D collection of material slabs
 using MaterialSlabMatrix = std::vector<MaterialSlabVector>;
 
 /// list of point used in the mapping of a volume
