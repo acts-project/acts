faa671fc6cb51884d801f04055df74a84cbb6a22
Andreas Salzburger <asalzburger@gmail.com>
2025-09-19 14:20:31 +0200

    fix lint

--- Python/Core/src/Seeding.cpp
+++ Python/Core/src/Seeding.cpp
@@ -6,8 +6,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "Acts/Seeding/SeedFilterConfig.hpp"
 #include "Acts/Seeding/SeedConfirmationRangeConfig.hpp"
+#include "Acts/Seeding/SeedFilterConfig.hpp"
 #include "Acts/Seeding/detail/CylindricalSpacePointGrid.hpp"
 #include "ActsPython/Utilities/Helpers.hpp"
 #include "ActsPython/Utilities/Macros.hpp"
