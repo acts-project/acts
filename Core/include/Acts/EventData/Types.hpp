02358906e16d9bdb561ff8396492c37a37805c0e
Andreas Stefl <stefl.andreas@gmail.com>
2025-09-26 16:16:58 +0200

    minor

--- Core/include/Acts/EventData/Types.hpp
+++ Core/include/Acts/EventData/Types.hpp
@@ -16,12 +16,6 @@
 
 namespace Acts {
 
-using SpacePointIndex2 = std::uint32_t;
-using SpacePointIndexRange2 = std::pair<SpacePointIndex2, SpacePointIndex2>;
-using SpacePointIndexSubset2 = std::span<const SpacePointIndex2>;
-
-using SeedIndex2 = std::uint32_t;
-
 using TrackIndexType = std::uint32_t;
 static constexpr TrackIndexType kTrackIndexInvalid =
     std::numeric_limits<TrackIndexType>::max();
@@ -34,5 +28,11 @@ using BoundSubspaceIndices = SubspaceIndices<eBoundSize>;
 static constexpr BoundSubspaceIndices kBoundSubspaceIndicesInvalid = {
     eBoundSize, eBoundSize, eBoundSize, eBoundSize, eBoundSize, eBoundSize};
 using SerializedSubspaceIndices = std::uint64_t;
+
+using SpacePointIndex2 = std::uint32_t;
+using SpacePointIndexRange2 = std::pair<SpacePointIndex2, SpacePointIndex2>;
+using SpacePointIndexSubset2 = std::span<const SpacePointIndex2>;
+
+using SeedIndex2 = std::uint32_t;
 
 }  // namespace Acts
