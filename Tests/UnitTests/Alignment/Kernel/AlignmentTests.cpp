711111b7f6d449825ffa4535085dc5c6e9c96336
Andreas Stefl <stefl.andreas@gmail.com>
2025-10-03 14:35:40 +0200

    make alignment compile

--- Tests/UnitTests/Alignment/Kernel/AlignmentTests.cpp
+++ Tests/UnitTests/Alignment/Kernel/AlignmentTests.cpp
@@ -35,7 +35,6 @@
 #include "Acts/Tests/CommonHelpers/FloatComparisons.hpp"
 #include "Acts/Tests/CommonHelpers/MeasurementsCreator.hpp"
 #include "Acts/Tests/CommonHelpers/PredefinedMaterials.hpp"
-#include "Acts/TrackFitting/GainMatrixSmoother.hpp"
 #include "Acts/TrackFitting/GainMatrixUpdater.hpp"
 #include "Acts/TrackFitting/KalmanFitter.hpp"
 #include "Acts/Utilities/CalibrationContext.hpp"
@@ -59,12 +58,10 @@ using ConstantFieldPropagator =
     Acts::Propagator<ConstantFieldStepper, Acts::Navigator>;
 
 using KalmanUpdater = Acts::GainMatrixUpdater;
-using KalmanSmoother = Acts::GainMatrixSmoother;
 using KalmanFitterType =
     Acts::KalmanFitter<ConstantFieldPropagator, VectorMultiTrajectory>;
 
 KalmanUpdater kfUpdater;
-KalmanSmoother kfSmoother;
 
 // Create a test context
 const GeometryContext geoCtx;
@@ -80,8 +77,6 @@ KalmanFitterExtensions<VectorMultiTrajectory> getExtensions() {
       .connect<&testSourceLinkCalibrator<VectorMultiTrajectory>>();
   extensions.updater.connect<&KalmanUpdater::operator()<VectorMultiTrajectory>>(
       &kfUpdater);
-  extensions.smoother
-      .connect<&KalmanSmoother::operator()<VectorMultiTrajectory>>(&kfSmoother);
   return extensions;
 }
 
