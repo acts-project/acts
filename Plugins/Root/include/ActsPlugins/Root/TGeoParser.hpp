30c9cca41b7a32b4633e8f5f70652a843c765bb8
Paul Gessinger <paul.gessinger@cern.ch>
2025-10-07 23:02:00 +0200

    docs: Fix doxygen warnings in the ROOT plugin (#4684)
    Broken out of https://github.com/acts-project/acts/pull/4607

--- Plugins/Root/include/ActsPlugins/Root/TGeoParser.hpp
+++ Plugins/Root/include/ActsPlugins/Root/TGeoParser.hpp
@@ -31,6 +31,7 @@ namespace ActsPlugins {
 /// It also buils up the global transform for the conversion
 /// into an ACTS Surface
 struct TGeoParser {
+  /// Type alias for parsing range as min/max bounds pair
   using ParseRange = std::pair<double, double>;
 
   struct SelectedNode {
@@ -46,13 +47,13 @@ struct TGeoParser {
   /// geometry, it collects the information during the parsing process
   /// and keeps track of the built up transform
   struct State {
-    // The current volume
+    /// The current ROOT geometry volume being parsed
     TGeoVolume* volume = nullptr;
-    // The current node
+    /// The current ROOT geometry node being parsed
     TGeoNode* node = nullptr;
-    // Bool on branch
+    /// Flag indicating if parsing is on the selected geometry branch
     bool onBranch = false;
-    // The currently collected nodes
+    /// Collection of nodes selected during parsing
     std::vector<SelectedNode> selectedNodes = {};
   };
 
