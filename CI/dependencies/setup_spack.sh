f34dee626c9c1085264bb23969bef8abeaf55ec9
Paul Gessinger <hello@paulgessinger.com>
2025-11-01 17:59:13 +0100

    ci: Optimize dependencies install time

--- CI/dependencies/setup_spack.sh
+++ CI/dependencies/setup_spack.sh
@@ -8,9 +8,9 @@ _spack_folder=$1
 
 if [ ! -d "${_spack_folder}" ]; then
     echo "Cloning spack"
-    git clone -c feature.manyFiles=true https://github.com/spack/spack.git "${_spack_folder}"
+    time git clone -c feature.manyFiles=true https://github.com/spack/spack.git "${_spack_folder}"
     pushd "${_spack_folder}" > /dev/null
-    git checkout "${_spack_version}"
+    time git checkout "${_spack_version}"
     git config user.name 'CI'
     git config user.email '<>'
     popd > /dev/null
