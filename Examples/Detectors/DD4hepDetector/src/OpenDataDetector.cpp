a7a4c4c011a6fa59fe5eb8a8bddf2442c20fcdce
Paul Gessinger <hello@paulgessinger.com>
2025-12-01 16:24:24 +0100

    feat: Test Gen3 DD4hep code on ODD

--- Examples/Detectors/DD4hepDetector/src/OpenDataDetector.cpp
+++ Examples/Detectors/DD4hepDetector/src/OpenDataDetector.cpp
@@ -8,9 +8,20 @@
 
 #include "ActsExamples/DD4hepDetector/OpenDataDetector.hpp"
 
-#include "Acts/Geometry/CylinderVolumeBounds.hpp"
+#include "Acts/Geometry/ContainerBlueprintNode.hpp"
+#include "Acts/Geometry/Extent.hpp"
+#include "Acts/Utilities/AxisDefinitions.hpp"
+#include "ActsPlugins/DD4hep/BlueprintBuilder.hpp"
 #include <Acts/Geometry/Blueprint.hpp>
 #include <Acts/Geometry/BlueprintOptions.hpp>
+#include <Acts/Geometry/CylinderVolumeBounds.hpp>
+#include <ActsPlugins/DD4hep/DD4hepConversionHelpers.hpp>
+
+#include <algorithm>
+#include <iterator>
+
+#include <DD4hep/DetElement.h>
+#include <DD4hep/Detector.h>
 
 namespace ActsExamples {
 
@@ -25,21 +36,54 @@ auto OpenDataDetector::config() const -> const Config& {
   return m_cfg;
 }
 
+std::shared_ptr<ActsPlugins::DD4hepDetectorElement>
+OpenDataDetector::defaultDetectorElementFactory(
+    const dd4hep::DetElement& element, const std::string& axes, double scale) {
+  return std::make_shared<ActsPlugins::DD4hepDetectorElement>(element, axes,
+                                                              scale);
+}
+
 void OpenDataDetector::construct(const Acts::GeometryContext& gctx) {
   using namespace Acts::Experimental;
   using namespace Acts;
   using namespace Acts::UnitLiterals;
+  using enum AxisDirection;
+
+  ActsPlugins::DD4hep::BlueprintBuilder builder{
+      {
+          .dd4hepDetector = &dd4hepDetector(),
+          .lengthScale = Acts::UnitConstants::cm,
+      },
+      logger().cloneWithSuffix("BlpBld")};
+
+  // func(world);
+
+  // ACTS_VERBOSE("Found detelement: " << pixelBrl0.name());
+
+  // std::vector sensitives = resolveSensitives(pixelBrl0);
+
+  // std::vector<Acts::Surface*> surfaces;
+  // surfaces.reserve(sensitives.size());
+  // std::vector<std::shared_ptr<ActsPlugins::DD4hepDetectorElement>>
+  //     detectorElements;
+  // BARREL: XYZ
+  // ENDCAP: XZY
+  // for (const auto& sensitive : sensitives) {
+  //   builder.createDetectorElement(sensitive, "XYZ");
+  // }
 
   Blueprint::Config cfg;
-  cfg.envelope[AxisDirection::AxisZ] = {20_mm, 20_mm};
-  cfg.envelope[AxisDirection::AxisR] = {0_mm, 20_mm};
+  cfg.envelope[AxisZ] = {20_mm, 20_mm};
+  cfg.envelope[AxisR] = {0_mm, 20_mm};
   Blueprint root{cfg};
 
-  auto volBounds = std::make_shared<CylinderVolumeBounds>(0_mm, 100_mm, 1_m);
-  auto vol =
-      std::make_unique<TrackingVolume>(Transform3::Identity(), volBounds);
+  dd4hep::DetElement pixelBarrelElem =
+      builder.findDetElementByName("PixelBarrel").value();
 
-  root.addStaticVolume(std::move(vol));
+  auto pixelBarrel = builder.addLayers(pixelBarrelElem, "XYZ", AxisR,
+                                       std::regex{"PixelLayer\\d"});
+
+  root.addChild(pixelBarrel);
 
   BlueprintOptions options;
 
