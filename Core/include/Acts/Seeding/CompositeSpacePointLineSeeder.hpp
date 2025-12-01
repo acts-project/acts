7ffdaab2462e199540feaa78a688fc3482e80dd7
Patrick Scholer <pscholer@atlmuonsw1.dyndns.cern.ch>
2025-12-01 15:27:07 +0100

    move space point sorter concept into seperate file

--- Core/include/Acts/Seeding/CompositeSpacePointLineSeeder.hpp
+++ Core/include/Acts/Seeding/CompositeSpacePointLineSeeder.hpp
@@ -10,6 +10,7 @@
 
 #include "Acts/EventData/CompositeSpacePoint.hpp"
 #include "Acts/EventData/CompositeSpacePointCalibrator.hpp"
+#include "Acts/EventData/CompositeSpacePointSorter.hpp"
 #include "Acts/Seeding/CompositeSpacePointLineFitter.hpp"
 #include "Acts/Seeding/detail/CompSpacePointAuxiliaries.hpp"
 #include "Acts/Utilities/Delegate.hpp"
@@ -25,36 +26,6 @@ template <typename T>
 using PointerConstDref_t = std::add_const_t<RemovePointer_t<T>>&;
 }  // namespace detail
 
-/// @brief Define the concept of the space point measurement sorter. The sorter shall take a collection
-///         of station space points and split them into straw && strip hits.
-///         Hits
-/// from each category are then subdivided further into the particular detector
-/// layers.
-///
-///      A possible implementation of the CompositeSpacePointSorter needs to
-///      have
-/// the following attributes
-///
-///      using SpVec_t =  Standard container satisfiyng the
-/// CompositeSpacePointContainer concept
-///
-///
-///      const std::vector<SpVec_t>& strawHits();
-///      const std::vector<SpVec_t>& stripHits();
-///  Each SpVec_t contains all measurements from a particular detector layer
-template <typename Splitter_t, typename SpacePointCont_t>
-concept CompositeSpacePointSorter =
-    CompositeSpacePointContainer<SpacePointCont_t> &&
-    requires(Splitter_t sorter) {
-      /// @brief Return the straw-hit space point sorted by straw layer
-      {
-        sorter.strawHits()
-      } -> std::same_as<const std::vector<SpacePointCont_t>&>;
-      /// @brief Return the strip-hit  space points sorted by detector layer
-      {
-        sorter.stripHits()
-      } -> std::same_as<const std::vector<SpacePointCont_t>&>;
-    };
 
 class CompositeSpacePointLineSeeder {
  public:
