6775237fdfb15f86eac508ed6396bacee67d2e71
Andreas Salzburger <asalzburger@gmail.com>
2026-01-07 12:20:34 +0100

    remove ProtoDetector and KdtTrackingGeometry builder

--- Python/Examples/src/plugins/Geant4.cpp
+++ Python/Examples/src/plugins/Geant4.cpp
@@ -165,8 +165,7 @@ PYBIND11_MODULE(ActsExamplesPythonBindingsGeant4, mod) {
             .def(py::init<const Geant4Detector::Config&>());
 
     auto c = py::class_<Geant4Detector::Config>(f, "Config").def(py::init<>());
-    ACTS_PYTHON_STRUCT(c, name, g4World, g4SurfaceOptions, protoDetector,
-                       geometryIdentifierHook, logLevel);
+    ACTS_PYTHON_STRUCT(c, name, g4World, g4SurfaceOptions, logLevel);
   }
 
   {
