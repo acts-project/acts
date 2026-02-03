ee1aae85a572140ce3cbda18d0b4992bf133ac3f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-02-03 08:53:00 -0500

    feat: speedup surface lookup Mat_map_surface_plot.C using precomputed map

--- Examples/Scripts/MaterialMapping/materialPlotHelper.cpp
+++ Examples/Scripts/MaterialMapping/materialPlotHelper.cpp
@@ -11,6 +11,10 @@
 #include <iomanip>
 #include <ostream>
 #include <string>
+#include <unordered_map>
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 /// Information on a given surface.
 
@@ -31,6 +35,27 @@ std::ostream& Acts::operator<<(std::ostream& os, Acts::GeometryIdentifier id) {
   os << " | " << std::setw(3) << id.approach();
   os << " | " << std::setw(4) << id.sensitive() << " ]";
   return os;
+}
+
+std::unordered_map<std::uint64_t, json> load_geometry_file(std::string geometry_file) {
+  json geom;
+  {
+    std::ifstream gj(geometry_file);
+    if (!gj.good()) {
+      std::cerr << "WARNING: " << geometry_file << " not found." << std::endl;
+    } else {
+      try { gj >> geom; } catch (...) {
+        std::cerr << "WARNING: Failed to parse " << geometry_file << "." << std::endl;
+      }
+    }
+  }
+  std::unordered_map<std::uint64_t, json> surface_bounds;
+  const auto &entries = geom["Surfaces"]["entries"];
+  for (const auto &entry : entries) {
+    std::uint64_t gid = entry["value"]["geo_id"].get<std::uint64_t>();
+    surface_bounds[gid] = entry["value"]["bounds"];
+  }
+  return surface_bounds;
 }
 
 /// Initialise the information on each surface.
