0d3b9f955dd102d128880bebe5317464c35721a6
Andreas Stefl <stefl.andreas@gmail.com>
2026-02-09 16:48:37 +0100

    build: Disable `clang-tidy` for thirdparty

--- cmake/ActsStaticAnalysis.cmake
+++ cmake/ActsStaticAnalysis.cmake
@@ -1,18 +1,27 @@
 if(ACTS_RUN_CLANG_TIDY)
     find_program(CLANG_TIDY_COMMAND NAMES clang-tidy)
+
     if(NOT CLANG_TIDY_COMMAND)
         message(
             WARNING
             "ACTS_RUN_CLANG_TIDY is ON but clang-tidy is not found!"
         )
-        set(CMAKE_CXX_CLANG_TIDY "" CACHE STRING "" FORCE)
+        set(ACTS_CXX_CLANG_TIDY "" CACHE STRING "" FORCE)
     else()
         message(STATUS "Setting up clang-tidy run")
 
         set(CLANG_TIDY_HEADER_FILTER ".*")
 
-        set(CMAKE_CXX_CLANG_TIDY
+        set(ACTS_CXX_CLANG_TIDY
             "${CLANG_TIDY_COMMAND};-header-filter=${CLANG_TIDY_HEADER_FILTER};-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy"
         )
     endif()
 endif()
+
+function(acts_enable_static_analysis)
+    set(CMAKE_CXX_CLANG_TIDY ${ACTS_CXX_CLANG_TIDY} CACHE STRING "" FORCE)
+endfunction()
+
+function(acts_disable_static_analysis)
+    set(CMAKE_CXX_CLANG_TIDY "" CACHE STRING "" FORCE)
+endfunction()
