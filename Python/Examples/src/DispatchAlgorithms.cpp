09a7a8b34519f4c393bbe20feec6ed6d40584e3a
Andreas Salzburger <asalzburger@gmail.com>
2026-02-13 13:53:54 +0100

    scripts adding

--- Python/Examples/src/DispatchAlgorithms.cpp
+++ Python/Examples/src/DispatchAlgorithms.cpp
@@ -15,6 +15,9 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <pybind11/complex.h>
+#include <pybind11/chrono.h>
 
 namespace py = pybind11;
 
