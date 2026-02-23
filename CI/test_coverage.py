98c88c658b94241fa310fcf227d032036fd4c7af
Paul Gessinger <hello@paulgessinger.com>
2026-02-23 13:12:49 +0100

    attempt at fix

--- CI/test_coverage.py
+++ CI/test_coverage.py
@@ -270,6 +270,7 @@ def _build_gcovr_common_args(
         + ["--gcov-object-directory", gcov_object_directory]
         + ["-j", str(jobs)]
         + ["--merge-mode-functions", "separate"]
+        + ["--gcov-ignore-errors", "source_not_found,no_working_dir_found"]
         + excludes
         + extra_flags
     )
