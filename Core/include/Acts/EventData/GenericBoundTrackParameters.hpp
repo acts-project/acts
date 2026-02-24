ff72e3a3a34787f1c54e3c6311979064ae804dd4
Andreas Stefl <stefl.andreas@gmail.com>
2026-02-24 11:28:01 +0100

    more deprecated headers

---
+++ Core/include/Acts/EventData/GenericBoundTrackParameters.hpp
@@ -0,0 +1,23 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/EventData/BoundTrackParameters.hpp"
+
+namespace {
+[[deprecated(
+    "This header is deprecated. Please include the bound track parameters "
+    "header "
+    "instead.")]]
+constexpr static int
+    Acts_EventData_GenericBoundTrackParameters_hpp_is_deprecated = 0;
+constexpr static int
+    please_dont_use_Acts_EventData_GenericBoundTrackParameters_hpp =
+        Acts_EventData_GenericBoundTrackParameters_hpp_is_deprecated;
+}  // namespace
---
+++ Core/include/Acts/EventData/GenericParticleHypothesis.hpp
@@ -0,0 +1,23 @@
+// This file is part of the ACTS project.
+//
+// Copyright (C) 2016 CERN for the benefit of the ACTS project
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Acts/EventData/ParticleHypothesis.hpp"
+
+namespace {
+[[deprecated(
+    "This header is deprecated. Please include the particle hypothesis "
+    "header "
+    "instead.")]]
+constexpr static int
+    Acts_EventData_GenericParticleHypothesis_hpp_is_deprecated = 0;
+constexpr static int
+    please_dont_use_Acts_EventData_GenericParticleHypothesis_hpp =
+        Acts_EventData_GenericParticleHypothesis_hpp_is_deprecated;
+}  // namespace
