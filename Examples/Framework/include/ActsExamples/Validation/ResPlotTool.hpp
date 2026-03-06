fe211f79f6aa4e248cd1562177ce519d23011be3
Andreas Stefl <stefl.andreas@gmail.com>
2026-03-06 18:04:10 +0100

    update t plotting range

--- Examples/Framework/include/ActsExamples/Validation/ResPlotTool.hpp
+++ Examples/Framework/include/ActsExamples/Validation/ResPlotTool.hpp
@@ -52,7 +52,7 @@ class ResPlotTool {
         {"Residual_theta",
          BoostRegularAxis(100, -0.01, 0.01, "r_{#theta} [rad]")},
         {"Residual_qop", BoostRegularAxis(100, -0.1, 0.1, "r_{q/p} [c/GeV]")},
-        {"Residual_t", BoostRegularAxis(100, -1000, 1000, "r_{t} [s]")}};
+        {"Residual_t", BoostRegularAxis(100, -100, 100, "r_{t} [mm/c]")}};
   };
 
   /// @param cfg Configuration struct
