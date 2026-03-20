436497e520f1676452fd47917e07de09cb45f1e6
Paul Gessinger <hello@paulgessinger.com>
2026-03-20 16:21:37 +0100

    prepare for tgeo backend

--- Plugins/DD4hep/include/ActsPlugins/DD4hep/OpenDataDetectorBuilder.hpp
+++ Plugins/DD4hep/include/ActsPlugins/DD4hep/OpenDataDetectorBuilder.hpp
@@ -8,7 +8,13 @@
 
 #pragma once
 
+#include "Acts/Geometry/Extent.hpp"
+#include "Acts/Utilities/AxisDefinitions.hpp"
+
 #include <memory>
+#include <regex>
+#include <string>
+#include <string_view>
 
 namespace dd4hep {
 class Detector;
@@ -21,6 +27,57 @@ class TrackingGeometry;
 }  // namespace Acts
 
 namespace ActsPlugins::DD4hep {
+
+namespace detail {
+
+inline const std::regex kPixelLayerFilter{"(?:PixelLayer|PixelEndcap[NP])(\\d)"};
+inline const std::regex kShortStripLayerFilter{
+    "(?:ShortStripLayer|ShortStripEndcap[NP])(\\d)"};
+inline const std::regex kLongStripLayerFilter{
+    "(?:LongStripLayer|LongStripEndcap[NP])(\\d)"};
+inline const std::regex kPixelBarrelLayerFilter{"PixelLayer\\d"};
+inline const std::regex kPixelNegativeEndcapLayerFilter{"PixelEndcapN\\d"};
+inline const std::regex kPixelPositiveEndcapLayerFilter{"PixelEndcapP\\d"};
+inline const std::regex kShortStripBarrelLayerFilter{"ShortStripLayer\\d"};
+inline const std::regex kShortStripNegativeEndcapLayerFilter{
+    "ShortStripEndcapN\\d"};
+inline const std::regex kShortStripPositiveEndcapLayerFilter{
+    "ShortStripEndcapP\\d"};
+inline const std::regex kLongStripBarrelLayerFilter{"LongStripLayer\\d"};
+inline const std::regex kLongStripNegativeEndcapLayerFilter{
+    "LongStripEndcapN\\d"};
+inline const std::regex kLongStripPositiveEndcapLayerFilter{
+    "LongStripEndcapP\\d"};
+
+inline const Acts::ExtentEnvelope kBlueprintEnvelope =
+    Acts::ExtentEnvelope::Zero()
+        .set(Acts::AxisDirection::AxisZ, {20., 20.})
+        .set(Acts::AxisDirection::AxisR, {0., 20.});
+
+inline const Acts::ExtentEnvelope kLayerEnvelope =
+    Acts::ExtentEnvelope::Zero()
+        .set(Acts::AxisDirection::AxisZ, {2., 2.})
+        .set(Acts::AxisDirection::AxisR, {2., 2.});
+
+inline int layerIndexFromName(std::string_view elemName,
+                              const std::regex& layerFilter) {
+  std::cmatch match;
+  if (std::regex_search(elemName.begin(), elemName.end(), match, layerFilter) &&
+      match.size() > 1) {
+    return std::stoi(match[1].str());
+  }
+
+  static const std::regex groupedLayerNameFilter{"layer(\\d+)"};
+  if (std::regex_search(elemName.begin(), elemName.end(), match,
+                        groupedLayerNameFilter) &&
+      match.size() > 1) {
+    return std::stoi(match[1].str());
+  }
+
+  return 0;
+}
+
+}  // namespace detail
 
 /// Build the Open Data Detector tracking geometry using the BarrelEndcap
 /// construction path (BarrelEndcapAssembler wrapping ElementLayerAssembler).
