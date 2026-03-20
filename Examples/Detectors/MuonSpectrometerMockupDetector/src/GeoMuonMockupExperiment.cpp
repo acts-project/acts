e80220e3a67d998ba9c59977ff203a6fc5dea1af
Andreas Stefl <stefl.andreas@gmail.com>
2026-03-20 10:08:04 +0100

    revert

--- Examples/Detectors/MuonSpectrometerMockupDetector/src/GeoMuonMockupExperiment.cpp
+++ Examples/Detectors/MuonSpectrometerMockupDetector/src/GeoMuonMockupExperiment.cpp
@@ -169,8 +169,13 @@ ActsPlugins::GeoModelTree GeoMuonMockupExperiment::constructMS() {
   VolumeMap_t publishedVol{};
   for (const auto& [fpV, pubKey] : m_publisher->getPublishedFPV()) {
     try {
-      const auto key = [pubKey]() {
-        return std::any_cast<std::string>(pubKey);
+      const auto key = [&pubKey]() {
+        if constexpr (std::is_same_v<std::remove_cvref_t<decltype(pubKey)>,
+                                     std::any>) {
+          return std::any_cast<std::string>(pubKey);
+        } else {
+          return std::get<std::string>(pubKey);
+        }
       }();
 
       if (!publishedVol
