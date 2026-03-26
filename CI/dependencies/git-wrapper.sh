89516e1625cf05045c26ca45473fd49a638f093c
Paul Gessinger <hello@paulgessinger.com>
2026-03-26 17:14:18 +0100

    more debugging

--- CI/dependencies/git-wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Spack sets DYLD_LIBRARY_PATH to its own libs (e.g. libiconv), which conflicts
-# with the brew git binary. Unset it so git resolves its own libraries.
-exec env -u DYLD_LIBRARY_PATH "$(brew --prefix git)/bin/git" "$@"
