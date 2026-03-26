264c6e9d475120bf0472dce8a99b35d57c4ee1d2
Doga Elitez <108287101+delitez@users.noreply.github.com>
2026-03-26 17:11:22 +0100

    Add lepton labelling to truth jets

--- Examples/Algorithms/Jets/src/TruthJetAlgorithm.cpp
+++ Examples/Algorithms/Jets/src/TruthJetAlgorithm.cpp
@@ -148,7 +148,7 @@ ProcessCode TruthJetAlgorithm::execute(const AlgorithmContext& ctx) const {
   }
 
   // Find hadrons for jet labeling with sim particles
-  std::vector<std::pair<ActsExamples::JetLabel, const SimParticle*>> hadrons;
+  std::vector<std::pair<ActsExamples::JetLabel, const SimParticle*>> labelCandidates;
 
   if (m_cfg.doJetLabeling) {
     Acts::ScopedTimer timer("hadron finding", logger(), Acts::Logging::DEBUG);
@@ -165,6 +165,8 @@ ProcessCode TruthJetAlgorithm::execute(const AlgorithmContext& ctx) const {
           }
 
           Acts::PdgParticle pdgId{particle->pdg()};
+          
+
           if (!Acts::ParticleIdHelper::isHadron(pdgId) &&
               !Acts::ParticleIdHelper::isMuon(pdgId) &&
               !Acts::ParticleIdHelper::isElectron(pdgId) &&
@@ -173,8 +175,15 @@ ProcessCode TruthJetAlgorithm::execute(const AlgorithmContext& ctx) const {
           }
 
           // Apply a pt cut on B or C hadrons
-          auto label =
-              jetLabelFromHadronType(Acts::ParticleIdHelper::hadronType(pdgId));
+          ActsExamples::JetLabel label;
+          if (Acts::ParticleIdHelper::isHadron(pdgId)) {
+          label =
+              jetLabelFromHadronType(Acts::ParticleIdHelper::hadronType(pdgId));}
+          else if (Acts::ParticleIdHelper::isLepton(pdgId)) {
+          label =
+                jetLabelFromLeptonType(Acts::ParticleIdHelper::leptonType(pdgId));
+          }
+
           using enum ActsExamples::JetLabel;
 
           if (label == BJet || label == CJet) {
@@ -186,77 +195,92 @@ ProcessCode TruthJetAlgorithm::execute(const AlgorithmContext& ctx) const {
         }) |
         std::views::transform([](const auto* particle) {
           Acts::PdgParticle pdgId{particle->pdg()};
-          auto type = Acts::ParticleIdHelper::hadronType(pdgId);
-          // TODO: if hadron get label from hadron type, else lepton type, else
-          // if other
-          auto label = jetLabelFromHadronType(type);
-          return std::make_pair(label, particle);
+
+          auto htype = Acts::ParticleIdHelper::hadronType(pdgId);
+          auto ltype = Acts::ParticleIdHelper::leptonType(pdgId);
+          // TODO: if hadron get label from hadron type, else lepton type, else if other
+          if (Acts::ParticleIdHelper::isHadron(pdgId)) {
+            auto label = jetLabelFromHadronType(htype);
+            return std::make_pair(label, particle);
+          }
+          else if (Acts::ParticleIdHelper::isLepton(pdgId)) {
+            auto label = jetLabelFromLeptonType(ltype);
+            return std::make_pair(label, particle);
+          }
         }) |
-        std::views::filter([](const auto& hadron) {
-          return hadron.first > ActsExamples::JetLabel::OtherJet;
+        std::views::filter([](const auto& labelCandidate) {
+          return labelCandidate.first > ActsExamples::JetLabel::OtherJet;
         });
 
-    std::ranges::copy(hadronView, std::back_inserter(hadrons));
+    std::ranges::copy(hadronView, std::back_inserter(labelCandidates));
 
-    // Deduplicate hadrons based on their pdg id
-    std::ranges::sort(hadrons, [](const auto& a, const auto& b) {
+    // Deduplicate label candidates based on their pdg id
+    std::ranges::sort(labelCandidates, [](const auto& a, const auto& b) {
       return a.second->pdg() < b.second->pdg();
     });
 
-    auto unique = std::ranges::unique(hadrons);
-    hadrons.erase(unique.begin(), unique.end());
+    auto unique = std::ranges::unique(labelCandidates);
+    labelCandidates.erase(unique.begin(), unique.end());
   }
 
   // Jet classification
-  // TODO: better naming instead hadron
+  // TODO: better naming instead hadron 
   /// ?? is it redundant
   auto classifyJet = [&](const fastjet::PseudoJet& jet) {
-    auto hadronsInJetView =
-        hadrons | std::views::filter([&jet, this](const auto& hadron) {
-          const auto& momentum = hadron.second->momentum();
-          Acts::Vector3 hadronJetMom{momentum[0], momentum[1], momentum[2]};
+    auto labelCandidatesInJetView =
+        labelCandidates | std::views::filter([&jet, this](const auto& labelCandidate) {
+          const auto& momentum = labelCandidate.second->momentum();
+          Acts::Vector3 labelCandidateJetMom{momentum[0], momentum[1], momentum[2]};
           Acts::Vector3 jetMom{jet.px(), jet.py(), jet.pz()};
-          return Acts::VectorHelpers::deltaR(jetMom, hadronJetMom) <
+          return Acts::VectorHelpers::deltaR(jetMom, labelCandidateJetMom) <
                  m_cfg.jetLabelingDeltaR;
         }) |
-        std::views::transform([](const auto& hadron) {
-          return std::pair{
-              hadron.second,
-              jetLabelFromHadronType(Acts::ParticleIdHelper::hadronType(
-                  Acts::PdgParticle{hadron.second->pdg()}))};
+        std::views::transform([](const auto& labelCandidate) {
+          if (Acts::ParticleIdHelper::isHadron(Acts::PdgParticle{labelCandidate.second->pdg()})) {
+            return std::pair{
+                labelCandidate.second,
+                jetLabelFromHadronType(Acts::ParticleIdHelper::hadronType(
+                    Acts::PdgParticle{labelCandidate.second->pdg()}))};
+          }
+          else if (Acts::ParticleIdHelper::isLepton(Acts::PdgParticle{labelCandidate.second->pdg()})) {
+            return std::pair{
+                labelCandidate.second,
+                jetLabelFromLeptonType(Acts::ParticleIdHelper::leptonType(
+                    Acts::PdgParticle{labelCandidate.second->pdg()}))};
+          }
         });
 
     std::vector<std::pair<const SimParticle*, ActsExamples::JetLabel>>
-        hadronsInJet;
-    std::ranges::copy(hadronsInJetView, std::back_inserter(hadronsInJet));
+        labelCandidatesInJet;
+    std::ranges::copy(labelCandidatesInJetView, std::back_inserter(labelCandidatesInJet));
 
-    ACTS_VERBOSE("-> hadrons in jet: " << hadronsInJet.size());
-    for (const auto& hadron : hadronsInJet) {
+    ACTS_VERBOSE("-> label candidates in jet: " << labelCandidatesInJet.size());
+    for (const auto& labelCandidate : labelCandidatesInJet) {
       ACTS_VERBOSE(
-          "  - " << hadron.first->pdg() << " "
-                 << Acts::findName(Acts::PdgParticle{hadron.first->pdg()})
+          "  - " << labelCandidate.first->pdg() << " "
+                 << Acts::findName(Acts::PdgParticle{labelCandidate.first->pdg()})
                         .value_or("OTHER")
-                 << " label=" << hadron.second);
+                 << " label=" << labelCandidate.second);
     }
 
     auto maxHadronIt = std::ranges::max_element(
-        hadronsInJet, [](const auto& a, const auto& b) { return a < b; },
+        labelCandidatesInJet, [](const auto& a, const auto& b) { return a < b; },
         [](const auto& a) {
-          const auto& [hadron, label] = a;
+          const auto& [labelCandidate, label] = a;
           return label;
         });
 
-    if (maxHadronIt == hadronsInJet.end()) {
+    if (maxHadronIt == labelCandidatesInJet.end()) {
       // Now hadronic "jet"
       return ActsExamples::JetLabel::OtherJet;
     }
 
     const auto& [maxHadron, maxHadronLabel] = *maxHadronIt;
 
-    ACTS_VERBOSE(
-        "-> max hadron type="
-        << Acts::findName(Acts::PdgParticle{maxHadron->pdg()}).value_or("OTHER")
-        << " label=" << maxHadronLabel);
+    ACTS_VERBOSE("-> max candidate type=" 
+                 << Acts::findName(Acts::PdgParticle{maxHadron->pdg()})
+                        .value_or("OTHER")
+                 << " label=" << maxHadronLabel);
 
     return maxHadronLabel;
   };  // jet classification
@@ -378,23 +402,15 @@ void TruthJetAlgorithm::trackJetMatching(const ConstTrackContainer& tracks,
     ACTS_VERBOSE("Jet " << ijet << " has " << nTracksAssociated
                         << " associated tracks after matching.");
     if (jets[ijet].associatedTracks().size() == 0) {
-      Acts::Vector3 jet_3mom{jets[ijet].fourMomentum()[0],
-                             jets[ijet].fourMomentum()[1],
-                             jets[ijet].fourMomentum()[2]};
-      ACTS_VERBOSE(
-          "Jet " << ijet << " has no associated tracks and its eta is "
-                 << std::atanh(std::cos(Acts::VectorHelpers::theta(jet_3mom))));
-      for (auto track : tracks) {
-        ACTS_VERBOSE("Track "
-                     << track.index() << " and jet " << ijet << " has deltaR: "
-                     << Acts::VectorHelpers::deltaR(
-                            Acts::Vector3{jets[ijet].fourMomentum()[0],
-                                          jets[ijet].fourMomentum()[1],
-                                          jets[ijet].fourMomentum()[2]},
-                            Acts::Vector3{track.fourMomentum()[0],
-                                          track.fourMomentum()[1],
-                                          track.fourMomentum()[2]}));
-      }
+      Acts::Vector3 jet_3mom{jets[ijet].fourMomentum()[0], jets[ijet].fourMomentum()[1],
+                    jets[ijet].fourMomentum()[2]};
+      ACTS_VERBOSE("Jet " << ijet << " has no associated tracks and its eta is " << std::atanh(std::cos(Acts::VectorHelpers::theta(jet_3mom))));
+      for(auto track : tracks) {
+        ACTS_VERBOSE("Track " << track.index() << " and jet " << ijet << " has deltaR: " << Acts::VectorHelpers::deltaR(
+          Acts::Vector3{jets[ijet].fourMomentum()[0], jets[ijet].fourMomentum()[1], jets[ijet].fourMomentum()[2]},
+          Acts::Vector3{track.fourMomentum()[0], track.fourMomentum()[1], track.fourMomentum()[2]}
+        ));
+      } 
     }
   }
 }
