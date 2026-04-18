fbd6731d5d09d45ffdd992e65f90ffc530164977
Andreas Stefl <stefl.andreas@gmail.com>
2026-04-18 13:02:16 +0200

    go bigger

--- Core/include/Acts/Propagator/RiddersStepper.hpp
+++ Core/include/Acts/Propagator/RiddersStepper.hpp
@@ -121,7 +121,7 @@ class RiddersStepper final {
 
     std::shared_ptr<const BoundParameterVariationGenerator> parameterVariation{
         std::make_shared<CovarianceBoundParameterVariationGenerator>(
-            std::vector<double>{-1e-8, 1e-8})};
+            std::vector<double>{-1e-1, 1e-1})};
   };
 
   using Options = typename StepperImpl::Options;
