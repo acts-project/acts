b3cab506207febea8c5da4d99bbbb175520bb08f
Alexander J. Pfleger <alexander.julian.pfleger@cern.ch>
2026-04-29 15:13:50 +0200

    improve EXCLUDES

--- CI/check_unused_files.py
+++ CI/check_unused_files.py
@@ -5,8 +5,7 @@ import os
 import sys
 import subprocess
 
-EXCLUDE_PATHS = ()
-EXCLUDE_DIRS = (
+EXCLUDE_PATHS = (
     "Scripts",
     "thirdparty",
     "CI",
@@ -17,12 +16,19 @@ EXCLUDE_DIRS = (
     ".github",
     ".idea",
     ".devcontainer",
+    "white_papers/figures",
+    # cmake for "DD4hep-tests" looks a bit different
+    "Tests/UnitTests/Plugins/DD4hep",
 )
 EXCLUDE_FILES = (
     "acts_logo_colored.svg",
     ".gitignore",
     "README.md",
     "CMakeLists.txt",
+    "pytest.ini",
+    ".pre-commit-config.yaml",
+    "CITATION.cff",
+    "sonar-project.properties",
     # Filename not completed in source
     "vertexing_event_mu20_beamspot.csv",
     "vertexing_event_mu20_tracks.csv",
@@ -58,11 +64,11 @@ EXCLUDE_FILES = (
     "GeometryModule.h",
     "runtime_geometry_modules.md",
 )
-SUFFIX_HEADER = (
+SUFFIX_CPP = (
     ".hpp",
     ".cuh",
-)
-SUFFIX_SOURCE = (
+    ".sycl",
+    ".hip",
     ".ipp",
     ".cpp",
     ".cu",
@@ -83,6 +89,7 @@ SUFFIX_DOC = (
 )
 SUFFIX_OTHER = (
     "",
+    ".C",
     ".csv",
     ".css",
     ".gdml",
@@ -101,28 +108,17 @@ SUFFIX_OTHER = (
 )
 
 
-def filter_paths(
-    names, root: Path, exclude_paths=(), exclude_dirs=(), exclude_files=()
-):
-    out = []
-    for n in names:
-        p = root / n
-        parts = p.parts
+def filter_paths(names, root, exclude_paths=(), exclude_files=()):
+    root = Path(root)
 
-        # full path substring
-        if any(ep in str(p) for ep in exclude_paths):
-            continue
+    def keep(name):
+        p = root / name
+        p_str = p.as_posix()
+        return not any(ep in p_str for ep in exclude_paths) and not any(
+            ef in p_str if "/" in ef else p.name == ef for ef in exclude_files
+        )
 
-        # exact dir segment match
-        if any(ed in parts for ed in exclude_dirs):
-            continue
-
-        # exact file name match
-        if p.name in exclude_files:
-            continue
-
-        out.append(n)
-    return out
+    return [name for name in names if keep(name)]
 
 
 def file_can_be_removed(searchstring, scope):
@@ -135,46 +131,69 @@ def file_can_be_removed(searchstring, scope):
 
 def count_files(path="."):
     count = 0
-    for root, _, files in os.walk(path):
-        files = filter_paths(
-            files, Path(root), EXCLUDE_PATHS, EXCLUDE_DIRS, EXCLUDE_FILES
-        )
+    for root, dirs, files in os.walk(path):
+        dirs[:] = filter_paths(dirs, Path(root), EXCLUDE_PATHS)
+        files = filter_paths(files, Path(root), EXCLUDE_PATHS, EXCLUDE_FILES)
 
         count += len(files)
 
     return count
 
 
-def main():
-    print("\033[32mINFO\033[0m Start check_unused_files.py ...")
-
+def check_wrong_extensions(walk_root):
     suffix_allowed = (
-        SUFFIX_HEADER
-        + SUFFIX_SOURCE
+        SUFFIX_CPP
         + SUFFIX_IMAGE
         + SUFFIX_PYTHON
         + SUFFIX_DOC
         + SUFFIX_OTHER
     )
 
+    wrong_extension = []
+    for root, dirs, files in os.walk(walk_root):
+        dirs[:] = filter_paths(dirs, Path(root), EXCLUDE_PATHS)
+        files = filter_paths(files, Path(root), EXCLUDE_PATHS, EXCLUDE_FILES)
+
+        for f in files:
+            p = Path(root) / f
+            if p.suffix not in suffix_allowed:
+                wrong_extension.append(str(p))
+
+    if len(wrong_extension) != 0:
+        print(
+            "\n\n\033[31mERROR\033[0m "
+            + f"The following {len(wrong_extension)} files have an unsupported extension:\n\n"
+            + "\033[31m"
+            + "\n".join(wrong_extension)
+            + "\033[0m"
+            + "\nCheck if you can change the format to one of the following:\n"
+            + "\n".join(suffix_allowed)
+            + "\nIf you really need that specific extension, add it to the list above.\n"
+        )
+
+    return len(wrong_extension)
+
+
+def main():
+    print("\033[32mINFO\033[0m Start check_unused_files.py ...")
+
     exit = 0
 
     dirs_base = next(os.walk("."))[1]
     dirs_base.append(".")
-    dirs_base[:] = [d for d in dirs_base if d not in EXCLUDE_DIRS]
+    dirs_base[:] = filter_paths(dirs_base, Path("."), EXCLUDE_PATHS)
     dirs_base_docs = ("docs",)
-    dirs_base_code = [d for d in dirs_base if d not in dirs_base_docs]
+    dirs_base_code = filter_paths(dirs_base, Path("."), dirs_base_docs)
 
-    # Collectors
-    wrong_extension = ()
+    exit += check_wrong_extensions(".")
+
+    # Collector
     unused_files = ()
 
     # walk over all files
     for root, dirs, files in os.walk("."):
-        dirs = filter_paths(dirs, Path(root), EXCLUDE_PATHS, EXCLUDE_DIRS)
-        files = filter_paths(
-            files, Path(root), EXCLUDE_PATHS, EXCLUDE_DIRS, EXCLUDE_FILES
-        )
+        dirs[:] = filter_paths(dirs, Path(root), EXCLUDE_PATHS)
+        files = filter_paths(files, Path(root), EXCLUDE_PATHS, EXCLUDE_FILES)
 
         # Skip base-directory
         if str(Path(root)) == ".":
@@ -188,30 +207,14 @@ def main():
             # print empty to start a new line
             print("")
 
-        # Skip "white-paper-figures"
-        # TODO Find a more elegant way
-        if str(root).find("white_papers/figures") != -1:
-            processed_files += count_files(root)
-            continue
-
-        # Skip "DD4hep-tests" since their cmake looks a bit different
-        # TODO Find a more elegant way
-        if str(root).find("Tests/UnitTests/Plugins/DD4hep") != -1:
-            processed_files += count_files(root)
-            continue
-
         root = Path(root)
         for filename in files:
             processed_files += 1
             # get the full path of the file
             filepath = root / filename
 
-            # Check for wrong extensions
-            if filepath.suffix not in suffix_allowed:
-                wrong_extension += (str(filepath),)
-
             # Check header files and remove
-            elif filepath.suffix in SUFFIX_HEADER + SUFFIX_SOURCE:
+            if filepath.suffix in SUFFIX_CPP:
                 if file_can_be_removed(filename, dirs_base_code):
                     unused_files += (str(filepath),)
                     remove_cmd = "rm " + str(filepath)
@@ -254,20 +257,6 @@ def main():
             % ("=" * progress, processed_files, number_files, current_base_dir)
         )
         sys.stdout.flush()
-
-    if len(wrong_extension) != 0:
-        print(
-            "\n\n\033[31mERROR\033[0m "
-            + f"The following {len(wrong_extension)} files have an unsupported extension:\n\n"
-            + "\033[31m"
-            + "\n".join(wrong_extension)
-            + "\033[0m"
-            + "\nCheck if you can change the format to one of the following:\n"
-            + "\n".join(suffix_allowed)
-            + "\nIf you really need that specific extension, add it to the list above.\n"
-        )
-
-        exit += 1
 
     if len(unused_files) != 0:
         print(
