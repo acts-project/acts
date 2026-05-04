b88dd2e4ad2c09a45cb3162fab5154d66d9b4f52
Joana Niermann <joana.niermann@cern.ch>
2026-05-04 15:14:34 +0200

    FIx some lincences

--- Detray/core/include/detray/utils/quiet_log_start.hpp
+++ Detray/core/include/detray/utils/quiet_log_start.hpp
@@ -6,14 +6,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-/**
- * DETRAY library, part of the ACTS project (R&D line)
- *
- * (c) 2026 CERN for the benefit of the ACTS project
- *
- * Mozilla Public License Version 2.0
- */
-
 // Make sure the logging macros are available
 #include "detray/utils/logging.hpp"
 
