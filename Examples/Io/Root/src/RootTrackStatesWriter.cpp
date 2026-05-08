a35bac67f2f0dc51ed232e83fc93287ea78997bf
Doga Elitez <108287101+delitez@users.noreply.github.com>
2026-05-08 14:41:37 +0200

    remove deduplicated check

--- Examples/Io/Root/src/RootTrackStatesWriter.cpp
+++ Examples/Io/Root/src/RootTrackStatesWriter.cpp
@@ -409,9 +409,8 @@ ProcessCode RootTrackStatesWriter::writeT(const AlgorithmContext& ctx,
       } else {
         // get the truth hits corresponding to this trackState
         // Use average truth in the case of multiple contributing sim hits
-        if (!state.hasUncalibratedSourceLink() ||
-            state.getUncalibratedSourceLink()
-                    .template getPtr<IndexSourceLink>() == nullptr) {
+        if (!state.getUncalibratedSourceLink()
+                 .template getPtr<IndexSourceLink>()) {
           continue;
         }
         const auto sl =
