83814c68e98ab960d9419c4f9b586636bd1c0fa2
Joana Niermann <joana.niermann@cern.ch>
2026-05-15 15:28:31 +0200

    Remove some leftover copyright statements

--- Detray/core/include/detray/definitions/detail/macros.hpp
+++ Detray/core/include/detray/definitions/detail/macros.hpp
@@ -6,14 +6,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-/**
- * DETRAY library, part of the ACTS project (R&D line)
- *
- * (c) 2024 CERN for the benefit of the ACTS project
- *
- * Mozilla Public License Version 2.0
- */
-
 #pragma once
 
 // Define a macro that assures there is no device compilation
