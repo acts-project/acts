e9cb5e6158388a50b50666bdccb3d4f8370b0e27
jpreston-cern <jasper.preston@cern.ch>
2026-05-19 13:07:00 +0100

    Update Core/include/Acts/Seeding2/GraphBasedTrackSeeder.hpp
    apply suggestions
    
    Co-authored-by: Andreas Stefl <stefl.andreas@gmail.com>

--- Core/include/Acts/Seeding2/GraphBasedTrackSeeder.hpp
+++ Core/include/Acts/Seeding2/GraphBasedTrackSeeder.hpp
@@ -95,10 +95,10 @@ class GraphBasedTrackSeeder {
     float cutDCurvMax = 0.001f;
     /// Minimum z0 value, set as optionl as if in pixel mode,
     /// The value is picked from the ROI
-    float minZ0;
+    float minZ0 = -600;
     /// Minimum z0 value, set as optionl as if in pixel mode,
     /// The value is picked from the ROI
-    float maxZ0;
+    float maxZ0 = 600;
     /// When old tunings are used, this defines the minimum phi window used
     float minDeltaPhi = 0.001f;
     /// Maximum radius of pixel detector
