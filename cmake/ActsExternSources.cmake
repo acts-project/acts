e241a8238b0d74e28117ecfd1589ebc29018a138
Maximilian Goblirsch-Kolb <goblirsc@cern.ch>
2026-05-21 10:55:23 +0200

    Revert "push Mille version (now on SPack)"
    This reverts commit 4c5e5a989cae03b9de886836304cb9fa18a69604.

--- cmake/ActsExternSources.cmake
+++ cmake/ActsExternSources.cmake
@@ -43,7 +43,7 @@ mark_as_advanced(ACTS_NLOHMANN_JSON_SOURCE)
 # translate version string to the historical Mille release naming convention
 string(REPLACE "." "-" _acts_mille_release_string ${_acts_mille_version})
 set(ACTS_MILLE_SOURCE
-    "URL;https://gitlab.desy.de/millepede/mille/-/archive/V${_acts_mille_release_string}/mille-V${_acts_mille_release_string}.tar.gz;URL_HASH;SHA256=bb232672003a8f13f848635e49a261acb79de26634e4ba76347358f209b5de05"
+    "URL;https://gitlab.desy.de/millepede/mille/-/archive/V${_acts_mille_release_string}/mille-V${_acts_mille_release_string}.tar.gz;URL_HASH;SHA256=ae4bf37de8d835aa8adc2960bb795a2080233a4c8af3d4b55adf395e20df0f3e"
     CACHE STRING
     "Source to take Mille from"
 )
