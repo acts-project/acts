5f7ba1256f076544b18f347d784bb1c8ef82e054
Neza Ribaric <neza.ribaric@cern.ch>
2026-05-21 16:42:44 +0200

    format tidy

--- Python/Plugins/src/Traccc.cpp
+++ Python/Plugins/src/Traccc.cpp
@@ -17,36 +17,32 @@
 #if defined(ACTS_ENABLE_CUDA)
 #include <traccc/cuda/utils/stream.hpp>
 
-void bind_traccc_cuda(py::module_& m){
-    py::class_<traccc::cuda::stream,
-               std::shared_ptr<traccc::cuda::stream>>(m, "CudaStream")
-        .def(py::init<>());
-
+void bind_traccc_cuda(py::module_& m) {
+  py::class_<traccc::cuda::stream, std::shared_ptr<traccc::cuda::stream>>(
+      m, "CudaStream")
+      .def(py::init<>());
 }
 #endif
 
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-void bind_traccc_host(py::module_& m){
-    // ---- traccc types ----
-    py::class_<traccc::memory_resource,
-               std::shared_ptr<traccc::memory_resource>>(m,
-                                                         "TracccMemoryResource")
-        .def(py::init([](vecmem::memory_resource& main,
-                         vecmem::memory_resource* host) {
-               return std::make_shared<traccc::memory_resource>(main, host);
-             }),
-             py::arg("main"), py::arg("host") = nullptr,
-             py::keep_alive<1, 2>(),   // keep main alive
-             py::keep_alive<1, 3>());  // keep host alive
+void bind_traccc_host(py::module_& m) {
+  // ---- traccc types ----
+  py::class_<traccc::memory_resource, std::shared_ptr<traccc::memory_resource>>(
+      m, "TracccMemoryResource")
+      .def(py::init([](vecmem::memory_resource& main,
+                       vecmem::memory_resource* host) {
+             return std::make_shared<traccc::memory_resource>(main, host);
+           }),
+           py::arg("main"), py::arg("host") = nullptr,
+           py::keep_alive<1, 2>(),   // keep main alive
+           py::keep_alive<1, 3>());  // keep host alive
 }
 
 PYBIND11_MODULE(ActsPluginsPythonBindingsTraccc, traccc) {
-
-    bind_traccc_host(traccc);
+  bind_traccc_host(traccc);
 #if defined(ACTS_ENABLE_CUDA)
-    bind_traccc_cuda(traccc);
+  bind_traccc_cuda(traccc);
 #endif
-    
 }
