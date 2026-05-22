2f7fa6aaf11c80c6cccb407121b002d815766b4f
Christian Wessel <christian.wessel@desy.de>
2026-05-22 16:32:31 +0200

    Introduce GBL as a new plugin for ACTS.

--- cmake/ActsExternSources.cmake
+++ cmake/ActsExternSources.cmake
@@ -38,6 +38,7 @@ set(ACTS_NLOHMANNJSON_SOURCE
     CACHE STRING
     "Source to take nlohmann_json from"
 )
+mark_as_advanced(ACTS_NLOHMANN_JSON_SOURCE)
 
 # translate version string to the historical Mille release naming convention
 string(REPLACE "." "-" _acts_mille_release_string ${_acts_mille_version})
@@ -46,7 +47,16 @@ set(ACTS_MILLE_SOURCE
     CACHE STRING
     "Source to take Mille from"
 )
-mark_as_advanced(ACTS_NLOHMANN_JSON_SOURCE)
+mark_as_advanced(ACTS_MILLE_SOURCE)
+
+# translate version string to the historical GBL release naming convention
+string(REPLACE "." "-" _acts_gbl_release_string ${_acts_gbl_version})
+set(ACTS_GBL_SOURCE
+    "URL;https://gitlab.desy.de/millepede/general-broken-lines/-/archive/V${_acts_gbl_release_string}/general-broken-lines-V${_acts_gbl_release_string}.tar.gz;URL_HASH;SHA256=e5361f8f3862f1567da43c965065f21b8ef7987568e65a50b91b300b40386af3"
+    CACHE STRING
+    "Source to take General Broken Lines (GBL) from"
+)
+mark_as_advanced(ACTS_GBL_SOURCE)
 
 set(ACTS_EIGEN3_SOURCE
     "URL;https://gitlab.com/libeigen/eigen/-/archive/${_acts_eigen3_version}/${_acts_eigen3_version}.tar.gz;URL_HASH;SHA256=ba6ef66ba2d319e0a871a267889411c550d4bdf5bc7c62f86c60276913f3f4ba"
