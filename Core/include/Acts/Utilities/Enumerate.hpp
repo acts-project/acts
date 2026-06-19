16ad16c7be706968e1d6cfa0f696571be60f25be
Andreas Stefl <stefl.andreas@gmail.com>
2026-06-19 13:54:17 +0200

    pr feedback

--- Core/include/Acts/Utilities/Enumerate.hpp
+++ Core/include/Acts/Utilities/Enumerate.hpp
@@ -10,6 +10,7 @@
 
 #include <cstddef>
 #include <iterator>
+#include <ranges>
 #include <tuple>
 #include <type_traits>
 #include <utility>
@@ -22,21 +23,13 @@ namespace detail {
 /// falling back to `std::size_t` for ranges without a size (e.g. input
 /// ranges).
 template <typename T, typename = void>
-struct enumerate_index_type {
+struct EnumerateIndexType {
   using type = std::size_t;
 };
 template <typename T>
-struct enumerate_index_type<
-    T, std::void_t<decltype(std::size(std::declval<T>()))>> {
+struct EnumerateIndexType<T,
+                          std::void_t<decltype(std::size(std::declval<T>()))>> {
   using type = decltype(std::size(std::declval<T>()));
-};
-
-/// A type is enumerable if it exposes `std::begin` and `std::end` (covers
-/// stl-like containers as well as C-style arrays).
-template <typename T>
-concept enumerable = requires(T &&t) {
-  std::begin(std::forward<T>(t));
-  std::end(std::forward<T>(t));
 };
 
 /// Iterator pairing an index with the element of an underlying iterator.
@@ -45,7 +38,7 @@ concept enumerable = requires(T &&t) {
 /// which is why it cannot model the C++20 `std::forward_iterator` concept (in
 /// C++20 `std::tuple` lacks the `common_reference` support that C++23 adds).
 template <typename index_type, typename iterator_t>
-struct enumerate_iterator {
+struct EnumerateIterator {
   using iterator_category = std::input_iterator_tag;
   using value_type = std::tuple<index_type, std::iter_value_t<iterator_t>>;
   using reference = std::tuple<index_type, std::iter_reference_t<iterator_t>>;
@@ -55,20 +48,20 @@ struct enumerate_iterator {
   index_type i;
   iterator_t iter;
 
-  bool operator==(const enumerate_iterator &rhs) const {
+  bool operator==(const EnumerateIterator &rhs) const {
     return iter == rhs.iter;
   }
 
   /// Pre-increment: advance index and underlying iterator together.
-  enumerate_iterator &operator++() {
+  EnumerateIterator &operator++() {
     ++i;
     ++iter;
     return *this;
   }
 
   /// Post-increment.
-  enumerate_iterator operator++(int) {
-    enumerate_iterator tmp = *this;
+  EnumerateIterator operator++(int) {
+    EnumerateIterator tmp = *this;
     ++*this;
     return tmp;
   }
@@ -76,16 +69,17 @@ struct enumerate_iterator {
   reference operator*() const { return reference{i, *iter}; }
 };
 
-/// Range wrapper that yields `enumerate_iterator`s over a (possibly owned)
+/// Range wrapper that yields `EnumerateIterator`s over a (possibly owned)
 /// container.
-template <typename container_type, typename index_type, typename iterator_t>
-struct enumerate_wrapper {
+template <std::ranges::range container_type, typename index_type,
+          typename iterator_t>
+struct EnumerateWrapper {
   container_type iterable;
   auto begin() {
-    return enumerate_iterator<index_type, iterator_t>{0, std::begin(iterable)};
+    return EnumerateIterator<index_type, iterator_t>{0, std::begin(iterable)};
   }
   auto end() {
-    return enumerate_iterator<index_type, iterator_t>{0, std::end(iterable)};
+    return EnumerateIterator<index_type, iterator_t>{0, std::end(iterable)};
   }
 };
 
@@ -100,14 +94,14 @@ struct enumerate_wrapper {
 /// with 'container' any stl-like container
 /// @param iterable Container to enumerate
 /// @return Enumerable wrapper with index and value pairs
-template <detail::enumerable container_type,
+template <std::ranges::range container_type,
           typename index_type =
-              typename detail::enumerate_index_type<container_type>::type,
+              typename detail::EnumerateIndexType<container_type>::type,
           typename container_type_iter =
               decltype(std::begin(std::declval<container_type>()))>
 constexpr auto enumerate(container_type &&iterable) {
-  return detail::enumerate_wrapper<container_type, index_type,
-                                   container_type_iter>{
+  return detail::EnumerateWrapper<container_type, index_type,
+                                  container_type_iter>{
       std::forward<container_type>(iterable)};
 }
 
