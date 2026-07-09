64949ad99ddbdf968dd7311449c2e80bd1180a8b
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2026-07-09 16:59:20 +0200

    Update

--- CI/changeset.sh
+++ CI/changeset.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-for arg in "$*"; do echo "Arg: $arg\n"; done
+if [ ! -z "$1" ]; then
+  # The base ref is not none, so we are a pull request and we can just find
+  # the changeset between this branch and main.
+  git diff --name-only origin/$1...HEAD > $4
+else
+  # Otherwise we are a push run, so we will compare the before and after.
+  git diff --name-only $2 $3 > $4
+fi
