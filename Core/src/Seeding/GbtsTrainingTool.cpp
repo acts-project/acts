50591988e16a1e369f00aa41776a45fed325d8eb
jasper preston <jasper.preston@cern.ch>
2026-07-13 15:34:58 +0100

    change include

--- Core/src/Seeding/GbtsTrainingTool.cpp
+++ Core/src/Seeding/GbtsTrainingTool.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "Acts/Seeding2/GbtsTrainingTool.hpp"
+#include "Acts/Seeding/GbtsTrainingTool.hpp"
 
 #include <cmath>
 #include <fstream>
