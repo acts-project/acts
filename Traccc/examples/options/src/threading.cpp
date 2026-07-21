472ef69942ff0f469b3600f22f33fa3e5e7dc9f2
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-07-21 17:27:58 +0200

    feat(traccc): control number of events processed concurrently in mt example

--- Traccc/examples/options/src/threading.cpp
+++ Traccc/examples/options/src/threading.cpp
@@ -15,16 +15,33 @@
 
 namespace traccc::opts {
 
+/// Type alias for concurrent slots option
+using concurrent_slots_type = std::size_t;
+/// Name of the concurrent slots option
+static const char* concurrent_slots_option = "concurrent-slots";
+
 threading::threading() : interface("Multi-Threading Options") {
   m_desc.add_options()(
       "cpu-threads",
       boost::program_options::value(&threads)->default_value(threads),
-      "The number of CPU threads to use");
+      "The number of CPU threads to use")(
+      concurrent_slots_option,
+      boost::program_options::value<concurrent_slots_type>(),
+      "The number of events that can be "
+      "processed concurrently, be default equal to cpu-threads");
 }
 
-void threading::read(const boost::program_options::variables_map &) {
+void threading::read(const boost::program_options::variables_map& vm) {
   if (threads == 0) {
     throw std::invalid_argument{"Must use threads>0"};
+  }
+  if (!vm.count(concurrent_slots_option)) {
+    concurrent_slots = threads;
+  } else {
+    concurrent_slots = vm[concurrent_slots_option].as<concurrent_slots_type>();
+    if (concurrent_slots == 0) {
+      throw std::invalid_argument{"Must use concurrent-slots>0"};
+    }
   }
 }
 
@@ -33,7 +50,8 @@ std::unique_ptr<configuration_printable> threading::as_printable() const {
 
   cat->add_child(std::make_unique<configuration_kv_pair>(
       "Number of CPU thread", std::to_string(threads)));
-
+  cat->add_child(std::make_unique<configuration_kv_pair>(
+      "Number of concurrent event slots", std::to_string(concurrent_slots)));
   return cat;
 }
 
