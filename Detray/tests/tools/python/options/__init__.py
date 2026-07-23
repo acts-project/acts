f6b8c1f7546c3ea69922e80cf3cd8fed44d9e275
Vladimir Drobny <vlad.drobn@gmail.com>
2026-07-23 13:18:12 +0200

    feat: Add function to fill DetectorReaderConfig (#5627)

--- Detray/tests/tools/python/options/__init__.py
+++ Detray/tests/tools/python/options/__init__.py
@@ -1,6 +1,10 @@
 from .common_options import common_options, parse_common_options
 from .plotting_options import plotting_options, parse_plotting_options
-from .detector_io_options import detector_io_options, parse_detector_io_options
+from .detector_io_options import (
+    detector_io_options,
+    parse_detector_io_options,
+    fill_reader_config,
+)
 from .propagation_options import propagation_options
 from .track_generator_options import (
     random_track_generator_options,
