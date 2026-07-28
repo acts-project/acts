4d08a0501132a6d34ad090328ed1fba5f29fe924
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-07-28 23:35:00 +0200

    feat(traccc): introduce await customization point for SYCL clustering and seeding

--- Traccc/device/sycl/include/traccc/sycl/clusterization/clusterization_algorithm.hpp
+++ Traccc/device/sycl/include/traccc/sycl/clusterization/clusterization_algorithm.hpp
@@ -9,6 +9,7 @@
 
 // SYCL library include(s).
 #include "traccc/sycl/utils/algorithm_base.hpp"
+#include "traccc/sycl/utils/await.hpp"
 
 // Project include(s).
 #include "traccc/clusterization/device/clusterization_algorithm.hpp"
@@ -34,12 +35,19 @@ class clusterization_algorithm : public device::clusterization_algorithm,
   /// @param queue is a wrapper for the for the sycl queue for kernel
   ///              invocation
   /// @param config the clustering configuration
+  /// @param logger The logger instance to use for messaging
+  /// @param await_func The function used to synchronize events
+  ///
   clusterization_algorithm(
       const traccc::memory_resource& mr, const vecmem::copy& copy,
       queue_wrapper& queue, const config_type& config,
-      std::unique_ptr<const Logger> logger = getDummyLogger().clone());
+      std::unique_ptr<const Logger> logger = getDummyLogger().clone(),
+      await_function_type await_func = await_sync_event);
 
  private:
+  /// The function used to synchronize events.
+  await_function_type m_await_func;
+
   /// @name Function(s) inherited from the base class
   /// @{
 
@@ -78,6 +86,11 @@ class clusterization_algorithm : public device::clusterization_algorithm,
       edm::silicon_cluster_collection::view& cluster_data,
       const vecmem::data::vector_view<const unsigned int>& permutation_map_view)
       const override;
+
+  /// Synchronize an event related to asynchronous operations
+  /// @param event The event to synchronize
+  ///
+  void await(vecmem::abstract_event& event) const override;
 
   /// @}
 
--- Traccc/device/sycl/include/traccc/sycl/seeding/silicon_pixel_spacepoint_formation_algorithm.hpp
+++ Traccc/device/sycl/include/traccc/sycl/seeding/silicon_pixel_spacepoint_formation_algorithm.hpp
@@ -9,6 +9,7 @@
 
 // Local include(s).
 #include "traccc/sycl/utils/algorithm_base.hpp"
+#include "traccc/sycl/utils/await.hpp"
 
 // Project include(s).
 #include "traccc/seeding/device/silicon_pixel_spacepoint_formation_algorithm.hpp"
@@ -31,13 +32,20 @@ class silicon_pixel_spacepoint_formation_algorithm
   ///             and host memory blocks
   /// @param queue The SYCL queue to use
   /// @param logger The logger instance to use
+  /// @param await_func The function used to synchronize events
   ///
   silicon_pixel_spacepoint_formation_algorithm(
       const traccc::memory_resource& mr, const vecmem::copy& copy,
       queue_wrapper& queue,
-      std::unique_ptr<const Logger> logger = getDummyLogger().clone());
+      std::unique_ptr<const Logger> logger = getDummyLogger().clone(),
+      await_function_type await_func = await_sync_event);
 
  private:
+  /// Synchronize an event related to asynchronous operations
+  /// @param event The event to synchronize
+  ///
+  await_function_type m_await_func;
+
   /// @name Function(s) inherited from
   /// @c traccc::device::silicon_pixel_spacepoint_formation_algorithm
   /// @{
@@ -48,6 +56,9 @@ class silicon_pixel_spacepoint_formation_algorithm
   ///
   void form_spacepoints_kernel(
       const form_spacepoints_kernel_payload& payload) const override;
+
+  /// Synchronize event
+  void await(vecmem::abstract_event& event) const override;
 
   /// @}
 
