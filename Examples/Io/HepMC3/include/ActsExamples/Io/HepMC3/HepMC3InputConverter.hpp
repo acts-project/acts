f81563ecdbbbb3b0270a90d3adfc593af2e87d88
Fabrizio <fabrizio.grosa@cern.ch>
2026-07-28 13:44:44 +0200

    Update Examples/Io/HepMC3/include/ActsExamples/Io/HepMC3/HepMC3InputConverter.hpp
    Co-authored-by: Andreas Stefl <stefl.andreas@gmail.com>

--- Examples/Io/HepMC3/include/ActsExamples/Io/HepMC3/HepMC3InputConverter.hpp
+++ Examples/Io/HepMC3/include/ActsExamples/Io/HepMC3/HepMC3InputConverter.hpp
@@ -69,7 +69,7 @@ class HepMC3InputConverter : public IAlgorithm {
   void convertHepMC3ToInternalEdm(const AlgorithmContext& ctx,
                                   const HepMC3::GenEvent& genEvent) const;
 
-  ActsFatras::HeavyFlavourOrigin deriveHeavyFlavourOrigin(
+  HeavyFlavourOrigin deriveHeavyFlavourOrigin(
       const std::shared_ptr<const HepMC3::GenParticle>& particleToCheck) const;
 
   void handleVertex(const HepMC3::GenVertex& genVertex, SimVertex& vertex,
