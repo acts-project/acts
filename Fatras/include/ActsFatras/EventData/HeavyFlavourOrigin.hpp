771d5cea4a8c58edb96753834fd275d2ed9ab356
Fabrizio <fabrizio.grosa@cern.ch>
2026-07-28 09:41:46 +0200

    Update Fatras/include/ActsFatras/EventData/HeavyFlavourOrigin.hpp
    Co-authored-by: Andreas Stefl <stefl.andreas@gmail.com>

--- Fatras/include/ActsFatras/EventData/HeavyFlavourOrigin.hpp
+++ Fatras/include/ActsFatras/EventData/HeavyFlavourOrigin.hpp
@@ -15,17 +15,17 @@ namespace ActsFatras {
 
 /// Encodes hadron heavy-flavour origin classification
 /// the enum values correspond to the quark flavour numbering scheme (from PDG)
-/// Bottom: decay product of a bottomed hadron (it can also be B->D->X),
-///         or particle originate in the fragmentation of a bottom quark
-///         depending on the searchUpToHfQuark flag
-/// Charm: decay product of a charmed hadron, or particle originate
-///        in the fragmentation of a charm quark depending
-///        on the searchUpToHfQuark flag
+enum class HeavyFlavourOrigin : std::uint8_t {
 /// None: no heavy-flavour origin, depending on the criterion regulated
 ///       by the searchUpToHfQuark flag
-enum class HeavyFlavourOrigin : std::uint8_t {
   None = 0,
+  /// Charm: decay product of a charmed hadron, or particle originate
+///        in the fragmentation of a charm quark depending
+///        on the searchUpToHfQuark flag
   Charm = 4,
+  /// Bottom: decay product of a bottomed hadron (it can also be B->D->X),
+///         or particle originate in the fragmentation of a bottom quark
+///         depending on the searchUpToHfQuark flag
   Bottom = 5
 };
 
