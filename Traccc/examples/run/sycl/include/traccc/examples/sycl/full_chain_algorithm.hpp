121789c15c2f80b1f8b75d87d153d280cc72d335
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-07-30 16:32:20 +0200

    feat(traccc): add TBB callback for sycl

--- Traccc/examples/run/sycl/include/traccc/examples/sycl/full_chain_algorithm.hpp
+++ Traccc/examples/run/sycl/include/traccc/examples/sycl/full_chain_algorithm.hpp
@@ -27,6 +27,9 @@
 #include "traccc/utils/algorithm.hpp"
 #include "traccc/utils/propagation.hpp"
 
+// Local includes(s).
+#include "traccc/examples/await_strategy.hpp"
+
 // VecMem include(s).
 #include <vecmem/memory/binary_page_memory_resource.hpp>
 #include <vecmem/memory/memory_resource.hpp>
@@ -85,7 +88,8 @@ class full_chain_algorithm
       const detector_design_description::host& det_descr,
       const detector_conditions_description::host& det_cond,
       const magnetic_field& field, host_detector* detector,
-      std::unique_ptr<const traccc::Logger> logger, bool useGBTS = false);
+      std::unique_ptr<const traccc::Logger> logger, bool useGBTS = false,
+      await_strategy await_mode = await_strategy::sync_event);
 
   /// Copy constructor
   ///
@@ -131,6 +135,8 @@ class full_chain_algorithm
   mutable vecmem::binary_page_memory_resource m_cached_device_mr;
   /// Memory copy object
   mutable vecmem::sycl::async_copy m_copy;
+  /// The function for awaiting asynchronous operations
+  await_function_type m_await_function;
 
   /// Constant B field for the (seed) track parameter estimation
   traccc::vector3 m_field_vec;
