4881447b32dad2a86f57502811a1af7b81fa4f17
yannikaMatt <yannikamatt@gmail.com>
2026-08-11 15:06:26 +0200

    Update Core/src/Visualization/ViewConfig.cpp
    Co-authored-by: Paul Gessinger <hello@paulgessinger.com>

--- Core/src/Visualization/ViewConfig.cpp
+++ Core/src/Visualization/ViewConfig.cpp
@@ -14,7 +14,6 @@ ViewConfig defaultGeometryColoring(const GeometryObject& geoObj) {
         return ViewConfig{.color = {255, 165, 0}};
     }
   
-    //std::cout << geoObj.geometryId() << std::endl;
     if (geoObj.geometryId().sensitive() != 0)
     {
         return ViewConfig{.color = {0, 255, 0}};
