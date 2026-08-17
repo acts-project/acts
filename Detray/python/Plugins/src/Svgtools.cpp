6955aa2f46574c529dcf0c47652db491ec1baba8
Vladimir Drobny <vlad.drobn@gmail.com>
2026-08-17 14:19:46 +0200

    feat: Bind svgtools views and styling (#5627)
    Views are bound opaquely, since defaults are all the script needs.
    styling::style is a nested struct. Rather than exposing all structs,
    this binding just exposes properties, whose setters write the same value
    to all of them.

--- Detray/python/Plugins/src/Svgtools.cpp
+++ Detray/python/Plugins/src/Svgtools.cpp
@@ -6,13 +6,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Detray plugin include(s)
+// Detray include(s)
+#include "detray/algebra/concepts.hpp"
+#include "detray/plugins/svgtools/styling/styling.hpp"
 #include "detray/plugins/svgtools/writer.hpp"
 
 // Actsvg include(s)
+#include <actsvg/core/defs.hpp>
 #include <actsvg/core/draw.hpp>
 #include <actsvg/core/style.hpp>
 #include <actsvg/core/svg.hpp>
+#include <actsvg/core/views.hpp>
 
 // Pybind11 include(s)
 #include <pybind11/pybind11.h>
@@ -24,6 +28,12 @@
 #include <vector>
 
 namespace py = pybind11;
+
+namespace {
+
+using style_t = detray::svgtools::styling::style;
+
+}  // namespace
 
 PYBIND11_MODULE(DetraySvgtoolsPythonBindings, m) {
   m.doc() = "Detray svgtools bindings";
@@ -62,5 +72,57 @@ PYBIND11_MODULE(DetraySvgtoolsPythonBindings, m) {
         py::arg("name"), py::arg("xRange"), py::arg("yRange"),
         py::arg("xLabel") = "", py::arg("yLabel") = "",
         py::arg("fontSize") = 12u, "Draw a pair of x-y axes");
+  }
+
+  // Svg views
+  {
+    py::class_<actsvg::views::x_y>(m, "ViewXY", py::module_local())
+        .def(py::init<>(), "Transverse view");
+    py::class_<actsvg::views::z_r>(m, "ViewZR", py::module_local())
+        .def(py::init<>(), "Longitudinal view");
+    py::class_<actsvg::views::z_phi>(m, "ViewZPhi", py::module_local())
+        .def(py::init<>(), "Unrolled cylinder view");
+    py::class_<actsvg::views::z_rphi>(m, "ViewZRPhi", py::module_local())
+        .def(py::init<>(), "Unrolled cylinder view at a fixed radius");
+  }
+
+  // Svg styling
+  {
+    py::class_<style_t>(m, "Style")
+        .def_property(
+            "fontSize",
+            [](const style_t &s) {
+              return s._detector_style._volume_style._sensitive_surface_style
+                  ._font_size;
+            },
+            [](style_t &s, unsigned int font_size) {
+              auto &vol_style = s._detector_style._volume_style;
+              vol_style._sensitive_surface_style._font_size = font_size;
+              vol_style._passive_surface_style._font_size = font_size;
+              vol_style._portal_style._surface_style._font_size = font_size;
+              s._eta_lines_style._font_size = font_size;
+            },
+            "Font size of all surface and eta line labels")
+        .def_property(
+            "materialGradientPos",
+            [](const style_t &s) {
+              return s._detector_style._volume_style._sensitive_surface_style
+                  ._material_style._gradient_pos;
+            },
+            [](style_t &s, const actsvg::point2 &pos) {
+              auto &vol_style = s._detector_style._volume_style;
+              vol_style._sensitive_surface_style._material_style._gradient_pos =
+                  pos;
+              vol_style._passive_surface_style._material_style._gradient_pos =
+                  pos;
+              vol_style._portal_style._surface_style._material_style
+                  ._gradient_pos = pos;
+            },
+            "Position of the material gradient box in all surface styles");
+
+    m.def(
+        "tableauColorblindStyle",
+        []() { return detray::svgtools::styling::tableau_colorblind::style; },
+        "Style that matches the colors used in the data plotting");
   }
 }
