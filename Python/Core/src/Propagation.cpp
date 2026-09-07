e4f33e0d647cf4d322e305a4871fc97b9bc88304
Anton Taleiko <56361643+H4m5t3r@users.noreply.github.com>
2026-09-07 09:38:24 +0200

    Update Python/Core/src/Propagation.cpp
    Co-authored-by: Paul Gessinger <hello@paulgessinger.com>

--- Python/Core/src/Propagation.cpp
+++ Python/Core/src/Propagation.cpp
@@ -43,7 +43,7 @@ BoundTrackParameters unwrapPropagateToSurface(
     Result<BoundTrackParameters> res) {
   if (!res.ok()) {
     std::stringstream ss;
-    ss << "Propagation to surface failed with error: \"" << res.error() << "\"";
+    ss << "Propagation to surface failed with error: \"" << res.message() << "\"";
     throw std::runtime_error{ss.str()};
   }
   return *res;
