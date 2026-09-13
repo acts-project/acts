1f01c61f14eb0daa2b0ecc5efc6acebf09dc0d01
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2026-09-13 15:46:32 +0200

    Add JSON material readre

---
+++ Plugins/GeoModel/include/ActsPlugins/GeoModel/GeoModelJsonMaterialManager.hpp
@@ -0,0 +1,77 @@
+
+
+#include "GeoModelHelpers/MaterialManager.h"
+
+#include "Acts/Utilities/Logger.hpp"
+
+namespace ActsPlugins {
+
+    /// GeoModelMaterialManager implementation reading a deserialized JSON file as database. 
+    /// The dictionary holds the table of the chemical elements and of the macroscopic materials.
+    /// The first one is encoded in the "periodicTable" block which is a list of dictionaries
+    ///
+    /// {
+    ///         "periodicTable" :  [
+    ///                 { "name" : "Hydrogen", "symbol" : "H", "charge" : 1, "atomicNumber" : 1.2 },
+    ///                 ....
+    ///         ]
+    ///
+    ///    where 'charge' is the number of protons in the nuclei, and the "atomicNumber" the 
+    ///    average number of nucleons. The 'name' field identifies the element uniquely in the
+    ///    database for later useage.
+    ///
+    ///  The actual materials used for the GeoModel tree are defined in the section "materials"
+    ///
+    ///          "materials": [
+    ///                  {"name" : "air" ,  "rho" : 0.001290,  
+    ///                   "composition": [
+    ///                                    {"component" : "Nitrogen", "parts" :  0.7494},
+    ///                                    {"component" : "Oxygen", "parts" : 0.2369},
+    ///                                    {"component" : "Argon", "parts" : 0.0129},
+    ///                                    {"component" : "Hydrogen", "parts" : 0.0008}
+    ///                                   ]},
+    ///                 {"name" : "CO2", "rho": 0.00184,
+    ///                   "composition" : [{"component" : "Carbon",  "parts": 1.},
+    ///                                    {"component"  : "Oxygen", "parts": 2.} ]},
+    ///                 {"name" : "ArCO2", "rho" : 0.0054, "namespace" : "Muon", 
+    ///                     "composition" : [{"component" : "Argon", "parts" : 0.93},
+    ///                                      {"component" : "std::CO2", 0.7}]}
+    ///          ]
+    ///     Each entry defines a material which can then be used to define the GeoLogVol
+    ///     "name": is the Identifier of the material which is combined with the current namespace
+    ///     state. By default the namespace is always "std" but can be changed to anything using
+    ///     the "namepace" key in the dictionary. The Material can then later be retrieved from 
+    ///     the Manager via
+    ///                 auto arCo2 = MaterialManager::getManager()->getMaterial("Muon::ArCO2");
+    ///     
+    ///     "rho" defines the material's density and "composition" lists all the chemical elements
+    ///      or other composed materials out of which the material is made of. Each "component"
+    ///      is associated with "parts" which defines the relative fractional component. Note:
+    ///      if the parts violate unity, the Manager automatically re-normalizes the parts
+    ///
+    ///
+    class GeoModelJsonMaterialManager: public MaterialManager {
+        public:
+            /// Use the singelton access pattern to instantiate the GeoModelJsonMaterialManager
+            /// the inherited class shares the same singelton pointer as the parent. In case
+            /// of an uninstantiated singelton, the GeoModelJsonMaterialManager is instantiated.
+            /// The manager pointer is dynmically casted and then returned. 
+            /// @return Return the pointer to the `GeoModelJsonMaterialManager` instance
+            /// which may be a nullptr if another implementation of the MaterialManager was
+            /// called before
+            static GeoModelJsonMaterialManager* getManager();
+
+            bool loadMaterialMap(const std::string& filePath);
+
+
+        protected:
+            GeoModelJsonMaterialManager() = default;
+        private:
+            std::unique_ptr<const Acts::Logger> m_logger{Acts::getDefaultLogger("GeoModelMaterialMgr", Acts::Logging::Level::INFO)};
+
+            const Acts::Logger& logger() const { return *m_logger; }
+
+
+
+    };
+}
