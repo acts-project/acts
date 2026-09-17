996b8d96c6d233f2bb6913a69c3479dd5fd38a38
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2026-09-17 20:09:47 +0200

    fix(traccc): Remove infinite loop in p-value computation
    The p-value computation function we use uses a convergence limit
    designed for use with double-precision floats. For single-precision use,
    this is obviously wrong and can lead to an infinite loop. This commit
    sets the convergence threshold to machine epsilon and adds a maximum
    iteration counter.

--- Traccc/core/include/traccc/utils/prob.hpp
+++ Traccc/core/include/traccc/utils/prob.hpp
@@ -14,6 +14,9 @@
 #include "traccc/definitions/math.hpp"
 #include "traccc/definitions/qualifiers.hpp"
 
+// System include(s).
+#include <limits>
+
 namespace traccc {
 
 /* Logarithm of gamma function */
@@ -79,7 +82,8 @@ struct log_gamma {
   }
 
   static constexpr scalar_t kMAXLGM = static_cast<scalar_t>(2.556348e305);
-  static constexpr scalar_t kMACHEP = 1.11022302462515654042363166809e-16f;
+  static constexpr scalar_t kMACHEP = std::numeric_limits<scalar_t>::epsilon();
+  static constexpr unsigned int kMaxIterations = 1000u;
   static constexpr scalar_t kMAXLOG = 709.782712893383973096206318587f;
   static constexpr scalar_t kBig = 4.503599627370496e15f;
   static constexpr scalar_t kBiginv = 2.22044604925031308085e-16f;
@@ -205,11 +209,13 @@ TRACCC_HOST_DEVICE inline scalar_t igam_impl(const scalar_t a,
   c = 1.0f;
   ans = 1.0f;
 
+  unsigned int n_iter = 0u;
   do {
     r += 1.0f;
     c *= x / r;
     ans += c;
-  } while (c / ans > log_gamma<scalar_t>::kMACHEP);
+  } while (c / ans > log_gamma<scalar_t>::kMACHEP &&
+           ++n_iter < log_gamma<scalar_t>::kMaxIterations);
 
   return (ans * ax / a);
 }
@@ -255,6 +261,7 @@ TRACCC_HOST_DEVICE inline scalar_t igamc_impl(const scalar_t a,
   qkm1 = z * x;
   ans = pkm1 / qkm1;
 
+  unsigned int n_iter = 0u;
   do {
     c += 1.0f;
     y += 1.0f;
@@ -279,7 +286,8 @@ TRACCC_HOST_DEVICE inline scalar_t igamc_impl(const scalar_t a,
       qkm2 *= log_gamma<scalar_t>::kBiginv;
       qkm1 *= log_gamma<scalar_t>::kBiginv;
     }
-  } while (t > log_gamma<scalar_t>::kMACHEP);
+  } while (t > log_gamma<scalar_t>::kMACHEP &&
+           ++n_iter < log_gamma<scalar_t>::kMaxIterations);
 
   return (ans * ax);
 }
