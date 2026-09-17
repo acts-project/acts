4e1dbcb2b8b9521baa0cba53d3634b2f6a4284ca
Dimitra Amperiadou <damperia@atlmuonsw1.dyndns.cern.ch>
2026-09-17 12:55:34 +0200

    refactor  multilayer grid based navigation for staggering

--- Tests/UnitTests/Core/Navigation/MultiWireNavigationTests.cpp
+++ Tests/UnitTests/Core/Navigation/MultiWireNavigationTests.cpp
@@ -164,6 +164,7 @@ BOOST_AUTO_TEST_CASE(MultiLayer_NavigationPolicy) {
   double angle = std::numbers::pi / 4.;
   startDir = {std::cos(angle), std::sin(angle), 0.};
   args.direction = startDir;
+
   // clear the candidates and re initialize with new arguments
   main.reset();
   NavigationPolicyStateManager stateManager2;
